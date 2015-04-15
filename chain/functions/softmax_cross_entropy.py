a8ded450b2f9a4cf4d85923106d00e67abaf9ea0
Seiya Tokui <beam.web@gmail.com>
2015-04-15 11:15:17 +0900

    Implement GPU softmax cross entropy

--- chain/functions/softmax_cross_entropy.py
+++ chain/functions/softmax_cross_entropy.py
@@ -1,26 +1,65 @@
 import numpy
 import pycuda.gpuarray as gpuarray
-from chain import Function
-from softmax import Softmax
+from pycuda.elementwise import ElementwiseKernel
+from pycuda.reduction import ReductionKernel
+from pytools import memoize
+
+from chain import Function, cudnn
+from chain.functions.softmax import Softmax
+
+@memoize
+def _get_cross_entropy_kernel():
+    return ReductionKernel(
+        numpy.float32,
+        arguments='int* t, float* y, int n_channel',
+        map_expr='-log(y[i * n_channel + t[i]])',
+        neutral='0', reduce_expr='a+b')
+
+def _cross_entropy_gpu(y, t):
+    assert y.dtype == numpy.dtype('float32')  # TODO(beam2d): Support double
+    assert t.dtype == numpy.dtype('int32')
+    kern = _get_cross_entropy_kernel()
+    return kern(t, y, y.shape[1])
+
+@memoize
+def _get_backward_kernel():
+    return ElementwiseKernel(
+        'float* gx, float* y, int* t, float* gloss, int n_channel',
+        'gx[i] = *gloss * (y[i] - ((i % n_channel) == t[i / n_channel]))')
+
+def _backward_gpu(gx, y, t, gloss):
+    kern = _get_backward_kernel()
+    return kern(gx, y, t, gloss, y.shape[1])
 
 class SoftmaxCrossEntropy(Function):
     """Softmax activation followed by a cross entropy loss."""
 
     def forward_cpu(self, inputs):
         x, t = inputs
-        self.y = x - numpy.amax(x, axis=1, keepdims=True)
-        numpy.exp(self.y, out=self.y)
-        self.y /= self.y.sum(axis=1, keepdims=True)
+        self.y, = Softmax().forward_cpu((x,))
         return -numpy.log(self.y[xrange(len(t)), t]).sum(keepdims=True),
+
+    def forward_gpu(self, inputs):
+        x, t = inputs
+        self.y, = Softmax().forward_gpu((x,))
+        return _cross_entropy_gpu(self.y, t),
 
     def backward_cpu(self, inputs, grad_outputs):
         t, gloss = inputs[1], grad_outputs[0]
-        gy = self.y
-        gy[xrange(len(t)), t] -= 1
-        gy *= gloss[0]
-        return gy, None
+        gx = self.y  # reuse the memory
+        gx[xrange(len(t)), t] -= 1
+        gx *= gloss[0]
+        return gx, None
 
-    # TODO(beam2d): Implement GPU forward/backward
+    def backward_gpu(self, inputs, grad_outputs):
+        t, gloss = inputs[1], grad_outputs[0]
+        print t
+        print self.y
+        gx = self.y  # reuse the memory
+        _backward_gpu(gx, self.y, t, gloss)
+        print gx
+        return gx, None
+
 
 def softmax_cross_entropy(x, t):
     return SoftmaxCrossEntropy()(x, t)
--- tests/functions_tests/test_softmax_cross_entropy.py
+++ tests/functions_tests/test_softmax_cross_entropy.py
@@ -18,22 +18,41 @@ class TestSoftmax(TestCase):
         t = Variable(self.t)
         loss = softmax_cross_entropy(x, t)
 
-        y = numpy.exp(x.data)
+        loss_expect = self._compute_expected_loss()
+        self.assertAlmostEqual(loss_expect, float(loss.data), places=5)
+
+    def test_forward_gpu(self):
+        x = Variable(gpuarray.to_gpu(self.x))
+        t = Variable(gpuarray.to_gpu(self.t))
+        loss = softmax_cross_entropy(x, t)
+
+        loss_expect = self._compute_expected_loss()
+        self.assertAlmostEqual(loss_expect, float(loss.data.get()), places=5)
+
+    def _compute_expected_loss(self):
+        y = numpy.exp(self.x)
         loss_expect = 0
         for i in xrange(y.shape[0]):
-            loss_expect -= math.log(y[i, t.data[i]] / y[i].sum())
-
-        self.assertAlmostEqual(loss_expect, loss.data[0], places=5)
+            loss_expect -= math.log(y[i, self.t[i]] / y[i].sum())
+        return loss_expect
 
     def test_backward_cpu(self):
         x = Variable(self.x)
         t = Variable(self.t)
+        self._check_backward(x, t)
+
+    def test_backward_gpu(self):
+        x = Variable(gpuarray.to_gpu(self.x))
+        t = Variable(gpuarray.to_gpu(self.t))
+        self._check_backward(x, t)
+
+    def _check_backward(self, x, t):
         loss = softmax_cross_entropy(x, t)
         loss.backward()
         self.assertEqual(None, t.grad)
 
         func = loss.creator
         f = lambda: func.forward((x.data, t.data))
-        gx = numerical_grad(f, (x.data,), (1,), eps=1e-2)
+        gx, = numerical_grad(f, (x.data,), (1,), eps=1e-2)
 
         self.assertLess(l_infty_dist(gx, x.grad), 1e-4)
