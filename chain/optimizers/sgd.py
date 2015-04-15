9c2a72a0f1fcf7b3c7ec43a731cfdda8d3cd72fe
Seiya Tokui <beam.web@gmail.com>
2015-04-15 14:20:16 +0900

    Short name

--- chain/optimizers/sgd.py
+++ chain/optimizers/sgd.py
@@ -3,8 +3,8 @@ from chain import Optimizer
 class SGD(Optimizer):
     """Vanilla Stochastic Gradient Descent."""
 
-    def __init__(self, learning_rate=0.01):
-        self.learning_rate = learning_rate
+    def __init__(self, lr=0.01):
+        self.lr = lr
 
     def update_one(self, param, grad, _):
-        param -= self.leraning_rate * grad
+        param -= self.lr * grad
