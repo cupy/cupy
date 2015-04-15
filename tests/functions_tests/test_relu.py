be515ccae182a244c503a4e621a4528109c75c47
Seiya Tokui <beam.web@gmail.com>
2015-04-15 22:51:54 +0900

    Refactor test

--- tests/functions_tests/test_relu.py
+++ tests/functions_tests/test_relu.py
@@ -12,26 +12,23 @@ class TestReLU(TestCase):
         self.x  = numpy.random.uniform(-.5, .5, (3, 2)).astype(numpy.float32)
         self.gy = numpy.random.uniform(-.1, .1, (3, 2)).astype(numpy.float32)
 
-    def test_backward_cpu(self):
-        x = Variable(self.x)
-        y = relu(x)
-        y.grad = self.gy
-        y.backward()
-
+    def check_backward(self, x, y):
         func = y.creator
         f = lambda: func.forward((x.data,))
         gx, = numerical_grad(f, (x.data,), (y.grad,))
 
         self.assertLess(l_infty_dist(gx, x.grad), 1e-5)
+
+    def test_backward_cpu(self):
+        x = Variable(self.x)
+        y = relu(x)
+        y.grad = self.gy
+        y.backward()
+        self.check_backward(x, y)
 
     def test_backward_gpu(self):
         x = Variable(gpuarray.to_gpu(self.x))
         y = relu(x)
         y.grad = gpuarray.to_gpu(self.gy)
         y.backward()
-
-        func = y.creator
-        f = lambda: func.forward((x.data,))
-        gx, = numerical_grad(f, (x.data,), (y.grad,))
-
-        self.assertLess(l_infty_dist(gx, x.grad), 1e-5)
+        self.check_backward(x, y)
