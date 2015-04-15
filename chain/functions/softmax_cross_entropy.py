2556aa93c816ac6bc1c132dad7a6823b2440bf56
Seiya Tokui <beam.web@gmail.com>
2015-04-15 22:49:10 +0900

    Simplify the code

--- chain/functions/softmax_cross_entropy.py
+++ chain/functions/softmax_cross_entropy.py
@@ -8,28 +8,18 @@ from chain import Function, cudnn
 from chain.functions.softmax import Softmax
 
 @memoize
-def _get_cross_entropy_kernel():
+def _cross_entropy_kernel():
     return ReductionKernel(
         numpy.float32,
         arguments='int* t, float* y, int n_channel',
         map_expr='-log(y[i * n_channel + t[i]])',
         neutral='0', reduce_expr='a+b')
 
-def _cross_entropy_gpu(y, t):
-    assert y.dtype == numpy.dtype('float32')  # TODO(beam2d): Support double
-    assert t.dtype == numpy.dtype('int32')
-    kern = _get_cross_entropy_kernel()
-    return kern(t, y, y.shape[1])
-
 @memoize
-def _get_backward_kernel():
+def _backward_kernel():
     return ElementwiseKernel(
         'float* gx, float* y, int* t, float* gloss, int n_channel',
         'gx[i] = *gloss * (y[i] - ((i % n_channel) == t[i / n_channel]))')
-
-def _backward_gpu(gx, y, t, gloss):
-    kern = _get_backward_kernel()
-    return kern(gx, y, t, gloss, y.shape[1])
 
 class SoftmaxCrossEntropy(Function):
     """Softmax activation followed by a cross entropy loss."""
@@ -42,7 +32,7 @@ class SoftmaxCrossEntropy(Function):
     def forward_gpu(self, inputs):
         x, t = inputs
         self.y, = Softmax().forward_gpu((x,))
-        return _cross_entropy_gpu(self.y, t),
+        return _cross_entropy_kernel()(t, self.y, self.y.shape[1]),
 
     def backward_cpu(self, inputs, grad_outputs):
         t, gloss = inputs[1], grad_outputs[0]
@@ -54,7 +44,7 @@ class SoftmaxCrossEntropy(Function):
     def backward_gpu(self, inputs, grad_outputs):
         t, gloss = inputs[1], grad_outputs[0]
         gx = self.y  # reuse the memory
-        _backward_gpu(gx, self.y, t, gloss)
+        _backward_kernel()(gx, self.y, t, gloss, self.y.shape[1])
         return gx, None
 
 
