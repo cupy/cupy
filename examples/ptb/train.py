0f64fe0a3983640fcff346994b6374236c9b0c32
Seiya Tokui <beam.web@gmail.com>
2015-04-19 10:16:16 +0900

    Update sample code

--- examples/ptb/train.py
+++ examples/ptb/train.py
@@ -36,7 +36,7 @@ test_data  = load_data('ptb.test.txt')
 print 'n_vocab =', n_vocab
 
 # Initialize model
-n_unit = 200
+n_unit = 650
 model = FunctionSet(
     embed=EmbedID(n_vocab, n_unit),
     l1_x=Linear(n_unit, 4*n_unit),
@@ -45,6 +45,8 @@ model = FunctionSet(
     l2_h=Linear(n_unit, 4*n_unit),
     l3  =Linear(n_unit, n_vocab)
 )
+for p in model.collect_parameters()[0]:
+    p[:] = np.random.uniform(-.05, .05, p.shape)
 model.to_gpu()
 
 # Setup optimizer
@@ -56,73 +58,98 @@ def forward_one_step(x_data, y_data, state, train=True):
     x = Variable(x_data, volatile=not train)
     t = Variable(y_data, volatile=not train)
     h0     = model.embed(x)
-    h1_in  = model.l1_x(h0) + model.l1_h(state['h1'])
-    c1, h1 = lstm(state['c1'], dropout(h1_in, train=train))
-    h2_in  = model.l2_x(h1) + model.l2_h(state['h2'])
-    c2, h2 = lstm(state['c2'], dropout(h2_in, train=train))
-    y      = model.l3(h2)
+    h1_in  = model.l1_x(dropout(h0, train=train)) + model.l1_h(state['h1'])
+    c1, h1 = lstm(state['c1'], h1_in)
+    h2_in  = model.l2_x(dropout(h1, train=train)) + model.l2_h(state['h2'])
+    c2, h2 = lstm(state['c2'], h2_in)
+    y      = model.l3(dropout(h2, train=train))
     state  = {'c1': c1, 'h1': h1, 'c2': c2, 'h2': h2}
     return state, softmax_cross_entropy(y, t)
 
-def make_initial_state(volatile=False):
+def make_initial_state(batchsize, volatile=False):
     return {name: Variable(gpuarray.zeros((batchsize, n_unit), dtype=np.float32),
                            volatile=volatile)
             for name in ('c1', 'h1', 'c2', 'h2')}
 
 whole_len = train_data.shape[0]
 batchsize = 20
-bpsize    = 20
+bpsize    = 35
 grad_clip = 5
 showloss  = 10000
-def train_one_epoch(state):
+n_epoch   = 39
+def train_whole():
     jump = whole_len / batchsize
     cur_log_perp = 0
-    sum_log_perp = 0
+    epoch   = 0
 
     start_at = time.time()
     cur_at   = start_at
-    for i in xrange(whole_len):
+    print 'epoch', epoch
+
+    state = make_initial_state(batchsize=batchsize)
+    accum_loss = 0
+    for i in xrange(jump * n_epoch):
         x_cpu = np.array([train_data[(jump * j + i) % whole_len]
                           for j in xrange(batchsize)])
         y_cpu = np.array([train_data[(jump * j + i + 1) % whole_len]
                           for j in xrange(batchsize)])
-        state, loss = forward_one_step(
+        state, loss_i = forward_one_step(
             gpuarray.to_gpu(x_cpu), gpuarray.to_gpu(y_cpu), state)
-        log_perp = float(loss.data.get()) / batchsize
-        cur_log_perp += log_perp
-        sum_log_perp += log_perp
+        accum_loss   += loss_i
+        cur_log_perp += float(loss_i.data.get()) / batchsize
 
         if (i + 1) % bpsize == 0:
             optimizer.zero_grads()
-            loss.backward()
-            loss.forget_backward()
+
+            accum_loss /= batchsize
+            accum_loss.backward()
+            accum_loss.forget_backward()
+            accum_loss = 0
+
             optimizer.clip_grads(grad_clip)
             optimizer.update()
 
         if (i + 1) % showloss == 0:
-            perp = math.exp(cur_log_perp / showloss)
-            print '\ncurrent perplexity:', perp
-            cur_log_perp = 0
-
             now = time.time()
-            print 'duration:', datetime.timedelta(seconds = now - cur_at)
+            duration = datetime.timedelta(seconds = now - cur_at)
+            print '\nduration:', duration
             cur_at = now
 
-        sys.stdout.write('\rtrain: {} / {}'.format(i + 1, whole_len))
+            perp = math.exp(cur_log_perp / showloss)
+            print 'iter', i+1, 'training perplexity:', perp
+            cur_log_perp = 0
+
+        if (i + 1) % jump == 0:  # epoch
+            epoch += 1
+
+            print '\nevaluate'
+            perp = evaluate(valid_data)
+            print 'epoch', epoch, 'validation perplexity:', perp
+            to_eval = False
+
+            print 'epoch', epoch
+            if epoch >= 6:
+                optimizer.lr /= 1.2
+                print 'learning rate =', optimizer.lr
+
+        sys.stderr.write('\rtrain: {} / {}'.format(i + 1, jump*n_epoch))
+        sys.stderr.flush()
         sys.stdout.flush()
 
+    print '\ntest'
+    test_perp = evaluate(test_data)
+    print 'test perplexity:', test_perp
     print '\nwhole time:', datetime.timedelta(seconds = time.time() - start_at)
-    return state, sum_log_perp / whole_len
 
 def evaluate(dataset):
     dataset_len = dataset.size
     sum_log_perp = 0
-    state = make_initial_state(volatile=True)
+    state = make_initial_state(batchsize=1, volatile=True)
 
     start_at = time.time()
     for i in xrange(dataset_len - 1):
-        sys.stdout.write('\reval: {} / {}'.format(i, dataset_len - 1))
-        sys.stdout.flush()
+        sys.stderr.write('\reval: {} / {}'.format(i, dataset_len - 1))
+        sys.err.flush()
 
         x_cpu = dataset[i   : i+1]
         y_cpu = dataset[i+1 : i+2]
@@ -131,27 +158,7 @@ def evaluate(dataset):
         sum_log_perp += float(loss.data.get())
 
     print '\nwhole time:', datetime.timedelta(seconds = time.time() - start_at)
-    return sum_log_perp / (dataset_len - 1)
-
-n_epoch = 1
-def train_whole():
-    state = make_initial_state()
-    start_at = time.time()
-    for epoch in xrange(n_epoch):
-        print 'epoch', n_epoch
-        print 'train'
-        state, train_log_perp = train_one_epoch(state)
-        print 'train perplexity:     ', math.exp(train_log_perp)
-        print 'validate'
-        valid_log_perp = evaluate(valid_data)
-        print 'validation perplexity:', math.exp(valid_log_perp)
-
-    print 'test'
-    test_log_perp = evaluate(test_data)
-    print 'test perplexity:      ', math.exp(test_log_perp)
-    print '\nwhole time:', datetime.timedelta(seconds = time.time() - start_at)
+    return math.exp(sum_log_perp / (dataset_len - 1))
 
 train_whole()
-
-with open('model', 'wb') as f:
-    pickle.dump(model, f, -1)
+pickle.dump(model, open('model', 'wb'), -1)
