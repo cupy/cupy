cb1c64681f80824c780bc30ee7ec5462cbdd166b
Seiya Tokui <beam.web@gmail.com>
2015-04-22 11:14:25 +0900

    Support Linear function without bias

--- chainer/functions/linear.py
+++ chainer/functions/linear.py
@@ -15,32 +15,51 @@ def _add_bias_kernel():
 class Linear(Function):
     """Implementation of fully-connected layer."""
 
-    parameter_names = ('W', 'b')
-    gradient_names  = ('gW', 'gb')
-
-    def __init__(self, in_size, out_size, wscale=1, bias=0):
+    def __init__(self, in_size, out_size, wscale=1, bias=0, nobias=False):
         self.W = numpy.random.normal(
             0, wscale * math.sqrt(1. / in_size),
             (out_size, in_size)).astype(numpy.float32)
-        self.b = numpy.repeat(numpy.float32(bias), out_size)
-
         self.gW = numpy.empty_like(self.W)
-        self.gb = numpy.empty_like(self.b)
+
+        if nobias:
+            self.b  = None
+            self.gb = None
+        else:
+            self.b  = numpy.repeat(numpy.float32(bias), out_size)
+            self.gb = numpy.empty_like(self.b)
+
+    @property
+    def parameter_names(self):
+        if self.b is None:
+            return 'W',
+        return 'W', 'b'
+
+    @property
+    def gradient_names(self):
+        if self.gb is None:
+            return 'gW',
+        return 'gW', 'gb'
 
     def forward_cpu(self, x):
-        return x[0].dot(self.W.T) + self.b,
+        Wx = x[0].dot(self.W.T)
+        if self.b is not None:
+            Wx += self.b
+        return Wx,
 
     def forward_gpu(self, x):
         Wx = culinalg.dot(x[0], self.W, transb='T')
-        _add_bias_kernel()(Wx, self.b, self.b.size)
+        if self.b is not None:
+            _add_bias_kernel()(Wx, self.b, self.b.size)
         return Wx,
 
     def backward_cpu(self, x, gy):
         self.gW += gy[0].T.dot(x[0])
-        self.gb += gy[0].sum(0)
+        if self.gb is not None:
+            self.gb += gy[0].sum(0)
         return gy[0].dot(self.W),
 
     def backward_gpu(self, x, gy):
         culinalg.add_dot(gy[0], x[0], self.gW, transa='T')
-        self.gb += cumisc.sum(gy[0], 0)
+        if self.gb is not None:
+            self.gb += cumisc.sum(gy[0], 0)
         return culinalg.dot(gy[0], self.W),
