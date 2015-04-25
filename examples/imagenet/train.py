7c3511749644acdc1070bc43b03f757d6ca7db6a
Seiya Tokui <beam.web@gmail.com>
2015-04-25 19:22:01 +0900

    Let main thread run nn computation

--- examples/imagenet/train.py
+++ examples/imagenet/train.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 import json, cPickle as pickle, Queue, random, sys, threading
 import cv2, numpy as np
-import pycuda.autoinit
-import pycuda.driver as cuda
 from pycuda.gpuarray import to_gpu
 from chainer import Variable, FunctionSet
 import chainer.functions as F
@@ -27,6 +25,118 @@ train_list = load_image_list(train_path)
 val_list   = load_image_list(val_path)
 
 mean_image = pickle.load(open('mean.npy', 'rb'))
+
+batchsize = 32
+val_batchsize = 250
+
+def read_image(path, center=False):
+    image = cv2.imread(path)
+    if center:
+        top = left = 16
+    else:
+        top  = random.randint(0, 31)
+        left = random.randint(0, 31)
+    bottom = 256 - (32 - top)
+    right  = 256 - (32 - left)
+
+    image = image[top:bottom, left:right, [2, 1, 0]].transpose(2, 0, 1).astype(np.float32)
+    image -= mean_image[:, top:bottom, left:right]
+    image /= 255
+    return image
+
+# Inter-thread communication
+data_q = Queue.Queue(maxsize=1)
+res_q  = Queue.Queue()
+
+# data feeder
+n_epoch = 6
+x_batch = np.ndarray((batchsize, 3, 224, 224), dtype=np.float32)
+y_batch = np.ndarray((batchsize,), dtype=np.int32)
+val_x_batch = np.ndarray((val_batchsize, 3, 224, 224), dtype=np.float32)
+val_y_batch = np.ndarray((val_batchsize,), dtype=np.int32)
+def feed_data():
+    for epoch in xrange(1, 1 + n_epoch):
+        print >> sys.stderr, 'epoch', epoch
+        print >> sys.stderr, 'learning rate', optimizer.lr
+        perm = np.random.permutation(len(train_list))
+        data_q.put('train')
+        i = 0
+        count = 0
+        for idx in perm:
+            path, label = train_list[idx]
+            x_batch[i] = read_image(path)
+            y_batch[i] = label
+            i += 1
+
+            if i == batchsize:
+                data_q.put((x_batch, y_batch))
+                i = 0
+
+            count += 1
+            if count % 100000 == 0:
+                data_q.put('val')
+                j = 0
+                for path, label in val_list:
+                    val_x_batch[j] = read_image(path, center=True)
+                    val_y_batch[j] = label
+                    j += 1
+
+                    if j == val_batchsize:
+                        data_q.put((val_x_batch, val_y_batch))
+                        j = 0
+                data_q.put('train')
+
+        optimizer.lr *= 0.97
+
+# Log result
+def log_result():
+    train_count = 0
+    train_cur_loss = 0
+    train_cur_accuracy = 0
+    while True:
+        result = res_q.get()
+        if result == 'end':
+            print >> sys.stderr, ''
+            break
+        elif result == 'train':
+            print >> sys.stderr, ''
+            train = True
+            continue
+        elif result == 'val':
+            print >> sys.stderr, ''
+            train = False
+            val_count = val_loss = val_accuracy = 0
+            continue
+
+        loss, accuracy = result
+        if train:
+            train_count += 1
+            sys.stderr.write('\rtrain {} updates ({} samples)'.format(train_count, train_count * batchsize))
+
+            train_cur_loss += loss
+            train_cur_accuracy += accuracy
+            if train_count % 1000 == 0:
+                mean_loss  = train_cur_loss / 1000
+                mean_error = 1 - train_cur_accuracy / 1000
+                print >> sys.stderr, ''
+                print json.dumps({'type': 'train', 'iteration': train_count,
+                                  'error': mean_error, 'loss': mean_loss})
+                sys.stdout.flush()
+                train_cur_loss = 0
+                train_cur_accuracy = 0
+        else:
+            val_count += val_batchsize
+            sys.stderr.write('\rval   {} batches ({} samples)'.format(val_count / val_batchsize, val_count))
+
+            val_loss += loss
+            val_accuracy += accuracy
+            if val_count == 50000:
+                mean_loss  = val_loss * val_batchsize / 50000
+                mean_error = 1 - val_accuracy * val_batchsize / 50000
+                print >> sys.stderr, ''
+                print json.dumps({'type': 'val', 'iteration': train_count,
+                                  'error': mean_error, 'loss': mean_loss})
+                sys.stdout.flush()
 
 # model
 model = FunctionSet(
@@ -58,6 +168,7 @@ model = FunctionSet(
     normb2 = F.BatchNormalization(1024),
     outb   = F.Linear(1024, 1000),
 )
+model.to_gpu()
 
 # Architecture
 def forward(x_data, y_data, volatile=False):
@@ -101,184 +212,46 @@ def forward(x_data, y_data, volatile=False):
 
 # Setup optimizer
 optimizer = O.MomentumSGD(lr=0.0075, momentum=0.9)
+optimizer.setup(model.collect_parameters())
 
-# Learning loop
-trash_q  = Queue.Queue()
-data_q   = Queue.Queue(maxsize=1)
-result_q = Queue.Queue()
-N     = len(train_list)
-N_val = len(val_list)
+# Main loop
 def train_loop():
-    device = pycuda.autoinit.device
-    ctx = device.make_context()
-    # GPU migration must be taken here with the context created on this thread
-    model.to_gpu()
-    optimizer.setup(model.collect_parameters())
-
-    try:
-        while True:
-            batch = data_q.get()
-            if batch == 'end':  # quit
-                data_q.task_done()
-                result_q.put('end')
-                break
-            elif batch == 'train':  # restart training
-                data_q.task_done()
-                result_q.put('train')
-                train = True
-                continue
-            elif batch == 'val':  # start validation
-                data_q.task_done()
-                result_q.put('val')
-                train = False
-                continue
-
-            if train:
-                optimizer.zero_grads()
-                loss, accuracy = forward(*batch)
-                loss.backward()
-                optimizer.update()
-            else:
-                loss, accuracy = forward(*batch, volatile=True)
-
-            result_q.put((float(loss.data.get()), float(accuracy.data.get())))
-            del loss, accuracy
-
-            trash_q.put(batch)
-            del batch
-
-            data_q.task_done()
-    finally:
-        cuda.Context.pop()
-
-batchsize = 32
-val_batchsize = 256
-
-# Show result
-def show_result():
-    train_count = 0
-    train_cur_loss = 0
-    train_cur_accuracy = 0
     while True:
-        result = result_q.get()
-        if result == 'end':
-            print >> sys.stderr, ''
-            result_q.task_done()
+        inp = data_q.get()
+        if inp == 'end':  # quit
+            res_q.put('end')
             break
-        elif result == 'train':
-            print >> sys.stderr, ''
+        elif inp == 'train':  # restart training
+            res_q.put('train')
             train = True
-            result_q.task_done()
             continue
-        elif result == 'val':
-            print >> sys.stderr, ''
+        elif inp == 'val':  # start validation
+            res_q.put('val')
             train = False
-            val_count = val_loss = val_accuracy = 0
-            result_q.task_done()
             continue
 
-        loss, accuracy = result
+        x = to_gpu(inp[0])
+        y = to_gpu(inp[1])
+
         if train:
-            train_count += 1
-            sys.stderr.write('\rtrain {} updates ({} samples)'.format(train_count, train_count * batchsize))
-
-            train_cur_loss += loss
-            train_cur_accuracy += accuracy
-            if train_count % 1000 == 0:
-                mean_loss  = train_cur_loss / 1000
-                mean_error = 1 - train_cur_accuracy / 1000
-                print >> sys.stderr, ''
-                print json.dumps({'type': 'train', 'iteration': train_count,
-                                  'error': mean_error, 'loss': mean_loss})
-                sys.stdout.flush()
-                train_cur_loss = 0
-                train_cur_accuracy = 0
+            optimizer.zero_grads()
+            loss, accuracy = forward(x, y)
+            loss.backward()
+            optimizer.update()
         else:
-            val_count += val_batchsize
-            sys.stderr.write('\rval   {} batches ({} samples)'.format(val_count / val_batchsize, val_count))
+            loss, accuracy = forward(x, y, volatile=True)
 
-            val_loss += loss
-            val_accuracy += accuracy
-            if val_count == 50000:
-                mean_loss  = val_loss * val_batchsize / 50000
-                mean_error = 1 - val_accuarcy * val_batchsize / 50000
-                print >> sys.stderr, ''
-                print json.dumps({'type': 'val', 'iteration': train_count,
-                                  'error': mean_error, 'loss': mean_loss})
-                sys.stdout.flush()
+        res_q.put((float(loss.data.get()), float(accuracy.data.get())))
+        del loss, accuracy
+        del x, y
 
-        result_q.task_done()
+feeder = threading.Thread(target=feed_data)
+feeder.start()
+logger = threading.Thread(target=log_result)
+logger.start()
 
-def read_image(path, center=False):
-    image = cv2.imread(path)
-    if center:
-        top = left = 16
-    else:
-        top  = random.randint(0, 31)
-        left = random.randint(0, 31)
-    bottom = 256 - (32 - top)
-    right  = 256 - (32 - left)
-
-    image = image[top:bottom, left:right, [2, 1, 0]].transpose(2, 0, 1).astype(np.float32)
-    image -= mean_image[:, top:bottom, left:right]
-    image /= 255
-    return image
-
-def trash():
-    while True:
-        try:
-            trash_q.get_nowait()
-            trash_q.task_done()
-        except:
-            break
-
-# data feeder
-x_batch = np.ndarray((batchsize, 3, 224, 224), dtype=np.float32)
-y_batch = np.ndarray((batchsize,), dtype=np.int32)
-val_x_batch = np.ndarray((val_batchsize, 3, 224, 224), dtype=np.float32)
-val_y_batch = np.ndarray((val_batchsize,), dtype=np.int32)
-def feed_data():
-    perm = np.random.permutation(len(train_list))
-    data_q.put('train')
-    i = 0
-    count = 0
-    for idx in perm:
-        path, label = train_list[idx]
-        x_batch[i] = read_image(path)
-        y_batch[i] = label
-        i += 1
-
-        if i == batchsize:
-            data_q.put((to_gpu(x_batch), to_gpu(y_batch)))
-            i = 0
-
-        count += 1
-        if count % 10000 == 0:
-            data_q.join()
-            trash()
-            data_q.put('val')
-            j = 0
-            for path, label in val_list:
-                val_x_batch[j] = read_image(path, center=True)
-                val_y_batch[j] = label
-                j += 1
-
-                if j == val_batchsize:
-                    data_q.put((to_gpu(val_x_batch), to_gpu(val_y_batch)))
-                    j = 0
-                trash()
-            data_q.put('train')
-        trash()
-
-learner = threading.Thread(target=train_loop)
-learner.start()
-shower  = threading.Thread(target=show_result)
-shower.start()
-
-for epoch in xrange(6):
-    print >> sys.stderr, 'epoch', epoch
-    print >> sys.stderr, 'learning rate', optimizer.lr
-    feed_data()
-    optimizer.lr *= 0.97
+train_loop()
+feeder.join()
+logger.join()
 
 pickle.dump(model, open('model', 'wb'), -1)
