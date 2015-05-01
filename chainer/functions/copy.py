88c85fa943c05be7729c75d3bf44ff842cd2953b
Seiya Tokui <beam.web@gmail.com>
2015-05-01 16:28:20 +0900

    Use sync copy_peer

--- chainer/functions/copy.py
+++ chainer/functions/copy.py
@@ -14,7 +14,8 @@ class Copy(Function):
     def forward_gpu(self, x):
         if self.from_device == self.to_device:
             return cuda.copy_async(x[0]),
-        return cuda.copy_peer_async(x[0], self.to_device, self.from_device),
+        # Use sync function, since async version is not provided by PyCUDA
+        return cuda.copy_peer(x[0], self.to_device, self.from_device),
 
     def backward_cpu(self, x, gy):
         return gy[0].copy(),
@@ -22,7 +23,8 @@ class Copy(Function):
     def backward_gpu(self, x, gy):
         if self.from_device == self.to_device:
             return cuda.copy_async(gy[0]),
-        return cuda.copy_peer_async(gy[0], self.from_device, self.to_device),
+        # Use sync function, since async version is not provided by PyCUDA
+        return cuda.copy_peer(gy[0], self.from_device, self.to_device),
 
 def copy(x, from_device, to_device):
     return Copy(from_device, to_device)(x)
