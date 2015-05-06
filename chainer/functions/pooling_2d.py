96f358ae7ccbf07066b24e4f492c1c3882196e2a
Seiya Tokui <beam.web@gmail.com>
2015-05-06 21:08:20 +0900

    Implement non-CuDNN pooling on GPU

--- chainer/functions/pooling_2d.py
+++ chainer/functions/pooling_2d.py
@@ -1,8 +1,14 @@
 from collections import Iterable
-import libcudnn
 import numpy
-from chainer import cuda, cudnn, Function
+from chainer import cuda, Function
 from chainer.utils import conv
+
+try:
+    import libcudnn
+    from chainer import cudnn
+    use_cudnn = cudnn.enabled
+except:
+    use_cudnn = False
 
 def _pair(x):
     if isinstance(x, Iterable):
@@ -12,7 +18,7 @@ def _pair(x):
 class Pooling2D(Function):
     """Base class of pooling function over a set of 2d planes."""
 
-    def __init__(self, ksize, stride=None, pad=0):
+    def __init__(self, ksize, stride=None, pad=0, use_cudnn=True):
         if stride is None:
             stride = ksize
 
@@ -20,23 +26,29 @@ class Pooling2D(Function):
         self.sy, self.sx = _pair(stride)
         self.ph, self.pw = _pair(pad)
 
+        self.use_cudnn = use_cudnn
+
     def forward_gpu(self, x):
+        # Implementation using cudnn
+        n, c, h, w = x[0].shape
+        y_h = conv.get_conv_outsize(h, self.kh, self.sy, self.ph)
+        y_w = conv.get_conv_outsize(w, self.kw, self.sx, self.pw)
+        y = cuda.empty((n, c, y_h, y_w), dtype=numpy.float32)
+
         handle = cudnn.get_default_handle()
         pool_desc = self.create_pool_desc()
         x_desc = cudnn.get_tensor_desc(x[0], x[0].shape[2], x[0].shape[3])
-
-        y_h = 1 + (x[0].shape[2] + self.ph * 2 - self.kh) / self.sy
-        y_w = 1 + (x[0].shape[3] + self.pw * 2 - self.kw) / self.sx
-        self.y = cuda.empty((x[0].shape[0], x[0].shape[1], y_h, y_w),
-                            dtype=numpy.float32)
-        y_desc = cudnn.get_tensor_desc(self.y, y_h, y_w)
+        y_desc = cudnn.get_tensor_desc(y, y_h, y_w)
 
         libcudnn.cudnnPoolingForward(
             handle, pool_desc.value, 1, x_desc.value, cudnn.get_ptr(x[0]),
-            0, y_desc.value, cudnn.get_ptr(self.y))
-        return self.y,
+            0, y_desc.value, cudnn.get_ptr(y))
+        self.y = y
+
+        return y,
 
     def backward_gpu(self, x, gy):
+        # Implementation using cudnn
         handle = cudnn.get_default_handle()
         pool_desc = self.create_pool_desc()
 
@@ -68,6 +80,53 @@ class MaxPooling2D(Pooling2D):
         y = self.indexes.choose(col)
         return y,
 
+    def forward_gpu(self, x):
+        if use_cudnn and self.use_cudnn:
+            return super(MaxPooling2D, self).forward_gpu(x)
+
+        n, c, h, w = x[0].shape
+        y_h = conv.get_conv_outsize(h, self.kh, self.sy, self.ph)
+        y_w = conv.get_conv_outsize(w, self.kw, self.sx, self.pw)
+        y = cuda.empty((n, c, y_h, y_w), dtype=numpy.float32)
+        self.indexes = cuda.empty((n, c, y_h, y_w), dtype=numpy.int32)
+
+        cuda.elementwise(
+            '''
+               float* out, int* indexes, const float* in,
+               int h, int w, int out_h, int out_w,
+               int kh, int kw, int sy, int sx, int ph, int pw
+            ''', '''
+               int c0    = i / (out_h * out_w);
+               int out_y = i / out_h % out_w;
+               int out_x = i % out_w;
+               int in_y_0 = max(0, out_y * sy - ph);
+               int in_y_1 = min(h, out_y * sy + kh - ph);
+               int in_x_0 = max(0, out_x * sx - pw);
+               int in_x_1 = min(w, out_x * sx + kw - pw);
+
+               float maxval = in[in_x_0 + w * (in_y_0 + h * c0)];
+               int argmax_y = in_y_0;
+               int argmax_x = in_x_0;
+               for (int y = in_y_0; y < in_y_1; ++y) {
+                 int offset_y = w * (y + h * c0);
+                 for (int x = in_x_0; x < in_x_1; ++x) {
+                   float v = in[x + offset_y];
+                   if (maxval < v) {
+                     maxval   = v;
+                     argmax_y = y;
+                     argmax_x = x;
+                   }
+                 }
+               }
+               out[i] = maxval;
+
+               int argmax_ky = argmax_y + ph - out_y * sy;
+               int argmax_kx = argmax_x + pw - out_x * sx;
+               indexes[i] = argmax_kx + kw * argmax_ky;
+            ''', 'max_pool_fwd')(y, self.indexes, x[0], h, w, y_h, y_w, self.kh,
+                                 self.kw, self.sy, self.sx, self.ph, self.pw)
+        return y,
+
     def backward_cpu(self, x, gy):
         n, c, out_h, out_w = gy[0].shape
         h, w = x[0].shape[2:]
@@ -82,13 +141,52 @@ class MaxPooling2D(Pooling2D):
         gx = conv.col2im_cpu(gcol, self.sy, self.sx, self.ph, self.pw, h, w)
         return gx,
 
+    def backward_gpu(self, x, gy):
+        if use_cudnn and self.use_cudnn:
+            return super(MaxPooling2D, self).backward_gpu(x, gy)
+
+        n, c, h, w = x[0].shape
+        y_h, y_w   = gy[0].shape[2:]
+        gx = cuda.empty_like(x[0])
+
+        cuda.elementwise(
+            '''
+               float* gx, const int* indexes, const float* gy,
+               int h, int w, int out_h, int out_w,
+               int kh, int kw, int sy, int sx, int ph, int pw
+            ''', '''
+               int c0 = i / (h * w);
+               int y  = i / w % h + ph;
+               int x  = i % w + pw;
+               int out_y_0 = max(0,     (y - kh + sy) / sy);
+               int out_y_1 = min(out_h, (y      + sy) / sy);
+               int out_x_0 = max(0,     (x - kw + sx) / sx);
+               int out_x_1 = min(out_w, (x      + sx) / sx);
+
+               float val = 0;
+               for (int out_y = out_y_0; out_y < out_y_1; ++out_y) {
+                 int ky = y - out_y * sy;
+                 for (int out_x = out_x_0; out_x < out_x_1; ++out_x) {
+                   int kx = x - out_x * sx;
+                   int offset = out_x + out_w * (out_y + out_h * c0);
+                   if (indexes[offset] == kx + kw * ky) {
+                     val += gy[offset];
+                   }
+                 }
+               }
+               gx[i] = val;
+            ''',
+            'max_pool_bwd')(gx, self.indexes, gy[0], h, w, y_h, y_w, self.kh,
+                            self.kw, self.sy, self.sx, self.ph, self.pw)
+        return gx,
+
     def create_pool_desc(self):
         return cudnn.get_pool2d_desc(
             (self.kh, self.kw), (self.sy, self.sx), (self.ph, self.pw),
             'CUDNN_POOLING_MAX')
 
-def max_pooling_2d(x, ksize, stride=None, pad=0):
-    return MaxPooling2D(ksize, stride, pad)(x)
+def max_pooling_2d(x, ksize, stride=None, pad=0, use_cudnn=True):
+    return MaxPooling2D(ksize, stride, pad, use_cudnn)(x)
 
 
 class AveragePooling2D(Pooling2D):
@@ -100,6 +198,41 @@ class AveragePooling2D(Pooling2D):
         y = col.mean(axis=(2, 3))
         return y,
 
+    def forward_gpu(self, x):
+        if use_cudnn and self.use_cudnn:
+            return super(AveragePooling2D, self).forward_gpu(x)
+
+        n, c, h, w = x[0].shape
+        y_h = conv.get_conv_outsize(h, self.kh, self.sy, self.ph)
+        y_w = conv.get_conv_outsize(w, self.kw, self.sx, self.pw)
+        y = cuda.empty((n, c, y_h, y_w), dtype=numpy.float32)
+        coeff = 1. / (self.kh * self.kw)
+
+        cuda.elementwise(
+            '''
+               float* out, const float* in, int h, int w, int out_h, int out_w,
+               int kh, int kw, int sy, int sx, int ph, int pw, float coeff
+            ''', '''
+               int c0    = i / (out_h * out_w);
+               int out_y = i / out_h % out_w;
+               int out_x = i % out_w;
+               int in_y_0 = max(0, out_y * sy - ph);
+               int in_y_1 = min(h, out_y * sy + kh - ph);
+               int in_x_0 = max(0, out_x * sx - pw);
+               int in_x_1 = min(w, out_x * sx + kw - pw);
+
+               float val = 0;
+               for (int y = in_y_0; y < in_y_1; ++y) {
+                 int offset_y = w * (y + h * c0);
+                 for (int x = in_x_0; x < in_x_1; ++x) {
+                   val += in[x + offset_y];
+                 }
+               }
+               out[i] = val * coeff;
+            ''', 'avg_pool_fwd')(y, x[0], h, w, y_h, y_w, self.kh, self.kw,
+                                 self.sy, self.sx, self.ph, self.pw, coeff)
+        return y,
+
     def backward_cpu(self, x, gy):
         h, w = x[0].shape[2:]
         gcol = numpy.tile(gy[0][:, :, numpy.newaxis, numpy.newaxis],
@@ -108,10 +241,44 @@ class AveragePooling2D(Pooling2D):
         gx /= self.kh * self.kw
         return gx,
 
+    def backward_gpu(self, x, gy):
+        if use_cudnn and self.use_cudnn:
+            return super(AveragePooling2D, self).backward_gpu(x, gy)
+
+        n, c, h, w = x[0].shape
+        y_h, y_w   = gy[0].shape[2:]
+        gx = cuda.empty_like(x[0])
+        coeff = 1. / (self.kh * self.kw)
+
+        cuda.elementwise(
+            '''
+               float* gx, const float* gy, int h, int w, int out_h, int out_w,
+               int kh, int kw, int sy, int sx, int ph, int pw, float coeff
+            ''', '''
+               int c0 = i / (h * w);
+               int y  = i / w % h + ph;
+               int x  = i % w + pw;
+               int out_y_0 = max(0,     (y - kh + sy) / sy);
+               int out_y_1 = min(out_h, (y      + sy) / sy);
+               int out_x_0 = max(0,     (x - kw + sx) / sx);
+               int out_x_1 = min(out_w, (x      + sx) / sx);
+               int hc0  = out_h * c0;
+
+               float val = 0;
+               for (int out_y = out_y_0; out_y < out_y_1; ++out_y) {
+                 for (int out_x = out_x_0; out_x < out_x_1; ++out_x) {
+                   val += gy[out_x + out_w * (out_y + hc0)];
+                 }
+               }
+               gx[i] = val * coeff;
+            ''', 'avg_pool_bwd')(gx, gy[0], h, w, y_h, y_w, self.kh, self.kw,
+                                 self.sy, self.sx, self.ph, self.pw, coeff)
+        return gx,
+
     def create_pool_desc(self):
         return cudnn.get_pool2d_desc(
             (self.kh, self.kw), (self.sy, self.sx), (self.ph, self.pw),
             'CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING')
 
-def average_pooling_2d(x, ksize, stride=None, pad=0):
-    return AveragePooling2D(ksize, stride, pad)(x)
+def average_pooling_2d(x, ksize, stride=None, pad=0, use_cudnn=True):
+    return AveragePooling2D(ksize, stride, pad, use_cudnn)(x)
--- tests/functions_tests/test_pooling_2d.py
+++ tests/functions_tests/test_pooling_2d.py
@@ -12,9 +12,9 @@ class TestMaxPooling2D(TestCase):
         self.x  = numpy.random.uniform(-1, 1, (2, 3, 4, 3)).astype(numpy.float32)
         self.gy = numpy.random.uniform(-1, 1, (2, 3, 2, 2)).astype(numpy.float32)
 
-    def check_forward(self, x_data):
+    def check_forward(self, x_data, use_cudnn=True):
         x = Variable(x_data)
-        y = max_pooling_2d(x, 3, stride=2, pad=1)
+        y = max_pooling_2d(x, 3, stride=2, pad=1, use_cudnn=use_cudnn)
         if isinstance(y.data, GPUArray):
             y_data = y.data.get()
         else:
@@ -34,9 +34,12 @@ class TestMaxPooling2D(TestCase):
     def test_forward_gpu(self):
         self.check_forward(to_gpu(self.x))
 
-    def check_backward(self, x_data, y_grad):
+    def test_forward_gpu_no_cudnn(self):
+        self.check_forward(to_gpu(self.x), False)
+
+    def check_backward(self, x_data, y_grad, use_cudnn=True):
         x = Variable(x_data)
-        y = max_pooling_2d(x, 3, stride=2, pad=1)
+        y = max_pooling_2d(x, 3, stride=2, pad=1, use_cudnn=use_cudnn)
         y.grad = y_grad
         y.backward()
 
@@ -52,15 +55,18 @@ class TestMaxPooling2D(TestCase):
     def test_backward_gpu(self):
         self.check_backward(to_gpu(self.x), to_gpu(self.gy))
 
+    def test_backward_gpu_no_cudnn(self):
+        self.check_backward(to_gpu(self.x), to_gpu(self.gy), False)
+
 
 class TestAveragePooling2D(TestCase):
     def setUp(self):
         self.x  = numpy.random.uniform(-1, 1, (2, 3, 4, 3)).astype(numpy.float32)
         self.gy = numpy.random.uniform(-1, 1, (2, 3, 2, 2)).astype(numpy.float32)
 
-    def check_forward(self, x_data):
+    def check_forward(self, x_data, use_cudnn=True):
         x = Variable(x_data)
-        y = average_pooling_2d(x, 3, stride=2, pad=1)
+        y = average_pooling_2d(x, 3, stride=2, pad=1, use_cudnn=use_cudnn)
         if type(y.data) == GPUArray:
             y_data = y.data.get()
         else:
@@ -81,9 +87,12 @@ class TestAveragePooling2D(TestCase):
     def test_forward_gpu(self):
         self.check_forward(to_gpu(self.x))
 
-    def check_backward(self, x_data, y_grad):
+    def test_forward_gpu_no_cudnn(self):
+        self.check_forward(to_gpu(self.x), False)
+
+    def check_backward(self, x_data, y_grad, use_cudnn=True):
         x = Variable(x_data)
-        y = average_pooling_2d(x, 3, stride=2, pad=1)
+        y = average_pooling_2d(x, 3, stride=2, pad=1, use_cudnn=use_cudnn)
         y.grad = y_grad
         y.backward()
 
@@ -98,3 +107,6 @@ class TestAveragePooling2D(TestCase):
 
     def test_backward_gpu(self):
         self.check_backward(to_gpu(self.x), to_gpu(self.gy))
+
+    def test_backward_gpu_no_cudnn(self):
+        self.check_backward(to_gpu(self.x), to_gpu(self.gy), False)
