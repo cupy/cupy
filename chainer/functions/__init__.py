95fffa0fbe744b9087547a14a97fb7dd0e68ba76
Seiya Tokui <beam.web@gmail.com>
2015-05-11 23:02:58 +0900

    Sort function imports to fit with documentation order

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -1,26 +1,34 @@
-# Non-parameterized functions
-from accuracy   import accuracy
+"""Collection of :class:`~chainer.Function` implementations."""
+
+# Parameterized function classes
+from batch_normalization import BatchNormalization
+from convolution_2d      import Convolution2D
+from embed_id            import EmbedID
+from inception           import Inception
+from linear              import Linear
+from parameter           import Parameter
+from prelu               import PReLU
+
+# Array manipulation functions
+from concat   import concat
+from copy     import copy
+from dropout  import dropout
+from identity import identity
+
+# Activation functions
 from basic_math import exp, log
-from concat     import concat
-from copy       import copy
-from dropout    import dropout
-from identity   import identity
 from leaky_relu import leaky_relu
 from lstm       import lstm
-from mean_squared_error import mean_squared_error
-from pooling_2d import average_pooling_2d, max_pooling_2d
 from relu       import relu
 from sigmoid    import sigmoid
 from softmax    import softmax
-from softmax_cross_entropy import softmax_cross_entropy
-from sum        import sum
 from tanh       import tanh
 
-# Parameterized layers
-from batch_normalization import BatchNormalization
-from convolution_2d import Convolution2D
-from embed_id       import EmbedID
-from inception      import Inception
-from linear         import Linear
-from parameter      import Parameter
-from prelu          import PReLU
+# Pooling functions
+from pooling_2d import average_pooling_2d, max_pooling_2d
+
+# Loss, evaluation and aggregation
+from accuracy              import accuracy
+from mean_squared_error    import mean_squared_error
+from softmax_cross_entropy import softmax_cross_entropy
+from sum                   import sum
