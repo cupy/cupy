2f8ca2c2d9e21e5ae42239199c11e0dd21bf48b0
Kenta OONO <oono@preferred.jp>
2015-06-09 11:12:15 +0900

    Fix author and copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,8 +54,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Chainer'
-copyright = u'2015, Seiya Tokui'
-author = u'Seiya Tokui'
+copyright = u'2015, Preferred Networks, inc. and Preferred Infrastructure, inc.'
+author = u'Preferred Networks, inc. and Preferred Infrastructure, inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -243,7 +243,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'Chainer.tex', u'Chainer Documentation',
-   u'Seiya Tokui', 'manual'),
+   u'Preferred Networks, inc. and Preferred Infrastructure, inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
