7c1345c09cffaef9710bed24f6ae4ed791c3ade0
Yuya Unno <unnonouno@gmail.com>
2015-06-17 17:15:13 +0900

    Add chainer to required packages

--- setup_cuda_deps.py
+++ setup_cuda_deps.py
@@ -10,6 +10,7 @@ setup(
     url='http://chainer.org/',
     packages=[],
     install_requires=[
+        'chainer',
         'pycuda>=2014.1',
         'scikits.cuda>=0.5.0b1,!=0.042',
         'Mako',
