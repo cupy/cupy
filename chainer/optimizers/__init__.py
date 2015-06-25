d332add0154707f536082018db7a1abb93b771ad
Yuya Unno <unnonouno@gmail.com>
2015-06-25 15:38:44 +0900

    Fix optimizers/__init__.py

--- chainer/optimizers/__init__.py
+++ chainer/optimizers/__init__.py
@@ -1,6 +1,13 @@
-from .ada_delta import AdaDelta
-from .ada_grad import AdaGrad
-from .adam import Adam
-from .momentum_sgd import MomentumSGD
-from .rmsprop import RMSprop
-from .sgd import SGD
+from chainer.optimizers import ada_delta
+from chainer.optimizers import ada_grad
+from chainer.optimizers import adam
+from chainer.optimizers import momentum_sgd
+from chainer.optimizers import rmsprop
+from chainer.optimizers import sgd
+
+AdaDelta = ada_delta.AdaDelta
+AdaGrad = ada_grad.AdaGrad
+Adam = adam.Adam
+MomentumSGD = momentum_sgd.MomentumSGD
+RMSprop = rmsprop.RMSprop
+SGD = sgd.SGD
