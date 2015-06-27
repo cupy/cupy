37dfe9e8d09c7d9670dc339b02280987fa4b2cc2
Yuya Unno <unnonouno@gmail.com>
2015-06-27 20:25:16 +0900

    Make test for typecheck

---
+++ tests/utils_tests/test_type_check.py
@@ -0,0 +1,167 @@
+import unittest
+
+import numpy
+
+from chainer.utils import type_check as T
+
+
+class TestIntConstant(unittest.TestCase):
+
+    def setUp(self):
+        self.x = T.IntConstant(10)
+
+    def test_str(self):
+        self.assertEqual('10', str(self.x))
+
+    def test_eval(self):
+        self.assertEqual(10, self.x.eval())
+
+
+class TestIntVariable(unittest.TestCase):
+
+    def setUp(self):
+        self.x = T.IntVariable(10, 'x')
+
+    def test_str(self):
+        self.assertEqual('x', str(self.x))
+
+    def test_eval(self):
+        self.assertEqual(10, self.x.eval())
+
+
+class TestMember(unittest.TestCase):
+
+    def setUp(self):
+        self.x = T.Member(10, 'xxx', 'yyy')
+
+    def test_str(self):
+        self.assertEqual('xxx.yyy', str(self.x))
+
+    def test_eval(self):
+        self.assertEqual(10, self.x.eval())
+
+
+class TestBinaryOperator(unittest.TestCase):
+
+    def setUp(self):
+        x = T.IntVariable(1, 'x')
+        y = T.IntVariable(1, 'y')
+        f = lambda x, y: (x, y)
+        self.op1 = T.BinaryOperator(x, y, '+', 7, f)
+        self.op2 = T.BinaryOperator(x, y, '+', 8, f)
+        self.op3 = T.BinaryOperator(x, y, '+', 9, f)
+
+    def test_str(self):
+        self.assertEqual('x + y', str(self.op1))
+        self.assertEqual('x + (y)', str(self.op2))
+        self.assertEqual('(x) + (y)', str(self.op3))
+
+    def test_eval(self):
+        self.assertEqual((1, 1), self.op1.eval())
+
+
+class TestUnaryOperator(unittest.TestCase):
+
+    def setUp(self):
+        x = T.IntVariable(1, 'x')
+        f = lambda x: (x,)
+        self.op1 = T.UnaryOperator(x, '-', 8, f)
+        self.op2 = T.UnaryOperator(x, '-', 9, f)
+
+    def test_str(self):
+        self.assertEqual('-x', str(self.op1))
+        self.assertEqual('-(x)', str(self.op2))
+
+    def test_eval(self):
+        self.assertEqual((1, ), self.op1.eval())
+
+
+class TestOperators(unittest.TestCase):
+
+    def setUp(self):
+        self.x = T.IntVariable(1, 'x')
+        self.y = T.IntVariable(1, 'y')
+
+    def test_str(self):
+        x = self.x
+        y = self.y
+        self.assertEqual('x + y', str(x + y))
+        self.assertEqual('1 + x', str(1 + x))
+        self.assertEqual('x - y', str(x - y))
+        self.assertEqual('1 - x', str(1 - x))
+        self.assertEqual('x * y', str(x * y))
+        self.assertEqual('1 * x', str(1 * x))
+        self.assertEqual('x // y', str(x // y))
+        self.assertEqual('1 // x', str(1 // x))
+        self.assertEqual('x % y', str(x % y))
+        self.assertEqual('1 % x', str(1 % x))
+        self.assertEqual('x ** y', str(x ** y))
+        self.assertEqual('x ** y', str(pow(x, y)))
+        self.assertEqual('x << y', str(x << y))
+        self.assertEqual('x >> y', str(x >> y))
+        self.assertEqual('x & y', str(x & y))
+        self.assertEqual('1 & x', str(1 & x))
+        self.assertEqual('x ^ y', str(x ^ y))
+        self.assertEqual('1 ^ x', str(1 ^ x))
+        self.assertEqual('x | y', str(x | y))
+        self.assertEqual('1 | x', str(1 | x))
+
+        self.assertEqual('-x', str(-x))
+        self.assertEqual('+x', str(+x))
+        self.assertEqual('~x', str(~x))
+
+        # left-assosiative
+        self.assertEqual('x + x - x', str(x + x - x))
+        self.assertEqual('x + (x - x)', str(x + (x - x)))
+        self.assertEqual('x << (x << x)', str(x << (x << x)))
+
+        self.assertEqual('-(x + x)', str(-(x + x)))
+
+        # pow has higher priority than unary operators
+        self.assertEqual('-x ** x', str(-x ** x))
+        self.assertEqual('(-x) ** x', str((-x) ** x))
+
+    def test_priority(self):
+        x = self.x
+        y = self.y
+
+        self.assertTrue((x << y).priority == (x >> y).priority)
+        self.assertTrue((x + y).priority == (x - y).priority)
+        self.assertTrue((x * y).priority ==
+                        (x // y).priority ==
+                        (x % y).priority)
+        self.assertTrue((-x).priority == (+x).priority == (~x).priority)
+
+        self.assertTrue((x | y).priority <
+                        (x ^ y).priority <
+                        (x & y).priority <
+                        (x << y).priority <
+                        (x + y).priority <
+                        (x * y).priority <
+                        (-x).priority <
+                        (x ** y).priority <
+                        x.priority)
+
+
+class TestGetType(unittest.TestCase):
+
+    def test_empty(self):
+        ts = T.get_types((), False)
+        self.assertIsInstance(ts, T.TypeInfoTuple)
+        self.assertEqual(0, len(ts))
+
+    def test_simple(self):
+        data = (numpy.zeros((1, 2, 3)).astype(numpy.float32),)
+        ts = T.get_types(data, False)
+        self.assertIsInstance(ts, T.TypeInfoTuple)
+        self.assertEqual(1, len(ts))
+
+        t = ts[0]
+        self.assertIsInstance(t, T.TypeInfo)
+        self.assertEqual(0, t.index)
+        self.assertEqual(3, len(t.shape))
+        self.assertEqual(1, t.shape[0].eval())
+        self.assertEqual(2, t.shape[1].eval())
+        self.assertEqual(3, t.shape[2].eval())
+        self.assertEqual(3, t.ndim.eval())
+        self.assertEqual(numpy.float32, t.dtype.eval())
