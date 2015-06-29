06ba899aaaf5b8ff47c8ed6f317076c43d3351a0
Kenta OONO <oono@preferred.jp>
2015-06-30 08:11:43 +0900

    Add version number to chainer module

--- chainer/__init__.py
+++ chainer/__init__.py
@@ -1,8 +1,13 @@
+import pkg_resources
+
 from chainer import function
 from chainer import function_set
 from chainer.functions import basic_math
 from chainer import optimizer
 from chainer import variable
+
+
+__version__ = pkg_resources.get_distribution('chainer').version
 
 Variable = variable.Variable
 Function = function.Function
