2efbbce49b3b2c63b4b0ea8fa5cc93404bb551db
Yuya Unno <unnonouno@gmail.com>
2015-07-04 17:55:27 +0900

    Simplify type test for linear

--- chainer/functions/linear.py
+++ chainer/functions/linear.py
@@ -79,10 +79,9 @@ class Linear(function.Function):
         type_check.expect(
             x_type.dtype == numpy.float32,
             x_type.ndim >= 2,
-        )
-        type_check.expect(
             (type_check.Variable(numpy.prod, 'prod')(x_type.shape[1:]) ==
-             type_check.Variable(self.W.shape[1], 'W.shape[1]')))
+             type_check.Variable(self.W.shape[1], 'W.shape[1]')),
+        )
 
     def check_type_backward(self, in_types, out_types):
         type_check.expect(
@@ -95,8 +94,6 @@ class Linear(function.Function):
         type_check.expect(
             y_type.dtype == numpy.float32,
             y_type.ndim == 2,
-        )
-        type_check.expect(
             y_type.shape[0] == x_type.shape[0],
             y_type.shape[1] == type_check.Variable(self.W.shape[0],
                                                    'W.shape[0]'),
