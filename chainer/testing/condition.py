6978de17d4a9c3491b128bcf96f973f99b79f51a
Kenta OONO <oono@preferred.jp>
2015-07-05 14:46:58 +0900

    Do not run remaining tests if the number of successful tests exceeds min_success

---
+++ chainer/testing/condition.py
@@ -0,0 +1,57 @@
+import functools
+import unittest
+
+import six
+
+
+class QuietTestRunner(object):
+    def run(self, suite):
+        result = unittest.TestResult()
+        suite(result)
+        return result
+
+
+def success_at_least(times, min_success):
+    assert times >= min_success
+
+    def _multiple(f):
+
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            assert len(args) > 0
+            cls = args[0]
+            assert isinstance(cls, unittest.TestCase)
+
+            suite = unittest.TestSuite()
+            for _ in six.moves.range(min_success):
+                suite.addTest(
+                    unittest.FunctionTestCase(
+                        lambda: f(*args, **kwargs),
+                        setUp=cls.setUp,
+                        tearDown=cls.tearDown))
+            success_counter = (min_success -
+                               len(QuietTestRunner().run(suite).failures))
+            if success_counter >= min_success:
+                cls.assertTrue(True)
+                return
+
+            for _ in six.moves.range(min_success, times):
+                suite = unittest.TestSuite()
+                suite.addTest(
+                    unittest.FunctionTestCase(
+                        lambda: f(*args, **kwargs),
+                        setUp=cls.setUp,
+                        tearDown=cls.tearDown))
+                if QuietTestRunner().run(suite).wasSuccessful():
+                    success_counter += 1
+                if success_counter >= min_success:
+                    cls.assertTrue(True)
+                    return
+
+            cls.fail()
+        return wrapper
+    return _multiple
+
+
+def all_success(times):
+    return success_at_least(times, times)
