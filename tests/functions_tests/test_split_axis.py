0df65543d1ab369aa38002742405748517550dc6
Ryosuke Okuta <okuta@preferred.jp>
2015-07-07 13:24:01 +0900

    Split test cases

--- tests/functions_tests/test_split_axis.py
+++ tests/functions_tests/test_split_axis.py
@@ -13,20 +13,13 @@ if cuda.available:
     cuda.init()
 
 
-class Split(unittest.TestCase):
+class TestSplitAxis0(unittest.TestCase):
 
     def setUp(self):
-        self.x0 = numpy.arange(42, dtype=numpy.float32).reshape(2, 7, 3)
-        self.ys0 = [self.x0[:, :2], self.x0[:, 2:5], self.x0[:, 5:]]
-        self.ys0_section = [2, 5]
-
-        self.x1 = numpy.arange(21, dtype=numpy.float32).reshape(7, 3)
-        self.ys1 = [self.x1[:2], self.x1[2:5], self.x1[5:]]
-        self.ys1_section = [2, 5]
-
-        self.x2 = numpy.arange(54, dtype=numpy.float32).reshape(2, 9, 3)
-        self.ys2 = [self.x2[:, :3], self.x2[:, 3:6], self.x2[:, 6:]]
-        self.ys2_section = 3
+        self.x = numpy.arange(42, dtype=numpy.float32).reshape(2, 7, 3)
+        self.ys = [self.x[:, :2], self.x[:, 2:5], self.x[:, 5:]]
+        self.ys_section = [2, 5]
+        self.axis = 1
 
     def check_forward(self, x_data, ys_data, indices_or_sections, axis):
         x = chainer.Variable(x_data)
@@ -34,35 +27,15 @@ class Split(unittest.TestCase):
         for yd, y in zip(ys_data, ys):
             gradient_check.assert_allclose(yd, y.data, atol=0, rtol=0)
 
-    def test_forward_cpu_0(self):
-        self.check_forward(self.x0, self.ys0, self.ys0_section, 1)
-
-    def test_forward_cpu_1(self):
-        self.check_forward(self.x1, self.ys1, self.ys1_section, 0)
-
-    def test_forward_cpu_2(self):
-        self.check_forward(self.x2, self.ys2, self.ys2_section, 1)
+    def test_forward_cpu(self):
+        self.check_forward(self.x, self.ys, self.ys_section, self.axis)
 
     @attr.gpu
-    def test_forward_gpu_0(self):
+    def test_forward_gpu(self):
         self.check_forward(
-            cuda.to_gpu(self.x0),
-            [cuda.to_gpu(y.copy()) for y in self.ys0],
-            self.ys0_section, axis=1)
-
-    @attr.gpu
-    def test_forward_gpu_1(self):
-        self.check_forward(
-            cuda.to_gpu(self.x1),
-            [cuda.to_gpu(y.copy()) for y in self.ys1],
-            self.ys1_section, axis=0)
-
-    @attr.gpu
-    def test_forward_gpu_2(self):
-        self.check_forward(
-            cuda.to_gpu(self.x2),
-            [cuda.to_gpu(y.copy()) for y in self.ys2],
-            self.ys2_section, axis=1)
+            cuda.to_gpu(self.x),
+            [cuda.to_gpu(y.copy()) for y in self.ys],
+            self.ys_section, axis=self.axis)
 
     def check_backward(self, x_data, indices_or_sections, axis):
         x = chainer.Variable(x_data)
@@ -73,23 +46,28 @@ class Split(unittest.TestCase):
 
         gradient_check.assert_allclose(x.data, x.grad, atol=0, rtol=0)
 
-    def test_backward_cpu_0(self):
-        self.check_backward(self.x0, self.ys0_section, axis=1)
-
-    def test_backward_cpu_1(self):
-        self.check_backward(self.x1, self.ys1_section, axis=0)
-
-    def test_backward_cpu_2(self):
-        self.check_backward(self.x2, self.ys2_section, axis=1)
+    def test_backward_cpu(self):
+        self.check_backward(self.x, self.ys_section, axis=self.axis)
 
     @attr.gpu
-    def test_backward_gpu_0(self):
-        self.check_backward(cuda.to_gpu(self.x0), self.ys0_section, axis=1)
+    def test_backward_gpu(self):
+        self.check_backward(
+            cuda.to_gpu(self.x), self.ys_section, axis=self.axis)
 
-    @attr.gpu
-    def test_backward_gpu_1(self):
-        self.check_backward(cuda.to_gpu(self.x1), self.ys1_section, axis=0)
 
-    @attr.gpu
-    def test_backward_gpu_2(self):
-        self.check_backward(cuda.to_gpu(self.x2), self.ys2_section, axis=1)
+class TestSplitAxis1(TestSplitAxis0):
+
+    def setUp(self):
+        self.x = numpy.arange(21, dtype=numpy.float32).reshape(7, 3)
+        self.ys = [self.x[:2], self.x[2:5], self.x[5:]]
+        self.ys_section = [2, 5]
+        self.axis = 0
+
+
+class TestSplitAxis2(TestSplitAxis0):
+
+    def setUp(self):
+        self.x = numpy.arange(54, dtype=numpy.float32).reshape(2, 9, 3)
+        self.ys = [self.x[:, :3], self.x[:, 3:6], self.x[:, 6:]]
+        self.ys_section = 3
+        self.axis = 1
