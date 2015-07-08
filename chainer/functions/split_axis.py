a4e59e72a64c22faabba3dc2eeefee0df38a3708
Ryosuke Okuta <okuta@preferred.jp>
2015-07-08 16:17:55 +0900

    Add argument check

--- chainer/functions/split_axis.py
+++ chainer/functions/split_axis.py
@@ -21,6 +21,8 @@ class SplitAxis(function.Function):
     """Function that splits multiple arrays towards the specified axis."""
 
     def __init__(self, indices_or_sections, axis):
+        if not isinstance(indices_or_sections, (int, collections.Iterable)):
+            raise TypeError('indices_or_sections must be integer or 1-D array')
         self.indices_or_sections = indices_or_sections
         self.axis = axis
 
