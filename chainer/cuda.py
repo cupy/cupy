128b1767c58dc17cb0e5db0ef821d8f29454c59a
Seiya Tokui <beam.web@gmail.com>
2015-07-27 12:59:18 +0900

    Fix document

--- chainer/cuda.py
+++ chainer/cuda.py
@@ -1,6 +1,6 @@
-"""Device, context and memory management on PyCUDA and scikit-cuda.
+"""Device, context and memory management on CuPy.
 
-Chainer uses PyCUDA facilities (with very thin wrapper) to exploit the speed of
+Chainer uses CuPy facilities (with very thin wrapper) to exploit the speed of
 GPU computation. Following modules and classes are imported to :mod:`cuda`
 module for convenience (refer to this table when reading chainer's source
 codes).
