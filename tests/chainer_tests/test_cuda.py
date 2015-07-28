bb7158e5d13e123cefd0d91990ffc97fbb85c60a
Ryosuke Okuta <okuta@preferred.jp>
2015-07-29 02:41:39 +0900

    Remove cuda_deps

--- tests/chainer_tests/test_cuda.py
+++ tests/chainer_tests/test_cuda.py
@@ -1,5 +1,3 @@
-import os
-import re
 import unittest
 
 import numpy
@@ -9,26 +7,6 @@ from chainer import testing
 
 
 class TestCuda(unittest.TestCase):
-
-    def _get_cuda_deps_requires(self):
-        cwd = os.path.dirname(__file__)
-
-        cuda_deps_path = os.path.join(cwd, '../..', 'cuda_deps', 'setup.py')
-        with open(cuda_deps_path) as f:
-            in_require = False
-            requires = []
-            for line in f:
-                if in_require:
-                    if ']' in line:
-                        in_require = False
-                    else:
-                        m = re.search(r'\'(.*)\',', line)
-                        requires.append(m.group(1))
-                else:
-                    if 'install_requires' in line:
-                        in_require = True
-
-        return requires
 
     def test_requires(self):
         requires = self._get_cuda_deps_requires()
