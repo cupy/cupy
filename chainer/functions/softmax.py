Fix softmax

chainer/functions/softmax.py
@@ -4,6 +4,7 @@ import numpy
 from chainer import cuda
 from chainer import function
 from chainer.utils import type_check
+import cupy
 
 if cuda.cudnn_enabled:
     cudnn = cuda.cudnn
@@ -43,38 +44,25 @@ class Softmax(function.Function):
             y_type.shape[1] == x_type.shape[1],
         )
 
-    def forward_cpu(self, x):
-        self.y = x[0] - numpy.amax(x[0], axis=1, keepdims=True)
-        numpy.exp(self.y, out=self.y)
-        self.y /= self.y.sum(axis=1, keepdims=True)
-        return self.y,
-
-    def forward_gpu(self, x):
-        y = cuda.empty_like(x[0])
-        if cuda.cudnn_enabled and self.use_cudnn:
+    def forward(self, x):
+        xpy = cuda.get_xpy(x[0])
+        if xpy == cupy and cuda.cudnn_enabled and self.use_cudnn:
+            self.y = cuda.empty_like(x[0])
             handle = cudnn.get_handle()
             x_mat = x[0].reshape(x[0].shape[0], -1, 1, 1)
             desc = cudnn.create_tensor_descriptor(x_mat)
             libcudnn.softmaxForward(
                 handle, _algorithm, _mode, ctypes.c_float(1), desc.value,
-                x[0].data.ptr, ctypes.c_float(0), desc.value, y.data.ptr)
-            self.y = y
+                x[0].data.ptr, ctypes.c_float(0), desc.value, self.y.data.ptr)
         else:
-            xpy = cuda.cupy
-            self.y = x[0] - xpy.amax(x[0], axis=1, keepdims=True)
+            self.y = x[0] - x[0].max(axis=1, keepdims=True)
             xpy.exp(self.y, out=self.y)
             self.y /= self.y.sum(axis=1, keepdims=True)
+        return self.y,
 
-        return y,
-
-    def backward_cpu(self, x, gy):
-        gx = self.y * gy[0]
-        sumdx = gx.sum(axis=1, keepdims=True)
-        gx -= self.y * sumdx
-        return gx,
-
-    def backward_gpu(self, x, gy):
-        if cuda.cudnn_enabled and self.use_cudnn:
+    def backward(self, x, gy):
+        xpy = cuda.get_xpy(x[0])
+        if xpy == cupy and cuda.cudnn_enabled and self.use_cudnn:
             handle = cudnn.get_handle()
             gx = cuda.empty_like(x[0])
             x_mat = x[0].reshape(x[0].shape[0], -1, 1, 1)
@@ -85,9 +73,7 @@ class Softmax(function.Function):
                 desc.value, gx.data.ptr)
         else:
             gx = self.y * gy[0]
-            sumdx = gx.sum(axis=1, keepdims=True)
-            gx -= self.y * sumdx
-            gx = self.y * gy[0]
+            gx -= self.y * gx.sum(axis=1, keepdims=True)
 
         return gx,
 