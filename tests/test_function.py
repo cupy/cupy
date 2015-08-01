da85ead37a259957a46fd221a07d28ef7a88b47f
Yuya Unno <unnonouno@gmail.com>
2015-08-01 23:22:45 +0900

    Fix import order

--- tests/test_function.py
+++ tests/test_function.py
@@ -7,8 +7,8 @@ import chainer
 from chainer import cuda
 import chainer.functions as F
 from chainer import testing
-from chainer.utils import type_check
 from chainer.testing import attr
+from chainer.utils import type_check
 
 
 if cuda.available:
