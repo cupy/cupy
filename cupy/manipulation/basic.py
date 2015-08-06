d4fd3f39d45f4bc13ce342fe0fe5bfd80ab45866
Seiya Tokui <beam.web@gmail.com>
2015-08-06 18:54:36 +0900

    Stop using peer APIs

--- cupy/manipulation/basic.py
+++ cupy/manipulation/basic.py
@@ -28,24 +28,16 @@ def copyto(dst, src, casting='same_kind', where=None):
         return
 
     if where is None:
-        if dst.data.device == src.data.device:
-            if _can_memcopy(dst, src):
-                dst.data.copy_from(src.data, src.nbytes)
-            else:
-                elementwise.copy(src, dst)
+        if _can_memcpy(dst, src):
+            dst.data.copy_from(src.data, src.nbytes)
         else:
-            # peer copy
-            if _can_memcopy(dst, src):
-                dst.data.copy_from_peer(src.data, src.nbytes)
-            else:
-                raise ValueError('Only contiguous arrays can be copied over '
-                                 'devices')
+            elementwise.copy(src, dst)
     else:
         # Copy with where does not support peer copy.
         elementwise.copy_where(src, where, dst)
 
 
-def _can_memcopy(dst, src):
+def _can_memcpy(dst, src):
     c_contiguous = dst.flags.c_contiguous and src.flags.c_contiguous
     f_contiguous = dst.flags.f_contiguous and src.flags.f_contiguous
     return (c_contiguous or f_contiguous) and dst.dtype == src.dtype and \
