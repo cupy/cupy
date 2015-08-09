c24b8dcb8b2932779b0b396f2df1ac039763bbb5
Seiya Tokui <beam.web@gmail.com>
2015-08-09 10:59:44 +0900

    Simplify LSTM implementation

--- chainer/functions/lstm.py
+++ chainer/functions/lstm.py
@@ -7,7 +7,7 @@ from chainer.utils import type_check
 
 
 def _extract_gates(x):
-    r = x.reshape((x.shape[0], x.shape[1] / 4, 4) + x.shape[2:])
+    r = x.reshape((x.shape[0], x.shape[1] // 4, 4) + x.shape[2:])
     return (r[:, :, i] for i in six.moves.range(4))
 
 
@@ -22,19 +22,17 @@ def _grad_sigmoid(x):
 def _grad_tanh(x):
     return 1 - x * x
 
+
 _preamble = '''
-__device__ float sigmoid(float x)      { return 1 / (1 + __expf(-x)); }
-__device__ float grad_sigmoid(float y) { return y * (1 - y); }
-__device__ float grad_tanh(float y)    { return 1 - y * y; }
+template <typename T> __device__ T sigmoid(T x) { return 1 / (1 + exp(-x)); }
+template <typename T> __device__ T grad_sigmoid(T y) { return y * (1 - y); }
+template <typename T> __device__ T grad_tanh(T y) { return 1 - y * y; }
 
 #define COMMON_ROUTINE \
-    int I = i / rsize; \
-    int J = i % rsize; \
-    int xi = I * 4 * rsize; \
-    float aa =   tanhf(x[xi +           J]); \
-    float ai = sigmoid(x[xi +   rsize + J]); \
-    float af = sigmoid(x[xi + 2*rsize + J]); \
-    float ao = sigmoid(x[xi + 3*rsize + J]);
+    T aa = tanh(a); \
+    T ai = sigmoid(i_); \
+    T af = sigmoid(f); \
+    T ao = sigmoid(o);
 '''
 
 
@@ -46,7 +44,6 @@ class LSTM(function.Function):
     state. x must have four times channels compared to the number of units.
 
     """
-
     def check_type_forward(self, in_types):
         type_check.expect(in_types.size() == 2)
         c_type, x_type = in_types
@@ -65,24 +62,36 @@ class LSTM(function.Function):
         for i in range(2, c_type.ndim.eval()):
             type_check.expect(x_type.shape[i] == c_type.shape[i])
 
-    def forward_cpu(self, inputs):
+    def forward(self, inputs):
         c_prev, x = inputs
-
         a, i, f, o = _extract_gates(x)
-        self.a = numpy.tanh(a)
-        self.i = _sigmoid(i)
-        self.f = _sigmoid(f)
-        self.o = _sigmoid(o)
 
-        self.c = self.a * self.i + self.f * c_prev
-        h = self.o * numpy.tanh(self.c)
+        if isinstance(x, numpy.ndarray):
+            self.a = numpy.tanh(a)
+            self.i = _sigmoid(i)
+            self.f = _sigmoid(f)
+            self.o = _sigmoid(o)
+
+            self.c = self.a * self.i + self.f * c_prev
+            h = self.o * numpy.tanh(self.c)
+        else:
+            self.c, h = cuda.elementwise(
+                'T c_prev, T a, T i_, T f, T o', 'T c, T h',
+                '''
+                    COMMON_ROUTINE;
+                    c = aa * ai + af * c_prev;
+                    h = ao * tanh(c);
+                ''',
+                'lstm_fwd', preamble=_preamble)(c_prev, a, i, f, o)
+
         return self.c, h
 
-    def backward_cpu(self, inputs, grad_outputs):
-        c_prev = inputs[0]
+    def backward(self, inputs, grad_outputs):
+        xp = cuda.get_array_module(*inputs)
+        c_prev, x = inputs
         gc, gh = grad_outputs
 
-        gx = numpy.empty_like(inputs[1])
+        gx = xp.empty_like(x)
         ga, gi, gf, go = _extract_gates(gx)
 
         # Consider the case that either gradient is not given
@@ -91,69 +100,33 @@ class LSTM(function.Function):
         if gh is None:
             gh = 0
 
-        co = numpy.tanh(self.c)
-        gc_prev = gh * self.o * _grad_tanh(co) + gc  # multiply f later
-        ga[:] = gc_prev * self.i * _grad_tanh(self.a)
-        gi[:] = gc_prev * self.a * _grad_sigmoid(self.i)
-        gf[:] = gc_prev * c_prev * _grad_sigmoid(self.f)
-        go[:] = gh * co * _grad_sigmoid(self.o)
-        gc_prev *= self.f  # multiply f here
-
-        return gc_prev, gx
-
-    def forward_gpu(self, inputs):
-        c_prev, x = inputs
-        lsize = c_prev.shape[0] * c_prev.shape[1]
-        rsize = c_prev.size // lsize
-
-        self.c, h = cuda.elementwise(
-            'T c_prev, raw T x, int32 lsize, int32 rsize', 'T c, T h',
-            '''COMMON_ROUTINE;
-               c = aa * ai + af * c_prev;
-               h = ao * tanhf(c);''',
-            'lstm_fwd', preamble=_preamble)(c_prev, x, lsize, rsize)
-
-        return self.c, h
-
-    def backward_gpu(self, inputs, grad_outputs):
-        c_prev, x = inputs
-        gc, gh = grad_outputs
-        lsize = c_prev.shape[0] * c_prev.shape[1]
-        rsize = c_prev.size // lsize
-
-        # Odd rule to determine whether the gradient is given or not.
-        if gc is None:
-            gc = cuda.empty((0, 0, 0))
-        if gh is None:
-            gh = cuda.empty((0, 0, 0))
-
-        gc_prev = cuda.empty_like(c_prev)
-        gx = cuda.empty_like(x)
-        cuda.elementwise(
-            'T c_prev, raw T x, T c, raw T gc, raw T gh,'
-            'int32 lsize, int32 rsize',
-            'T gc_prev, raw T gx',
-            '''
-               COMMON_ROUTINE;
-               int gxi = I * 4 * rsize;
-               float& ga = gx[gxi +           J];
-               float& gi = gx[gxi +   rsize + J];
-               float& gf = gx[gxi + 2*rsize + J];
-               float& go = gx[gxi + 3*rsize + J];
-
-               float co  = tanhf(c);
-               float gc1 = (gh.size() == 0 ? 0 : gh[i] * ao * grad_tanh(co))
-                         + (gc.size() == 0 ? 0 : gc[i]);
-               go        =  gh.size() == 0 ? 0 : gh[i] * co * grad_sigmoid(ao);
-
-               gc_prev = gc1 * af;
-               ga      = gc1 * ai     * grad_tanh(aa);
-               gi      = gc1 * aa     * grad_sigmoid(ai);
-               gf      = gc1 * c_prev * grad_sigmoid(af);
-            ''',
-            'lstm_bwd', preamble=_preamble)(
-                c_prev, x, self.c, gc, gh, lsize, rsize,
-                gc_prev, gx)
+        if xp is numpy:
+            co = numpy.tanh(self.c)
+            gc_prev = gh * self.o * _grad_tanh(co) + gc  # multiply f later
+            ga[:] = gc_prev * self.i * _grad_tanh(self.a)
+            gi[:] = gc_prev * self.a * _grad_sigmoid(self.i)
+            gf[:] = gc_prev * c_prev * _grad_sigmoid(self.f)
+            go[:] = gh * co * _grad_sigmoid(self.o)
+            gc_prev *= self.f  # multiply f here
+        else:
+            a, i, f, o = _extract_gates(x)
+            gc_prev = xp.empty_like(c_prev)
+            cuda.elementwise(
+                'T c_prev, T c, T gc, T gh, T a, T i_, T f, T o',
+                'T gc_prev, T ga, T gi, T gf, T go',
+                '''
+                    COMMON_ROUTINE;
+                    T co = tanh(c);
+                    T temp = gh * ao * grad_tanh(co) + gc;
+                    ga = temp * ai * grad_tanh(aa);
+                    gi = temp * aa * grad_sigmoid(ai);
+                    gf = temp * c_prev * grad_sigmoid(af);
+                    go = gh * co * grad_sigmoid(ao);
+                    gc_prev = temp * af;
+                ''',
+                'lstm_bwd', preamble=_preamble)(
+                    c_prev, self.c, gc, gh, a, i, f, o,
+                    gc_prev, ga, gi, gf, go)
 
         return gc_prev, gx
 
