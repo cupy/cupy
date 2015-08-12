32317f469b24364bcd722c8b170d86c03a80b8da
Kenta OONO <oono@preferred.jp>
2015-08-12 21:37:21 +0900

    Add unittest of inception that checks each function is called in expected order

--- tests/functions_tests/test_inception.py
+++ tests/functions_tests/test_inception.py
@@ -6,7 +6,6 @@ import numpy
 import chainer
 from chainer import cuda
 from chainer import functions
-from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
@@ -58,8 +57,11 @@ class TestInception(unittest.TestCase):
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
 
 
-def _zeros(*shape):
-    return chainer.Variable(numpy.zeros(shape).astype(numpy.float32))
+def _ones(gpu, *shape):
+    if gpu:
+        return chainer.Variable(cuda.ones(shape).astype(numpy.float32))
+    return chainer.Variable(numpy.ones(shape).astype(numpy.float32))
+
 
 class TestInception2(unittest.TestCase):
 
@@ -69,29 +71,57 @@ class TestInception2(unittest.TestCase):
     insize = 10
 
     def setUp(self):
-        self.x = numpy.random.uniform(-1, 1, (10, self.in_channels, 5, 5)).astype(numpy.float32)
-        out = self.out1 + self.out3 + self.out5 + self.proj_pool
-        self.f = functions.Inception(self.in_channels, self.out1, self.proj3, self.out3, self.proj5, self.out5, self.proj_pool)
-        self.f.f.conv1 = mock.MagicMock(return_value=_zeros(self.batchsize, self.out1, self.insize, self.insize))
-        self.f.f.proj3 = mock.MagicMock(return_value=_zeros(self.batchsize, self.proj3, self.insize, self.insize))
-        self.f.f.conv3 = mock.MagicMock(return_value=_zeros(self.batchsize, self.out3, self.insize, self.insize))
-        self.f.f.proj5 = mock.MagicMock(return_value=_zeros(self.batchsize, self.proj5, self.insize, self.insize))
-        self.f.f.conv5 = mock.MagicMock(return_value=_zeros(self.batchsize, self.out5, self.insize, self.insize))
-        self.f.f.projp = mock.MagicMock(return_value=_zeros(self.batchsize, self.proj_pool, self.insize, self.insize))
+        self.x = numpy.random.uniform(
+            -1, 1, (10, self.in_channels, 5, 5)
+        ).astype(numpy.float32)
+        self.f = functions.Inception(
+            self.in_channels, self.out1,
+            self.proj3, self.out3,
+            self.proj5, self.out5, self.proj_pool)
+        self.f.f = mock.MagicMock()
 
-    def test_cpu(self):
-        x = chainer.Variable(self.x)
-        y = self.f(x)
-        conv3_in = functions.relu(self.f.f.proj3(x))
-        conv5_in = functions.relu(self.f.f.proj5(x))
-        projp_in = functions.max_pooling_2d(x, 3, stride=1, pad=1)
+    def setup_mock(self, gpu):
+        self.f.f.conv1.return_value = _ones(gpu,
+                                            self.batchsize, self.out1,
+                                            self.insize, self.insize)
+        self.f.f.proj3.return_value = _ones(gpu,
+                                            self.batchsize, self.proj3,
+                                            self.insize, self.insize)
+        self.f.f.conv3.return_value = _ones(gpu,
+                                            self.batchsize, self.out3,
+                                            self.insize, self.insize)
+        self.f.f.proj5.return_value = _ones(gpu,
+                                            self.batchsize, self.proj5,
+                                            self.insize, self.insize)
+        self.f.f.conv5.return_value = _ones(gpu, self.batchsize, self.out5,
+                                            self.insize, self.insize)
+        self.f.f.projp.return_value = _ones(gpu, self.batchsize,
+                                            self.proj_pool, self.insize,
+                                            self.insize)
 
-        self.f.f.conv1.assert_called_with(x)
-        self.f.f.proj3.assert_called_with(x)
-        self.f.f.conv3.assert_called_with(conv3_in)
-        self.f.f.proj5.assert_called_with(x)
-        self.f.f.conv5.assert_called_with(conv5_in)
-        self.f.f.projp.assert_called_with(projp_in)
+    def check_call(self, x, f, gpu):
+        self.setup_mock(gpu)
+        f(chainer.Variable(x))
+
+        expected = [mock.call.conv1(mock.ANY), mock.call.proj3(mock.ANY),
+                    mock.call.conv3(mock.ANY), mock.call.proj5(mock.ANY),
+                    mock.call.conv5(mock.ANY), mock.call.projp(mock.ANY)]
+
+        if gpu:
+            expected.insert(0, mock.call.to_gpu(None))
+
+        # Variable.__eq__ raises NotImplementedError,
+        # so we cannot check arguments
+        self.assertListEqual(self.f.f.mock_calls, expected)
+
+    def test_call_cpu(self):
+        self.check_call(self.x, self.f, False)
+
+    @attr.gpu
+    def test_call_gpu(self):
+        x = cuda.to_gpu(self.x)
+        self.f.to_gpu()
+        self.check_call(x, self.f, True)
 
 
 testing.run_module(__name__, __file__)
