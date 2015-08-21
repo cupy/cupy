264fa94f598b54cacbee90331a5f347003b504c9
kosuke_miyawaki <kosuke_miyawaki@dwango.co.jp>
2015-08-21 11:57:14 +0900

    functions/sum add axis option and apply cupy

--- chainer/functions/sum.py
+++ chainer/functions/sum.py
@@ -6,8 +6,10 @@ from chainer.utils import type_check
 
 
 class Sum(function.Function):
+    """Sum of array elements over a given axis."""
 
-    """Summation over all elements."""
+    def __init__(self, axis=None):
+        self.axis = axis
 
     def check_type_forward(self, in_types):
         type_check.expect(
@@ -15,20 +17,28 @@ class Sum(function.Function):
             in_types[0].dtype == numpy.float32
         )
 
-    def forward_cpu(self, x):
-        return numpy.array(x[0].sum()),
+        if self.axis is not None:
+            type_check.expect(
+                self.axis < in_types[0].ndim,
+            )
 
-    def forward_gpu(self, x):
-        return x[0].sum(),
+    def forward(self, x):
+        xp = cuda.get_array_module(*x)
+        return xp.asarray(x[0].sum(axis=self.axis)),
 
-    def backward_cpu(self, x, gy):
-        return numpy.full_like(x[0], gy[0]),
+    def backward(self, x, gy):
+        xp = cuda.get_array_module(*x)
+        if self.axis is None:
+            # TODO(beam2d): Make it async
+            return xp.full_like(x[0], gy[0]),
 
-    def backward_gpu(self, x, gy):
-        # TODO(beam2d): Make it async
-        return cuda.full_like(x[0], gy[0].get()),
+        s = list(x[0].shape)
+        s[self.axis] = 1
+        gy = gy[0].reshape(s)
+
+        return xp.concatenate([gy] * x[0].shape[self.axis], axis=self.axis),
 
 
-def sum(x):
-    """Computes sum of all elements."""
-    return Sum()(x)
+def sum(x, axis=None):
+    """Sum of array elements over a given axis."""
+    return Sum(axis)(x)
