0182e7bdc26e9177faba33ea896d3e86f72e44d4
Jiro Nishitoba <j.nshtb@gmail.com>
2015-08-21 15:45:41 +0900

    Refocterd around CuPy.

--- chainer/functions/ctc.py
+++ chainer/functions/ctc.py
@@ -15,10 +15,14 @@ class ConnectionistTemporalClassification(function.Function):
     See also
     https://blog.wtf.sg/2014/10/06/connectionist-temporal-classification-ctc-with-theano/
     '''
-    def recurrence_relation(self, size):
+    def recurrence_relation(self, size, vtype):
         big_I = numpy.eye(size+2)
-        return (numpy.eye(size) + big_I[2:, 1:-1] +
-                big_I[2:, :-2] * (numpy.arange(size) % 2))
+        rr = numpy.log((numpy.eye(size) + big_I[2:, 1:-1] +
+                        big_I[2:, :-2] * (numpy.arange(size) % 2)))
+        if vtype == numpy:
+            return rr
+        else:
+            return cuda.to_gpu(rr)
 
     def label_to_path(self, labels):
         label_length = labels.shape[0]
@@ -28,23 +32,40 @@ class ConnectionistTemporalClassification(function.Function):
             path[i * 2 + 1] = labels[i]
         return path
 
+    def log_dot(self, prob, rr):
+        vtype = cuda.cupy.get_array_module(prob)
+        res = vtype.zeros(prob.shape)
+        rtrans = vtype.swapaxes(rr, 1, 0)
+        for i in range(rtrans.shape[0]):
+            res[i] = self.logsumexp(prob + rtrans[i])
+        return res
+
+    def logsumexp(self, a):
+        vtype = cuda.cupy.get_array_module(a)
+        vmax = vtype.amax(a)
+        res = vtype.log(vtype.sum(vtype.exp(a - vmax)))
+        return (res + vmax).astype(numpy.float32)
+
+    # path probablity to label probability
+    def label_probability(self, label_size, path, multiply):
+        labels_prob = numpy.log(numpy.zeros(label_size))
+        chars = set([c for c in path])
+        for c in chars:
+            pos = numpy.where(path == c)[0]
+            labels_prob[c] = self.logsumexp(multiply[pos, ])
+        return labels_prob
+
     def forward_cpu(self, inputs):
         t = inputs[0]
         yseq = numpy.log(inputs[1::])
         path = self.label_to_path(t)
-        rr = numpy.log(self.recurrence_relation(path.shape[0]))
+        rr = self.recurrence_relation(path.shape[0],
+                                      cuda.cupy.get_array_module(yseq))
         forward_prob_trans, backward_prob_trans \
             = self.calc_trans_cpu(path, yseq, rr)
         return utils.force_array(
             - self.logsumexp(forward_prob_trans[-1]
                              + backward_prob_trans[-1])).astype(numpy.float32),
-
-    def log_dot(self, prob, rr):
-        res = numpy.zeros(prob.shape)
-        rtrans = numpy.swapaxes(rr, 1, 0)
-        for i in range(rtrans.shape[0]):
-            res[i] = self.logsumexp(prob + rtrans[i])
-        return res
 
     def calc_trans_cpu(self, path, yseq, rr):
         forward_prob = numpy.log(numpy.eye(path.shape[0])[0]+self.epsilon)
@@ -66,20 +87,12 @@ class ConnectionistTemporalClassification(function.Function):
             backward_prob = y_inv[path[::-1]] + backward_prob
         return alpha, beta[::-1]
 
-    # path probablity to label probability
-    def label_probability(self, label_size, path, multiply):
-        labels_prob = numpy.log(numpy.zeros(label_size))
-        chars = set([c for c in path])
-        for c in chars:
-            pos = numpy.where(path == c)[0]
-            labels_prob[c] = self.logsumexp(multiply[pos, ])
-        return labels_prob
-
     def backward_cpu(self, inputs, grad_output):
         labels = inputs[0]
         yseq = numpy.log(inputs[1::])
         path = self.label_to_path(labels)
-        rr = numpy.log(self.recurrence_relation(path.shape[0]))
+        rr = self.recurrence_relation(path.shape[0],
+                                      cuda.cupy.get_array_module(yseq))
         forward_prob_trans, backward_prob_trans \
             = self.calc_trans_cpu(path, yseq, rr)
         total_probability = self.logsumexp(forward_prob_trans[0]
@@ -98,18 +111,12 @@ class ConnectionistTemporalClassification(function.Function):
         t = cuda.to_cpu(inputs[0])
         yseq = inputs[1::]
         path = self.label_to_path(t)
-        rr = cuda.to_gpu(numpy.log(self.recurrence_relation(path.shape[0])))
+        rr = self.recurrence_relation(path.shape[0],
+                                      cuda.cupy.get_array_module(yseq[0]))
         forward_prob_trans, backward_prob_trans \
             = self.calc_trans_gpu(path, yseq, rr)
         return - self.logsumexp(forward_prob_trans[-1]
                                 + backward_prob_trans[-1]),
-
-    def log_dot_gpu(self, prob, rr):
-        res = cuda.cupy.zeros(prob.shape)
-        rrtrans = cuda.cupy.swapaxes(rr, 1, 0)
-        for i in range(rrtrans.shape[0]):
-            res[i] = self.logsumexp(prob + rrtrans[i])
-        return res
 
     def calc_trans_gpu(self, path, yseq, rr):
         forward_prob = cuda.cupy.log(cuda.cupy.eye(
@@ -122,14 +129,14 @@ class ConnectionistTemporalClassification(function.Function):
 
         for t in range(len(yseq)):
             # calc forward probability
-            y = numpy.log(cuda.to_cpu(yseq[t]))
+            y = cuda.to_cpu(cuda.cupy.log(yseq[t]))
             forward_prob = cuda.to_gpu(y[path]) \
-                + self.log_dot_gpu(forward_prob, rr)
+                + self.log_dot(forward_prob, rr)
             alpha += forward_prob,
 
             # calc backward probability
-            y_inv = numpy.log(cuda.to_cpu(yseq[len(yseq) - t - 1]))
-            backward_prob = self.log_dot_gpu(backward_prob, rr)
+            y_inv = cuda.to_cpu(cuda.cupy.log(yseq[len(yseq) - t - 1]))
+            backward_prob = self.log_dot(backward_prob, rr)
             beta += backward_prob[::-1],
             backward_prob = cuda.to_gpu(y_inv[path[::-1]]) + backward_prob
         return alpha, beta[::-1]
@@ -138,7 +145,8 @@ class ConnectionistTemporalClassification(function.Function):
         t = cuda.to_cpu(inputs[0])
         yseq = inputs[1::]
         path = self.label_to_path(t)
-        rr = cuda.to_gpu(numpy.log(self.recurrence_relation(path.shape[0])))
+        rr = self.recurrence_relation(path.shape[0],
+                                      cuda.cupy.get_array_module(yseq[0]))
         result = (None,)
         forward_prob_trans, backward_prob_trans\
             = self.calc_trans_gpu(path, yseq, rr)
@@ -146,7 +154,7 @@ class ConnectionistTemporalClassification(function.Function):
             self.logsumexp(forward_prob_trans[0]
                            + backward_prob_trans[0]))
         for t in range(len(yseq)):
-            y = numpy.log(cuda.to_cpu(yseq[t]))
+            y = cuda.to_cpu(cuda.cupy.log(yseq[t]))
             multiply = cuda.to_cpu(forward_prob_trans[t]
                                    + backward_prob_trans[t])
             label_prob = self.label_probability(y.shape[0], path, multiply)
@@ -155,12 +163,6 @@ class ConnectionistTemporalClassification(function.Function):
                           - (y + total_probability))).astype(numpy.float32)
                        * grad_output[0]),
         return result
-
-    def logsumexp(self, a):
-        vtype = cuda.cupy.get_array_module(a)
-        vmax = vtype.amax(a)
-        res = vtype.log(vtype.sum(vtype.exp(a - vmax)))
-        return (res + vmax).astype(numpy.float32)
 
 
 def connectionist_temporal_classification(blank_symbol, t, x):
