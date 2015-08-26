5287abf980434f53715ef708eec16305486b8860
Seiya Tokui <beam.web@gmail.com>
2015-08-26 16:01:26 +0900

    Move exp/log/sin/cos tests to appropriate places

--- tests/chainer_tests/functions_tests/math_tests/test_basic_math.py
+++ tests/chainer_tests/functions_tests/math_tests/test_basic_math.py
@@ -932,22 +932,6 @@ class UnaryFunctionsTestBase(object):
     def test_abs_forward_cpu(self):
         self.forward_cpu(lambda x: abs(x), lambda x: abs(x))
 
-    @condition.retry(3)
-    def test_exp_forward_cpu(self):
-        self.forward_cpu(F.exp, numpy.exp)
-
-    @condition.retry(3)
-    def test_log_forward_cpu(self):
-        self.forward_cpu(F.log, numpy.log)
-
-    @condition.retry(3)
-    def test_sin_forward_cpu(self):
-        self.forward_cpu(F.sin, numpy.sin)
-
-    @condition.retry(3)
-    def test_cos_forward_cpu(self):
-        self.forward_cpu(F.cos, numpy.cos)
-
     def forward_gpu(self, op, op_np):
         self.check_forward(op, op_np, cuda.to_gpu(self.x))
 
@@ -960,26 +944,6 @@ class UnaryFunctionsTestBase(object):
     @condition.retry(3)
     def test_abs_forward_gpu(self):
         self.forward_gpu(lambda x: abs(x), lambda x: abs(x))
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_exp_forward_gpu(self):
-        self.forward_gpu(F.exp, numpy.exp)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_log_forward_gpu(self):
-        self.forward_gpu(F.log, numpy.log)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_sin_forward_gpu(self):
-        self.forward_gpu(F.sin, numpy.sin)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_cos_forward_gpu(self):
-        self.forward_gpu(F.cos, numpy.cos)
 
     def check_backward(self, op, x_data, y_grad):
         x = chainer.Variable(x_data)
@@ -1004,22 +968,6 @@ class UnaryFunctionsTestBase(object):
     def test_abs_backward_cpu(self):
         self.backward_cpu(lambda x: abs(x))
 
-    @condition.retry(3)
-    def test_exp_backward_cpu(self):
-        self.backward_cpu(F.exp)
-
-    @condition.retry(3)
-    def test_log_backward_cpu(self):
-        self.backward_cpu(F.log)
-
-    @condition.retry(3)
-    def test_sin_backward_cpu(self):
-        self.backward_cpu(F.sin)
-
-    @condition.retry(3)
-    def test_cos_backward_cpu(self):
-        self.backward_cpu(F.cos)
-
     def backward_gpu(self, op):
         self.check_backward(op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
 
@@ -1032,26 +980,6 @@ class UnaryFunctionsTestBase(object):
     @condition.retry(3)
     def test_abs_backward_gpu(self):
         self.backward_gpu(lambda x: abs(x))
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_exp_backward_gpu(self):
-        self.backward_gpu(F.exp)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_log_backward_gpu(self):
-        self.backward_gpu(F.log)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_sin_backward_gpu(self):
-        self.backward_gpu(F.sin)
-
-    @attr.gpu
-    @condition.retry(3)
-    def test_cos_backward_gpu(self):
-        self.backward_gpu(F.cos)
 
 
 class TestUnaryFunctionsSimple(UnaryFunctionsTestBase, unittest.TestCase):
---
+++ tests/chainer_tests/functions_tests/math_tests/test_exponential.py
@@ -0,0 +1,106 @@
+import unittest
+
+import numpy
+
+import chainer
+from chainer import cuda
+import chainer.functions as F
+from chainer import gradient_check
+from chainer import testing
+from chainer.testing import attr
+from chainer.testing import condition
+
+
+class UnaryFunctionsTestBase(object):
+
+    def make_data(self):
+        raise NotImplementedError
+
+    def setUp(self):
+        self.x, self.gy = self.make_data()
+
+    def check_forward(self, op, op_np, x_data):
+        x = chainer.Variable(x_data)
+        y = op(x)
+        gradient_check.assert_allclose(
+            op_np(self.x), y.data, atol=1e-7, rtol=1e-7)
+
+    def check_forward_cpu(self, op, op_np):
+        self.check_forward(op, op_np, self.x)
+
+    def check_forward_gpu(self, op, op_np):
+        self.check_forward(op, op_np, cuda.to_gpu(self.x))
+
+    @condition.retry(3)
+    def test_exp_forward_cpu(self):
+        self.check_forward_cpu(F.exp, numpy.exp)
+
+    @condition.retry(3)
+    def test_log_forward_cpu(self):
+        self.check_forward_cpu(F.log, numpy.log)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_exp_forward_gpu(self):
+        self.check_forward_gpu(F.exp, numpy.exp)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_log_forward_gpu(self):
+        self.check_forward_gpu(F.log, numpy.log)
+
+    def check_backward(self, op, x_data, y_grad):
+        x = chainer.Variable(x_data)
+        y = op(x)
+        y.grad = y_grad
+        y.backward()
+
+        func = y.creator
+        f = lambda: func.forward((x.data,))
+        gx, = gradient_check.numerical_grad(f, (x.data,), (y.grad,))
+
+        gradient_check.assert_allclose(gx, x.grad)
+
+    def check_backward_cpu(self, op):
+        self.check_backward(op, self.x, self.gy)
+
+    def check_backward_gpu(self, op):
+        self.check_backward(op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+
+    @condition.retry(3)
+    def test_exp_backward_cpu(self):
+        self.check_backward_cpu(F.exp)
+
+    @condition.retry(3)
+    def test_log_backward_cpu(self):
+        self.check_backward_cpu(F.log)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_exp_backward_gpu(self):
+        self.check_backward_gpu(F.exp)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_log_backward_cpu(self):
+        self.check_backward_gpu(F.log)
+
+
+class TestUnaryFunctionsSimple(UnaryFunctionsTestBase, unittest.TestCase):
+
+    def make_data(self):
+        x = numpy.random.uniform(.5, 1, (3, 2)).astype(numpy.float32)
+        gy = numpy.random.uniform(-1, 1, (3, 2)).astype(numpy.float32)
+        return x, gy
+
+
+class TestUnaryFunctionsZeroDimension(UnaryFunctionsTestBase,
+                                      unittest.TestCase):
+
+    def make_data(self):
+        x = numpy.random.uniform(.5, 1, ()).astype(numpy.float32)
+        gy = numpy.random.uniform(-1, 1, ()).astype(numpy.float32)
+        return x, gy
+
+
+testing.run_module(__name__, __file__)
