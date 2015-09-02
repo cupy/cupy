aceb38ae0533ff0ec98ad915b9f4e2663f3ed996
Kenta OONO <oono@preferred.jp>
2015-09-02 14:55:44 +0900

    v1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='chainer',
-    version='1.2.0',
+    version='1.3.0',
     description='A flexible framework of neural networks',
     author='Seiya Tokui',
     author_email='tokui@preferred.jp',
