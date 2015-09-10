010d3501afce9ae9ae79a01d5c2e6118a9009df2
Kenta OONO <oono@preferred.jp>
2015-09-10 16:22:16 +0900

    Add unittest for rand and randn

--- tests/cupy_tests/random_tests/test_sample.py
+++ tests/cupy_tests/random_tests/test_sample.py
@@ -1,5 +1,9 @@
+import mock
 import unittest
 
+import numpy
+
+from cupy import random
 from cupy import testing
 
 
@@ -7,3 +11,22 @@ from cupy import testing
 class TestSample(unittest.TestCase):
 
     _multiprocess_can_split_ = True
+
+    def setUp(self):
+        random.random_sample = mock.Mock()
+
+    def test_rand(self):
+        random.rand(1, 2, 3, dtype=numpy.float32)
+        random.random_sample.assert_call_once_with((1, 2, 3), numpy.float32)
+
+    def test_rand_invalid_argument(self):
+        with self.assertRaises(TypeError):
+            random.rand(1, 2, 3, unnecessary='unnecessary_argument')
+
+    def test_randn(self):
+        random.randn(1, 2, 3, dtype=numpy.float32)
+        random.random_sample.assert_call_once_with((1, 2, 3), numpy.float32)
+
+    def test_randn_invalid_argument(self):
+        with self.assertRaises(TypeError):
+            random.randn(1, 2, 3, unnecessary='unnecessary_argument')
