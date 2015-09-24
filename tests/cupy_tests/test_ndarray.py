ee1f31a134e8dff9f09cfab11e2aaeb9505f9635
Kenta OONO <oono@preferred.jp>
2015-09-24 16:44:07 +0900

    Add tests for cupy.ndarray.__init__

--- tests/cupy_tests/test_ndarray.py
+++ tests/cupy_tests/test_ndarray.py
@@ -23,3 +23,14 @@ class TestGetSize(unittest.TestCase):
     def test_float(self):
         with self.assertRaises(ValueError):
             cupy._get_size(1.0)
+
+
+class TestNdarrayInit(unittest.TestCase):
+
+    def test_shape_none(self):
+        a = cupy.ndarray(None)
+        self.assertTupleEqual(a.shape, ())
+
+    def test_shape_int(self):
+        a = cupy.ndarray(3)
+        self.assertTupleEqual(a.shape, (3, ))
