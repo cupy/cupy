d1bd00f0f274265d88adf04d6f48b16f52e5eaae
Yuya Unno <unnonouno@gmail.com>
2015-10-02 16:53:28 +0900

    Support negative reshape

--- chainer/functions/array/reshape.py
+++ chainer/functions/array/reshape.py
@@ -4,7 +4,11 @@ from chainer import function
 from chainer.utils import type_check
 
 
-_type_check_prod = type_check.Variable(numpy.prod, 'prod')
+def _count_unknown_dims(shape):
+    cnt = 0
+    for dim in shape:
+        cnt += dim < 0
+    return cnt
 
 
 class Reshape(function.Function):
@@ -12,14 +16,31 @@ class Reshape(function.Function):
     """Reshapes an input array without copy."""
 
     def __init__(self, shape):
+        cnt = _count_unknown_dims(shape)
+        assert cnt == 0 or cnt == 1
+
         self.shape = shape
 
     def check_type_forward(self, in_types):
         type_check.expect(
             in_types.size() == 1,
-            _type_check_prod(in_types[0].shape) ==
-            _type_check_prod(self.shape)
         )
+
+        x_type, = in_types
+
+        cnt = _count_unknown_dims(self.shape)
+        if cnt == 0:
+            type_check.expect(
+                type_check.prod(x_type.shape) == type_check.prod(self.shape))
+        else:
+            known_size = 1
+            for s in self.shape:
+                if s > 0:
+                    known_size *= s
+            size_var = type_check.Variable(known_size,
+                                           'known_size(=%d)' % known_size)
+            type_check.expect(
+                type_check.prod(x_type.shape) % size_var == 0)
 
     def forward(self, x):
         return x[0].reshape(self.shape),
