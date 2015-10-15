b52a2018df64c48d6be6ca31f247a235fafb1d9b
Seiya Tokui <beam.web@gmail.com>
2015-10-15 17:17:25 +0900

    Add serializer

--- chainer/__init__.py
+++ chainer/__init__.py
@@ -4,14 +4,18 @@ from chainer import function
 from chainer import function_set
 from chainer.functions import basic_math
 from chainer import optimizer
+from chainer import serializer
 from chainer import variable
 
 
 __version__ = pkg_resources.get_distribution('chainer').version
 
-Variable = variable.Variable
+AbstractSerializer = chainer.AbstractSerializer
+Deserializer = chainer.Deserializer
 Function = function.Function
 FunctionSet = function_set.FunctionSet
 Optimizer = optimizer.Optimizer
+Serializer = chainer.Serializer
+Variable = variable.Variable
 
 basic_math.install_variable_arithmetics()
