99d4a1a0b98e64bc24efdcc8092f0f6d32a4ceab
Seiya Tokui <beam.web@gmail.com>
2015-10-18 23:27:50 +0900

    Add Link

--- chainer/__init__.py
+++ chainer/__init__.py
@@ -4,6 +4,7 @@ from chainer import flag
 from chainer import function
 from chainer import function_set
 from chainer.functions import basic_math
+from chainer import link
 from chainer import optimizer
 from chainer import serializer
 from chainer import variable
@@ -12,10 +13,13 @@ from chainer import variable
 __version__ = pkg_resources.get_distribution('chainer').version
 
 AbstractSerializer = serializer.AbstractSerializer
+Chain = link.Chain
+ChainList = link.ChainList
 Deserializer = serializer.Deserializer
 Flag = flag.Flag
 Function = function.Function
 FunctionSet = function_set.FunctionSet
+Link = link.Link
 Optimizer = optimizer.Optimizer
 Serializer = serializer.Serializer
 Variable = variable.Variable
