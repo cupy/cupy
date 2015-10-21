e2d59e27cdb9083eb4c268d114b577967918e973
Yuya Unno <unnonouno@gmail.com>
2015-10-21 15:23:30 +0900

    Fix test

--- tests/chainer_tests/functions_tests/activation_tests/test_gru.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_gru.py
@@ -6,6 +6,8 @@ import chainer
 from chainer import cuda
 from chainer import functions
 from chainer import gradient_check
+from chainer import testing
+from chainer.testing import attr
 
 
 def _sigmoid(x):
@@ -43,6 +45,7 @@ class TestGRU(unittest.TestCase):
     def test_forward_cpu(self):
         self.check_forward(self.h, self.x)
 
+    @attr.gpu
     def test_forward_gpu(self):
         self.func.to_gpu()
         self.check_forward(cuda.to_gpu(self.h),
@@ -62,5 +65,14 @@ class TestGRU(unittest.TestCase):
         # gradient_check.assert_allclose(gh, h.grad)
         # gradient_check.assert_allclose(gx, x.grad)
 
-    def test_backward(self):
+    def test_backward_cpu(self):
         self.check_backward(self.h, self.x, self.gy)
+
+    @attr.gpu
+    def test_backward_gpu(self):
+        self.check_backward(cuda.to_gpu(self.h),
+                            cuda.to_gpu(self.x),
+                            cuda.to_gpu(self.gy))
+
+
+testing.run_module(__name__, __file__)
