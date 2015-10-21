7c76ac9024b6971891a44ca627f1dfefd9690cc0
Seiya Tokui <beam.web@gmail.com>
2015-10-21 11:42:51 +0900

    Add LSTM and MLPConvolution2D

--- chainer/links/__init__.py
+++ chainer/links/__init__.py
@@ -7,6 +7,8 @@ from chainer.links.connection import embed_id
 from chainer.links.connection import inception
 from chainer.links.connection import inceptionbn
 from chainer.links.connection import linear
+from chainer.links.connection import lstm
+from chainer.links.connection import mlp_convolution_2d
 from chainer.links.connection import parameter
 from chainer.links.loss import hierarchical_softmax
 from chainer.links.loss import negative_sampling
@@ -21,6 +23,8 @@ EmbedID = embed_id.EmbedID
 Inception = inception.Inception
 InceptionBN = inceptionbn.InceptionBN
 Linear = linear.Linear
+LSTM = lstm.LSTM
+MLPConvolution2D = mlp_convolution_2d.MLPConvolution2D
 Parameter = parameter.Parameter
 
 BinaryHierarchicalSoftmax = hierarchical_softmax.BinaryHierarchicalSoftmax
