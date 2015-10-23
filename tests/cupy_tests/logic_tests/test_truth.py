f69b669b1a2ea958ada19b449332162806fc8612
Kenta <oono@preferred.jp>
2015-10-23 23:13:04 +0900

    Unify TestAll and TestAny. Make them parametrized.

--- tests/cupy_tests/logic_tests/test_truth.py
+++ tests/cupy_tests/logic_tests/test_truth.py
@@ -24,81 +24,60 @@ def _calc_out_shape(shape, axis, keepdims):
     return tuple(shape)
 
 
+@testing.parametrize(
+    _product({'f': ['all', 'any'],
+              'x': [numpy.arange((2, 3, 4)) - 10,
+                    numpy.zeros((2, 3, 4)),
+                    numpy.ones((2, 3, 4))],
+              'axis': [None, (0, 1, 2), 0, 1, 2, (0, 1)],
+              'keepdims': [False, True]}))
 @testing.gpu
-class TestAll(unittest.TestCase):
+class TestAllAny(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
-    def setUp(self):
-        self.f = 'all'
-
-    def check_one_axis(self, x, xp, axis):
-        a = getattr(xp, self.f)(x, axis)
-
-        out_shape = _calc_out_shape(x.shape, axis, False)
-        b = xp.empty(out_shape, dtype=x.dtype)
-        getattr(xp, self.f)(x, axis, out=b)
-
-        c = getattr(xp, self.f)(x, axis, keepdims=True)
-
-        out_shape2 = _calc_out_shape(x.shape, axis, True)
-        d = xp.empty(out_shape2, dtype=x.dtype)
-        getattr(xp, self.f)(x, axis, out=d, keepdims=True)
-        return (a, b, c, d)
-
-    def check(self, x, xp, *axes):
-        return sum([self.check_one_axis(x, xp, a) for a in axes], ())
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_list_equal()
+    def test_without_out(self, xp, dtype):
+        x = xp.asarray(self.x).astype(dtype)
+        return getattr(xp, self.f)(x, self.axis, None, self.keepdims)
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_list_equal()
-    def test_general(self, xp, dtype):
-        x = testing.shaped_arange((2, 3, 4), xp, dtype) - 10
-        return self.check(x, xp, None, (0, 1, 2), 0, 1, 2, (0, 1))
-
-    @testing.for_all_dtypes()
-    @testing.numpy_cupy_array_list_equal()
-    def test_all_zero(self, xp, dtype):
-        x = xp.zeros((2, 3, 4), dtype=dtype)
-        return self.check(x, xp, None, (0, 1, 2), 0, 1, 2, (0, 1))
-
-    @testing.for_all_dtypes()
-    @testing.numpy_cupy_array_list_equal()
-    def test_all_one(self, xp, dtype):
-        x = xp.ones((2, 3, 4), dtype=dtype)
-        return self.check(x, xp, None, (0, 1, 2), 0, 1, 2, (0, 1))
-
-    @testing.for_dtypes(
-        (numpy.float64, numpy.float32, numpy.float16, numpy.bool_))
-    @testing.numpy_cupy_array_list_equal()
-    def test_nan(self, xp, dtype):
-        x = xp.array([[numpy.nan]], dtype=dtype)
-        return self.check(x, xp, None, 0, 1, (0, 1))
-
-    @testing.for_dtypes(
-        (numpy.float64, numpy.float32, numpy.float16, numpy.bool_))
-    @testing.numpy_cupy_array_list_equal()
-    def test_nan_2(self, xp, dtype):
-        x = xp.array([[numpy.nan, 0]], dtype=dtype)
-        return self.check(x, xp, None, 0, 1, (0, 1))
-
-    @testing.for_dtypes(
-        (numpy.float64, numpy.float32, numpy.float16, numpy.bool_))
-    @testing.numpy_cupy_array_list_equal()
-    def test_nan_3(self, xp, dtype):
-        x = xp.array([[numpy.nan, 1]], dtype=dtype)
-        return self.check(x, xp, None, 0, 1, (0, 1))
-
-    @testing.for_dtypes(
-        (numpy.float64, numpy.float32, numpy.float16, numpy.bool_))
-    @testing.numpy_cupy_array_list_equal()
-    def test_nan_4(self, xp, dtype):
-        x = xp.array([[numpy.nan, 0, 1]], dtype=dtype)
-        return self.check(x, xp, None, 0, 1, (0, 1))
+    def test_with_out(self, xp, dtype):
+        x = xp.asarray(self.x).astype(dtype)
+        out_shape = _calc_out_shape(x.shape, axis, keepdims)
+        out = xp.empty(out_shape, dtype=x.dtype)
+        getattr(xp, self.f)(x, self.axis, out, self.keepdims)
+        return out
 
 
+@testing.parametrize(
+    _product({'f': ['all', 'any'],
+              'x': [numpy.array([[[numpy.nan]]]),
+                    numpy.array([[[numpy.nan, 0]]]),
+                    numpy.array([[[numpy.nan, 1]]]),
+                    numpy.array([[[numpy.nan, 0, 1]]])],
+              'axis': [None, (0, 1, 2), 0, 1, 2, (0, 1)],
+              'keepdims': [False, True]}))
 @testing.gpu
-class TestAny(TestAll):
+class TestAllAnyWithNaN(unittest.TestCase):
 
-    def setUp(self):
-        super(TestAny, self).setUp()
-        self.f = 'any'
+    _multiprocess_can_split_ = True
+
+    @testing.for_dtypes(
+        (numpy.float64, numpy.float32, numpy.float16, numpy.bool_))
+    @testing.numpy_cupy_array_list_equal()
+    def test_without_out(self, xp, dtype):
+        x = xp.asarray(self.x).astype(dtype)
+        return getattr(xp, self.f)(x, self.axis, None, self.keepdims)
+
+    @testing.for_dtypes(
+        (numpy.float64, numpy.float32, numpy.float16, numpy.bool_))
+    @testing.numpy_cupy_array_list_equal()
+    def test_with_out(self, xp, dtype):
+        x = xp.asarray(self.x).astype(dtype)
+        out_shape = _calc_out_shape(x.shape, axis, keepdims)
+        out = xp.empty(out_shape, dtype=x.dtype)
+        getattr(xp, self.f)(x, self.axis, out, self.keepdims)
+        return out
