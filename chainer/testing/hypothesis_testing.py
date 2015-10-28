8b1196a934ddcdae8288c10258c3cc3cdbc9a102
Yuya Unno <unnonouno@gmail.com>
2015-10-28 09:37:45 +0900

    Fix docstring

--- chainer/testing/hypothesis_testing.py
+++ chainer/testing/hypothesis_testing.py
@@ -7,11 +7,11 @@ def chi_square_test(observed, expected, alpha=0.05, df=None):
     Args:
         observed (list of ints): List of # of counts each element is observed.
         expected (list of floats): List of # of counts each element is expected
-        to be observed.
+            to be observed.
         alpha (float): Significance level. Currently,
-        only 0.05 and 0.01 are acceptable.
+            only 0.05 and 0.01 are acceptable.
         df (int): Degree of freedom. If ``None``,
-        it is set to the length of ``observed`` minus 1.
+            it is set to the length of ``observed`` minus 1.
 
     Returns:
         bool: ``True`` if null hypothesis is **NOT** reject.
