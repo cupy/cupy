56b4901d72bf01c590e2d7cb328240ff083e7244
Shunta Saito <shunta.saito@gmail.com>
2015-11-03 08:40:28 +0900

    Support multi-class classification task

--- chainer/functions/loss/hinge.py
+++ chainer/functions/loss/hinge.py
@@ -1,4 +1,5 @@
 import numpy
+import six
 
 from chainer import cuda
 from chainer import function
@@ -9,8 +10,8 @@ class Hinge(function.Function):
 
     """Hinge loss."""
 
-    def __init__(self, use_cudnn=True):
-        self.use_cudnn = use_cudnn
+    def __init__(self, norm='L1'):
+        self.norm = norm
 
     def check_type_forward(self, in_types):
         type_check.expect(in_types.size() == 2)
@@ -19,57 +20,114 @@ class Hinge(function.Function):
         type_check.expect(
             x_type.dtype == numpy.float32,
             t_type.dtype == numpy.int32,
-            x_type.shape == t_type.shape
+            x_type.ndim == 2,
+            t_type.ndim == 1,
+            x_type.shape[0] == t_type.shape[0],
         )
 
-    def forward(self, inputs):
-        xp = cuda.get_array_module(*inputs)
+    def forward_cpu(self, inputs):
         x, t = inputs
-        self.xt = xp.array(x * t, dtype=x.dtype)
-        loss = xp.array(xp.sum(xp.maximum(0, 1 - self.xt)), dtype=x.dtype)
-        loss /= x.shape[0]
+        num, dim = x.shape
+        self.bottom_diff = numpy.copy(x)
+        for i in six.moves.range(num):
+            self.bottom_diff[i, t[i]] *= -1
+        for i in six.moves.range(num):
+            for j in six.moves.range(dim):
+                self.bottom_diff[i, j] = max(0, 1 + self.bottom_diff[i, j])
+        loss = 0
+        if self.norm == 'L1':
+            loss = numpy.sum(self.bottom_diff) / num
+        elif self.norm == 'L2':
+            loss = numpy.sum(self.bottom_diff ** 2) / num
+        else:
+            raise NotImplementedError()
 
-        return loss,
+        return numpy.array(loss, dtype=numpy.float32),
+
+    def forward_gpu(self, inputs):
+        x, t = inputs
+        self.bottom_diff = cuda.cupy.copy(x)
+        self.bottom_diff = cuda.elementwise(
+            'S t, int32 dim', 'raw T bottom_diff',
+            'bottom_diff[i * dim + t] *= -1',
+            'hinge_fwd')(t, x.shape[1], self.bottom_diff)
+        self.bottom_diff = cuda.cupy.maximum(0, 1 + self.bottom_diff)
+        loss = 0
+        if self.norm == 'L1':
+            loss = cuda.cupy.sum(self.bottom_diff) / x.shape[0]
+        elif self.norm == 'L2':
+            loss = cuda.cupy.sum(self.bottom_diff ** 2) / x.shape[0]
+        else:
+            raise NotImplementedError()
+
+        return cuda.cupy.array(loss, dtype=cuda.cupy.float32),
 
     def backward_cpu(self, inputs, grad_outputs):
         t, gloss = inputs[1], grad_outputs[0]
-        gx = numpy.zeros_like(t, dtype=numpy.float32)
-        wrong_pos = numpy.where(self.xt < 1)
-        gx[wrong_pos] = gloss * -t[wrong_pos] / t.shape[0]
+        for i in six.moves.range(t.shape[0]):
+            self.bottom_diff[i, t[i]] *= -1
+        gx = numpy.empty_like(self.bottom_diff)
+        if self.norm == 'L1':
+            gx = gloss * numpy.sign(self.bottom_diff) / t.shape[0]
+        elif self.norm == 'L2':
+            gx = gloss * 2 * self.bottom_diff / t.shape[0]
+        else:
+            raise NotImplementedError()
 
         return gx, None
 
     def backward_gpu(self, inputs, grad_outputs):
+        xp = cuda.get_array_module(*inputs)
         t, gloss = inputs[1], grad_outputs[0]
-        gx = cuda.elementwise(
-            'T xt, S t, raw T gloss, T inv_cnt', 'T gx',
-            '''
-            if (xt < 1) gx = -gloss[0] * inv_cnt * t;
-            else gx = 0
-            ''', 'hinge_bwd')(self.xt, t, gloss, 1.0 / t.shape[0])
+        self.bottom_diff = cuda.elementwise(
+            'S t, int32 dim', 'raw T bottom_diff',
+            'bottom_diff[i * dim + t] *= -1',
+            'hinge_bwd')(t, inputs[0].shape[1], self.bottom_diff)
+        gx = xp.empty_like(self.bottom_diff)
+        if self.norm == 'L1':
+            gx = gloss * xp.sign(self.bottom_diff) / t.shape[0]
+        elif self.norm == 'L2':
+            gx = gloss * 2 * self.bottom_diff / t.shape[0]
+        else:
+            raise NotImplementedError()
 
         return gx, None
 
 
-def hinge(x, t, use_cudnn=True):
-    """Computes hinge loss as below:
+def hinge(x, t, norm='L1'):
+    """Computes the hinge loss for a one-of-many classification task.
 
         .. math::
-            L = \sum_{n=1}^N max(0, 1 - x_n t_n)
+            L = \\frac{1}{N} \\sum_{n=1}^N \\sum_{k=1}^K \\left[
+            \\max(0, 1 - \\delta\\{l_n = k\\} t_{nk}) \\right]^p
 
-        where :math:`N` denotes the batchsize.
+        where :math:`N` denotes the batchsize, :math:`K` is the number of
+        classes of interest,
+
+        .. math::
+            \\delta \\{ {\\rm condition} \\} = \\left \\{ \\begin{array}{cc}
+            1 & {\\rm if~condition} \\\\
+            -1 & {\\rm otherwise,}
+            \\end{array} \\right.
+
+        .. math::
+            p = \\left \\{ \\begin{array}{cc}
+            1 & {\\rm if~norm} = {\\rm 'L1'} \\\\
+            2 & {\\rm if~norm} = {\\rm 'L2'.}
+            \\end{array} \\right.
 
     Args:
-        x (~chainer.Variable): Input variable. The shape of ``x`` should be the
-            same as ``t``
-        t (~chainer.Variable): Corresponding labels. The shape of ``t`` should
-            be the same as ``x``
-        use_cudnn (bool): If True and CuDNN is enabled, then this function
-            uses CuDNN as the core implementation.
+        x (~chainer.Variable): Input variable. The shape of ``x`` should be
+            (:math:`N`, :math:`K`).
+        t (~chainer.Variable): The :math:`N`-dimensional label vector
+            :math:`{\\bf l}` with values :math:`l_n \in [0, 1, 2, \dots, K-1]`.
+            The shape of ``t`` should be (:math:`N`,).
+        norm (string): Specifies norm type. Only either 'L1' or 'L2' is
+            acceptable.
 
     Returns:
         ~chainer.Variable: A variable object holding a scalar array of the
-            hinge loss.
+            hinge loss :math:`L`.
 
     """
-    return Hinge(use_cudnn)(x, t)
+    return Hinge(norm)(x, t)
--- tests/chainer_tests/functions_tests/loss_tests/test_hinge.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_hinge.py
@@ -15,44 +15,53 @@ from chainer.testing import condition
 class TestHinge(unittest.TestCase):
 
     def setUp(self):
-        self.x = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
-        self.t = numpy.random.randint(0, 2, (4, 3)).astype(numpy.int32)
+        self.x = numpy.random.uniform(-1, 1, (10, 5)).astype(numpy.float32)
+        self.t = numpy.random.randint(0, 5, (10,)).astype(numpy.int32)
 
-    def check_forward(self, x_data, t_data, use_cudnn=True):
+    def check_forward(self, x_data, t_data, norm):
         x_val = chainer.Variable(x_data)
         t_val = chainer.Variable(t_data)
-        loss = functions.hinge(x_val, t_val, use_cudnn)
+        loss = functions.hinge(x_val, t_val, norm)
         self.assertEqual(loss.data.shape, ())
         self.assertEqual(loss.data.dtype, numpy.float32)
         loss_value = float(cuda.to_cpu(loss.data))
 
         # Compute expected value
-        loss_expect = 0
+        for i in six.moves.range(self.x.shape[0]):
+            self.x[i, self.t[i]] *= -1
         for i in six.moves.range(self.x.shape[0]):
             for j in six.moves.range(self.x.shape[1]):
-                xd, td = self.x[i, j], self.t[i, j]
-                loss_expect += max(0, 1.0 - xd * td)
-        loss_expect /= self.t.shape[0]
+                self.x[i, j] = max(0, 1.0 + self.x[i, j])
+        loss_expect = 0
+        if norm == 'L1':
+            loss_expect = numpy.sum(self.x) / self.x.shape[0]
+        elif norm == 'L2':
+            loss_expect += numpy.sum(self.x ** 2) / self.x.shape[0]
+
         self.assertAlmostEqual(loss_expect, loss_value, places=5)
 
     @condition.retry(3)
-    def test_forward_cpu(self):
-        self.check_forward(self.x, self.t)
+    def test_forward_cpu_l1(self):
+        self.check_forward(self.x, self.t, 'L1')
 
-    @attr.cudnn
     @condition.retry(3)
-    def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t))
+    def test_forward_cpu_l2(self):
+        self.check_forward(self.x, self.t, 'L2')
 
     @attr.gpu
     @condition.retry(3)
-    def test_forward_gpu_no_cudnn(self):
-        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t), False)
+    def test_forward_gpu_l1(self):
+        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t), 'L1')
 
-    def check_backward(self, x_data, t_data, use_cudnn=True):
+    @attr.gpu
+    @condition.retry(3)
+    def test_forward_gpu_l2(self):
+        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t), 'L2')
+
+    def check_backward(self, x_data, t_data, norm):
         x = chainer.Variable(x_data)
         t = chainer.Variable(t_data)
-        loss = functions.hinge(x, t, use_cudnn)
+        loss = functions.hinge(x, t, norm)
         loss.backward()
         self.assertEqual(None, t.grad)
 
@@ -60,21 +69,24 @@ class TestHinge(unittest.TestCase):
         f = lambda: func.forward((x.data, t.data))
         gx, = gradient_check.numerical_grad(f, (x.data,), (1,), eps=0.01)
 
-        gradient_check.assert_allclose(gx, x.grad)
+        gradient_check.assert_allclose(gx, x.grad, atol=1e-4)
 
     @condition.retry(3)
-    def test_backward_cpu(self):
-        self.check_backward(self.x, self.t)
+    def test_backward_cpu_l1(self):
+        self.check_backward(self.x, self.t, 'L1')
 
-    @attr.cudnn
     @condition.retry(3)
-    def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.t))
+    def test_backward_cpu_l2(self):
+        self.check_backward(self.x, self.t, 'L2')
 
     @attr.gpu
     @condition.retry(3)
-    def test_backward_gpu_no_cudnn(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.t), False)
+    def test_backward_gpu_l1(self):
+        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.t), 'L1')
 
+    @attr.gpu
+    @condition.retry(3)
+    def test_backward_gpu_l2(self):
+        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.t), 'L2')
 
 testing.run_module(__name__, __file__)
