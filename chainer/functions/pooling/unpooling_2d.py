7bfd6d375fff154f3b32986a421c97a707655141
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-06 13:00:41 +0900

    Support stride and pad in Unpooling2D forward

--- chainer/functions/pooling/unpooling_2d.py
+++ chainer/functions/pooling/unpooling_2d.py
@@ -5,8 +5,13 @@ import six
 
 import numpy
 
-from chainer.functions.pooling import pooling_2d
 from chainer import cuda
+from chainer.functions.pooling import pooling_2d
+from chainer.utils import conv
+
+
+def get_deconv_outsize(size, k, s, p):
+    return s * (size - 1) + k - 2 * p
 
 
 class Unpooling2D(pooling_2d.Pooling2D):
@@ -14,15 +19,23 @@ class Unpooling2D(pooling_2d.Pooling2D):
     """Unpooling over a set of 2d planes."""
 
     def forward_cpu(self, x):
-        y = x[0]
-        y = y.repeat(self.kh, axis=2)
-        y = y.repeat(self.kw, axis=3)
+        h, w = x[0].shape[2:]
+        out_h = get_deconv_outsize(h, self.kh, self.sy, self.ph)
+        out_w = get_deconv_outsize(w, self.kw, self.sx, self.pw)
+        col = numpy.tile(x[0][:, :, numpy.newaxis, numpy.newaxis],
+                         (1, 1, self.kh, self.kw, 1, 1))
+        y = conv.col2im_cpu(col, self.sy, self.sx, self.ph, self.pw,
+                            out_h, out_w)
         return y,
 
     def forward_gpu(self, x):
-        y = x[0]
-        y = y.repeat(self.kh, axis=2)
-        y = y.repeat(self.kw, axis=3)
+        h, w = x[0].shape[2:]
+        out_h = get_deconv_outsize(h, self.kh, self.sy, self.ph)
+        out_w = get_deconv_outsize(w, self.kw, self.sx, self.pw)
+        col = cuda.cupy.tile(x[0][:, :, cuda.cupy.newaxis, cuda.cupy.newaxis],
+                             (1, 1, self.kh, self.kw, 1, 1))
+        y = conv.col2im_gpu(col, self.sy, self.sx, self.ph, self.pw,
+                            out_h, out_w)
         return y,
 
     def backward_cpu(self, x, gy):
