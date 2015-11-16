b561a0742d98b2cb3de359ee54431a1be52b2774
Yuya Unno <unnonouno@gmail.com>
2015-11-16 19:52:32 +0900

    Fix import order

--- tests/chainer_tests/links_tests/connection_tests/test_gru.py
+++ tests/chainer_tests/links_tests/connection_tests/test_gru.py
@@ -4,8 +4,8 @@ import numpy
 
 import chainer
 from chainer import cuda
-from chainer import links
 from chainer import gradient_check
+from chainer import links
 from chainer import testing
 from chainer.testing import attr
 
