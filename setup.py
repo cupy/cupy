58f65daffcf6031b24ed37ddde37b93440ac9029
Ryosuke Okuta <okuta@preferred.jp>
2015-11-16 20:08:43 +0900

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
+import os
+
 from setuptools import setup
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+install_requires = [
+    'filelock',
+    'nose',
+    'numpy>=1.9.0',
+    'protobuf',
+    'six>=1.9.0']
+
+if not on_rtd:
+    install_requires.append('h5py>=2.5.0')
 
 setup(
     name='chainer',
@@ -48,12 +63,7 @@ setup(
     package_data={
         'cupy': ['carray.cuh'],
     },
-    install_requires=['filelock',
-                      'h5py>=2.5.0',
-                      'nose',
-                      'numpy>=1.9.0',
-                      'protobuf',
-                      'six>=1.9.0'],
+    install_requires=install_requires,
     # Cython is required to setup h5py, not for chainer itself.
     # This line is required for h5py-2.5.0, as `setup_requires` is missing in
     # its `setup.py` and you cannot install h5py directly.
