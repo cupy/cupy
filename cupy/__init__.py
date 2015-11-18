afa49650b7e01658a1fab776b9c7ae008df5a0fa
Ryosuke Okuta <okuta@preferred.jp>
2015-11-18 20:39:35 +0900

    Reorder import

--- cupy/__init__.py
+++ cupy/__init__.py
@@ -4,8 +4,9 @@ import sys
 import numpy
 import six
 
-from cupy import binary
 from cupy import core
+
+from cupy import binary
 from cupy import creation
 from cupy import indexing
 from cupy import io
