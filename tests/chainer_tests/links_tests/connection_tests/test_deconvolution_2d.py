a7e1a86876f4603e07a27fe992a70215aa18eb49
Kenta OONO <oono@preferred.jp>
2015-11-21 20:55:31 +0900

    Make nobias option parameter of unittests

--- tests/chainer_tests/links_tests/connection_tests/test_deconvolution_2d.py
+++ tests/chainer_tests/links_tests/connection_tests/test_deconvolution_2d.py
@@ -20,18 +20,23 @@ def _pair(x):
 
 
 @parameterize(
-    {'in_channels': 3, 'out_channels': 2, 'ksize': 3, 'stride': 2, 'pad': 1},
+    {'in_channels': 3, 'out_channels': 2, 'ksize': 3,
+     'stride': 2, 'pad': 1, 'nobias': True},
+    {'in_channels': 3, 'out_channels': 2, 'ksize': 3,
+     'stride': 2, 'pad': 1, 'nobias': False},
 )
 class TestDeconvolution2D(unittest.TestCase):
 
     def setUp(self):
         self.link = L.Deconvolution2D(
             self.in_channels, self.out_channels, self.ksize,
-            stride=self.stride, pad=self.pad)
+            stride=self.stride, pad=self.pad, nobias=self.nobias)
         self.link.W.data[...] = numpy.random.uniform(
             -1, 1, self.link.W.data.shape).astype(numpy.float32)
-        self.link.b.data[...] = numpy.random.uniform(
-            -1, 1, self.link.b.data.shape).astype(numpy.float32)
+        if not self.nobias:
+            self.link.b.data[...] = numpy.random.uniform(
+                -1, 1, self.link.b.data.shape).astype(numpy.float32)
+
         self.link.zerograds()
 
         N = 2
@@ -61,24 +66,13 @@ class TestDeconvolution2D(unittest.TestCase):
     def test_forward_consistency(self):
         self.check_forward_consistency()
 
-    @attr.cudnn
-    @condition.retry(3)
-    def test_forward_consistency_nobias(self):
-        self.check_forward_consistency(nobias=True)
-
     @attr.gpu
     @condition.retry(3)
     def test_forward_consistency_im2col(self):
         self.link.use_cudnn = False
         self.check_forward_consistency()
 
-    @attr.gpu
-    @condition.retry(3)
-    def test_forward_consistency_im2col_nobias(self):
-        self.link.use_cudnn = False
-        self.check_forward_consistency(nobias=True)
-
-    def check_backward(self, x_data, y_grad, nobias=True):
+    def check_backward(self, x_data, y_grad):
         x = chainer.Variable(x_data)
         y = self.link(x)
         y.grad = y_grad
@@ -87,35 +81,24 @@ class TestDeconvolution2D(unittest.TestCase):
         f = lambda: (self.link(x).data,)
         gx, gW = gradient_check.numerical_grad(
             f, (x.data, self.link.W.data), (y.grad,), eps=1e-2)
-        if not nobias:
-            gb = gradient_check.numerical_grad(
-                f, (self.link.b.data), (y.grad,), eps=1e-2)
+        if not self.nobias:
+            gb, = gradient_check.numerical_grad(
+                f, (self.link.b.data,), (y.grad,), eps=1e-2)
 
         gradient_check.assert_allclose(gx, x.grad)
         gradient_check.assert_allclose(gW, self.link.W.grad)
-        if not nobias:
+        if not self.nobias:
             gradient_check.assert_allclose(gb, self.link.b.grad)
 
     @condition.retry(3)
     def test_backward_cpu(self):
         self.check_backward(self.x, self.gy)
 
-    @condition.retry(3)
-    def test_backward_cpu_nobias(self):
-        self.check_backward(self.x, self.gy, nobias=True)
-
     @attr.cudnn
     @condition.retry(3)
     def test_backward_gpu(self):
         self.link.to_gpu()
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
-
-    @attr.cudnn
-    @condition.retry(3)
-    def test_backward_gpu_nobias(self):
-        self.link.to_gpu()
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy),
-                            nobias=True)
 
     @attr.gpu
     @condition.retry(3)
@@ -124,12 +107,5 @@ class TestDeconvolution2D(unittest.TestCase):
         self.link.to_gpu()
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
 
-    @attr.gpu
-    @condition.retry(3)
-    def test_backward_gpu_im2col_nobias(self):
-        self.link.use_cudnn = False
-        self.link.to_gpu()
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy),
-                            nobias=True)
 
 testing.run_module(__name__, __file__)
