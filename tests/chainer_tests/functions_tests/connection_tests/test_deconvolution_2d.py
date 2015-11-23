c8530bb9fb2d1a453bff4ed74155265c2f26ad3a
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-23 19:04:14 +0900

    Correctly run test for deconvolution_2d

--- tests/chainer_tests/functions_tests/connection_tests/test_deconvolution_2d.py
+++ tests/chainer_tests/functions_tests/connection_tests/test_deconvolution_2d.py
@@ -4,20 +4,28 @@ import numpy
 
 import chainer
 from chainer import cuda
-from chainer import functions
+import chainer.functions as F
 from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
 from chainer.testing import parameterize
+from chainer.utils import conv
+
+
+def _pair(x):
+    if hasattr(x, '__getitem__'):
+        return x
+    return (x, x)
 
 
 @parameterize(
     *testing.product({
         'in_channels': [3],
         'out_channels': [2],
+        'wscale': [1],
         'ksize': [3],
-        'stride': [2],
+        'stride': [1, 2],
         'pad': [1],
         'nobias': [True, False],
         'use_cudnn': [True, False]
@@ -26,25 +34,31 @@ from chainer.testing import parameterize
 class TestDeconvolution2DFunction(unittest.TestCase):
 
     def setUp(self, use_cudnn=True):
-        kh, kw = self.ksize, self.ksize
+        kh, kw = _pair(self.ksize)
+        sh, sw = _pair(self.stride)
+        ph, pw = _pair(self.pad)
         self.W = numpy.random.normal(
-            0, numpy.sqrt(1. / (kh * kw * self.in_channels)),
-            (self.out_channels, self.in_channels, kh, kw)
+            0, self.wscale * numpy.sqrt(1. / (kh * kw * self.in_channels)),
+            (self.in_channels, self.out_channels, kh, kw)
         ).astype(numpy.float32)
         self.b = None if self.nobias else numpy.random.uniform(
             -1, 1, self.out_channels).astype(numpy.float32)
 
-        self.x = numpy.random.uniform(-1, 1,
-                                      (2, 3, 4, 3)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1,
-                                       (2, 2, 2, 2)).astype(numpy.float32)
+        N = 2
+        inh, inw = 4, 3
+        outh = conv.get_deconv_outsize(inh, kh, sh, ph)
+        outw = conv.get_deconv_outsize(inw, kw, sw, pw)
+        self.x = numpy.random.uniform(
+            -1, 1, (N, self.in_channels, inh, inw)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+            -1, 1, (N, self.out_channels, outh, outw)).astype(numpy.float32)
 
     @attr.cudnn
     def test_forward_consistency(self):
         x_cpu = chainer.Variable(self.x)
         W_cpu = chainer.Variable(self.W)
         b_cpu = None if self.nobias else chainer.Variable(self.b)
-        y_cpu = functions.convolution_2d(
+        y_cpu = F.deconvolution_2d(
             x_cpu, W_cpu, b_cpu, stride=self.stride, pad=self.pad,
             use_cudnn=self.use_cudnn)
 
@@ -52,7 +66,7 @@ class TestDeconvolution2DFunction(unittest.TestCase):
         W_gpu = chainer.Variable(cuda.to_gpu(self.W))
         b_gpu = None if self.nobias else chainer.Variable(
             cuda.to_gpu(self.b))
-        y_gpu = functions.convolution_2d(
+        y_gpu = F.deconvolution_2d(
             x_gpu, W_gpu, b_gpu, stride=self.stride, pad=self.pad,
             use_cudnn=self.use_cudnn)
 
@@ -66,9 +80,8 @@ class TestDeconvolution2DFunction(unittest.TestCase):
         x = chainer.Variable(x_data)
         W = chainer.Variable(W_data)
         b = None if b_data is None else chainer.Variable(b_data)
-        y = functions.convolution_2d(
-            x, W, b, stride=self.stride, pad=self.pad,
-            use_cudnn=self.use_cudnn)
+        y = F.deconvolution_2d(x, W, b, stride=self.stride, pad=self.pad,
+                               use_cudnn=self.use_cudnn)
 
         y.grad = y_grad
         y.backward()
