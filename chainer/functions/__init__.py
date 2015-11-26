e591d477024894dd4a89e7eb8f6c38ae941a19d3
Yuya Unno <unnonouno@gmail.com>
2015-11-26 20:22:12 +0900

    Implement expand_dim function

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -12,6 +12,7 @@ from chainer.functions.activation import tanh
 from chainer.functions.array import broadcast
 from chainer.functions.array import concat
 from chainer.functions.array import copy
+from chainer.functions.array import expand_dims
 from chainer.functions.array import reshape
 from chainer.functions.array import select_item
 from chainer.functions.array import split_axis
@@ -88,6 +89,8 @@ Concat = concat.Concat
 concat = concat.concat
 Copy = copy.Copy
 copy = copy.copy
+ExpandDims = expand_dims.ExpandDims
+expand_dims = expand_dims.expand_dims
 Reshape = reshape.Reshape
 reshape = reshape.reshape
 SplitAxis = split_axis.SplitAxis
