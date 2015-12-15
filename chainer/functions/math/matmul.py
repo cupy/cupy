666b611064cba8278dc03c8b558890d5a097b7f0
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-12-15 13:32:51 +0900

    feedback: Set dtype expressly for chainer.cuda.cupy.empty

--- chainer/functions/math/matmul.py
+++ chainer/functions/math/matmul.py
@@ -244,8 +244,8 @@ class BatchMatMul(function.Function):
     def backward_gpu(self, x, gy):
         a, b = x
         batch_size = a.shape[0]
-        ga = cuda.cupy.empty((batch_size,) + _as_mat(a[0]).shape)
-        gb = cuda.cupy.empty((batch_size,) + _as_mat(b[0]).shape)
+        ga = cuda.cupy.empty((batch_size,) + _as_mat(a[0]).shape, a.dtype)
+        gb = cuda.cupy.empty((batch_size,) + _as_mat(b[0]).shape, a.dtype)
         _batch_matmul_gpu(
             gy[0], b, transb=not self.transb, transout=self.transa, out=ga)
         _batch_matmul_gpu(
