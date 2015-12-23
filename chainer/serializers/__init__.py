73c35a3e536d6c1e70f91d1de8e1e4cbefd68690
Yuya Unno <unnonouno@gmail.com>
2015-12-23 18:12:37 +0900

    Make hdf5 serialization optional

--- chainer/serializers/__init__.py
+++ chainer/serializers/__init__.py
@@ -1,6 +1,35 @@
-from chainer.serializers import hdf5
+def _unsupported(msg):
+    def f(*args, **nargs):
+        raise RuntimeError(msg)
+    return f
 
-HDF5Serializer = hdf5.HDF5Serializer
-HDF5Deserializer = hdf5.HDF5Deserializer
-save_hdf5 = hdf5.save_hdf5
-load_hdf5 = hdf5.load_hdf5
+
+def _unsupported_class(msg):
+
+    class _Unsupported(object):
+
+        def __init__(self, *args, **vargs):
+            raise RuntimeError(msg)
+
+    return _Unsupported
+
+
+try:
+    from chainer.serializers import hdf5
+
+    HDF5Serializer = hdf5.HDF5Serializer
+    HDF5Deserializer = hdf5.HDF5Deserializer
+    save_hdf5 = hdf5.save_hdf5
+    load_hdf5 = hdf5.load_hdf5
+
+except ImportError:
+
+    msg = '''h5py is not installed on your environment.
+Please install h5py to activate hdf5 serializers.
+
+  $ pip install h5py
+    '''
+    HDF5Serializer = _unsupported_class(msg)
+    HDF5Deserializer = _unsupported_class(msg)
+    save_hdf5 = _unsupported(msg)
+    load_hdf5 = _unsupported(msg)
