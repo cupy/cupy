a3862992cd78fe9a1f9fc49a417b958d2ebf1208
Kenta OONO <k.oono.delta@gmail.com>
2015-12-26 15:45:37 +0900

    Expose cupy.testing.for_dtypes_combination

--- cupy/testing/__init__.py
+++ cupy/testing/__init__.py
@@ -26,6 +26,7 @@ for_float_dtypes = helper.for_float_dtypes
 for_signed_dtypes = helper.for_signed_dtypes
 for_unsigned_dtypes = helper.for_unsigned_dtypes
 for_int_dtypes = helper.for_int_dtypes
+for_dtypes_combination = helper.for_dtypes_combination
 for_all_dtypes_combination = helper.for_all_dtypes_combination
 for_signed_dtypes_combination = helper.for_signed_dtypes_combination
 for_unsigned_dtypes_combination = helper.for_unsigned_dtypes_combination
