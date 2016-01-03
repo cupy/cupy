c1d79ec0a7166774d885f786c3fea927fa960cc6
EdwardRaff <Raff.Edward@gmail.com>
2016-01-03 14:12:40 -0500

    Adding copy of the Keras intializations methods (also MIT)

---
+++ chainer/initializations.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import
+import numpy as np
+from . import backend as K
+
+
+#Original code forked from MIT licensed keras project https://github.com/fchollet/keras/blob/master/keras/initializations.py
+
+def get_fans(shape):
+    fan_in = shape[0] if len(shape) == 2 else np.prod(shape[1:])
+    fan_out = shape[1] if len(shape) == 2 else shape[0]
+    return fan_in, fan_out
+
+
+def uniform(shape, scale=0.05):
+    return K.variable(np.random.uniform(low=-scale, high=scale, size=shape))
+
+
+def normal(shape, scale=0.05):
+    return K.variable(np.random.normal(loc=0.0, scale=scale, size=shape))
+
+
+def lecun_uniform(shape):
+    ''' Reference: LeCun 98, Efficient Backprop
+        http://yann.lecun.com/exdb/publis/pdf/lecun-98b.pdf
+    '''
+    fan_in, fan_out = get_fans(shape)
+    scale = np.sqrt(3. / fan_in)
+    return uniform(shape, scale)
+
+
+def glorot_normal(shape):
+    ''' Reference: Glorot & Bengio, AISTATS 2010
+    '''
+    fan_in, fan_out = get_fans(shape)
+    s = np.sqrt(2. / (fan_in + fan_out))
+    return normal(shape, s)
+
+
+def glorot_uniform(shape):
+    fan_in, fan_out = get_fans(shape)
+    s = np.sqrt(6. / (fan_in + fan_out))
+    return uniform(shape, s)
+
+
+def he_normal(shape):
+    ''' Reference:  He et al., http://arxiv.org/abs/1502.01852
+    '''
+    fan_in, fan_out = get_fans(shape)
+    s = np.sqrt(2. / fan_in)
+    return normal(shape, s)
+
+
+def he_uniform(shape):
+    fan_in, fan_out = get_fans(shape)
+    s = np.sqrt(6. / fan_in)
+    return uniform(shape, s)
+
+
+def orthogonal(shape, scale=1.1):
+    ''' From Lasagne. Reference: Saxe et al., http://arxiv.org/abs/1312.6120
+    '''
+    flat_shape = (shape[0], np.prod(shape[1:]))
+    a = np.random.normal(0.0, 1.0, flat_shape)
+    u, _, v = np.linalg.svd(a, full_matrices=False)
+    # pick the one with the correct shape
+    q = u if u.shape == flat_shape else v
+    q = q.reshape(shape)
+    return K.variable(scale * q[:shape[0], :shape[1]])
+
+
+def identity(shape, scale=1):
+    if len(shape) != 2 or shape[0] != shape[1]:
+        raise Exception('Identity matrix initialization can only be used '
+                        'for 2D square matrices.')
+    else:
+        return K.variable(scale * np.identity(shape[0]))
+
+
+def zero(shape):
+    return K.zeros(shape)
+
+
+def one(shape):
+    return K.ones(shape)
+
+
+from .utils.generic_utils import get_from_module
+def get(identifier):
+    return get_from_module(identifier, globals(), 'initialization')
