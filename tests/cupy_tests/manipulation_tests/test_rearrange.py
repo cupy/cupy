006e3185f21908311ef180c494f5370d353fc9c2
Yuya Unno <unnonouno@gmail.com>
2016-01-05 23:50:49 +0900

    Write test for roll

--- tests/cupy_tests/manipulation_tests/test_rearrange.py
+++ tests/cupy_tests/manipulation_tests/test_rearrange.py
@@ -7,3 +7,27 @@ from cupy import testing
 class TestRearrange(unittest.TestCase):
 
     _multiprocess_can_split_ = True
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal()
+    def test_roll(self, xp, dtype):
+        x = xp.arange(10)
+        return xp.roll(x, 2)
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal()
+    def test_roll2(self, xp, dtype):
+        x = testing.shaped_arange((5, 2), xp, dtype)
+        return xp.roll(x, 1)
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal()
+    def test_roll_with_axis(self, xp, dtype):
+        x = testing.shaped_arange((5, 2), xp, dtype)
+        return xp.roll(x, 1, axis=0)
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_raises()
+    def test_roll_invalid_axis(self, xp, dtype):
+        x = testing.shaped_arange((5, 2), xp, dtype)
+        return xp.roll(x, 1, axis=3)
