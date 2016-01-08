752ddb924461e04fdff2831315502c69eab09678
Kenta OONO <k.oono.delta@gmail.com>
2016-01-08 11:55:32 +0900

    Add unittests for numpy_cupy decorators

--- tests/cupy_tests/testing_tests/test_helper.py
+++ tests/cupy_tests/testing_tests/test_helper.py
@@ -3,6 +3,8 @@ import unittest
 
 import numpy
 
+import cupy
+from cupy import testing
 from cupy.testing import helper
 
 
@@ -78,3 +80,140 @@ class TestCheckCupyNumpyError(unittest.TestCase):
             helper._check_cupy_numpy_error(
                 self, cupy_error, cupy_tb,
                 numpy_error, numpy_tb, accept_error=False)
+
+
+def make_func(np_result, cp_result):
+    def f(self, xp):
+        if xp == numpy:
+            return np_result
+        elif xp == cupy:
+            return cp_result
+    return f
+
+
+def make_func_foo(np_result, cp_result):
+    def f(self, foo):
+        if foo == numpy:
+            return np_result
+        elif foo == cupy:
+            return cp_result
+    return f
+
+
+class NumPyCuPyDecoratorBase(object):
+
+    def test_valid(self):
+        decorator = getattr(testing, self.decorator)()
+        decorated_func = decorator(self.valid_func)
+        decorated_func(self)
+
+    def test_invalid(self):
+        decorator = getattr(testing, self.decorator)()
+        decorated_func = decorator(self.invalid_func)
+        with self.assertRaises(AssertionError):
+            decorated_func(self)
+
+    def test_name(self):
+        decorator = getattr(testing, self.decorator)(name='foo')
+        decorated_func = decorator(self.strange_kw_func)
+        decorated_func(self)
+
+
+def numpy_error(self, xp):
+    if xp == numpy:
+        raise ValueError()
+    elif xp == cupy:
+        return cupy.array(1)
+
+
+def cupy_error(self, xp):
+    if xp == numpy:
+        return numpy.array(1)
+    elif xp == cupy:
+        raise ValueError()
+
+
+class NumPyCuPyDecoratorBase2(object):
+
+    def test_accept_error_numpy(self):
+        decorator = getattr(testing, self.decorator)(accept_error=False)
+        decorated_func = decorator(numpy_error)
+        with self.assertRaises(AssertionError):
+            decorated_func(self)
+
+    def test_accept_error_cupy(self):
+        decorator = getattr(testing, self.decorator)(accept_error=False)
+        decorated_func = decorator(cupy_error)
+        with self.assertRaises(AssertionError):
+            decorated_func(self)
+
+
+@testing.parameterize(
+    {'decorator': 'numpy_cupy_allclose'},
+    {'decorator': 'numpy_cupy_array_almost_equal'},
+    {'decorator': 'numpy_cupy_array_almost_equal_nulp'},
+    {'decorator': 'numpy_cupy_array_max_ulp'},
+    {'decorator': 'numpy_cupy_array_equal'}
+)
+class TestNumPyCuPyEqual(unittest.TestCase, NumPyCuPyDecoratorBase,
+                         NumPyCuPyDecoratorBase2):
+
+    def setUp(self):
+        self.valid_func = make_func(numpy.array(1), cupy.array(1))
+        self.invalid_func = make_func(numpy.array(1), cupy.array(2))
+        self.strange_kw_func = make_func_foo(numpy.array(1), cupy.array(1))
+
+
+@testing.parameterize(
+    {'decorator': 'numpy_cupy_array_list_equal'}
+)
+class TestNumPyCuPyListEqual(unittest.TestCase, NumPyCuPyDecoratorBase):
+
+    def setUp(self):
+        self.valid_func = make_func([numpy.array(1)], [cupy.array(1)])
+        self.invalid_func = make_func([numpy.array(1)], [cupy.array(2)])
+        self.strange_kw_func = make_func_foo([numpy.array(1)], [cupy.array(1)])
+
+
+@testing.parameterize(
+    {'decorator': 'numpy_cupy_array_less'}
+)
+class TestNumPyCuPyLess(unittest.TestCase, NumPyCuPyDecoratorBase,
+                        NumPyCuPyDecoratorBase2):
+
+    def setUp(self):
+        self.valid_func = make_func(numpy.array(2), cupy.array(1))
+        self.invalid_func = make_func(numpy.array(1), cupy.array(2))
+        self.strange_kw_func = make_func_foo(
+            numpy.array(2), cupy.array(1))
+
+
+def numpy_cupy_raise(self, xp):
+    raise ValueError()
+
+
+def numpy_cupy_raise_foo(self, foo):
+    raise ValueError()
+
+
+@testing.parameterize(
+    {'decorator': 'numpy_cupy_raises'}
+)
+class TestNumPyCuPyRaise(unittest.TestCase, NumPyCuPyDecoratorBase):
+
+    def setUp(self):
+        self.valid_func = numpy_cupy_raise
+        self.invalid_func = make_func(numpy.array(1), cupy.array(1))
+        self.strange_kw_func = numpy_cupy_raise_foo
+
+    def test_accept_error_numpy(self):
+        decorator = getattr(testing, self.decorator)()
+        decorated_func = decorator(numpy_error)
+        with self.assertRaises(AssertionError):
+            decorated_func(self)
+
+    def test_accept_error_cupy(self):
+        decorator = getattr(testing, self.decorator)()
+        decorated_func = decorator(cupy_error)
+        with self.assertRaises(AssertionError):
+            decorated_func(self)
