f6303a599bea4320b21ea9294e48b96992bb710f
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-02-05 19:39:29 +0900

    Unify the code for cpu and gpu on Unpooling2D

--- chainer/functions/pooling/unpooling_2d.py
+++ chainer/functions/pooling/unpooling_2d.py
@@ -37,39 +37,30 @@ class Unpooling2D(pooling_2d.Pooling2D):
                 conv.get_conv_outsize(self.outw, self.kw, self.sx, self.pw),
             )
 
-    def forward_cpu(self, x):
+    def forward(self, x):
         h, w = x[0].shape[2:]
         if self.outh is None:
             self.outh = conv.get_deconv_outsize(h, self.kh, self.sy, self.ph)
         if self.outw is None:
             self.outw = conv.get_deconv_outsize(w, self.kw, self.sx, self.pw)
-        col = numpy.tile(x[0][:, :, numpy.newaxis, numpy.newaxis],
-                         (1, 1, self.kh, self.kw, 1, 1))
-        y = conv.col2im_cpu(col, self.sy, self.sx, self.ph, self.pw,
-                            self.outh, self.outw)
+        xp = cuda.get_array_module(x)
+        col = xp.tile(x[0][:, :, xp.newaxis, xp.newaxis],
+                      (1, 1, self.kh, self.kw, 1, 1))
+        if any(isinstance(xi, cuda.ndarray) for xi in x):
+            y = conv.col2im_gpu(col, self.sy, self.sx, self.ph, self.pw,
+                                self.outh, self.outw)
+        else:
+            y = conv.col2im_cpu(col, self.sy, self.sx, self.ph, self.pw,
+                                self.outh, self.outw)
         return y,
 
-    def forward_gpu(self, x):
-        h, w = x[0].shape[2:]
-        if self.outh is None:
-            self.outh = conv.get_deconv_outsize(h, self.kh, self.sy, self.ph)
-        if self.outw is None:
-            self.outw = conv.get_deconv_outsize(w, self.kw, self.sx, self.pw)
-        col = cuda.cupy.tile(x[0][:, :, cuda.cupy.newaxis, cuda.cupy.newaxis],
-                             (1, 1, self.kh, self.kw, 1, 1))
-        y = conv.col2im_gpu(col, self.sy, self.sx, self.ph, self.pw,
-                            self.outh, self.outw)
-        return y,
-
-    def backward_cpu(self, x, gy):
-        gcol = conv.im2col_cpu(
-            gy[0], self.kh, self.kw, self.sy, self.sx, self.ph, self.pw)
-        gx = gcol.sum(axis=(2, 3))
-        return gx,
-
-    def backward_gpu(self, x, gy):
-        gcol = conv.im2col_gpu(
-            gy[0], self.kh, self.kw, self.sy, self.sx, self.ph, self.pw)
+    def backward(self, x, gy):
+        if any(isinstance(xi, cuda.ndarray) for xi in x):
+            gcol = conv.im2col_gpu(
+                gy[0], self.kh, self.kw, self.sy, self.sx, self.ph, self.pw)
+        else:
+            gcol = conv.im2col_cpu(
+                gy[0], self.kh, self.kw, self.sy, self.sx, self.ph, self.pw)
         gx = gcol.sum(axis=(2, 3))
         return gx,
 
