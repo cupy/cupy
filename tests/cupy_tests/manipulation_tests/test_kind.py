28c809310dbb15db33d096de9927f3f822c56fdf
Ryosuke Okuta <okuta@preferred.jp>
2016-02-18 19:47:39 +0900

    Add test

--- tests/cupy_tests/manipulation_tests/test_kind.py
+++ tests/cupy_tests/manipulation_tests/test_kind.py
@@ -19,3 +19,9 @@ class TestKind(unittest.TestCase):
     def test_asfortranarray2(self, xp, dtype):
         x = xp.zeros((2, 3, 4))
         return xp.asfortranarray(x).strides
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal(type_check=False)
+    def test_asfortranarray3(self, xp, dtype):
+        x = xp.zeros((2, 3, 4))
+        return xp.asfortranarray(xp.asfortranarray(x)).strides
