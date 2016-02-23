745b98aa465bc05500ffd893c15844cb454011f9
Ryosuke Okuta <okuta@preferred.jp>
2016-02-23 15:47:54 +0900

    Improve tests

--- tests/cupy_tests/manipulation_tests/test_kind.py
+++ tests/cupy_tests/manipulation_tests/test_kind.py
@@ -11,17 +11,26 @@ class TestKind(unittest.TestCase):
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal(type_check=False)
     def test_asfortranarray1(self, xp, dtype):
-        x = xp.zeros((2, 3))
-        return xp.asfortranarray(x).strides
+        x = xp.zeros((2, 3), dtype)
+        ret = xp.asfortranarray(x)
+        self.assertTrue(x.flags.c_contiguous)
+        self.assertTrue(ret.flags.f_contiguous)
+        return ret.strides
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal(type_check=False)
     def test_asfortranarray2(self, xp, dtype):
-        x = xp.zeros((2, 3, 4))
-        return xp.asfortranarray(x).strides
+        x = xp.zeros((2, 3, 4), dtype)
+        ret = xp.asfortranarray(x)
+        self.assertTrue(x.flags.c_contiguous)
+        self.assertTrue(ret.flags.f_contiguous)
+        return ret.strides
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal(type_check=False)
     def test_asfortranarray3(self, xp, dtype):
-        x = xp.zeros((2, 3, 4))
-        return xp.asfortranarray(xp.asfortranarray(x)).strides
+        x = xp.zeros((2, 3, 4), dtype)
+        ret = xp.asfortranarray(xp.asfortranarray(x))
+        self.assertTrue(x.flags.c_contiguous)
+        self.assertTrue(ret.flags.f_contiguous)
+        return ret.strides
