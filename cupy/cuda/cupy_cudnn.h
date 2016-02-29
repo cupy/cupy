c71b2a1ec0decc461b02231e1a9212a141a7a379
Ryosuke Okuta <okuta@preferred.jp>
2016-02-29 17:46:15 +0900

    Change type

--- cupy/cuda/cupy_cudnn.h
+++ cupy/cuda/cupy_cudnn.h
@@ -13,10 +13,10 @@
 // ***_v3 functions are not declared in cuDNN v4.
 // Following definitions are for compatibility with cuDNN v2 and v3.
 
-typedef void* cudnnConvolutionBwdDataAlgo_t;
-typedef void* cudnnConvolutionBwdDataPreference_t;
-typedef void* cudnnConvolutionBwdFilterAlgo_t;
-typedef void* cudnnConvolutionBwdFilterPreference_t;
+typedef int cudnnConvolutionBwdDataAlgo_t;
+typedef int cudnnConvolutionBwdDataPreference_t;
+typedef int cudnnConvolutionBwdFilterAlgo_t;
+typedef int cudnnConvolutionBwdFilterPreference_t;
 
 cudnnStatus_t cudnnConvolutionBackwardFilter_v3(
         cudnnHandle_t handle, const void* alpha,
