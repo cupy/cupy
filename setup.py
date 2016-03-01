3caf9d24bdbbd806e5cf04f4cbaac4b6ca0bd904
Kenta OONO <oono@preferred.jp>
2016-03-01 18:15:58 +0900

    Update version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ chainer_setup_build.parse_args()
 
 setup(
     name='chainer',
-    version='1.6.2.1',
+    version='1.7.0',
     description='A flexible framework of neural networks',
     author='Seiya Tokui',
     author_email='tokui@preferred.jp',
