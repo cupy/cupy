0ba46c83fa0ca03d5c5804a43f570da365412619
Ryosuke Okuta <okuta@preferred.jp>
2016-03-01 17:40:04 +0900

    Fix tests

--- tests/chainer_tests/functions_tests/loss_tests/test_ctc.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_ctc.py
@@ -20,14 +20,9 @@ class TestCTC(unittest.TestCase):
         self.l = numpy.array([[2, 0, 2, 1, 2],
                               [2, 1, 2, 0, 2]]).astype(numpy.int32)
         self.blank_symbol = 2
-        self.g = numpy.array(0.1, dtype=numpy.float32)
-        self.gx = self.g
-        self.x_length = numpy.full((len(self.x[0]),),
-                                   len(self.x),
-                                   dtype='i')
-        self.l_length = numpy.full((len(self.t),),
-                                   len(self.t[0]),
-                                   dtype='i')
+        self.x_length = numpy.full((len(self.x[0]),), len(self.x), dtype='i')
+        self.l_length = numpy.full((len(self.t),), len(self.t[0]), dtype='i')
+        self.use_length = True
 
     # recursive forward computation.
     def alpha(self, x, l, t, u):
@@ -41,21 +36,23 @@ class TestCTC(unittest.TestCase):
             else:
                 return 0.0
         elif l[u] == self.blank_symbol or l[u] == l[u-2]:
-            return x[t][l[u]] * \
-                (self.alpha(x, l, t-1, u-1) + self.alpha(x, l, t-1, u))
+            return (x[t][l[u]] *
+                    (self.alpha(x, l, t - 1, u - 1) +
+                     self.alpha(x, l, t - 1, u)))
         else:
-            return x[t][l[u]] * \
-                (self.alpha(x, l, t-1, u-2) +
-                 self.alpha(x, l, t-1, u-1) +
-                 self.alpha(x, l, t-1, u))
+            return (x[t][l[u]] *
+                    (self.alpha(x, l, t - 1, u - 2) +
+                     self.alpha(x, l, t - 1, u - 1) +
+                     self.alpha(x, l, t - 1, u)))
 
     def check_forward(self, t_data, xs_data, l_length, x_length):
         x = tuple(chainer.Variable(x_data) for x_data in xs_data)
         t = chainer.Variable(t_data)
-        loss = functions.connectionist_temporal_classification(
-            x, t, 2,
-            input_length=chainer.Variable(x_length),
-            label_length=chainer.Variable(l_length))
+
+        args = (x, t, 2)
+        if self.use_length:
+            args += (chainer.Variable(x_length), chainer.Variable(l_length))
+        loss = functions.connectionist_temporal_classification(*args)
         loss_value = float(loss.data)
 
         # compute expected value by recursive computation.
@@ -68,21 +65,17 @@ class TestCTC(unittest.TestCase):
         batch_size = xt.shape[0]
         path_length = 2 * l_length + 1
         for b in range(batch_size):
-            loss_expect += -math.log(self.alpha(xt[b],
-                                                self.l[b],
-                                                x_length[b]-1,
-                                                path_length[b]-1)
-                                     + self.alpha(xt[b],
-                                                  self.l[b],
-                                                  x_length[b]-1,
-                                                  path_length[b]-2))
+            loss_expect += -math.log(
+                self.alpha(
+                    xt[b], self.l[b], x_length[b] - 1, path_length[b] - 1)
+                + self.alpha(
+                    xt[b], self.l[b], x_length[b] - 1, path_length[b] - 2))
         loss_expect /= batch_size
         self.assertAlmostEqual(loss_expect, loss_value, places=5)
 
     def test_forward_cpu(self):
         self.check_forward(self.t, tuple(x_data for x_data in self.x),
-                           self.l_length,
-                           self.x_length)
+                           self.l_length, self.x_length)
 
     @attr.gpu
     def test_forward_gpu(self):
@@ -92,33 +85,15 @@ class TestCTC(unittest.TestCase):
                            cuda.to_gpu(self.x_length))
 
     # expected value(via numerical differentiation) from t_data
-    def check_backward(self, t_data, xs_data, l_length, x_length, grad, gx):
-        xs = tuple(chainer.Variable(x_data) for x_data in xs_data)
-        t = chainer.Variable(t_data)
-
-        loss = functions.connectionist_temporal_classification(
-            xs, t, 2,
-            input_length=chainer.Variable(x_length),
-            label_length=chainer.Variable(l_length))
-
-        loss.grad = grad
-        loss.backward()
-
-        func = loss.creator
-        xs_data = tuple(x.data for x in xs)
-        f = lambda: func.forward((x_length,
-                                  l_length, t.data,) + xs_data)
-        gx_0, gx_1, gx_2, gx_3 = gradient_check.numerical_grad(
-            f, (xs_data), (gx,))
-        gradient_check.assert_allclose(xs[0].grad, gx_0, atol=1e-04)
-        gradient_check.assert_allclose(xs[1].grad, gx_1, atol=1e-04)
-        gradient_check.assert_allclose(xs[2].grad, gx_2, atol=1e-04)
-        gradient_check.assert_allclose(xs[3].grad, gx_3, atol=1e-04)
+    def check_backward(self, t_data, xs_data, l_length, x_length):
+        gradient_check.check_backward(
+            functions.ConnectionistTemporalClassification(self.blank_symbol),
+            (x_length, l_length, t_data) + xs_data, None, eps=1e-2)
 
     @condition.retry(3)
     def test_backward_cpu(self):
         self.check_backward(self.t, tuple(x_data for x_data in self.x),
-                            self.l_length, self.x_length, self.g, self.gx)
+                            self.l_length, self.x_length)
 
     @condition.retry(3)
     @attr.gpu
@@ -126,9 +101,24 @@ class TestCTC(unittest.TestCase):
         self.check_backward(cuda.to_gpu(self.t),
                             tuple(cuda.to_gpu(x_data) for x_data in self.x),
                             cuda.to_gpu(self.l_length),
-                            cuda.to_gpu(self.x_length),
-                            cuda.to_gpu(self.g),
-                            cuda.to_gpu(self.gx))
+                            cuda.to_gpu(self.x_length))
+
+
+class TestCTCWithoutLength(TestCTC):
+
+    def setUp(self):
+        self.x = numpy.random.uniform(-1, 1, (4, 2, 3)).astype(numpy.float32)
+        self.t = numpy.array([[0, 1], [1, 0]]).astype(numpy.int32)
+        self.l = numpy.array([[2, 0, 2, 1, 2],
+                              [2, 1, 2, 0, 2]]).astype(numpy.int32)
+        self.blank_symbol = 2
+        self.x_length = numpy.full((len(self.x[0]),),
+                                   len(self.x),
+                                   dtype='i')
+        self.l_length = numpy.full((len(self.t),),
+                                   len(self.t[0]),
+                                   dtype='i')
+        self.use_length = False
 
 
 class TestCTCWithLabelPadding(TestCTC):
@@ -139,8 +129,6 @@ class TestCTCWithLabelPadding(TestCTC):
         self.l = numpy.array([[2, 0, 2, 3, 2],
                               [2, 1, 2, 0, 2]]).astype(numpy.int32)
         self.blank_symbol = 2
-        self.g = numpy.array(0.1, dtype=numpy.float32)
-        self.gx = self.g
         self.x_length = numpy.full((len(self.x[0]),),
                                    len(self.x),
                                    dtype=numpy.int32)
@@ -148,6 +136,7 @@ class TestCTCWithLabelPadding(TestCTC):
                                    len(self.t[0]),
                                    dtype=numpy.int32)
         self.l_length[0] = 1
+        self.use_length = True
 
 
 class TestCTCWithInputPadding(TestCTC):
@@ -158,8 +147,6 @@ class TestCTCWithInputPadding(TestCTC):
         self.l = numpy.array([[2, 0, 2, 1, 2],
                               [2, 1, 2, 0, 2]]).astype(numpy.int32)
         self.blank_symbol = 2
-        self.g = numpy.array(0.1, dtype=numpy.float32)
-        self.gx = self.g
         self.x_length = numpy.full((len(self.x[0]),),
                                    len(self.x),
                                    dtype=numpy.int32)
@@ -167,6 +154,7 @@ class TestCTCWithInputPadding(TestCTC):
         self.l_length = numpy.full((len(self.t),),
                                    len(self.t[0]),
                                    dtype=numpy.int32)
+        self.use_length = True
 
 
 class TestCTCWithAllPadding(TestCTC):
@@ -177,8 +165,6 @@ class TestCTCWithAllPadding(TestCTC):
         self.l = numpy.array([[2, 0, 2, 1, 2],
                               [2, 1, 2, 0, 2]]).astype(numpy.int32)
         self.blank_symbol = 2
-        self.g = numpy.array(0.1, dtype=numpy.float32)
-        self.gx = self.g
         self.x_length = numpy.full((len(self.x[0]),),
                                    len(self.x),
                                    dtype=numpy.int32)
@@ -189,6 +175,7 @@ class TestCTCWithAllPadding(TestCTC):
                                    dtype=numpy.int32)
         self.l_length[0] = 1
         self.l_length[1] = 1
+        self.use_length = True
 
 
 class TestCTCError(unittest.TestCase):
