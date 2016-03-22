6e2a94e80f33844a1e7713169f77947a698d5884
Ryosuke Okuta <okuta@preferred.jp>
2016-03-22 16:26:09 +0900

    Fix installation for old cl compiler

---
+++ cupy/cuda/cupy_stdint.h
@@ -0,0 +1,16 @@
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
+
+typedef signed __int8     int8_t;
+typedef signed __int16    int16_t;
+typedef signed __int32    int32_t;
+typedef unsigned __int8   uint8_t;
+typedef unsigned __int16  uint16_t;
+typedef unsigned __int32  uint32_t;
+typedef signed __int64       int64_t;
+typedef unsigned __int64     uint64_t;
+
+#else // #if defined(_MSC_VER) && (_MSC_VER < 1600)
+
+#include <stdint.h>
+
+#endif // #if defined(_MSC_VER) && (_MSC_VER < 1600)
