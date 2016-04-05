ae95e10b1a98f6987f1f227395c1eb290adfe608
Yuya Unno <unnonouno@gmail.com>
2016-04-06 01:56:37 +0900

    Add description

--- chainer/links/loss/crf1d.py
+++ chainer/links/loss/crf1d.py
@@ -4,6 +4,20 @@ from chainer import link
 
 class CRF1d(link.Link):
 
+    """Linear-chain conditional random field loss layer.
+
+    This link wraps the :func:`~chainer.functions.crf1d` function.
+    It holds a transition cost matrix as a parameter.
+
+    Args:
+        n_label (int): Number of labels.
+
+    .. seealso:: :func:`~chainer.functions.crf1d` for more detail.
+
+    Attributes:
+        cost (~chainer.Variable): Transition cost parameter.
+    """
+
     def __init__(self, n_label):
         super(CRF1d, self).__init__(cost=(n_label, n_label))
         self.cost.data[...] = 0
