7c8abc95a0d05d39ae153f592be7acd587ca375c
hamatz <makoto.hamatsu@guest2-76.csl.sony.co.jp>
2016-04-08 15:53:07 +0900

    Fixed genetext.py for supporting GPU
    Fixed genetext.py for supporting GPU

--- examples/ptb/genetxt.py
+++ examples/ptb/genetxt.py
@@ -3,10 +3,10 @@
 
 """ Sample script of recurrent neural network language model
     for generating text
-
+    
     This code is ported from following implementation.
     https://github.com/longjie/chainer-char-rnn/blob/master/sample.py
-
+    
     """
 import argparse
 import sys
@@ -21,7 +21,6 @@ import chainer.links as L
 from chainer import serializers
 from chainer.variable import Variable
 
-
 import net
 
 parser = argparse.ArgumentParser()
@@ -29,7 +28,7 @@ parser.add_argument('--model', '-m', type=str, required=True,
                     help='model data, saved by train_ptb.py')
 parser.add_argument('--vocabulary', '-v', type=str, required=True,
                     help='vocabulary data, saved by train_ptb.py')
-parser.add_argument('--primetext', '-p', type=str, default='',
+parser.add_argument('--primetext', '-p', type=str, required=True, default='',
                     help='base text data, used for text generation')
 parser.add_argument('--seed', '-s', type=int, default=123,
                     help='random seeds for text generation')
@@ -44,6 +43,8 @@ parser.add_argument('--gpu', type=int, default=-1,
 args = parser.parse_args()
 
 np.random.seed(args.seed)
+
+xp = cuda.cupy if args.gpu >= 0 else np
 
 # load vocabulary
 with open(args.vocabulary, 'rb') as f:
@@ -61,7 +62,7 @@ model = L.Classifier(lm)
 serializers.load_npz(args.model, model)
 
 if args.gpu >= 0:
-    cuda.init()
+    cuda.get_device(args.gpu).use()
     model.to_gpu()
 
 model.predictor.reset_state()
@@ -70,12 +71,14 @@ primetext = args.primetext
 if isinstance(primetext, six.binary_type):
     primetext = primetext.decode('utf-8')
 
-if len(primetext) > 0:
-    prev_word = Variable(np.ones((1,)).astype(np.int32) * vocab[primetext])
-    if args.gpu >= 0:
-        prev_word = cuda.to_gpu(prev_word)
-    prob = F.softmax(model.predictor(prev_word))
-    sys.stdout.write(primetext + ' ')
+if primetext in vocab:
+    prev_word = Variable(xp.array([vocab[primetext]], xp.int32))
+else:
+    print('ERROR: Unfortunately ' + primetext + ' is unknown.')
+    exit()
+
+prob = F.softmax(model.predictor(prev_word))
+sys.stdout.write(primetext + ' ')
 
 for i in six.moves.range(args.length):
     prob = F.softmax(model.predictor(prev_word))
@@ -85,12 +88,10 @@ for i in six.moves.range(args.length):
         index = np.random.choice(range(len(probability)), p=probability)
     else:
         index = np.argmax(cuda.to_cpu(prob.data))
-
+    
     if ivocab[index] == '<eos>':
         sys.stdout.write('.')
     else:
         sys.stdout.write(ivocab[index] + ' ')
-
-    prev_word = Variable(np.array([index], dtype=np.int32))
-    if args.gpu >= 0:
-        prev_word = cuda.to_gpu(prev_word)
+    
+    prev_word = Variable(xp.array([index], dtype=xp.int32))
