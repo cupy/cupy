48ac0971d515ae8304556c447754863315a2ff30
Yuya Unno <unnonouno@gmail.com>
2016-04-16 12:58:40 +0900

    Add run_module

--- tests/chainer_tests/functions_tests/activation_tests/test_hard_sigmoid.py
+++ tests/chainer_tests/functions_tests/activation_tests/test_hard_sigmoid.py
@@ -6,6 +6,7 @@ import chainer
 from chainer import cuda
 from chainer import functions
 from chainer import gradient_check
+from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
 
@@ -45,3 +46,6 @@ class TestHardSigmoid(unittest.TestCase):
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(self.x),
                             cuda.to_gpu(self.g))
+
+
+testing.run_module(__name__, __file__)
