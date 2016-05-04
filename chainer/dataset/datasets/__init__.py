b85d3e6df65a0bfb2f9e0d07b13291ac6000236f
Seiya Tokui <beam.web@gmail.com>
2016-05-04 17:59:23 +0900

    Add datasets to datasets module

---
+++ chainer/dataset/datasets/__init__.py
@@ -0,0 +1,17 @@
+from chainer.dataset.datasets import dict_dataset
+from chainer.dataset.datasets import image_dataset
+from chainer.dataset.datasets import sub_dataset
+from chainer.dataset.datasets import tuple_dataset
+
+
+DictDataset = dict_dataset.DictDataset
+ImageDataset = image_dataset.ImageDataset
+SubDataset = sub_dataset.SubDataset
+TupleDataset = tuple_dataset.TupleDataset
+
+get_cross_validation_datasets = sub_dataset.get_cross_validation_datasets
+get_cross_validation_datasets_random = (
+    sub_dataset.get_cross_validation_datasets_random)
+
+split_dataset = sub_dataset.split_dataset
+split_dataset_random = sub_dataset.split_dataset_random
