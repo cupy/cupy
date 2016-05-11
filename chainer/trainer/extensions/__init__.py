4c178226bc8974c4bf84a09be084c9cdb1fdb7ad
Seiya Tokui <beam.web@gmail.com>
2016-05-12 00:59:11 +0900

    Add extensions to adjust optimizer attributes

--- chainer/trainer/extensions/__init__.py
+++ chainer/trainer/extensions/__init__.py
@@ -1,6 +1,10 @@
 from chainer.trainer.extensions import _snapshot
 from chainer.trainer.extensions import computational_graph
+from chainer.trainer.extensions import exponential_decay
+from chainer.trainer.extensions import linear_shift
 
 
 dump_graph = computational_graph.dump_graph
+ExponentialDecay = exponential_decay.ExponentialDecay
+LinearShift = linear_shift.LinearShift
 snapshot = _snapshot.snapshot
