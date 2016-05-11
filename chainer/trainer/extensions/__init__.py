e771de9276c3cec2d88d5d568e746a916aa5f342
Seiya Tokui <beam.web@gmail.com>
2016-05-12 00:40:47 +0900

    Add dump_graph extension

--- chainer/trainer/extensions/__init__.py
+++ chainer/trainer/extensions/__init__.py
@@ -1,4 +1,6 @@
 from chainer.trainer.extensions import _snapshot
+from chainer.trainer.extensions import computational_graph
 
 
+dump_graph = computational_graph.dump_graph
 snapshot = _snapshot.snapshot
---
+++ chainer/trainer/extensions/computational_graph.py
@@ -0,0 +1,38 @@
+import os
+
+from chainer import computational_graph
+from chainer.trainer import extension
+from chainer import variable
+
+
+def dump_graph(root_name, out_name='cg.dot'):
+    """Returns a trainer extension to dump a computational graph.
+
+    This extension dumps a computational graph. The graph is output in DOT
+    language.
+
+    It only dumps a graph at the first iteration by default.
+
+    Args:
+        root_name (str): Name of the root of the computational graph. The
+            actual root variable is retrieved from the report object by this
+            name.
+        out_name (str): Output file name.
+
+    """
+    def trigger(trainer):
+        return trainer.updater.iteration == 1
+
+    @extension.make_extension(name='dump_graph', trigger=trigger)
+    def ext(trainer):
+        var = trainer.observation[root_name]
+        if not isinstance(variable.Variable):
+            raise TypeError('root value is not a Variable')
+        cg = computational_graph.build_computational_graph([var]).dump()
+
+        out_path = os.path.join(trainer.out, out_name)
+        # TODO(beam2d): support outputting images by the dot command
+        with open(out_path, 'w') as f:
+            f.write(cg)
+
+    return ext
