bb2fa72d8460754d6c0eaa57a4fadf74be5069fc
Seiya Tokui <beam.web@gmail.com>
2016-05-16 09:51:55 +0900

    Clean the coding style

--- examples/mnist/train_mnist.py
+++ examples/mnist/train_mnist.py
@@ -29,15 +29,15 @@ def main():
     parser = argparse.ArgumentParser(description='Chainer example: MNIST')
     parser.add_argument('--batchsize', '-b', type=int, default=100,
                         help='Number of images in each mini batch')
-    parser.add_argument('--epoch', '-e', default=20, type=int,
+    parser.add_argument('--epoch', '-e', type=int, default=20,
                         help='Number of sweeps over the dataset to train')
-    parser.add_argument('--gpu', '-g', default=-1, type=int,
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
     parser.add_argument('--out', '-o', default='result',
                         help='Directory to output the result')
     parser.add_argument('--resume', '-r', default='',
                         help='Resume the training from snapshot')
-    parser.add_argument('--unit', '-u', default=1000, type=int,
+    parser.add_argument('--unit', '-u', type=int, default=1000,
                         help='Number of units')
     args = parser.parse_args()
 
