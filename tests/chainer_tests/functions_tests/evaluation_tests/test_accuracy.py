18851e96b9c938e6d814b630df6c034045d7e17e
Kenta OONO <oono@preferred.jp>
2016-05-19 07:52:56 +0900

    Add test cases of invalid types to Accuracy Function

--- tests/chainer_tests/functions_tests/evaluation_tests/test_accuracy.py
+++ tests/chainer_tests/functions_tests/evaluation_tests/test_accuracy.py
@@ -9,6 +9,7 @@ from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
+from chainer.utils import type_check
 
 
 @testing.parameterize(
@@ -53,6 +54,35 @@ class TestAccuracy(unittest.TestCase):
     @condition.retry(3)
     def test_forward_gpu(self):
         self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t))
+
+
+@testing.parameterize(
+    {'x_shape': (10, 3), 't_shape': (4,)},
+    {'x_shape': (10, 3, 2), 't_shape': (10,)},
+    {'x_shape': (10, 3, 1, 2), 't_shape': (10,)},
+    {'x_shape': (10, 3, 4), 't_shape': (10, 5)},
+    {'x_shape': (10, 3, 5, 2), 't_shape': (10, 5)},
+    {'x_shape': (10, 3, 5, 1, 2), 't_shape': (10, 5)},
+)
+class TestInvalidShape(unittest.TestCase):
+
+    def setUp(self):
+        self.x = numpy.random.uniform(-1, 1,
+                                      self.x_shape).astype(numpy.float32)
+        self.t = numpy.random.randint(3, size=self.t_shape).astype(numpy.int32)
+
+    def check_invalid_shape(self, xp):
+        x = chainer.Variable(xp.asarray(self.x))
+        t = chainer.Variable(xp.asarray(self.t))
+        with self.assertRaises(type_check.InvalidType):
+            chainer.functions.accuracy(x, t)
+
+    def test_invalid_shape_cpu(self):
+        self.check_invalid_shape(numpy)
+
+    @attr.gpu
+    def test_invalid_shape_gpu(self):
+        self.check_invalid_shape(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)
