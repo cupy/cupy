429d671502e44268b57fc4846ecefa15f8bf5a62
Yuya Unno <unnonouno@gmail.com>
2016-05-18 10:48:50 +0900

    Write test case for profiler

---
+++ tests/cupy_tests/cuda_tests/test_profile.py
@@ -0,0 +1,17 @@
+import unittest
+
+import mock
+
+from cupy import cuda
+
+
+class TestProfile(unittest.TestCase):
+
+    def test_profile(self):
+        start_patch = mock.patch('cupy.cuda.profiler.start')
+        stop_patch = mock.patch('cupy.cuda.profiler.stop')
+        with start_patch as start, stop_patch as stop:
+            with cuda.profile():
+                pass
+            start.assert_called_once_with()
+            stop.assert_called_once_with()
