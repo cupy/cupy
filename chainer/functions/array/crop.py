ba0c5491fc3884d3561c70e6fca22c2ae5428245
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-22 06:13:48 +0900

    Set cropping shape at the initializing state
    But it can be changed as a data member.

--- chainer/functions/array/crop.py
+++ chainer/functions/array/crop.py
@@ -9,27 +9,29 @@ class Crop(function.Function):
 
     """Function that crops one array with the other with axes and offset."""
 
-    def __init__(self, axes, offset):
+    def __init__(self, shape, axes, offset):
+        if not isinstance(shape, collections.Iterable):
+            raise TypeError('shape must be 1-D array')
         if isinstance(axes, int):
             axes = tuple(axes)
         elif not isinstance(axes, collections.Iterable):
             raise TypeError('axes must be integer or 1-D array')
         if not isinstance(offset, int):
             raise TypeError('offset must be integer')
+        self.shape = shape
         self.axes = axes
         self.offset = offset
 
     def check_type_forward(self, in_types):
-        type_check.expect(in_types.size() == 2)
+        type_check.expect(in_types.size() == 1)
         for axis in self.axes:
             type_check.expect(in_types[0].ndim > axis)
-            type_check.expect(in_types[1].ndim > axis)
 
     def forward(self, xs):
-        ary, ref_ary = xs
+        ary = xs[0]
         for axis in self.axes:
             ary = ary.swapaxes(axis, 0)
-            size = ref_ary.shape[axis]
+            size = self.shape[axis]
             start = self.offset
             end = start + size
             ary = ary[start:end].swapaxes(axis, 0)
@@ -37,26 +39,26 @@ class Crop(function.Function):
 
     def backward(self, xs, gys):
         xp = cuda.get_array_module(*xs)
-        ref_ary = xs[1]
         gy = gys[0]
         gx = xp.zeros_like(xs[0])
         gx_orig = gx
         for axis in self.axes:
             gx = gx.swapaxes(axis, 0)
-            size = ref_ary.shape[axis]
+            size = self.shape[axis]
             start = self.offset
             end = start + size
             gx = gx[start:end].swapaxes(axis, 0)
         gx[...] = gy
-        return gx_orig, None
+        return gx_orig,
 
 
-def crop(x0, x1, axes, offset=0):
+def crop(x, shape, axes, offset=0):
     """Crop one array with the other with axes and offset.
 
     Args:
-        x0 (tuple of Variables): Variable to be cropped.
-        x1 (tuple of Variables): Variable to crop the ``x0`` with.
+        x (tuple of Variables): Variable to be cropped.
+        shape (tuple or list of ints): Reference shape for cropping.
+            The size in each axis specified by 'axes' is used.
         axes (int or tuple of ints):
             Axes that the input array is cropped along with.
         offset (int): Offset for the starting point of cropping.
@@ -66,4 +68,4 @@ def crop(x0, x1, axes, offset=0):
             which is cropped array of ``x0``.
 
     """
-    return Crop(axes, offset)(x0, x1)
+    return Crop(shape, axes, offset)(x)
--- tests/chainer_tests/functions_tests/array_tests/test_crop.py
+++ tests/chainer_tests/functions_tests/array_tests/test_crop.py
@@ -12,51 +12,38 @@ from chainer.testing import attr
 
 class TestCrop(unittest.TestCase):
     axes = [1, 2]
+    offset = 0
 
     def setUp(self):
-        self.x0_data = numpy.random.uniform(-1, 1, (4, 3, 2))
-        self.x1_data = numpy.random.uniform(-1, 1, (4, 2, 1))
-        self.gy_data = numpy.random.uniform(-1, 1, (4, 2, 1))
+        self.x_data = numpy.random.uniform(-1, 1, (4, 3, 2))
+        self.shape = (4, 2, 1)
+        self.gy_data = numpy.random.uniform(-1, 1, self.shape)
 
-    def check_forward(self, x0_data, x1_data):
-        x0 = chainer.Variable(x0_data)
-        x1 = chainer.Variable(x1_data)
-        y = functions.crop(x0, x1, self.axes)
+    def check_forward(self, x_data):
+        x = chainer.Variable(x_data)
+        y = functions.crop(x, self.shape, self.axes)
         self.assertEqual(y.data.dtype, numpy.float)
-        numpy.testing.assert_equal(cuda.to_cpu(x0_data)[:, :2, :1],
+        numpy.testing.assert_equal(cuda.to_cpu(x_data)[:, :2, :1],
                                    cuda.to_cpu(y.data))
 
     def test_forward_cpu(self):
-        self.check_forward(self.x0_data, self.x1_data)
+        self.check_forward(self.x_data)
 
     @attr.gpu
     def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x0_data),
-                           cuda.to_gpu(self.x1_data))
+        self.check_forward(cuda.to_gpu(self.x_data))
 
-    def check_backward(self, x0_data, x1_data, y_grad):
-        x0 = chainer.Variable(x0_data)
-        x1 = chainer.Variable(x1_data)
-        y = functions.crop(x0, x1, self.axes)
-        y.grad = y_grad
-        y.backward()
-
-        xs = (x0.data, x1.data)
-
-        def f():
-            func = y.creator
-            return func.forward(xs)
-
-        gx, _ = gradient_check.numerical_grad(f, xs, (y.grad,))
-        gradient_check.assert_allclose(cuda.to_cpu(gx), cuda.to_cpu(x0.grad))
+    def check_backward(self, x_data, y_grad):
+        gradient_check.check_backward(
+            functions.Crop(self.shape, self.axes, self.offset),
+            (x_data,), y_grad)
 
     def test_backward_cpu(self):
-        self.check_backward(self.x0_data, self.x1_data, self.gy_data)
+        self.check_backward(self.x_data, self.gy_data)
 
     @attr.gpu
     def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x0_data),
-                            cuda.to_gpu(self.x1_data),
+        self.check_backward(cuda.to_gpu(self.x_data),
                             cuda.to_gpu(self.gy_data))
 
 
