07b0b103859364cdece8f6570b27be48a18bfb50
yuji-mizobuchi <mizobuchi.yuji@jp.fujitsu.com>
2016-05-26 05:20:22 +0000

    modify test_exponetial_m1.py and test_logarithm_1p.py

--- tests/chainer_tests/functions_tests/math_tests/test_exponential_m1.py
+++ tests/chainer_tests/functions_tests/math_tests/test_exponential_m1.py
@@ -20,44 +20,32 @@ class Expm1FunctionTest(unittest.TestCase):
         self.x = numpy.random.uniform(.5, 1, self.shape).astype(numpy.float32)
         self.gy = numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
 
-    def check_forward(self, op, op_np, x_data):
+    def check_forward(self, x_data):
         x = chainer.Variable(x_data)
-        y = op(x)
+        y = F.expm1(x)
         gradient_check.assert_allclose(
-            op_np(self.x), y.data, atol=1e-7, rtol=1e-7)
-
-    def check_forward_cpu(self, op, op_np):
-        self.check_forward(op, op_np, self.x)
-
-    def check_forward_gpu(self, op, op_np):
-        self.check_forward(op, op_np, cuda.to_gpu(self.x))
+            numpy.expm1(self.x), y.data, atol=1e-7, rtol=1e-7)
 
     @condition.retry(3)
     def test_expm1_forward_cpu(self):
-        self.check_forward_cpu(F.expm1, numpy.expm1)
+        self.check_forward(self.x)
 
     @attr.gpu
     @condition.retry(3)
     def test_expm1_forward_gpu(self):
-        self.check_forward_gpu(F.expm1, numpy.expm1)
+        self.check_forward(cuda.to_gpu(self.x))
 
-    def check_backward(self, op, x_data, y_grad):
-        gradient_check.check_backward(op, x_data, y_grad)
-
-    def check_backward_cpu(self, op):
-        self.check_backward(op, self.x, self.gy)
-
-    def check_backward_gpu(self, op):
-        self.check_backward(op, cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+    def check_backward(self, x_data, y_grad):
+        gradient_check.check_backward(F.expm1, x_data, y_grad)
 
     @condition.retry(3)
     def test_expm1_backward_cpu(self):
-        self.check_backward_cpu(F.expm1)
+        self.check_backward(self.x, self.gy)
 
     @attr.gpu
     @condition.retry(3)
     def test_expm1_backward_gpu(self):
-        self.check_backward_gpu(F.expm1)
+        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
 
     def test_expm1(self):
         self.assertEqual(F.Expm1().label, 'expm1')
