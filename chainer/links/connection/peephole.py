76d9e2afb17b914d0263ca850988620843bbce70
setoyama <kotaro.setoyama@gmail.com>
2016-06-07 19:25:02 +0900

    implement CPU test

--- chainer/links/connection/peephole.py
+++ chainer/links/connection/peephole.py
@@ -1,38 +1,25 @@
-from chainer.functions.activation import peephole 
-from chainer import link
-from chainer.links.connection import linear
-from chainer import variable
 from chainer.functions.activation import sigmoid
 from chainer.functions.activation import tanh
 from chainer.functions.array import reshape
 from chainer.functions.array import split_axis
-
-import six
-import numpy
+from chainer import link
+from chainer.links.connection import linear
+from chainer import variable
 
 
 class Peephole(link.Chain):
 
     """Fully-connected LSTM with peephole connections layer.
 
-    This is a fully-connected LSTM with peephole connections layer as a chain. Unlike the
-    :func:`~chainer.functions.peephole` function, which is defined as a stateless
-    activation function, this chain holds upward, lateral and peephole connections as
-    child links.
-
-    It also maintains *states*, including the cell state and the output
-    at the previous time step. Therefore, it can be used as a *stateful LSTM*.
+    This is a fully-connected LSTM layer with peephole connections as a chain.
+    Unlike the :link:`~chainer.links.lstm` link, this chain holds peep_i,
+    peep_f and peep_o as child links besides upward and lateral.
 
     Args:
         in_size (int): Dimensionality of input vectors.
         out_size (int): Dimensionality of output vectors.
 
     Attributes:
-        upward (~chainer.links.Linear): Linear layer of upward connections.
-        lateral (~chainer.links.Linear): Linear layer of lateral connections.
-        peep_i (~chainer.links.Linear): Linear layer of peephole input connections.
-        peep_f (~chainer.links.Linear): Linear layer of peephole forget connections.
-        peep_o (~chainer.links.Linear): Linear layer of peephole output connections.
         c (~chainer.Variable): Cell states of LSTM units.
         h (~chainer.Variable): Output at the previous time step.
 
@@ -42,8 +29,8 @@ class Peephole(link.Chain):
             upward=linear.Linear(in_size, 4 * out_size),
             lateral=linear.Linear(out_size, 4 * out_size, nobias=True),
             peep_i=linear.Linear(out_size, out_size, nobias=True),
-            peep_f=linear.Linear(out_size, out_size, nobias=True), 
-            peep_o=linear.Linear(out_size, out_size, nobias=True), 
+            peep_f=linear.Linear(out_size, out_size, nobias=True),
+            peep_o=linear.Linear(out_size, out_size, nobias=True),
         )
         self.state_size = out_size
         self.reset_state()
@@ -88,23 +75,24 @@ class Peephole(link.Chain):
             self.c = variable.Variable(
                 xp.zeros((len(x.data), self.state_size), dtype=x.data.dtype),
                 volatile='auto')
-        
-        lstm_in = reshape.reshape(lstm_in, (lstm_in.data.shape[0], lstm_in.data.shape[1] // 4, 4) + lstm_in.data.shape[2:])
-        a, i, f, o = split_axis.split_axis(lstm_in, 4, 2) 
-        a = reshape.reshape(a, (a.data.shape[0], a.data.shape[1])) 
-        i = reshape.reshape(i, (i.data.shape[0], i.data.shape[1])) 
-        f = reshape.reshape(f, (f.data.shape[0], f.data.shape[1])) 
-        o = reshape.reshape(o, (o.data.shape[0], o.data.shape[1])) 
+        lstm_in = reshape.reshape(lstm_in, (lstm_in.data.shape[0],
+                                            lstm_in.data.shape[1] // 4,
+                                            4) + lstm_in.data.shape[2:])
+        a, i, f, o = split_axis.split_axis(lstm_in, 4, 2)
+        a = reshape.reshape(a, (a.data.shape[0], a.data.shape[1]))
+        i = reshape.reshape(i, (i.data.shape[0], i.data.shape[1]))
+        f = reshape.reshape(f, (f.data.shape[0], f.data.shape[1]))
+        o = reshape.reshape(o, (o.data.shape[0], o.data.shape[1]))
         peep_in_i = self.peep_i(self.c)
         peep_in_f = self.peep_f(self.c)
         a = tanh.tanh(a)
-            #peep_in_i.data = numpy.reshape(peep_in_i.data, i.data.shape)
+        # peep_in_i.data = numpy.reshape(peep_in_i.data, i.data.shape)
         i = sigmoid.sigmoid(i + peep_in_i)
-            #peep_in_f.data = numpy.reshape(peep_in_f.data, f.data.shape)
+        # peep_in_f.data = numpy.reshape(peep_in_f.data, f.data.shape)
         f = sigmoid.sigmoid(f + peep_in_f)
         self.c = a * i + f * self.c
         peep_in_o = self.peep_o(self.c)
-            #peep_in_o.data = numpy.reshape(peep_in_o.data, o.data.shape)
+        # peep_in_o.data = numpy.reshape(peep_in_o.data, o.data.shape)
         o = sigmoid.sigmoid(o + peep_in_o)
         self.h = o * tanh.tanh(self.c)
         return self.h
--- tests/chainer_tests/links_tests/connection_tests/test_peephole.py
+++ tests/chainer_tests/links_tests/connection_tests/test_peephole.py
@@ -4,11 +4,42 @@ import numpy
 
 import chainer
 from chainer import cuda
-from chainer import functions
 from chainer import gradient_check
 from chainer import links
 from chainer import testing
-from chainer.testing import attr
+
+
+def _sigmoid(x):
+    xp = cuda.get_array_module(x)
+    return 1 / (1 + xp.exp(-x))
+
+
+def _peephole(func, c, h, x):
+    xp = cuda.get_array_module(x)
+
+    lstm_in = x.dot(func.upward.W.data.T)
+    lstm_in += h.dot(func.lateral.W.data.T)
+    lstm_in = xp.reshape(lstm_in, (lstm_in.shape[0],
+                                   lstm_in.shape[1] // 4,
+                                   4) + lstm_in.shape[2:])
+    a, i, f, o = xp.split(lstm_in, 4, 2)
+    a = xp.reshape(a, (a.shape[0], a.shape[1]))
+    i = xp.reshape(i, (i.shape[0], i.shape[1]))
+    f = xp.reshape(f, (f.shape[0], f.shape[1]))
+    o = xp.reshape(o, (o.shape[0], o.shape[1]))
+    peep_in_i = c.dot(func.peep_i.W.data.T)
+    peep_in_f = c.dot(func.peep_f.W.data.T)
+    a = xp.tanh(a)
+    # peep_in_i.data = xp.reshape(peep_in_i.data, i.shape)
+    i = _sigmoid(i + peep_in_i)
+    # peep_in_f.data = xp.reshape(peep_in_f.data, f.shape)
+    f = _sigmoid(f + peep_in_f)
+    c_next = a * i + f * c
+    peep_in_o = c_next.dot(func.peep_o.W.data.T)
+    # peep_in_o.data = xp.reshape(peep_in_o.data, o.shape)
+    o = _sigmoid(o + peep_in_o)
+    y = o * xp.tanh(c_next)
+    return c_next, y
 
 
 @testing.parameterize(
@@ -23,138 +54,161 @@ class TestPeephole(unittest.TestCase):
         upward[...] = numpy.random.uniform(-1, 1, upward.shape)
         lateral = self.link.lateral.W.data
         lateral[...] = numpy.random.uniform(-1, 1, lateral.shape)
-        peep_i = self.link.peep_i.W.data 
-        peep_i[...] = numpy.random.uniform(-1, 1, peep_i.shape)   
-        peep_f = self.link.peep_f.W.data 
-        peep_f[...] = numpy.random.uniform(-1, 1, peep_f.shape)   
-        peep_o = self.link.peep_o.W.data 
-        peep_o[...] = numpy.random.uniform(-1, 1, peep_o.shape)   
-        self.link.zerograds()
+        peep_i = self.link.peep_i.W.data
+        peep_i[...] = numpy.random.uniform(-1, 1, peep_i.shape)
+        peep_f = self.link.peep_f.W.data
+        peep_f[...] = numpy.random.uniform(-1, 1, peep_f.shape)
+        peep_o = self.link.peep_o.W.data
+        peep_o[...] = numpy.random.uniform(-1, 1, peep_o.shape)
+        # self.link.zerograds()
 
-        self.upward = upward.copy()  # fixed on CPU
-        self.lateral = lateral.copy()  # fixed on CPU
-        self.peep_i = peep_i.copy()  # fixed on CPU
-        self.peep_f = peep_f.copy()  # fixed on CPU
-        self.peep_o = peep_o.copy()  # fixed on CPU
+        self.upward = upward.copy()
+        self.lateral = lateral.copy()
+        self.peep_i = peep_i.copy()
+        self.peep_f = peep_f.copy()
+        self.peep_o = peep_o.copy()
 
+        c_shape = (1, self.out_size)
+        h_shape = (1, self.out_size)
         x_shape = (4, self.in_size)
+        gy_shape = (4, self.out_size)
+        self.c = numpy.zeros(c_shape).astype(numpy.float32)
+        self.h = numpy.zeros(h_shape).astype(numpy.float32)
         self.x = numpy.random.uniform(-1, 1, x_shape).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, gy_shape).astype(numpy.float32)
 
-    def check_forward(self, x_data):
-        xp = self.link.xp
+    def _forward(self, link, x):
+        return link(x)
+
+    def check_forward(self, c_data, h_data, x_data):
         x = chainer.Variable(x_data)
+
         h1 = self.link(x)
-        c0 = chainer.Variable(xp.zeros((len(self.x), self.out_size),
-                                       dtype=self.x.dtype))
-        c1_expect, h1_expect = \
-            functions.peephole(c0, self.link.upward(x), self.link.peep_i, self.link.peep_f, self.link.peep_o)  #  genin
-        gradient_check.assert_allclose(h1.data, h1_expect.data)
-        gradient_check.assert_allclose(self.link.h.data, h1_expect.data)
-        gradient_check.assert_allclose(self.link.c.data, c1_expect.data)
+        c1_expect, h1_expect = _peephole(self.link, c_data, h_data, x_data)
+        gradient_check.assert_allclose(h1.data, h1_expect)
+        gradient_check.assert_allclose(self.link.c.data, c1_expect)
+        gradient_check.assert_allclose(self.link.h.data, h1_expect)
 
         h2 = self.link(x)
-        c2_expect, h2_expect = \
-            functions.peephole(c1_expect,
-                           self.link.upward(x) + self.link.lateral(h1), self.link.peep_i, self.link.peep_f, self.link.peep_o)
-        gradient_check.assert_allclose(h2.data, h2_expect.data)
+        c2_expect, h2_expect = _peephole(self.link,
+                                         c1_expect, h1_expect, x_data)
+        gradient_check.assert_allclose(h2.data, h2_expect)
+        gradient_check.assert_allclose(self.link.c.data, c2_expect)
+        gradient_check.assert_allclose(self.link.h.data, h2_expect)
 
     def test_forward_cpu(self):
-        self.check_forward(self.x)
+        self.check_forward(self.c, self.h, self.x)
 
-    #@attr.gpu
-    #def test_forward_gpu(self):
-    #    self.link.to_gpu()
-    #    self.check_forward(cuda.to_gpu(self.x))
+    # @attr.gpu
+    # def test_forward_gpu(self):
+    #     self.link.to_gpu()
+    #     self.check_forward(cuda.to_gpu(self.h),
+    #                        cuda.to_gpu(self.x))
 
+    def check_backward(self, c_data, h_data, x_data, y_grad):
+        x = chainer.Variable(x_data)
+        y = self._forward(self.link, x)
+        y.grad = y_grad
+        y.backward()
 
-class TestpPeepholeRestState(unittest.TestCase):
+        def f():
+            c, y = _peephole(self.link, c_data, h_data, x_data)
+            return y,
+        gx, = gradient_check.numerical_grad(f, (x.data,), (y.grad,))
+        gradient_check.assert_allclose(gx, x.grad, atol=1e-3)
 
-    def setUp(self):
-        self.link = links.Peephole(5, 7)
-        self.x = chainer.Variable(
-            numpy.random.uniform(-1, 1, (3, 5)).astype(numpy.float32))
+    def test_backward_cpu(self):
+        self.check_backward(self.c, self.h, self.x, self.gy)
 
-    def check_state(self):
-        self.assertIsNone(self.link.c)
-        self.assertIsNone(self.link.h)
-        self.link(self.x)
-        self.assertIsNotNone(self.link.c)
-        self.assertIsNotNone(self.link.h)
-
-    def test_state_cpu(self):
-        self.check_state()
-
-    #@attr.gpu
-    #def test_state_gpu(self):
-    #    self.link.to_gpu()
-    #    self.x.to_gpu()
-    #    self.check_state()
-
-    def check_reset_state(self):
-        self.link(self.x)
-        self.link.reset_state()
-        self.assertIsNone(self.link.c)
-        self.assertIsNone(self.link.h)
-
-    def test_reset_state_cpu(self):
-        self.check_reset_state()
-
-    #@attr.gpu
-    #def test_reset_state_gpu(self):
-    #    self.link.to_gpu()
-    #    self.x.to_gpu()
-    #    self.check_reset_state()
+    # @attr.gpu
+    # def test_backward_gpu(self):
+    #     self.link.to_gpu()
+    #     self.check_backward(cuda.to_gpu(self.h),
+    #                         cuda.to_gpu(self.x),
+    #                         cuda.to_gpu(self.gy))
 
 
-#class TestPeepholeToCPUToGPU(unittest.TestCase):
+# @testing.parameterize(
+#     *testing.product({
+#         'link_array_module': ['to_cpu', 'to_gpu'],
+#         'state_array_module': ['to_cpu', 'to_gpu']
+#     }))
+# class TestGRUState(unittest.TestCase):
 
-    #def setUp(self):
-    #    self.link = links.Peephole(5, 7)
-    #    self.x = chainer.Variable(
-    #        numpy.random.uniform(-1, 1, (3, 5)).astype(numpy.float32))
+    # def setUp(self):
+    #     in_size, out_size = 10, 8
+    #     self.link = links.StatefulGRU(in_size, out_size)
+    #     self.h = chainer.Variable(
+    #         numpy.random.uniform(-1, 1, (3, out_size)).astype(numpy.float32))
 
-    #def check_to_cpu(self, s):
-    #    self.link.to_cpu()
-    #    self.assertIsInstance(s.data, self.link.xp.ndarray)
-    #    self.link.to_cpu()
-    #    self.assertIsInstance(s.data, self.link.xp.ndarray)
+    # def check_set_state(self, h):
+    #     self.link.set_state(h)
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
 
-    #def test_to_cpu_cpu(self):
-    #    self.link(self.x)
-    #    self.check_to_cpu(self.link.c)
-    #    self.check_to_cpu(self.link.h)
+    # def test_set_state_cpu(self):
+    #     self.check_set_state(self.h)
 
-    #@attr.gpu
-    #def test_to_cpu_gpu(self):
-    #    self.link.to_gpu()
-    #    self.x.to_gpu()
-    #    self.link(self.x)
-    #    self.check_to_cpu(self.link.c)
-    #    self.check_to_cpu(self.link.h)
+    # @attr.gpu
+    # def test_set_state_gpu(self):
+    #     getattr(self.link, self.link_array_module)()
+    #     getattr(self.h, self.state_array_module)()
+    #     self.check_set_state(self.h)
 
-    #def check_to_cpu_to_gpu(self, s):
-    #    self.link.to_gpu()
-    #    self.assertIsInstance(s.data, self.link.xp.ndarray)
-    #    self.link.to_gpu()
-    #    self.assertIsInstance(s.data, self.link.xp.ndarray)
-    #    self.link.to_cpu()
-    #    self.assertIsInstance(s.data, self.link.xp.ndarray)
-    #    self.link.to_gpu()
-    #    self.assertIsInstance(s.data, self.link.xp.ndarray)
+    # def check_reset_state(self):
+    #     self.link.reset_state()
+    #     self.assertIsNone(self.link.h)
 
-    #@attr.gpu
-    #def test_to_cpu_to_gpu_cpu(self):
-    #    self.link(self.x)
-    #    self.check_to_cpu_to_gpu(self.link.c)
-    #    self.check_to_cpu_to_gpu(self.link.h)
+    # def test_reset_state_cpu(self):
+    #     self.check_reset_state()
 
-    #@attr.gpu
-    #def test_to_cpu_to_gpu_gpu(self):
-    #    self.link.to_gpu()
-    #    self.x.to_gpu()
-    #    self.link(self.x)
-    #    self.check_to_cpu_to_gpu(self.link.c)
-    #    self.check_to_cpu_to_gpu(self.link.h)
+    # @attr.gpu
+    # def test_reset_state_gpu(self):
+    #     getattr(self.link, self.link_array_module)()
+    #     self.check_reset_state()
+
+
+# class TestGRUToCPUToGPU(unittest.TestCase):
+
+    # def setUp(self):
+    #     in_size, out_size = 10, 8
+    #     self.link = links.StatefulGRU(in_size, out_size)
+    #     self.h = chainer.Variable(
+    #         numpy.random.uniform(-1, 1, (3, out_size)).astype(numpy.float32))
+
+    # def check_to_cpu(self, h):
+    #     self.link.set_state(h)
+    #     self.link.to_cpu()
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
+    #     self.link.to_cpu()
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
+
+    # def test_to_cpu_cpu(self):
+    #     self.check_to_cpu(self.h)
+
+    # @attr.gpu
+    # def test_to_cpu_gpu(self):
+    #     self.h.to_gpu()
+    #     self.check_to_cpu(self.h)
+
+    # def check_to_cpu_to_gpu(self, h):
+    #     self.link.set_state(h)
+    #     self.link.to_gpu()
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
+    #     self.link.to_gpu()
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
+    #     self.link.to_cpu()
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
+    #     self.link.to_gpu()
+    #     self.assertIsInstance(self.link.h.data, self.link.xp.ndarray)
+
+    # @attr.gpu
+    # def test_to_cpu_to_gpu_cpu(self):
+    #     self.check_to_cpu_to_gpu(self.h)
+
+    # @attr.gpu
+    # def test_to_cpu_to_gpu_gpu(self):
+    #     self.h.to_gpu()
+    #     self.check_to_cpu_to_gpu(self.h)
 
 
 testing.run_module(__name__, __file__)
