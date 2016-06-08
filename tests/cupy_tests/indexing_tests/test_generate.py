d6d55a8383ef2ed9898a5363111bc01defeb68d5
kikusu <kikusumk3@gmail.com>
2016-06-08 11:25:55 +0900

    fix cupy.ix_ test

--- tests/cupy_tests/indexing_tests/test_generate.py
+++ tests/cupy_tests/indexing_tests/test_generate.py
@@ -12,13 +12,10 @@ class TestIX_(unittest.TestCase):
     def test_ix_list(self, xp):
         return xp.ix_([0, 1], [2, 4])
 
+    @testing.for_all_dtypes()
     @testing.numpy_cupy_array_list_equal()
-    def test_ix_ndarray(self, xp):
-        return xp.ix_(xp.array([0, 1]), xp.array([2, 4]))
-
-    @testing.numpy_cupy_array_list_equal()
-    def test_ix_ndarray_int(self, xp):
-        return xp.ix_(xp.array([0, 1]), xp.array([2, 4]))
+    def test_ix_ndarray(self, xp, dtype):
+        return xp.ix_(xp.array([0, 1], dtype), xp.array([2, 3], dtype))
 
     @testing.numpy_cupy_array_list_equal()
     def test_ix_empty_ndarray(self, xp):
