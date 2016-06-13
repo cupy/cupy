2f160beb46b71adb897c6f90d4d5cb18c80c29e4
Seiya Tokui <beam.web@gmail.com>
2016-06-13 19:08:32 +0900

    Improve support of device specification in concat_examples

--- chainer/dataset/convert.py
+++ chainer/dataset/convert.py
@@ -32,9 +32,9 @@ def concat_examples(batch, device=None, padding=None):
     Args:
         batch (list): A list of examples. This is typically given by a dataset
             iterator.
-        device: Device to which each array is sent. Negative value indicates
-            the host memory (CPU). If it is omitted, all arrays are left in the
-            original device.
+        device (int): Device ID to which each array is sent. Negative value
+            indicates the host memory (CPU). If it is omitted, all arrays are
+            left in the original device.
         padding: Padding value for extra elements. If this is None (default),
             an error is raised on shape mismatch. Otherwise, an array of
             minimum dimensionalities that can accomodate all arrays is created,
@@ -56,36 +56,44 @@ def concat_examples(batch, device=None, padding=None):
     else:
         to_device = cuda.to_gpu
 
-    with cuda.get_device(device):
-        first_elem = batch[0]
+    first_elem = batch[0]
 
-        if isinstance(first_elem, tuple):
-            result = []
-            if not isinstance(padding, tuple):
-                padding = [padding] * len(first_elem)
+    if isinstance(first_elem, tuple):
+        result = []
+        if not isinstance(padding, tuple):
+            padding = [padding] * len(first_elem)
 
-            for i in six.moves.range(len(first_elem)):
-                result.append(to_device(_concat_arrays(
-                    [example[i] for example in batch], padding[i])))
+        for i in six.moves.range(len(first_elem)):
+            result.append(to_device(_concat_arrays(
+                [example[i] for example in batch], padding[i])))
 
-            return tuple(result)
+        return tuple(result)
 
-        elif isinstance(first_elem, dict):
-            result = {}
-            if not isinstance(padding, dict):
-                padding = {key: padding for key in first_elem}
+    elif isinstance(first_elem, dict):
+        result = {}
+        if not isinstance(padding, dict):
+            padding = {key: padding for key in first_elem}
 
-            for key in first_elem:
-                result[key] = to_device(_concat_arrays(
-                    [example[key] for example in batch], padding[key]))
+        for key in first_elem:
+            result[key] = to_device(_concat_arrays(
+                [example[key] for example in batch], padding[key]))
 
-            return result
+        return result
 
-        else:
-            return to_device(_concat_arrays(batch, padding))
+    else:
+        return to_device(_concat_arrays(batch, padding))
 
 
-def _concat_arrays(arrays, padding=None):
+def _concat_arrays(arrays, padding):
+    if padding is not None:
+        return _concate_arrays_with_padding(arrays, padding)
+
+    xp = cuda.get_array_module(arrays[0])
+    with cuda.get_device(arrays[0]):
+        return xp.concatenate([array[None] for array in arrays])
+
+
+def _concate_arrays_with_padding(arrays, padding):
     shape = numpy.array(arrays[0].shape, dtype=int)
     for array in arrays[1:]:
         if numpy.any(shape != array.shape):
@@ -96,11 +104,7 @@ def _concat_arrays(arrays, padding=None):
     shape = tuple(numpy.insert(shape, 0, len(arrays)))
 
     xp = cuda.get_array_module(arrays[0])
-    if padding is None:
-        result = xp.empty(shape, dtype=arrays[0].dtype)
-        for i in six.moves.range(len(arrays)):
-            result[i] = arrays[i]
-    else:
+    with cuda.get_device(arrays[0]):
         result = xp.full(shape, padding, dtype=arrays[0].dtype)
         for i in six.moves.range(len(arrays)):
             src = arrays[i]
