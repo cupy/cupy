6c9ea6c3702375a7892700e7b9c89c8d03eb75d5
Yuya Unno <unnonouno@gmail.com>
2016-06-19 17:19:44 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
               'chainer.functions.noise',
               'chainer.functions.normalization',
               'chainer.functions.pooling',
+              'chainer.functions.theano',
               'chainer.functions.util',
               'chainer.function_hooks',
               'chainer.iterators',
