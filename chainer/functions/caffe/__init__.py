b589d44501b676ec42da9f95fc731b8fe2631752
Ryosuke Okuta <okuta@preferred.jp>
2016-06-20 15:29:18 +0900

    Remove chainer's files

--- chainer/functions/caffe/__init__.py
+++
@@ -1,5 +0,0 @@
-from chainer.links.caffe import caffe_function
-
-
-# for backward compatibility
-CaffeFunction = caffe_function.CaffeFunction
--- chainer/testing/attr.py
+++
@@ -1,9 +0,0 @@
-from nose.plugins import attrib
-
-gpu = attrib.attr('gpu')
-cudnn = attrib.attr('gpu', 'cudnn')
-slow = attrib.attr('slow')
-
-
-def multi_gpu(gpu_num):
-    return attrib.attr(gpu=gpu_num)
