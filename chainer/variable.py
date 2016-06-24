56b0a673c86c96e38c2b2e0d4d6cd4ff82522aea
Hitoshi Nishimura <ht-nishimura@kddilabs.jp>
2016-06-24 10:40:31 +0900

    Remove a bug

--- chainer/variable.py
+++ chainer/variable.py
@@ -102,7 +102,7 @@ Actual: {0}'''.format(type(data))
         self.name = name
 
     def __reduce__(self):
-        return Variable, (self.data, self.volatile, self.name, self.grad) #add "self.grad"
+        return Variable, (self.data, self.volatile, self.name, self._grad) #add "self._grad"
 
     def __repr__(self):
         if self.name:
