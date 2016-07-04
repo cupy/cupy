84376cad6483fc7249e9a8859082786e69337ba4
Seiya Tokui <beam.web@gmail.com>
2016-07-04 18:19:31 +0900

    Remove redundant indexing

--- tests/chainer_tests/iterators_tests/test_sequential_iterator.py
+++ tests/chainer_tests/iterators_tests/test_sequential_iterator.py
@@ -11,11 +11,11 @@ class TestSequentialIterator(unittest.TestCase):
         it = iterators.SequentialIterator(dataset, 2)
         for i in range(3):
             self.assertEqual(it.epoch, i)
-            self.assertEqual(it.next()[:], [1, 2])
+            self.assertEqual(it.next(), [1, 2])
             self.assertFalse(it.is_new_epoch)
-            self.assertEqual(it.next()[:], [3, 4])
+            self.assertEqual(it.next(), [3, 4])
             self.assertFalse(it.is_new_epoch)
-            self.assertEqual(it.next()[:], [5, 6])
+            self.assertEqual(it.next(), [5, 6])
             self.assertTrue(it.is_new_epoch)
 
     def test_iterator_repeat_not_even(self):
@@ -23,17 +23,17 @@ class TestSequentialIterator(unittest.TestCase):
         it = iterators.SequentialIterator(dataset, 2)
 
         self.assertEqual(it.epoch, 0)
-        self.assertEqual(it.next()[:], [1, 2])
+        self.assertEqual(it.next(), [1, 2])
         self.assertFalse(it.is_new_epoch)
-        self.assertEqual(it.next()[:], [3, 4])
+        self.assertEqual(it.next(), [3, 4])
         self.assertFalse(it.is_new_epoch)
-        self.assertEqual(it.next()[:], [5, 1])
+        self.assertEqual(it.next(), [5, 1])
         self.assertTrue(it.is_new_epoch)
         self.assertEqual(it.epoch, 1)
 
-        self.assertEqual(it.next()[:], [2, 3])
+        self.assertEqual(it.next(), [2, 3])
         self.assertFalse(it.is_new_epoch)
-        self.assertEqual(it.next()[:], [4, 5])
+        self.assertEqual(it.next(), [4, 5])
         self.assertTrue(it.is_new_epoch)
         self.assertEqual(it.epoch, 2)
 
@@ -41,9 +41,9 @@ class TestSequentialIterator(unittest.TestCase):
         dataset = [1, 2, 3, 4, 5, 6]
         it = iterators.SequentialIterator(dataset, 2, repeat=False)
 
-        self.assertEqual(it.next()[:], [1, 2])
-        self.assertEqual(it.next()[:], [3, 4])
-        self.assertEqual(it.next()[:], [5, 6])
+        self.assertEqual(it.next(), [1, 2])
+        self.assertEqual(it.next(), [3, 4])
+        self.assertEqual(it.next(), [5, 6])
         self.assertTrue(it.is_new_epoch)
         self.assertEqual(it.epoch, 1)
         for i in range(2):
@@ -53,9 +53,9 @@ class TestSequentialIterator(unittest.TestCase):
         dataset = [1, 2, 3, 4, 5]
         it = iterators.SequentialIterator(dataset, 2, repeat=False)
 
-        self.assertEqual(it.next()[:], [1, 2])
-        self.assertEqual(it.next()[:], [3, 4])
-        self.assertEqual(it.next()[:], [5])
+        self.assertEqual(it.next(), [1, 2])
+        self.assertEqual(it.next(), [3, 4])
+        self.assertEqual(it.next(), [5])
         self.assertTrue(it.is_new_epoch)
         self.assertEqual(it.epoch, 1)
         self.assertRaises(StopIteration, it.next)
