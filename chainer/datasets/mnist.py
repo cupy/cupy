f9b366b5c7e215f3ef0ca3011e5fc75b0edd13b7
Seiya Tokui <beam.web@gmail.com>
2016-07-05 16:17:05 +0900

    Make dtypes configurable in get_mnist

--- chainer/datasets/mnist.py
+++ chainer/datasets/mnist.py
@@ -9,7 +9,8 @@ from chainer.dataset import download
 from chainer.datasets import tuple_dataset
 
 
-def get_mnist(withlabel=True, ndim=1, scale=1.):
+def get_mnist(withlabel=True, ndim=1, scale=1.,
+              image_dtype=numpy.float32, label_dtype=numpy.int32):
     """Gets the MNIST dataset.
 
     `MNIST <http://yann.lecun.com/exdb/mnist/>`_ is a set of hand-written
@@ -18,35 +19,39 @@ def get_mnist(withlabel=True, ndim=1, scale=1.):
     scales the pixels to floating point values in the interval ``[0, scale]``.
 
     This function returns the training set and the test set of the official
-    MNIST dataset. If ``withlabel`` is True, each dataset consists of tuples of
-    images and labels, otherwise it only consists of images.
+    MNIST dataset. If ``withlabel`` is ``True``, each dataset consists of
+    tuples of images and labels, otherwise it only consists of images.
 
     Args:
-        withlabel (bool): If True, it returns datasets with labels. In this
+        withlabel (bool): If ``True``, it returns datasets with labels. In this
             case, each example is a tuple of an image and a label. Otherwise,
             the datasets only contain images.
         ndim (int): Number of dimensions of each image. The shape of each image
-            is determined depending on ndim as follows:
+            is determined depending on ``ndim`` as follows:
                 - ``ndim == 1``: the shape is ``(784,)``
                 - ``ndim == 2``: the shape is ``(28, 28)``
                 - ``ndim == 3``: the shape is ``(1, 28, 28)``
         scale (float): Pixel value scale. If it is 1 (default), pixels are
             scaled to the interval ``[0, 1]``.
+        image_dtype: Dtype of image arrays.
+        label_dtype: Dtype of label arrays.
 
     Returns:
-        A tuple of two datasets. If ``withlabel`` is True, both datasets are
-        :class:`~chainer.datasets.TupleDataset` instances. Othewrise, both
+        A tuple of two datasets. If ``withlabel`` is ``True``, both datasets
+        are :class:`~chainer.datasets.TupleDataset` instances. Othewrise, both
         datasets are arrays of images.
 
     """
     train_raw = _retrieve_mnist_training()
-    train = _preprocess_mnist(train_raw, withlabel, ndim, scale)
+    train = _preprocess_mnist(train_raw, withlabel, ndim, scale,
+                              image_dtype, label_dtype)
     test_raw = _retrieve_mnist_test()
-    test = _preprocess_mnist(test_raw, withlabel, ndim, scale)
+    test = _preprocess_mnist(test_raw, withlabel, ndim, scale,
+                             image_dtype, label_dtype)
     return train, test
 
 
-def _preprocess_mnist(raw, withlabel, ndim, scale):
+def _preprocess_mnist(raw, withlabel, ndim, scale, image_dtype, label_dtype):
     images = raw['x']
     if ndim == 2:
         images = images.reshape(-1, 28, 28)
@@ -54,11 +59,11 @@ def _preprocess_mnist(raw, withlabel, ndim, scale):
         images = images.reshape(-1, 1, 28, 28)
     elif ndim != 1:
         raise ValueError('invalid ndim for MNIST dataset')
-    images = images.astype(numpy.float32)
+    images = images.astype(image_dtype)
     images *= scale / 255.
 
     if withlabel:
-        labels = raw['y'].astype(numpy.int32)
+        labels = raw['y'].astype(label_dtype)
         return tuple_dataset.TupleDataset(images, labels)
     else:
         return images
