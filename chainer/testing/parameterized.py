6d7306722b132860bd536e507f3f4ab89fcb8679
Kenta OONO <oono@preferred.jp>
2016-07-05 09:39:56 +0900

    Fix _gen_case so that testing.parameterize can accept callables

--- chainer/testing/parameterized.py
+++ chainer/testing/parameterized.py
@@ -1,6 +1,7 @@
 import itertools
 import sys
 import unittest
+import types
 
 import six
 
@@ -12,9 +13,18 @@ def _gen_case(base, module, i, param):
         name = base.__str__(self)
         return '%s  parameter: %s' % (name, param)
 
-    mb = dict(param)
+    mb = {'__str__': __str__}
+    for k, v in six.iteritems(param):
+        if isinstance(v, types.FunctionType):
+            def create_new_v():
+                f = v
+                def new_v(self, *args, **kwargs):
+                    return f(*args, **kwargs)
+                return new_v
+            mb[k] = create_new_v()
+        else:
+            mb[k] = v
 
-    mb['__str__'] = __str__
     cls = type(cls_name, (base,), mb)
     setattr(module, cls_name, cls)
 
