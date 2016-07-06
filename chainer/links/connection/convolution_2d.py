25b77e63f8d22fc9466253daa15988831c66e7e9
Brian Vogel <vogel@preferred.jp>
2016-07-06 17:59:07 +0900

    Initial implementation of parameter shape placeholder. Not compatible with optimizer yet.

--- chainer/links/connection/convolution_2d.py
+++ chainer/links/connection/convolution_2d.py
@@ -13,7 +13,9 @@ class Convolution2D(link.Link):
     holds the filter weight and bias vector as parameters.
 
     Args:
-        in_channels (int): Number of channels of input arrays.
+        in_channels (int): Number of channels of input arrays. If None,
+            parameter initialization will be deferred until the first forward
+            data pass at which time the size will be determined.
         out_channels (int): Number of channels of output arrays.
         ksize (int or pair of ints): Size of filters (a.k.a. kernels).
             ``ksize=k`` and ``ksize=(k, k)`` are equivalent.
@@ -47,18 +49,19 @@ class Convolution2D(link.Link):
     def __init__(self, in_channels, out_channels, ksize, stride=1, pad=0,
                  wscale=1, bias=0, nobias=False, use_cudnn=True,
                  initialW=None, initial_bias=None):
-        kh, kw = _pair(ksize)
+        super(Convolution2D, self).__init__()
+        self.ksize = ksize
         self.stride = _pair(stride)
         self.pad = _pair(pad)
         self.use_cudnn = use_cudnn
+        self.out_channels = out_channels
+        self.initialW = initialW
+        self.wscale = wscale
 
-        W_shape = (out_channels, in_channels, kh, kw)
-        super(Convolution2D, self).__init__(W=W_shape)
-
-        # For backward compatibility, the scale of weights is proportional to
-        # the square root of wscale.
-        initializers.init_weight(self.W.data, initialW,
-                                 scale=math.sqrt(wscale))
+        if in_channels is None:
+            self.add_uninitialized_param('W')
+        else:
+            self._initialize_params(in_channels)
 
         if nobias:
             self.b = None
@@ -67,6 +70,15 @@ class Convolution2D(link.Link):
             if initial_bias is None:
                 initial_bias = bias
             initializers.init_weight(self.b.data, initial_bias)
+
+    def _initialize_params(self, in_channels):
+        kh, kw = _pair(self.ksize)
+        W_shape = (self.out_channels, in_channels, kh, kw)
+        self.add_param('W', W_shape)
+        # For backward compatibility, the scale of weights is proportional to
+        # the square root of wscale.
+        initializers.init_weight(self.W.data, self.initialW,
+                                 scale=math.sqrt(self.wscale))
 
     def __call__(self, x):
         """Applies the convolution layer.
@@ -78,6 +90,8 @@ class Convolution2D(link.Link):
             ~chainer.Variable: Output of the convolution.
 
         """
+        if self.has_uninitialized_params:
+            self._initialize_params(x.data.shape[1])
         return convolution_2d.convolution_2d(
             x, self.W, self.b, self.stride, self.pad, self.use_cudnn)
 
--- tests/chainer_tests/links_tests/connection_tests/test_linear.py
+++ tests/chainer_tests/links_tests/connection_tests/test_linear.py
@@ -66,6 +66,62 @@ class TestLinear(unittest.TestCase):
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
 
 
+class TestLinearParameterShapePlaceholder(unittest.TestCase):
+
+    in_size = 3
+    in_shape = (in_size,)
+    out_size = 2
+    in_size_or_none = None
+
+    def setUp(self):
+        self.link = links.Linear(self.in_size_or_none, self.out_size)
+        temp_x = numpy.random.uniform(-1, 1, (self.out_size, self.in_size)).astype(numpy.float32)
+        self.link(chainer.Variable(temp_x)) # This will initialize the parameters W.
+        W = self.link.W.data
+        W[...] = numpy.random.uniform(-1, 1, W.shape)
+        b = self.link.b.data
+        b[...] = numpy.random.uniform(-1, 1, b.shape)
+        self.link.zerograds()
+
+        self.W = W.copy()  # fixed on CPU
+        self.b = b.copy()  # fixed on CPU
+
+        x_shape = (4,) + self.in_shape
+        self.x = numpy.random.uniform(-1, 1, x_shape).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+            -1, 1, (4, self.out_size)).astype(numpy.float32)
+        self.y = self.x.reshape(4, -1).dot(W.T) + b
+
+    def check_forward(self, x_data):
+        x = chainer.Variable(x_data)
+        y = self.link(x)
+        self.assertEqual(y.data.dtype, numpy.float32)
+        gradient_check.assert_allclose(self.y, y.data)
+
+    @condition.retry(3)
+    def test_forward_cpu(self):
+        self.check_forward(self.x)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_forward_gpu(self):
+        self.link.to_gpu()
+        self.check_forward(cuda.to_gpu(self.x))
+
+    def check_backward(self, x_data, y_grad):
+        gradient_check.check_backward(
+            self.link, x_data, y_grad, (self.link.W, self.link.b), eps=1e-2)
+
+    @condition.retry(3)
+    def test_backward_cpu(self):
+        self.check_backward(self.x, self.gy)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_backward_gpu(self):
+        self.link.to_gpu()
+        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+
 class TestLinearWithSpatialDimensions(TestLinear):
 
     in_shape = (3, 2, 2)
