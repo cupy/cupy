2faa273882ce5da23d9be1be91f4a1b0a2e69179
Masayuki Takagi <kamonama@gmail.com>
2016-07-11 17:40:25 +1000

    Add test for cuDNN call to N-dimensional average pooling function.

--- tests/chainer_tests/functions_tests/pooling_tests/test_average_pooling_nd.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_average_pooling_nd.py
@@ -2,6 +2,7 @@ import unittest
 
 import functools
 import itertools
+import mock
 import operator
 import numpy
 import six
@@ -111,6 +112,52 @@ class TestAveragePoolingND(unittest.TestCase):
     @condition.retry(3)
     def test_backward_gpu_no_cudnn(self):
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy), False)
+
+
+@testing.parameterize(*testing.product({
+    'dims': [(4, 3, 2), (3, 2), (2,)],
+    'use_cudnn': [True, False],
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
+@attr.cudnn
+class TestAveragePoolingNDCudnnCall(unittest.TestCase):
+
+    def setUp(self):
+        self.ndim = len(self.dims)
+        self.ksize = (3,) * self.ndim
+        self.stride = (2,) * self.ndim
+        self.pad = (1,) * self.ndim
+        x_shape = (2, 3) + self.dims
+        self.x = cuda.cupy.arange(functools.reduce(operator.mul, x_shape),
+                                  dtype=self.dtype).reshape(x_shape)
+        gy_shape = (2, 3) + tuple(
+            [conv.get_conv_outsize(d, k, s, p)
+             for (d, k, s, p)
+             in zip(self.dims, self.ksize, self.stride, self.pad)])
+        self.gy = cuda.cupy.random.uniform(-1, 1, gy_shape).astype(self.dtype)
+
+    def forward(self):
+        x = chainer.Variable(self.x)
+        return functions.average_pooling_nd(
+            x, self.ksize, self.stride, self.pad, use_cudnn=self.use_cudnn)
+
+    @unittest.skipIf(cuda.cudnn_enabled and
+                     cuda.cudnn.cudnn.getVersion() < 3000,
+                     'Only cudnn ver>=3 supports average-pooling-nd')
+    def test_call_cudnn_forward(self):
+        with mock.patch('cupy.cudnn.cudnn.poolingForward') as func:
+            self.forward()
+            self.assertEqual(func.called, self.use_cudnn and self.ndim > 1)
+
+    @unittest.skipIf(cuda.cudnn_enabled and
+                     cuda.cudnn.cudnn.getVersion() < 3000,
+                     'Only cudnn ver>=3 supports average-pooling-nd')
+    def test_call_cudnn_backward(self):
+        y = self.forward()
+        y.grad = self.gy
+        with mock.patch('cupy.cudnn.cudnn.poolingBackward') as func:
+            y.backward()
+            self.assertEqual(func.called, self.use_cudnn and self.ndim > 1)
 
 
 testing.run_module(__name__, __file__)
