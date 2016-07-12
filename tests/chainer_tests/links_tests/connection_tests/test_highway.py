64c3f2c9fc44a99a0d1773cf3b2f3438fa8d23cb
fukatani <nannyakannya@gmail.com>
2016-07-12 11:19:59 +0900

    Test back-prop calculated value.

--- tests/chainer_tests/links_tests/connection_tests/test_highway.py
+++ tests/chainer_tests/links_tests/connection_tests/test_highway.py
@@ -8,6 +8,7 @@ from chainer import gradient_check
 from chainer import links
 from chainer import testing
 from chainer.testing import attr
+from chainer.testing import condition
 
 
 class TestHighway(unittest.TestCase):
@@ -23,10 +24,20 @@ class TestHighway(unittest.TestCase):
             self.in_out_size)
 
         Wh = self.link.plain.W.data
+        Wh[...] = numpy.random.uniform(-1, 1, Wh.shape)
         bh = self.link.plain.b.data
+        bh[...] = numpy.random.uniform(-1, 1, bh.shape)
 
         Wt = self.link.transform.W.data
+        Wt[...] = numpy.random.uniform(-1, 1, Wt.shape)
         bt = self.link.transform.b.data
+        bt[...] = numpy.random.uniform(-1, 1, bt.shape)
+        self.link.zerograds()
+
+        self.Wh = Wh.copy()  # fixed on CPU
+        self.bh = bh.copy()  # fixed on CPU
+        self.Wt = Wt.copy()  # fixed on CPU
+        self.bt = bt.copy()  # fixed on CPU
 
         a = self.relu(self.x.dot(Wh.T) + bh)
         b = self.sigmoid(self.x.dot(Wt.T) + bt)
@@ -46,24 +57,29 @@ class TestHighway(unittest.TestCase):
         self.assertEqual(y.data.dtype, numpy.float32)
         gradient_check.assert_allclose(self.y, y.data)
 
+    @condition.retry(3)
     def test_forward_cpu(self):
         self.check_forward(self.x)
 
     @attr.gpu
+    @condition.retry(3)
     def test_forward_gpu(self):
         self.link.to_gpu()
         self.check_forward(cuda.to_gpu(self.x))
 
     def check_backward(self, x_data, y_grad):
-        x = chainer.Variable(x_data)
-        y = self.link(x)
-        y.grad = y_grad
-        y.backward()
+        gradient_check.check_backward(
+            self.link, x_data, y_grad,
+            (self.link.plain.W, self.link.plain.b,
+             self.link.transform.W, self.link.transform.b),
+            eps=1e-2, atol=1.5e-3, rtol=1e-2)
 
+    @condition.retry(3)
     def test_backward_cpu(self):
         self.check_backward(self.x, self.gy)
 
     @attr.gpu
+    @condition.retry(3)
     def test_backward_gpu(self):
         self.link.to_gpu()
         self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
