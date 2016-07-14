7a29cfd1c0a09d7d085c0a13bebfd69e016c55bb
Yuya Unno <unnonouno@gmail.com>
2016-07-14 11:35:23 +0900

    Remove random behavior from gradient noise test

--- tests/chainer_tests/test_optimizer.py
+++ tests/chainer_tests/test_optimizer.py
@@ -159,28 +159,34 @@ class TestOptimizerLasso(unittest.TestCase):
 
 class TestOptimizerGradientNoise(unittest.TestCase):
 
+    eta = 0.01
+
     def setUp(self):
         self.target = SimpleLink(
             np.arange(6, dtype=np.float32).reshape(2, 3),
             np.arange(3, -3, -1, dtype=np.float32).reshape(2, 3))
 
+        self.noise_value = np.random.normal(
+            loc=0, scale=np.sqrt(self.eta / np.power(1, 0.55)),
+            size=(2, 3)).astype(np.float32)
+
     def check_gradient_noise(self):
         w = self.target.param.data
         g = self.target.param.grad
         xp = cuda.get_array_module(w)
+        noise_value = xp.asarray(self.noise_value)
 
-        eta = 0.01
-        expect = w - g - xp.random.normal(loc=0,
-                                          scale=np.sqrt(
-                                              eta / np.power(1, 0.55)),
-                                          size=g.shape).astype(np.float32)
+        expect = w - g - noise_value
 
+        noise = mock.Mock(return_value=noise_value)
         opt = optimizers.SGD(lr=1)
         opt.setup(self.target)
-        opt.add_hook(optimizer.GradientNoise(eta))
+        hook = optimizer.GradientNoise(self.eta, noise_func=noise)
+        opt.add_hook(hook)
         opt.update()
 
         testing.assert_allclose(expect, w, rtol=0.4)
+        noise.assert_called_once_with(xp, (2, 3), np.float32, hook, opt)
 
     def test_gradient_noise_cpu(self):
         self.check_gradient_noise()
