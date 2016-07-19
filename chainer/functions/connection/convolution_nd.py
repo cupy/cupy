4cf5c6ba842080ed832d197474d8ddb286e8f725
Masayuki Takagi <kamonama@gmail.com>
2016-07-19 17:46:32 +1000

    Refactor ConvolutionND function's forward computation.

--- chainer/functions/connection/convolution_nd.py
+++ chainer/functions/connection/convolution_nd.py
@@ -55,32 +55,39 @@ class ConvolutionND(function.Function):
                 b_type.shape[0] == w_type.shape[0],
             )
 
-    def forward_cpu(self, inputs):
-        x, W = inputs[:2]
-        b = inputs[2] if len(inputs) == 3 else None
+    def _use_cudnn(self, x, W):
+        return (not self.cover_all and
+                cuda.cudnn_enabled and
+                self.use_cudnn and
+                self.ndim > 1 and
+                convolution_2d._check_cudnn_acceptable_type(x.dtype, W.dtype))
+
+    def _forward_xp(self, x, W, b, xp):
         ndim = self.ndim
         ksize = W.shape[2:]
         stride = self.stride
         pad = self.pad
 
         # Make patch array.
-        self.col = conv_nd.im2col_nd_cpu(
-            x, ksize, stride, pad, cover_all=self.cover_all)
+        if xp is numpy:
+            self.col = conv_nd.im2col_nd_cpu(
+                x, ksize, stride, pad, cover_all=self.cover_all)
+        else:
+            self.col = conv_nd.im2col_nd_gpu(
+                x, ksize, stride, pad, cover_all=self.cover_all)
 
         # Compute correlation.
         axes = tuple(moves.range(1, ndim + 2))  # (1, 2, ..., N+1)
-        y = numpy.tensordot(self.col, W, (axes, axes)).astype(x.dtype)
+        y = xp.tensordot(self.col, W, (axes, axes)).astype(x.dtype)
 
         # Apply bias if given.
         if b is not None:
             y += b
 
         # Roll c_O before the second in (n, y_1, y_2, ..., y_N, c_O).
-        return numpy.rollaxis(y, ndim + 1, 1),
+        return xp.rollaxis(y, ndim + 1, 1),
 
-    def forward_gpu(self, inputs):
-        x, W = inputs[:2]
-        b = inputs[2] if len(inputs) == 3 else None
+    def _forward_cudnn(self, x, W, b):
         out_c = W.shape[0]      # (c_O, _, k_1, k_2, ..., k_N)
         ksize = W.shape[2:]
         n, c = x.shape[:2]      # (n, c_I, d_1, d_2, ..., d_N)
@@ -98,67 +105,62 @@ class ConvolutionND(function.Function):
         # Make empty array for result.
         y_shape = (n, out_c) + outs  # (n, c_O, out_1, out_2, ..., out_N)
         y = cuda.cupy.empty(y_shape, dtype=x.dtype)
-        # Implementation using cuDNN.
-        if (not self.cover_all and cuda.cudnn_enabled and self.use_cudnn and
-            ndim > 1 and convolution_2d._check_cudnn_acceptable_type(
-                x.dtype, W.dtype)):
-            x = cuda.cupy.ascontiguousarray(x)
-            W = cuda.cupy.ascontiguousarray(W)
-            if b is not None:
-                b = cuda.cupy.ascontiguousarray(b)
 
-            handle = cudnn.get_handle()
-            x_desc = cudnn.create_tensor_descriptor(x)
-            y_desc = cudnn.create_tensor_descriptor(y)
+        # Convert to C-contiguous arrays.
+        x = cuda.cupy.ascontiguousarray(x)
+        W = cuda.cupy.ascontiguousarray(W)
+        if b is not None:
+            b = cuda.cupy.ascontiguousarray(b)
 
-            self.filter_desc = cudnn.create_filter_descriptor(W)
-            self.conv_desc = cudnn.create_convolution_descriptor(
-                pad, stride)
-            if b is not None:
-                b_index = (None, colon) + (None,) * ndim
-                self.bias_desc = cudnn.create_tensor_descriptor(b[b_index])
+        # Get cuDNN handler and descriptors.
+        handle = cudnn.get_handle()
+        x_desc = cudnn.create_tensor_descriptor(x)
+        y_desc = cudnn.create_tensor_descriptor(y)
 
-            workspace_size = cuda.get_max_workspace_size()
-            workspace = cuda.cupy.empty((workspace_size,), dtype='b')
-            algo = libcudnn.getConvolutionForwardAlgorithm(
-                handle, x_desc.value, self.filter_desc.value,
-                self.conv_desc.value, y_desc.value, _fwd_pref,
-                workspace_size)
+        self.filter_desc = cudnn.create_filter_descriptor(W)
+        self.conv_desc = cudnn.create_convolution_descriptor(pad, stride)
+        if b is not None:
+            b_index = (None, colon) + (None,) * ndim
+            self.bias_desc = cudnn.create_tensor_descriptor(b[b_index])
 
-            oz_dtype = 'd' if x.dtype == 'd' else 'f'
-            one = numpy.array(1, dtype=oz_dtype).ctypes
-            zero = numpy.array(0, dtype=oz_dtype).ctypes
-            libcudnn.convolutionForward(
-                handle, one.data, x_desc.value, x.data.ptr,
-                self.filter_desc.value, W.data.ptr, self.conv_desc.value,
-                algo, workspace.data.ptr, workspace_size, zero.data,
-                y_desc.value, y.data.ptr)
+        # Find cuDNN algorithm to be used.
+        workspace_size = cuda.get_max_workspace_size()
+        workspace = cuda.cupy.empty((workspace_size,), dtype='b')
+        algo = libcudnn.getConvolutionForwardAlgorithm(
+            handle, x_desc.value, self.filter_desc.value,
+            self.conv_desc.value, y_desc.value, _fwd_pref,
+            workspace_size)
 
-            # TODO(takagi) Support unshared bias
-            if b is not None:
-                cudnn.add_tensor(
-                    handle, one.data, self.bias_desc.value, b.data.ptr,
-                    one.data, y_desc.value, y.data.ptr)
+        # cuDNN forward computation.
+        oz_dtype = 'd' if x.dtype == 'd' else 'f'
+        one = numpy.array(1, dtype=oz_dtype).ctypes
+        zero = numpy.array(0, dtype=oz_dtype).ctypes
+        libcudnn.convolutionForward(
+            handle, one.data, x_desc.value, x.data.ptr,
+            self.filter_desc.value, W.data.ptr, self.conv_desc.value,
+            algo, workspace.data.ptr, workspace_size, zero.data,
+            y_desc.value, y.data.ptr)
 
-            return y,
+        # Add bias if given.
+        # TODO(takagi) Support unshared bias
+        if b is not None:
+            cudnn.add_tensor(
+                handle, one.data, self.bias_desc.value, b.data.ptr,
+                one.data, y_desc.value, y.data.ptr)
 
-        # Implementation using im2col.
+        return y,
+
+    def forward(self, inputs):
+        x, W = inputs[:2]
+        b = inputs[2] if len(inputs) == 3 else None
+
+        xp = cuda.get_array_module(*inputs)
+        if xp is numpy:
+            return self._forward_xp(x, W, b, numpy)
+        elif not self._use_cudnn(x, W):
+            return self._forward_xp(x, W, b, cuda.cupy)
         else:
-            # Make patch array.
-            self.col = conv_nd.im2col_nd_gpu(
-                x, ksize, stride, pad, cover_all=self.cover_all)
-
-            # Compute correlation.
-            axes = tuple(moves.range(1, ndim + 2))  # (1, 2, ..., N+1)
-            y = cuda.cupy.tensordot(self.col, W, (axes, axes)).astype(x.dtype)
-
-            # Apply bias if given.
-            # TODO(takagi): Support unshared bias
-            if b is not None:
-                y += b
-
-            # Roll c_O before the second in (n, y_1, y_2, ..., y_N, c_O).
-            return cuda.cupy.rollaxis(y, ndim + 1, 1),
+            return self._forward_cudnn(x, W, b)
 
     def backward_cpu(self, inputs, grad_outputs):
         x, W = inputs[:2]
