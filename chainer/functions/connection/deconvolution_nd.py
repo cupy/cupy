f509af950ca279f1d3280b14b20d8e705c8b4912
Masayuki Takagi <kamonama@gmail.com>
2016-07-24 15:52:55 +1000

    First implementation for N-dimensional deconvolution function's backward computation.

--- chainer/functions/connection/deconvolution_nd.py
+++ chainer/functions/connection/deconvolution_nd.py
@@ -1,4 +1,5 @@
 import numpy
+import six
 
 from chainer import cuda
 from chainer import function
@@ -169,6 +170,111 @@ class DeconvolutionND(function.Function):
             return self._forward_cudnn(x, W, b)
         else:
             return self._forward_xp(x, W, b, cuda.cupy)
+
+    def _backward_xp(self, x, W, b, gy, xp):
+        ndim = self.ndim
+        ksize = W.shape[2:]
+        stride = self.stride
+        pad = self.pad
+        if xp is numpy:
+            col = conv_nd.im2col_nd_cpu(gy, ksize, stride, pad)
+        else:
+            col = conv_nd.im2col_nd_gpu(gy, ksize, stride, pad)
+
+        # x  : n, C_I, d_1, d_2, ..., d_N
+        # col: n, C_I, k_1, k_2, ..., k_N, d_1, d_2, ..., d_N
+        x_axes = (0,) + tuple(six.moves.range(2, ndim + 2))
+        col_axes = (0,) + tuple(six.moves.range(ndim + 2, ndim * 2 + 2))
+        gW = xp.tensordot(x, col, (x_axes, col_axes)).astype(W.dtype)
+
+        # col: n, C_I, k_1, k_2, ..., k_N, d_1, d_2, ..., d_N
+        # W  : C_I, C_O, k_1, k_2, ..., k_N
+        axes = (1,) + tuple(six.moves.range(2, ndim + 2))
+        gx = xp.tensordot(col, W, (axes, axes)).astype(x.dtype)
+        gx = xp.rollaxis(gx, ndim + 1, 1)
+
+        if b is None:
+            return gx, gW
+        else:
+            sum_axis = (0,) + tuple(six.moves.range(2, ndim + 2))
+            gb = gy.sum(axis=sum_axis)
+            return gx, gW, gb
+
+    def _backward_cudnn(self, x, W, b, gy):
+        # Convert to C-contiguous arrays.
+        x = cuda.cupy.ascontiguousarray(x)
+        W = cuda.cupy.ascontiguousarray(W)
+        gy = cuda.cupy.ascontiguousarray(gy)
+
+        # Make empty arrays for results.
+        gx = cuda.cupy.empty_like(x)
+        gW = cuda.cupy.empty_like(W)
+
+        # Get cuDNN handler and descriptors.
+        handle = cudnn.get_handle()
+        gy_desc = cudnn.create_tensor_descriptor(gy)
+        gx_desc = cudnn.create_tensor_descriptor(gx)
+
+        # Chance to choose implicit-precom-gemm algorithm.
+        workspace_size = cuda.get_max_workspace_size()
+        algo = libcudnn.getConvolutionForwardAlgorithm(
+            handle, gy_desc.value, self.filter_desc.value,
+            self.conv_desc.value, gx_desc.value, _fwd_pref,
+            workspace_size)
+        workspace = cuda.cupy.empty((workspace_size,), dtype='b')
+
+        # Compute input gradient.
+        oz_dtype = 'd' if x.dtype == 'd' else 'f'
+        one = numpy.array(1, dtype=oz_dtype).ctypes
+        zero = numpy.array(0, dtype=oz_dtype).ctypes
+        libcudnn.convolutionForward(
+            handle, one.data, gy_desc.value, gy.data.ptr,
+            self.filter_desc.value, W.data.ptr,
+            self.conv_desc.value, algo, workspace.data.ptr, workspace_size,
+            zero.data, gx_desc.value, gx.data.ptr)
+
+        # Compute bias gradient.
+        if b is not None:
+            gb = cuda.cupy.empty_like(b)
+            libcudnn.convolutionBackwardBias(
+                handle, one.data, gy_desc.value, gy.data.ptr,
+                zero.data, self.bias_desc.value, gb.data.ptr)
+
+        # Compute filter gradient.
+        if _cudnn_version >= 4000:
+            algo = libcudnn.getConvolutionBackwardFilterAlgorithm(
+                handle, gy_desc.value, gx_desc.value,
+                self.conv_desc.value, self.filter_desc.value,
+                _bwd_filter_pref, workspace_size)
+
+            libcudnn.convolutionBackwardFilter_v3(
+                handle, one.data, gy_desc.value, gy.data.ptr,
+                gx_desc.value, x.data.ptr, self.conv_desc.value,
+                algo, workspace.data.ptr, workspace_size,
+                zero.data, self.filter_desc.value, gW.data.ptr)
+        else:
+            libcudnn.convolutionBackwardFilter_v2(
+                handle, one.data, gy_desc.value, gy.data.ptr,
+                gx_desc.value, x.data.ptr, self.conv_desc.value,
+                zero.data, self.filter_desc.value, gW.data.ptr)
+
+        if b is None:
+            return gx, gW
+        else:
+            return gx, gW, gb
+
+    def backward(self, inputs, grad_outputs):
+        x, W = inputs[:2]
+        b = inputs[2] if len(inputs) == 3 else None
+        gy = grad_outputs[0]
+
+        xp = cuda.get_array_module(*inputs)
+        if xp is numpy:
+            return self._backward_xp(x, W, b, gy, numpy)
+        elif self._use_cudnn(x, W):
+            return self._backward_cudnn(x, W, b, gy)
+        else:
+            return self._backward_xp(x, W, b, gy, cuda.cupy)
 
 
 def deconvolution_nd(x, W, b=None, stride=1, pad=0,
