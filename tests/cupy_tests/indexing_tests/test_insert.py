7480c76ef75b3fed744a7f14bebba389ca7f9709
Masaki Saito <msaito@preferred.jp>
2016-07-25 20:13:27 +0900

    Fix typo

--- tests/cupy_tests/indexing_tests/test_insert.py
+++ tests/cupy_tests/indexing_tests/test_insert.py
@@ -12,26 +12,26 @@ class TestInsert(unittest.TestCase):
     @testing.numpy_cupy_array_equal()
     def test_fill_diagonal_shape1(self, xp, dtype):
         a = testing.shaped_arange((3, 3), xp, dtype)
-        xp.fill_diag(a, val=0)
+        xp.fill_diagonal(a, val=0)
         return a
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal()
     def test_fill_diagonal_shape2(self, xp, dtype):
         a = testing.shaped_arange((2, 2, 2), xp, dtype)
-        xp.fill_diag(a, val=1)
+        xp.fill_diagonal(a, val=1)
         return a
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal()
     def test_fill_diagonal_wrap1(self, xp, dtype):
         a = testing.shaped_arange((3, 5), xp, dtype)
-        xp.fill_diag(a, val=0, wrap=True)
+        xp.fill_diagonal(a, val=0, wrap=True)
         return a
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal()
     def test_fill_diagonal_wrap2(self, xp, dtype):
         a = testing.shaped_arange((5, 3), xp, dtype)
-        xp.fill_diag(a, val=0, wrap=True)
+        xp.fill_diagonal(a, val=0, wrap=True)
         return a
