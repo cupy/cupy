7cf96ea4f57c325a67d9894b4051c149a89ef325
Masayuki Takagi <kamonama@gmail.com>
2016-07-29 15:49:51 +1000

    Fix ConvolutionND link's specification and its documentation.

--- chainer/links/connection/convolution_nd.py
+++ chainer/links/connection/convolution_nd.py
@@ -20,23 +20,26 @@ class ConvolutionND(link.Link):
             ``stride=s`` and ``stride=(s, s, ..., s)`` are equivalent.
         pad (int or tuple of ints): Spatial padding width for input arrays.
             ``pad=p`` and ``pad=(p, p, ..., p)`` are equivalent.
-        initialW: Value used to initialize the filter weight. May be a scalar
-            to set all values to, an ``numpy.ndarray`` to be assigned, or a
-            callable that takes :class:`numpy.ndarray` or :class:`cupy.ndarray`
-            and edits its value.
-        initial_bias: Value used to initialize the bias vector. May be a scalar
-            to set all values to, an ``numpy.ndarray`` to be assigned, a
-            callable that takes :class:`numpy.ndarray` or :class:`cupy.ndarray`
-            and edits its value, or ``None``. If ``None`` is given, this link
-            does not use the bias term.
+        initialW: Value used to initialize the filter weight. May be an
+            initializer instance or another value that
+            :func:`~chainer.init_weight` helper function can take. This link
+            uses :func:`~chainer.init_weight` to initialize the filter weight
+            and passes the value of ``initialW`` to it as it is.
+        initial_bias: Value used to initialize the bias vector. May be an
+            initializer instance or another value except ``None`` that
+            :func:`~chainer.init_weight` helper function can take. If ``None``
+            is given, this link does not use the bias vector. This link uses
+            :func:`~chainer.init_weight` to initialize the bias vector and
+            passes the value of ``initial_bias`` other than ``None`` to it as
+            it is.
         use_cudnn (bool): If ``True``, then this link uses cuDNN if available.
         cover_all (bool): If ``True``, all spatial locations are convoluted
             into some output pixels. It may make the output size larger.
 
     .. seealso::
-        See :func:`chainer.functions.convolution_nd` for the definition of
-        N-dimensional convolution.
-        See :func:`chainer.functions.convolution_2d` for the definition of
+        See :func:`~chainer.functions.convolution_nd` for the definition of
+        N-dimensional convolution. See
+        :func:`~chainer.functions.convolution_2d` for the definition of
         two-dimensional convolution.
 
     Attributes:
@@ -47,15 +50,14 @@ class ConvolutionND(link.Link):
     """
 
     def __init__(self, ndim, in_channels, out_channels, ksize, stride=1, pad=0,
-                 initialW=1, initial_bias=0, use_cudnn=True, cover_all=False):
+                 initialW=None, initial_bias=None, use_cudnn=True,
+                 cover_all=False):
         ksize = conv_nd.as_tuple(ksize, ndim)
         self.stride = stride
         self.pad = pad
         self.use_cudnn = use_cudnn
         self.cover_all = cover_all
 
-        if initialW is None:
-            raise ValueError('Invalid filter weight initializer.')
         W_shape = (out_channels, in_channels) + ksize
         super(ConvolutionND, self).__init__(W=W_shape)
         initializers.init_weight(self.W.data, initialW)
