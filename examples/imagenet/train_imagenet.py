eaa9e0fc4b5f647126dc978050fb54639eb44cd6
Takuya Akiba <t.akiba.65536@gmail.com>
2016-08-08 16:54:03 +0900

    Modify imagenet example to print time and learning rate

--- examples/imagenet/train_imagenet.py
+++ examples/imagenet/train_imagenet.py
@@ -146,9 +146,11 @@ def main():
     # Be careful to pass the interval directly to LogReport
     # (it determines when to emit log rather than when to read observations)
     trainer.extend(extensions.LogReport(trigger=log_interval))
+    trainer.extend(extensions.observe_time(), trigger=log_interval)
+    trainer.extend(extensions.observe_lr(optimizer), trigger=log_interval)
     trainer.extend(extensions.PrintReport([
-        'epoch', 'iteration', 'main/loss', 'validation/main/loss',
-        'main/accuracy', 'validation/main/accuracy',
+        'time', 'epoch', 'iteration', 'main/loss', 'validation/main/loss',
+        'main/accuracy', 'validation/main/accuracy', 'lr'
     ]), trigger=log_interval)
     trainer.extend(extensions.ProgressBar(update_interval=10))
 
