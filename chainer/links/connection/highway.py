add9640946036225bd71fa1f7d3aa30ce31ad669
Eiichi Matsumoto <matsumoto@preferred.jp>
2016-08-08 16:22:50 +0900

    highway doc fix

--- chainer/links/connection/highway.py
+++ chainer/links/connection/highway.py
@@ -7,25 +7,21 @@ from chainer.links.connection import linear
 class Highway(link.Chain):
 
     """Highway module.
-
     In highway network, two gates are added to the ordinal non-linear
-    transformation (:math:H(x) = activate(W_h x + b_h)).
-    One gate is the transform gate :math:T(x) = sigma(W_t x + b_t), and the
-    other is the carry gate :math:C(x).
-    For simplicity, the author defined :math:C = 1 - T.
-
+    transformation (:math:`H(x) = activate(W_h x + b_h)`).
+    One gate is the transform gate :math:`T(x) = \\sigma(W_t x + b_t)`, and the
+    other is the carry gate :math:`C(x)`.
+    For simplicity, the author defined :math:`C = 1 - T`.
     Highway module returns :math:`y` defined as
-
+    
     .. math::
-
-    y &=& activate(W_h x + b_h)\\odot\\sigma(W_t x + b_t)) +
-          x\\odot(1 - \\sigma(W_t x + b_t))
-
+    
+        y = activate(W_h x + b_h) \\odot \\sigma(W_t x + b_t) +
+        x \\odot(1 - \\sigma(W_t x + b_t))
+          
     The output array has the same spatial size as the input. In order to
     satisfy this, :math:`W_h` and :math:`W_t` must be square matrix.
-
-    See: `Highway Networks <https://arxiv.org/abs/1505.00387>`_.
-
+    
     Args:
         in_out_size (int): Dimension of input and output vectors.
         nobias (bool): If ``True``, then this function does not use the bias.
@@ -48,7 +44,9 @@ class Highway(link.Chain):
             May also be a callable that takes ``numpy.ndarray`` or
             ``cupy.ndarray`` and edits its value.
             Negative value is sufficient for learning (e.g. -1, -3, ...).
-
+    
+    See:
+        `Highway Networks <https://arxiv.org/abs/1505.00387>`_.
     """
 
     def __init__(self, in_out_size, nobias=False, activate=relu.relu,
@@ -63,14 +61,11 @@ class Highway(link.Chain):
 
     def __call__(self, x):
         """Computes the output of the Highway module.
-
         Args:
             x (~chainer.Variable): Input variable.
-
         Returns:
             Variable: Output variable. Its array has the same spatial size and
             the same minibatch size as the input array.
-
         """
         out_plain = self.activate(self.plain(x))
         out_transform = sigmoid.sigmoid(self.transform(x))
