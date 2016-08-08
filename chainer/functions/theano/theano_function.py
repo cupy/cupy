3ed8c7f13314eadb520b220e69a4ff7a1e90e978
Yuya Unno <unnonouno@gmail.com>
2016-08-08 18:04:32 +0900

    Flake8

--- chainer/functions/theano/theano_function.py
+++ chainer/functions/theano/theano_function.py
@@ -68,7 +68,8 @@ Please install theano to activate theano function.
     def check_type_forward(self, in_types):
         type_check.expect(in_types.size() == len(self.func.input_storage))
 
-        for actual_type, storage in six.moves.zip(in_types, self.func.input_storage):
+        for actual_type, storage in six.moves.zip(
+                in_types, self.func.input_storage):
             expect_type = storage.type
             # Theano cannot check shapes of variables
             type_check.expect(
