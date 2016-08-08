5ec9913f2aa414d317b122cd74f641c51338ebd0
Daiki.Sanno <daiki.sanno@gmail.com>
2016-08-08 21:45:08 +0900

    Fix to use get_trigger instead of IntervalTrigger constructor.

--- chainer/training/triggers/minmax_value_trigger.py
+++ chainer/training/triggers/minmax_value_trigger.py
@@ -1,5 +1,5 @@
 from chainer import reporter
-from chainer.training.trigger import IntervalTrigger
+from chainer.training import trigger as trigger_module
 
 
 class BestValueTrigger(object):
@@ -20,7 +20,7 @@ class BestValueTrigger(object):
     def __init__(self, key, compare, trigger=(1, 'epoch')):
         self._key = key
         self._best_value = None
-        self._interval_trigger = IntervalTrigger(*trigger)
+        self._interval_trigger = trigger_module.get_trigger(trigger)
         self._init_summary()
         self._compare = compare
 
