a2a6698b18e848085982e199e44ac3e9ea8c9ffa
Kenta OONO <oono@preferred.jp>
2016-08-15 17:11:02 +0900

    Extract IntervalTrigger from trigger module and put it under triggers namespace

--- chainer/training/triggers/minmax_value_trigger.py
+++ chainer/training/triggers/minmax_value_trigger.py
@@ -1,5 +1,5 @@
 from chainer import reporter
-from chainer.training import trigger as trigger_module
+from chainer.training.triggers import interval
 
 
 class BestValueTrigger(object):
@@ -20,7 +20,7 @@ class BestValueTrigger(object):
     def __init__(self, key, compare, trigger=(1, 'epoch')):
         self._key = key
         self._best_value = None
-        self._interval_trigger = trigger_module.get_trigger(trigger)
+        self._interval_trigger = interval.get_trigger(trigger)
         self._init_summary()
         self._compare = compare
 
