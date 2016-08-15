616167b070c954f4cd1f7edddbd7a26ebe3ce844
Masayuki Takagi <kamonama@gmail.com>
2016-08-16 01:32:13 +0900

    Fix to clear memo for valid tests.

--- tests/chainer_tests/utils_tests/test_conv_nd_kernel.py
+++ tests/chainer_tests/utils_tests/test_conv_nd_kernel.py
@@ -2,6 +2,7 @@ import unittest
 
 import mock
 
+import chainer
 from chainer import testing
 from chainer.testing import attr
 from chainer.utils import conv_nd_kernel
@@ -17,6 +18,7 @@ class TestIm2colNDKernelMemo(unittest.TestCase):
         ndim = self.ndim
         with mock.patch(
                 'chainer.utils.conv_nd_kernel.Im2colNDKernel._generate') as m:
+            chainer.cuda.clear_memo()
             conv_nd_kernel.Im2colNDKernel.generate(ndim)
             m.assert_called_once_with(ndim)
             conv_nd_kernel.Im2colNDKernel.generate(ndim)
@@ -33,6 +35,7 @@ class TestCol2imNDKernelMemo(unittest.TestCase):
         ndim = self.ndim
         with mock.patch(
                 'chainer.utils.conv_nd_kernel.Col2imNDKernel._generate') as m:
+            chainer.cuda.clear_memo()
             conv_nd_kernel.Col2imNDKernel.generate(ndim)
             m.assert_called_once_with(ndim)
             conv_nd_kernel.Col2imNDKernel.generate(ndim)
