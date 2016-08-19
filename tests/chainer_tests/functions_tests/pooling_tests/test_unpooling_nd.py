b10480b8df68d13c8421e96907f9bf443f9788ab
Masayuki Takagi <kamonama@gmail.com>
2016-08-19 17:37:28 +1000

    Add test for outsize.

--- tests/chainer_tests/functions_tests/pooling_tests/test_unpooling_nd.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_unpooling_nd.py
@@ -11,6 +11,7 @@ from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
 from chainer.utils import conv
+from chainer.utils import type_check
 
 
 def xs_iter(dims):
@@ -21,6 +22,21 @@ def kxs_iter(x, outs, ksize, stride, pad):
     return itertools.product(
         *[range(max(0, -p + s * _x), min(-p + s * _x + k, out))
           for (_x, out, k, s, p) in zip(x, outs, ksize, stride, pad)])
+
+
+def expected_unpooling_nd(x_data, outs, ksize, stride, pad):
+    N, c = x_data.shape[:2]
+    dims = x_data.shape[2:]
+    y_expected_shape = (N, c) + outs
+    y_expected = numpy.zeros(y_expected_shape, dtype=x_data.dtype)
+    for i in six.moves.range(N):
+        for _c in six.moves.range(c):
+            for x in xs_iter(dims):
+                x_idx = (i, _c) + x
+                for kx in kxs_iter(x, outs, ksize, stride, pad):
+                    y_idx = (i, _c) + kx
+                    y_expected[y_idx] += x_data[x_idx]
+    return y_expected
 
 
 @testing.parameterize(*testing.product({
@@ -52,7 +68,6 @@ class TestUnpoolingND(unittest.TestCase):
         self.gy = numpy.random.uniform(-1, 1, gy_shape).astype(self.dtype)
 
     def check_forward(self, x_data):
-        dims = self.dims
         ksize = self.ksize
         stride = self.stride
         pad = self.pad
@@ -60,24 +75,15 @@ class TestUnpoolingND(unittest.TestCase):
         # Compute unpooling.
         x = chainer.Variable(x_data)
         y = functions.unpooling_nd(
-            x, self.ksize, self.stride, self.pad, cover_all=self.cover_all)
+            x, ksize, stride, pad, cover_all=self.cover_all)
 
         # Test output's dtype and shape.
         self.assertEqual(y.data.dtype, self.dtype)
         self.assertEqual(y.data.shape, self.gy.shape)
 
         # Test output's value.
-        N, c = x_data.shape[:2]
         outs = self.gy.shape[2:]
-        y_expected_shape = (N, c) + outs
-        y_expected = numpy.zeros(y_expected_shape, dtype=x_data.dtype)
-        for i in six.moves.range(N):
-            for _c in six.moves.range(c):
-                for x in xs_iter(dims):
-                    x_idx = (i, _c) + x
-                    for kx in kxs_iter(x, outs, ksize, stride, pad):
-                        y_idx = (i, _c) + kx
-                        y_expected[y_idx] += self.x[x_idx]
+        y_expected = expected_unpooling_nd(self.x, outs, ksize, stride, pad)
         testing.assert_allclose(y_expected, y.data)
 
     @condition.retry(3)
@@ -102,9 +108,66 @@ class TestUnpoolingND(unittest.TestCase):
         pass
 
 
-# TODO(takgai) test outsize
+@testing.parameterize(*testing.product({
+    'outsize': [(10,), (10, 9), (10, 9, 8)],
+    '_ksize': [1, 2, 3],
+    '_stride': [1, 2, 3],
+    '_pad': [0, 1],
+    'cover_all': [True, False],
+}))
 class TestUnpoolingNDOutsize(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.N = 2
+        self.c = 3
+        ndim = len(self.outsize)
+        self.ksize = (self._ksize,) * ndim
+        self.stride = (self._stride,) * ndim
+        self.pad = (self._pad,) * ndim
+
+    def test_valid_insize(self):
+        N = self.N
+        c = self.c
+        ksize = self.ksize
+        stride = self.stride
+        pad = self.pad
+        outs = self.outsize
+        cover_all = self.cover_all
+
+        # Make input.
+        dims = tuple(conv.get_conv_outsize(out, k, s, p, cover_all=cover_all)
+                     for (out, k, s, p) in zip(outs, ksize, stride, pad))
+        x_shape = (N, c) + dims
+        x_data = numpy.random.uniform(-1, 1, x_shape).astype(numpy.float32)
+        x = chainer.Variable(x_data)
+
+        # Compute unpooling.
+        y = functions.unpooling_nd(
+            x, ksize, stride, pad, outsize=outs, cover_all=cover_all)
+
+        # Test output's value.
+        y_expected = expected_unpooling_nd(x_data, outs, ksize, stride, pad)
+        testing.assert_allclose(y_expected, y.data)
+
+    def test_invalid_insize(self):
+        ksize = self.ksize
+        stride = self.stride
+        pad = self.pad
+        outs = self.outsize
+        cover_all = self.cover_all
+
+        # Make input with invalid shape.
+        dims = tuple(conv.get_conv_outsize(out, k, s, p, cover_all=cover_all)
+                     for (out, k, s, p) in zip(outs, ksize, stride, pad))
+        dims = tuple(d + 1 for d in dims)  # Make invalid input shape.
+        x_shape = (self.N, self.c) + dims
+        x_data = numpy.random.uniform(-1, 1, x_shape).astype(numpy.float32)
+        x = chainer.Variable(x_data)
+
+        # Computing unpooling raises exception.
+        with self.assertRaises(type_check.InvalidType):
+            functions.unpooling_nd(
+                x, ksize, stride, pad, outsize=outs, cover_all=cover_all)
 
 
 testing.run_module(__name__, __file__)
