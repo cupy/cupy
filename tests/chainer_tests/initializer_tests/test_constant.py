thinkother/chainer

Support dtype option and initialization from shape

tests/chainer_tests/initializer_tests/test_constant.py
@@ -8,27 +8,52 @@ from chainer.testing import attr
 import numpy
 
 
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
 class TestIdentity(unittest.TestCase):
 
+    scale = 0.1
+    shape = (2, 2)
+
     def setUp(self):
-        self.scale = 0.1
-        self.shape = (2, 2)
-        self.initializer = initializers.Identity(scale=self.scale)
-        self.w = numpy.empty((2, 2), dtype=numpy.float32)
+        self.check_options = {}
+        if self.dtype == numpy.float16:
+            self.check_options = {'atol': 1e-4, 'rtol': 1e-3}
 
     def check_initializer(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        self.assertIsInstance(w, xp.ndarray)
+        initializer = initializers.Identity(scale=self.scale)
+        initializer(w)
         gradient_check.assert_allclose(
-            w, self.scale * numpy.identity(len(self.shape)))
+            w, self.scale * numpy.identity(len(self.shape)),
+            **self.check_options)
 
     def test_initializer_cpu(self):
-        self.check_initializer(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
     @attr.gpu
     def test_initializer_gpu(self):
-        self.check_initializer(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
+
+    def check_shaped_initializer(self, xp):
+        initializer = initializers.Identity(
+            scale=self.scale, dtype=self.dtype)
+        w = initializer(None, self.shape, xp)
+        self.assertIs(cuda.get_array_module(w), xp)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
+        gradient_check.assert_allclose(
+            w, self.scale * numpy.identity(len(self.shape)),
+            **self.check_options)
+
+    def test_shaped_initializer_cpu(self):
+        self.check_shaped_initializer(numpy)
+
+    @attr.gpu
+    def test_shaped_initializer_gpu(self):
+        self.check_shaped_initializer(cuda.cupy)
 
 
 @testing.parameterize(
@@ -42,32 +67,57 @@ class TestIdentityInvalid(unittest.TestCase):
         self.initializer = initializers.Identity()
 
     def test_invalid_shape(self):
-        w = numpy.empty(self.shape)
+        w = numpy.empty(self.shape, dtype=numpy.float32)
         with self.assertRaises(ValueError):
             self.initializer(w)
 
 
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
 class TestConstant(unittest.TestCase):
 
+    fill_value = 0.1
+    shape = (2, 3)
+
     def setUp(self):
-        self.fill_value = 0.1
-        self.initializer = initializers.Constant(fill_value=self.fill_value)
-        self.shape = (2, 3)
-        self.w = numpy.empty(self.shape, dtype=numpy.float32)
+        self.check_options = {}
+        if self.dtype == numpy.float16:
+            self.check_options = {'atol': 1e-4, 'rtol': 1e-3}
 
     def check_initializer(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        self.assertIsInstance(w, xp.ndarray)
+        initializer = initializers.Constant(fill_value=self.fill_value)
+        initializer(w)
         gradient_check.assert_allclose(
-            w, numpy.full(self.shape, self.fill_value))
+            w, numpy.full(self.shape, self.fill_value),
+            **self.check_options)
 
     def test_initializer_cpu(self):
-        self.check_initializer(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
     @attr.gpu
     def test_initializer_gpu(self):
-        self.check_initializer(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
+
+    def check_shaped_initializer(self, xp):
+        initializer = initializers.Constant(
+            fill_value=self.fill_value, dtype=self.dtype)
+        w = initializer(None, self.shape, xp)
+        self.assertIs(cuda.get_array_module(w), xp)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
+        gradient_check.assert_allclose(
+            w, numpy.full(self.shape, self.fill_value),
+            **self.check_options)
+
+    def test_shaped_initializer_cpu(self):
+        self.check_shaped_initializer(numpy)
+
+    @attr.gpu
+    def test_shaped_initializer_gpu(self):
+        self.check_shaped_initializer(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)

tests/chainer_tests/initializer_tests/test_orthogonal.py
@@ -8,66 +8,63 @@ from chainer.testing import attr
 import numpy
 
 
-class OrthogonalBase(object):
+@testing.parameterize(*testing.product({
+    'shape': [(), (1,), (3, 4), (3, 4, 5)],
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
+class OrthogonalBase(unittest.TestCase):
+
+    def setUp(self):
+        self.check_options = {}
+        if self.dtype == numpy.float16:
+            self.check_options = {'atol': 5e-3, 'rtol': 5e-2}
 
     def check_initializer(self, w):
-        expected_shape = w.shape
-        self.initializer(w)
-        self.assertTupleEqual(w.shape, expected_shape)
-        self.assertEqual(w.dtype, numpy.float32)
-        xp = cuda.get_array_module(w)
-        self.assertIsInstance(w, xp.ndarray)
+        initializer = initializers.Orthogonal(scale=2.0)
+        initializer(w)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
 
     def test_initializer_cpu(self):
-        self.check_initializer(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
     @attr.gpu
     def test_initializer_gpu(self):
-        self.check_initializer(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
+    def check_shaped_initializer(self, xp):
+        initializer = initializers.Orthogonal(scale=2.0, dtype=self.dtype)
+        w = initializer(None, self.shape, xp)
+        self.assertIs(cuda.get_array_module(w), xp)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
 
-@testing.parameterize(
-    {'shape': (1,)},
-    {'shape': (3, 4)},
-    {'shape': (3, 4, 5)})
-class TestOrthogonal(OrthogonalBase, unittest.TestCase):
+    def test_shaped_initializer_cpu(self):
+        self.check_shaped_initializer(numpy)
 
-    def setUp(self):
-        self.w = numpy.empty(self.shape, dtype=numpy.float32)
-        self.initializer = initializers.Orthogonal(scale=1.0)
+    @attr.gpu
+    def test_shaped_initializer_gpu(self):
+        self.check_shaped_initializer(cuda.cupy)
 
     def check_orthogonality(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        w = w.reshape(len(w), -1)
-        dots = xp.tensordot(w, w, (1, 1))
-        gradient_check.assert_allclose(dots, xp.identity(len(w)))
+        initializer = initializers.Orthogonal(scale=2.0)
+        initializer(w)
+        n = 1 if w.ndim == 0 else len(w)
+        w = w.astype(numpy.float64).reshape(n, -1)
+        dots = w.dot(w.T)
+        gradient_check.assert_allclose(
+            dots, numpy.identity(n) * 4, **self.check_options)
 
     def test_orthogonality_cpu(self):
-        self.check_orthogonality(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_orthogonality(w)
 
     @attr.gpu
     def test_orthogonality_gpu(self):
-        self.check_orthogonality(cuda.to_gpu(self.w))
-
-
-class TestZeroDim(OrthogonalBase, unittest.TestCase):
-
-    def setUp(self):
-        self.w = numpy.empty([], dtype=numpy.float32)
-        self.initializer = initializers.Orthogonal(scale=2.0)
-
-    def check_orthogonality(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        gradient_check.assert_allclose(w, xp.ones((), dtype=numpy.float32) * 2)
-
-    def test_orthogonality_cpu(self):
-        self.check_orthogonality(self.w)
-
-    @attr.gpu
-    def test_orthogonality_gpu(self):
-        self.check_orthogonality(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_orthogonality(w)
 
 
 class TestEmpty(unittest.TestCase):

cupy/cupy

tests/chainer_tests/initializer_tests/test_constant.py
@@ -7,27 +7,52 @@ from chainer.testing import attr
 import numpy
 
 
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
 class TestIdentity(unittest.TestCase):
 
+    scale = 0.1
+    shape = (2, 2)
+
     def setUp(self):
-        self.scale = 0.1
-        self.shape = (2, 2)
-        self.initializer = initializers.Identity(scale=self.scale)
-        self.w = numpy.empty((2, 2), dtype=numpy.float32)
+        self.check_options = {}
+        if self.dtype == numpy.float16:
+            self.check_options = {'atol': 1e-4, 'rtol': 1e-3}
 
     def check_initializer(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        self.assertIsInstance(w, xp.ndarray)
+        initializer = initializers.Identity(scale=self.scale)
+        initializer(w)
         testing.assert_allclose(
-            w, self.scale * numpy.identity(len(self.shape)))
+            w, self.scale * numpy.identity(len(self.shape)),
+            **self.check_options)
 
     def test_initializer_cpu(self):
-        self.check_initializer(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
     @attr.gpu
     def test_initializer_gpu(self):
-        self.check_initializer(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
+
+    def check_shaped_initializer(self, xp):
+        initializer = initializers.Identity(
+            scale=self.scale, dtype=self.dtype)
+        w = initializer(None, self.shape, xp)
+        self.assertIs(cuda.get_array_module(w), xp)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
+        testing.assert_allclose(
+            w, self.scale * numpy.identity(len(self.shape)),
+            **self.check_options)
+
+    def test_shaped_initializer_cpu(self):
+        self.check_shaped_initializer(numpy)
+
+    @attr.gpu
+    def test_shaped_initializer_gpu(self):
+        self.check_shaped_initializer(cuda.cupy)
 
 
 @testing.parameterize(
@@ -41,32 +66,57 @@ class TestIdentityInvalid(unittest.TestCase):
         self.initializer = initializers.Identity()
 
     def test_invalid_shape(self):
-        w = numpy.empty(self.shape)
+        w = numpy.empty(self.shape, dtype=numpy.float32)
         with self.assertRaises(ValueError):
             self.initializer(w)
 
 
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
 class TestConstant(unittest.TestCase):
 
+    fill_value = 0.1
+    shape = (2, 3)
+
     def setUp(self):
-        self.fill_value = 0.1
-        self.initializer = initializers.Constant(fill_value=self.fill_value)
-        self.shape = (2, 3)
-        self.w = numpy.empty(self.shape, dtype=numpy.float32)
+        self.check_options = {}
+        if self.dtype == numpy.float16:
+            self.check_options = {'atol': 1e-4, 'rtol': 1e-3}
 
     def check_initializer(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        self.assertIsInstance(w, xp.ndarray)
+        initializer = initializers.Constant(fill_value=self.fill_value)
+        initializer(w)
         testing.assert_allclose(
-            w, numpy.full(self.shape, self.fill_value))
+            w, numpy.full(self.shape, self.fill_value),
+            **self.check_options)
 
     def test_initializer_cpu(self):
-        self.check_initializer(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
     @attr.gpu
     def test_initializer_gpu(self):
-        self.check_initializer(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
+
+    def check_shaped_initializer(self, xp):
+        initializer = initializers.Constant(
+            fill_value=self.fill_value, dtype=self.dtype)
+        w = initializer(None, self.shape, xp)
+        self.assertIs(cuda.get_array_module(w), xp)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
+        testing.assert_allclose(
+            w, numpy.full(self.shape, self.fill_value),
+            **self.check_options)
+
+    def test_shaped_initializer_cpu(self):
+        self.check_shaped_initializer(numpy)
+
+    @attr.gpu
+    def test_shaped_initializer_gpu(self):
+        self.check_shaped_initializer(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)

tests/chainer_tests/initializer_tests/test_orthogonal.py
@@ -7,66 +7,63 @@ from chainer.testing import attr
 import numpy
 
 
-class OrthogonalBase(object):
+@testing.parameterize(*testing.product({
+    'shape': [(), (1,), (3, 4), (3, 4, 5)],
+    'dtype': [numpy.float16, numpy.float32, numpy.float64],
+}))
+class OrthogonalBase(unittest.TestCase):
+
+    def setUp(self):
+        self.check_options = {}
+        if self.dtype == numpy.float16:
+            self.check_options = {'atol': 5e-3, 'rtol': 5e-2}
 
     def check_initializer(self, w):
-        expected_shape = w.shape
-        self.initializer(w)
-        self.assertTupleEqual(w.shape, expected_shape)
-        self.assertEqual(w.dtype, numpy.float32)
-        xp = cuda.get_array_module(w)
-        self.assertIsInstance(w, xp.ndarray)
+        initializer = initializers.Orthogonal(scale=2.0)
+        initializer(w)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
 
     def test_initializer_cpu(self):
-        self.check_initializer(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
     @attr.gpu
     def test_initializer_gpu(self):
-        self.check_initializer(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_initializer(w)
 
+    def check_shaped_initializer(self, xp):
+        initializer = initializers.Orthogonal(scale=2.0, dtype=self.dtype)
+        w = initializer(None, self.shape, xp)
+        self.assertIs(cuda.get_array_module(w), xp)
+        self.assertTupleEqual(w.shape, self.shape)
+        self.assertEqual(w.dtype, self.dtype)
 
-@testing.parameterize(
-    {'shape': (1,)},
-    {'shape': (3, 4)},
-    {'shape': (3, 4, 5)})
-class TestOrthogonal(OrthogonalBase, unittest.TestCase):
+    def test_shaped_initializer_cpu(self):
+        self.check_shaped_initializer(numpy)
 
-    def setUp(self):
-        self.w = numpy.empty(self.shape, dtype=numpy.float32)
-        self.initializer = initializers.Orthogonal(scale=1.0)
+    @attr.gpu
+    def test_shaped_initializer_gpu(self):
+        self.check_shaped_initializer(cuda.cupy)
 
     def check_orthogonality(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        w = w.reshape(len(w), -1)
-        dots = xp.tensordot(w, w, (1, 1))
-        testing.assert_allclose(dots, xp.identity(len(w)))
+        initializer = initializers.Orthogonal(scale=2.0)
+        initializer(w)
+        n = 1 if w.ndim == 0 else len(w)
+        w = w.astype(numpy.float64).reshape(n, -1)
+        dots = w.dot(w.T)
+        testing.assert_allclose(
+            dots, numpy.identity(n) * 4, **self.check_options)
 
     def test_orthogonality_cpu(self):
-        self.check_orthogonality(self.w)
+        w = numpy.empty(self.shape, dtype=self.dtype)
+        self.check_orthogonality(w)
 
     @attr.gpu
     def test_orthogonality_gpu(self):
-        self.check_orthogonality(cuda.to_gpu(self.w))
-
-
-class TestZeroDim(OrthogonalBase, unittest.TestCase):
-
-    def setUp(self):
-        self.w = numpy.empty([], dtype=numpy.float32)
-        self.initializer = initializers.Orthogonal(scale=2.0)
-
-    def check_orthogonality(self, w):
-        self.initializer(w)
-        xp = cuda.get_array_module(w)
-        testing.assert_allclose(w, xp.ones((), dtype=numpy.float32) * 2)
-
-    def test_orthogonality_cpu(self):
-        self.check_orthogonality(self.w)
-
-    @attr.gpu
-    def test_orthogonality_gpu(self):
-        self.check_orthogonality(cuda.to_gpu(self.w))
+        w = cuda.cupy.empty(self.shape, dtype=self.dtype)
+        self.check_orthogonality(w)
 
 
 class TestEmpty(unittest.TestCase):