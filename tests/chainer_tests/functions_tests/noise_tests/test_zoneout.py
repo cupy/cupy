13c2cb89ca85b8dd376842fd26fd7ac21dc84c40
tohmae <t.ohmae.nx01@gmail.com>
2016-09-20 22:22:29 +0900

    Modify test_zoneout.py

--- tests/chainer_tests/functions_tests/noise_tests/test_zoneout.py
+++ tests/chainer_tests/functions_tests/noise_tests/test_zoneout.py
@@ -5,7 +5,6 @@ import numpy
 import chainer
 from chainer import cuda
 from chainer import functions
-from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
@@ -14,28 +13,35 @@ from chainer.testing import condition
 @testing.parameterize(
     {'ratio': 1},
     {'ratio': 0},
+    {'ratio': 0.5},
+    {'ratio': 0.25},
 )
 class TestZoneout(unittest.TestCase):
 
     def setUp(self):
-        self.h = numpy.random.uniform(-1, 1, (2, 3)).astype(numpy.float32)
-        self.x = numpy.random.uniform(-1, 1, (2, 3)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (2, 3)).astype(numpy.float32)
+        self.h = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
+        self.x = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (3, 4)).astype(numpy.float32)
 
     def check_forward(self, h_data, x_data):
         h = chainer.Variable(h_data)
         x = chainer.Variable(x_data)
-        h_next = functions.zoneout(h, x, ratio=self.ratio)
-        if self.ratio == 0:
-            h_next_expect = x
-        elif self.ratio == 1:
-            h_next_expect = h
+        f = functions.Zoneout(self.ratio)
+        h_next = f(h, x)
+        h_next_expect = h * f.flag_h + x * f.flag_x
         testing.assert_allclose(h_next.data, h_next_expect.data)
 
     def check_backward(self, h_data, x_data, y_grad):
-        gradient_check.check_backward(
-            functions.Zoneout(self.ratio), (h_data, x_data), y_grad,
-            atol=1e-4, rtol=1e-3)
+        h = chainer.Variable(h_data)
+        x = chainer.Variable(x_data)
+        f = functions.Zoneout(self.ratio)
+        h_next = f(h, x)
+        h_next.grad = y_grad
+        h_next.backward()
+        gh_expect = y_grad * f.flag_h
+        gx_expect = y_grad * f.flag_x
+        testing.assert_allclose(h.grad, gh_expect)
+        testing.assert_allclose(x.grad, gx_expect)
 
     def test_forward_cpu(self):
         self.check_forward(self.h, self.x)
