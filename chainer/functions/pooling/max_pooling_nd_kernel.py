d7cfd233bb5ada490f3eb368608a564d64674f9f
Masayuki Takagi <kamonama@gmail.com>
2016-09-26 17:58:30 +1000

    Remove unused import.

--- chainer/functions/pooling/max_pooling_nd_kernel.py
+++ chainer/functions/pooling/max_pooling_nd_kernel.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from chainer.functions.pooling import pooling_nd_kernel
 from chainer.utils.conv_nd_kernel import _map
 from chainer.utils.conv_nd_kernel import muladdexp
