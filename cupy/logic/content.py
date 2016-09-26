d165777eb678bdce32034aefc5712e9047e99009
Christoph <cbj@mail.uni-paderborn.de>
2016-09-26 11:49:25 +0200

    Bugfix: isfinite

--- cupy/logic/content.py
+++ cupy/logic/content.py
@@ -4,10 +4,8 @@ from cupy import core
 def _create_float_test_ufunc(name, doc):
     return core.create_ufunc(
         'cupy_' + name,
-        ('e->?', 'f->?', 'd->?',
-         ('F->?', '{0}(in0.real()) && {0}(in0.real())'.format(name)),
-         ('D->?', '{0}(in0.real()) && {0}(in0.real())'.format(name)),
-         ), 'out0 = %s(in0)' % name,
+        ('e->?', 'f->?', 'd->?', 'F->?', 'D->?',
+        ), 'out0 = %s(in0)' % name,
         doc=doc)
 
 
