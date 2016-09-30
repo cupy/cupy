4f27740b1c0f494f74ace063801c9b99fba3cd0c
Masayuki Takagi <kamonama@gmail.com>
2016-09-30 17:21:52 +1000

    Change to use default bias.

--- chainer/links/connection/deconvolution_nd.py
+++ chainer/links/connection/deconvolution_nd.py
@@ -44,8 +44,7 @@ class DeconvolutionND(link.Link):
     """
 
     def __init__(self, ndim, in_channels, out_channels, ksize, stride=1, pad=0,
-                 outsize=None, initialW=None, initial_bias=None,
-                 use_cudnn=True):
+                 outsize=None, initialW=None, initial_bias=0, use_cudnn=True):
         ksize = conv_nd.as_tuple(ksize, ndim)
         self.stride = stride
         self.pad = pad
