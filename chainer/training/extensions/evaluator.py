0416b098b45f3f52936d82be72c28704acabccb6
Kosuke Kusano <cocuh.kk@gmail.com>
2016-10-03 23:37:24 +0900

    fix flake8 errors

--- chainer/training/extensions/evaluator.py
+++ chainer/training/extensions/evaluator.py
@@ -1,6 +1,7 @@
 from chainer.dataset import convert
 from chainer import reporter as reporter_module
-from chainer.training import extension, StandardEvaluator
+from chainer.training import extension
+from chainer.training import StandardEvaluator
 
 
 class Evaluator(extension.Extension):
@@ -66,11 +67,11 @@ class Evaluator(extension.Extension):
     def __init__(self, iterator, target, converter=convert.concat_examples,
                  device=None, eval_hook=None, eval_func=None, evaluator=None):
         if evaluator is None:
-            evaluator = StandardEvaluator(
-                    iterator=iterator, target=target,
-                    converter=converter, device=device,
-                    eval_hook_before=eval_hook, eval_func=eval_func,
-            )
+            evaluator = StandardEvaluator(iterator=iterator, target=target,
+                                          converter=converter, device=device,
+                                          eval_hook_before=eval_hook,
+                                          eval_func=eval_func,
+                                          )
         self.evaluator = evaluator
 
     def get_iterator(self, name):
