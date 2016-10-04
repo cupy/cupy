08593825182c8b8a8ad3ad81b86eecd2ebe5831c
fukatani <nannyakannya@gmail.com>
2016-10-04 23:41:17 +0900

    fix test

--- tests/chainer_tests/functions_tests/math_tests/test_square.py
+++ tests/chainer_tests/functions_tests/math_tests/test_square.py
@@ -1,18 +1,10 @@
 import unittest
 
-import numpy
-
 import chainer.functions as F
 from chainer import testing
 
 
-def make_data(shape, dtype):
-    x = numpy.random.uniform(0.1, 5, shape).astype(dtype)
-    gy = numpy.random.uniform(-1, 1, shape).astype(dtype)
-    return x, gy
-
-
-@testing.unary_math_function_test(F.Square(), make_data=make_data)
+@testing.unary_math_function_test(F.Square())
 class TestSquare(unittest.TestCase):
     pass
 
