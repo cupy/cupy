fdf4d48cfaa7b1556d6713320a8548cfd2ca459f
Yuya Unno <unnonouno@gmail.com>
2016-10-09 12:32:30 +0900

    Add one more test case

--- tests/chainer_tests/functions_tests/loss_tests/test_crf1d.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_crf1d.py
@@ -14,6 +14,7 @@ from chainer.testing import attr
 @testing.parameterize(
     {'lengths': [3, 3], 'batches': [2, 2, 2]},
     {'lengths': [3, 2], 'batches': [2, 2, 1]},
+    {'lengths': [3, 1, 1], 'batches': [3, 1, 1]},
     {'lengths': [1, 1], 'batches': [2]},
 )
 class TestCRF1d(unittest.TestCase):
