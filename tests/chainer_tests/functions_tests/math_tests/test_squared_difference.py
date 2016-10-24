b2f2419ec1bed76c34f117acdc0d27717780caac
aonotas <nanigashi03@gmail.com>
2016-10-24 20:56:47 +0900

    Modify test code

--- tests/chainer_tests/functions_tests/math_tests/test_squared_difference.py
+++ tests/chainer_tests/functions_tests/math_tests/test_squared_difference.py
@@ -8,6 +8,7 @@ import numpy
 import chainer
 from chainer import cuda
 from chainer import functions
+from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 
@@ -21,11 +22,14 @@ class TestSquaredDifference(unittest.TestCase):
     def setUp(self):
         self.x1 = numpy.random.uniform(-1, 1, self.in_shape).astype(self.dtype)
         self.x2 = numpy.random.uniform(-1, 1, self.in_shape).astype(self.dtype)
+        self.g1 = numpy.random.uniform(-1, 1, self.in_shape).astype(self.dtype)
+        self.g2 = numpy.random.uniform(-1, 1, self.in_shape).astype(self.dtype)
 
     def check_forward(self, x1_data, x2_data):
         x1 = chainer.Variable(x1_data)
         x2 = chainer.Variable(x2_data)
         y = functions.squared_difference(x1, x2)
+        self.assertEqual(y.data.shape, self.in_shape)
         self.assertEqual(y.data.dtype, self.dtype)
 
     def test_forward_cpu(self):
@@ -35,21 +39,18 @@ class TestSquaredDifference(unittest.TestCase):
     def test_forward_gpu(self):
         self.check_forward(cuda.to_gpu(self.x1), cuda.to_gpu(self.x2))
 
-    def check_backward(self, x1_data, x2_data):
-        x1 = chainer.Variable(x1_data)
-        x2 = chainer.Variable(x2_data)
-        y = functions.squared_difference(x1, x2)
-        y.grad = y.data
-        y.backward()
-        testing.assert_allclose(x1.data, x1.grad, atol=0, rtol=0)
-        testing.assert_allclose(x2.data, x2.grad, atol=0, rtol=0)
+    def check_backward(self, x1, x2, g1, g2):
+        x_data = tuple([x1, x2])
+        g_data = tuple([g1, g2])
+        gradient_check.check_backward(
+            functions.SquaredDifference(), x_data, g_data, dtype=numpy.float64)
 
     def test_backward_cpu(self):
-        self.check_backward(self.x1, self.x2)
+        self.check_backward(self.x1, self.x2, self.g1, self.g2)
 
     @attr.gpu
     def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x1), cuda.to_gpu(self.x2))
+        self.check_backward(cuda.to_gpu(self.x1), cuda.to_gpu(self.x2), cuda.to_gpu(self.g1), cuda.to_gpu(self.g2))
 
 
 testing.run_module(__name__, __file__)
