795076f5bb02fbd73e0c4657c4c343e5062bef14
Ryosuke Okuta <okuta@preferred.jp>
2016-10-25 20:36:59 +0900

    Fix condition

--- cupy/testing/helper.py
+++ cupy/testing/helper.py
@@ -101,17 +101,13 @@ def _make_decorator(check_func, name, type_check, accept_error):
             # nVidia GPUs and Intel CPUs behave differently.
             # To avoid this difference, we need to ignore dimensions whose
             # values are negative.
-            skip_check = False
             if _contains_signed_and_unsigned(kw):
                 inds = _make_positive_indices(self, impl, args, kw)
-                if cupy_result.shape == ():
-                    skip_check = inds[0].size == 0
-                else:
+                if cupy_result.shape != () or inds[0].size == 0:
                     cupy_result = cupy.asnumpy(cupy_result)[inds]
                     numpy_result = cupy.asnumpy(numpy_result)[inds]
 
-            if skip_check:
-                check_func(cupy_result, numpy_result)
+            check_func(cupy_result, numpy_result)
             if type_check:
                 self.assertEqual(cupy_result.dtype, numpy_result.dtype)
         return test_func
