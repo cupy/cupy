8b5219749ba125a357a4ef53dfca17d5c6270e36
setoyama <kotaro.setoyama@gmail.com>
2016-11-10 15:30:53 +0900

    fix constant_values

--- cupy/padding/pad.py
+++ cupy/padding/pad.py
@@ -4,34 +4,31 @@ import cupy
 def pad(array, pad_width, mode=None, **kwargs):
     if mode != 'constant':
         raise NotImplementedError
-
-    if kwargs.keys() == []:
-        fill_value = 0
-    elif kwargs.keys() == ['constant_values']:
-        if not isinstance(kwargs['constant_values'], int):
-            raise NotImplementedError
-        fill_value = kwargs['constant_values']
-    else:
+    constant_value = kwargs.pop('constant_values', None)
+    if len(kwargs) != 0:
         raise NotImplementedError
-
+    if constant_value is None:
+        constant_value = 0
+    if not isinstance(constant_value, int):
+        raise NotImplementedError
     if isinstance(pad_width, int):
         shape = tuple([s + pad_width * 2 for s in array.shape])
-        ret = cupy.full(shape, fill_value, dtype=array.dtype)
+        ret = cupy.full(shape, constant_value, dtype=array.dtype)
         index = tuple([slice(pad_width, pad_width + s) for s in array.shape])
         ret[index] = array
     elif isinstance(pad_width, (list, tuple, cupy.ndarray)):
         if array.ndim == len(pad_width):
             shape = tuple([s + pad_width[i] * 2
                           for i, s in enumerate(array.shape)])
-            ret = cupy.full(shape, fill_value,
+            ret = cupy.full(shape, constant_value,
                             dtype=array.dtype)
             index = tuple([slice(pad_width[i], pad_width[i] + s)
                           for i, s in enumerate(array.shape)])
             ret[index] = array
         elif array.ndim == 1 and len(pad_width) == 2:
-            front = cupy.zeros(pad_width[0])  # fix
-            end = cupy.zeros(pad_width[1])  # fix
-            ret = cupy.hstack((cupy.hstack((front, array)), end))
+            front = cupy.zeros(pad_width[0]) + constant_value
+            end = cupy.zeros(pad_width[1]) + constant_value
+            ret = cupy.hstack((front, array, end))
         else:
             fmt = 'Unable to create correctly shaped tuple from %s'
             raise ValueError(fmt % (shape,))
