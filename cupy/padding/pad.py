ddd4cca73f347def7ade8d80bc22a8e9f2167a05
setoyama <kotaro.setoyama@gmail.com>
2016-11-10 15:49:51 +0900

    fix cupy

--- cupy/padding/pad.py
+++ cupy/padding/pad.py
@@ -1,4 +1,4 @@
-import cupy
+from chainer import cuda
 
 
 def pad(array, pad_width, mode=None, **kwargs):
@@ -13,22 +13,22 @@ def pad(array, pad_width, mode=None, **kwargs):
         raise NotImplementedError
     if isinstance(pad_width, int):
         shape = tuple([s + pad_width * 2 for s in array.shape])
-        ret = cupy.full(shape, constant_value, dtype=array.dtype)
+        ret = cuda.cupy.full(shape, constant_value, dtype=array.dtype)
         index = tuple([slice(pad_width, pad_width + s) for s in array.shape])
         ret[index] = array
-    elif isinstance(pad_width, (list, tuple, cupy.ndarray)):
+    elif isinstance(pad_width, (list, tuple, cuda.cupy.ndarray)):
         if array.ndim == len(pad_width):
             shape = tuple([s + pad_width[i] * 2
                           for i, s in enumerate(array.shape)])
-            ret = cupy.full(shape, constant_value,
+            ret = cuda.cupy.full(shape, constant_value,
                             dtype=array.dtype)
             index = tuple([slice(pad_width[i], pad_width[i] + s)
                           for i, s in enumerate(array.shape)])
             ret[index] = array
         elif array.ndim == 1 and len(pad_width) == 2:
-            front = cupy.zeros(pad_width[0]) + constant_value
-            end = cupy.zeros(pad_width[1]) + constant_value
-            ret = cupy.hstack((front, array, end))
+            front = cuda.cupy.zeros(pad_width[0]) + constant_value
+            end = cuda.cupy.zeros(pad_width[1]) + constant_value
+            ret = cuda.cupy.hstack((front, array, end))
         else:
             fmt = 'Unable to create correctly shaped tuple from %s'
             raise ValueError(fmt % (shape,))
