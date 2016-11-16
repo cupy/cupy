3e2d1de9a8256f4f4a4b801ea919844d5d7234cb
Masayuki Takagi <kamonama@gmail.com>
2016-11-16 14:27:34 +0900

    Addy tests of cupy.ndarray.sort for dtypes.

--- tests/cupy_tests/sorting_tests/test_sort.py
+++ tests/cupy_tests/sorting_tests/test_sort.py
@@ -1,5 +1,8 @@
 import unittest
 
+import numpy
+
+import cupy
 from cupy import testing
 
 
@@ -7,3 +10,34 @@ from cupy import testing
 class TestSort(unittest.TestCase):
 
     _multiprocess_can_split_ = True
+
+    # Test ranks
+
+    # @testing.numpy_cupy_raises()
+    # def test_sort_zero_dim(self, xp):
+    #     a = testing.shaped_random((), xp)
+    #     a.sort()
+
+    # def test_sort_two_or_more_dim(self):
+    #     a = testing.shaped_random((2,3), cupy)
+    #     with self.assertRaises(ValueError):
+    #         a.sort()
+
+    # Test dtypes
+
+    # TODO(takagi): Test 'bqBHILQ' dtypes
+    # TODO(takagi): Test numpy.float16
+    # TODO(takagi): Test numpy.bool_
+    @testing.for_dtypes(['h', 'i', 'l', numpy.float32, numpy.float64,])
+    @testing.numpy_cupy_allclose()
+    def test_sort_dtype(self, xp, dtype):
+        a = testing.shaped_random((10,), xp, dtype)
+        a.sort()
+        return a
+
+    # Test views
+
+    # def test_sort_view(self):
+    #     a = testing.shaped_random((10,), cupy)
+    #     with self.assertRaises(ValueError):
+    #         a.sort()
