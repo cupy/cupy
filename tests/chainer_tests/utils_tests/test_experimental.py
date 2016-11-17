d20efb8d6fe8319094f7984ab9d1d23d2724f56e
Kenta OONO <oono@preferred.jp>
2016-11-17 14:59:56 +0900

    Use assertion methods of UnitTest class

--- tests/chainer_tests/utils_tests/test_experimental.py
+++ tests/chainer_tests/utils_tests/test_experimental.py
@@ -45,42 +45,41 @@ class TestExperimental(unittest.TestCase):
         with warnings.catch_warnings(record=True) as w:
             f()
 
-        assert len(w) == 1
-        assert issubclass(w[0].category, FutureWarning)
-        assert 'test.f is an experimental API.' in str(w[0].message)
+        self.assertEqual(len(w), 1)
+        self.assertIs(w[0].category, FutureWarning)
+        self.assertIn('test.f is an experimental API.', str(w[0].message))
 
     def test_experimental_with_no_api_name(self):
         with warnings.catch_warnings(record=True) as w:
             g()
 
-        assert len(w) == 1
-        assert issubclass(w[0].category, FutureWarning)
-        assert 'g is an experimental API.' in str(w[0].message)
+        self.assertEqual(len(w), 1)
+        self.assertIs(w[0].category, FutureWarning)
+        self.assertIn('g is an experimental API.', str(w[0].message))
 
     def test_experimental_with_no_api_name_2(self):
         with warnings.catch_warnings(record=True) as w:
             C()
 
-        assert len(w) == 1
-        assert issubclass(w[0].category, FutureWarning)
-        assert 'C.__init__ is an experimental API.' in str(w[0].message)
+        self.assertEqual(len(w), 1)
+        self.assertIs(w[0].category, FutureWarning)
+        self.assertIn('C.__init__ is an experimental API.', str(w[0].message))
 
     def test_experimental_static_method(self):
         with warnings.catch_warnings(record=True) as w:
             C.static_method()
 
-        assert len(w) == 1
-        assert issubclass(w[0].category, FutureWarning)
-        assert 'static_method is an experimental API.' in str(w[0].message)
-
+        self.assertEqual(len(w), 1)
+        self.assertIs(w[0].category, FutureWarning)
+        self.assertIn('static_method is an experimental API.', str(w[0].message))
 
     def test_experimental_class_method(self):
         with warnings.catch_warnings(record=True) as w:
             C.class_method()
 
-        assert len(w) == 1
-        assert issubclass(w[0].category, FutureWarning)
-        assert 'C.class_method is an experimental API.' in str(w[0].message)
+        self.assertEqual(len(w), 1)
+        self.assertIs(w[0].category, FutureWarning)
+        self.assertIn('C.class_method is an experimental API.', str(w[0].message))
 
 
 class TestExperimentalDuplicate(unittest.TestCase):
@@ -97,28 +96,28 @@ class TestExperimentalDuplicate(unittest.TestCase):
             f()
             f()
 
-        assert len(w) == 1
+        self.assertEqual(len(w), 1)
 
     def test_different_functions(self):
         with warnings.catch_warnings(record=True) as w:
             f()
             g()
 
-        assert len(w) == 2
+        self.assertEqual(len(w), 2)
 
     def test_multiple_same_class_instantiation(self):
         with warnings.catch_warnings(record=True) as w:
             C()
             C()
 
-        assert len(w) == 1
+        self.assertEqual(len(w), 1)
 
     def test_multiple_calls_with_different_argument(self):
         with warnings.catch_warnings(record=True) as w:
             h(0)
             h(1)
 
-        assert len(w) == 1
+        self.assertEqual(len(w), 1)
 
 
 class TestDisableExperimentalWarning(unittest.TestCase):
@@ -134,7 +133,7 @@ class TestDisableExperimentalWarning(unittest.TestCase):
         with warnings.catch_warnings(record=True) as w:
             f()
 
-        assert len(w) == 0
+        self.assertEqual(len(w), 0)
 
 
 testing.run_module(__name__, __file__)
