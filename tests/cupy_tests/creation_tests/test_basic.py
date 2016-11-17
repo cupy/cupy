3c363f71b975fc6fe45acdafb0ff20bccbab03e8
Akihiro Uchida <uchida@idein.jp>
2016-11-17 16:03:35 +0900

    mark test_empty_huge{,_int} as slow

--- tests/cupy_tests/creation_tests/test_basic.py
+++ tests/cupy_tests/creation_tests/test_basic.py
@@ -19,6 +19,7 @@ class TestBasic(unittest.TestCase):
         a.fill(0)
         return a
 
+    @testing.slow
     @testing.for_dtypes(['?', 'b', 'h', 'e'])
     @testing.numpy_cupy_array_equal()
     def test_empty_huge(self, xp, dtype):
@@ -42,6 +43,7 @@ class TestBasic(unittest.TestCase):
         a.fill(0)
         return a
 
+    @testing.slow
     @testing.for_dtypes(['?', 'b', 'h', 'e'])
     @testing.numpy_cupy_array_equal()
     def test_empty_huge_int(self, xp, dtype):
