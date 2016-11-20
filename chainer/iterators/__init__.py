8a0e9e3e3178fe4ea06036f05549a8906700fe27
Ryosuke Okuta <okuta@preferred.jp>
2016-11-20 17:55:22 +0900

    Remove chainer files

--- chainer/iterators/__init__.py
+++
@@ -1,6 +0,0 @@
-from chainer.iterators import multiprocess_iterator
-from chainer.iterators import serial_iterator
-
-
-MultiprocessIterator = multiprocess_iterator.MultiprocessIterator
-SerialIterator = serial_iterator.SerialIterator
