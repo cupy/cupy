352df4a599dd479b887466b37a3402b6cec7715b
Ryosuke Okuta <okuta@preferred.jp>
2016-11-20 17:55:22 +0900

    Remove chainer files

--- chainer/iterators/__init__.py
+++
@@ -1,7 +0,0 @@
-from chainer.iterators import multiprocess_iterator  # NOQA
-from chainer.iterators import serial_iterator  # NOQA
-
-
-# import class and function
-from chainer.iterators.multiprocess_iterator import MultiprocessIterator  # NOQA
-from chainer.iterators.serial_iterator import SerialIterator  # NOQA
--- chainer/training/trigger.py
+++
@@ -1,8 +0,0 @@
-from chainer.training.triggers import interval
-from chainer.training import util
-
-
-# For backward compatibility
-IntervalTrigger = interval.IntervalTrigger
-get_trigger = util.get_trigger
-_never_fire_trigger = util._never_fire_trigger
