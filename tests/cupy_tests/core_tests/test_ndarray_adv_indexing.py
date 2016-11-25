yuyu2172/chainer

test for advanced indexing with multiple arrays

tests/cupy_tests/core_tests/test_ndarray_adv_indexing.py
@@ -14,6 +14,19 @@ from cupy import testing
     {'shape': (2, 3, 4), 'indexes': ([1, -1], Ellipsis)},
     {'shape': (2, 3, 4),
      'indexes': (slice(None), slice(None), [[1, -1], [0, 3]])},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], [2, 1])},
+    {'shape': (2, 3, 4), 'indexes': (slice(None), [1, 0], [2, 1])},
+    {'shape': (2, 3), 'indexes': ([[0, 1], [1, 0]], [[1, 1], [2, 1]])},
+    # array appears with split
+    {'shape': (2, 3, 4), 'indexes': ([0, 1], slice(None), [1, 0])},
+    {'shape': (2, 3, 4), 'indexes': ([0, 1], slice(None), 1)},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], slice(0, 3, 2), [1, 0])},
+    # three arrays
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], [2, 1], [3, 1])},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], 1, [3, 1])},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], 1, None, [3, 1])},
+    # index broadcasting
+    {'shape': (2, 3, 4), 'indexes': (slice(None), [1, 2], [[1, 0], [0, 1], [-1, 1]])},
 )
 @testing.gpu
 class TestArrayAdvancedIndexingParametrized(unittest.TestCase):
@@ -26,8 +39,24 @@ class TestArrayAdvancedIndexingParametrized(unittest.TestCase):
 
 
 @testing.parameterize(
+    {'shape': (2, 3, 4), 'indexes': (None, [1, 0], [0, 2], slice(None)),
+     'shape': (2, 3, 4), 'indexes': (None, [0, 1], None, [2, 1], slice(None))
+     }
+)
+@testing.gpu
+class TestArrayAdvancedIndexingParametrize(unittest.TestCase):
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal()
+    def test_adv_getitem(self, xp, dtype):
+        a = testing.shaped_arange(self.shape, xp, dtype)
+        return a[self.indexes]
+
+@testing.parameterize(
     {'shape': (2, 3, 4), 'transpose': (1, 2, 0),
-     'indexes': (slice(None), [1, 0])},
+     'indexes': (slice(None), [1, 0]),
+     'shape': (2, 3, 4), 'transpose': (1, 0, 2),
+     'indexes': (None, [1, 2], [0, -1])},
 )
 @testing.gpu
 class TestArrayAdvancedIndexingParametrizedTransp(unittest.TestCase):
@@ -43,7 +72,8 @@ class TestArrayAdvancedIndexingParametrizedTransp(unittest.TestCase):
 
 @testing.parameterize(
     {'shape': (2, 3, 4), 'indexes': (slice(None), cupy.array([1, 0]))},
-    {'shape': (2, 3, 4), 'indexes': (numpy.array([1, 0],))},
+    {'shape': (2, 3, 4), 'indexes': (slice(None), numpy.array([1, 0],))},
+    {'shape': (2, 3, 4), 'indexes': (cupy.array([1, 0]), numpy.array([0, 1]))},
 )
 @testing.gpu
 class TestArrayAdvancedIndexingArrayClass(unittest.TestCase):
@@ -74,16 +104,3 @@ class TestArrayInvalidIndexAdv(unittest.TestCase):
     def test_invalid_adv_getitem(self, xp, dtype):
         a = testing.shaped_arange(self.shape, xp, dtype)
         a[self.indexes]
-
-
-@testing.parameterize(
-    {'shape': (2, 3, 4), 'indexes': ([1, 0], [2, 1])},
-)
-@testing.gpu
-class TestArrayAdvancedIndexingNotSupported(unittest.TestCase):
-
-    @testing.for_all_dtypes()
-    def test_not_supported_adv_indexing(self, dtype):
-        a = testing.shaped_arange(self.shape, cupy, dtype)
-        with self.assertRaises(NotImplementedError):
-            a[self.indexes]

cupy/cupy

tests/cupy_tests/core_tests/test_ndarray_adv_indexing.py
@@ -14,6 +14,19 @@ from cupy import testing
     {'shape': (2, 3, 4), 'indexes': ([1, -1], Ellipsis)},
     {'shape': (2, 3, 4),
      'indexes': (slice(None), slice(None), [[1, -1], [0, 3]])},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], [2, 1])},
+    {'shape': (2, 3, 4), 'indexes': (slice(None), [1, 0], [2, 1])},
+    {'shape': (2, 3), 'indexes': ([[0, 1], [1, 0]], [[1, 1], [2, 1]])},
+    # array appears with split
+    {'shape': (2, 3, 4), 'indexes': ([0, 1], slice(None), [1, 0])},
+    {'shape': (2, 3, 4), 'indexes': ([0, 1], slice(None), 1)},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], slice(0, 3, 2), [1, 0])},
+    # three arrays
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], [2, 1], [3, 1])},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], 1, [3, 1])},
+    {'shape': (2, 3, 4), 'indexes': ([1, 0], 1, None, [3, 1])},
+    # index broadcasting
+    {'shape': (2, 3, 4), 'indexes': (slice(None), [1, 2], [[1, 0], [0, 1], [-1, 1]])},
 )
 @testing.gpu
 class TestArrayAdvancedIndexingParametrized(unittest.TestCase):
@@ -26,8 +39,23 @@ class TestArrayAdvancedIndexingParametrized(unittest.TestCase):
 
 
 @testing.parameterize(
+    {'shape': (2, 3, 4), 'indexes': (None, [1, 0], [0, 2], slice(None))},
+    {'shape': (2, 3, 4), 'indexes': (None, [0, 1], None, [2, 1], slice(None))}
+)
+@testing.gpu
+class TestArrayAdvancedIndexingParametrized(unittest.TestCase):
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal()
+    def test_adv_getitem(self, xp, dtype):
+        a = testing.shaped_arange(self.shape, xp, dtype)
+        return a[self.indexes]
+
+@testing.parameterize(
     {'shape': (2, 3, 4), 'transpose': (1, 2, 0),
      'indexes': (slice(None), [1, 0])},
+    {'shape': (2, 3, 4), 'transpose': (1, 0, 2),
+     'indexes': (None, [1, 2], [0, -1])},
 )
 @testing.gpu
 class TestArrayAdvancedIndexingParametrizedTransp(unittest.TestCase):
@@ -74,16 +102,3 @@ class TestArrayInvalidIndexAdv(unittest.TestCase):
     def test_invalid_adv_getitem(self, xp, dtype):
         a = testing.shaped_arange(self.shape, xp, dtype)
         a[self.indexes]
-
-
-@testing.parameterize(
-    {'shape': (2, 3, 4), 'indexes': ([1, 0], [2, 1])},
-)
-@testing.gpu
-class TestArrayAdvancedIndexingNotSupported(unittest.TestCase):
-
-    @testing.for_all_dtypes()
-    def test_not_supported_adv_indexing(self, dtype):
-        a = testing.shaped_arange(self.shape, cupy, dtype)
-        with self.assertRaises(NotImplementedError):
-            a[self.indexes]