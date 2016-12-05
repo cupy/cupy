73094c4626c4a3de6ad0faccb003cd002d50ead7
Masayuki Takagi <kamonama@gmail.com>
2016-12-05 15:19:53 +0900

    Export module, function and class.

--- chainer/functions/__init__.py
+++ chainer/functions/__init__.py
@@ -99,6 +99,7 @@ from chainer.functions.normalization import l2_normalization  # NOQA
 from chainer.functions.normalization import local_response_normalization  # NOQA
 from chainer.functions.pooling import average_pooling_2d  # NOQA
 from chainer.functions.pooling import max_pooling_2d  # NOQA
+from chainer.functions.pooling import max_pooling_nd  # NOQA
 from chainer.functions.pooling import roi_pooling_2d  # NOQA
 from chainer.functions.pooling import spatial_pyramid_pooling_2d  # NOQA
 from chainer.functions.pooling import unpooling_2d  # NOQA
@@ -328,6 +329,8 @@ from chainer.functions.pooling.average_pooling_2d import average_pooling_2d  # N
 from chainer.functions.pooling.average_pooling_2d import AveragePooling2D  # NOQA
 from chainer.functions.pooling.max_pooling_2d import max_pooling_2d  # NOQA
 from chainer.functions.pooling.max_pooling_2d import MaxPooling2D  # NOQA
+from chainer.functions.pooling.max_pooling_nd import max_pooling_nd  # NOQA
+from chainer.functions.pooling.max_pooling_nd import MaxPoolingND  # NOQA
 from chainer.functions.pooling.roi_pooling_2d import roi_pooling_2d  # NOQA
 from chainer.functions.pooling.roi_pooling_2d import ROIPooling2D  # NOQA
 from chainer.functions.pooling.spatial_pyramid_pooling_2d import spatial_pyramid_pooling_2d  # NOQA
