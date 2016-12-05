fc5a223c07e66e21086e93299d1aeb55e950d5c0
Masayuki Takagi <kamonama@gmail.com>
2016-12-05 23:01:45 +0900

    Remove unnecessary template specialization.

--- cupy/cuda/cupy_thrust.h
+++ cupy/cuda/cupy_thrust.h
@@ -26,17 +26,6 @@ void stable_sort(void *, ssize_t) {
     return;
 }
 
-template void cupy::thrust::stable_sort<cpy_byte>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_ubyte>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_short>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_ushort>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_int>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_uint>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_long>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_ulong>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_float>(void *, ssize_t);
-template void cupy::thrust::stable_sort<cpy_double>(void *, ssize_t);
-
 } // namespace thrust
 
 } // namespace cupy
