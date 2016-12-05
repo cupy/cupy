b817ae32d5d8df1fdd25a6ae7bf5dee580912d61
Shunta Saito <shunta@preferred.jp>
2016-12-05 12:59:30 +0900

    Make the test runnable

--- tests/chainer_tests/functions_tests/pooling_tests/test_upsampling_2d.py
+++ tests/chainer_tests/functions_tests/pooling_tests/test_upsampling_2d.py
@@ -70,3 +70,5 @@ class TestUpsampling2D(unittest.TestCase):
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(
             self.pooled_y.data), cuda.to_gpu(self.gy))
+
+testing.run_module(__name__, __file__)
