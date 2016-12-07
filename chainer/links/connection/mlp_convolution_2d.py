0e6c79f40348423fc5fc03fababeb6559047c6ed
fukatani <nannyakannya@gmail.com>
2016-12-07 22:36:28 +0900

    Fix doc
    Enable to click link

--- chainer/links/connection/mlp_convolution_2d.py
+++ chainer/links/connection/mlp_convolution_2d.py
@@ -37,7 +37,7 @@ class MLPConvolution2D(link.ChainList):
         bias_init: An initializer of bias vectors
             passed to the convolution layers.
 
-    See: `Network in Network <http://arxiv.org/abs/1312.4400v3>`.
+    See: `Network in Network <http://arxiv.org/abs/1312.4400v3>`_.
 
     Attributes:
         activation (function): Activation function.
