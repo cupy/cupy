047acc137da62fd4f6d885f9f28e92650d76627b
setoyama <kotaro.setoyama@gmail.com>
2016-12-13 18:19:38 +0900

    fix undesirable style

--- cupy/padding/pad.py
+++ cupy/padding/pad.py
@@ -1,43 +1,47 @@
+import numpy
+
 import cupy
 import six
 
 
-def _prepend_const(arr, pad_amt, val, axis=-1):
-    if pad_amt == 0:
-        return arr
-    padshape = tuple(x if i != axis else pad_amt
-                     for (i, x) in enumerate(arr.shape))
-    if val == 0:
-        return cupy.concatenate((cupy.zeros(padshape, dtype=arr.dtype), arr),
-                                axis=axis)
+def _prepend_const(narray, pad_amount, value, axis=-1):
+    if pad_amount == 0:
+        return narray
+    padshape = tuple(x if i != axis else pad_amount
+                     for i, x in enumerate(narray.shape))
+    if value == 0:
+        return cupy.concatenate((cupy.zeros(padshape, dtype=narray.dtype),
+                                 narray), axis=axis)
     else:
-        return cupy.concatenate(((cupy.zeros(padshape) + val).astype(
-                                 arr.dtype), arr), axis=axis)
+        return cupy.concatenate(((cupy.zeros(padshape) + value).astype(
+                                 narray.dtype), narray), axis=axis)
 
 
-def _append_const(arr, pad_amt, val, axis=-1):
-    if pad_amt == 0:
-        return arr
-    padshape = tuple(x if i != axis else pad_amt
-                     for (i, x) in enumerate(arr.shape))
-    if val == 0:
-        return cupy.concatenate((arr, cupy.zeros(padshape, dtype=arr.dtype)),
+def _append_const(narray, pad_amount, value, axis=-1):
+    if pad_amount == 0:
+        return narray
+    padshape = tuple(x if i != axis else pad_amount
+                     for i, x in enumerate(narray.shape))
+    if value == 0:
+        return cupy.concatenate((narray,
+                                cupy.zeros(padshape, dtype=narray.dtype)),
                                 axis=axis)
     else:
         return cupy.concatenate(
-            (arr, (cupy.zeros(padshape) + val).astype(arr.dtype)), axis=axis)
+            (narray, (cupy.zeros(padshape) + value).astype(narray.dtype)),
+            axis=axis)
 
 
 def _normalize_shape(ndarray, shape, cast_to_int=True):
     ndims = ndarray.ndim
     if shape is None:
         return ((None, None), ) * ndims
-    arr = cupy.asarray(shape)
+    arr = numpy.asarray(shape)
     if arr.ndim <= 1:
         if arr.shape == () or arr.shape == (1,):
-            arr = cupy.ones((ndims, 2), dtype=ndarray.dtype) * arr
+            arr = numpy.ones((ndims, 2), dtype=ndarray.dtype) * arr
         elif arr.shape == (2,):
-            arr = arr[cupy.newaxis, :].repeat(ndims, axis=0)
+            arr = arr[numpy.newaxis, :].repeat(ndims, axis=0)
         else:
             fmt = "Unable to create correctly shaped tuple from %s"
             raise ValueError(fmt % (shape,))
@@ -52,47 +56,49 @@ def _normalize_shape(ndarray, shape, cast_to_int=True):
     else:
         fmt = "Unable to create correctly shaped tuple from %s"
         raise ValueError(fmt % (shape,))
-    if cast_to_int is True:
-        arr = cupy.rint(arr).astype(int)
+    if cast_to_int:
+        arr = numpy.rint(arr).astype(int)
     return tuple(tuple(axis) for axis in arr.tolist())
 
 
 def _validate_lengths(narray, number_elements):
-    normshp = _normalize_shape(narray, number_elements)
-    for i in normshp:
-        chk = [1 if x is None else x for x in i]
-        chk = [1 if x >= 0 else -1 for x in chk]
-        if (chk[0] < 0) or (chk[1] < 0):
-            fmt = "%s cannot contain negative values."
-            raise ValueError(fmt % (number_elements,))
-    return normshp
+    shape = _normalize_shape(narray, number_elements)
+    for axis_shape in shape:
+        axis_shape = [1 if x is None else x for x in axis_shape]
+        axis_shape = [1 if x >= 0 else -1 for x in axis_shape]
+        if axis_shape[0] < 0 or axis_shape[1] < 0:
+            message = '%s cannot contain negative values.' % number_elements
+            raise ValueError(message)
+    return shape
 
 
-def pad(array, pad_width, mode, **kwargs):
+def pad(array, pad_width, mode, **keywords):
     if not cupy.asarray(pad_width).dtype.kind == 'i':
         raise TypeError('pad_width must be of integral type.')
     narray = cupy.array(array)
     pad_width = _validate_lengths(narray, pad_width)
-    allowedkwargs = {
+    allowed_keywords = {
         'constant': ['constant_values'],
     }
-    kwdefaults = {
+    keyword_defaults = {
         'constant_values': 0,
     }
     if mode != 'constant':
         raise NotImplementedError
-    for key in kwargs:
-        if key not in allowedkwargs[mode]:
+    for key in keywords:
+        if key not in allowed_keywords[mode]:
             raise ValueError('%s keyword not in allowed keywords %s' %
-                             (key, allowedkwargs[mode]))
-    for kw in allowedkwargs[mode]:
-        kwargs.setdefault(kw, kwdefaults[kw])
-    for i in kwargs:
-        if i in ['constant_values']:
-            kwargs[i] = _normalize_shape(narray, kwargs[i], cast_to_int=False)
-    newmat = narray.copy()
-    for axis, ((pad_before, pad_after), (before_val, after_val)) \
-            in enumerate(six.moves.zip(pad_width, kwargs['constant_values'])):
-        newmat = _prepend_const(newmat, pad_before, before_val, axis)
-        newmat = _append_const(newmat, pad_after, after_val, axis)
-    return newmat
+                             (key, allowed_keywords[mode]))
+    for allowed_keyword in allowed_keywords[mode]:
+        keywords.setdefault(allowed_keyword, keyword_defaults[allowed_keyword])
+    for key in keywords:
+        if key in ['constant_values']:
+            keywords[key] = _normalize_shape(narray, keywords[key],
+                                             cast_to_int=False)
+    newmatrix = narray.copy()
+    for axis, ((pad_before, pad_after), (before_value, after_value)) \
+            in enumerate(six.moves.zip(pad_width,
+                                       keywords['constant_values'])):
+        newmatrix = _prepend_const(newmatrix, pad_before, before_value, axis)
+        newmatrix = _append_const(newmatrix, pad_after, after_value, axis)
+    return newmatrix
