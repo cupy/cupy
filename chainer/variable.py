67a3a2b77c8a24a101d0302ddb7c1d5e13177b13
Yuya Unno <unnonouno@gmail.com>
2016-12-16 21:12:01 +0900

    Fix backward accumulation for zero-dim array

--- chainer/variable.py
+++ chainer/variable.py
@@ -9,6 +9,7 @@ import six
 import chainer
 from chainer import cuda
 from chainer import flag
+from chainer import utils
 
 
 def _check_grad_type(func, x, gx):
@@ -416,7 +417,7 @@ Actual: {0}'''.format(type(data))
                             x.grad = gx
                             need_copy.add(id_x)
                         elif id_x in need_copy:
-                            x.grad = x.grad + gx  # copy
+                            x.grad = utils.force_array(x.grad + gx)  # copy
                             need_copy.remove(id_x)
                         else:
                             x._grad += gx
@@ -427,7 +428,7 @@ Actual: {0}'''.format(type(data))
                             seen_vars.add(id_x)
                             need_copy.add(id_x)
                         elif id_x in need_copy:  # 2nd visit
-                            x._grad = gx + x._grad  # copied
+                            x._grad = utils.force_array(gx + x._grad)  # copied
                             need_copy.remove(id_x)
                         else:  # 3rd or later visit
                             x._grad += gx
