6302691f658cb8e742ca26b9a6f75a8a432d9276
Akihiro Uchida <uchida@idein.jp>
2017-01-26 19:54:31 +0900

    add tests pass in cupy.cuda.nvtx.range{,C} blocks

--- tests/cupy_tests/cuda_tests/test_nvtx.py
+++ tests/cupy_tests/cuda_tests/test_nvtx.py
@@ -22,3 +22,13 @@ class TestNVTX(unittest.TestCase):
     def test_RangePushC(self):
         nvtx.RangePushC("test:RangePushC", 0xFF000000)
         nvtx.RangePop()
+
+    @attr.gpu
+    def test_range(self):
+        with nvtx.range('test:range'):
+            pass
+
+    @attr.gpu
+    def test_rangeC(self):
+        with nvtx.rangeC('test:rangeC'):
+            pass
