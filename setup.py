877f87ac7ed107180411fa61eeff25e42c76b0e0
Ryosuke Okuta <okuta@preferred.jp>
2017-02-09 11:55:03 +0900

    Fix version

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ ext_modules = cupy_setup_build.get_ext_modules()
 
 setup(
     name='cupy',
-    version='1.0.0',
-    description=('CuPy: NumPy-like API accelerated with CUDA'),
+    version='0.0.1',
+    description='CuPy: NumPy-like API accelerated with CUDA',
     author='Seiya Tokui',
     author_email='tokui@preferred.jp',
     url='http://cupy-ndarray.org/',
