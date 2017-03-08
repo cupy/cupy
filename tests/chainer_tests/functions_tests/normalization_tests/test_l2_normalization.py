410c3d077e31b2ed8c90b014957e18e5872b181b
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-08 15:58:13 +0900

    add axis option to l2_normalization

--- tests/chainer_tests/functions_tests/normalization_tests/test_l2_normalization.py
+++ tests/chainer_tests/functions_tests/normalization_tests/test_l2_normalization.py
@@ -1,5 +1,6 @@
 import unittest
 
+import itertools
 import numpy
 import six
 
@@ -10,11 +11,14 @@ from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 from chainer.testing import condition
-from chainer.utils import type_check
 
 
 @testing.parameterize(
-    {'shape': (4, 15)},
+    {'shape': (4, 15), 'axis': 1},
+    {'shape': (4, 3, 2, 5), 'axis': 0},
+    {'shape': (4, 3, 2, 5), 'axis': 1},
+    {'shape': (4, 3, 2, 5), 'axis': 2},
+    {'shape': (4, 3, 2, 5), 'axis': 3},
 )
 class TestL2Normalization(unittest.TestCase):
 
@@ -22,45 +26,54 @@ class TestL2Normalization(unittest.TestCase):
         self.x = numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
         self.gy = numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
 
-    def check_forward(self, x_data):
+    def check_forward(self, x_data, axis):
         x = chainer.Variable(x_data)
 
-        y = functions.normalize(x)
+        y = functions.normalize(x, axis=axis)
         self.assertEqual(y.data.dtype, numpy.float32)
         y_data = cuda.to_cpu(y.data)
 
         y_expect = numpy.empty_like(self.x)
-        for n in six.moves.range(len(self.x)):
-            y_expect[n] = self.x[n] / numpy.linalg.norm(self.x[n])
-
+        shape = self.x.shape
+        indices = []
+        for i in six.moves.range(len(shape)):
+            if i != axis:
+                indices.append(six.moves.range(shape[i]))
+            else:
+                indices.append([slice(None)])
+        indices_tuple = list(itertools.product(*indices))
+        for index in indices_tuple:
+            y_expect[index] = self.x[index] / numpy.linalg.norm(self.x[index])
         testing.assert_allclose(y_expect, y_data)
 
     @condition.retry(3)
     def test_forward_cpu(self):
-        self.check_forward(self.x)
+        self.check_forward(self.x, self.axis)
 
     @attr.gpu
     @condition.retry(3)
     def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x))
+        self.check_forward(cuda.to_gpu(self.x), self.axis)
 
-    def check_backward(self, x_data, y_grad):
+    def check_backward(self, x_data, axis, y_grad):
         gradient_check.check_backward(
-            functions.NormalizeL2(), x_data, y_grad, rtol=1e-3, atol=1e-4)
+            functions.NormalizeL2(axis=axis), x_data, y_grad,
+            rtol=1e-3, atol=1e-4)
 
     @condition.retry(3)
     def test_backward_cpu(self):
-        self.check_backward(self.x, self.gy)
+        self.check_backward(self.x, self.axis, self.gy)
 
     @attr.gpu
     @condition.retry(3)
     def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+        self.check_backward(
+            cuda.to_gpu(self.x), self.axis, cuda.to_gpu(self.gy))
 
     def check_eps(self, x_data):
         x = chainer.Variable(x_data)
 
-        y = functions.normalize(x)
+        y = functions.normalize(x, axis=self.axis)
         self.assertEqual(y.data.dtype, numpy.float32)
         y_data = cuda.to_cpu(y.data)
 
@@ -73,15 +86,6 @@ class TestL2Normalization(unittest.TestCase):
     @attr.gpu
     def test_eps_gpu(self):
         self.check_eps(cuda.to_gpu(numpy.zeros_like(self.x)))
-
-
-class TestL2NormalizationTypeError(unittest.TestCase):
-
-    def test_invalid_shape(self):
-        x = chainer.Variable(numpy.zeros((4, 3, 24), dtype=numpy.float32))
-
-        with self.assertRaises(type_check.InvalidType):
-            chainer.functions.normalize(x)
 
 
 testing.run_module(__name__, __file__)
