5521a7e87cc1ad42d3fb3e693c58a8af20e4f0f1
Masaki Saito <msaito@preferred.jp>
2017-03-12 19:51:33 +0900

    Flake8

--- cupy/linalg/decomposition.py
+++ cupy/linalg/decomposition.py
@@ -77,7 +77,8 @@ def cholesky(a):
 def _assert_cupy_array(*arrays):
     for a in arrays:
         if not isinstance(a, cupy.core.ndarray):
-            raise linalg.LinAlgError('cupy.linalg only supports cupy.core.ndarray')
+            raise linalg.LinAlgError(
+                'cupy.linalg only supports cupy.core.ndarray')
 
 
 def _assert_rank2(*arrays):
@@ -91,7 +92,8 @@ def _assert_rank2(*arrays):
 def _assert_nd_squareness(*arrays):
     for a in arrays:
         if max(a.shape[-2:]) != min(a.shape[-2:]):
-            raise linalg.LinAlgError('Last 2 dimensions of the array must be square')
+            raise linalg.LinAlgError(
+                'Last 2 dimensions of the array must be square')
 
 
 def _tril(x, k=0):
