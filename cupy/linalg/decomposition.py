ebfbd4fa0b44fae293b7fe44a24493977bae4fb2
Masaki Saito <msaito@preferred.jp>
2017-03-16 15:52:56 +0900

    Add cupy.linalg.qr

--- cupy/linalg/decomposition.py
+++ cupy/linalg/decomposition.py
@@ -67,7 +67,108 @@ def cholesky(a):
     return x
 
 
-# TODO(okuta): Implement qr
+def qr(a, mode='reduced'):
+    '''QR decomposition.
+
+    Decompose a given two-dimensional matrix into ``Q * R``, where ``Q``
+    is an orthonormal and ``R`` is an upper-triangular matrix.
+
+    Args:
+        a (cupy.ndarray): The input matrix.
+        mode (str): The mode of decomposition. Currently 'reduced',
+            'complete', 'r', and 'raw' modes are supported. The default mode
+            is 'reduced', and decompose a matrix ``A = (M, N)`` into ``Q``,
+            ``R`` with dimensions ``(M, K)``, ``(K, N)``, where
+            ``K = min(M, N)``.
+
+    .. seealso:: :func:`numpy.linalg.qr`
+    '''
+    if not cusolver_enabled:
+        raise RuntimeError('Current cupy only supports cusolver in CUDA 8.0')
+
+    # TODO(Saito): Current implementation only accepts two-dimensional arrays
+    _assert_cupy_array(a)
+    _assert_rank2(a)
+
+    if mode not in ('reduced', 'complete', 'r', 'raw'):
+        if mode in ('f', 'full', 'e', 'economic'):
+            msg = 'The deprecated mode \'{}\' is not supported'.format(mode)
+            raise ValueError(msg)
+        else:
+            raise ValueError('Unrecognized mode \'{}\''.format(mode))
+
+    ret_dtype = a.dtype.char
+    # Cast to float32 or float64
+    if ret_dtype == 'f' or ret_dtype == 'd':
+        dtype = ret_dtype
+    else:
+        dtype = numpy.find_common_type((ret_dtype, 'f'), ()).char
+
+    m, n = a.shape
+    x = a.transpose().astype(dtype, copy=True)
+    mn = min(m, n)
+    handle = device.get_cusolver_handle()
+    devInfo = cupy.empty(1, dtype=numpy.int32)
+    # compute working space of geqrf and ormqr, and solve R
+    if x.dtype.char == 'f':
+        buffersize = cusolver.sgeqrf_bufferSize(handle, m, n, x.data.ptr, n)
+        workspace = cupy.empty(buffersize, dtype=numpy.float32)
+        tau = cupy.empty(mn, dtype=numpy.float32)
+        cusolver.sgeqrf(
+            handle, m, n, x.data.ptr, m,
+            tau.data.ptr, workspace.data.ptr, buffersize, devInfo.data.ptr)
+    else:  # a.dtype.char == 'd'
+        buffersize = cusolver.dgeqrf_bufferSize(handle, n, m, x.data.ptr, n)
+        workspace = cupy.empty(buffersize, dtype=numpy.float64)
+        tau = cupy.empty(mn, dtype=numpy.float64)
+        cusolver.dgeqrf(
+            handle, m, n, x.data.ptr, m,
+            tau.data.ptr, workspace.data.ptr, buffersize, devInfo.data.ptr)
+    status = int(devInfo[0])
+    if status < 0:
+        raise linalg.LinAlgError(
+            'Parameter error (maybe caused by a bug in cupy.linalg?)')
+
+    if mode == 'r':
+        r = x[:, :mn].transpose().astype(dtype, copy=True)
+        return _triu(r)
+
+    if mode == 'raw':
+        if ret_dtype == 'f':
+            # The original numpy.linalg.qr returns float64 in raw mode,
+            # whereas the cusolver returns float32. We agree that the
+            # following code would be inappropriate, however, in this time
+            # we explicitly convert them to float64 for compatibility.
+            return x.astype(numpy.float64), tau.astype(numpy.float64)
+        return x, tau
+
+    if mode == 'complete' and m > n:
+        mc = m
+        q = cupy.empty((m, m), dtype)
+    else:
+        mc = mn
+        q = cupy.empty((n, m), dtype)
+    q[:n] = x
+
+    # solve Q
+    if x.dtype.char == 'f':
+        buffersize = cusolver.sorgqr_bufferSize(
+            handle, m, mc, mn, q.data.ptr, m, tau.data.ptr)
+        workspace = cupy.empty(buffersize, dtype=numpy.float32)
+        cusolver.sorgqr(
+            handle, m, mc, mn, q.data.ptr, m, tau.data.ptr,
+            workspace.data.ptr, buffersize, devInfo.data.ptr)
+    else:
+        buffersize = cusolver.dorgqr_bufferSize(
+            handle, m, mc, mn, q.data.ptr, m, tau.data.ptr)
+        workspace = cupy.empty(buffersize, dtype=numpy.float64)
+        cusolver.dorgqr(
+            handle, m, mc, mn, q.data.ptr, m, tau.data.ptr,
+            workspace.data.ptr, buffersize, devInfo.data.ptr)
+
+    q = q[:mc].transpose().astype(dtype, copy=True)
+    r = x[:, :mc].transpose().astype(dtype, copy=True)
+    return q, _triu(r)
 
 
 # TODO(okuta): Implement svd
@@ -100,5 +201,14 @@ def _tril(x, k=0):
     u = cupy.arange(m).reshape(m, 1)
     v = cupy.arange(n).reshape(1, n)
     mask = v - u <= k
+    x *= mask
+    return x
+
+
+def _triu(x, k=0):
+    m, n = x.shape
+    u = cupy.arange(m).reshape(m, 1)
+    v = cupy.arange(n).reshape(1, n)
+    mask = v - u >= k
     x *= mask
     return x
--- tests/cupy_tests/linalg_tests/test_decomposition.py
+++ tests/cupy_tests/linalg_tests/test_decomposition.py
@@ -28,3 +28,50 @@ class TestCholeskyDecomposition(unittest.TestCase):
         self.check_L(A)
         # np.linalg.cholesky only uses a lower triangle of an array
         self.check_L(numpy.array([[1, 2], [1, 9]]))
+
+
+@unittest.skipUnless(
+    cusolver_enabled, 'Only cusolver in CUDA 8.0 is supported')
+@testing.gpu
+class TestQRDecomposition(unittest.TestCase):
+
+    _multiprocess_can_split_ = True
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(atol=1e-5)
+    def check_mode(self, array, xp, dtype, mode, index=None):
+        a = xp.asarray(array, dtype=dtype)
+        result = xp.linalg.qr(a, mode=mode)
+        if type(result) == tuple:
+            return result[index]
+        else:
+            return result
+
+    def test_r_mode(self):
+        self.check_mode(numpy.random.randn(2, 3), mode='r')
+        self.check_mode(numpy.random.randn(3, 3), mode='r')
+        self.check_mode(numpy.random.randn(4, 2), mode='r')
+
+    def test_raw_mode(self):
+        self.check_mode(numpy.random.randn(2, 4), mode='raw', index=0)
+        self.check_mode(numpy.random.randn(1, 5), mode='raw', index=1)
+        self.check_mode(numpy.random.randn(2, 3), mode='raw', index=0)
+        self.check_mode(numpy.random.randn(4, 3), mode='raw', index=1)
+        self.check_mode(numpy.random.randn(4, 5), mode='raw', index=0)
+        self.check_mode(numpy.random.randn(3, 3), mode='raw', index=1)
+
+    def test_complete_mode(self):
+        self.check_mode(numpy.random.randn(2, 4), mode='complete', index=0)
+        self.check_mode(numpy.random.randn(1, 5), mode='complete', index=1)
+        self.check_mode(numpy.random.randn(2, 3), mode='complete', index=0)
+        self.check_mode(numpy.random.randn(4, 3), mode='complete', index=1)
+        self.check_mode(numpy.random.randn(4, 5), mode='complete', index=0)
+        self.check_mode(numpy.random.randn(3, 3), mode='complete', index=1)
+
+    def test_reduced_mode(self):
+        self.check_mode(numpy.random.randn(2, 4), mode='reduced', index=0)
+        self.check_mode(numpy.random.randn(1, 5), mode='reduced', index=1)
+        self.check_mode(numpy.random.randn(2, 3), mode='reduced', index=0)
+        self.check_mode(numpy.random.randn(4, 3), mode='reduced', index=1)
+        self.check_mode(numpy.random.randn(4, 5), mode='reduced', index=0)
+        self.check_mode(numpy.random.randn(3, 3), mode='reduced', index=1)
