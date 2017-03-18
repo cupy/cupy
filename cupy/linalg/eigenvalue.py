185c2f1cb2104be888babba292a3b79858fdb33f
Yuya Unno <unnonouno@gmail.com>
2017-03-18 19:29:55 +0900

    Implement eigh and make test

--- cupy/linalg/eigenvalue.py
+++ cupy/linalg/eigenvalue.py
@@ -1,13 +1,72 @@
-# flake8: NOQA
-# "flake8: NOQA" to suppress warning "H104  File contains nothing but comments"
+import numpy
+
+import cupy
+from cupy.cuda import cublas
+from cupy.cuda import cusolver
+from cupy.cuda import device
+
+
+def _syevd(a, UPLO, with_eigen_vector):
+    if UPLO not in ('L', 'U'):
+        raise ValueError("UPLO argument must be 'L' or 'U'")
+
+    # Note that cuSolver uses tranpose of a matrix.
+    if not issubclass(a.dtype.type, numpy.inexact):
+        # NumPy uses float64 when an input is not floating point number.
+        v = a.T.astype('d', copy=True)
+        ret_type = numpy.float64
+    elif a.dtype == 'e':
+        # cuSolver does not have API for float16.
+        v = a.T.astype('f', copy=True)
+        ret_type = numpy.float16
+    else:
+        v = a.T.copy()
+        ret_type = a.dtype
+
+    dtype = v.dtype
+    m, lda = a.shape
+    W = cupy.empty(m, dtype)
+    dev_info = cupy.empty((), 'i')
+    handle = device.Device().cusolver_handle
+
+    if with_eigen_vector:
+        jobz = cusolver.CUSOLVER_EIG_MODE_VECTOR
+    else:
+        jobz = cusolver.CUSOLVER_EIG_MODE_NOVECTOR
+
+    if UPLO == 'L':
+        uplo = cublas.CUBLAS_FILL_MODE_LOWER
+    else:  # UPLO == 'U'
+        uplo = cublas.CUBLAS_FILL_MODE_UPPER
+
+    if dtype == 'f':
+        buffer_size = cupy.cuda.cusolver.ssyevd_bufferSize
+        syevd = cupy.cuda.cusolver.ssyevd
+    elif dtype == 'd':
+        buffer_size = cupy.cuda.cusolver.dsyevd_bufferSize
+        syevd = cupy.cuda.cusolver.dsyevd
+    else:
+        raise RuntimeError('Only float and double are supported')
+
+    work_size = buffer_size(
+        handle, jobz, uplo, m, v.data.ptr, lda, W.data.ptr)
+    work = cupy.empty(work_size, dtype)
+    syevd(
+        handle, jobz, uplo, m, v.data.ptr, lda,
+        W.data.ptr, work.data.ptr, work_size, dev_info.data.ptr)
+
+    return W.astype(ret_type, copy=False), v.T.astype(ret_type, copy=False)
+
 
 # TODO(okuta): Implement eig
 
 
-# TODO(okuta): Implement eigh
+def eigh(a, UPLO='L'):
+    return _syevd(a, UPLO, True)
 
 
 # TODO(okuta): Implement eigvals
 
 
-# TODO(okuta): Implement eigvalsh
+def eigvals(a, UPLO='L'):
+    return _syevd(a, UPLO, False)[0]
--- tests/cupy_tests/linalg_tests/test_eigenvalue.py
+++ tests/cupy_tests/linalg_tests/test_eigenvalue.py
@@ -1,5 +1,8 @@
 import unittest
 
+import numpy
+
+import cupy
 from cupy import testing
 
 
@@ -7,3 +10,48 @@ from cupy import testing
 class TestEigenvalue(unittest.TestCase):
 
     _multiprocess_can_split_ = True
+
+    @testing.for_all_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_eigh(self, xp, dtype):
+        a = xp.array([[1, 0, 3], [0, 5, 0], [3, 0, 5]], dtype)
+        w, v = xp.linalg.eigh(a)
+
+        # Order of eigen values is not defined.
+        # They must be sorted to compare them.
+        if xp is numpy:
+            inds = numpy.argsort(w)
+        else:
+            inds = cupy.array(numpy.argsort(w.get()))
+        w = w[inds]
+        v = v[inds]
+        return xp.concatenate([w[None], v])
+
+    def test_eigh_float16(self):
+        # NumPy's eigh deos not support float16
+        a = cupy.array([[1, 0, 3], [0, 5, 0], [3, 0, 5]], 'e')
+        w, v = cupy.linalg.eigh(a)
+
+        self.assertEqual(w.dtype, numpy.float16)
+        self.assertEqual(v.dtype, numpy.float16)
+
+        a = a.astype('d')
+        w = w.astype('d')
+        v = v.astype('d')
+
+        testing.assert_allclose(a.dot(v), w * v, rtol=1e-3, atol=1e-4)
+
+    @testing.for_all_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_eigvals(self, xp, dtype):
+        a = xp.array([[1, 0, 3], [0, 5, 0], [3, 0, 5]], dtype)
+        w = xp.linalg.eigvals(a)
+
+        # Order of eigen values is not defined.
+        # They must be sorted to compare them.
+        if xp is numpy:
+            inds = numpy.argsort(w)
+        else:
+            inds = cupy.array(numpy.argsort(w.get()))
+        w = w[inds]
+        return w
