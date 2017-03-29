d1f70124b6398144db2c709cc641011e59e22570
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2017-03-30 02:36:37 +0900

    Update __init__.py

--- chainer/training/extensions/__init__.py
+++ chainer/training/extensions/__init__.py
@@ -19,8 +19,8 @@ from chainer.training.extensions.evaluator import Evaluator  # NOQA
 from chainer.training.extensions.exponential_shift import ExponentialShift  # NOQA
 from chainer.training.extensions.linear_shift import LinearShift  # NOQA
 from chainer.training.extensions.log_report import LogReport  # NOQA
-from chainer.training.extensions.parameter_statistics import ParameterStatistics  # NOQA
 from chainer.training.extensions.micro_average import MicroAverage  # NOQA
+from chainer.training.extensions.parameter_statistics import ParameterStatistics  # NOQA
 from chainer.training.extensions.plot_report import PlotReport  # NOQA
 from chainer.training.extensions.print_report import PrintReport  # NOQA
 from chainer.training.extensions.progress_bar import ProgressBar  # NOQA
