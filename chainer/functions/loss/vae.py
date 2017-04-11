fcb98f4ba6631d7c42651bcfc4ed57bf489c5af1
Kenta OONO <oono@preferred.jp>
2017-04-11 14:08:25 +0900

    Add reduce option to GaussianNLL

--- chainer/functions/loss/vae.py
+++ chainer/functions/loss/vae.py
@@ -74,12 +74,13 @@ def bernoulli_nll(x, y):
     return sum.sum(softplus.softplus(y)) - sum.sum(x * y)
 
 
-def gaussian_nll(x, mean, ln_var):
+def gaussian_nll(x, mean, ln_var, reduce='sum'):
     """Computes the negative log-likelihood of a Gaussian distribution.
 
     Given two variable ``mean`` representing :math:`\\mu` and ``ln_var``
-    representing :math:`\\log(\\sigma^2)`, this function returns the negative
-    log-likelihood of :math:`x` on a Gaussian distribution :math:`N(\\mu, S)`,
+    representing :math:`\\log(\\sigma^2)`, this function computes in
+    elementwise manner the negative log-likelihood of :math:`x` on a
+    Gaussianx distribution :math:`N(\\mu, S)`,
 
     .. math::
 
@@ -90,23 +91,40 @@ def gaussian_nll(x, mean, ln_var):
     where :math:`D` is a dimension of :math:`x` and :math:`S` is a diagonal
     matrix where :math:`S_{ii} = \\sigma_i^2`.
 
+    The output is a varialbe whose value depends on the value of
+    the option `reduce`. If it is `'no'`, it holds the elementwise
+    loss values. If it is `'sum'`, loss values are summed up.
+
     Args:
         x (~chainer.Variable): Input variable.
         mean (~chainer.Variable): A variable representing mean of a Gaussian
             distribution, :math:`\\mu`.
         ln_var (~chainer.Variable): A variable representing logarithm of
             variance of a Gaussian distribution, :math:`\\log(\\sigma^2)`.
+        recude (str): Reduction option. Its value must be either
+            `'sum'` or `'no'`. Otherwise, `ValueError` is raised.
 
     Returns:
         ~chainer.Variable: A variable representing the negative log-likelihood.
+            If `reduce` is `'no'`, the output varialbe holds array
+            whose shape is same as one of (hence both of) input variables.
+            If it is `'sum'`, the output variable holds a scalar value.
 
     """
     assert isinstance(x, variable.Variable)
     assert isinstance(mean, variable.Variable)
     assert isinstance(ln_var, variable.Variable)
 
-    D = x.size
+    if reduce not in ('sum', 'no'):
+        raise ValueError(
+            "only 'sum' and 'no' are valid for 'reduce', but '%s' is "
+            'given' % reduce)
+
     x_prec = exponential.exp(-ln_var)
     x_diff = x - mean
     x_power = (x_diff * x_diff) * x_prec * -0.5
-    return (sum.sum(ln_var) + D * math.log(2 * math.pi)) / 2 - sum.sum(x_power)
+    loss = (ln_var + math.log(2 * math.pi)) / 2 - x_power
+    if reduce == 'sum':
+        return sum.sum(loss)
+    else:
+        return loss
--- tests/chainer_tests/functions_tests/loss_tests/test_vae.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_vae.py
@@ -69,6 +69,10 @@ class TestBernoulliNLL(unittest.TestCase):
                                  cuda.to_gpu(self.y))
 
 
+@testing.parameterize(
+    {'reduce': 'no'},
+    {'reduce': 'sum'}
+)
 class TestGaussianNLL(unittest.TestCase):
 
     def setUp(self):
@@ -82,15 +86,17 @@ class TestGaussianNLL(unittest.TestCase):
         x_d = self.x - self.mean
         var = numpy.exp(self.ln_var)
 
-        self.expect = (0.5 * D * numpy.log(2 * numpy.pi) +
-                       0.5 * numpy.sum(self.ln_var) +
-                       numpy.sum(x_d * x_d / var) * 0.5)
+        self.expect = (0.5 * numpy.log(2 * numpy.pi) +
+                       0.5 * self.ln_var +
+                       x_d * x_d / var * 0.5)
+        if self.reduce == 'sum':
+            self.expect = numpy.sum(self.expect)
 
     def check_gaussian_nll(self, x_data, mean_data, ln_var_data):
         x = chainer.Variable(x_data)
         mean = chainer.Variable(mean_data)
         ln_var = chainer.Variable(ln_var_data)
-        actual = cuda.to_cpu(vae.gaussian_nll(x, mean, ln_var).data)
+        actual = cuda.to_cpu(vae.gaussian_nll(x, mean, ln_var, self.reduce).data)
         testing.assert_allclose(self.expect, actual)
 
     @condition.retry(3)
@@ -103,6 +109,29 @@ class TestGaussianNLL(unittest.TestCase):
         self.check_gaussian_nll(cuda.to_gpu(self.x),
                                 cuda.to_gpu(self.mean),
                                 cuda.to_gpu(self.ln_var))
+
+
+class TestGaussianNLLInvalidReductionOption(unittest.TestCase):
+
+    def setUp(self):
+        self.x = numpy.random.uniform(-1, 1, (3,)).astype(numpy.float32)
+        self.mean = numpy.random.uniform(-1, 1, (3,)).astype(numpy.float32)
+        self.ln_var = numpy.random.uniform(-1, 1, (3,)).astype(numpy.float32)
+
+    def check_invalid_option(self, xp):
+        x = chainer.Variable(xp.asarray(self.x))
+        mean = chainer.Variable(xp.asarray(self.mean))
+        ln_var = chainer.Variable(xp.asarray(self.ln_var))
+
+        with self.assertRaises(ValueError):
+            vae.gaussian_nll(x, mean, ln_var, 'invalid_option')
+
+    def test_invalid_option_cpu(self):
+        self.check_invalid_option(numpy)
+
+    @attr.gpu
+    def test_invalid_option_gpu(self):
+        self.check_invalid_option(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)
