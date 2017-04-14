6ebe31c4a694a927ec635df60616ad07480a0ecb
Kenta OONO <oono@preferred.jp>
2017-04-14 19:24:17 +0900

    Add reduce option to huber loss

--- tests/chainer_tests/functions_tests/loss_tests/test_huber_loss.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_huber_loss.py
@@ -11,6 +11,10 @@ from chainer.testing import attr
 from chainer.testing import condition
 
 
+@testing.parameterize(
+    {'reduce': 'no'},
+    {'reduce': 'sum_along_second_axis'}
+)
 class TestHuberLoss(unittest.TestCase):
 
     def setUp(self):
@@ -18,21 +22,26 @@ class TestHuberLoss(unittest.TestCase):
         self.x = (numpy.random.random(self.shape) - 0.5) * 20
         self.x = self.x.astype(numpy.float32)
         self.t = numpy.random.random(self.shape).astype(numpy.float32)
-        self.gy = numpy.random.random(self.shape[0]).astype(numpy.float32)
+        if self.reduce == 'sum_along_second_axis':
+            gy_shape = self.shape[0]
+        else:
+            gy_shape = self.shape
+        self.gy = numpy.random.random(gy_shape).astype(numpy.float32)
 
     def check_forward(self, x_data, t_data):
         x = chainer.Variable(x_data)
         t = chainer.Variable(t_data)
-        loss = functions.huber_loss(x, t, delta=1)
+        loss = functions.huber_loss(x, t, delta=1, reduce=self.reduce)
         self.assertEqual(loss.data.dtype, numpy.float32)
         loss_value = cuda.to_cpu(loss.data)
 
         diff_data = cuda.to_cpu(x_data) - cuda.to_cpu(t_data)
-        expected_result = numpy.zeros(self.shape)
+        loss_expect = numpy.zeros(self.shape)
         mask = numpy.abs(diff_data) < 1
-        expected_result[mask] = 0.5 * diff_data[mask] ** 2
-        expected_result[~mask] = numpy.abs(diff_data[~mask]) - 0.5
-        loss_expect = numpy.sum(expected_result, axis=1)
+        loss_expect[mask] = 0.5 * diff_data[mask] ** 2
+        loss_expect[~mask] = numpy.abs(diff_data[~mask]) - 0.5
+        if self.reduce == 'sum_along_second_axis':
+            loss_expect = numpy.sum(loss_expect, axis=1)
         testing.assert_allclose(loss_value, loss_expect)
 
     @condition.retry(3)
@@ -46,7 +55,7 @@ class TestHuberLoss(unittest.TestCase):
 
     def check_backward(self, x_data, t_data, y_grad):
         gradient_check.check_backward(
-            functions.HuberLoss(delta=1),
+            functions.HuberLoss(delta=1, reduce=self.reduce),
             (x_data, t_data), y_grad, eps=1e-2, atol=1e-3)
 
     @condition.retry(3)
