e0124af0cce393dd42ad2da40094533b64436d87
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-04-21 18:32:55 +0000

    Allow user to specify statistic functions

--- chainer/training/extensions/parameter_statistics.py
+++ chainer/training/extensions/parameter_statistics.py
@@ -1,127 +1,82 @@
 import numpy
 import six
 
-from chainer import cuda
 from chainer import reporter
 from chainer import training
 from chainer.training import extension
 
 
-def _prefix_dict_keys(prefix, x):
-    return {'{}/{}'.format(prefix, k): v for k, v in six.iteritems(x)}
-
-
-def _statistics(x, functions):
-    """Compute statisticts for the given array.
-
-    Args:
-        x (numpy.ndarray or cupy.ndarray): Target array for which statistics
-            are computed.
-        functions (iterable of str): Statistics to collect, mapping directly
-            to NumPy or CuPy functions.
-
-    Returns:
-        dict: Mappings from functions keys to statistic values.
-    """
-    stats = {}
-    for f in functions:
-        try:
-            # nan if x.size == 0 and f in ('mean', 'std')
-            stats[f] = getattr(x, f)()
-        except ValueError:  # x.size == 0 and f in ('min, 'max')
-            stats[f] = float('NaN')
-
-    return stats
-
-
-def _percentiles(x, sigmas):
-    """Compute percentiles for the given array.
-
-    Args:
-        x (numpy.ndarray or cupy.ndarray): Target array for which percentiles
-            are computed.
-        sigmas (iterable or float): Percentile sigma values.
-
-    Returns:
-        numpy.ndarray or cupy.ndarray: List of percentiles. The list has the
-            same length as the given ``sigma``.
-    """
-    def _percentiles_cpu(_x):
-        if _x.size == 0:
-            return numpy.array((float('NaN'),) * len(sigmas))
-        return numpy.percentile(_x, sigmas).astype(_x.dtype)
-
-    if cuda.available and isinstance(x, cuda.ndarray):
-        # cuda.cupy.percentile() is not implemented
-        return cuda.to_gpu(_percentiles_cpu(cuda.to_cpu(x)))
-
-    return _percentiles_cpu(x)
-
-
-def _zeros(x):
-    """Count the number of zeros in the given array.
-
-    Args:
-        x (numpy.ndarray or cupy.ndarray): Target array for which sparsity is
-            computed.
-
-    Returns:
-        int: Number of zeros.
-    """
-    return x.size - cuda.get_array_module(x).count_nonzero(x)
-
-
 class ParameterStatistics(extension.Extension):
     """Trainer extension to report parameter statistics.
 
-    The statistics are collected for a given :class:`~chainer.Link` or an
-    iterable of :class:`~chainer.Link`s. If a link contains child links, the
-    statistics are collected separately for each child.
+    Statistics are collected and reported for a given :class:`~chainer.Link`
+    or an iterable of :class:`~chainer.Link`s. If a link contains child links,
+    the statistics are reported separately for each child.
 
-    Statistics that can be collected and reporter using the current scope are
-    minimum and maximum values, means, standard deviations, percentiles and
-    zero counts.
+    Any function that takes a one-dimensional :class:`numpy.ndarray` or a
+    :class:`cupy.ndarray` and outputs a single or multiple real numbers can be
+    registered to handle the collection of statistics, e.g.
+    :meth:`numpy.ndarray.mean`.
+
+    The keys of reported statistics follow the convention of link name
+    followed by parameter name, attribute name and function name, e.g.
+    ``VGG16Layers/conv1_1/W/data/mean``. They are prepended with an optional
+    prefix and appended with integer indices if the statistics generating
+    function return multiple values.
 
     Args:
-        links (~chainer.Link or iterable of ~chainer.Link): Links containing
+        links (~chainer.Link or iterable of ~chainer.Link): Link(s) containing
             the parameters to observe. The link is expected to have a ``name``
-            attribute which is used as a part of a key in the report.
+            attribute which is used as a part of the report key.
+        statistics (dict): Dictionary with function name to function mappings.
+            The name is a string and is used as a part of the report key. The
+            function is responsible for generating the statistics.
+        report_params (bool): If ``True``, report statistics for parameter
+            values such as weights and biases.
+        report_grads (bool): If ``True``, report statistics for parameter
+            gradients.
+        prefix (str): Optional prefix to prepend to the report keys.
         trigger: Trigger that decides when to aggregate the results and report
             the values.
-        report_params (bool): If ``True``, include parameter statistics in the
-            report such as weight and bias values.
-        report_param_grads (bool): If ``True``, include parameter statistics
-            in the report such as weight and bias gradients.
-        count_zeros (bool): If ``True``, count the number of zero elements and
-            include those statistics in the report. Else, do not compute the
-            number of zero elements.
-        prefix (str): Prefix to prepend to the report keys.
-        targets (iterable of tuples): Target parameters and their attributes
-            to consider in this extension.
     """
     default_name = 'parameter_statistics'
     priority = extension.PRIORITY_WRITER
 
-    def __init__(self, links, trigger=(1, 'epoch'), report_params=True,
-                 report_param_grads=True, count_zeros=False, prefix=None):
+    # Exclude the first '/' since param_name starts with one
+    report_key_template = '{link_name}{param_name}/{attr_name}/{function_name}'
+
+    default_statistics = {
+            'mean': numpy.mean,
+            'std': numpy.std,
+            'min': numpy.min,
+            'max': numpy.max,
+            'zeros': lambda x: numpy.count_nonzero(x == 0),
+            'percentile': lambda x: numpy.percentile(x, (0.13, 2.28, 15.87,
+                                                         50, 84.13, 97.72,
+                                                         99.87))
+        }
+
+    def __init__(self, links, statistics=default_statistics,
+                 report_params=True, report_grads=True, prefix=None,
+                 trigger=(1, 'epoch')):
 
         if not isinstance(links, (list, tuple)):
             links = links,
         self._links = links
 
-        self._trigger = training.trigger.get_trigger(trigger)
-        self._count_zeros = count_zeros
-        self._prefix = prefix
+        if statistics is None:
+            statistics = {}
+        self._statistics = statistics
 
-        self._attrs = []
+        attrs = []
         if report_params:
-            self._attrs.append('data')
-        if report_param_grads:
-            self._attrs.append('grad')
+            attrs.append('data')
+        if report_grads:
+            attrs.append('grad')
+        self._attrs = attrs
 
-        self._statistic_functions = ('min', 'max', 'mean', 'std')
-        self._percentile_sigmas = (0.13, 2.28, 15.87, 50, 84.13, 97.72, 99.87)
-
+        self._prefix = prefix
+        self._trigger = training.trigger.get_trigger(trigger)
         self._summary = reporter.DictSummary()
 
     def __call__(self, trainer):
@@ -137,40 +92,53 @@ class ParameterStatistics(extension.Extension):
             trainer (~chainer.training.Trainer): Associated trainer that
                 invoked this extension.
         """
+        statistics = {}
 
         for link in self._links:
             link_name = getattr(link, 'name', 'None')
             for param_name, param in link.namedparams():
                 for attr_name in self._attrs:
-                    prefix = '{}{}/{}/'.format(link_name,
-                                               param_name,
-                                               attr_name)
+                    for function_name, function in \
+                            six.iteritems(self._statistics):
+                        # Get parameters as a flattend one dimensional array
+                        # since the statistics function should make no
+                        # assumption about the axes
+                        params = getattr(param, attr_name).ravel()
+                        value = function(params)
+                        key = self.report_key_template.format(
+                                link_name=link_name,
+                                param_name=param_name,
+                                attr_name=attr_name,
+                                function_name=function_name
+                            )
+                        if hasattr(value, '__iter__'):
+                            # Append integer indices to the keys if the
+                            # statistic function return multiple values
+                            statistics.update({'{}/{}'.format(key, i): v for
+                                               i, v in enumerate(value)})
+                        else:
+                            statistics[key] = value
 
-                    params = getattr(param, attr_name).ravel()
-
-                    stats = {}
-                    stats.update(self.statistics_report(params, prefix))
-                    stats.update(self.percentiles_report(params, prefix))
-                    if self._count_zeros:
-                        stats.update(self.zeros_report(params, prefix))
-                    self._summary.add(self.post_process(stats))
+        if self._prefix is not None:
+            statistics = {'{}/{}'.format(self._prefix, k): v for k, v in
+                          six.iteritems(statistics)}
+        self._summary.add(statistics)
 
         if self._trigger(trainer):
             reporter.report(self._summary.compute_mean())
             self._summary = reporter.DictSummary()  # Clear summary
 
-    def statistics_report(self, params, prefix=''):
-        return {'{}{}'.format(prefix, f): s for f, s in
-                six.iteritems(_statistics(params, self._statistic_functions))}
+    def register_statistics(self, name, function):
+        """Register a function to compute a certain statistic.
 
-    def percentiles_report(self, params, prefix=''):
-        return {'{}percentile/{}'.format(prefix, i): p for i, p
-                in enumerate(_percentiles(params, self._percentile_sigmas))}
+        The registered function will be called each time the extension runs and
+        the results will be included in the report.
 
-    def zeros_report(self, params, prefix=''):
-        return {'{}zeros'.format(prefix): _zeros(params)}
-
-    def post_process(self, stats):
-        if self._prefix is not None:
-            stats = _prefix_dict_keys(self._prefix, stats)
-        return stats
+        Args:
+            name (str): Name of the statistic.
+            function: Function to generate the statistic. Any function that
+                takes a one-dimensional :class:`numpy.ndarray` or a
+                :class:`cupy.ndarray` and outputs a single or multiple real
+                numbers is allowed.
+        """
+        self._statistics[name] = function
