631126cb0d517dcc1d7688e355e44e654a0e1a9c
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-21 14:12:08 +0900

    fix ResNet101 to load pretrained model correctly

--- chainer/links/model/vision/resnet.py
+++ chainer/links/model/vision/resnet.py
@@ -628,7 +628,7 @@ def _transfer_resnet101(src, dst):
     _transfer_block(src, dst.res2, ['2a', '2b', '2c'])
     _transfer_block(src, dst.res3, ['3a', '3b1', '3b2', '3b3'])
     _transfer_block(src, dst.res4,
-                    ['4a'] + ['4b'.format(i) for i in range(1, 23)])
+                    ['4a'] + ['4b{}'.format(i) for i in range(1, 23)])
     _transfer_block(src, dst.res5, ['5a', '5b', '5c'])
 
     dst.fc6.W.data[:] = src.fc1000.W.data
