a14e6828b378a598bd09a8f53b9f4649f22edbd3
aonotas <nanigashi03@gmail.com>
2017-04-24 16:59:04 +0900

    fix for autopep8

--- chainer/functions/connection/n_step_rnn.py
+++ chainer/functions/connection/n_step_rnn.py
@@ -160,6 +160,7 @@ if cuda.cudnn_enabled and _cudnn_version >= 5000:
 
 
 class BaseNStepRNN(function.Function):
+
     def __init__(self, n_layers, states, rnn_dir, rnn_mode, train=True):
         if rnn_dir not in _rnn_dirs:
             candidate_list = ','.join(_rnn_dirs.keys())
