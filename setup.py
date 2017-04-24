9173160ec95ea28049f845b99fb238c04fe805b8
Ryosuke Okuta <okuta@preferred.jp>
2017-04-24 12:29:19 +0900

    Fix environment variable

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ import cupy_setup_build
 
 
 if sys.version_info[:3] == (3, 5, 0):
-    if not int(os.getenv('CHAINER_PYTHON_350_FORCE', '0')):
+    if not int(os.getenv('CUPY_PYTHON_350_FORCE', '0')):
         msg = """
-Chainer does not work with Python 3.5.0.
+CuPy does not work with Python 3.5.0.
 
 We strongly recommend to use another version of Python.
-If you want to use Chainer with Python 3.5.0 at your own risk,
-set 1 to CHAINER_PYTHON_350_FORCE environment variable."""
+If you want to use CuPy with Python 3.5.0 at your own risk,
+set 1 to CUPY_PYTHON_350_FORCE environment variable."""
         print(msg)
         sys.exit(1)
 
