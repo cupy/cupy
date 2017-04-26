d64f9026d3acd3297361547f2b1861539d60c6b1
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-26 18:44:12 +0900

    sample from a padded image

--- chainer/functions/array/spatial_transformer_sampler.py
+++ chainer/functions/array/spatial_transformer_sampler.py
@@ -74,20 +74,24 @@ class SpatialTransformerSampler(function.Function):
         u = grid[:, 0]
         v = grid[:, 1]
 
-        # clip coordinates to [-1, 1]
-        u = u.clip(-1, 1)
-        v = v.clip(-1, 1)
+        # Pad the image so that points locating outside of the original
+        # image's size can be sampled.
+        x_pad = xp.pad(x, ((0, 0), (0, 0), (1, 1), (1, 1)), mode='constant')
 
-        # rescale coordinates from [-1, 1] to [0, width or height - 1]
-        u = (u + 1) / 2 * (W - 1)
-        v = (v + 1) / 2 * (H - 1)
+        # Rescale coordinates from [-1, 1] to [0, width or height - 1],
+        # and adjust them to the padded image.
+        u = (u + 1) / 2 * (W - 1) + 1
+        v = (v + 1) / 2 * (H - 1) + 1
+
+        u = u.clip(0, W + 1)
+        v = v.clip(0, H + 1)
 
         # indices of the 2x2 pixel neighborhood surrounding the coordinates
         u0 = xp.floor(u).astype(numpy.int32)
-        u0 = u0.clip(0, W - 2)
+        u0 = u0.clip(0, W)
         u1 = u0 + 1
         v0 = xp.floor(v).astype(numpy.int32)
-        v0 = v0.clip(0, H - 2)
+        v0 = v0.clip(0, H)
         v1 = v0 + 1
 
         # weights
@@ -95,19 +99,19 @@ class SpatialTransformerSampler(function.Function):
         w2 = (u - u0) * (v1 - v)
         w3 = (u1 - u) * (v - v0)
         w4 = (u - u0) * (v - v0)
-        w1 = w1.astype(x.dtype)
-        w2 = w2.astype(x.dtype)
-        w3 = w3.astype(x.dtype)
-        w4 = w4.astype(x.dtype)
+        w1 = w1.astype(x_pad.dtype)
+        w2 = w2.astype(x_pad.dtype)
+        w3 = w3.astype(x_pad.dtype)
+        w4 = w4.astype(x_pad.dtype)
 
         x_indexed_1 = xp.concatenate([xp.expand_dims(
-            x[b, :, v0[b], u0[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v0[b], u0[b]], axis=0) for b in range(B)], axis=0)
         x_indexed_2 = xp.concatenate([xp.expand_dims(
-            x[b, :, v0[b], u1[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v0[b], u1[b]], axis=0) for b in range(B)], axis=0)
         x_indexed_3 = xp.concatenate([xp.expand_dims(
-            x[b, :, v1[b], u0[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v1[b], u0[b]], axis=0) for b in range(B)], axis=0)
         x_indexed_4 = xp.concatenate([xp.expand_dims(
-            x[b, :, v1[b], u1[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v1[b], u1[b]], axis=0) for b in range(B)], axis=0)
         y = w1[:, :, None] * x_indexed_1
         y += w2[:, :, None] * x_indexed_2
         y += w3[:, :, None] * x_indexed_3
@@ -162,20 +166,24 @@ class SpatialTransformerSampler(function.Function):
         u = grid[:, 0]
         v = grid[:, 1]
 
-        # clip coordinates to [-1, 1]
-        u = u.clip(-1, 1)
-        v = v.clip(-1, 1)
+        # Pad the image so that points locating outside of the original
+        # image's size can be sampled.
+        x_pad = xp.pad(x, ((0, 0), (0, 0), (1, 1), (1, 1)), mode='constant')
 
-        # rescale coordinates from [-1, 1] to [0, width or height - 1]
-        u = (u + 1) / 2. * (W - 1)
-        v = (v + 1) / 2. * (H - 1)
+        # Rescale coordinates from [-1, 1] to [0, width or height - 1],
+        # and adjust them to the padded image.
+        u = (u + 1) / 2. * (W - 1) + 1
+        v = (v + 1) / 2. * (H - 1) + 1
+
+        u = u.clip(0, W + 1)
+        v = v.clip(0, H + 1)
 
         # indices of the 2x2 pixel neighborhood surrounding the coordinates
         u0 = xp.floor(u).astype(numpy.int32)
-        u0 = u0.clip(0, W - 2)
+        u0 = u0.clip(0, W)
         u1 = u0 + 1
         v0 = xp.floor(v).astype(numpy.int32)
-        v0 = v0.clip(0, H - 2)
+        v0 = v0.clip(0, H)
         v1 = v0 + 1
 
         # weights
@@ -190,13 +198,13 @@ class SpatialTransformerSampler(function.Function):
 
         # --- gu, gv
         x_indexed_1 = xp.concatenate([xp.expand_dims(
-            x[b, :, v0[b], u0[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v0[b], u0[b]], axis=0) for b in range(B)], axis=0)
         x_indexed_2 = xp.concatenate([xp.expand_dims(
-            x[b, :, v0[b], u1[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v0[b], u1[b]], axis=0) for b in range(B)], axis=0)
         x_indexed_3 = xp.concatenate([xp.expand_dims(
-            x[b, :, v1[b], u0[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v1[b], u0[b]], axis=0) for b in range(B)], axis=0)
         x_indexed_4 = xp.concatenate([xp.expand_dims(
-            x[b, :, v1[b], u1[b]], axis=0) for b in range(B)], axis=0)
+            x_pad[b, :, v1[b], u1[b]], axis=0) for b in range(B)], axis=0)
 
         gu = -wv1[:, :, None] * x_indexed_1
         gu += wv1[:, :, None] * x_indexed_2
@@ -226,20 +234,18 @@ class SpatialTransformerSampler(function.Function):
             scatter_add = numpy.add.at
         else:
             scatter_add = xp.scatter_add
-        gxs = []
+        gx = xp.zeros_like(x_pad)
         gy = gy.reshape(B, C, -1)
         for b in range(B):
-            gx = xp.zeros_like(x[b])
-            scatter_add(gx, (slice(None), v0[b], u0[b]),
+            scatter_add(gx[b], (slice(None), v0[b], u0[b]),
                         gy[b] * wu1[b] * wv1[b])
-            scatter_add(gx, (slice(None), v0[b], u1[b]),
+            scatter_add(gx[b], (slice(None), v0[b], u1[b]),
                         gy[b] * wu0[b] * wv1[b])
-            scatter_add(gx, (slice(None), v1[b], u0[b]),
+            scatter_add(gx[b], (slice(None), v1[b], u0[b]),
                         gy[b] * wu1[b] * wv0[b])
-            scatter_add(gx, (slice(None), v1[b], u1[b]),
+            scatter_add(gx[b], (slice(None), v1[b], u1[b]),
                         gy[b] * wu0[b] * wv0[b])
-            gxs.append(gx)
-        gx = xp.concatenate([xp.expand_dims(g, axis=0) for g in gxs], axis=0)
+        gx = gx[:, :, 1:-1, 1:-1]
         return gx, ggrid
 
 
