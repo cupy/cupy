aa5a4486b5f2dce515437630ebbf13a29417898b
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-04-28 05:53:29 +0000

    Use concrete Trainer in parameter statistics test

--- tests/chainer_tests/training_tests/extensions_tests/test_parameter_statistics.py
+++ tests/chainer_tests/training_tests/extensions_tests/test_parameter_statistics.py
@@ -1,4 +1,5 @@
 import re
+import time
 import unittest
 
 import mock
@@ -7,108 +8,138 @@ import six
 
 import chainer
 from chainer import testing
+from chainer import training
 from chainer.training import extensions
 
 
-@testing.parameterize(
-    {'statistics': {'min': numpy.min},
-     'links': chainer.links.Linear(10, 10),
-     'expect': 4},
-    {'statistics': {'mean': numpy.mean, 'std': numpy.std},
-     'links': chainer.links.Linear(10, 10),
-     'expect': 8},
-    {'statistics': extensions.ParameterStatistics.default_statistics,
-     'links': chainer.links.Linear(10, 10),
-     'expect': 48},
-    {'statistics': {},
-     'links': chainer.links.Linear(10, 10),
-     'expect': 0}
-)
-class TestParameterStatistic(unittest.TestCase):
+class TestParameterStatisticsBase(object):
 
     def setUp(self):
-        self.trainer = mock.MagicMock()
-        self.reporter = chainer.Reporter()
+        self.trainer = _get_mocked_trainer()
+
+    @property
+    def links(self):
+        links = []
+        for optimizer in six.itervalues(
+                self.trainer.updater.get_all_optimizers()):
+            for namedlink in optimizer.target.namedlinks():
+                _, link = namedlink
+                links.append(link)
+        return links
+
+
+@testing.parameterize(
+    {'statistics': {'min': numpy.min}, 'expect': 4},
+    {'statistics': {'mean': numpy.mean, 'std': numpy.std}, 'expect': 8},
+    {'statistics': extensions.ParameterStatistics.default_statistics,
+     'expect': 48},
+    {'statistics': {}, 'expect': 0}
+)
+class TestParameterStatistics(TestParameterStatisticsBase, unittest.TestCase):
 
     def test_report(self):
         extension = extensions.ParameterStatistics(self.links,
                                                    statistics=self.statistics)
-        with self.reporter:
-            extension(self.trainer)
-            self.assertEqual(len(self.reporter.observation), self.expect)
+        self.trainer.extend(extension)
+        self.trainer.run()
+
+        self.assertEqual(len(self.trainer.observation), self.expect)
 
     def test_report_late_register(self):
         extension = extensions.ParameterStatistics(self.links, statistics={})
         for name, function in six.iteritems(self.statistics):
             extension.register_statistics(name, function)
-        with self.reporter:
-            extension(self.trainer)
-            self.assertEqual(len(self.reporter.observation), self.expect)
+        self.trainer.extend(extension)
+        self.trainer.run()
+
+        self.assertEqual(len(self.trainer.observation), self.expect)
 
     def test_report_key_pattern(self):
         extension = extensions.ParameterStatistics(self.links)
+        self.trainer.extend(extension)
+        self.trainer.run()
+
         pattern = r'^(.+/){2,}(data|grad)/.+[^/]$'
-        with self.reporter:
-            extension(self.trainer)
-            for name in six.iterkeys(self.reporter.observation):
-                self.assertTrue(re.match(pattern, name))
+        for name in six.iterkeys(self.trainer.observation):
+            self.assertTrue(re.match(pattern, name))
 
 
 @testing.parameterize(
-    {'statistics': {'one': lambda x: 1.0},
-     'links': chainer.links.Linear(10, 10),
-     'expect': 1.0}
+    {'statistics': {'one': lambda x: 1.0}, 'expect': 1.0}
 )
-class TestParameterStatisticsCustomFunction(unittest.TestCase):
-
-    def setUp(self):
-        self.trainer = mock.MagicMock()
-        self.reporter = chainer.Reporter()
-        self.extension = extensions.ParameterStatistics(
-            self.links, statistics=self.statistics)
+class TestParameterStatisticsCustomFunction(TestParameterStatisticsBase,
+                                            unittest.TestCase):
 
     def test_custom_function(self):
-        with self.reporter:
-            self.extension(self.trainer)
-            for value in six.itervalues(self.reporter.observation):
-                self.assertEqual(value, self.expect)
+        extension = extensions.ParameterStatistics(
+            self.links, statistics=self.statistics)
+        self.trainer.extend(extension)
+        self.trainer.run()
+
+        for value in six.itervalues(self.trainer.observation):
+            self.assertEqual(value, self.expect)
 
 
 @testing.parameterize(
-    {'statistics': extensions.ParameterStatistics.default_statistics,
-     'links': chainer.links.Linear(10, 10)}
+    {'statistics': extensions.ParameterStatistics.default_statistics}
 )
-class TestParameterStatisticsArguments(unittest.TestCase):
-
-    def setUp(self):
-        self.trainer = mock.MagicMock()
-        self.reporter = chainer.Reporter()
+class TestParameterStatisticsArguments(TestParameterStatisticsBase,
+                                       unittest.TestCase):
 
     def test_skip_params(self):
         extension = extensions.ParameterStatistics(
             self.links, statistics=self.statistics, report_params=False)
-        with self.reporter:
-            extension(self.trainer)
-            for name in six.iterkeys(self.reporter.observation):
-                self.assertIn('grad', name)
-                self.assertNotIn('data', name)
+        self.trainer.extend(extension)
+        self.trainer.run()
+
+        for name in six.iterkeys(self.trainer.observation):
+            self.assertIn('grad', name)
+            self.assertNotIn('data', name)
 
     def test_skip_grads(self):
         extension = extensions.ParameterStatistics(
             self.links, statistics=self.statistics, report_grads=False)
-        with self.reporter:
-            extension(self.trainer)
-            for name in six.iterkeys(self.reporter.observation):
-                self.assertIn('data', name)
-                self.assertNotIn('grad', name)
+        self.trainer.extend(extension)
+        self.trainer.run()
+
+        for name in six.iterkeys(self.trainer.observation):
+            self.assertIn('data', name)
+            self.assertNotIn('grad', name)
 
     def test_report_key_prefix(self):
         extension = extensions.ParameterStatistics(
             self.links, statistics=self.statistics, prefix='prefix')
-        with self.reporter:
-            extension(self.trainer)
-            for name in six.iterkeys(self.reporter.observation):
-                self.assertTrue(name.startswith('prefix'))
+        self.trainer.extend(extension)
+        self.trainer.run()
+
+        for name in six.iterkeys(self.trainer.observation):
+            self.assertTrue(name.startswith('prefix'))
+
+
+def _get_mocked_trainer(stop_trigger=(10, 'iteration')):
+    updater = mock.Mock()
+    optimizer = mock.Mock()
+    target = mock.Mock()
+    target.namedlinks.return_value = ('link_name',
+                                      chainer.links.Linear(10, 10)),
+    optimizer.target = target
+    updater.get_all_optimizers.return_value = {'optimizer_name': optimizer}
+    updater.iteration = 0
+    updater.epoch = 0
+    updater.epoch_detail = 0
+    updater.is_new_epoch = True
+    iter_per_epoch = 10
+
+    def update():
+        time.sleep(0.001)
+        updater.iteration += 1
+        updater.epoch = updater.iteration // iter_per_epoch
+        updater.epoch_detail = updater.iteration / iter_per_epoch
+        updater.is_new_epoch = updater.epoch == updater.epoch_detail
+
+    updater.update = update
+
+    return training.Trainer(updater, stop_trigger)
 
 
 testing.run_module(__name__, __file__)
