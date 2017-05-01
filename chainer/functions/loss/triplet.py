54a69a84f57b37f45450d8b0f7b264611d3a3282
Kenta OONO <oono@preferred.jp>
2017-05-01 15:53:19 -0700

    Add reduce option to triplet loss

--- chainer/functions/loss/triplet.py
+++ chainer/functions/loss/triplet.py
@@ -9,10 +9,16 @@ class Triplet(function.Function):
 
     """Triplet loss function."""
 
-    def __init__(self, margin):
+    def __init__(self, margin, reduce='mean'):
         if margin <= 0:
             raise ValueError('margin should be positive value.')
         self.margin = margin
+
+        if reduce not in ('mean', 'no'):
+            raise ValueError(
+                "only 'mean' and 'no' are valid for 'reduce', but '%s' is "
+                'given' % reduce)
+        self.reduce = reduce
 
     def check_type_forward(self, in_types):
         type_check.expect(in_types.size() == 3)
@@ -30,14 +36,16 @@ class Triplet(function.Function):
         xp = cuda.get_array_module(*inputs)
 
         anchor, positive, negative = inputs
-        N = anchor.shape[0]
 
         dist = xp.sum(
             (anchor - positive) ** 2 - (anchor - negative) ** 2,
             axis=1) + self.margin
         self.dist_hinge = xp.maximum(dist, 0)
-        loss = xp.sum(self.dist_hinge) / N
-
+        if self.reduce == 'mean':
+            N = anchor.shape[0]
+            loss = xp.sum(self.dist_hinge) / N
+        else:
+            loss = self.dist_hinge
         return xp.array(loss, dtype=numpy.float32),
 
     def backward(self, inputs, gy):
@@ -50,7 +58,12 @@ class Triplet(function.Function):
         tmp = xp.repeat(self.dist_hinge[:, None], x_dim, axis=1)
         mask = xp.array(tmp > 0, dtype=numpy.float32)
 
-        tmp = 2 * gy[0] * mask / N
+        if self.reduce == 'mean':
+            g = gy[0] / N
+        else:
+            g = gy[0][:, None]
+
+        tmp = 2 * g * mask
         gx0 = (tmp * (negative - positive)).astype(numpy.float32)
         gx1 = (tmp * (positive - anchor)).astype(numpy.float32)
         gx2 = (tmp * (anchor - negative)).astype(numpy.float32)
@@ -58,7 +71,7 @@ class Triplet(function.Function):
         return gx0, gx1, gx2
 
 
-def triplet(anchor, positive, negative, margin=0.2):
+def triplet(anchor, positive, negative, margin=0.2, reduce='mean'):
     """Computes triplet loss.
 
     It takes a triplet of variables as inputs, :math:`a`, :math:`p` and
@@ -74,6 +87,11 @@ def triplet(anchor, positive, negative, margin=0.2):
 
     where :math:`d(x_i, y_i) = \\| {\\bf x}_i - {\\bf y}_i \\|_2^2`.
 
+    The output is a variable whose value depends on the value of
+    the option ``reduce``. If it is ``'no'``, it holds the elementwise
+    loss values. If it is ``'mean'``, this function takes a mean of
+    loss values.
+
     Args:
         anchor (~chainer.Variable): The anchor example variable. The shape
             should be :math:`(N, K)`, where :math:`N` denotes the minibatch
@@ -84,14 +102,20 @@ def triplet(anchor, positive, negative, margin=0.2):
             should be the same as anchor.
         margin (float): A parameter for triplet loss. It should be a positive
             value.
+        recude (str): Reduction option. Its value must be either
+            ``'mean'`` or ``'no'``. Otherwise, :class:`ValueError` is raised.
 
     Returns:
-        ~chainer.Variable: A variable holding a scalar that is the loss value
+        ~chainer.Variable:
+            A variable holding a scalar that is the loss value
             calculated by the above equation.
+            If ``reduce`` is ``'no'``, the output variable holds array
+            whose shape is same as one of (hence both of) input variables.
+            If it is ``'mean'``, the output variable holds a scalar value.
 
     .. note::
         This cost can be used to train triplet networks. See `Learning \
         Fine-grained Image Similarity with Deep Ranking \
         <https://arxiv.org/abs/1404.4661>`_ for details.
     """
-    return Triplet(margin)(anchor, positive, negative)
+    return Triplet(margin, reduce)(anchor, positive, negative)
