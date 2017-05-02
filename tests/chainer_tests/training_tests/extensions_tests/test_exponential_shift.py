301406700ad4b31d4dd5b76add458d1aefcca9f1
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-05-02 14:10:14 +0900

    add backward compat test

--- tests/chainer_tests/training_tests/extensions_tests/test_exponential_shift.py
+++ tests/chainer_tests/training_tests/extensions_tests/test_exponential_shift.py
@@ -1,12 +1,45 @@
-import tempfile
+import numpy
 import unittest
 
 import mock
 
-from chainer import serializers
+from chainer import serializer
 from chainer import testing
 from chainer.training import extensions
 from chainer.training.util import get_trigger
+
+
+class DummySerializer(serializer.Serializer):
+
+    def __init__(self, target):
+        super(DummySerializer, self).__init__()
+        self.target = target
+
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+    def __call__(self, key, value):
+        self.target[key] = value
+        return self.target[key]
+
+
+class DummyDeserializer(serializer.Deserializer):
+
+    def __init__(self, target):
+        super(DummyDeserializer, self).__init__()
+        self.target = target
+
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+    def __call__(self, key, value):
+        if value is None:
+            value = self.target[key]
+        elif isinstance(value, numpy.ndarray):
+            numpy.copyto(value, self.target[key])
+        else:
+            value = type(value)(numpy.asarray(self.target[key]))
+        return value
 
 
 def _get_mocked_trainer(init):
@@ -58,30 +91,6 @@ class TestExponentialShift(unittest.TestCase):
             'x', self.rate, target=self.target)
         self._run_trainer(extension, self.expect)
 
-    def test_serialize(self):
-        with tempfile.NamedTemporaryFile(delete=False) as f:
-            extension = extensions.ExponentialShift(
-                'x', self.rate, target=self.target)
-            self._run_trainer(extension, self.expect[:len(self.expect) // 2])
-            serializers.save_npz(f.name, extension)
-
-            extension = extensions.ExponentialShift(
-                'x', self.rate, target=self.target)
-            serializers.load_npz(f.name, extension)
-            self._run_trainer(extension, self.expect[len(self.expect) // 2:])
-
-    def test_serialize_before_first_interval(self):
-        with tempfile.NamedTemporaryFile(delete=False) as f:
-            extension = extensions.ExponentialShift(
-                'x', self.rate, target=self.target)
-            self._run_trainer(extension, self.expect[:self.interval - 1])
-            serializers.save_npz(f.name, extension)
-
-            extension = extensions.ExponentialShift(
-                'x', self.rate, target=self.target)
-            serializers.load_npz(f.name, extension)
-            self._run_trainer(extension, self.expect[self.interval - 1:])
-
     def test_with_init(self):
         self.trainer.updater.optimizer.x = 0
         extension = extensions.ExponentialShift(
@@ -94,6 +103,45 @@ class TestExponentialShift(unittest.TestCase):
         extension = extensions.ExponentialShift(
             'x', self.rate, target=self.target, optimizer=optimizer)
         self._run_trainer(extension, self.expect, optimizer)
+
+    def test_serialize(self):
+        extension = extensions.ExponentialShift(
+            'x', self.rate, target=self.target)
+        self._run_trainer(extension, self.expect[:len(self.expect) // 2])
+        target = dict()
+        extension.serialize(DummySerializer(target))
+
+        extension = extensions.ExponentialShift(
+            'x', self.rate, target=self.target)
+        extension.serialize(DummyDeserializer(target))
+        self._run_trainer(extension, self.expect[len(self.expect) // 2:])
+
+    def test_serialize_before_first_interval(self):
+        extension = extensions.ExponentialShift(
+            'x', self.rate, target=self.target)
+        self._run_trainer(extension, self.expect[:self.interval - 1])
+        target = dict()
+        extension.serialize(DummySerializer(target))
+
+        extension = extensions.ExponentialShift(
+            'x', self.rate, target=self.target)
+        extension.serialize(DummyDeserializer(target))
+        self._run_trainer(extension, self.expect[self.interval - 1:])
+
+    def test_serialize_backward_compat(self):
+        extension = extensions.ExponentialShift(
+            'x', self.rate, target=self.target)
+        self._run_trainer(extension, self.expect[:len(self.expect) // 2])
+        target = dict()
+        extension.serialize(DummySerializer(target))
+
+        # older version does not have '_init'
+        del target['_init']
+
+        extension = extensions.ExponentialShift(
+            'x', self.rate, target=self.target)
+        extension.serialize(DummyDeserializer(target))
+        self._run_trainer(extension, self.expect[len(self.expect) // 2:])
 
 
 class TestExponentialShiftInvalidArgument(unittest.TestCase):
