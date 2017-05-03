b8dc20e6bc9620cc6e39a61d57d6c28b35b2e253
Yuya Unno <unnonouno@gmail.com>
2017-05-03 22:03:19 +0900

    Make backward test using gradient_check

--- tests/chainer_tests/links_tests/loss_tests/test_negative_sampling.py
+++ tests/chainer_tests/links_tests/loss_tests/test_negative_sampling.py
@@ -63,41 +63,27 @@ class TestNegativeSampling(unittest.TestCase):
         self.link.to_gpu()
         self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t))
 
-    def check_backward(self, x_data, t_data, y_grad):
-        x = chainer.Variable(x_data)
+    def check_backward(self, x_data, t_data, w_data, sample, y_grad):
         t = chainer.Variable(t_data)
-        W = self.link.W
+        ns = negative_sampling.NegativeSamplingFunction(
+            sample, self.link.sample_size, self.reduce)
 
-        y = self.link(x, t)
-        y.grad = y_grad
-        y.backward()
+        def f(x, w):
+            return ns(x, t, w)
 
-        # fix samples
-        negative_sampling.NegativeSamplingFunction.samples = y.creator.samples
+        gradient_check.check_backward(
+            f, (x_data, w_data), y_grad, eps=1e-2, atol=1e-4, rtol=1e-4)
 
-        def f():
-            return self.link(x, t).data,
-        gx, gW = gradient_check.numerical_grad(
-            f, (x.data, W.data), (y.grad,), eps=1e-2)
-        del negative_sampling.NegativeSamplingFunction.samples  # clean up
-
-        testing.assert_allclose(
-            cuda.to_cpu(gx), cuda.to_cpu(x.grad), atol=1.e-4)
-        testing.assert_allclose(
-            cuda.to_cpu(gW), cuda.to_cpu(W.grad), atol=1.e-4)
-        return x.grad, W.grad
-
-    @condition.retry(3)
     def test_backward_cpu(self):
-        self.check_backward(self.x, self.t, self.gy)
+        self.check_backward(
+            self.x, self.t, self.link.W.data, self.link.sampler.sample, self.gy)
 
     @attr.gpu
-    @condition.retry(3)
     def test_backward_gpu(self):
         self.link.to_gpu()
-        self.check_backward(cuda.to_gpu(self.x),
-                            cuda.to_gpu(self.t),
-                            cuda.to_gpu(self.gy))
+        self.check_backward(
+            cuda.to_gpu(self.x), cuda.to_gpu(self.t),
+            self.link.W.data, self.link.sampler.sample, cuda.to_gpu(self.gy))
 
     @attr.gpu
     def test_to_cpu(self):
@@ -105,27 +91,6 @@ class TestNegativeSampling(unittest.TestCase):
         self.assertTrue(self.link.sampler.use_gpu)
         self.link.to_cpu()
         self.assertFalse(self.link.sampler.use_gpu)
-
-    @attr.gpu
-    def test_backward_cpu_gpu(self):
-        x = chainer.Variable(self.x)
-        t = chainer.Variable(self.t)
-        y = self.link(x, t)
-        y.grad = self.gy
-        y.backward()
-
-        # fix samples
-        negative_sampling.NegativeSamplingFunction.samples = cuda.to_gpu(
-            y.creator.samples)
-        self.link.to_gpu()
-        del negative_sampling.NegativeSamplingFunction.samples
-        xg = chainer.Variable(cuda.to_gpu(self.x))
-        tg = chainer.Variable(cuda.to_gpu(self.t))
-        y_g = self.link(xg, tg)
-        y_g.grad = cuda.to_gpu(self.gy)
-        y_g.backward()
-
-        testing.assert_allclose(x.grad, xg.grad, atol=1.e-4)
 
 
 class TestNegativeSamplingIgnoreMask(TestNegativeSampling):
@@ -151,32 +116,6 @@ class TestNegativeSamplingIgnoreMask(TestNegativeSampling):
             self.gy0 = self.gy.copy()[self.idx]
         else:
             self.gy0 = self.gy.copy()
-
-    def check_ignore_backward(
-            self, x_data, t_data, gy_data, x0_data, t0_data, gy0_data):
-        # Ensure that the gradient when an ignore target is included is the
-        # same as when it is excluded explicitly.
-        gx, gw = self.check_backward(x_data, t_data, gy_data)
-        self.link.zerograds()
-        gx0, gw0 = self.check_backward(x0_data, t0_data, gy0_data)
-        testing.assert_allclose(
-            cuda.to_cpu(gx)[self.idx, :], gx0, atol=1.e-4)
-        testing.assert_allclose(gw, gw0, atol=1.e-4)
-
-    def test_ignore_backward_cpu(self):
-        self.check_ignore_backward(
-            self.x, self.t, self.gy, self.x0, self.t0, self.gy0)
-
-    @attr.gpu
-    def test_ignore_backward_gpu(self):
-        self.link.to_gpu()
-        self.check_ignore_backward(
-            cuda.to_gpu(self.x),
-            cuda.to_gpu(self.t),
-            cuda.to_gpu(self.gy),
-            cuda.to_gpu(self.x0),
-            cuda.to_gpu(self.t0),
-            cuda.to_gpu(self.gy0))
 
 
 testing.run_module(__name__, __file__)
