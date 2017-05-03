5faf3883763c60fbe36d1bd3ecb00797dddcd3b4
Kenta OONO <oono@preferred.jp>
2017-05-02 21:59:37 -0700

    Add reduce option to decov

--- tests/chainer_tests/functions_tests/loss_tests/test_decov.py
+++ tests/chainer_tests/functions_tests/loss_tests/test_decov.py
@@ -12,36 +12,55 @@ from chainer.testing import attr
 from chainer.testing import condition
 
 
+def _deconv(h):
+    h = cuda.to_cpu(h)
+    h_mean = h.mean(axis=0)
+    N, M = h.shape
+    loss_expect = numpy.zeros((M, M), dtype=numpy.float32)
+    for i in six.moves.range(M):
+        for j in six.moves.range(M):
+            if i != j:
+                for n in six.moves.range(N):
+                    loss_expect[i, j] += (h[n, i] - h_mean[i]) * (
+                        h[n, j] - h_mean[j])
+    return loss_expect / N
+
+
+@testing.parameterize(
+    {'reduce': 'half_frobenius_norm'},
+    {'reduce': 'no'}
+)
 class TestDeCov(unittest.TestCase):
 
     def setUp(self):
         self.h = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
+        if self.reduce == 'half_frobenius_norm':
+            gloss_shape = ()
+        else:
+            gloss_shape = (3, 3)
+        self.gloss = numpy.random.uniform(
+            -1, 1, gloss_shape).astype(numpy.float32)
 
     def check_forward(self, h_data):
         h = chainer.Variable(h_data)
-        loss = functions.decov(h)
-        self.assertEqual(loss.data.shape, ())
+        loss = functions.decov(h, self.reduce)
+        if self.reduce == 'half_frobenius_norm':
+            loss_shape = ()
+        else:
+            loss_shape = (3, 3)
+        self.assertEqual(loss.data.shape, loss_shape)
         self.assertEqual(loss.data.dtype, numpy.float32)
-        loss_value = float(loss.data)
+        loss_value = cuda.to_cpu(loss.data)
 
         # Compute expected value
         h_data = cuda.to_cpu(h_data)
-        h_mean = h_data.mean(axis=0)
-        N = h_data.shape[0]
 
-        loss_expect = 0
-        for i in six.moves.range(h_data.shape[1]):
-            for j in six.moves.range(h_data.shape[1]):
-                ij_loss = 0.
-                if i != j:
-                    for n in six.moves.range(N):
-                        ij_loss += (h_data[n, i] - h_mean[i]) * (
-                            h_data[n, j] - h_mean[j])
-                    ij_loss /= N
-                loss_expect += ij_loss ** 2
-        loss_expect *= 0.5
+        loss_expect = _deconv(h_data)
+        if self.reduce == 'half_frobenius_norm':
+            loss_expect = (loss_expect ** 2).sum() * 0.5
 
-        self.assertAlmostEqual(loss_expect, loss_value, places=5)
+        numpy.testing.assert_allclose(
+            loss_expect, loss_value, rtol=1e-4, atol=1e-4)
 
     @condition.retry(3)
     def test_forward_cpu(self):
@@ -52,31 +71,54 @@ class TestDeCov(unittest.TestCase):
     def test_forward_gpu(self):
         self.check_forward(cuda.to_gpu(self.h))
 
-    def check_backward(self, h_data):
+    def check_backward(self, h_data, gloss_data):
         gradient_check.check_backward(
-            functions.DeCov(), (h_data,), None, eps=0.02, atol=1e-3)
+            functions.DeCov(self.reduce), (h_data,), gloss_data,
+            eps=0.02, atol=1e-3)
 
-    def check_type(self, h_data):
+    def check_type(self, h_data, gloss_data):
         h = chainer.Variable(h_data)
-        loss = functions.decov(h)
+        loss = functions.decov(h, self.reduce)
+        loss.grad = gloss_data
         loss.backward()
         self.assertEqual(h_data.dtype, h.grad.dtype)
 
     @condition.retry(3)
     def test_backward_cpu(self):
-        self.check_backward(self.h)
+        self.check_backward(self.h, self.gloss)
 
     def test_backward_type_cpu(self):
-        self.check_type(self.h)
+        self.check_type(self.h, self.gloss)
 
     @attr.gpu
     def test_backward_type_gpu(self):
-        self.check_type(cuda.to_gpu(self.h))
+        self.check_type(cuda.to_gpu(self.h),
+                        cuda.to_gpu(self.gloss))
 
     @attr.gpu
     @condition.retry(3)
     def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.h))
+        self.check_backward(cuda.to_gpu(self.h),
+                            cuda.to_gpu(self.gloss))
+
+
+class TestDeconvInvalidReductionOption(unittest.TestCase):
+
+    def setUp(self):
+        self.h = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
+
+    def check_invalid_option(self, xp):
+        h = xp.asarray(self.h)
+
+        with self.assertRaises(ValueError):
+            functions.decov(h, 'invalid_option')
+
+    def test_invalid_option_cpu(self):
+        self.check_invalid_option(numpy)
+
+    @attr.gpu
+    def test_invalid_option_gpu(self):
+        self.check_invalid_option(cuda.cupy)
 
 
 testing.run_module(__name__, __file__)
