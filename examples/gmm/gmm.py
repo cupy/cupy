7e8416a935fbd6cca3ea55bb922a1f8f59c85159
setoyama <kotaro.setoyama@gmail.com>
2017-05-11 19:39:23 +0900

    change initialize way

--- examples/gmm/gmm.py
+++ examples/gmm/gmm.py
@@ -1,3 +1,10 @@
+try:
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+except ImportError:
+    pass
+
 import numpy as np
 
 import cupy
@@ -5,28 +12,14 @@ import cupy
 
 class GaussianMixture(object):
 
-    def __init__(self, n_components=1, tol=1e-3, reg_covar=1e-6, max_iter=100,
-                 seed=None):
+    def __init__(self, n_components=1, tol=1e-3, reg_covar=1e-6, max_iter=100):
         self.n_components = n_components
         self.tol = tol
         self.reg_covar = reg_covar
         self.max_iter = max_iter
-        self.seed = seed
         self.xp = None
 
-    def _compute_precision_chol(self, covariances):
-        estimate_precision_error_message = (
-            "Fitting the mixture model failed because some components have "
-            "ill-defined empirical covariance (for instance caused by "
-            "singleton or collapsed samples). Try to decrease the "
-            "number of components, or increase reg_covar.")
-
-        if self.xp.any(self.xp.less_equal(covariances, 0.0)):
-            raise ValueError(estimate_precision_error_message)
-        precisions_chol = 1. / self.xp.sqrt(covariances)
-        return precisions_chol
-
-    def _estimate_gaussian_parameters(self, X, resp, reg_covar):
+    def estimate_gaussian_parameters(self, X, resp, reg_covar):
         nk = self.xp.sum(resp, axis=0) + 10 * self.xp.finfo(resp.dtype).eps
         means = self.xp.dot(resp.T, X) / nk[:, self.xp.newaxis]
         avg_X2 = self.xp.dot(resp.T, X * X) / nk[:, self.xp.newaxis]
@@ -35,82 +28,62 @@ class GaussianMixture(object):
         covariances = avg_X2 - 2 * avg_X_means + avg_means2 + reg_covar
         return nk, means, covariances
 
-    def _initialize(self, X, resp, n_samples):
-        weights, means, covariances = \
-            self._estimate_gaussian_parameters(X, resp, self.reg_covar)
-        weights /= n_samples
-        self.weights_ = weights
-        self.means_ = means
-        self.covariances_ = covariances
-        self.precisions_chol_ = \
-            self._compute_precision_chol(covariances)
-
-    def _initialize_parameters(self, X, n_samples, random_state):
-        # add kmeans pattern
-        resp = random_state.rand(n_samples, self.n_components)
-        resp /= self.xp.sum(resp, axis=1)[:, self.xp.newaxis]
-        self._initialize(X, resp, n_samples)
-
-    def _estimate_log_prob(self, X, means, precisions_chol):
-        n_samples, n_features = X.shape
+    def estimate_log_prob(self, X, means, precisions_chol):
+        n_features = X.shape[1]
         n_components, _ = means.shape
         log_det = (self.xp.sum(self.xp.log(precisions_chol), axis=1))
         precisions = precisions_chol ** 2
         log_prob = (self.xp.sum((means ** 2 * precisions), 1) -
-                    2. * self.xp.dot(X, (means * precisions).T) +
+                    2 * self.xp.dot(X, (means * precisions).T) +
                     self.xp.dot(X ** 2, precisions.T))
-        return -.5 * (n_features * self.xp.log(2 * np.pi) + log_prob) + log_det
+        return -0.5 * (n_features * self.xp.log(2 * np.pi) + log_prob) + log_det
 
-    def _e_step(self, X):
-        weighted_log_prob = self._estimate_log_prob(
-            X, self.means_, self.precisions_chol_) + self.xp.log(self.weights_)
+    def e_step(self, X):
+        weighted_log_prob = self.estimate_log_prob(
+            X, self.means, self.precisions_chol) + self.xp.log(self.weights)
         log_prob_norm = self.xp.log(self.xp.sum(self.xp.exp(weighted_log_prob),
                                                 axis=1))
         with np.errstate(under='ignore'):
             log_resp = weighted_log_prob - log_prob_norm[:, self.xp.newaxis]
         return self.xp.mean(log_prob_norm), log_resp
 
-    def _m_step(self, X, log_resp):
-        n_samples = X.shape[0]
-        self.weights_, self.means_, self.covariances_ = \
-            (self._estimate_gaussian_parameters(X, self.xp.exp(log_resp),
-                                                self.reg_covar))
-        self.weights_ /= n_samples
-        self.precisions_chol_ = self._compute_precision_chol(self.covariances_)
+    def m_step(self, X, log_resp):
+        self.weights, self.means, self.covariances = \
+            (self.estimate_gaussian_parameters(X, self.xp.exp(log_resp),
+                                               self.reg_covar))
+        self.weights /= len(X)
+        self.precisions_chol = 1 / self.xp.sqrt(self.covariances)
 
     def fit(self, X, y=None):
         self.xp = cupy.get_array_module(X)
-        random_state = self.xp.random.RandomState(self.seed)
-        max_lower_bound = -np.infty
         lower_bound = -np.infty
         converged = False
-        n_samples = X.shape[0]
-        self._initialize_parameters(X, n_samples, random_state)
+        self.weights = self.xp.array([0.5, 0.5], dtype=np.float32)
+        self.means = self.xp.random.rand(self.n_components, 2)
+        self.covariances = self.xp.random.rand(self.n_components, 2)
+        self.precisions_chol = 1 / self.xp.sqrt(self.covariances)
 
         for n_iter in range(self.max_iter):
             prev_lower_bound = lower_bound
-            log_prob_norm, log_resp = self._e_step(X)
-            self._m_step(X, log_resp)
+            log_prob_norm, log_resp = self.e_step(X)
+            self.m_step(X, log_resp)
             lower_bound = log_prob_norm
             change = lower_bound - prev_lower_bound
             if abs(change) < self.tol:
                 converged = True
                 break
 
-        if lower_bound > max_lower_bound:
-            max_lower_bound = lower_bound
-            best_n_iter = n_iter
-
         if not converged:
             msg = 'Failed to converge. Try different init parameters, \
                    or increase max_iter, tol or check for degenerate data.'
             print(msg)
 
-        self.precisions_ = self.precisions_chol_ ** 2
-        self.n_iter_ = best_n_iter
-
-        return self
-
     def predict(self, X):
-        result = self._estimate_log_prob(X, self.means_, self.precisions_chol_)
-        return (result + self.xp.log(self.weights_)).argmax(axis=1)
+        result = self.estimate_log_prob(X, self.means, self.precisions_chol)
+        return (result + self.xp.log(self.weights)).argmax(axis=1)
+
+    def draw(self, X):
+        plt.scatter(X[:, 0], X[:, 1])
+        plt.scatter(self.means[:, 0], self.means[:, 1], s=120, marker='s',
+                    facecolors='y', edgecolors='k')
+        plt.savefig('test.png')
--- examples/gmm/gmm_train.py
+++ examples/gmm/gmm_train.py
@@ -29,31 +29,34 @@ def run_gmm(X_train, y_train, X_test, y_test, estimator, n_classes):
 
 
 def run(gpuid, n_classes, max_iter):
-    X_train = np.random.rand(60000, 784)
-    X_test = np.random.rand(10000, 784)
-    y_train = np.random.randint(10, size=60000)
-    y_test = np.random.randint(10, size=10000)
+    trains = np.random.rand(100, 2).astype(np.float32)
+    X_train = np.r_[trains + 0.3, trains -0.3]
+    tests = np.random.rand(10, 2).astype(np.float32)
+    X_test = np.r_[tests + 0.3, tests - 0.3]
+    y_train = np.r_[np.ones(100), np.zeros(100)].astype(np.int32)
+    y_test = np.r_[np.ones(10), np.zeros(10)].astype(np.int32)
     repeat = 1
 
     estimator_cpu = gmm.GaussianMixture(n_components=n_classes,
-                                        max_iter=max_iter, seed=0)
+                                        max_iter=max_iter)
     with timer(' CPU '):
         for i in range(repeat):
             train_acc, test_acc = run_gmm(X_train, y_train, X_test, y_test,
                                           estimator_cpu, n_classes)
     print('train_accuracy : %f' % train_acc)
     print('test_accuracy : %f' % test_acc)
+    estimator_cpu.draw(X_train)
 
     with cupy.cuda.Device(gpuid):
-        X_train = cupy.asarray(X_train)
-        y_train = cupy.asarray(y_train)
-        y_test = cupy.asarray(y_test)
-        X_test = cupy.asarray(X_test)
+        X_train_gpu = cupy.array(X_train)
+        y_train_gpu = cupy.array(y_train)
+        y_test_gpu = cupy.array(y_test)
+        X_test_gpu = cupy.array(X_test)
         estimator_gpu = gmm.GaussianMixture(n_components=n_classes,
-                                            max_iter=max_iter, seed=0)
+                                            max_iter=max_iter)
         with timer(' GPU '):
             for i in range(repeat):
-                train_acc, test_acc = run_gmm(X_train, y_train, X_test, y_test,
+                train_acc, test_acc = run_gmm(X_train_gpu, y_train_gpu, X_test_gpu, y_test_gpu,
                                               estimator_gpu, n_classes)
         print('train_accuracy : %f' % train_acc)
         print('test_accuracy : %f' % test_acc)
@@ -63,7 +66,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu-id', '-g', default=0, type=int, dest='gpuid',
                         help='ID of GPU.')
-    parser.add_argument('--n_classes', '-n', default=10, type=int,
+    parser.add_argument('--n_classes', '-n', default=2, type=int,
                         dest='n_classes', help='number of classes')
     parser.add_argument('--max_iter', '-m', default=30, type=int,
                         dest='max_iter', help='number of iterations')
