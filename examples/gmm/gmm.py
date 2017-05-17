0e321ea26c907435bc38d575d5de8e3d8e565c65
setoyama <kotaro.setoyama@gmail.com>
2017-05-17 19:38:45 +0900

    break class

--- examples/gmm/gmm.py
+++ examples/gmm/gmm.py
@@ -9,94 +9,92 @@ import numpy as np
 import cupy
 
 
-class GaussianMixture(object):
+def estimate_gaussian_parameters(X, xp, resp):
+    nk = xp.sum(resp, axis=0)
+    means = xp.dot(resp.T, X) / nk[:, None]
+    avg_X2 = xp.dot(resp.T, X * X) / nk[:, None]
+    avg_X_means = means * xp.dot(resp.T, X) / nk[:, None]
+    covariances = avg_X2 - 2 * avg_X_means + means ** 2
+    return nk / len(X), means, covariances
 
-    def __init__(self, tol=1e-3, reg_covar=1e-6, max_iter=100):
-        self.tol = tol
-        self.reg_covar = reg_covar
-        self.max_iter = max_iter
-        self.xp = None
 
-    def estimate_gaussian_parameters(self, X, resp, reg_covar):
-        nk = self.xp.sum(resp, axis=0)
-        means = self.xp.dot(resp.T, X) / nk[:, None]
-        avg_X2 = self.xp.dot(resp.T, X * X) / nk[:, None]
-        avg_X_means = means * self.xp.dot(resp.T, X) / nk[:, None]
-        covariances = avg_X2 - 2 * avg_X_means + means ** 2 + reg_covar
-        return nk / len(X), means, covariances
+def estimate_log_prob(X, xp, inv_cov, means):
+    n_features = X.shape[1]
+    log_det = xp.sum(xp.log(inv_cov), axis=1)
+    precisions = inv_cov ** 2
+    log_prob = xp.sum((means ** 2 * precisions), 1) - \
+        2 * xp.dot(X, (means * precisions).T) + xp.dot(X ** 2, precisions.T)
+    return -0.5 * (n_features * xp.log(2 * np.pi) + log_prob) + log_det
 
-    def estimate_log_prob(self, X, inv_cov):
-        n_features = X.shape[1]
-        det = self.xp.sum(self.xp.log(inv_cov), axis=1)
-        precisions = inv_cov ** 2
-        log_prob = self.xp.sum((self.means ** 2 * precisions), 1) - \
-            2 * self.xp.dot(X, (self.means * precisions).T) + \
-            self.xp.dot(X ** 2, precisions.T)
-        return -0.5 * (n_features * self.xp.log(2 * np.pi) + log_prob) + det
 
-    def e_step(self, X):
-        weighted_log_prob = self.estimate_log_prob(X, self.inv_cov) \
-            + self.xp.log(self.weights)
-        log_prob_norm = self.xp.log(self.xp.sum(self.xp.exp(weighted_log_prob),
-                                                axis=1))
-        log_resp = weighted_log_prob - log_prob_norm[:, None]
-        return self.xp.mean(log_prob_norm), log_resp
+def e_step(X, xp, inv_cov, means, weights):
+    weighted_log_prob = estimate_log_prob(X, xp, inv_cov, means) + \
+        xp.log(weights)
+    log_prob_norm = xp.log(xp.sum(xp.exp(weighted_log_prob), axis=1))
+    log_resp = weighted_log_prob - log_prob_norm[:, None]
+    return xp.mean(log_prob_norm), log_resp
 
-    def m_step(self, X, log_resp):
-        self.weights, self.means, self.covariances = \
-            self.estimate_gaussian_parameters(X, self.xp.exp(log_resp),
-                                              self.reg_covar)
-        self.inv_cov = 1 / self.xp.sqrt(self.covariances)
 
-    def train_gmm(self, X):
-        self.xp = cupy.get_array_module(X)
-        lower_bound = -np.infty
-        converged = False
-        self.weights = self.xp.array([0.5, 0.5], dtype=np.float32)
-        mean1 = self.xp.random.normal(3, self.xp.array([1, 2]), size=2)
-        mean2 = self.xp.random.normal(-3, self.xp.array([2, 1]), size=2)
-        self.means = self.xp.stack((mean1, mean2))
-        self.covariances = self.xp.random.rand(2, 2)
-        self.inv_cov = 1 / self.xp.sqrt(self.covariances)
+def m_step(X, xp, log_resp):
+    weights, means, covariances = \
+        estimate_gaussian_parameters(X, xp, xp.exp(log_resp))
+    inv_cov = 1 / xp.sqrt(covariances)
+    return weights, means, covariances, inv_cov
 
-        for n_iter in six.moves.range(self.max_iter):
-            prev_lower_bound = lower_bound
-            log_prob_norm, log_resp = self.e_step(X)
-            self.m_step(X, log_resp)
-            lower_bound = log_prob_norm
-            change = lower_bound - prev_lower_bound
-            if abs(change) < self.tol:
-                converged = True
-                break
 
-        if not converged:
-            msg = 'Failed to converge. Try different init parameters, \
-                   or increase max_iter, tol or check for degenerate data.'
-            print(msg)
+def train_gmm(X, max_iter, tol):
+    xp = cupy.get_array_module(X)
+    lower_bound = -np.infty
+    converged = False
+    weights = xp.array([0.5, 0.5], dtype=np.float32)
+    mean1 = xp.random.normal(3, xp.array([1, 2]), size=2)
+    mean2 = xp.random.normal(-3, xp.array([2, 1]), size=2)
+    means = xp.stack((mean1, mean2))
+    covariances = xp.random.rand(2, 2)
+    inv_cov = 1 / xp.sqrt(covariances)
 
-    def predict(self, X):
-        log_prob = self.estimate_log_prob(X, self.inv_cov)
-        return (log_prob + self.xp.log(self.weights)).argmax(axis=1)
+    for n_iter in six.moves.range(max_iter):
+        prev_lower_bound = lower_bound
+        log_prob_norm, log_resp = e_step(X, xp, inv_cov, means, weights)
+        weights, means, covariances, inv_cov = m_step(X, xp, log_resp)
+        lower_bound = log_prob_norm
+        change = lower_bound - prev_lower_bound
+        if abs(change) < tol:
+            converged = True
+            break
 
-    def draw(self, X, pred, output):
-        xp = cupy.get_array_module(X)
-        for i in six.moves.range(2):
-            labels = X[pred == i]
-            if xp == cupy:
-                labels = labels.get()
-            plt.scatter(labels[:, 0], labels[:, 1], color=np.random.rand(3, 1))
+    if not converged:
+        msg = 'Failed to converge. Try different init parameters' \
+              'or increase max_iter, tol or check for degenerate data.'
+        print(msg)
+
+    return inv_cov, means, weights, covariances
+
+
+def predict(X, inv_cov, means, weights):
+    xp = cupy.get_array_module(X)
+    log_prob = estimate_log_prob(X, xp, inv_cov, means)
+    return (log_prob + xp.log(weights)).argmax(axis=1)
+
+
+def draw(X, pred, means, covariances, output):
+    xp = cupy.get_array_module(X)
+    for i in six.moves.range(2):
+        labels = X[pred == i]
         if xp == cupy:
-            self.means = self.means.get()
-            self.covariances = self.covariances.get()
-        plt.scatter(self.means[:, 0], self.means[:, 1], s=120, marker='s',
-                    facecolors='y', edgecolors='k')
-        x = np.linspace(-10, 10, 1000)
-        y = np.linspace(-10, 10, 1000)
-        X, Y = np.meshgrid(x, y)
-        for i in six.moves.range(2):
-            Z = mlab.bivariate_normal(X, Y,
-                                      np.sqrt(self.covariances[i][0]),
-                                      np.sqrt(self.covariances[i][1]),
-                                      self.means[i][0], self.means[i][1])
-            plt.contour(X, Y, Z)
-        plt.savefig(output + '.png')
+            labels = labels.get()
+        plt.scatter(labels[:, 0], labels[:, 1], color=np.random.rand(3, 1))
+    if xp == cupy:
+        means = means.get()
+        covariances = covariances.get()
+    plt.scatter(means[:, 0], means[:, 1], s=120, marker='s', facecolors='y',
+                edgecolors='k')
+    x = np.linspace(-10, 10, 1000)
+    y = np.linspace(-10, 10, 1000)
+    X, Y = np.meshgrid(x, y)
+    for i in six.moves.range(2):
+        Z = mlab.bivariate_normal(X, Y, np.sqrt(covariances[i][0]),
+                                  np.sqrt(covariances[i][1]),
+                                  means[i][0], means[i][1])
+        plt.contour(X, Y, Z)
+    plt.savefig(output + '.png')
--- examples/gmm/gmm_train.py
+++ examples/gmm/gmm_train.py
@@ -18,19 +18,19 @@ def timer(message):
     print('%s:  %f sec' % (message, end - start))
 
 
-def run_gmm(X_train, y_train, X_test, y_test, estimator):
+def run_gmm(X_train, y_train, X_test, y_test, max_iter, tol):
     xp = cupy.get_array_module(X_train)
-    estimator.train_gmm(X_train)
-    y_train_pred = estimator.predict(X_train)
+    inv_cov, means, weights, cov = gmm.train_gmm(X_train, max_iter, tol)
+    y_train_pred = gmm.predict(X_train, inv_cov, means, weights)
     train_accuracy = xp.mean(y_train_pred == y_train) * 100
-    y_test_pred = estimator.predict(X_test)
+    y_test_pred = gmm.predict(X_test, inv_cov, means, weights)
     test_accuracy = xp.mean(y_test_pred == y_test) * 100
     print('train_accuracy : %f' % train_accuracy)
     print('test_accuracy : %f' % test_accuracy)
-    return y_test_pred
+    return y_test_pred, means, cov
 
 
-def run(gpuid, max_iter, output):
+def run(gpuid, max_iter, tol, output):
     train1 = np.random.normal(3, [1, 2], size=(500000, 2)).astype(np.float32)
     train2 = np.random.normal(-3, [2, 1], size=(500000, 2)).astype(np.float32)
     X_train = np.r_[train1, train2]
@@ -41,24 +41,23 @@ def run(gpuid, max_iter, output):
     y_test = np.r_[np.zeros(100), np.ones(100)].astype(np.int32)
     repeat = 1
 
-    estimator_cpu = gmm.GaussianMixture(max_iter=max_iter)
     with timer(' CPU '):
         for i in range(repeat):
-            y_test_pred = run_gmm(X_train, y_train, X_test, y_test,
-                                  estimator_cpu)
+            y_test_pred, means, cov = run_gmm(X_train, y_train, X_test, y_test,
+                                              max_iter, tol)
 
     with cupy.cuda.Device(gpuid):
         X_train_gpu = cupy.array(X_train)
         y_train_gpu = cupy.array(y_train)
         y_test_gpu = cupy.array(y_test)
         X_test_gpu = cupy.array(X_test)
-        estimator_gpu = gmm.GaussianMixture(max_iter=max_iter)
         with timer(' GPU '):
             for i in range(repeat):
-                y_test_pred = run_gmm(X_train_gpu, y_train_gpu, X_test_gpu,
-                                      y_test_gpu, estimator_gpu)
+                y_test_pred, means, cov = run_gmm(X_train_gpu, y_train_gpu,
+                                                  X_test_gpu, y_test_gpu,
+                                                  max_iter, tol)
         if output is not None:
-            estimator_gpu.draw(X_test_gpu, y_test_pred, output)
+            gmm.draw(X_test_gpu, y_test_pred, means, cov, output)
 
 
 if __name__ == '__main__':
@@ -67,7 +66,9 @@ if __name__ == '__main__':
                         help='ID of GPU.')
     parser.add_argument('--max-iter', '-m', default=30, type=int,
                         dest='max_iter', help='number of iterations')
-    parser.add_argument('--output', '-o', default=None, type=str,
+    parser.add_argument('--tol', '-t', default=1e-3, type=float,
+                        dest='tol', help='')
+    parser.add_argument('--output-image', '-o', default=None, type=str,
                         dest='output', help='output image file name')
     args = parser.parse_args()
-    run(args.gpuid, args.max_iter, args.output)
+    run(args.gpuid, args.max_iter, args.tol, args.output)
