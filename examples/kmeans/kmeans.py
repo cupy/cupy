2204bdee8d91922a06cf34a0d68ecc44ab7b3830
setoyama <kotaro.setoyama@gmail.com>
2017-05-17 17:50:29 +0900

    combine two files to one

--- examples/kmeans/kmeans.py
+++ examples/kmeans/kmeans.py
@@ -1,11 +1,24 @@
+import argparse
+import contextlib
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import six
+import time
 
 import numpy as np
 
 import cupy
+
+
+@contextlib.contextmanager
+def timer(message):
+    cupy.cuda.Stream.null.synchronize()
+    start = time.time()
+    yield
+    cupy.cuda.Stream.null.synchronize()
+    end = time.time()
+    print('%s:  %f sec' % (message, end - start))
 
 
 def use_custom_kernel(X, n_clusters, max_iter, elem):
@@ -81,3 +94,43 @@ def draw(X, n_clusters, centers, pred, output):
     plt.scatter(centers[:, 0], centers[:, 1], s=120, marker='s',
                 facecolors='y', edgecolors='k')
     plt.savefig(output + '.png')
+
+
+def run(gpuid, n_clusters, max_iter, elem, output):
+    samples = np.random.randn(5000000, 2).astype(np.float32)
+    X_train = np.r_[samples + 1, samples - 1]
+    repeat = 1
+
+    with timer(' CPU '):
+        for i in range(repeat):
+            centers, pred = use_custom_kernel(X_train, n_clusters,
+                                              max_iter, elem)
+
+    with cupy.cuda.Device(gpuid):
+        X_train = cupy.asarray(X_train)
+        with timer(' GPU '):
+            for i in range(repeat):
+                centers, pred = use_custom_kernel(X_train, n_clusters,
+                                                  max_iter, elem)
+        if output is not None:
+            index = np.random.choice(10000000, 300, replace=False)
+            draw(X_train[index], n_clusters, centers, pred[index], output)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu-id', '-g', default=0, type=int, dest='gpuid',
+                        help='ID of GPU.')
+    parser.add_argument('--n-clusters', '-n', default=2, type=int,
+                        dest='n_clusters', help='number of clusters')
+    parser.add_argument('--maxiter', '-m', default=10, type=int,
+                        dest='max_iter', help='number of iterations')
+    parser.add_argument('--elem', action='store_true', default=False,
+                        help='use Elementwise kernel')
+    parser.add_argument('--output-image', '-o', default=None, type=str,
+                        dest='output', help='output image file name')
+    args = parser.parse_args()
+    if args.n_clusters != 2 and args.elem:
+        msg = 'Can use Elementwise Kernel only when n-clusters is 2'
+        raise ValueError(msg)
+    run(args.gpuid, args.n_clusters, args.max_iter, args.elem, args.output)
