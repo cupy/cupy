f0f352a91f07e48295807e10090033288b93bc55
KotaroSetoyama <kotaro.setoyama@gmail.com>
2017-06-05 16:59:24 +0900

    fix initial matrix A

--- examples/cg/cg.py
+++ examples/cg/cg.py
@@ -3,6 +3,8 @@ import contextlib
 import time
 
 import numpy as np
+from scipy.sparse.linalg import cg
+from scipy.sparse import rand
 import six
 
 import cupy
@@ -25,7 +27,7 @@ def fit(A, b, x0, tol, max_iter):
     p = r0
     for i in six.moves.range(max_iter):
         a = xp.dot(r0.T, r0) / xp.dot(xp.dot(p.T, A), p)
-        x += p * a
+        x = x + p * a
         r1 = r0 - xp.dot(A * a, p)
         if xp.linalg.norm(r1) < tol:
             return x
@@ -38,39 +40,56 @@ def fit(A, b, x0, tol, max_iter):
 
 
 def run(gpuid, tol, max_iter):
-    N = 1000
-    max_val = 100
-    A = np.random.randint(max_val, size=(N, N), dtype=np.float32)
-    A = A + A.T
-    b = np.random.randint(max_val, size=N).astype(np.float32)
-    x0 = np.zeros(N, dtype=np.float32)
-    print('b =')
-    print(b[:10])
+    # Solve simultaneous linear equations, Ax = b.
+    for repeat in range(1):
+        print("Trial: %d" % repeat)
+        # create the large sparse symmetric matrix 'A'.
+        N = 2000
+        max_val = 50
+        density = 0.3
+        A = rand(N, N, density / 2).A
+        ran = np.random.randint(max_val, size=(N, N))
+        A *= ran
+        A = (A + A.T).astype(np.int32)
+        b = rand(1, N, density).A.reshape((N),)
+        ran = np.random.randint(max_val, size=N)
+        b *= ran
+        b = b.astype(np.int32)
+        x0 = np.zeros(N, dtype=np.int32)
 
-    with timer(' CPU '):
-        print()
-        x = fit(A, b, x0, tol, max_iter)
-        b_calc = np.dot(A, x)
-        print(b_calc[:10])
+        msg = 'b[:20]='
+        print(msg)
+        print(b[:20])
 
-    with cupy.cuda.Device(gpuid):
-        A = cupy.asarray(A)
-        b = cupy.asarray(b)
-        x0 = cupy.asarray(x0)
-        with timer(' GPU '):
-            print()
+        with timer(' CPU '):
             x = fit(A, b, x0, tol, max_iter)
-            b_calc = cupy.dot(A, x)
-            print(b_calc[:10])
+            b_calc = np.dot(A, x)
+            print(np.rint(b_calc[:20]).astype(np.int32))
+
+        with cupy.cuda.Device(gpuid):
+            A_gpu = cupy.asarray(A)
+            b_gpu = cupy.asarray(b)
+            x0_gpu = cupy.asarray(x0)
+            with timer(' GPU '):
+                x = fit(A_gpu, b_gpu, x0_gpu, tol, max_iter)
+                b_calc = cupy.dot(A_gpu, x)
+                print(cupy.rint(b_calc[:20]).astype(np.int32))
+
+        with timer(' SciPy '):
+            x = cg(A, b)
+            b_calc = np.dot(A, x[0])
+            print(np.rint(b_calc[:20]).astype(np.int32))
+
+        print()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu-id', '-g', default=0, type=int,
                         help='ID of GPU.')
-    parser.add_argument('--tol', '-t', default=1.0e-5, type=float,
+    parser.add_argument('--tol', '-t', default=5.0, type=float,
                         help='tolerance to stop iteration')
-    parser.add_argument('--max-iter', '-m', default=100000000, type=int,
+    parser.add_argument('--max-iter', '-m', default=5000, type=int,
                         help='number of iterations')
     args = parser.parse_args()
     run(args.gpu_id, args.tol, args.max_iter)
