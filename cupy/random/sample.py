4b5b0e690ed374ab3b1e65df253b44b4c90dafe3
Yuya Unno <unnonouno@gmail.com>
2017-06-08 02:08:39 +0900

    Fix docstring

--- cupy/random/sample.py
+++ cupy/random/sample.py
@@ -175,12 +175,12 @@ def multinomial(n, pvals, size=None):
 
     Args:
         n (int): Number of trials.
-        pvals (cupy.ndarray): Probabilities of `p` outcomes. The sum of this
-            values must be 1.
+        pvals (cupy.ndarray): Probabilities of each of the `p` different
+            outcomes. The sum of these values must be 1.
         size (int or tuple of ints or None): Shape of a sample in each trial.
             For example when `size` is `(a, b)`, shape of returned value is
             `(a, b, p)` where `p` is `len(pvals)`.
-            If `size` is `None`, it is treated as `()`. So, shape of retured
+            If `size` is `None`, it is treated as `()`. So, shape of returned
             value is `(p,)`.
 
     Returns:
