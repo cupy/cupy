4448148cad69176274d4d9e2314aabe2a2fd6942
Yuya Unno <unnonouno@gmail.com>
2017-06-08 02:13:11 +0900

    Fix docstring

--- cupy/random/sample.py
+++ cupy/random/sample.py
@@ -175,16 +175,19 @@ def multinomial(n, pvals, size=None):
 
     Args:
         n (int): Number of trials.
-        pvals (cupy.ndarray): Probabilities of each of the `p` different
+        pvals (cupy.ndarray): Probabilities of each of the ``p`` different
             outcomes. The sum of these values must be 1.
         size (int or tuple of ints or None): Shape of a sample in each trial.
-            For example when `size` is `(a, b)`, shape of returned value is
-            `(a, b, p)` where `p` is `len(pvals)`.
-            If `size` is `None`, it is treated as `()`. So, shape of returned
-            value is `(p,)`.
+            For example when ``size`` is ``(a, b)``, shape of returned value is
+            ``(a, b, p)`` where ``p`` is ``len(pvals)``.
+            If ``size`` is ``None``, it is treated as ``()``. So, shape of
+            returned value is ``(p,)``.
 
     Returns:
         cupy.ndarray: An array drawn from multinomial distribution.
+
+    .. note::
+       It does not support ``sum(pvals) < 1`` case.
 
     .. seealso:: :func:`numpy.random.multinomial`
     """
