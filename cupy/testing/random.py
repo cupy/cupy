7096260df671c14a1a80745774b5e799a3ce3864
niboshi <niboshi000@gmail.com>
2017-06-19 10:16:11 +0900

    Control test random determinacy with environment variable

--- cupy/testing/random.py
+++ cupy/testing/random.py
@@ -1,3 +1,4 @@
+import os
 import atexit
 import numpy
 
@@ -8,6 +9,9 @@ import cupy
 # setUp/tearDown is nested. setup_random() and teardown_random() do their
 # work only in the outermost setUp/tearDown pair.
 _nest_count = 0
+
+_old_numpy_random_state = None
+_old_cupy_random_states = None
 
 
 @atexit.register
@@ -21,9 +25,26 @@ def setup_random():
 
     """
     global _nest_count
+    global _old_numpy_random_state
+    global _old_cupy_random_states
     if _nest_count == 0:
-        numpy.random.seed(100)
-        cupy.random.seed(101)
+        _old_numpy_random_state = numpy.random.get_state()
+        _old_cupy_random_states = cupy.random.generator._random_states
+        cupy.random.reset_states()
+        # Check that _random_state has been recreated in
+        # cupy.random.reset_states(). Otherwise the contents of
+        # _old_cupy_random_states would be overwritten.
+        assert (cupy.random.generator._random_states is not
+                _old_cupy_random_states)
+
+        nondeterministic = bool(int(os.environ.get(
+            'CUPY_TEST_RANDOM_NONDETERMINISTIC', '0')))
+        if nondeterministic:
+            numpy.random.seed()
+            cupy.random.seed()
+        else:
+            numpy.random.seed(100)
+            cupy.random.seed(101)
     _nest_count += 1
 
 
@@ -32,8 +53,12 @@ def teardown_random():
 
     """
     global _nest_count
+    global _old_numpy_random_state
+    global _old_cupy_random_states
     assert _nest_count > 0
     _nest_count -= 1
     if _nest_count == 0:
-        numpy.random.seed(None)
-        cupy.random.reset_states()
+        numpy.random.set_state(_old_numpy_random_state)
+        cupy.random.generator._random_states = _old_cupy_random_states
+        _old_numpy_random_state = None
+        _old_cupy_random_states = None
