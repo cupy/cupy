a331d12a916dbc9dac2f91b23930317235b09cc9
Yuya Unno <unnonouno@gmail.com>
2017-06-21 00:55:58 +0900

    Skip test when scipy is not installed

--- tests/cupy_tests/sparse_tests/test_base.py
+++ tests/cupy_tests/sparse_tests/test_base.py
@@ -1,20 +1,25 @@
 import unittest
 
-import scipy.sparse
+try:
+    import scipy.sparse
+    scipy_available = True
+except ImportError:
+    scipy_available = False
 
 import cupy.sparse
 from cupy import testing
 
 
-class DummySparseCPU(scipy.sparse.spmatrix):
+if scipy_available:
+    class DummySparseCPU(scipy.sparse.spmatrix):
 
-    def __init__(self, maxprint=50, shape=None, nnz=0):
-        super(DummySparseCPU, self).__init__(maxprint)
-        self._shape = shape
-        self._nnz = nnz
+        def __init__(self, maxprint=50, shape=None, nnz=0):
+            super(DummySparseCPU, self).__init__(maxprint)
+            self._shape = shape
+            self._nnz = nnz
 
-    def getnnz(self):
-        return self._nnz
+        def getnnz(self):
+            return self._nnz
 
 
 class DummySparseGPU(cupy.sparse.spmatrix):
@@ -31,13 +36,14 @@ class DummySparseGPU(cupy.sparse.spmatrix):
         return self._nnz
 
 
-dummies = {
-    scipy.sparse: DummySparseCPU,
-    cupy.sparse: DummySparseGPU,
-}
-
-
+@testing.with_requires('scipy')
 class TestSpmatrix(unittest.TestCase):
+
+    def dummy_class(self, sp):
+        if sp is cupy.sparse:
+            return DummySparseGPU
+        else:
+            return DummySparseCPU
 
     @testing.numpy_cupy_raises(sp_name='sp', accept_error=ValueError)
     def test_instantiation(self, xp, sp):
@@ -45,30 +51,30 @@ class TestSpmatrix(unittest.TestCase):
 
     @testing.numpy_cupy_raises(sp_name='sp', accept_error=TypeError)
     def test_len(self, xp, sp):
-        s = dummies[sp]()
+        s = self.dummy_class(sp)()
         len(s)
 
     @testing.numpy_cupy_equal(sp_name='sp')
     def test_bool_true(self, xp, sp):
-        s = dummies[sp](shape=(1, 1), nnz=1)
+        s = self.dummy_class(sp)(shape=(1, 1), nnz=1)
         return bool(s)
 
     @testing.numpy_cupy_equal(sp_name='sp')
     def test_bool_false(self, xp, sp):
-        s = dummies[sp](shape=(1, 1), nnz=0)
+        s = self.dummy_class(sp)(shape=(1, 1), nnz=0)
         return bool(s)
 
     @testing.numpy_cupy_raises(sp_name='sp', accept_error=ValueError)
     def test_bool_invalid(self, xp, sp):
-        s = dummies[sp](shape=(2, 1))
+        s = self.dummy_class(sp)(shape=(2, 1))
         bool(s)
 
     @testing.numpy_cupy_equal(sp_name='sp')
     def test_asformat_none(self, xp, sp):
-        s = dummies[sp]()
+        s = self.dummy_class(sp)()
         self.assertIs(s.asformat(None), s)
 
     @testing.numpy_cupy_equal(sp_name='sp')
     def test_maxprint(self, xp, sp):
-        s = dummies[sp](maxprint=30)
+        s = self.dummy_class(sp)(maxprint=30)
         return s.getmaxprint()
