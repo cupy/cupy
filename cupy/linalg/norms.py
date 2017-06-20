2b7ee5757d90948422db09916b1b404bccecbf03
Yuya Unno <unnonouno@gmail.com>
2017-06-20 17:53:57 +0900

    Update docstring

--- cupy/linalg/norms.py
+++ cupy/linalg/norms.py
@@ -163,14 +163,15 @@ def slogdet(a):
         a (cupy.ndarray): The input matrix with dimension ``(..., N, N)``.
 
     Returns:
-        tuple of ~cupy.ndarray:
-            It returns a tuple `(sign, logdet)`. `sign` represents each sign
-            of the deteminant as a real number `0`, `1` or `-1`.
+        tuple of :class:`~cupy.ndarray`:
+            It returns a tuple ``(sign, logdet)``. ``sign`` represents each
+            sign of the deteminant as a real number ``0``, ``1`` or ``-1``.
             'logdet' represents the natural logarithm of the absolute of the
             deteminant.
-            If the deteninant is zero, `sign` will be `0` and `logdet` will be
-            `-inf`.
-            The shapes of both `sign` and `logdet` are equal to `a.shape[:-2]`.
+            If the deteninant is zero, ``sign`` will be ``0`` and ``logdet``
+            will be ``-inf``.
+            The shapes of both ``sign`` and ``logdet`` are equal to
+            ``a.shape[:-2]``.
 
     .. seealso:: :func:`numpy.linalg.slogdet`
     """
