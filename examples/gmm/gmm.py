ce76d5c6b19a3f8257266a3b6ebd615dc0eef153
setoyama <kotaro.setoyama@gmail.com>
2017-06-20 17:45:10 +0900

    fix

--- examples/gmm/gmm.py
+++ examples/gmm/gmm.py
@@ -22,16 +22,8 @@ def timer(message):
     print('%s:  %f sec' % (message, end - start))
 
 
-def estimate_gaussian_parameters(X, xp, resp):
-    nk = xp.sum(resp, axis=0)
-    means = xp.dot(resp.T, X) / nk[:, None]
-    avg_X = xp.dot(resp.T, X * X) / nk[:, None]
-    avg_X_means = means * xp.dot(resp.T, X) / nk[:, None]
-    covariances = avg_X - 2 * avg_X_means + means ** 2
-    return nk / len(X), means, covariances
-
-
-def estimate_log_prob(X, xp, inv_cov, means):
+def estimate_log_prob(X, inv_cov, means):
+    xp = cupy.get_array_module(X)
     n_features = X.shape[1]
     log_det = xp.sum(xp.log(inv_cov), axis=1)
     precisions = inv_cov ** 2
@@ -40,19 +32,22 @@ def estimate_log_prob(X, xp, inv_cov, means):
     return -0.5 * (n_features * xp.log(2 * np.pi) + log_prob) + log_det
 
 
-def e_step(X, xp, inv_cov, means, weights):
-    weighted_log_prob = estimate_log_prob(X, xp, inv_cov, means) + \
+def m_step(X, resp):
+    xp = cupy.get_array_module(X)
+    nk = xp.sum(resp, axis=0)
+    means = xp.dot(resp.T, X) / nk[:, None]
+    X2 = xp.dot(resp.T, X * X) / nk[:, None]
+    covariances = X2 - means ** 2
+    return nk / len(X), means, covariances
+
+
+def e_step(X, inv_cov, means, weights):
+    xp = cupy.get_array_module(X)
+    weighted_log_prob = estimate_log_prob(X, inv_cov, means) + \
         xp.log(weights)
     log_prob_norm = xp.log(xp.sum(xp.exp(weighted_log_prob), axis=1))
     log_resp = weighted_log_prob - log_prob_norm[:, None]
     return xp.mean(log_prob_norm), log_resp
-
-
-def m_step(X, xp, log_resp):
-    weights, means, covariances = \
-        estimate_gaussian_parameters(X, xp, xp.exp(log_resp))
-    inv_cov = 1 / xp.sqrt(covariances)
-    return weights, means, covariances, inv_cov
 
 
 def train_gmm(X, max_iter, tol):
@@ -62,14 +57,15 @@ def train_gmm(X, max_iter, tol):
     weights = xp.array([0.5, 0.5], dtype=np.float32)
     mean1 = xp.random.normal(0, xp.array([1, 2]), size=2)
     mean2 = xp.random.normal(-3, xp.array([2, 1]), size=2)
-    means = xp.stack((mean1, mean2))
+    means = xp.c_[mean1, mean2]
     covariances = xp.random.rand(2, 2)
     inv_cov = 1 / xp.sqrt(covariances)
 
     for n_iter in six.moves.range(max_iter):
         prev_lower_bound = lower_bound
-        log_prob_norm, log_resp = e_step(X, xp, inv_cov, means, weights)
-        weights, means, covariances, inv_cov = m_step(X, xp, log_resp)
+        log_prob_norm, log_resp = e_step(X, inv_cov, means, weights)
+        weights, means, covariances = m_step(X, xp.exp(log_resp))
+        inv_cov = 1 / xp.sqrt(covariances)
         lower_bound = log_prob_norm
         change = lower_bound - prev_lower_bound
         if abs(change) < tol:
@@ -77,15 +73,14 @@ def train_gmm(X, max_iter, tol):
             break
 
     if not converged:
-        msg = 'Failed to converge. Increase max-iter or tol.'
-        print(msg)
+        print('Failed to converge. Increase max-iter or tol.')
 
     return inv_cov, means, weights, covariances
 
 
 def predict(X, inv_cov, means, weights):
     xp = cupy.get_array_module(X)
-    log_prob = estimate_log_prob(X, xp, inv_cov, means)
+    log_prob = estimate_log_prob(X, inv_cov, means)
     return (log_prob + xp.log(weights)).argmax(axis=1)
 
 
@@ -105,10 +100,10 @@ def draw(X, pred, means, covariances, output):
     xp = cupy.get_array_module(X)
     for i in six.moves.range(2):
         labels = X[pred == i]
-        if xp == cupy:
+        if xp is cupy:
             labels = labels.get()
-        plt.scatter(labels[:, 0], labels[:, 1], color=np.random.rand(3, 1))
-    if xp == cupy:
+        plt.scatter(labels[:, 0], labels[:, 1], c=np.random.rand(3))
+    if xp is cupy:
         means = means.get()
         covariances = covariances.get()
     plt.scatter(means[:, 0], means[:, 1], s=120, marker='s', facecolors='y',
@@ -135,8 +130,9 @@ def run(gpuid, max_iter, tol, output):
     y_test = np.r_[np.zeros(100), np.ones(100)].astype(np.int32)
     repeat = 1
 
+    print('Running CPU...')
     with timer(' CPU '):
-        for i in range(repeat):
+        for i in six.moves.range(repeat):
             y_test_pred, means, cov = calc_acc(X_train, y_train, X_test,
                                                y_test, max_iter, tol)
 
@@ -145,13 +141,16 @@ def run(gpuid, max_iter, tol, output):
         y_train_gpu = cupy.array(y_train)
         y_test_gpu = cupy.array(y_test)
         X_test_gpu = cupy.array(X_test)
+        print('Running GPU...')
         with timer(' GPU '):
-            for i in range(repeat):
+            for i in six.moves.range(repeat):
                 y_test_pred, means, cov = calc_acc(X_train_gpu, y_train_gpu,
                                                    X_test_gpu, y_test_gpu,
                                                    max_iter, tol)
         if output is not None:
             draw(X_test_gpu, y_test_pred, means, cov, output)
+
+    print()
 
 
 if __name__ == '__main__':
