33e8656b8cffb9e091741068d5014bf276de5c48
Masaki Saito <msaito@preferred.jp>
2017-06-26 15:00:19 +0900

    Implement cupy.linalg.solve

--- cupy/linalg/decomposition.py
+++ cupy/linalg/decomposition.py
@@ -5,6 +5,11 @@ import cupy
 from cupy import cuda
 from cupy.cuda import cublas
 from cupy.cuda import device
+from cupy.linalg.util import _assert_cupy_array
+from cupy.linalg.util import _assert_nd_squareness
+from cupy.linalg.util import _assert_rank2
+from cupy.linalg.util import _tril
+from cupy.linalg.util import _triu
 
 if cuda.cusolver_enabled:
     from cupy.cuda import cusolver
@@ -263,43 +268,3 @@ def svd(a, full_matrices=True, compute_uv=True):
             return vt, s, u
     else:
         return s
-
-
-def _assert_cupy_array(*arrays):
-    for a in arrays:
-        if not isinstance(a, cupy.core.ndarray):
-            raise linalg.LinAlgError(
-                'cupy.linalg only supports cupy.core.ndarray')
-
-
-def _assert_rank2(*arrays):
-    for a in arrays:
-        if a.ndim != 2:
-            raise linalg.LinAlgError(
-                '{}-dimensional array given. Array must be '
-                'two-dimensional'.format(a.ndim))
-
-
-def _assert_nd_squareness(*arrays):
-    for a in arrays:
-        if max(a.shape[-2:]) != min(a.shape[-2:]):
-            raise linalg.LinAlgError(
-                'Last 2 dimensions of the array must be square')
-
-
-def _tril(x, k=0):
-    m, n = x.shape
-    u = cupy.arange(m).reshape(m, 1)
-    v = cupy.arange(n).reshape(1, n)
-    mask = v - u <= k
-    x *= mask
-    return x
-
-
-def _triu(x, k=0):
-    m, n = x.shape
-    u = cupy.arange(m).reshape(m, 1)
-    v = cupy.arange(n).reshape(1, n)
-    mask = v - u >= k
-    x *= mask
-    return x
--- cupy/linalg/solve.py
+++ cupy/linalg/solve.py
@@ -1,7 +1,92 @@
-# flake8: NOQA
-# "flake8: NOQA" to suppress warning "H104  File contains nothing but comments"
+import numpy
+from numpy import linalg
 
-# TODO(okuta): Implement solve
+import cupy
+from cupy import cuda
+from cupy.cuda import cublas
+from cupy.cuda import device
+from cupy.linalg.util import _assert_cupy_array
+from cupy.linalg.util import _assert_nd_squareness
+from cupy.linalg.util import _assert_rank2
+
+if cuda.cusolver_enabled:
+    from cupy.cuda import cusolver
+
+
+def solve(a, b):
+    # NOTE: Since cusolver in CUDA 8.0 does not support gesv,
+    #       we manually solve a linear system with QR decomposition.
+    #       For details, please see the following:
+    #       http://docs.nvidia.com/cuda/cusolver/index.html#qr_examples
+    if not cuda.cusolver_enabled:
+        raise RuntimeError('Current cupy only supports cusolver in CUDA 8.0')
+
+    # TODO(Saito): Current implementation only accepts two-dimensional arrays
+    _assert_cupy_array(a, b)
+    _assert_rank2(a)
+    _assert_nd_squareness(a)
+    if 2 < b.ndim:
+        raise linalg.LinAlgError(
+            '{}-dimensional array given. Array must be '
+            'one or two-dimensional'.format(b.ndim))
+    if a.shape[0] != b.shape[0]:
+        raise linalg.LinAlgError(
+            'The number of rows of array a must be '
+            'the same as that of array b')
+
+    # Cast to float32 or float64
+    if a.dtype.char == 'f' or a.dtype.char == 'd':
+        dtype = a.dtype.char
+    else:
+        dtype = numpy.find_common_type((a.dtype.char, 'f'), ()).char
+
+    if b.ndim == 1:
+        b = b[:, None]
+    m, k = b.shape
+    a = a.transpose().astype(dtype, order='C', copy=True)
+    b = b.transpose().astype(dtype, order='C', copy=True)
+    cusolver_handle = device.get_cusolver_handle()
+    cublas_handle = device.get_cublas_handle()
+    dev_info = cupy.empty(1, dtype=numpy.int32)
+
+    if dtype == 'f':
+        geqrf = cusolver.sgeqrf
+        geqrf_bufferSize = cusolver.sgeqrf_bufferSize
+        ormqr = cusolver.sormqr
+        trsm = cublas.strsm
+    else:  # dtype == 'd'
+        geqrf = cusolver.dgeqrf
+        geqrf_bufferSize = cusolver.dgeqrf_bufferSize
+        ormqr = cusolver.dormqr
+        trsm = cublas.dtrsm
+
+    # 1. QR decomposition (A = Q * R)
+    buffersize = geqrf_bufferSize(cusolver_handle, m, m, a.data.ptr, m)
+    workspace = cupy.empty(buffersize, dtype=dtype)
+    tau = cupy.empty(m, dtype=dtype)
+    geqrf(
+        cusolver_handle, m, m, a.data.ptr, m,
+        tau.data.ptr, workspace.data.ptr, buffersize, dev_info.data.ptr)
+    _check_status(dev_info)
+    # 2. ormqr (Q^T * B)
+    ormqr(
+        cusolver_handle, cublas.CUBLAS_SIDE_LEFT, cublas.CUBLAS_OP_T,
+        m, k, m, a.data.ptr, m, tau.data.ptr, b.data.ptr, m,
+        workspace.data.ptr, buffersize, dev_info.data.ptr)
+    _check_status(dev_info)
+    # 3. trsm (X = R^{-1} * (Q^T * B))
+    trsm(
+        cublas_handle, cublas.CUBLAS_SIDE_LEFT, cublas.CUBLAS_FILL_MODE_UPPER,
+        cublas.CUBLAS_OP_N, cublas.CUBLAS_DIAG_NON_UNIT,
+        m, k, 1, a.data.ptr, m, b.data.ptr, m)
+    return b
+
+
+def _check_status(dev_info):
+    status = int(dev_info[0])
+    if status < 0:
+        raise linalg.LinAlgError(
+            'Parameter error (maybe caused by a bug in cupy.linalg?)')
 
 
 # TODO(okuta): Implement tensorsolve
