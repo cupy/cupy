228694c9f6a8f929c18b8f0c0dafbadf6c43989e
Ryosuke Okuta <okuta@preferred.jp>
2017-07-14 01:20:37 +0900

    Fix create_math_ufunc

--- cupy/math/ufunc.py
+++ cupy/math/ufunc.py
@@ -5,7 +5,7 @@ def create_math_ufunc(math_name, nargs, name, doc):
     assert 1 <= nargs <= 2
     if nargs == 1:
         return core.create_ufunc(
-            name, ('ee->e', 'ff->f', 'dd->d', 'FF->F', 'DD->D'),
+            name, ('e->e', 'f->f', 'd->d', 'F->F', 'D->D'),
             'out0 = %s(in0)' % math_name, doc=doc)
     else:
         return core.create_ufunc(
