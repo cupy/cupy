913ba157fc067a47827e42870baf51cd32f8a85d
Yuya Unno <unnonouno@gmail.com>
2017-07-24 20:02:30 +0900

    Fix import

--- cupy/sparse/csr.py
+++ cupy/sparse/csr.py
@@ -4,7 +4,9 @@ try:
 except ImportError:
     _scipy_available = False
 
+import cupy
 from cupy import cusparse
+from cupy.sparse import base
 from cupy.sparse import compressed
 from cupy.sparse import csc
 
