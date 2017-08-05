6ae9d2210a57001035bc7256a3af27008fe30245
Yuya Unno <unnonouno@gmail.com>
2017-08-06 01:02:45 +0900

    Flake8

--- cupy/sparse/compressed.py
+++ cupy/sparse/compressed.py
@@ -161,8 +161,8 @@ __device__ double atomicAdd(double* address, double val) {
     unsigned long long int old = *address_as_ull, assumed;
     do {
         assumed = old;
-        old = atomicCAS(address_as_ull, assumed, 
-                        __double_as_longlong(val + 
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
                         __longlong_as_double(assumed)));
     } while (assumed != old);
     return __longlong_as_double(old);
