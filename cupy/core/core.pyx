078efee53cf2656b872f8c46e782214ad3aca39b
Ryosuke Okuta <okuta@preferred.jp>
2017-08-07 16:34:02 +0900

    Move IndexOrValueError

--- cupy/core/core.pyx
+++ cupy/core/core.pyx
@@ -36,14 +36,14 @@ cdef inline _should_use_rop(x, y):
     return xp < yp and not isinstance(y, ndarray)
 
 
-class IndexOrValueError(IndexError, ValueError):
-
-    def __init__(self, *args, **kwargs):
-        super(IndexOrValueError, self).__init__(*args, **kwargs)
-
 try:
     _AxisError = numpy.AxisError
 except AttributeError:
+    class IndexOrValueError(IndexError, ValueError):
+
+        def __init__(self, *args, **kwargs):
+            super(IndexOrValueError, self).__init__(*args, **kwargs)
+
     _AxisError = IndexOrValueError
 
 
