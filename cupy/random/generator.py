8ed6cb881e0c0c8eee771951b96dd1a9a111a746
Yuya Unno <unnonouno@gmail.com>
2017-08-09 23:00:37 +0900

    Implement tomaxint

--- cupy/random/generator.py
+++ cupy/random/generator.py
@@ -223,6 +223,29 @@ class RandomState(object):
         """
         return self.normal(size=size, dtype=dtype)
 
+    def tomaxint(self, size=None):
+        """Draws integers between 0 and max integer inclusive.
+
+        Args:
+            size (int or tuple of ints): Output shape.
+
+        Returns:
+            Drawn samples.
+
+        .. seealso::
+            :meth:`numpy.random.RandomState.tomaxint`
+
+        """
+        if size is None:
+            size = ()
+        sample = cupy.empty(size, dtype='l')
+        # cupy.random only uses int32 random generator
+        curand.generate(
+            self._generator, sample.data.ptr, sample.size * 2)
+        # Disable sign bit
+        sample &= 0x7FFFFFFFFFFFFFFF
+        return sample
+
     def uniform(self, low=0.0, high=1.0, size=None, dtype=float):
         """Returns an array of uniformly-distributed samples over an interval.
 
