691adc13c71927771e2f5fc6d80176f461e28af0
Masayuki Takagi <kamonama@gmail.com>
2017-08-19 17:21:54 +0900

    Fix variable name.

--- cupy/core/core.pyx
+++ cupy/core/core.pyx
@@ -919,13 +919,13 @@ cdef class ndarray:
         if not (0 <= axis < ndim):
             raise ValueError('Axis out of range')
 
-        len = data.shape[axis]
+        length = data.shape[axis]
         kth = (kth,) if isinstance(kth, int) else kth
         for k in kth:
             if k < 0:
-                k += len
-            if not (0 <= k < len):
-                raise ValueError('kth(={}) out of bounds {}'.format(k, len))
+                k += length
+            if not (0 <= k < length):
+                raise ValueError('kth(={}) out of bounds {}'.format(k, length))
 
         # kth is ignored.
         return cupy.argsort(data, axis=axis)
