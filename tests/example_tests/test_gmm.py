bc3bcc576932e3b8d80bd904a674282759082382
kmatsuura <k-matsuura@flatcorp.jp>
2017-08-21 19:12:22 +0900

    Fix test and flake8

--- tests/example_tests/test_gmm.py
+++ tests/example_tests/test_gmm.py
@@ -17,14 +17,14 @@ class TestGMM(unittest.TestCase):
         output = example_test.run_example('gmm/gmm.py', '--num', '10')
         six.assertRegex(
             self, output.decode('utf-8'),
-            r'''Running CPU\.\.\.''' + os.linesep +
-            r'''train_accuracy : [0-9\.]+''' + os.linesep +
-            r'''test_accuracy : [0-9\.]+''' + os.linesep +
-            r''' CPU :  [0-9\.]+ sec''' + os.linesep +
-            r'''Running GPU\.\.\.''' + os.linesep +
-            r'''train_accuracy : [0-9\.]+''' + os.linesep +
-            r'''test_accuracy : [0-9\.]+''' + os.linesep +
-            r''' GPU :  [0-9\.]+ sec''' + os.linesep)
+            os.linesep.join([r'''Running CPU\.\.\.''',
+                             r'''train_accuracy : [0-9\.]+''',
+                             r'''test_accuracy : [0-9\.]+''',
+                             r''' CPU :  [0-9\.]+ sec''',
+                             r'''Running GPU\.\.\.''',
+                             r'''train_accuracy : [0-9\.]+''',
+                             r'''test_accuracy : [0-9\.]+''',
+                             r''' GPU :  [0-9\.]+ sec''']))
 
     def test_output_image(self):
         dir_path = tempfile.mkdtemp()
