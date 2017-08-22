ad161395c8e2aff472b957caaaa9f4cfc638cd67
kmatsuura <k-matsuura@flatcorp.jp>
2017-08-22 12:01:52 +0900

    Replace os.linesep with \s+

--- tests/example_tests/test_kmeans.py
+++ tests/example_tests/test_kmeans.py
@@ -18,8 +18,7 @@ class TestKmeans(unittest.TestCase):
             'kmeans/kmeans.py', '-m', '1', '--num', '10')
         six.assertRegex(
             self, output.decode('utf-8'),
-            r''' CPU :  [0-9\.]+ sec''' + os.linesep +
-            r''' GPU :  [0-9\.]+ sec''')
+            ''' CPU :  [0-9\\.]+ sec\s+GPU :  [0-9\\.]+ sec''')
 
     def test_custom_kernel(self):
         output = example_test.run_example(
@@ -27,8 +26,7 @@ class TestKmeans(unittest.TestCase):
             '--use-custom-kernel')
         six.assertRegex(
             self, output.decode('utf-8'),
-            r''' CPU :  [0-9\.]+ sec''' + os.linesep +
-            r''' GPU :  [0-9\.]+ sec''')
+            ''' CPU :  [0-9\\.]+ sec\s+GPU :  [0-9\\.]+ sec''')
 
     def test_result_image(self):
         dir_path = tempfile.mkdtemp()
