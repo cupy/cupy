b768bf5f03cb25ad98198d36d8eaa8ebd56a3eb8
niboshi <niboshi000@gmail.com>
2017-09-05 19:21:12 +0900

    Revert tests in commit 91d321c

--- tests/cupy_tests/logic_tests/test_type_test.py
+++ tests/cupy_tests/logic_tests/test_type_test.py
@@ -1,23 +1,9 @@
-import numpy
+import unittest
 
 from cupy import testing
 
 
-class TestBinaryRepr(testing.NumpyAliasBasicTestBase):
+@testing.gpu
+class TestTypeTest(unittest.TestCase):
 
-    func = 'isscalar'
-
-
-@testing.parameterize(
-    *testing.product({
-        'value': [
-            0, 0.0, True,
-            numpy.int32(1), numpy.array([1, 2], numpy.int32),
-            numpy.complex(1), numpy.complex(1j), numpy.complex(1 + 1j),
-            None, object(), 'abc', '', int, numpy.int32]}))
-class TestBinaryReprValues(testing.NumpyAliasValuesTestBase):
-
-    func = 'isscalar'
-
-    def setUp(self):
-        self.args = (self.value,)
+    _multiprocess_can_split_ = True
