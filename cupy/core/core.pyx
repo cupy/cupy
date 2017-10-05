166dd30f0baf8eda990105f3a37d8896bc05cf8d
Masayuki Takagi <kamonama@gmail.com>
2017-10-05 12:31:10 +0900

    Use _AxisError.

--- cupy/core/core.pyx
+++ cupy/core/core.pyx
@@ -917,7 +917,7 @@ cdef class ndarray:
         if axis < 0:
             axis += ndim
         if not (0 <= axis < ndim):
-            raise ValueError('Axis out of range')
+            raise _AxisError('Axis out of range')
 
         length = data.shape[axis]
         if isinstance(kth, int):
