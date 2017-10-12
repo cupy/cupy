ee28ede60ad70c0e23edd743fc9b4ff83c13dfaa
ishihara1989 <ashitahamatsuri@gmail.com>
2017-10-12 23:35:49 +0900

    use parameterized test

--- tests/cupy_tests/math_tests/test_window.py
+++ tests/cupy_tests/math_tests/test_window.py
@@ -3,42 +3,16 @@ import unittest
 from cupy import testing
 
 
-@testing.gpu
+@testing.parameterize(
+    *testing.product({
+        'm': [0, 1, -1, 1024],
+        'name': ['blackman', 'hamming', 'hanning'],
+    })
+)
 class TestWindow(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
     @testing.numpy_cupy_allclose(atol=1e-5)
-    def check_0(self, name, xp):
-        a = 0
-        return getattr(xp, name)(a)
-
-    @testing.numpy_cupy_allclose(atol=1e-5)
-    def check_1(self, name, xp):
-        a = 1
-        return getattr(xp, name)(a)
-
-    @testing.numpy_cupy_allclose(atol=1e-5)
-    def check_negative(self, name, xp):
-        a = -1
-        return getattr(xp, name)(a)
-
-    @testing.numpy_cupy_allclose(atol=1e-5)
-    def check_large(self, name, xp):
-        a = 1024
-        return getattr(xp, name)(a)
-
-    def check_all(self, name):
-        self.check_0(name)
-        self.check_1(name)
-        self.check_negative(name)
-        self.check_large(name)
-
-    def test_blackman(self):
-        self.check_all('blackman')
-
-    def test_hamming(self):
-        self.check_all('hamming')
-
-    def test_hanning(self):
-        self.check_all('hanning')
+    def test_window(self, xp):
+        return getattr(xp, self.name)(self.m)
