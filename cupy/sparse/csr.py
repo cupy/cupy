b25947f39084634f7f8ec01b0d813e97e292cf5e
Yuya Unno <unnonouno@gmail.com>
2017-10-15 04:16:06 +0900

    Use coo_matrix.eliminate_zeros in csr_matrix.eliminate_zeros when cuda is old

--- cupy/sparse/csr.py
+++ cupy/sparse/csr.py
@@ -137,7 +137,13 @@ class csr_matrix(compressed._compressed_sparse_matrix):
 
     def eliminate_zeros(self):
         """Removes zero entories in place."""
-        compress = cusparse.csr2csr_compress(self, 0)
+        self.sum_duplicates()
+        if cupy.cuda.runtime.runtimeGetVersion() >= 8000:
+            compress = cusparse.csr2csr_compress(self, 0)
+        else:
+            coo = self.tocoo()
+            coo.eliminate_zeros()
+            compress = coo.tocsr()
         self.data = compress.data
         self.indices = compress.indices
         self.indptr = compress.indptr
