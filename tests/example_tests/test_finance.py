42a20a9678503f008a2cd7bfe62947344d949235
Masaki Saito <msaito@preferred.jp>
2017-11-21 14:19:20 +0900

    Add Tests

--- tests/example_tests/test_finance.py
+++ tests/example_tests/test_finance.py
@@ -34,3 +34,21 @@ class TestMonteCarlo(unittest.TestCase):
             r'    # of samples per option: 100\n' +
             r'GPU \(CuPy, Monte Carlo method\):\t[0-9\.]+ sec\n' +
             r'Error: [0-9\.]+')
+
+
+class TestMonteCarloWithMultiGPU(unittest.TestCase):
+
+    def test_monte_carlo(self):
+        output = example_test.run_example(
+            'finance/monte_carlo_multigpu.py', '--n-options', '10',
+            '--n-samples-per-thread', '10',
+            '--n-threads-per-option', '10')
+        six.assertRegex(
+            self, output.decode('utf-8'),
+            r'initializing...\n' +
+            r'start computation\n' +
+            r'    # of gpus: 1\n' +
+            r'    # of options: 10\n' +
+            r'    # of samples per option: 100\n' +
+            r'GPU \(CuPy, Monte Carlo method\):\t[0-9\.]+ sec\n' +
+            r'Error: [0-9\.]+')
