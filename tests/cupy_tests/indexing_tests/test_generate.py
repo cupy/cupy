832785673bfe393c6f607a1f758a8008c5665264
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-11-28 10:52:08 +0900

    improve test

--- tests/cupy_tests/indexing_tests/test_generate.py
+++ tests/cupy_tests/indexing_tests/test_generate.py
@@ -156,26 +156,13 @@ class TestUnravelIndex(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
+    @testing.for_orders(['C', 'F', None])
     @testing.for_int_dtypes()
     @testing.numpy_cupy_array_list_equal()
-    def test_C(self, xp, dtype):
+    def test(self, xp, order, dtype):
         a = testing.shaped_arange((4, 3, 2), xp, dtype)
         a = xp.minimum(a, 6 * 4 - 1)
-        return xp.unravel_index(a, (6, 4))
-
-    @testing.for_int_dtypes()
-    @testing.numpy_cupy_array_list_equal()
-    def test_F(self, xp, dtype):
-        a = testing.shaped_arange((4, 3, 2), xp, dtype)
-        a = xp.minimum(a, 6 * 4 - 1)
-        return xp.unravel_index(a, (6, 4), order='F')
-
-    @testing.for_int_dtypes()
-    @testing.numpy_cupy_array_list_equal()
-    def test_none(self, xp, dtype):
-        a = testing.shaped_arange((4, 3, 2), xp, dtype)
-        a = xp.minimum(a, 6 * 4 - 1)
-        return xp.unravel_index(a, (6, 4), order=None)
+        return xp.unravel_index(a, (6, 4), order=order)
 
     @testing.for_int_dtypes()
     @testing.numpy_cupy_raises(accept_error=TypeError)
@@ -183,14 +170,16 @@ class TestUnravelIndex(unittest.TestCase):
         a = testing.shaped_arange((4, 3, 2), xp, dtype)
         xp.unravel_index(a, (6, 4), order='V')
 
+    @testing.for_orders(['C', 'F', None])
     @testing.for_int_dtypes(no_bool=True)
     @testing.numpy_cupy_raises(accept_error=ValueError)
-    def test_invalid_index(self, xp, dtype):
+    def test_invalid_index(self, xp, order, dtype):
         a = testing.shaped_arange((4, 3, 2), xp, dtype)
-        xp.unravel_index(a, (6, 4))
+        xp.unravel_index(a, (6, 4), order=order)
 
+    @testing.for_orders(['C', 'F', None])
     @testing.for_float_dtypes()
     @testing.numpy_cupy_raises(accept_error=TypeError)
-    def test_invalid_dtype(self, xp, dtype):
+    def test_invalid_dtype(self, xp, order, dtype):
         a = testing.shaped_arange((4, 3, 2), xp, dtype)
-        xp.unravel_index(a, (6, 4))
+        xp.unravel_index(a, (6, 4), order=order)
