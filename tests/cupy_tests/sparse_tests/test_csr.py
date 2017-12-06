76a67a90ba29f00eb24b741ba0b999a659a47c11
Yuya Unno <unnonouno@gmail.com>
2017-12-07 04:03:50 +0900

    Remove case

--- tests/cupy_tests/sparse_tests/test_csr.py
+++ tests/cupy_tests/sparse_tests/test_csr.py
@@ -362,8 +362,7 @@ class TestCsrMatrixInit(unittest.TestCase):
 
 @testing.parameterize(*testing.product({
     'make_method': [
-        '_make', '_make_unordered', '_make_empty', '_make_duplicate',
-        '_make_shape'],
+        '_make', '_make_unordered', '_make_empty', '_make_shape'],
     'dtype': [numpy.float32, numpy.float64],
 }))
 @unittest.skipUnless(scipy_available, 'requires scipy')
