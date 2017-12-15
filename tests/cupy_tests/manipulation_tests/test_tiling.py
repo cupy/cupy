81cd0ffd3fd82aa66cce8ce81da46bcd0d63be85
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-12-15 12:14:49 +0900

    fix tests to pass with NumPy 1.10

--- tests/cupy_tests/manipulation_tests/test_tiling.py
+++ tests/cupy_tests/manipulation_tests/test_tiling.py
@@ -8,8 +8,6 @@ from cupy import testing
     {'repeats': 2, 'axis': None},
     {'repeats': 2, 'axis': 1},
     {'repeats': 2, 'axis': -1},
-    {'repeats': [2], 'axis': None},
-    {'repeats': [2], 'axis': 1},
     {'repeats': [0, 0, 0], 'axis': 1},
     {'repeats': [1, 2, 3], 'axis': 1},
     {'repeats': [1, 2, 3], 'axis': -2},
@@ -26,16 +24,49 @@ class TestRepeat(unittest.TestCase):
 
 
 @testing.parameterize(
+    {'repeats': [2], 'axis': None},
+    {'repeats': [2], 'axis': 1},
+)
+@testing.gpu
+@testing.with_requires('numpy>=1.10')
+class TestRepeatListBroadcast(unittest.TestCase):
+
+    """Test for `repeats` argument using single element list.
+
+    This feature is only supported in NumPy 1.10 or later.
+    """
+
+    @testing.numpy_cupy_array_equal()
+    def test_array_repeat(self, xp):
+        x = testing.shaped_arange((2, 3, 4), xp)
+        return xp.repeat(x, self.repeats, self.axis)
+
+
+@testing.parameterize(
     {'repeats': 0, 'axis': None},
     {'repeats': 2, 'axis': None},
     {'repeats': 2, 'axis': 0},
-    {'repeats': [2], 'axis': None},
-    {'repeats': [2], 'axis': 0},
     {'repeats': [1, 2, 3, 4], 'axis': None},
     {'repeats': [1, 2, 3, 4], 'axis': 0},
 )
 @testing.gpu
 class TestRepeat1D(unittest.TestCase):
+
+    @testing.numpy_cupy_array_equal()
+    def test_array_repeat(self, xp):
+        x = testing.shaped_arange((4,), xp)
+        return xp.repeat(x, self.repeats, self.axis)
+
+
+@testing.parameterize(
+    {'repeats': [2], 'axis': None},
+    {'repeats': [2], 'axis': 0},
+)
+@testing.gpu
+@testing.with_requires('numpy>=1.10')
+class TestRepeat1DListBroadcast(unittest.TestCase):
+
+    """See comment in TestRepeatListBroadcast class."""
 
     @testing.numpy_cupy_array_equal()
     def test_array_repeat(self, xp):
