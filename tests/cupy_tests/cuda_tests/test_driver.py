c56a6f315611e77467c9bbb3bc4d17d1199ef052
Ryosuke Okuta <okuta@preferred.jp>
2017-12-25 16:52:29 +0900

    Merge pull request #856 from kmaehashi/fix-mt-mempool
    Fix memory pool to correctly detect that memory pool is not established

--- tests/cupy_tests/cuda_tests/test_driver.py
+++ tests/cupy_tests/cuda_tests/test_driver.py
@@ -1,3 +1,4 @@
+import threading
 import unittest
 
 import cupy
@@ -9,3 +10,27 @@ class TestDriver(unittest.TestCase):
         # Make sure to create context.
         cupy.arange(1)
         self.assertNotEqual(0, driver.ctxGetCurrent())
+
+    def test_ctxGetCurrent_thread(self):
+        # Make sure to create context in main thread.
+        cupy.arange(1)
+
+        def f(self):
+            self._result0 = driver.ctxGetCurrent()
+            cupy.arange(1)
+            self._result1 = driver.ctxGetCurrent()
+
+        self._result0 = None
+        self._result1 = None
+        t = threading.Thread(target=f, args=(self,))
+        t.daemon = True
+        t.start()
+        t.join()
+
+        # The returned context pointer must be NULL on sub thread
+        # without valid context.
+        self.assertEqual(0, self._result0)
+
+        # After the context is created, it should return the valid
+        # context pointer.
+        self.assertNotEqual(0, self._result1)
