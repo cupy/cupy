b2dc8ce46c8e8e15a4c6dd8c8f3bb1e8e4a6661b
anaruse <anaruse@nvidia.com>
2018-01-09 19:16:00 +0900

    Change compiler options of nvcc

--- cupy_setup_build.py
+++ cupy_setup_build.py
@@ -414,19 +414,18 @@ def _nvcc_gencode_options(cuda_version):
     #
     #     arch_list = [('compute_61', 'sm_61')]
 
-    arch_list = ['compute_30', 'compute_50']
-    if cuda_version >= 8000:
-        arch_list += ['compute_60']
+    arch_list = ['compute_30',
+                 'compute_50']
 
     if cuda_version >= 9000:
-        arch_list = [('compute_30', 'sm_30'),
-                     ('compute_35', 'sm_35'),
-                     ('compute_50', 'sm_50'),
-                     ('compute_52', 'sm_52'),
-                     ('compute_60', 'sm_60'),
-                     ('compute_61', 'sm_61'),
-                     ('compute_70', 'sm_70'),
-                     'compute_70']
+        arch_list += [('compute_60', 'sm_60'),
+                      ('compute_61', 'sm_61'),
+                      ('compute_70', 'sm_70'),
+                      'compute_70']
+    elif cuda_version >= 8000:
+        arch_list += [('compute_60', 'sm_60'),
+                      ('compute_61', 'sm_61'),
+                      'compute_60']
 
     options = []
     for arch in arch_list:
