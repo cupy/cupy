1623f1955aa73970c6a43eaa7ed6d2d5fbbc23ef
ronekko <ronekko@gmail.com>
2018-02-22 19:11:15 +0900

    Fix ndarray.diagonal to accept axis2=-ndim

--- cupy/core/core.pyx
+++ cupy/core/core.pyx
@@ -3209,7 +3209,7 @@ cpdef _scatter_op(ndarray a, slices, value, op):
 cpdef ndarray _diagonal(ndarray a, Py_ssize_t offset=0, Py_ssize_t axis1=0,
                         Py_ssize_t axis2=1):
     cdef Py_ssize_t ndim = a.ndim
-    if (axis1 >= ndim or abs(axis2) >= ndim or
+    if (axis1 >= ndim or axis2 >= ndim or
             axis1 < - ndim or axis2 < - ndim):
         raise ValueError('axis1(={0}) and axis2(={1}) must be within range '
                          '(ndim={2})'.format(axis1, axis2, ndim))
