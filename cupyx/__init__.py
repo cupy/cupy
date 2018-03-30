f2c2a3174303d445cc80b3577bdc0a5e1646a6f4
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-03-30 16:46:16 +0900

    flake8

--- cupyx/__init__.py
+++ cupyx/__init__.py
@@ -1,4 +1,4 @@
 # "NOQA" to suppress flake8 warning
-from cupyx.runtime import get_runtime_info  # NOQA
 from cupyx.rsqrt import rsqrt  # NOQA
+from cupyx.runtime import get_runtime_info  # NOQA
 from cupyx.scatter import scatter_add  # NOQA
