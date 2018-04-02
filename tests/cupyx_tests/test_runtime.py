40b880373fc1a333cfbee48d8d36e83f7f7aa018
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-04-02 11:44:26 +0900

    fix test code to check stringfied result of runtime object

--- tests/cupyx_tests/test_runtime.py
+++ tests/cupyx_tests/test_runtime.py
@@ -8,4 +8,4 @@ class TestRuntime(unittest.TestCase):
     def test_runtime(self):
         runtime = cupyx.get_runtime_info()
         assert cupy.__version__ == runtime.cupy_version
-        assert cupy.__version__ in str(runtime.cupy_version)
+        assert cupy.__version__ in str(runtime)
