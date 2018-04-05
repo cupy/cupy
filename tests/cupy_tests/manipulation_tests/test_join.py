8396ad04c4b1d3b5fc9d337a3cc03bcfddc2155d
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-04-05 19:28:48 +0900

    add test for 32-bit boundary for cupy.concatenate

--- tests/cupy_tests/manipulation_tests/test_join.py
+++ tests/cupy_tests/manipulation_tests/test_join.py
@@ -108,6 +108,13 @@ class TestJoin(unittest.TestCase):
         b = testing.shaped_arange((2, 1), xp, 'f')
         return xp.concatenate((a, b) * 1024, axis=1)
 
+    @testing.numpy_cupy_array_equal()
+    @testing.slow
+    def test_concatenate_32bit_boundary(self, xp):
+        a = xp.zeros((2 ** 30,), dtype=xp.int8)
+        b = xp.zeros((2 ** 30,), dtype=xp.int8)
+        return xp.concatenate([a, b])
+
     def test_concatenate_wrong_ndim(self):
         a = cupy.empty((2, 3))
         b = cupy.empty((2,))
