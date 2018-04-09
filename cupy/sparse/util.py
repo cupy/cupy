13f85835a9f59087574f3df5f8006cc14d307ad0
Ryosuke Okuta <okuta@preferred.jp>
2018-04-09 15:43:52 +0900

    Merge pull request #1071 from anaruse/atomic_add_double
    Support double precision atomicAdd on Maxwell or older GPUs

--- cupy/sparse/util.py
+++ cupy/sparse/util.py
@@ -2,24 +2,6 @@ import cupy
 import cupy.sparse.base
 
 
-_preamble_atomic_add = '''
-#if __CUDA_ARCH__ < 600
-__device__ double atomicAdd(double* address, double val) {
-    unsigned long long* address_as_ull =
-                                          (unsigned long long*)address;
-    unsigned long long old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(val +
-                        __longlong_as_double(assumed)));
-    } while (assumed != old);
-    return __longlong_as_double(old);
-}
-#endif
-'''
-
-
 def isintlike(x):
     try:
         return bool(int(x) == x)
