e7dc9ac59146a19bea39b3739347a6131a994df3
Akifumi Imanishi <imanishi@preferred.jp>
2018-04-13 18:02:07 +0900

    add test

--- tests/cupy_tests/random_tests/test_permutations.py
+++ tests/cupy_tests/random_tests/test_permutations.py
@@ -12,6 +12,41 @@ class TestPermutations(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
+    # Test ranks
+
+    @testing.numpy_cupy_raises()
+    def test_permutation_zero_dim(self, xp):
+        a = testing.shaped_random((), xp)
+        xp.random.permutation(a)
+
+    # Test same values
+
+    @testing.for_all_dtypes(no_float16=True, no_bool=True, no_complex=True)
+    def test_permutation_sort_1dim(self, dtype):
+        a = cupy.arange(10, dtype=dtype)
+        b = cupy.copy(a)
+        cupy.random.permutation(a)
+        testing.assert_allclose(cupy.sort(a), b)
+
+    @testing.for_all_dtypes(no_float16=True, no_bool=True, no_complex=True)
+    def test_permutation_sort_ndim(self, dtype):
+        a = cupy.arange(15, dtype=dtype).reshape(5, 3)
+        b = cupy.copy(a)
+        cupy.random.permutation(a)
+        testing.assert_allclose(cupy.sort(a, axis=0), b)
+
+    # Test seed
+
+    @testing.for_all_dtypes()
+    def test_permutation_seed1(self, dtype):
+        a = testing.shaped_random((10,), cupy, dtype)
+        b = cupy.copy(a)
+        cupy.random.seed(0)
+        cupy.random.permutation(a)
+        cupy.random.seed(0)
+        cupy.random.permutation(b)
+        testing.assert_allclose(a, b)
+
 
 @testing.gpu
 class TestShuffle(unittest.TestCase):
