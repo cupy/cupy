5600fae9eda653d9baaf56d75668def82cf5f0ab
Ryosuke Okuta <okuta@preferred.jp>
2018-04-15 20:56:05 +0900

    Merge pull request #1136 from kmaehashi/fix-runtime-cudnn-error
    fix to capture CuDNNError in cupyx.runtime

--- tests/cupyx_tests/test_runtime.py
+++ tests/cupyx_tests/test_runtime.py
@@ -1,7 +1,21 @@
 import unittest
 
+import mock
+
 import cupy
 import cupyx
+
+
+try:
+    import cupy.cuda.cudnn as cudnn
+except ImportError:
+    cudnn = None
+
+
+def _get_error_func(error, *args, **kwargs):
+    def raise_error(*_args, **_kwargs):
+        raise error(*args, **kwargs)
+    return raise_error
 
 
 class TestRuntime(unittest.TestCase):
@@ -9,3 +23,28 @@ class TestRuntime(unittest.TestCase):
         runtime = cupyx.get_runtime_info()
         assert cupy.__version__ == runtime.cupy_version
         assert cupy.__version__ in str(runtime)
+
+    @unittest.skipUnless(cudnn is not None, 'cuDNN is required')
+    def test_error(self):
+        runtime = cupyx.get_runtime_info()
+        assert 'Error' not in str(runtime)
+
+        with mock.patch(
+                'cupy.cuda.runtime.driverGetVersion',
+                side_effect=_get_error_func(
+                    cupy.cuda.runtime.CUDARuntimeError, 0)):
+            runtime = cupyx.get_runtime_info()
+            assert 'CUDARuntimeError' in str(runtime)
+
+        with mock.patch(
+                'cupy.cuda.runtime.runtimeGetVersion',
+                side_effect=_get_error_func(
+                    cupy.cuda.runtime.CUDARuntimeError, 0)):
+            runtime = cupyx.get_runtime_info()
+            assert 'CUDARuntimeError' in str(runtime)
+
+        with mock.patch(
+                'cupy.cuda.cudnn.getVersion',
+                side_effect=_get_error_func(cudnn.CuDNNError, 0)):
+            runtime = cupyx.get_runtime_info()
+            assert 'CuDNNError' in str(runtime)
