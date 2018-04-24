4b360d4cea81aeb50277fb295e03d20761127ce8
Shunta Saito <shunta.saito@gmail.com>
2018-04-24 16:28:27 +0900

    Fix dlpack.pyx

--- cupy/core/dlpack.pyx
+++ cupy/core/dlpack.pyx
@@ -5,10 +5,66 @@ import cupy
 
 from cupy.core.core cimport ndarray
 from cupy.cuda.runtime cimport free
+# DLPACK_VERSION: 010
+
+from libc.stdint cimport uint8_t
+from libc.stdint cimport uint16_t
+from libc.stdint cimport int64_t
+from libc.stdint cimport uint64_t
+
+from cupy.core.core cimport ndarray
+from cupy.cuda cimport device as device_mod
+from cupy.cuda cimport memory
+from cpython cimport pycapsule
+
+
+ctypedef enum DLDeviceType:
+    kDLCPU = 1
+    kDLGPU = 2
+    kDLCPUPinned = 3
+    kDLOpenCL = 4
+    kDLMetal = 8
+    kDLVPI = 9
+    kDLROCM = 10
+
+
+ctypedef struct DLContext 'DLContext':
+    DLDeviceType device_type
+    int device_id
+
+
+ctypedef enum DLDataTypeCode:
+    kDLInt = <unsigned int>0
+    kDLUInt = <unsigned int>1
+    kDLFloat = <unsigned int>2
+
+
+ctypedef struct DLDataType 'DLDataType':
+    uint8_t code
+    uint8_t bits
+    uint16_t lanes
+
+
+ctypedef struct DLTensor 'DLTensor':
+    void* data
+    DLContext ctx
+    int ndim
+    DLDataType dtype
+    int64_t* shape
+    int64_t* strides
+    uint64_t byte_offset
+
+
+ctypedef struct DLManagedTensor 'DLManagedTensor':
+    DLTensor dl_tensor
+    void* manager_ctx
+    void (*deleter)(DLManagedTensor*)
 
 
 cdef void deleter(DLManagedTensor* tensor):
-    free(<size_t>tensor.manager_ctx)
+    free(&tensor.dl_tensor.ctx)
+    free(&tensor.dl_tensor.dtype)
+    free(&tensor.dl_tensor)
 
 
 cpdef object toDlpack(ndarray array):
@@ -48,8 +104,7 @@ cpdef object toDlpack(ndarray array):
 
     dl_tensor.byte_offset = 0
 
-    cdef DLManagedTensor* dlm_tensor = \
-        <DLManagedTensor*>malloc(sizeof(DLManagedTensor))
+    cdef DLManagedTensor* dlm_tensor = <DLManagedTensor*>malloc(sizeof(DLManagedTensor))
     dlm_tensor.dl_tensor = dereference(dl_tensor)
     dlm_tensor.manager_ctx = array.get_pointer().ptr
     dlm_tensor.deleter = deleter
