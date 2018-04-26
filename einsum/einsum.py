1221a4dd877e3870b4bfd60bd5be32ae14f0966c
Toshiki Kataoka <kataoka@preferred.jp>
2018-04-26 16:24:08 +0900

    wip

--- einsum/einsum.py
+++ einsum/einsum.py
@@ -2,7 +2,24 @@ einsum_symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 einsum_symbols_set = set(einsum_symbols)
 
 
-def _parse_einsum_input(operands):
+def _concat(lists):
+    return sum(lists, [])
+
+
+def _parse_int_subscript(sub):
+    subscripts = ""
+    for s in sub:
+        if s is Ellipsis:
+            subscripts += "@"
+        elif isinstance(s, int):
+            subscripts += einsum_symbols[s]
+        else:
+            raise TypeError("For this input type lists must contain "
+                            "either int or Ellipsis")
+    return subscripts
+
+
+def _parse_einsum_input(operands, parse_ellipsis=True):
     """Parse einsum operands.
 
     This function is based on `numpy.core.einsumfunc._parse_einsum_input`
@@ -44,142 +61,88 @@ def _parse_einsum_input(operands):
             if s not in einsum_symbols:
                 raise ValueError("Character %s is not a valid symbol." % s)
 
+        # Parse "..."
+        subscripts = subscripts.replace("...", "@")
+        if "." in subscripts:
+            raise ValueError("Invalid Ellipses.")
+
+        # Parse "->"
+        if ("-" in subscripts) or (">" in subscripts):
+            # Check for proper "->"
+            invalid = (subscripts.count("-") > 1) or (subscripts.count(">") > 1)
+            subscripts = subscripts.split("->")
+            if invalid or len(subscripts) != 2:
+                raise ValueError("Subscripts can only contain one '->'.")
+            input_subscripts, output_subscript = subscripts
+
+        else:
+            input_subscripts = subscripts
+            output_subscript = None
+
+        input_subscripts = input_subscripts.split(",")
+        if len(input_subscripts) != len(operands):
+            raise ValueError("Number of einsum subscripts must be equal to the "
+                             "number of operands.")
+
     else:
         tmp_operands = list(operands)
-        operand_list = []
-        subscript_list = []
-        for p in range(len(operands) // 2):
-            operand_list.append(tmp_operands.pop(0))
-            subscript_list.append(tmp_operands.pop(0))
-
-        output_list = tmp_operands[-1] if len(tmp_operands) else None
-        operands = operand_list
-        subscripts = ""
-        last = len(subscript_list) - 1
-        for num, sub in enumerate(subscript_list):
-            for s in sub:
-                if s is Ellipsis:
-                    subscripts += "..."
-                elif isinstance(s, int):
-                    subscripts += einsum_symbols[s]
-                else:
-                    raise TypeError("For this input type lists must contain "
-                                    "either int or Ellipsis")
-            if num != last:
-                subscripts += ","
-
-        if output_list is not None:
-            subscripts += "->"
-            for s in output_list:
-                if s is Ellipsis:
-                    subscripts += "..."
-                elif isinstance(s, int):
-                    subscripts += einsum_symbols[s]
-                else:
-                    raise TypeError("For this input type lists must contain "
-                                    "either int or Ellipsis")
-    # Check for proper "->"
-    if ("-" in subscripts) or (">" in subscripts):
-        invalid = (subscripts.count("-") > 1) or (subscripts.count(">") > 1)
-        if invalid or (subscripts.count("->") != 1):
-            raise ValueError("Subscripts can only contain one '->'.")
-
-    # Parse ellipses
-    if "." in subscripts:
-        used = subscripts.replace(".", "").replace(",", "").replace("->", "")
-        unused = list(einsum_symbols_set - set(used))
-        ellipse_inds = "".join(unused)
-        longest = 0
-
-        if "->" in subscripts:
-            input_tmp, output_sub = subscripts.split("->")
-            split_subscripts = input_tmp.split(",")
-            out_sub = True
+        operands = []
+        input_subscripts = []
+        while len(tmp_operands) >= 2:
+            operands.append(tmp_operands.popleft())
+            input_subscripts.append(_parse_int_subscript(
+                tmp_operands.popleft()))
+        if tmp_operands:
+            output_subscript = _parse_int_subscript(tmp_operands[0])
         else:
-            split_subscripts = subscripts.split(',')
-            out_sub = False
+            output_subscript = None
 
-        for num, sub in enumerate(split_subscripts):
-            if "." in sub:
-                if (sub.count(".") != 3) or (sub.count("...") != 1):
-                    raise ValueError("Invalid Ellipses.")
+    return input_subscripts, output_subscript, operands
 
-                # Take into account numerical values
-                if operands[num].shape == ():
-                    ellipse_count = 0
-                else:
-                    ellipse_count = max(operands[num].ndim, 1)
-                    ellipse_count -= (len(sub) - 3)
 
-                if ellipse_count > longest:
-                    longest = ellipse_count
-
-                if ellipse_count < 0:
-                    raise ValueError("Ellipses lengths do not match.")
-                elif ellipse_count == 0:
-                    split_subscripts[num] = sub.replace('...', '')
-                else:
-                    rep_inds = ellipse_inds[-ellipse_count:]
-                    split_subscripts[num] = sub.replace('...', rep_inds)
-
-        subscripts = ",".join(split_subscripts)
-        if longest == 0:
-            out_ellipse = ""
-        else:
-            out_ellipse = ellipse_inds[-longest:]
-
-        if out_sub:
-            subscripts += "->" + output_sub.replace("...", out_ellipse)
-        else:
-            # Special care for outputless ellipses
-            output_subscript = ""
-            tmp_subscripts = subscripts.replace(",", "")
-            for s in sorted(set(tmp_subscripts)):
-                if s not in (einsum_symbols):
-                    raise ValueError("Character %s is not a valid symbol." % s)
-                if tmp_subscripts.count(s) == 1:
-                    output_subscript += s
-            normal_inds = ''.join(sorted(set(output_subscript) -
-                                         set(out_ellipse)))
-
-            subscripts += "->" + out_ellipse + normal_inds
-
-    # Build output string if does not exist
-    if "->" in subscripts:
-        input_subscripts, output_subscript = subscripts.split("->")
+def _chr(char):
+    if char < 0:
+        return "...[%d]" % char
     else:
-        input_subscripts = subscripts
-        # Build output subscripts
-        tmp_subscripts = subscripts.replace(",", "")
-        output_subscript = ""
-        for s in sorted(set(tmp_subscripts)):
-            if s not in einsum_symbols:
-                raise ValueError("Character %s is not a valid symbol." % s)
-            if tmp_subscripts.count(s) == 1:
-                output_subscript += s
+        return chr(char)
 
-    # Make sure output subscripts are in the input
-    for char in output_subscript:
-        if char not in input_subscripts:
-            raise ValueError("Output character %s did not appear in the input"
-                             % char)
 
-    # Make sure number operands is equivalent to the number of terms
-    if len(input_subscripts.split(',')) != len(operands):
-        raise ValueError("Number of einsum subscripts must be equal to the "
-                         "number of operands.")
+def _parse_ellipsis_subscript(subscript, ndim=None, ellipsis_len=None):
+    subs = subscript.split('@')
+    if len(subs) == 1:
+        sub, = subs
+        if ndim is not None and len(sub) != ndim:
+            # raise ValueError later
+            return "Einstein sum subscript %s does not contain the correct number of indices " % subs
+        return list(map(ord, sub))
+    elif len(subs) == 2:
+        if ndim is not None:
+            left_sub, right_sub = subs
+            ellipsis_len = ndim - (len(left_sub) + len(right_sub))
+        if ellipsis_len < 0:
+            # raise ValueError later
+            return "Einstein sum subscript %s...%s does not contain the correct number of indices " % (left_sub, right_sub)
+        return list(map(ord, left_sub)) + list(range(-ellipsis_len, 0)) + list(map(ord, right_sub))
+    else:
+        # >= 2 ellipses for an operand
+        raise ValueError("Invalid Ellipses.")
 
-    return (input_subscripts, output_subscript, operands)
 
+def einsum(*operands):
+    input_subscripts, output_subscript, operands = _parse_einsum_input(operands)
+
+    input_subscripts = [
+        _parse_ellipsis_subscript(sub, ndim=arr.ndim)
+        for sub, arr in zip(input_subscripts, operands)
+    ]
+    for i, sub_or_err in enumerate(input_subscripts):
+        if isinstance(sub_or_err, str):
+            raise ValueError(sub_or_err + "for operand %d." % i)
 
     # Get length of each unique dimension and ensure all dimensions are correct
     dimension_dict = {}
-    for tnum, term in enumerate(input_list):
+    for tnum, term in enumerate(input_subscripts):
         sh = operands[tnum].shape
-        if len(sh) != len(term):
-            raise ValueError("Einstein sum subscript %s does not contain the "
-                             "correct number of indices for operand %d."
-                             % (input_subscripts[tnum], tnum))
         for cnum, char in enumerate(term):
             dim = sh[cnum]
             if char in dimension_dict.keys():
@@ -187,8 +150,57 @@ def _parse_einsum_input(operands):
                 if dimension_dict[char] == 1:
                     dimension_dict[char] = dim
                 elif dim not in (1, dimension_dict[char]):
+                    dim_old = dimension_dict[char]
                     raise ValueError("Size of label '%s' for operand %d (%d) "
                                      "does not match previous terms (%d)."
-                                     % (char, tnum, dimension_dict[char], dim))
+                                     % (_chr(char), tnum, dim, dim_old))
             else:
                 dimension_dict[char] = dim
+
+    if output_subscript is None:
+        # Build output subscripts
+        tmp_subscripts = _concat(input_subscripts)
+        output_subscript = [
+            s
+            for s in sorted(set(tmp_subscripts))
+            if s < 0 or tmp_subscripts.count(s) == 1
+        ]
+    else:
+        output_subscript = _parse_ellipsis_subscript(
+            output_subscript,
+            ellipsis_len=len(list(s for s in dimension_dict.keys() if s < 0))
+       )
+
+        # Make sure output subscripts are in the input
+        tmp_subscripts = set(_concat(input_subscripts))
+        for char in output_subscript:
+            if char not in tmp_subscripts:
+                raise ValueError(
+                    "Output character %s did not appear in the input" % _chr(char))
+
+    path = [(0, 1)] * (len(operands) - 1)  # TODO(kataoka): optimize
+
+    if len(operands) >= 2:
+        # We don't have to return a view
+    for num in range(len(operands)):
+        op = operands[num]
+        squeeze_indices = [
+            i
+            for i, n in enumerate(op.shape)
+            if n == 1
+        ]
+        if squeeze_indices:
+
+
+    count_dict = {k: 0 for k in dimension_dict}
+    for sub in input_subscripts:
+        for s in sub:
+            count_dict[s] += 1
+
+    for idx0, idx1 in path:
+
+    input_subscripts = [sub.split("@") for sub in input_subscripts]
+    if any(len(sub) > 2 for sub in input_subscripts):
+        # Each subscript
+        raise ValueError("Invalid Ellipses.")
+
