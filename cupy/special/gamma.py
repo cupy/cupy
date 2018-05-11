2f7cd921da305ae7aa557efdd21dbaecabd65a6d
yoshikawa <yoshikawa@preferred.jp>
2018-05-11 10:51:35 +0900

    [add] test_gamma

--- cupy/special/gamma.py
+++ cupy/special/gamma.py
@@ -10,7 +10,13 @@ def _get_gamma_kernel():
     if _gamma_kernel is None:
         _gamma_kernel = core.ElementwiseKernel(
             'T x', 'T y',
-            'y = tgammaf(x)',
+            """
+            if(x < 0. && x == floor(x)){
+                y = 1.0 / 0.0;
+                return;
+            }
+            y = tgammaf(x);
+            """,
             'gamma_kernel'
         )
     return _gamma_kernel
@@ -22,6 +28,14 @@ def gamma(x):
     .. seealso:: :data:`scipy.special.gamma`
 
     """
+    if (x.dtype == cupy.float16 or x.dtype == cupy.dtype('b') or
+            x.dtype == cupy.dtype('h') or x.dtype == cupy.dtype('B') or
+            x.dtype == cupy.dtype('H') or x.dtype == cupy.bool_):
+        x = x.astype(cupy.float32)
+    elif (x.dtype == cupy.dtype('i') or x.dtype == cupy.dtype('l') or
+            x.dtype == cupy.dtype('q') or x.dtype == cupy.dtype('I') or
+            x.dtype == cupy.dtype('L') or x.dtype == cupy.dtype('Q')):
+        x = x.astype(cupy.float64)
     y = cupy.zeros_like(x)
     _get_gamma_kernel()(x, y)
     return y
