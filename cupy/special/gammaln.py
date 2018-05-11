2e1f451ad4f101a4696aef9a570043f9a2e734f3
yoshikawa <yoshikawa@preferred.jp>
2018-05-11 10:21:32 +0900

    [add] test_gammaln

--- cupy/special/gammaln.py
+++ cupy/special/gammaln.py
@@ -22,6 +22,14 @@ def gammaln(x):
     .. seealso:: :data:`scipy.special.gammaln`
 
     """
+    if (x.dtype == cupy.float16 or x.dtype == cupy.dtype('b') or
+        x.dtype == cupy.dtype('h') or x.dtype == cupy.dtype('B') or
+        x.dtype == cupy.dtype('H') or x.dtype == cupy.bool_):
+        x = x.astype(cupy.float32)
+    elif (x.dtype == cupy.dtype('i') or x.dtype == cupy.dtype('l') or
+          x.dtype == cupy.dtype('q') or x.dtype == cupy.dtype('I') or
+          x.dtype == cupy.dtype('L') or x.dtype == cupy.dtype('Q')):
+        x = x.astype(cupy.float64)
     y = cupy.zeros_like(x)
     _get_gammaln_kernel()(x, y)
     return y
---
+++ tests/cupy_tests/special_test/test_gammaln.py
@@ -0,0 +1,28 @@
+import unittest
+
+from cupy import testing
+import numpy
+
+
+@testing.gpu
+@testing.with_requires('scipy')
+class TestGammaln(unittest.TestCase):
+
+    @testing.for_all_dtypes(no_complex=True)
+    @testing.numpy_cupy_allclose(atol=1e-5, mod='sp', mod_name='special')
+    def test_arange(self, xp, dtype, sp):
+        a = testing.shaped_arange((2, 3), xp, dtype)
+        return sp.gammaln(a)
+
+    @testing.for_all_dtypes(no_complex=True)
+    @testing.numpy_cupy_allclose(atol=1e-2, rtol=1e-4, mod='sp',
+                                 mod_name='special')
+    def test_linspace(self, xp, dtype, sp):
+        if (dtype == xp.dtype('B') or dtype == xp.dtype('H')
+           or dtype == xp.dtype('I') or dtype == xp.dtype('L')
+           or dtype == xp.dtype('Q')):
+            a = numpy.linspace(-30, 30, 1000, dtype=dtype)
+            a = xp.asarray(a)
+        else:
+            a = xp.linspace(-30, 30, 1000, dtype=dtype)
+        return sp.gammaln(a)
