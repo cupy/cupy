c5572a2af57a13949987ec24933372c7600e2e9a
Martin Durant <martin.durant@utoronto.ca>
2018-05-19 13:31:02 -0400

    use arrays

--- tests/cupy_tests/core_tests/test_ndarray_ufunc.py
+++ tests/cupy_tests/core_tests/test_ndarray_ufunc.py
@@ -11,7 +11,7 @@ class TestArrayUfunc(unittest.TestCase):
 
     @testing.for_all_dtypes()
     def test_unary_op(self, dtype):
-        a = cupy.array([0, 1, 2], dtype=dtype)
+        a = cupy.array(np.array([0, 1, 2]), dtype=dtype)
         outa = np.sin(a)
         # numpy operation produced a cupy array
         self.assertTrue(isinstance(outa, cupy.ndarray))
@@ -21,8 +21,8 @@ class TestArrayUfunc(unittest.TestCase):
 
     @testing.for_all_dtypes()
     def test_binary_op(self, op, xp, dtype):
-        a1 = cupy.array([0, 1, 2], dtype=dtype)
-        a2 = cupy.array([0, 1, 2], dtype=dtype)
+        a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
         outa = np.add(a1, a2)
         # numpy operation produced a cupy array
         self.assertTrue(isinstance(outa, cupy.ndarray))
@@ -33,8 +33,8 @@ class TestArrayUfunc(unittest.TestCase):
 
     @testing.for_all_dtypes()
     def test_binary_mixed_op(self, op, xp, dtype):
-        a1 = cupy.array([0, 1, 2], dtype=dtype)
-        a2 = cupy.array([0, 1, 2], dtype=dtype).get()
+        a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype).get()
         with self.assertRaises(TypeError):
             # attempt to add cupy and numpy arrays
             np.add(a1, a2)
