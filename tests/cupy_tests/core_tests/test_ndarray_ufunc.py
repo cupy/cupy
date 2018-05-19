83a894a53bf44f354a5ef171447a2eea28c06805
Martin Durant <martin.durant@utoronto.ca>
2018-05-19 13:59:37 -0400

    more tests

--- tests/cupy_tests/core_tests/test_ndarray_ufunc.py
+++ tests/cupy_tests/core_tests/test_ndarray_ufunc.py
@@ -20,6 +20,15 @@ class TestArrayUfunc(unittest.TestCase):
         self.assertTrue(np.allclose(outa.get(), outb))
 
     @testing.for_all_dtypes()
+    def test_unary_op_out(self, dtype):
+        a = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        outa = cupy.array(np.array([0, 1, 2]), dtype=np.float64)
+        np.sin(a, out=outa)
+        b = a.get()
+        outb = np.sin(b)
+        self.assertTrue(np.allclose(outa.get(), outb))
+
+    @testing.for_all_dtypes()
     def test_binary_op(self, dtype):
         a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
         a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
@@ -32,9 +41,29 @@ class TestArrayUfunc(unittest.TestCase):
         self.assertTrue(np.allclose(outa.get(), outb))
 
     @testing.for_all_dtypes()
+    def test_binary_op_out(self, dtype):
+        a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        outa = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        np.add(a1, a2, out=outa)
+        b1 = a1.get()
+        b2 = a2.get()
+        outb = np.add(b1, b2)
+        self.assertTrue(np.allclose(outa.get(), outb))
+
+    @testing.for_all_dtypes()
     def test_binary_mixed_op(self, dtype):
         a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
         a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype).get()
         with self.assertRaises(TypeError):
             # attempt to add cupy and numpy arrays
             np.add(a1, a2)
+        with self.assertRaises(TypeError):
+            # check reverse order
+            np.add(a2, a1)
+        with self.assertRaises(TypeError):
+            # reject numpy output from cupy
+            np.add(a1, a1, out=a2)
+        with self.assertRaises(TypeError):
+            # reject cupy output from numpy
+            np.add(a2, a2, out=a1)
