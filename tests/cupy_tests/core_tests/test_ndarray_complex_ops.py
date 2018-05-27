dbcf0b382fe90a9f1efafde52b76948d12180cdc
Yuya Unno <unnonouno@gmail.com>
2018-05-28 01:41:53 +0900

    Fix real and imag for zero-dim arrays

--- tests/cupy_tests/core_tests/test_ndarray_complex_ops.py
+++ tests/cupy_tests/core_tests/test_ndarray_complex_ops.py
@@ -44,8 +44,20 @@ class TestRealImag(unittest.TestCase):
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_almost_equal(accept_error=False)
+    def test_real_zero_dim(self, xp, dtype):
+        x = xp.array(1, dtype=dtype)
+        return x.real
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_almost_equal(accept_error=False)
     def test_imag(self, xp, dtype):
         x = testing.shaped_arange((2, 3), xp, dtype)
+        return x.imag
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_almost_equal(accept_error=False)
+    def test_imag_zero_dim(self, xp, dtype):
+        x = xp.array(1, dtype=dtype)
         return x.imag
 
     @testing.for_all_dtypes()
@@ -55,11 +67,25 @@ class TestRealImag(unittest.TestCase):
         x.real = testing.shaped_reverse_arange((2, 3), xp, dtype).real
         return x
 
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_almost_equal(accept_error=False)
+    def test_real_setter_zero_dim(self, xp, dtype):
+        x = xp.array(1, dtype=dtype)
+        x.real = 2
+        return x
+
     @testing.for_dtypes('FD')
     @testing.numpy_cupy_array_almost_equal(accept_error=False)
     def test_imag_setter(self, xp, dtype):
         x = testing.shaped_arange((2, 3), xp, dtype)
         x.imag = testing.shaped_reverse_arange((2, 3), xp, dtype).real
+        return x
+
+    @testing.for_dtypes('FD')
+    @testing.numpy_cupy_array_almost_equal(accept_error=False)
+    def test_imag_setter_zero_dim(self, xp, dtype):
+        x = xp.array(1, dtype=dtype)
+        x.imag = 2
         return x
 
     @testing.for_all_dtypes(no_complex=True)
