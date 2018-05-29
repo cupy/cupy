a551840f957803170ef989038cf023295f264734
Yuya Unno <unnonouno@gmail.com>
2018-05-29 14:34:57 +0900

    Make helper functions as fix tests

--- tests/cupy_tests/core_tests/test_ndarray_copy_and_view.py
+++ tests/cupy_tests/core_tests/test_ndarray_copy_and_view.py
@@ -6,6 +6,15 @@ import cupy
 from cupy import testing
 
 
+def astype_without_warning(x, dtype, *args, **kwargs):
+    if (numpy.dtype(x.dtype).kind == 'c' and
+            numpy.dtype(dtype).kind not in ['b', 'c']):
+        with testing.assert_warns(numpy.ComplexWarning):
+            return x.astype(dtype, *args, **kwargs)
+    else:
+        return x.astype(dtype, *args, **kwargs)
+
+
 @testing.gpu
 class TestArrayCopyAndView(unittest.TestCase):
 
@@ -91,32 +100,15 @@ class TestArrayCopyAndView(unittest.TestCase):
     @testing.numpy_cupy_array_equal()
     def test_astype(self, xp, src_dtype, dst_dtype, order):
         a = testing.shaped_arange((2, 3, 4), xp, src_dtype)
-        if (numpy.dtype(src_dtype).kind == 'c' and
-                numpy.dtype(dst_dtype).kind not in ['b', 'c']):
-            with testing.assert_warns(numpy.ComplexWarning):
-                return a.astype(dst_dtype, order=order)
-        else:
-            return a.astype(dst_dtype, order=order)
+        return astype_without_warning(a, dst_dtype, order=order)
 
     @testing.for_orders('CFAK')
     @testing.for_all_dtypes_combination(('src_dtype', 'dst_dtype'))
     def test_astype_type(self, src_dtype, dst_dtype, order):
-        complex_warning = (numpy.dtype(src_dtype).kind == 'c' and
-                           numpy.dtype(dst_dtype).kind not in ['b', 'c'])
-
         a = testing.shaped_arange((2, 3, 4), cupy, src_dtype)
-        if complex_warning:
-            with testing.assert_warns(numpy.ComplexWarning):
-                b = a.astype(dst_dtype, order=order)
-        else:
-            b = a.astype(dst_dtype, order=order)
-
+        b = astype_without_warning(a, dst_dtype, order=order)
         a_cpu = testing.shaped_arange((2, 3, 4), numpy, src_dtype)
-        if complex_warning:
-            with testing.assert_warns(numpy.ComplexWarning):
-                b_cpu = a_cpu.astype(dst_dtype, order=order)
-        else:
-            b_cpu = a_cpu.astype(dst_dtype, order=order)
+        b_cpu = astype_without_warning(a_cpu, dst_dtype, order=order)
         self.assertEqual(b.dtype.type, b_cpu.dtype.type)
 
     @testing.for_orders('CAK')
@@ -134,34 +126,34 @@ class TestArrayCopyAndView(unittest.TestCase):
         b = a.astype(dtype, order=order, copy=False)
         self.assertTrue(b is a)
 
-    @testing.for_all_dtypes(name='src_dtype', no_complex=True)
-    @testing.for_all_dtypes(name='dst_dtype')
+    @testing.for_all_dtypes_combination(('src_dtype', 'dst_dtype'))
     @testing.numpy_cupy_array_equal()
     def test_astype_strides(self, xp, src_dtype, dst_dtype):
         src = xp.empty((1, 2, 3), dtype=src_dtype)
-        return numpy.array(src.astype(dst_dtype, order='K').strides)
+        return numpy.array(
+            astype_without_warning(src, dst_dtype, order='K').strides)
 
-    @testing.for_all_dtypes(name='src_dtype', no_complex=True)
-    @testing.for_all_dtypes(name='dst_dtype')
+    @testing.for_all_dtypes_combination(('src_dtype', 'dst_dtype'))
     @testing.numpy_cupy_array_equal()
     def test_astype_strides_negative(self, xp, src_dtype, dst_dtype):
         src = xp.empty((2, 3), dtype=src_dtype)[::-1, :]
-        return numpy.array(src.astype(dst_dtype, order='K').strides)
+        return numpy.array(
+            astype_without_warning(src, dst_dtype, order='K').strides)
 
-    @testing.for_all_dtypes(name='src_dtype', no_complex=True)
-    @testing.for_all_dtypes(name='dst_dtype')
+    @testing.for_all_dtypes_combination(('src_dtype', 'dst_dtype'))
     @testing.numpy_cupy_array_equal()
     def test_astype_strides_swapped(self, xp, src_dtype, dst_dtype):
         src = xp.swapaxes(xp.empty((2, 3, 4), dtype=src_dtype), 1, 0)
-        return numpy.array(src.astype(dst_dtype, order='K').strides)
+        return numpy.array(
+            astype_without_warning(src, dst_dtype, order='K').strides)
 
-    @testing.for_all_dtypes(name='src_dtype', no_complex=True)
-    @testing.for_all_dtypes(name='dst_dtype')
+    @testing.for_all_dtypes_combination(('src_dtype', 'dst_dtype'))
     @testing.numpy_cupy_array_equal()
     def test_astype_strides_broadcast(self, xp, src_dtype, dst_dtype):
         src, _ = xp.broadcast_arrays(xp.empty((2,), dtype=src_dtype),
                                      xp.empty((2, 3, 2), dtype=src_dtype))
-        return numpy.array(src.astype(dst_dtype, order='K').strides)
+        return numpy.array(
+            astype_without_warning(src, dst_dtype, order='K').strides)
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal()
