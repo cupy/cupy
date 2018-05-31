75b13aa26425b0c499d19c86b77f5c9bff39426d
Yuya Unno <unnonouno@gmail.com>
2018-05-31 11:08:59 +0900

    Fix test

--- tests/cupy_tests/sparse_tests/test_csc.py
+++ tests/cupy_tests/sparse_tests/test_csc.py
@@ -848,7 +848,11 @@ class TestCscMatrixData(unittest.TestCase):
     @testing.numpy_cupy_allclose(sp_name='sp')
     def test_astype(self, xp, sp):
         m = _make(xp, sp, self.dtype)
-        return m.astype('d').toarray()
+        if numpy.dtype(self.dtype).kind == 'c':
+            t = 'D'
+        else:
+            t = 'd'
+        return m.astype(t).toarray()
 
     @testing.numpy_cupy_equal(sp_name='sp')
     def test_count_nonzero(self, xp, sp):
@@ -863,7 +867,11 @@ class TestCscMatrixData(unittest.TestCase):
     @testing.numpy_cupy_allclose(sp_name='sp')
     def test_power_with_dtype(self, xp, sp):
         m = _make(xp, sp, self.dtype)
-        return m.power(2, 'd').toarray()
+        if numpy.dtype(self.dtype).kind == 'c':
+            t = 'D'
+        else:
+            t = 'd'
+        return m.power(2, t).toarray()
 
 
 @testing.parameterize(*testing.product({
