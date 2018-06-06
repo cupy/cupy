9dd02747483b98767e42814c6cf038d7f3488150
yoshikawa <yoshikawa@preferred.jp>
2018-06-06 11:40:27 +0900

    [fix] kernel initialization move to global space

--- cupy/random/kernels.py
+++ cupy/random/kernels.py
@@ -1,27 +1,14 @@
 from cupy import core
 
-_gumbel_kernel = None
-_laplace_kernel = None
+gumbel_kernel = core.ElementwiseKernel(
+    'T x, T loc, T scale', 'T y',
+    'y = loc - log(-log(1 - x)) * scale',
+    'gumbel_kernel'
+)
 
-
-def _get_gumbel_kernel():
-    global _gumbel_kernel
-    if _gumbel_kernel is None:
-        _gumbel_kernel = core.ElementwiseKernel(
-            'T x, T loc, T scale', 'T y',
-            'y = loc - log(-log(1 - x)) * scale',
-            'gumbel_kernel'
-        )
-    return _gumbel_kernel
-
-
-def _get_laplace_kernel():
-    global _laplace_kernel
-    if _laplace_kernel is None:
-        _laplace_kernel = core.ElementwiseKernel(
-            'T x, T loc, T scale', 'T y',
-            'y = (x < 0.5)? loc + scale * log(x + x):'
-            ' loc - scale * log(2.0 - x - x)',
-            'laplace_kernel'
-        )
-    return _laplace_kernel
+laplace_kernel = core.ElementwiseKernel(
+    'T x, T loc, T scale', 'T y',
+    'y = (x < 0.5)? loc + scale * log(x + x):'
+    ' loc - scale * log(2.0 - x - x)',
+    'laplace_kernel'
+)
