b2365fbd6fc73d245353036363ab4771e5a54f3f
Ryosuke Okuta <okuta@preferred.jp>
2018-07-09 15:15:31 +0900

    Remove overhead in generator and distribution

--- cupy/random/_kernels.py
+++ cupy/random/_kernels.py
@@ -277,19 +277,6 @@ __device__ long rk_binomial(rk_state *state, long n, double p) {
 '''
 
 
-gumbel_kernel = core.ElementwiseKernel(
-    'T x, T loc, T scale', 'T y',
-    'y = loc - log(-log(1 - x)) * scale',
-    'gumbel_kernel'
-)
-
-laplace_kernel = core.ElementwiseKernel(
-    'T x, T loc, T scale', 'T y',
-    'y = (x < 0.5)? loc + scale * log(x + x):'
-    ' loc - scale * log(2.0 - x - x)',
-    'laplace_kernel'
-)
-
 definitions = \
     [rk_state_difinition, rk_seed_definition, rk_random_definition,
      rk_double_definition, rk_binomial_btpe_definition,
