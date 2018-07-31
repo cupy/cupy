e41698338f9f0bb784e0892f230ca0e173a32e3f
Ryosuke Okuta <okuta@preferred.jp>
2018-08-01 02:17:43 +0900

    Fix thrust memory problem

--- cupy/cuda/thrust.pyx
+++ cupy/cuda/thrust.pyx
@@ -8,7 +8,36 @@ cimport cython  # NOQA
 from libcpp cimport vector
 
 from cupy.cuda cimport common
-from cupy.cuda cimport stream as stream_module
+from cupy.cuda cimport memory
+from cupy.cuda cimport stream
+
+
+###############################################################################
+# Memory Management
+###############################################################################
+
+cdef class _MemoryManager:
+    cdef:
+        dict memory
+
+    def __init__(self):
+        self.memory = dict()
+
+
+cdef public char* cupy_malloc(void *m, ptrdiff_t size) with gil:
+    if size == 0:
+        return <char *>0
+    cdef _MemoryManager mm= <_MemoryManager>m
+    mem = memory.alloc(size)
+    mm.memory[mem.ptr] = mem
+    return <char *>mem.ptr
+
+
+cdef public void cupy_free(void *m, char* ptr) with gil:
+    if ptr == <char *>0:
+        return
+    cdef _MemoryManager mm = <_MemoryManager>m
+    del mm.memory[<size_t>ptr]
 
 
 ###############################################################################
@@ -16,10 +45,11 @@ from cupy.cuda cimport stream as stream_module
 ###############################################################################
 
 cdef extern from "../cuda/cupy_thrust.h" namespace "cupy::thrust":
-    void _sort[T](void *, size_t *, const vector.vector[ptrdiff_t]&, size_t)
-    void _lexsort[T](size_t *, void *, size_t, size_t, size_t)
+    void _sort[T](void *, size_t *, const vector.vector[ptrdiff_t]&, size_t,
+                  void *)
+    void _lexsort[T](size_t *, void *, size_t, size_t, size_t, void *)
     void _argsort[T](size_t *, void *, void *, const vector.vector[ptrdiff_t]&,
-                     size_t)
+                     size_t, void *)
 
 
 ###############################################################################
@@ -35,29 +65,31 @@ cpdef sort(dtype, size_t data_start, size_t keys_start,
 
     _data_start = <void *>data_start
     _keys_start = <size_t *>keys_start
-    _strm = <size_t>(stream_module.get_current_stream_ptr())
+    _strm = <size_t>(stream.get_current_stream_ptr())
+    mem_obj = _MemoryManager()
+    cdef void* mem = <void *>mem_obj
 
     # TODO(takagi): Support float16 and bool
     if dtype == numpy.int8:
-        _sort[common.cpy_byte](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_byte](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint8:
-        _sort[common.cpy_ubyte](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_ubyte](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.int16:
-        _sort[common.cpy_short](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_short](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint16:
-        _sort[common.cpy_ushort](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_ushort](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.int32:
-        _sort[common.cpy_int](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_int](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint32:
-        _sort[common.cpy_uint](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_uint](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.int64:
-        _sort[common.cpy_long](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_long](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint64:
-        _sort[common.cpy_ulong](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_ulong](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.float32:
-        _sort[common.cpy_float](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_float](_data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.float64:
-        _sort[common.cpy_double](_data_start, _keys_start, shape, _strm)
+        _sort[common.cpy_double](_data_start, _keys_start, shape, _strm, mem)
     else:
         raise NotImplementedError('Sorting arrays with dtype \'{}\' is not '
                                   'supported'.format(dtype))
@@ -70,29 +102,31 @@ cpdef lexsort(dtype, size_t idx_start, size_t keys_start,
 
     idx_ptr = <size_t *>idx_start
     keys_ptr = <void *>keys_start
-    _strm = <size_t>(stream_module.get_current_stream_ptr())
+    _strm = <size_t>(stream.get_current_stream_ptr())
+    mem_obj = _MemoryManager()
+    cdef void* mem = <void *>mem_obj
 
     # TODO(takagi): Support float16 and bool
     if dtype == numpy.int8:
-        _lexsort[common.cpy_byte](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_byte](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.uint8:
-        _lexsort[common.cpy_ubyte](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_ubyte](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.int16:
-        _lexsort[common.cpy_short](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_short](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.uint16:
-        _lexsort[common.cpy_ushort](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_ushort](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.int32:
-        _lexsort[common.cpy_int](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_int](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.uint32:
-        _lexsort[common.cpy_uint](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_uint](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.int64:
-        _lexsort[common.cpy_long](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_long](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.uint64:
-        _lexsort[common.cpy_ulong](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_ulong](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.float32:
-        _lexsort[common.cpy_float](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_float](idx_ptr, keys_ptr, k, n, _strm, mem)
     elif dtype == numpy.float64:
-        _lexsort[common.cpy_double](idx_ptr, keys_ptr, k, n, _strm)
+        _lexsort[common.cpy_double](idx_ptr, keys_ptr, k, n, _strm, mem)
     else:
         raise TypeError('Sorting keys with dtype \'{}\' is not '
                         'supported'.format(dtype))
@@ -108,39 +142,41 @@ cpdef argsort(dtype, size_t idx_start, size_t data_start, size_t keys_start,
     _idx_start = <size_t *>idx_start
     _data_start = <void *>data_start
     _keys_start = <size_t *>keys_start
-    _strm = <size_t>(stream_module.get_current_stream_ptr())
+    _strm = <size_t>(stream.get_current_stream_ptr())
+    mem_obj = _MemoryManager()
+    cdef void* mem = <void *>mem_obj
 
     # TODO(takagi): Support float16 and bool
     if dtype == numpy.int8:
         _argsort[common.cpy_byte](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint8:
         _argsort[common.cpy_ubyte](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.int16:
         _argsort[common.cpy_short](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint16:
         _argsort[common.cpy_ushort](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.int32:
         _argsort[common.cpy_int](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint32:
         _argsort[common.cpy_uint](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.int64:
         _argsort[common.cpy_long](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.uint64:
         _argsort[common.cpy_ulong](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.float32:
         _argsort[common.cpy_float](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     elif dtype == numpy.float64:
         _argsort[common.cpy_double](
-            _idx_start, _data_start, _keys_start, shape, _strm)
+            _idx_start, _data_start, _keys_start, shape, _strm, mem)
     else:
         raise NotImplementedError('Sorting arrays with dtype \'{}\' is not '
                                   'supported'.format(dtype))
