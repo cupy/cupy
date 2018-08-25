014ce3a77518333b4ee46be8e989ca69d82b7c7b
Gentaro Watanabe <g.wtnb@preferred.jp>
2018-08-25 18:42:26 +0900

    Import scipy.special to use it in test

--- tests/cupyx_tests/scipy_tests/special_tests/test_bessel.py
+++ tests/cupyx_tests/scipy_tests/special_tests/test_bessel.py
@@ -2,6 +2,7 @@ import unittest
 
 import cupy
 from cupy import testing
+import cupyx.scipy.special  # NOQA
 
 
 @testing.gpu
@@ -11,6 +12,8 @@ class TestSpecial(unittest.TestCase):
     @testing.for_dtypes(['f', 'd'])
     @testing.numpy_cupy_allclose(atol=1e-5, scipy_name='scp')
     def check_unary(self, name, xp, scp, dtype):
+        import scipy.special  # NOQA
+
         a = testing.shaped_arange((2, 3), xp, dtype)
         return getattr(scp.special, name)(a)
 
@@ -40,6 +43,8 @@ class TestFusionSpecial(unittest.TestCase):
     @testing.for_dtypes(['f', 'd'])
     @testing.numpy_cupy_allclose(atol=1e-5, scipy_name='scp')
     def check_unary(self, name, xp, scp, dtype):
+        import scipy.special  # NOQA
+
         a = testing.shaped_arange((2, 3), xp, dtype)
 
         @cupy.fuse()
