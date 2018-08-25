cebea1abcca978954ab9cb2db33646b5abe499ae
Gentaro Watanabe <g.wtnb@preferred.jp>
2018-08-25 19:50:16 +0900

    Add erf, erfc, and erfcx to cupyx

--- cupyx/scipy/special/__init__.py
+++ cupyx/scipy/special/__init__.py
@@ -11,3 +11,7 @@ from cupyx.scipy.special.digamma import digamma  # NOQA
 from cupyx.scipy.special.gamma import gamma  # NOQA
 from cupyx.scipy.special.gammaln import gammaln  # NOQA
 from cupyx.scipy.special.zeta import zeta  # NOQA
+
+from cupyx.scipy.special.erf import erf  # NOQA
+from cupyx.scipy.special.erf import erfc  # NOQA
+from cupyx.scipy.special.erf import erfcx  # NOQA
---
+++ cupyx/scipy/special/erf.py
@@ -0,0 +1,37 @@
+import cupy.core.fusion
+from cupy.math import ufunc
+
+
+_erf = ufunc.create_math_ufunc(
+    'erf', 1, 'cupyx_scipy_erf',
+    '''Error function.
+
+    .. seealso:: :meth: scipy.special.erf
+
+    ''',
+    support_complex=False)
+
+
+_erfc = ufunc.create_math_ufunc(
+    'erfc', 1, 'cupyx_scipy_erfc',
+    '''Complementary error function.
+
+    .. seealso:: :meth: scipy.special.erfc
+
+    ''',
+    support_complex=False)
+
+
+_erfcx = ufunc.create_math_ufunc(
+    'erfcx', 1, 'cupyx_scipy_erfcx',
+    '''Scaled complementary error function.
+
+    .. seealso:: :meth: scipy.special.erfcx
+
+    ''',
+    support_complex=False)
+
+
+erf = cupy.core.fusion.ufunc(_erf)
+erfc = cupy.core.fusion.ufunc(_erfc)
+erfcx = cupy.core.fusion.ufunc(_erfcx)
