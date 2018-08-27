e8c461d3c21b2367c08626ce09f79fe0fe92cdf9
Gentaro Watanabe <g.wtnb@preferred.jp>
2018-08-27 13:22:30 +0900

    Fix docstring of error functions

--- cupyx/scipy/special/erf.py
+++ cupyx/scipy/special/erf.py
@@ -6,7 +6,7 @@ _erf = ufunc.create_math_ufunc(
     'erf', 1, 'cupyx_scipy_erf',
     '''Error function.
 
-    .. seealso:: :meth: scipy.special.erf
+    .. seealso:: :meth:`scipy.special.erf`
 
     ''',
     support_complex=False)
@@ -16,7 +16,7 @@ _erfc = ufunc.create_math_ufunc(
     'erfc', 1, 'cupyx_scipy_erfc',
     '''Complementary error function.
 
-    .. seealso:: :meth: scipy.special.erfc
+    .. seealso:: :meth:`scipy.special.erfc`
 
     ''',
     support_complex=False)
@@ -26,7 +26,7 @@ _erfcx = ufunc.create_math_ufunc(
     'erfcx', 1, 'cupyx_scipy_erfcx',
     '''Scaled complementary error function.
 
-    .. seealso:: :meth: scipy.special.erfcx
+    .. seealso:: :meth:`scipy.special.erfcx`
 
     ''',
     support_complex=False)
