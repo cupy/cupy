be1536c08c40573ed7f264d033d8b1be571ab998
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-08-31 12:27:47 +0900

    Merge pull request #1592 from okuta/reset-error-status
    Reset cuda runtime error

--- cupy/cuda/runtime.pyx
+++ cupy/cuda/runtime.pyx
@@ -51,6 +51,7 @@ cdef extern from "cupy_cuda.h" nogil:
     # Error handling
     const char* cudaGetErrorName(Error error)
     const char* cudaGetErrorString(Error error)
+    int cudaGetLastError()
 
     # Initialization
     int cudaDriverGetVersion(int* driverVersion)
@@ -132,6 +133,8 @@ class CUDARuntimeError(RuntimeError):
 @cython.profile(False)
 cpdef inline check_status(int status):
     if status != 0:
+        # to reset error status
+        cudaGetLastError()
         raise CUDARuntimeError(status)
 
 
