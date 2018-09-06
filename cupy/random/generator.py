41fa0b886e198f71b8710893152db65cc72c28b8
yoshikawa <yoshikawa@preferred.jp>
2018-09-07 08:32:20 +0900

    [fix] based on the comments

--- cupy/random/generator.py
+++ cupy/random/generator.py
@@ -108,6 +108,24 @@ class RandomState(object):
             self.rk_seed += cupy.core.internal.prod(size)
         return y
 
+    def chisquare(self, df, size=None, dtype=float):
+        """Returns an array of samples drawn from the chi-square distribution.
+
+        .. seealso::
+            :func:`cupy.random.chisquare` for full documentation,
+            :meth:`numpy.random.RandomState.chisquare`
+        """
+        df = cupy.asarray(df)
+        if size is None:
+            size = df.shape
+        y = cupy.zeros(shape=size, dtype=dtype)
+        _kernels.chisquare_kernel(df, self.rk_seed, y)
+        if size is None:
+            self.rk_seed += 1
+        else:
+            self.rk_seed += numpy.prod(size)
+        return y
+
     def dirichlet(self, alpha, size=None, dtype=float):
         """Returns an array of samples drawn from the dirichlet distribution.
 
@@ -127,24 +145,6 @@ class RandomState(object):
             self.rk_seed += 1
         else:
             self.rk_seed += cupy.core.internal.prod(size)
-        return y
-
-    def chisquare(self, df, size=None, dtype=float):
-        """Returns an array of samples drawn from the chi-square distribution.
-
-        .. seealso::
-            :func:`cupy.random.chisquare` for full documentation,
-            :meth:`numpy.random.RandomState.chisquare`
-        """
-        df = cupy.asarray(df)
-        if size is None:
-            size = df.shape
-        y = cupy.zeros(shape=size, dtype=dtype)
-        _kernels.chisquare_kernel(df, self.rk_seed, y)
-        if size is None:
-            self.rk_seed += 1
-        else:
-            self.rk_seed += numpy.prod(size)
         return y
 
     _laplace_kernel = core.ElementwiseKernel(
