28d64d33cfc24b17d9a782fa60428bd1499940f1
Masayuki Takagi <kamonama@gmail.com>
2018-09-12 16:30:42 +0900

    Support double in scatter_add

--- tests/cupyx_tests/test_scatter.py
+++ tests/cupyx_tests/test_scatter.py
@@ -8,11 +8,14 @@ import cupyx
 
 
 @testing.gpu
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float32, numpy.float64],
+}))
 class TestScatter(unittest.TestCase):
 
     def test_scatter_add(self):
-        a = cupy.zeros((3,), dtype=numpy.float32)
+        a = cupy.zeros((3,), dtype=self.dtype)
         i = cupy.array([1, 1], numpy.int32)
-        v = cupy.array([2., 1.], dtype=numpy.float32)
+        v = cupy.array([2., 1.], dtype=self.dtype)
         cupyx.scatter_add(a, i, v)
         testing.assert_array_equal(a, cupy.array([0, 3, 0]))
