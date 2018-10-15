5c9e7f64c84fe9e8a9dcc3293ca6edb665d5f62f
niboshi <niboshi000@gmail.com>
2018-10-15 19:20:06 +0900

    Skip comparison test in Python 2

--- tests/cupy_tests/cuda_tests/test_device.py
+++ tests/cupy_tests/cuda_tests/test_device.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pytest
+import six
 
 from cupy import cuda
 
@@ -65,6 +66,8 @@ class TestDeviceComparison(unittest.TestCase):
         with pytest.raises(TypeError):
             obj2 >= obj1
 
+    @unittest.skipIf(
+        six.PY2, 'Python 2 comparison result of objects is arbitrary')
     def test_comparison_other_type(self):
         self.check_comparison_other_type(cuda.Device(0), 0)
         self.check_comparison_other_type(cuda.Device(0), 1)
