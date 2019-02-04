548fef5de15a63a78684e58e9cb1bf2bcce7e07d
Ryosuke Okuta <okuta@preferred.jp>
2019-02-04 17:45:13 +0900

    Merge pull request #1995 from msakai/fix-cudnn-descriptor-leak
    define __dealloc__ instead of __del__ for cdef-classes to fix memory leak

--- cupy/cudnn.pyx
+++ cupy/cudnn.pyx
@@ -85,7 +85,7 @@ cdef class Descriptor:
         self.value = descriptor
         self.destroy = destroyer
 
-    def __del__(self):
+    def __dealloc__(self):
         if self.value:
             self.destroy(self.value)
             self.value = 0
@@ -528,7 +528,7 @@ cdef class _DescriptorArray:
     def __init__(self, destroyer):
         self._destroy = destroyer
 
-    def __del__(self):
+    def __dealloc__(self):
         for desc in self._value:
             self._destroy(desc)
 
