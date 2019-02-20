5f3e1edc68fc4d9948b8e8c2be6db040b160449c
UENISHI Kota <kota@preferred.jp>
2019-02-20 11:37:45 +0900

    Add cupy.cuda.nccl_enabled flag

--- tests/cupy_tests/cuda_tests/test_nccl.py
+++ tests/cupy_tests/cuda_tests/test_nccl.py
@@ -1,29 +1,30 @@
 import unittest
 
-from cupy.cuda import nccl
+from cupy import cuda
 from cupy.testing import attr
 
 
+@unittest.skipUnless(cuda.nccl_enabled, 'nccl is not installed')
 class TestNCCL(unittest.TestCase):
 
     @attr.gpu
     def test_single_proc_ring(self):
-        id = nccl.get_unique_id()
-        comm = nccl.NcclCommunicator(1, id, 0)
+        id = cuda.nccl.get_unique_id()
+        comm = cuda.nccl.NcclCommunicator(1, id, 0)
         assert 0 == comm.rank_id()
         comm.destroy()
 
     @attr.gpu
-    @unittest.skipUnless(nccl.get_version() >= 2400, "Using old NCCL")
+    @unittest.skipUnless(cuda.nccl.get_version() >= 2400, "Using old NCCL")
     def test_abort(self):
-        id = nccl.get_unique_id()
-        comm = nccl.NcclCommunicator(1, id, 0)
+        id = cuda.nccl.get_unique_id()
+        comm = cuda.nccl.NcclCommunicator(1, id, 0)
         comm.abort()
 
     @attr.gpu
-    @unittest.skipUnless(nccl.get_version() >= 2400, "Using old NCCL")
+    @unittest.skipUnless(cuda.nccl.get_version() >= 2400, "Using old NCCL")
     def test_check_async_error(self):
-        id = nccl.get_unique_id()
-        comm = nccl.NcclCommunicator(1, id, 0)
+        id = cuda.nccl.get_unique_id()
+        comm = cuda.nccl.NcclCommunicator(1, id, 0)
         comm.check_async_error()
         comm.destroy()
