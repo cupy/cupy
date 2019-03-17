e01600d0f116aa2282348ee84d8561cf473af4b8
Ryosuke Okuta <okuta@preferred.jp>
2019-03-18 02:14:48 +0900

    Add strides test

--- tests/cupy_tests/core_tests/test_ndarray_cuda_array_interface.py
+++ tests/cupy_tests/core_tests/test_ndarray_cuda_array_interface.py
@@ -1,13 +1,11 @@
 import unittest
 
-import numpy
-
 import cupy
 from cupy import core
 from cupy import testing
 
 
-class DummyObjectWithCudaArrayInterface():
+class DummyObjectWithCudaArrayInterface(object):
 
     def __init__(self, a):
         self.a = a
@@ -16,6 +14,7 @@ class DummyObjectWithCudaArrayInterface():
     def __cuda_array_interface__(self):
         desc = {
             'shape': self.a.shape,
+            'strides': self.a.strides,
             'typestr': self.a.dtype.str,
             'descr': self.a.dtype.descr,
             'data': (self.a.data.mem.ptr, False),
@@ -28,25 +27,22 @@ class DummyObjectWithCudaArrayInterface():
 class TestArrayUfunc(unittest.TestCase):
 
     @testing.for_all_dtypes_combination(names=['x_type', 'y_type'])
-    @testing.numpy_cupy_allclose(rtol=1e-6, accept_error=TypeError)
-    def check_array_scalar_op(self, op, xp, x_type, y_type, swap=False,
-                              no_bool=False, no_complex=False):
-        x_dtype = numpy.dtype(x_type)
-        y_dtype = numpy.dtype(y_type)
-        if no_bool and x_dtype == '?' and y_dtype == '?':
-            return xp.array(True)
-        if no_complex and (x_dtype.kind == 'c' or y_dtype.kind == 'c'):
-            return xp.array(True)
+    @testing.numpy_cupy_allclose(rtol=1e-6, accept_error=TypeError,
+                                 contiguous_check=False)
+    def check_array_scalar_op(self, op, xp, x_type, y_type, trans=False):
         a = xp.array([[1, 2, 3], [4, 5, 6]], x_type)
+        if trans:
+            a = a.T
+
         if xp is cupy:
             a = DummyObjectWithCudaArrayInterface(a)
-        if swap:
-            return getattr(xp, op)(y_type(3), a)
-        else:
-            return getattr(xp, op)(a, y_type(3))
+        return getattr(xp, op)(a, y_type(3))
 
     def test_add_scalar(self):
         self.check_array_scalar_op('add')
+
+    def test_add_scalar_with_strides(self):
+        self.check_array_scalar_op('add', trans=True)
 
 
 @testing.gpu
@@ -57,8 +53,12 @@ class SimpleReductionFunction(unittest.TestCase):
             'my_sum', ('b->b',), ('in0', 'a + b', 'out0 = a', None))
 
     @testing.numpy_cupy_allclose()
-    def check_int8_sum(self, shape, xp, axis=None, keepdims=False):
+    def check_int8_sum(self, shape, xp, axis=None, keepdims=False,
+                       trans=False):
         a = testing.shaped_random(shape, xp, 'b')
+        if trans:
+            a = a.T
+
         if xp == cupy:
             a = DummyObjectWithCudaArrayInterface(a)
             return self.my_int8_sum(
@@ -69,6 +69,9 @@ class SimpleReductionFunction(unittest.TestCase):
     def test_shape(self):
         self.check_int8_sum((2 ** 10,))
 
+    def test_shape_with_strides(self):
+        self.check_int8_sum((2 ** 10, 16), trans=True)
+
 
 @testing.gpu
 class TestReductionKernel(unittest.TestCase):
@@ -78,8 +81,12 @@ class TestReductionKernel(unittest.TestCase):
             'T x', 'T out', 'x', 'a + b', 'out = a', '0', 'my_sum')
 
     @testing.numpy_cupy_allclose()
-    def check_int8_sum(self, shape, xp, axis=None, keepdims=False):
+    def check_int8_sum(self, shape, xp, axis=None, keepdims=False,
+                       trans=False):
         a = testing.shaped_random(shape, xp, 'b')
+        if trans:
+            a = a.T
+
         if xp == cupy:
             a = DummyObjectWithCudaArrayInterface(a)
             return self.my_sum(
@@ -89,3 +96,6 @@ class TestReductionKernel(unittest.TestCase):
 
     def test_shape(self):
         self.check_int8_sum((2 ** 10,))
+
+    def test_shape_with_strides(self):
+        self.check_int8_sum((2 ** 10, 16), trans=True)
