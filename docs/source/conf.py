14f92cdc6168d251959048cd38708a6ae893a847
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2019-05-16 07:35:17 +0000

    Update intersphinx mapping

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -343,7 +343,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
     'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
-    'chainer': ('https://docs.chainer.org/en/latest/', None),
+    'chainer': ('https://docs.chainer.org/en/stable/', None),
 }
 
 doctest_global_setup = '''
