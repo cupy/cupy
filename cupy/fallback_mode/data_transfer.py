18c99ba187faa41a476742c2e112aece926decfa
Piyush-555 <piyushraikwar555@gmail.com>
2019-06-18 18:51:44 +0530

    Add comments

--- cupy/fallback_mode/data_transfer.py
+++ cupy/fallback_mode/data_transfer.py
@@ -7,6 +7,17 @@ import numpy as np
 
 
 def _get_xp_args(org_module, to_xp, arg):
+    """
+    Converts org_module.ndarray object using to_xp.
+
+    Args:
+        (org_module.ndarray, tuple, list, dict): These will be returned by
+        either converting the object or it's elements if object is iterable.
+        (int, float, str, numpy.ScalarType (constant)): Returned as it is.
+
+    Returns:
+        Return data structure will be same as before after converting ndarrays.
+    """
 
     if isinstance(arg, org_module.ndarray):
         return to_xp(arg)
