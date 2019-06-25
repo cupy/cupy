45c124ab5841af7335ae1578dd4a3951e0a78c38
Piyush-555 <piyushraikwar555@gmail.com>
2019-06-25 20:20:09 +0530

    Add geterr, seterr, dispatch_notification

--- cupyx/fallback_mode/utils.py
+++ cupyx/fallback_mode/utils.py
@@ -2,6 +2,10 @@
 Utilities needed for fallback_mode.
 """
 
+import logging
+import warnings
+import threading
+
 from cupyx.fallback_mode import data_transfer
 
 
@@ -41,3 +45,40 @@ def _call_numpy(func, args, kwargs):
     cupy_res = data_transfer._get_cupy_result(numpy_res)
 
     return cupy_res
+
+
+_thread_locals = threading.local()
+_thread_locals.dispatch_type = 'warn'
+
+FallbackWarning = type('FallbackWarning', (Warning,), {})
+warnings.simplefilter(action='always', category=FallbackWarning)
+
+
+def geterr():
+    return _thread_locals.dispatch_type
+
+
+def seterr(new_dispatch):
+
+    old = _thread_locals.dispatch_type
+
+    if new_dispatch in ['print', 'warn', 'log', 'ignore']:
+        _thread_locals.dispatch_type = new_dispatch
+        return old
+
+    raise ValueError('{} is not valid dispatch type'.format(new_dispatch))
+
+
+def dispatch_notification(func):
+
+    msg = "Requested method not in cupy, falling back to '{}.{}'".format(
+        func.__module__, func.__name__)
+
+    if _thread_locals.dispatch_type == 'print':
+        print("Warning: {}".format(msg))
+
+    elif _thread_locals.dispatch_type == 'warn':
+        warnings.warn(msg, FallbackWarning)
+
+    elif _thread_locals.dispatch_type == 'log':
+        logging.warning(msg)
