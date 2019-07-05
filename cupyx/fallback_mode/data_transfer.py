e5380f7b0d5504b38645e9407ded7a6363536c68
Piyush-555 <piyushraikwar555@gmail.com>
2019-07-05 19:09:37 +0530

    Update _get_xp_args doc

--- cupyx/fallback_mode/data_transfer.py
+++ cupyx/fallback_mode/data_transfer.py
@@ -10,12 +10,14 @@ import cupy as cp
 def _get_xp_args(ndarray_instance, to_xp, arg):
     """
     Converts ndarray_instance type object to target object using to_xp.
-    ndarray_instance: numpy.ndarray, cupy.ndarray or utils.ndarray
 
     Args:
-        (ndarray_instance, tuple, list, dict): These will be returned by
-        either converting the object or it's elements if object is iterable.
-        Everything else is returned as it is.
+        ndarray_instance (numpy.ndarray, cupy.ndarray or utils.ndarray):
+        Objects of type `ndarray_instance` will be converted using `to_xp`.
+        to_xp (FunctionType): Method to convert ndarray_instance type objects.
+        arg (object): `ndarray_instance`, `tuple`, `list` and `dict` type objects
+        will be returned by either converting the object or it's elements
+        if object is iterable. Objects of other types is returned as it is.
 
     Returns:
         Return data structure will be same as before after converting ndarrays.
