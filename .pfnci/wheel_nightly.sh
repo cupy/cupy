918f8a0e9464fc308f472b592623e1cfd6353d09
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-08-20 19:12:40 +0900

    Make wheel of master for CI (#2144)
    make wheel of master for CI

---
+++ .pfnci/wheel_nightly.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+set -eux
+
+systemctl stop docker.service
+mount -t tmpfs tmpfs /var/lib/docker/
+systemctl start docker.service
+
+TEMP=$(mktemp -d)
+mount -t tmpfs tmpfs ${TEMP}/
+cp -a . ${TEMP}/
+cd ${TEMP}/
+
+echo -n 2.7 3.6 | xargs -i -d ' ' -P $(nproc) sh -euxc '
+PYTHON={}
+
+docker build \
+       --build-arg PYTHON=${PYTHON} \
+       -t devel:${PYTHON} \
+       .pfnci/docker/devel/
+docker run --rm \
+       --volume $(pwd):/cupy/ --workdir /cupy/ \
+       devel:${PYTHON} \
+       pip${PYTHON} wheel -e .
+'
+
+gsutil -q cp cupy-*.whl gs://tmp-pfn-public-ci/cupy/wheel/${CI_COMMIT_ID}/cuda9.2/
+echo ${CI_COMMIT_ID} | gsutil -q cp - gs://tmp-pfn-public-ci/cupy/wheel/master
