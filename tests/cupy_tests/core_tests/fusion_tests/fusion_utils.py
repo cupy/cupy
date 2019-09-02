7f340bc17a3c2d4854fc44e9f0ad88a63a5c75a4
Akifumi Imanishi <imanishi@preferred.jp>
2019-09-02 05:44:55 +0000

    Write comments

--- tests/cupy_tests/core_tests/fusion_tests/fusion_utils.py
+++ tests/cupy_tests/core_tests/fusion_tests/fusion_utils.py
@@ -16,7 +16,8 @@ def check_fusion(
         accept_error=()):
     """Decorator for tests for ``cupy.fuse``.
 
-    This decorator checks
+    This decorator checks the results of the original function is equals to
+    that of the fused function.
 
     Args:
         generate_input_args(tuple): argument tuple passed to
@@ -62,7 +63,10 @@ def check_fusion(
         else:
             assert False
 
-    def run(func, args, kwargs):
+    # Calls `func` with the arguments `args` and `kwargs`, and returns
+    # the tuple of the return value and the exception object raised by the
+    # function.
+    def call(func, args, kwargs):
         try:
             ret = func(*args, **kwargs)
             err = None
@@ -78,7 +82,7 @@ def check_fusion(
         ret_e, err_e = expected
 
         if err_e is None:
-            # Success
+            # Exception not raised
             assert err_a is None
             check(xp, ret_a, ret_e)
 
@@ -91,6 +95,8 @@ def check_fusion(
 
             # TODO(imanishi): Fix these workaround after `cupy.fuse`
             # supports lambda function.
+            # If `cupy.fuse` supports lambda function, these lines can be
+            # written more simply (as `impl_fuse_np = cupy.fuse(impl_np)`).
             @cupy.fuse()
             def impl_fuse_np(*args, **kwargs):
                 return impl_np(*args, **kwargs)
@@ -108,10 +114,10 @@ def check_fusion(
             args_fuse_cp, kwargs_fuse_cp = generate_inputs(
                 cupy, *generate_inputs_args, **generate_inputs_kwargs)
 
-            result_np = run(impl_np, args_np, kwargs_np)
-            result_cp = run(impl_cp, args_cp, kwargs_cp)
-            result_fuse_np = run(impl_fuse_np, args_fuse_np, kwargs_fuse_np)
-            result_fuse_cp = run(impl_fuse_cp, args_fuse_cp, kwargs_fuse_cp)
+            result_np = call(impl_np, args_np, kwargs_np)
+            result_cp = call(impl_cp, args_cp, kwargs_cp)
+            result_fuse_np = call(impl_fuse_np, args_fuse_np, kwargs_fuse_np)
+            result_fuse_cp = call(impl_fuse_cp, args_fuse_cp, kwargs_fuse_cp)
 
             check_result(cupy, result_cp, result_np)
             check_result(numpy, result_fuse_np, result_np)
