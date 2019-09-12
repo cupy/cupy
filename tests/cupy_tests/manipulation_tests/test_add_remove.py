08c7c4952072bb81521872bf0fd274b590bc2485
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2019-09-12 16:31:46 +0900

    add test

--- tests/cupy_tests/manipulation_tests/test_add_remove.py
+++ tests/cupy_tests/manipulation_tests/test_add_remove.py
@@ -4,6 +4,78 @@ from cupy import testing
 
 
 @testing.gpu
+class TestAppend(unittest.TestCase):
+
+    @testing.numpy_cupy_array_equal()
+    def test(self, xp):
+        a = testing.shaped_random((3, 4, 5), xp, xp.float32)
+        b = testing.shaped_random((6, 7), xp, xp.float32)
+        return xp.append(a, b)
+
+    @testing.numpy_cupy_array_equal()
+    def test_scalar_lhs(self, xp):
+        return xp.append(10, xp.arange(20))
+
+    @testing.numpy_cupy_array_equal()
+    def test_scalar_rhs(self, xp):
+        return xp.append(xp.arange(20), 10)
+
+    @testing.numpy_cupy_array_equal()
+    def test_scalar_both(self, xp):
+        return xp.append(10, 10)
+
+    @testing.numpy_cupy_array_equal()
+    def test_axis(self, xp):
+        a = testing.shaped_random((3, 4, 5), xp, xp.float32)
+        b = testing.shaped_random((3, 10, 5), xp, xp.float32)
+        return xp.append(a, b, axis=1)
+
+    @testing.numpy_cupy_array_equal()
+    def test_zerodim(self, xp):
+        return xp.append(xp.array(0), xp.arange(10))
+
+    @testing.numpy_cupy_array_equal()
+    def test_empty(self, xp):
+        return xp.append(xp.array([]), xp.arange(10))
+
+
+@testing.gpu
+class TestResize(unittest.TestCase):
+
+    @testing.numpy_cupy_array_equal()
+    def test(self, xp):
+        return xp.resize(xp.arange(10), (10, 10))
+
+    @testing.numpy_cupy_array_equal()
+    def test_remainder(self, xp):
+        return xp.resize(xp.arange(8), (10, 10))
+
+    @testing.numpy_cupy_array_equal()
+    def test_shape_int(self, xp):
+        return xp.resize(xp.arange(10), 15)
+
+    @testing.numpy_cupy_array_equal()
+    def test_scalar(self, xp):
+        return xp.resize(2, (10, 10))
+
+    @testing.numpy_cupy_array_equal()
+    def test_scalar_shape_int(self, xp):
+        return xp.resize(2, 10)
+
+    @testing.numpy_cupy_array_equal()
+    def test_typed_scalar(self, xp):
+        return xp.resize(xp.float32(10.0), (10, 10))
+
+    @testing.numpy_cupy_array_equal()
+    def test_zerodim(self, xp):
+        return xp.resize(xp.array(0), (10, 10))
+
+    @testing.numpy_cupy_array_equal()
+    def test_empty(self, xp):
+        return xp.resize(xp.array([]), (10, 10))
+
+
+@testing.gpu
 class TestUnique(unittest.TestCase):
 
     @testing.for_all_dtypes(no_float16=True, no_bool=True, no_complex=True)
