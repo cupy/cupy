c913b406dc6893f056b0ad9abc8340388d55ac69
Joe <xuzijian@g.ecc.u-tokyo.ac.jp>
2019-09-12 08:32:02 +0000

    Update run.py

--- benchmarks/cupy-perf/run.py
+++ benchmarks/cupy-perf/run.py
@@ -7,6 +7,17 @@ import cupy_perf
 class Perf1(cupy_perf.PerfCases):
     enable_line_profiler = False
 
+    def add_f(self, f):
+        name = f.__name__
+        setattr(self, name, f)
+        setattr(self, name + '_fuse', cupy.fuse(f))
+        setattr(self, name + '_fusex', cupy.fusex(f))
+
+    def add_f2(self, f):
+        name = f.__name__
+        setattr(self, name, f)
+        setattr(self, name + '_fusex', cupy.fusex(f))
+
     def setUp(self):
         shape_tiny = (2, 1, 2)
         shape_huge = (2000, 50, 100)
@@ -17,42 +28,49 @@ class Perf1(cupy_perf.PerfCases):
         self.b_huge = cupy.empty(shape_huge, numpy.float32)
         self.c_huge = cupy.empty(shape_huge, numpy.float32)
 
-        self.kernel = self._get_kernel()
+        def add20(x):
+            return x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x
 
-    def _get_kernel(self):
-        return cupy.ElementwiseKernel(
-            '''T a, T b''', '''T c''',
-            '''c = a + b;''', 'test_kernel')
+        self.add_f(add20)
 
-    def perf_empty(self):
-        pass
+        def sum(x):
+            return cupy.sum(x)
+
+        self.add_f(add20)
+        self.add_f(sum)
+
+        def sum10(x):
+            return cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x) + cupy.sum(x)
+
+        self.add_f2(sum10)
+
+    def perf_add20(self):
+        self.add20(self.a)
+
+    def perf_add20_fuse(self):
+        self.add20_fuse(self.a)
+
+    def perf_add20_fusex(self):
+        self.add20_fusex(self.a)
 
     def perf_sum(self):
-        cupy.sum(self.a)
+        return self.sum(self.a)
 
-    @cupy_perf.attr(n=500)
-    def perf_sum_huge(self):
-        a = cupy.sum(self.a_huge)
-        assert a.shape == ()
+    def perf_sum_fuse(self):
+        return self.sum_fuse(self.a)
 
-    def perf_add(self):
-        cupy.add(self.a, self.b)
+    def perf_sum_fusex(self):
+        return self.sum_fusex(self.a)
 
-    def perf_add_out(self):
-        cupy.add(self.a, self.b, out=self.c)
+    def perf_sum10(self):
+        return self.sum10(self.a)
 
-    def perf_add_out_huge(self):
-        cupy.add(self.a_huge, self.b_huge, out=self.c_huge)
+    def perf_sum10_fusex(self):
+        return self.sum10_fusex(self.a)
 
-    def perf_userkernel_create(self):
-        self._get_kernel()
-
-    def perf_userkenel(self):
-        self.kernel(self.a, self.b, self.c)
-
-    @cupy_perf.attr(n=10000)
-    def perf_userkernel_huge(self):
-        self.kernel(self.a_huge, self.b_huge, self.c_huge)
-
+    # @cupy_perf.attr(n=500)
+    # def perf_sum_huge(self):
+    #     a = cupy.sum(self.a_huge)
+    #     assert a.shape == ()
 
 cupy_perf.run(__name__)
