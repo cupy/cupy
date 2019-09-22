f1528cadf2eda6b49660dca3227537433a6bbcef
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-09-22 13:02:21 +0530

    modified docstrings

--- cupy/logic/truth.py
+++ cupy/logic/truth.py
@@ -63,30 +63,23 @@ def in1d(ar1, ar2, assume_unique=False, invert=False):
     """
     Test whether each element of a 1-D array is also present in a second array.
 
-    Returns a boolean array the same length as `ar1` that is True
-    where an element of `ar1` is in `ar2` and False otherwise.
+    Returns a boolean array the same length as ``ar1`` that is True
+    where an element of ``ar1`` is in ``ar2`` and False otherwise.
 
-    We recommend using :func:`isin` instead of `in1d` for new code.
+    Args:
+        ar1 (cupy.ndarray): Input array.
+        ar2  (cupy.ndarray):
+            The values against which to test each value of ``ar1``.
+        assume_unique (bool, optional):
+            If True, the input arrays are both assumed to be unique, which
+            can speed up the calculation. Default is False.
+        invert(bool, optional):
+            If True, the values in the returned array are inverted (that is,
+            False where an element of ``ar1`` is in ``ar2`` and True otherwise)
+            Default is False.
 
-    Parameters
-    ----------
-    ar1 : (M,) cupy.ndarray
-        Input array.
-    ar2 : cupy.ndarray
-        The values against which to test each value of `ar1`.
-    assume_unique : bool, optional
-        If True, the input arrays are both assumed to be unique, which
-        can speed up the calculation.  Default is False.
-    invert : bool, optional
-        If True, the values in the returned array are inverted (that is,
-        False where an element of `ar1` is in `ar2` and True otherwise).
-        Default is False. ``np.in1d(a, b, invert=True)`` is equivalent
-        to (but is faster than) ``np.invert(in1d(a, b))``.
-
-    Returns
-    -------
-    in1d : (M,) cupy.ndarray, bool
-        The values `ar1[in1d]` are in `ar2`.
+    Returns:
+        cupy.ndarray, bool: The values ``ar1[in1d]`` are in ``ar2``.
 
     """
     # Ravel both arrays, behavior for the first array could be different
@@ -132,32 +125,29 @@ def in1d(ar1, ar2, assume_unique=False, invert=False):
 
 def isin(element, test_elements, assume_unique=False, invert=False):
     """
-    Calculates `element in test_elements`, broadcasting over `element` only.
-    Returns a boolean array of the same shape as `element` that is True
-    where an element of `element` is in `test_elements` and False otherwise.
+    Calculates ``element in test_elements``, broadcasting over ``element`` only
+    Returns a boolean array of the same shape as ``element`` that is True
+    where an element of ``element`` is in ``test_elements``
+    and False otherwise.
 
-    Parameters
-    ----------
-    element : cupy.ndarray
-        Input array.
-    test_elements : cupy.ndarray
-        The values against which to test each value of `element`.
-        This argument is flattened if it is an array or array_like.
-        See notes for behavior with non-array-like parameters.
-    assume_unique : bool, optional
-        If True, the input arrays are both assumed to be unique, which
-        can speed up the calculation.  Default is False.
-    invert : bool, optional
-        If True, the values in the returned array are inverted, as if
-        calculating `element not in test_elements`. Default is False.
-        ``np.isin(a, b, invert=True)`` is equivalent to (but faster
-        than) ``np.invert(np.isin(a, b))``.
 
-    Returns
-    -------
-    isin : cupy.ndarray, bool
-        Has the same shape as `element`. The values `element[isin]`
-        are in `test_elements`.
+    Args:
+        element (cupy.ndarray): Input array.
+        test_elements (cupy.ndarray):
+            The values against which to test each value of `element`.
+            This argument is flattened if it is an array or array_like.
+            See notes for behavior with non-array-like parameters.
+        assume_unique (bool, optional):
+            If True, the input arrays are both assumed to be unique, which
+            can speed up the calculation.  Default is False.
+        invert (bool, optional):
+            If True, the values in the returned array are inverted, as if
+            calculating `element not in test_elements`. Default is False.
+
+    Returns:
+        cupy.ndarray, bool:
+            Has the same shape as `element`. The values `element[isin]`
+            are in `test_elements`.
 
     """
     element = cupy.asarray(element)
