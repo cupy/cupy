15174799d0c0f902d23c8cfdf7a9739b3986cc18
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-09-27 21:00:41 +0530

    Added suggested changes

--- cupy/__init__.py
+++ cupy/__init__.py
@@ -488,8 +488,8 @@ from cupy.logic.type_test import isfortran  # NOQA
 from cupy.logic.type_test import isreal  # NOQA
 from cupy.logic.type_test import isrealobj  # NOQA
 
-from cupy.logic.truth import isin  # NOQA
 from cupy.logic.truth import in1d  # NOQA
+from cupy.logic.truth import isin  # NOQA
 
 
 def isscalar(num):
--- cupy/logic/truth.py
+++ cupy/logic/truth.py
@@ -65,18 +65,19 @@ def in1d(ar1, ar2, assume_unique=False, invert=False):
     array.
 
     Returns a boolean array the same length as ``ar1`` that is ``True``
-    where an element of ``ar1`` is in ``ar2`` and False otherwise.
+    where an element of ``ar1`` is in ``ar2`` and ``False`` otherwise.
+
     Args:
         ar1 (cupy.ndarray): Input array.
         ar2  (cupy.ndarray):
             The values against which to test each value of ``ar1``.
         assume_unique (bool, optional):
-            If True, the input arrays are both assumed to be unique, which
-            can speed up the calculation. Default is False.
+            If ``True``, the input arrays are both assumed to be unique, which
+            can speed up the calculation. Default is ``False``.
         invert(bool, optional):
-            If True, the values in the returned array are inverted (that is,
-            False where an element of ``ar1`` is in ``ar2`` and True otherwise)
-            Default is False.
+            If ``True``, the values in the returned array are inverted (that
+            is, ``False`` where an element of ``ar1`` is in ``ar2`` and
+            ``True`` otherwise). Default is ``False``.
 
     Returns:
         cupy.ndarray, bool: The values ``ar1[in1d]`` are in ``ar2``.
@@ -124,28 +125,26 @@ def in1d(ar1, ar2, assume_unique=False, invert=False):
 def isin(element, test_elements, assume_unique=False, invert=False):
     """
     Calculates element in ``test_elements``, broadcasting over ``element`` only
-    Returns a boolean array of the same shape as ``element`` that is True
+    Returns a boolean array of the same shape as ``element`` that is ``True``
     where an element of ``element`` is in ``test_elements``
-    and False otherwise.
-
+    and ``False`` otherwise.
 
     Args:
         element (cupy.ndarray): Input array.
         test_elements (cupy.ndarray):
-            The values against which to test each value of `element`.
+            The values against which to test each value of ``element``.
             This argument is flattened if it is an array or array_like.
-            See notes for behavior with non-array-like parameters.
         assume_unique (bool, optional):
-            If True, the input arrays are both assumed to be unique, which
-            can speed up the calculation.  Default is False.
+            If ``True``, the input arrays are both assumed to be unique, which
+            can speed up the calculation.  Default is ``False``.
         invert (bool, optional):
-            If True, the values in the returned array are inverted, as if
-            calculating `element not in test_elements`. Default is False.
+            If ``True``, the values in the returned array are inverted, as if
+            calculating element not in ``test_elements``. Default is ``False``.
 
     Returns:
         cupy.ndarray, bool:
-            Has the same shape as `element`. The values `element[isin]`
-            are in `test_elements`.
+            Has the same shape as ``element``. The values ``element[isin]``
+            are in ``test_elements``.
 
     """
     return in1d(element, test_elements, assume_unique=assume_unique,
