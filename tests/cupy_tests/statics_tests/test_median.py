88e12f812e85ae6d3c646ebc33236b5b6da98df0
andy6975 <shivam.iitmandi@gmail.com>
2019-10-05 04:20:47 +0530

    Added test_median.py

--- tests/cupy_tests/statics_tests/test_median.py
+++ tests/cupy_tests/statics_tests/test_median.py
@@ -11,45 +11,45 @@ ignore_runtime_warnings = pytest.mark.filterwarnings(
 
 
 @testing.gpu
-class TestAverage(unittest.TestCase):
+class TestMedian(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose()
-    def test_average_all(self, xp, dtype):
+    def test_median_all(self, xp, dtype):
         a = testing.shaped_arange((2, 3), xp, dtype)
-        return xp.average(a)
+        return xp.median(a)
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose()
-    def test_average_axis(self, xp, dtype):
+    def test_median_axis(self, xp, dtype):
         a = testing.shaped_arange((2, 3, 4), xp, dtype)
-        return xp.average(a, axis=1)
+        return xp.median(a, axis=1)
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose()
-    def test_average_weights(self, xp, dtype):
+    def test_median_weights(self, xp, dtype):
         a = testing.shaped_arange((2, 3), xp, dtype)
         w = testing.shaped_arange((2, 3), xp, dtype)
-        return xp.average(a, weights=w)
+        return xp.median(a, weights=w)
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose()
-    def test_average_axis_weights(self, xp, dtype):
+    def test_median_axis_weights(self, xp, dtype):
         a = testing.shaped_arange((2, 3, 4), xp, dtype)
         w = testing.shaped_arange((2, 3, 4), xp, dtype)
-        return xp.average(a, axis=2, weights=w)
+        return xp.median(a, axis=2, weights=w)
 
     def check_returned(self, a, axis, weights):
-        average_cpu, sum_weights_cpu = numpy.average(
+        median_cpu, sum_weights_cpu = numpy.median(
             a, axis, weights, returned=True)
-        result = cupy.average(
+        result = cupy.median(
             cupy.asarray(a), axis, weights, returned=True)
         self.assertTrue(isinstance(result, tuple))
         self.assertEqual(len(result), 2)
-        average_gpu, sum_weights_gpu = result
-        testing.assert_allclose(average_cpu, average_gpu)
+        median_gpu, sum_weights_gpu = result
+        testing.assert_allclose(median_cpu, median_gpu)
         testing.assert_allclose(sum_weights_cpu, sum_weights_gpu)
 
     @testing.for_all_dtypes()
