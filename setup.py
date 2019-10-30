476f032cf64ec4b92c54b8211b347574229105a3
Ryosuke Okuta <okuta@preferred.jp>
2019-10-30 21:24:54 +0900

    Fix classifiers

--- setup.py
+++ setup.py
@@ -143,12 +143,11 @@ Intended Audience :: Science/Research
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3 :: Only
 Programming Language :: Python :: Implementation :: Cython
 Topic :: Software Development
 Topic :: Scientific/Engineering
