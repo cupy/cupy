02b8447024b92e52db11bff9964f71d387a7606c
Gregory R. Lee <gregory.lee@cchmc.org>
2019-11-07 17:32:14 -0500

    TST: have parameterize generate descriptive test case ids

--- cupy/testing/parameterized.py
+++ cupy/testing/parameterized.py
@@ -1,15 +1,84 @@
+import enum
 import functools
 import inspect
 import itertools
+import re
 import sys
 import types
 import unittest
 
 import six
 
+STRING_TYPES = bytes, str
+
+# The type of re.compile objects is not exposed in Python.
+REGEX_TYPE = type(re.compile(""))
+
+_non_printable_ascii_translate_table = {
+    i: "\\x{:02x}".format(i) for i in range(128) if i not in range(32, 127)
+}
+_non_printable_ascii_translate_table.update(
+    {ord("\t"): "\\t", ord("\r"): "\\r", ord("\n"): "\\n"}
+)
+
+
+def _translate_non_printable(s):
+    return s.translate(_non_printable_ascii_translate_table)
+
+
+def _ascii_escaped(val):
+    """If val is pure ascii, returns it as a str().  Otherwise, escapes
+    bytes objects into a sequence of escaped bytes:
+
+    b'\xc3\xb4\xc5\xd6' -> '\\xc3\\xb4\\xc5\\xd6'
+
+    and escapes unicode objects into a sequence of escaped unicode
+    ids, e.g.:
+
+    '4\\nV\\U00043efa\\x0eMXWB\\x1e\\u3028\\u15fd\\xcd\\U0007d944'
+
+    note:
+       the obvious "v.decode('unicode-escape')" will return
+       valid utf-8 unicode if it finds them in bytes, but we
+       want to return escaped bytes for any byte, even if they match
+       a utf-8 string.
+
+    """
+    if isinstance(val, bytes):
+        ret = _bytes_to_ascii(val)
+    else:
+        ret = val.encode("unicode_escape").decode("ascii")
+    return _translate_non_printable(ret)
+
+
+def _bytes_to_ascii(val):
+    return val.decode("ascii", "backslashreplace")
+
+
+def _idval(val, argname, i):
+    if isinstance(val, STRING_TYPES):
+        return _ascii_escaped(val)
+    elif val is None or isinstance(val, (float, int, bool, enum.Enum)):
+        return str(val)
+    elif isinstance(val, REGEX_TYPE):
+        return _ascii_escaped(val.pattern)
+    elif ((inspect.isclass(val) or inspect.isfunction(val)) and
+            hasattr(val, "__name__")):
+        return val.__name__
+    return str(argname) + str(i)
+
+
+def _idmaker(param, i):
+    """Generate a descriptive id for the parameter set.
+
+    This is simplified from private code internal to pytest.
+    """
+    ids = [_idval(v, k, i) for k, v in param.items()]
+    return '[' + '-'.join(ids) + ']'
+
 
 def _gen_case(base, module, i, param):
-    cls_name = '%s_param_%d' % (base.__name__, i)
+    cls_name = '%s%s' % (base.__name__, _idmaker(param, i))
 
     # Add parameters as members
 
