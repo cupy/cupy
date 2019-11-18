3fbe9f7630f128a1d98b85ff933d0a3d722cde21
Emilio Castillo <ecastill@preferred.jp>
2019-11-18 01:46:39 +0000

    Add support of complex dtypes for `sinc`

--- cupy/math/special.py
+++ cupy/math/special.py
@@ -12,8 +12,9 @@ i0 = ufunc.create_math_ufunc(
 
 
 sinc = core.create_ufunc(
-    'cupy_sinc', ('e->e', 'f->f', 'd->d'),
-    'out0 = abs(in0) > 1e-9 ? sinpi(in0) / (M_PI * in0) : 1',
+    'cupy_sinc', ('e->e', 'f->f', 'd->d', 'F->F', 'D->D'),
+    'in0_type pi_in0 = (in0_type) M_PI * in0;'
+    'out0 = abs(in0) > 1e-9 ? sin(pi_in0) / (pi_in0) : 1',
     doc='''Elementwise sinc function.
 
     .. seealso:: :func:`numpy.sinc`
