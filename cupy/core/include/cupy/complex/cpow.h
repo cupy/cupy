7e8907b13880a3c427d5a01b68673475dc98c854
Leo Fang <leofang@bnl.gov>
2019-12-02 01:21:06 -0500

    add __host__ qualifiers

--- cupy/core/include/cupy/complex/cpow.h
+++ cupy/core/include/cupy/complex/cpow.h
@@ -5,8 +5,8 @@
 namespace thrust {
 
 template <typename T>
-__device__ inline complex<T> pow(const complex<T>& z,
-                                 const complex<T>& exponent) {
+__host__ __device__ inline complex<T> pow(const complex<T>& z,
+                                          const complex<T>& exponent) {
   const T absz = abs(z);
   if (absz == 0) {
     return complex<T>(0, 0);
@@ -24,31 +24,31 @@ __device__ inline complex<T> pow(const complex<T>& z,
 }
 
 template <typename T>
-__device__ inline complex<T> pow(const complex<T>& z, const T& exponent) {
+__host__ __device__ inline complex<T> pow(const complex<T>& z, const T& exponent) {
   return pow(z, complex<T>(exponent));
 }
 
 template <typename T>
-__device__ inline complex<T> pow(const T& x, const complex<T>& exponent) {
+__host__ __device__ inline complex<T> pow(const T& x, const complex<T>& exponent) {
   return pow(complex<T>(x), exponent);
 }
 
 template <typename T, typename U>
-__device__ inline complex<typename _select_greater_type<T, U>::type> pow(
+__host__ __device__ inline complex<typename _select_greater_type<T, U>::type> pow(
     const complex<T>& z, const complex<T>& exponent) {
   typedef typename _select_greater_type<T, U>::type PromotedType;
   return pow(complex<PromotedType>(z), complex<PromotedType>(exponent));
 }
 
 template <typename T, typename U>
-__device__ inline complex<typename _select_greater_type<T, U>::type> pow(
+__host__ __device__ inline complex<typename _select_greater_type<T, U>::type> pow(
     const complex<T>& z, const U& exponent) {
   typedef typename _select_greater_type<T, U>::type PromotedType;
   return pow(complex<PromotedType>(z), PromotedType(exponent));
 }
 
 template <typename T, typename U>
-__device__ inline complex<typename _select_greater_type<T, U>::type> pow(
+__host__ __device__ inline complex<typename _select_greater_type<T, U>::type> pow(
     const T& x, const complex<U>& exponent) {
   typedef typename _select_greater_type<T, U>::type PromotedType;
   return pow(PromotedType(x), complex<PromotedType>(exponent));
