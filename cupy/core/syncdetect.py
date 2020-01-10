0dc9751dc4af57db4bb87654a994609718a175e5
niboshi <niboshi000@gmail.com>
2020-01-10 05:44:49 +0000

    Make cupyx.allow_synchronize experimental

--- cupy/core/syncdetect.py
+++ cupy/core/syncdetect.py
@@ -1,6 +1,8 @@
 import contextlib
 import threading
 
+from cupy import util
+
 
 _thread_local = threading.local()
 
@@ -45,7 +47,7 @@ thread.
     Note that there can be false negatives and positives.
     Device synchronization outside CuPy will not be detected.
     """
-
+    util.experimental('cupyx.allow_synchronize')
     old = _is_allowed()
     _thread_local.allowed = allow
     try:
