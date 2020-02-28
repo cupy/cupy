0357799cc65f67ec6c4a048f6d855df8f5af34ab
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-02-28 19:57:23 +0530

    Handle methods not having __module__

--- cupyx/fallback_mode/notification.py
+++ cupyx/fallback_mode/notification.py
@@ -21,11 +21,24 @@ def _dispatch_notification(func):
     dispatch_type = _ufunc_config.get_config_fallback_mode()
     callback_func = _ufunc_config.get_fallback_mode_callback()
 
-    msg = "'{}' method not in cupy, falling back to '{}.{}'".format(
-        func.__name__, func.__module__, func.__name__)
+    _module = hasattr(func, '__module__')
+    _name = hasattr(func, '__name__')
 
-    raise_msg = "'{}' method not found in cupy".format(
-        func.__name__)
+    if _name and _module:
+        msg = "'{}' method not in cupy, falling back to '{}.{}'".format(
+            func.__name__, func.__module__, func.__name__)
+    elif _name:
+        msg = "'{}' method not in cupy, ".format(func.__name__)
+        msg += "falling back to numpy implementation of '{}'".format(
+            func.__name__)
+    else:
+        msg = "This method is not available in cupy, "
+        msg += "falling back to numpy"
+
+    if _name:
+        raise_msg = "'{}' method not found in cupy".format(func.__name__)
+    else:
+        raise_msg = "This method is not available in cupy"
 
     if dispatch_type == 'print':
         print("Warning: {}".format(msg))
