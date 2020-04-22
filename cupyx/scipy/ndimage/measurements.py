088476d143809758bb891a29e87d7da99d47ad51
niteya-shah <niteya.56@gmail.com>
2020-04-22 16:06:10 +0530

    small bugfix

--- cupyx/scipy/ndimage/measurements.py
+++ cupyx/scipy/ndimage/measurements.py
@@ -262,8 +262,8 @@ def _ndimage_mean_kernel_2(input, labels, index, batch_size=4,
     for i in range(0, index.size, batch_size):
         matched = labels == index[i:i + batch_size].reshape(
             (-1,) + (1,) * input.ndim)
-        count[i:i + batch_size] = matched.sum(axis=1)
         mean_axes = tuple(range(1, 1 + input.ndim))
+        count[i:i + batch_size] = matched.sum(axis=mean_axes)
         out[i:i + batch_size] = cupy.where(matched, input, 0).sum(
             axis=mean_axes)
     if return_count:
