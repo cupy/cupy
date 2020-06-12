aa9637f31341481db650588e9f348d7c5a9f2497
Leo Fang <leofang@bnl.gov>
2020-06-12 12:07:49 -0400

    revert lexsort key order

--- cupyx/scipy/sparse/coo.py
+++ cupyx/scipy/sparse/coo.py
@@ -215,7 +215,7 @@ class coo_matrix(sparse_data._data_matrix):
         if self.data.size == 0:
             self._has_canonical_format = True
             return
-        keys = cupy.stack([self.row, self.col])
+        keys = cupy.stack([self.col, self.row])
         order = cupy.lexsort(keys)
         src_data = self.data[order]
         src_row = self.row[order]
