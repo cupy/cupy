4f7bdb6d03e7e9556d24b60f7c34c4792f979074
Masayuki Takagi <kamonama@gmail.com>
2020-06-30 06:17:49 +0000

    Support `axis=None` in sparse min/max

--- tests/cupyx_tests/scipy_tests/sparse_tests/test_csr.py
+++ tests/cupyx_tests/scipy_tests/sparse_tests/test_csr.py
@@ -1026,6 +1026,33 @@ class TestCsrMatrixScipyCompressed(unittest.TestCase):
 @testing.with_requires('scipy>=0.19.0')
 class TestCsrMatrixScipyCompressedMinMax(unittest.TestCase):
 
+    def test_min_sparse_axis_none(self):
+        dm_data = numpy.random.random((10, 20))
+        dm_data[dm_data < 0.95] = 0
+
+        dm_data = scipy.sparse.csr_matrix(dm_data)
+        cp_matrix = sparse.csr_matrix((cupy.array(dm_data.data),
+                                       cupy.array(dm_data.indices),
+                                       cupy.array(dm_data.indptr)),
+                                      shape=(10, 20))
+
+        da_cupy_values = cp_matrix.min()
+        da_scipy_values = dm_data.min()
+        testing.assert_array_equal(da_cupy_values, da_scipy_values)
+
+    def test_min_sparse_axis_none_nonzero(self):
+        dm_data = numpy.arange(0, 100, 1).reshape((10, 10)).astype(float)
+
+        dm_sparse = scipy.sparse.csr_matrix(dm_data)
+        cp_matrix = sparse.csr_matrix((cupy.array(dm_sparse.data),
+                                       cupy.array(dm_sparse.indices),
+                                       cupy.array(dm_sparse.indptr)),
+                                      shape=(10, 10))
+
+        da_cupy_values = cp_matrix.min(nonzero=True)
+        da_numpy_values = numpy.array(1).astype(float)
+        testing.assert_array_equal(da_cupy_values, da_numpy_values)
+
     def test_min_sparse_axis_0(self):
         dm_data = numpy.random.random((10, 20))
         dm_data[dm_data < 0.95] = 0
