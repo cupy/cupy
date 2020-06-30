cupy/cupy

commit 46c8ed2302ab27f240ce3a603d28f0f156af8f19

remove unnecessary flag assignments

cupyx/scipy/sparse/csc.py
@@ -279,11 +279,8 @@ class csc_matrix(compressed._compressed_sparse_matrix):
                 'swapping dimensions is the only logical permutation.')
 
         shape = self.shape[1], self.shape[0]
-        trans = cupyx.scipy.sparse.csr.csr_matrix(
+        return cupyx.scipy.sparse.csr.csr_matrix(
             (self.data, self.indices, self.indptr), shape=shape, copy=copy)
-        trans.has_canonical_format = self.has_canonical_format
-        trans.has_sorted_indices = self.has_sorted_indices
-        return trans
 
 
 def isspmatrix_csc(x):

cupyx/scipy/sparse/csr.py
@@ -331,11 +331,8 @@ class csr_matrix(compressed._compressed_sparse_matrix):
                 'swapping dimensions is the only logical permutation.')
 
         shape = self.shape[1], self.shape[0]
-        trans = csc.csc_matrix(
+        return csc.csc_matrix(
             (self.data, self.indices, self.indptr), shape=shape, copy=copy)
-        trans.has_canonical_format = self.has_canonical_format
-        trans.has_sorted_indices = self.has_sorted_indices
-        return trans
 
 
 def isspmatrix_csr(x):

commit 9002f6a4d465b6ceb94020088963aad0c52328a7

fix performance regression introduced in 46c8ed2

cupyx/scipy/sparse/csc.py
@@ -280,8 +280,10 @@ class csc_matrix(compressed._compressed_sparse_matrix):
                 'swapping dimensions is the only logical permutation.')
 
         shape = self.shape[1], self.shape[0]
-        return cupyx.scipy.sparse.csr.csr_matrix(
+        trans = cupyx.scipy.sparse.csr.csr_matrix(
             (self.data, self.indices, self.indptr), shape=shape, copy=copy)
+        trans.has_canonical_format = self.has_canonical_format
+        return trans
 
 
 def isspmatrix_csc(x):

cupyx/scipy/sparse/csr.py
@@ -332,8 +332,10 @@ class csr_matrix(compressed._compressed_sparse_matrix):
                 'swapping dimensions is the only logical permutation.')
 
         shape = self.shape[1], self.shape[0]
-        return csc.csc_matrix(
+        trans = csc.csc_matrix(
             (self.data, self.indices, self.indptr), shape=shape, copy=copy)
+        trans.has_canonical_format = self.has_canonical_format
+        return trans
 
 
 def isspmatrix_csr(x):