444bc1d8f3d75bf9b90f9815c38d60b8ca6244a1
Corey J. Nolet <cjnolet@gmail.com>
2020-07-02 19:12:34 -0400

    Updating docstring for mean

--- cupyx/scipy/sparse/base.py
+++ cupyx/scipy/sparse/base.py
@@ -338,27 +338,22 @@ class spmatrix(object):
         over all elements in the matrix by default, otherwise over the
         specified axis. `float64` intermediate and return values are used
         for integer inputs.
-        Parameters
-        ----------
-        axis : {-2, -1, 0, 1, None} optional
-            Axis along which the mean is computed. The default is to compute
-            the mean of all elements in the matrix (i.e., `axis` = `None`).
-        dtype : data-type, optional
-            Type to use in computing the mean. For integer inputs, the default
-            is `float64`; for floating point inputs, it is the same as the
-            input dtype.
-            .. versionadded:: 0.18.0
-        out : np.matrix, optional
-            Alternative output matrix in which to place the result. It must
-            have the same shape as the expected output, but the type of the
-            output values will be cast if necessary.
-            .. versionadded:: 0.18.0
-        Returns
-        -------
-        m : np.matrix
-        See Also
-        --------
-        numpy.matrix.mean : NumPy's implementation of 'mean' for matrices
+
+        Args:
+            axis : {-2, -1, 0, 1, None} optional
+                Axis along which the mean is computed. The default is to compute
+                the mean of all elements in the matrix (i.e., `axis` = `None`).
+            dtype : data-type, optional
+                Type to use in computing the mean. For integer inputs, the default
+                is `float64`; for floating point inputs, it is the same as the
+                input dtype.
+            out : np.matrix, optional
+                Alternative output matrix in which to place the result. It must
+                have the same shape as the expected output, but the type of the
+                output values will be cast if necessary.
+
+        Returns:
+            m : np.matrix
         """
         def _is_integral(dtype):
             return (cupy.issubdtype(dtype, cupy.integer) or
