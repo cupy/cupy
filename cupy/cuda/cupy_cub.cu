b0c2ba2aee99b8879c63528f295e1ecfaf5730f2
Leo Fang <leofang@bnl.gov>
2020-07-05 00:52:31 -0400

    move dispatcher to a standalone header

--- cupy/cuda/cupy_cub.cu
+++ cupy/cuda/cupy_cub.cu
@@ -1,15 +1,9 @@
-#include <cupy/complex.cuh>
+#include <cupy/type_dispatcher.cuh>
 #include <cub/device/device_reduce.cuh>
 #include <cub/device/device_segmented_reduce.cuh>
 #include <cub/device/device_spmv.cuh>
 #include <cub/device/device_scan.cuh>
 #include "cupy_cub.h"
-#include <stdexcept>
-
-#if (__CUDACC_VER_MAJOR__ > 9 || (__CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ == 2)) \
-    && (__CUDA_ARCH__ >= 530 || !defined(__CUDA_ARCH__))
-#include <cuda_fp16.h>
-#endif
 
 
 using namespace cub;
@@ -402,38 +396,6 @@ __host__ __device__ __forceinline__ KeyValuePair<int, __half> ArgMin::operator()
 }
 #endif
 /* ------------------------------------ End of "patches" ------------------------------------ */
-
-
-//
-// **** dtype_dispatcher ****
-//
-// This is implemented with reference to the following implementation.
-// https://github.com/rapidsai/cudf/blob/branch-0.6/cpp/src/utilities/type_dispatcher.hpp
-//
-template <class functor_t, typename... Ts>
-void dtype_dispatcher(int dtype_id, functor_t f, Ts&&... args)
-{
-    switch (dtype_id) {
-    case CUPY_CUB_INT8:	      return f.template operator()<char>(std::forward<Ts>(args)...);
-    case CUPY_CUB_INT16:      return f.template operator()<short>(std::forward<Ts>(args)...);
-    case CUPY_CUB_INT32:      return f.template operator()<int>(std::forward<Ts>(args)...);
-    case CUPY_CUB_INT64:      return f.template operator()<long>(std::forward<Ts>(args)...);
-    case CUPY_CUB_UINT8:      return f.template operator()<unsigned char>(std::forward<Ts>(args)...);
-    case CUPY_CUB_UINT16:     return f.template operator()<unsigned short>(std::forward<Ts>(args)...);
-    case CUPY_CUB_UINT32:     return f.template operator()<unsigned int>(std::forward<Ts>(args)...);
-    case CUPY_CUB_UINT64:     return f.template operator()<unsigned long>(std::forward<Ts>(args)...);
-#if (__CUDACC_VER_MAJOR__ > 9 || (__CUDACC_VER_MAJOR__ == 9 && __CUDACC_VER_MINOR__ == 2)) \
-    && (__CUDA_ARCH__ >= 530 || !defined(__CUDA_ARCH__))
-    case CUPY_CUB_FLOAT16:    return f.template operator()<__half>(std::forward<Ts>(args)...);
-#endif
-    case CUPY_CUB_FLOAT32:    return f.template operator()<float>(std::forward<Ts>(args)...);
-    case CUPY_CUB_FLOAT64:    return f.template operator()<double>(std::forward<Ts>(args)...);
-    case CUPY_CUB_COMPLEX64:  return f.template operator()<complex<float>>(std::forward<Ts>(args)...);
-    case CUPY_CUB_COMPLEX128: return f.template operator()<complex<double>>(std::forward<Ts>(args)...);
-    default:
-	throw std::runtime_error("Unsupported dtype ID");
-    }
-}
 
 //
 // **** CUB Sum ****
