7403d743f2f5393b5e39c3bff8622d8912903c2b
Jeffrey Bush <jeff@coderforlife.com>
2020-07-22 12:04:18 -0400

    Adding scipy.signal functions that are simple wrappers of ndimage functions.

---
+++ cupyx/scipy/signal/bsplines.py
@@ -0,0 +1,22 @@
+def sepfir2d(input, hrow, hcol):
+    """Convolve with a 2-D separable FIR filter.
+
+    Convolve the rank-2 input array with the separable filter defined by the
+    rank-1 arrays hrow, and hcol. Mirror symmetric boundary conditions are
+    assumed. This function can be used to find an image given its B-spline
+    representation.
+
+    Args:
+        input (cupy.ndarray): The input signal
+        hrow (cupy.ndarray): Row direction filter
+        hcol (cupy.ndarray): Column direction filter
+
+    Returns:
+        cupy.ndarray: The filtered signal
+
+    .. seealso:: :func:`scipy.signal.sepfir2d`
+    """
+    from cupyx.scipy.ndimage.filters import _run_1d_correlates
+    filters = (hcol[::-1], hrow[::-1])
+    return _run_1d_correlates(input, (0, 1), lambda i: filters[i],
+                              None, 'reflect', 0.0)
--- cupyx/scipy/signal/signaltools.py
+++ cupyx/scipy/signal/signaltools.py
@@ -1,3 +1,5 @@
+import cupy
+
 
 def choose_conv_method(in1, in2, mode='full'):
     """Find the fastest convolution/correlation method.
@@ -9,7 +11,7 @@ def choose_conv_method(in1, in2, mode='full'):
 
     Returns:
         str: A string indicating which convolution method is fastest,
-        either ``direct`` or ``fft1``.
+        either ``direct`` or ``fft``.
 
     .. warning::
         This function currently doesn't support measure option,
@@ -38,3 +40,156 @@ def _fftconv_faster(x, h, mode):
     """
     # TODO(Dahlia-Chehata): replace with GPU-based constants.
     return True
+
+
+def wiener(im, mysize=None, noise=None):
+    """Perform a Wiener filter on an N-dimensional array.
+
+    Apply a Wiener filter to the N-dimensional array `im`.
+
+    Args:
+        im (cupy.ndarray): An N-dimensional array.
+        mysize (int or cupy.ndarray, optional): A scalar or an N-length list
+            giving the size of the Wiener filter window in each dimension.
+            Elements of mysize should be odd. If mysize is a scalar, then this
+            scalar is used as the size in each dimension.
+        noise (float, optional): The noise-power to use. If None, then noise is
+            estimated as the average of the local variance of the input.
+
+    Returns:
+        cupy.ndarray: Wiener filtered result with the same shape as `im`.
+
+    .. seealso:: :func:`scipy.signal.wiener`
+    """
+    from cupyx.scipy.ndimage import uniform_filter
+    from cupyx.scipy.ndimage._filters_core import _fix_sequence_arg
+
+    if mysize is None:
+        mysize = 3
+    mysize = _fix_sequence_arg(mysize, im.ndim, 'mysize', int)
+
+    # Estimate the local mean
+    local_mean = uniform_filter(im, mysize, mode='constant')
+
+    # Estimate the local variance
+    local_var = uniform_filter(im*im, mysize, mode='constant')
+    local_var -= local_mean*local_mean
+
+    # Estimate the noise power if needed.
+    if noise is None:
+        noise = local_var.mean()
+
+    # Perform the filtering
+    res = im - local_mean
+    res *= (1 - noise / local_var)
+    res += local_mean
+    return cupy.where(local_var < noise, local_mean, res)
+
+
+def order_filter(a, domain, rank):
+    """Perform an order filter on an N-D array.
+
+    Perform an order filter on the array in. The domain argument acts as a mask
+    centered over each pixel. The non-zero elements of domain are used to
+    select elements surrounding each input pixel which are placed in a list.
+    The list is sorted, and the output for that pixel is the element
+    corresponding to rank in the sorted list.
+
+    Args:
+        a (cupy.ndarray): The N-dimensional input array.
+        domain (cupy.ndarray): A mask array with the same number of dimensions
+            as `a`. Each dimension should have an odd number of elements.
+        rank (int): A non-negative integer which selects the element from the
+            sorted list (0 corresponds to the smallest element).
+
+    Returns:
+        cupy.ndarray: The results of the order filter in an array with the same
+            shape as `a`.
+
+    .. seealso:: :func:`cupyx.scipy.ndimage.rank_filter`
+    .. seealso:: :func:`scipy.signal.order_filter`
+    """
+    from cupyx.scipy.ndimage import rank_filter
+    size = domain.shape
+    for k in range(len(size)):
+        if (size[k] % 2) != 1:
+            raise ValueError("Each dimension of domain argument "
+                             " should have an odd number of elements.")
+    return rank_filter(a, rank, footprint=domain,
+                       output=float, mode='constant')
+
+
+def medfilt(volume, kernel_size=None):
+    """Perform a median filter on an N-dimensional array.
+
+    Apply a median filter to the input array using a local window-size
+    given by `kernel_size`. The array will automatically be zero-padded.
+
+    Args:
+        volume (cupy.ndarray): An N-dimensional input array.
+        kernel_size (int or list of ints): Gives the size of the median filter
+            window in each dimension. Elements of `kernel_size` should be odd.
+            If `kernel_size` is a scalar, then this scalar is used as the size
+            in each dimension. Default size is 3 for each dimension.
+
+    Returns:
+        cupy.ndarray: An array the same size as input containing the median
+        filtered result.
+
+    .. seealso:: :func:`cupyx.scipy.ndimage.median_filter`
+    .. seealso:: :func:`scipy.signal.medfilt`
+    """
+    from cupyx.scipy.ndimage import rank_filter
+    from warnings import warn
+
+    kernel_size = _get_kernel_size(kernel_size, volume.ndim)
+    if any(k > s for k, s in zip(kernel_size, volume.shape)):
+        warn('kernel_size exceeds volume extent: volume will be zero-padded')
+
+    size = 1
+    for ks in kernel_size:
+        size *= ks
+    return rank_filter(volume, size // 2, size=kernel_size,
+                       output=float, mode='constant')
+
+
+def medfilt2d(input, kernel_size=3):
+    """Median filter a 2-dimensional array.
+
+    Apply a median filter to the `input` array using a local window-size given
+    by `kernel_size` (must be odd). The array is zero-padded automatically.
+
+    Args:
+        input (cupy.ndarray): A 2-dimensional input array.
+        kernel_size (int of list of ints of length 2): Gives the size of the
+            median filter window in each dimension. Elements of `kernel_size`
+            should be odd. If `kernel_size` is a scalar, then this scalar is
+            used as the size in each dimension. Default is a kernel of size
+            (3, 3).
+
+    Returns:
+        cupy.ndarray: An array the same size as input containing the median
+            filtered result.
+    See also
+    --------
+    .. seealso:: :func:`cupyx.scipy.ndimage.median_filter`
+    .. seealso:: :func:`cupyx.scipy.signal.medfilt`
+    .. seealso:: :func:`scipy.signal.medfilt2d`
+    """
+    # Scipy's version only supports uint8, float32, and float64
+    from cupyx.scipy.ndimage import rank_filter
+    kernel_size = _get_kernel_size(kernel_size, input.ndim)
+    order = kernel_size[0] * kernel_size[1] // 2
+    return rank_filter(input, order, size=kernel_size, mode='constant')
+
+
+def _get_kernel_size(kernel_size, ndim):
+    if kernel_size is None:
+        kernel_size = (3,) * ndim
+    try:
+        kernel_size = tuple(int(x) for x in kernel_size)
+    except TypeError:
+        kernel_size = (int(kernel_size),)*ndim
+    if any((k % 2) != 1 for k in kernel_size):
+        raise ValueError("Each element of kernel_size should be odd")
+    return kernel_size
