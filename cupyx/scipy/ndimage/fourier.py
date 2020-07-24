822b36c0e0ffdb34fe4f93b379d4366f36ad0e64
Gregory Lee <grlee77@gmail.com>
2020-07-24 03:42:24 -0400

    ENH: add fourier_gaussian and fourier_uniform

--- cupyx/scipy/ndimage/fourier.py
+++ cupyx/scipy/ndimage/fourier.py
@@ -4,6 +4,23 @@ import cupy
 from cupyx.scipy.ndimage import filters
 
 
+def _get_output_fourier(output, input):
+    if output is None:
+        if input.dtype.type in [cupy.complex64, cupy.complex128,
+                                cupy.float32]:
+            output = cupy.zeros(input.shape, dtype=input.dtype)
+        else:
+            output = cupy.zeros(input.shape, dtype=cupy.float64)
+    elif type(output) is type:
+        if output not in [cupy.complex64, cupy.complex128,
+                          cupy.float32, cupy.float64]:
+            raise RuntimeError("output type not supported")
+        output = cupy.zeros(input.shape, dtype=output)
+    elif output.shape != input.shape:
+        raise RuntimeError("output shape not correct")
+    return output
+
+
 def _get_output_fourier_complex(output, input):
     if output is None:
         if input.dtype.type in [cupy.complex64, cupy.complex128]:
@@ -25,6 +42,109 @@ def _reshape_nd(arr, ndim, axis):
     return arr.reshape(nd_shape)
 
 
+def fourier_gaussian(input, sigma, n=-1, axis=-1, output=None):
+    """Multidimensional Gaussian shift filter.
+
+    The array is multiplied with the Fourier transform of a (separable)
+    Gaussian kernel.
+
+    Args:
+        input (cupy.ndarray): The input array.
+        sigma (float or sequence of float):  The sigma of the Gaussian kernel.
+            If a float, `sigma` is the same for all axes. If a sequence,
+            `sigma` has to contain one value for each axis.
+        n (int, optional):  If `n` is negative (default), then the input is
+            assumed to be the result of a complex fft. If `n` is larger than or
+            equal to zero, the input is assumed to be the result of a real fft,
+            and `n` gives the length of the array before transformation along
+            the real transform direction.
+        axis (int, optional): The axis of the real transform (only used when
+            ``n > -1``).
+        output (cupy.ndarray, optional):
+            If given, the result of shifting the input is placed in this array.
+
+    Returns:
+        output (cupy.ndarray): The filtered output.
+    """
+    ndim = input.ndim
+    output = _get_output_fourier(output, input)
+    axis = cupy.util._normalize_axis_index(axis, ndim)
+    sigmas = filters._fix_sequence_arg(sigma, ndim, 'sigma')
+
+    output[...] = input
+    for ax, (sigmak, ax_size) in enumerate(zip(sigmas, output.shape)):
+
+        # compute the frequency grid in Hz
+        if ax == axis and n > 0:
+            arr = cupy.arange(ax_size, dtype=output.real.dtype)
+            arr /= n
+        else:
+            arr = cupy.fft.fftfreq(ax_size)
+        arr = arr.astype(output.real.dtype, copy=False)
+
+        # compute the Gaussian weights
+        arr *= arr
+        scale = sigmak * sigmak / -2
+        arr *= (4 * np.pi * np.pi) * scale
+        cupy.exp(arr, out=arr)
+
+        # reshape for broadcasting
+        arr = _reshape_nd(arr, ndim=ndim, axis=ax)
+        output *= arr
+
+    return output
+
+
+def fourier_uniform(input, size, n=-1, axis=-1, output=None):
+    """Multidimensional uniform shift filter.
+
+    The array is multiplied with the Fourier transform of a box of given size.
+
+    Args:
+        input (cupy.ndarray): The input array.
+        size (float or sequence of float):  The sigma of the box used for
+            filtering. If a float, `size` is the same for all axes. If a
+            sequence, `size` has to contain one value for each axis.
+        n (int, optional):  If `n` is negative (default), then the input is
+            assumed to be the result of a complex fft. If `n` is larger than or
+            equal to zero, the input is assumed to be the result of a real fft,
+            and `n` gives the length of the array before transformation along
+            the real transform direction.
+        axis (int, optional): The axis of the real transform (only used when
+            ``n > -1``).
+        output (cupy.ndarray, optional):
+            If given, the result of shifting the input is placed in this array.
+
+    Returns:
+        output (cupy.ndarray): The filtered output.
+    """
+    ndim = input.ndim
+    output = _get_output_fourier(output, input)
+    axis = cupy.util._normalize_axis_index(axis, ndim)
+    sizes = filters._fix_sequence_arg(size, ndim, 'size')
+
+    output[...] = input
+    for ax, (size, ax_size) in enumerate(zip(sizes, output.shape)):
+
+        # compute the frequency grid in Hz
+        if ax == axis and n > 0:
+            arr = cupy.arange(ax_size, dtype=output.real.dtype)
+            arr /= n
+        else:
+            arr = cupy.fft.fftfreq(ax_size)
+        arr = arr.astype(output.real.dtype, copy=False)
+
+        # compute the uniform filter weights
+        arr *= size
+        cupy.sinc(arr, out=arr)
+
+        # reshape for broadcasting
+        arr = _reshape_nd(arr, ndim=ndim, axis=ax)
+        output *= arr
+
+    return output
+
+
 def fourier_shift(input, shift, n=-1, axis=-1, output=None):
     """Multidimensional Fourier shift filter.
 
@@ -43,11 +163,11 @@ def fourier_shift(input, shift, n=-1, axis=-1, output=None):
             the real transform direction.
         axis (int, optional): The axis of the real transform (only used when
             ``n > -1``).
-        output (ndarray, optional):
+        output (cupy.ndarray, optional):
             If given, the result of shifting the input is placed in this array.
 
     Returns:
-        output (ndarray): The shifted output (in the Fourier domain).
+        output (cupy.ndarray): The shifted output (in the Fourier domain).
     """
     ndim = input.ndim
     output = _get_output_fourier_complex(output, input)
--- tests/cupyx_tests/scipy_tests/ndimage_tests/test_fourier.py
+++ tests/cupyx_tests/scipy_tests/ndimage_tests/test_fourier.py
@@ -97,3 +97,176 @@ class TestFourierShift(unittest.TestCase):
         if not x.dtype.kind == 'c':
             a = a.real
         return xp.ascontiguousarray(a)
+
+
+@testing.parameterize(
+    *(
+        testing.product(
+            {
+                "shape": [(32, 16), (31, 15)],
+                "dtype": [numpy.float32, numpy.float64, numpy.complex64,
+                          numpy.complex128],
+                "sigma": [1, (5, 5.3), (3, 5)],
+            }
+        )
+        + testing.product(
+            {
+                "shape": [(5, 16, 7), ],
+                "dtype": [numpy.float32, numpy.float64, numpy.complex64,
+                          numpy.complex128],
+                "sigma": [3, (1, 2.5, 3)],
+            }
+        )
+        + testing.product(
+            {
+                "shape": [(15, ), ],
+                "dtype": [numpy.float32, numpy.float64, numpy.complex64,
+                          numpy.complex128],
+                "sigma": [8.5, (5,)],
+            }
+        )
+    )
+)
+@testing.gpu
+@testing.with_requires("scipy")
+class TestFourierGaussian(unittest.TestCase):
+
+    def _test_real_nd(self, xp, scp, x, real_axis):
+        a = scp.fft.rfft(x, axis=real_axis)
+        # complex-valued FFTs on all other axes
+        complex_axes = tuple([ax for ax in range(x.ndim) if ax != real_axis])
+        if complex_axes:
+            a = scp.fft.fftn(a, axes=complex_axes)
+
+        a = scp.ndimage.fourier_gaussian(
+            a, self.sigma, n=x.shape[real_axis], axis=real_axis)
+
+        if complex_axes:
+            a = scp.fft.ifftn(a, axes=complex_axes)
+        a = scp.fft.irfft(a, axis=real_axis)
+        if not x.dtype.kind == 'c':
+            a = a.real
+        return xp.ascontiguousarray(a)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_real_fft_axis0(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        if x.dtype.kind == 'c':
+            # skip: can't use rfft on complex-valued x
+            return x
+        return self._test_real_nd(xp, scp, x, 0)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_real_fft_axis1(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        if x.dtype.kind == 'c' or x.ndim < 2:
+            # skip: can't use rfft along axis 1 on complex-valued x or 1d x
+            return x
+        return self._test_real_nd(xp, scp, x, 1)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_complex_fft(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        a = scp.fft.fftn(x)
+        a = scp.ndimage.fourier_gaussian(a, self.sigma)
+        a = scp.fft.ifftn(a)
+        if not x.dtype.kind == 'c':
+            a = a.real
+        return xp.ascontiguousarray(a)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_complex_fft_with_output(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        a = scp.fft.fftn(x)
+        scp.ndimage.fourier_gaussian(a, self.sigma, output=a)
+        a = scp.fft.ifftn(a)
+        if not x.dtype.kind == 'c':
+            a = a.real
+        return xp.ascontiguousarray(a)
+
+
+
+@testing.parameterize(
+    *(
+        testing.product(
+            {
+                "shape": [(32, 16), (31, 15)],
+                "dtype": [numpy.float32, numpy.float64, numpy.complex64,
+                          numpy.complex128],
+                "size": [1, (5, 5.3), (3, 5)],
+            }
+        )
+        + testing.product(
+            {
+                "shape": [(5, 16, 7), ],
+                "dtype": [numpy.float32, numpy.float64, numpy.complex64,
+                          numpy.complex128],
+                "size": [3, (1, 2.5, 3)],
+            }
+        )
+        + testing.product(
+            {
+                "shape": [(15, ), ],
+                "dtype": [numpy.float32, numpy.float64, numpy.complex64,
+                          numpy.complex128],
+                "size": [8.5, (5,)],
+            }
+        )
+    )
+)
+@testing.gpu
+@testing.with_requires("scipy")
+class TestFourierUniform(unittest.TestCase):
+
+    def _test_real_nd(self, xp, scp, x, real_axis):
+        a = scp.fft.rfft(x, axis=real_axis)
+        # complex-valued FFTs on all other axes
+        complex_axes = tuple([ax for ax in range(x.ndim) if ax != real_axis])
+        if complex_axes:
+            a = scp.fft.fftn(a, axes=complex_axes)
+
+        a = scp.ndimage.fourier_uniform(
+            a, self.size, n=x.shape[real_axis], axis=real_axis)
+
+        if complex_axes:
+            a = scp.fft.ifftn(a, axes=complex_axes)
+        a = scp.fft.irfft(a, axis=real_axis)
+        if not x.dtype.kind == 'c':
+            a = a.real
+        return xp.ascontiguousarray(a)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_real_fft_axis0(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        if x.dtype.kind == 'c':
+            # skip: can't use rfft on complex-valued x
+            return x
+        return self._test_real_nd(xp, scp, x, 0)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_real_fft_axis1(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        if x.dtype.kind == 'c' or x.ndim < 2:
+            # skip: can't use rfft along axis 1 on complex-valued x or 1d x
+            return x
+        return self._test_real_nd(xp, scp, x, 1)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_complex_fft(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        a = scp.fft.fftn(x)
+        a = scp.ndimage.fourier_uniform(a, self.size)
+        a = scp.fft.ifftn(a)
+        if not x.dtype.kind == 'c':
+            a = a.real
+        return xp.ascontiguousarray(a)
+
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name="scp")
+    def test_complex_fft_with_output(self, xp, scp):
+        x = testing.shaped_random(self.shape, xp, self.dtype)
+        a = scp.fft.fftn(x)
+        scp.ndimage.fourier_uniform(a, self.size, output=a)
+        a = scp.fft.ifftn(a)
+        if not x.dtype.kind == 'c':
+            a = a.real
+        return xp.ascontiguousarray(a)
