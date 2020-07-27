5ad234ad6114fee6de2d6f7fa103471364ddb3bb
Corey J. Nolet <cjnolet@gmail.com>
2020-07-27 14:46:41 -0400

    Only keeping simple slicing tests

--- tests/cupyx_tests/scipy_tests/sparse_tests/test_index.py
+++ tests/cupyx_tests/scipy_tests/sparse_tests/test_index.py
@@ -65,38 +65,12 @@ class TestIndexing(unittest.TestCase):
     def test_major_scalar(self):
         self._run(10)
 
-    def test_major_fancy(self):
-        with pytest.raises(NotImplementedError):
-            self._run([1, 5, 4])
-            self._run([10, 2])
-            self._run([2])
-
-    def test_major_bool_fancy(self):
-        with pytest.raises(NotImplementedError):
-            rand_bool = cupy.random.random(self.n_rows).astype(cupy.bool)
-            self._run(rand_bool)
-
     def test_major_slice_minor_slice(self):
         self._run(slice(1, 5), slice(1, 5))
 
     def test_major_slice_minor_all(self):
         self._run(slice(1, 5), slice(None))
         self._run(slice(5, 1), slice(None))
-
-    # def test_major_slice_minor_scalar(self):
-    #     self._run(slice(1, 5), 5)
-    #     self._run(slice(5, 1), 5)
-    #
-    #     def do_run():
-    #         self._run(slice(5, 1, -1), 5)
-    #
-    #     if self.format == 'csr':
-    #         with pytest.raises(NotImplementedError):
-    #             do_run()
-
-    def test_major_slice_minor_fancy(self):
-        with pytest.raises(NotImplementedError):
-            self._run(slice(1, 10, 2), [1, 5, 4])
 
     def test_major_scalar_minor_slice(self):
         self._run(5, slice(1, 5))
@@ -107,10 +81,6 @@ class TestIndexing(unittest.TestCase):
     def test_major_scalar_minor_scalar(self):
         self._run(5, 5)
 
-    def test_major_scalar_minor_fancy(self):
-        with pytest.raises(NotImplementedError):
-            self._run(5, [1, 5, 4])
-
     def test_major_all_minor_scalar(self):
         self._run(slice(None), 5)
 
@@ -119,128 +89,6 @@ class TestIndexing(unittest.TestCase):
 
     def test_major_all_minor_all(self):
         self._run(slice(None), slice(None))
-
-    def test_major_all_minor_fancy(self):
-        with pytest.raises(NotImplementedError):
-            self._run(slice(None), [1, 5, 2, 3, 4, 5, 4, 1, 5])
-            self._run(slice(None), [0, 3, 4, 1, 1, 5, 5, 2, 3, 4, 5, 4, 1, 5])
-
-    def test_major_fancy_minor_fancy(self):
-        with pytest.raises(NotImplementedError):
-            self._run([1, 5, 4], [1, 5, 4])
-            self._run([2, 0, 10], [9, 2, 1])
-            self._run([2, 0], [2, 1])
-
-    def test_major_fancy_minor_all(self):
-        with pytest.raises(NotImplementedError):
-            self._run([1, 5, 4], slice(None))
-
-    def test_major_fancy_minor_scalar(self):
-        with pytest.raises(NotImplementedError):
-            self._run([1, 5, 4], 5)
-
-    def test_major_fancy_minor_slice(self):
-        with pytest.raises(NotImplementedError):
-            self._run([1, 5, 4], slice(1, 5))
-            self._run([1, 5, 4], slice(5, 1, -1))
-
-    def test_major_slice_with_step(self):
-
-        def test_runs():
-
-            # positive step
-            self._run(slice(1, 10, 2))
-            self._run(slice(2, 10, 5))
-            self._run(slice(0, 10, 10))
-
-            self._run(slice(1, None, 2))
-            self._run(slice(2, None, 5))
-            self._run(slice(0, None,  10))
-
-            # negative step
-            self._run(slice(10, 1, -2))
-            self._run(slice(10, 2, -5))
-            self._run(slice(10, 0, -10))
-
-            self._run(slice(10, None, -2))
-            self._run(slice(10, None, -5))
-            self._run(slice(10, None, -10))
-
-        if self.format == 'csc':
-            with pytest.raises(NotImplementedError):
-                test_runs()
-        else:
-            test_runs()
-
-    # def test_major_slice_with_step_minor_slice_with_step(self):
-    #
-    #     with pytest.raises(NotImplementedError):
-    #         # positive step
-    #         self._run(slice(1, 10, 2), slice(1, 10, 2))
-    #         self._run(slice(2, 10, 5), slice(2, 10, 5))
-    #         self._run(slice(0, 10, 10), slice(0, 10, 10))
-    #
-    #         # negative step
-    #         self._run(slice(10, 1, 2), slice(10, 1, 2))
-    #         self._run(slice(10, 2, 5), slice(10, 2, 5))
-    #         self._run(slice(10, 0, 10), slice(10, 0, 10))
-
-    def test_major_slice_with_step_minor_all(self):
-        def test_runs():
-            # positive step
-            self._run(slice(1, 10, 2), slice(None))
-            self._run(slice(2, 10, 5), slice(None))
-            self._run(slice(0, 10, 10), slice(None))
-
-            # negative step
-            self._run(slice(10, 1, 2), slice(None))
-            self._run(slice(10, 2, 5), slice(None))
-            self._run(slice(10, 0, 10), slice(None))
-
-        if self.format == 'csc':
-            with pytest.raises(NotImplementedError):
-                test_runs()
-        else:
-            test_runs()
-
-    # def test_major_all_minor_slice_step(self):
-    #
-    #     with pytest.raises(NotImplementedError):
-    #         # positive step incr
-    #         self._run(slice(None), slice(1, 10, 2))
-    #         self._run(slice(None), slice(2, 10, 5))
-    #         self._run(slice(None), slice(0, 10, 10))
-    #
-    #         # positive step decr
-    #         self._run(slice(None), slice(10, 1, 2))
-    #         self._run(slice(None), slice(10, 2, 5))
-    #         self._run(slice(None), slice(10, 0, 10))
-    #
-    #         # positive step incr
-    #         self._run(slice(None), slice(10, 1, 2))
-    #         self._run(slice(None), slice(10, 2, 5))
-    #         self._run(slice(None), slice(10, 0, 10))
-    #
-    #         # negative step decr
-    #         self._run(slice(None), slice(10, 1, -2))
-    #         self._run(slice(None), slice(10, 2, -5))
-
-    def test_major_reorder(self):
-        def test_runs():
-            self._run(slice(None, None, -1))
-            self._run(slice(None, None, -2))
-            self._run(slice(None, None, -50))
-
-        if self.format == 'csc':
-            with pytest.raises(NotImplementedError):
-                test_runs()
-        else:
-            test_runs()
-
-    def test_major_reorder_minor_reorder(self):
-        with pytest.raises(NotImplementedError):
-            self._run(slice(None, None, -1), slice(None, None, -1))
-            self._run(slice(None, None, -3), slice(None, None, -3))
 
     def test_ellipsis(self):
         self._run(Ellipsis)
