3d8267d02899ffae28acee8f71d155087873ffc5
Corey J. Nolet <cjnolet@gmail.com>
2020-07-27 14:58:34 -0400

    Stripping csr and csc tests to only include the minimal 2d slicing tests

--- tests/cupyx_tests/scipy_tests/sparse_tests/test_csr.py
+++ tests/cupyx_tests/scipy_tests/sparse_tests/test_csr.py
@@ -1622,42 +1622,9 @@ class TestCsrMatrixGetitem(unittest.TestCase):
         return _make(xp, sp, self.dtype)[slice(None, None, None)]
 
     @testing.numpy_cupy_allclose(sp_name='sp')
-    def test_getitem_rowslice_reverse(self, xp, sp):
-        # This test is adapted from Scipy
-        return _make(xp, sp, self.dtype)[slice(None, None, -1)]
-
-    @testing.numpy_cupy_allclose(sp_name='sp')
     def test_getitem_rowslice_negative_stop(self, xp, sp):
         # This test is adapted from Scipy
         return _make(xp, sp, self.dtype)[slice(1, -2, 2)]
-
-    @testing.numpy_cupy_allclose(sp_name='sp')
-    def test_getitem_rowslice_negative_start_step(self, xp, sp):
-        # This test is adapted from Scipy
-        return _make(xp, sp, self.dtype)[slice(-2, 1, -2)]
-
-    def test_getitem_bool_indexing(self):
-        # This test is adapted from Scipy's CSR tests
-        sp_data = scipy.sparse.csr_matrix([[0, 1, 2], [3, 4, 5], [6, 7, 8]],
-                                          dtype=self.dtype)
-        data = sparse.csr_matrix(sp_data)
-        list_indices1 = [False, True, False]
-        array_indices1 = cupy.array(list_indices1)
-        list_indices2 = [[False, True, False], [
-            False, True, False], [False, True, False]]
-        array_indices2 = cupy.array(list_indices2)
-        list_indices3 = ([False, True, False], [False, True, False])
-        array_indices3 = (cupy.array(
-            list_indices3[0]), cupy.array(list_indices3[1]))
-        slice_list1 = data[list_indices1].toarray()
-        slice_array1 = data[array_indices1].toarray()
-        slice_list2 = data[list_indices2]
-        slice_array2 = data[array_indices2]
-        slice_list3 = data[list_indices3]
-        slice_array3 = data[array_indices3]
-        assert (slice_list1 == slice_array1).all()
-        assert (slice_list2 == slice_array2).all()
-        assert (slice_list3 == slice_array3).all()
 
     def test_getrow(self):
 
