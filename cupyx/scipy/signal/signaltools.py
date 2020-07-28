98bf72cc8547c3cb45c2144dfc11646d8726b42b
Jeffrey Bush <jeff@coderforlife.com>
2020-07-28 12:36:18 -0400

    Fix typo.

--- cupyx/scipy/signal/signaltools.py
+++ cupyx/scipy/signal/signaltools.py
@@ -184,8 +184,8 @@ def medfilt2d(input, kernel_size=3):
 def _get_kernel_size(kernel_size, ndim):
     if kernel_size is None:
         kernel_size = (3,) * ndim
-    kernel_size = ._util._fix_sequence_arg(kernel_size, ndim,
-                                           'kernel_size', int)
+    kernel_size = _util._fix_sequence_arg(kernel_size, ndim,
+                                          'kernel_size', int)
     if any((k % 2) != 1 for k in kernel_size):
         raise ValueError("Each element of kernel_size should be odd")
     return kernel_size
