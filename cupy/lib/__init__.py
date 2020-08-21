22e9175bc4429463fe92cc67c643a5aa296d612b
Dahlia-Chehata <dahliachehata@gmail.com>
2020-08-21 12:21:11 +0200

    fix import

--- cupy/lib/__init__.py
+++ cupy/lib/__init__.py
@@ -1,5 +1,6 @@
 from cupy.lib import stride_tricks  # NOQA
 from cupy.lib.polynomial import poly1d  # NOQA
+from cupy.lib._routines_poly import poly  # NOQA
 from cupy.lib._routines_poly import polyadd  # NOQA
 from cupy.lib._routines_poly import polysub  # NOQA
 from cupy.lib._routines_poly import polymul  # NOQA
