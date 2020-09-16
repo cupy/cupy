03e1f32fee441e2a8b87733d5c8cdc3ece754b93
Akira Naruse <anaruse@nvidia.com>
2020-09-16 21:41:53 +0900

    Add tests for cupyx.scipy.linalg.lu

--- tests/cupyx_tests/scipy_tests/linalg_tests/test_decomp_lu.py
+++ tests/cupyx_tests/scipy_tests/linalg_tests/test_decomp_lu.py
@@ -35,7 +35,8 @@ class TestLUFactor(unittest.TestCase):
     @testing.for_dtypes('fdFD')
     def test_lu_factor_reconstruction(self, dtype):
         m, n = self.shape
-        A = testing.shaped_random(self.shape, numpy, dtype=dtype)
+        k = min(m, n)
+        A = testing.shaped_random(self.shape, cupy, dtype=dtype)
         lu, piv = cupyx.scipy.linalg.lu_factor(A)
         # extract ``L`` and ``U`` from ``lu``
         L = cupy.tril(lu, k=-1)
@@ -45,19 +46,55 @@ class TestLUFactor(unittest.TestCase):
         U = U[:n, :]
         # check output shapes
         assert lu.shape == (m, n)
-        assert L.shape == (m, min(m, n))
-        assert U.shape == (min(m, n), n)
-        assert piv.shape == (min(m, n),)
-        # apply pivot (on CPU since slaswp is not available in cupy)
-        piv = cupy.asnumpy(piv)
-        rows = numpy.arange(m)
-        for i, row in enumerate(piv):
-            if i != row:
-                rows[i], rows[row] = rows[row], rows[i]
-        PA = A[rows]
+        assert L.shape == (m, k)
+        assert U.shape == (k, n)
+        assert piv.shape == (k,)
+        # apply pivot
+        PA = A.copy()
+        cupyx.scipy.linalg.decomp_lu.cupy_laswp(PA, 0, k-1, piv, 1)
         # check that reconstruction is close to original
         LU = L.dot(U)
         cupy.testing.assert_allclose(LU, PA, atol=1e-5)
+
+
+@testing.gpu
+@testing.parameterize(*testing.product({
+    'shape': [(1, 1), (2, 2), (3, 3), (5, 5), (1, 5), (5, 1), (2, 5), (5, 2)],
+    'permute_l': [False, True],
+}))
+@testing.fix_random()
+@testing.with_requires('scipy')
+class TestLU(unittest.TestCase):
+
+    @testing.for_dtypes('fdFD')
+    def test_lu(self, dtype):
+        a_cpu = testing.shaped_random(self.shape, numpy, dtype=dtype)
+        a_gpu = cupy.asarray(a_cpu)
+        result_cpu = scipy.linalg.lu(a_cpu, permute_l=self.permute_l)
+        result_gpu = cupyx.scipy.linalg.lu(a_gpu, permute_l=self.permute_l)
+        self.assertEqual(len(result_cpu), len(result_gpu))
+        if not self.permute_l:
+            # check permutation matrix
+            result_cpu = list(result_cpu)
+            result_gpu = list(result_gpu)
+            P_cpu = result_cpu.pop(0)
+            P_gpu = result_gpu.pop(0)
+            cupy.testing.assert_array_equal(P_gpu, P_cpu)
+        cupy.testing.assert_allclose(result_gpu[0], result_cpu[0], atol=1e-5)
+        cupy.testing.assert_allclose(result_gpu[1], result_cpu[1], atol=1e-5)
+
+    @testing.for_dtypes('fdFD')
+    def test_lu_reconstruction(self, dtype):
+        m, n = self.shape
+        A = testing.shaped_random(self.shape, cupy, dtype=dtype)
+        if self.permute_l:
+            PL, U = cupyx.scipy.linalg.lu(A, permute_l=self.permute_l)
+            PLU = PL @ U
+        else:
+            P, L, U = cupyx.scipy.linalg.lu(A, permute_l=self.permute_l)
+            PLU = P @ L @ U
+        # check that reconstruction is close to original
+        cupy.testing.assert_allclose(PLU, A, atol=1e-5)
 
 
 @testing.gpu
