087d91dd08bca9a0afe46b871e693bcdc8be7d32
Emilio Castillo <ecastill@preferred.jp>
2020-09-29 06:25:18 +0000

    Dont check cuda version for hip

--- cupy_setup_build.py
+++ cupy_setup_build.py
@@ -737,7 +737,7 @@ def cythonize(extensions, arg_options):
         compile_time_env = {}
         cythonize_options['compile_time_env'] = compile_time_env
     compile_time_env['use_hip'] = arg_options['use_hip']
-    if arg_options['no_cuda']:
+    if use_hip or arg_options['no_cuda']:
         compile_time_env['CUDA_VERSION'] = 0
     else:
         compile_time_env['CUDA_VERSION'] = build.get_cuda_version()
