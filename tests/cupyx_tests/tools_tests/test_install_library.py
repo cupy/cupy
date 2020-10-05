37bf5c1738d7df6c778279924dcd4142c1619f95
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-10-05 13:29:14 +0900

    add tests for cuDNN urls

--- tests/cupyx_tests/tools_tests/test_install_library.py
+++ tests/cupyx_tests/tools_tests/test_install_library.py
@@ -1,5 +1,6 @@
 import tempfile
 import unittest
+import urllib
 
 from cupy import testing
 from cupyx.tools import install_library
@@ -14,6 +15,16 @@ class TestInstallLibrary(unittest.TestCase):
             cuda = rec['cuda']
             with tempfile.TemporaryDirectory() as d:
                 install_library.install_cudnn(cuda, d)
+
+    def test_cudnn(self):
+        assets = [r['assets'] for r in install_library._cudnn_records]
+        for asset in assets:
+            for platform in asset.keys():
+                url = asset[platform]['url']
+                with urllib.request.urlopen(
+                        urllib.request.Request(url, method='HEAD')) as resp:
+                    assert resp.getcode() == 200
+
 
     def test_main(self):
         install_library.main(
