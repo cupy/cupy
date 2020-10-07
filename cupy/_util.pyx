e5ac2d3617ccf061cb1194f862ba35bf235cd518
Masayuki Takagi <kamonama@gmail.com>
2020-10-07 12:43:59 +0900

    Merge pull request #4057 from asi1024/internal_normalize
    Move `_normalize_axis_index` to `cupy/core/internal.pyx`

--- cupy/_util.pyx
+++ cupy/_util.pyx
@@ -19,60 +19,6 @@ ENABLE_SLICE_COPY = bool(
 cdef list _memos = []
 
 
-def _normalize_axis_index(axis, ndim):
-    """
-    Normalizes an axis index, ``axis``, such that is a valid positive index
-    into the shape of array with ``ndim`` dimensions. Raises a ValueError
-    with an appropriate message if this is not possible.
-
-    Args:
-        axis (int):
-            The un-normalized index of the axis. Can be negative
-        ndim (int):
-            The number of dimensions of the array that ``axis`` should be
-            normalized against
-
-    Returns:
-        int:
-            The normalized axis index, such that `0 <= normalized_axis < ndim`
-
-    """
-    if axis < 0:
-        axis += ndim
-    if not (0 <= axis < ndim):
-        raise numpy.AxisError('axis out of bounds')
-    return axis
-
-
-def _normalize_axis_indices(axes, ndim):
-    """Normalize axis indices.
-
-    Args:
-        axis (int, tuple of int or None):
-            The un-normalized indices of the axis. Can be negative.
-        ndim (int):
-            The number of dimensions of the array that ``axis`` should be
-            normalized against
-
-    Returns:
-        tuple of int:
-            The tuple of normalized axis indices.
-    """
-    if axes is None:
-        axes = tuple(range(ndim))
-    elif not isinstance(axes, tuple):
-        axes = axes,
-
-    res = []
-    for axis in axes:
-        axis = _normalize_axis_index(axis, ndim)
-        if axis in res:
-            raise ValueError('Duplicate value in \'axis\'')
-        res.append(axis)
-
-    return tuple(sorted(res))
-
-
 def memoize(bint for_each_device=False):
     """Makes a function memoizing the result for each argument and device.
 
