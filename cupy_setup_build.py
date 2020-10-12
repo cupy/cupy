ab394cd204e0c089737c99d8943bce0cf6644c7b
Leo Fang <leofang@bnl.gov>
2020-10-12 05:00:58 -0400

    pass use_hip to cython

--- cupy_setup_build.py
+++ cupy_setup_build.py
@@ -731,6 +731,7 @@ def cythonize(extensions, arg_options):
     if compile_time_env is None:
         compile_time_env = {}
         cythonize_options['compile_time_env'] = compile_time_env
+    compile_time_env['use_hip'] = arg_options['use_hip']
     if use_hip or arg_options['no_cuda']:
         compile_time_env['CUDA_VERSION'] = 0
     else:
