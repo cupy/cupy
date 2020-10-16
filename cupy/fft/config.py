96ab97191e4bdbd7920235d53e0a47a31d3d6885
Leo Fang <leofang@bnl.gov>
2020-10-16 17:20:50 -0400

    cache python modules

--- cupy/fft/config.py
+++ cupy/fft/config.py
@@ -1,12 +1,24 @@
 import contextlib
+import hashlib
+import importlib
+import os
+import shutil
+import string
+import subprocess
+import sys
+import sysconfig
+import tempfile
 
+from cupy import __version__
 from cupy import _util
+from cupy._environment import get_nvcc_path
 from cupy.cuda.cufft import (CUFFT_C2C, CUFFT_C2R, CUFFT_R2C,
                              CUFFT_Z2Z, CUFFT_Z2D, CUFFT_D2Z,
                              CUFFT_CB_LD_COMPLEX, CUFFT_CB_LD_COMPLEX_DOUBLE,
                              CUFFT_CB_LD_REAL, CUFFT_CB_LD_REAL_DOUBLE,
                              CUFFT_CB_ST_COMPLEX, CUFFT_CB_ST_COMPLEX_DOUBLE,
                              CUFFT_CB_ST_REAL, CUFFT_CB_ST_REAL_DOUBLE,)
+from cupy.cuda.cufft import getVersion as get_cufft_version
 from cupy.cuda.device import get_compute_capability
 from cupy_backends.cuda.api.driver import get_build_version
 
@@ -62,36 +74,26 @@ _callback_load = ''
 _callback_store = ''
 _callback_mgr = []
 _callback_dev_code = None
+_default_cache_dir = os.path.expanduser('~/.cupy/callback_cache')
 
 
+def _get_cache_dir():
+    return os.environ.get('CUPY_CACHE_DIR', _default_cache_dir)
+
+
+# TODO(leofang): would it be more robust if we use distutils +
+# setuptools here? I'm worried that the number of lines of code
+# might inflate too much...
+# TODO(leofang): make sure Cython is installed at runtime
+# TODO(leofang): make sure all needed source files are included
+# in sdist/wheel
+# TODO(leofang): find a way to implement a lock-free method for
+# cached shared libraries like what's done in cupy/cuda/compiler.py
+# TODO(leofang): investigate if callerInfo can be supported. Looks
+# like in that case we can't cache the Python modules?
 class _CallbackManager:
     def __init__(self, plan_args, cb_load='', cb_store=''):
-        import importlib
-        import os
-        import shutil
-        import string
-        import subprocess
-        import sys
-        import sysconfig
-        import tempfile
-        from cupy._environment import get_nvcc_path
-
-        # TODO(leofang): cache the generated .so file based on:
-        #   - Python version
-        #   - CuPy version
-        #   - cuFFT version
-        #   - plan_args
-        #   - cb_load & cb_store
-
-        # TODO(leofang): would it be more robust if we use distutils +
-        # setuptools here? I'm worried that the number of lines of code
-        # might inflate too much...
-
-        # TODO(leofang): make sure Cython is installed at runtime
-
-        # TODO(leofang): make sure all needed source files are included
-        # in sdist/wheel
-
+        # Sanity checks
         if not cb_load and not cb_store:
             raise ValueError('need to specify either cb_load or cb_store, '
                              'or both')
@@ -103,76 +105,109 @@ class _CallbackManager:
         self.cb_load = cb_load
         self.cb_store = cb_store
 
+        # Set up some variables...
         python_include = sysconfig.get_paths()['include']
         nvcc = get_nvcc_path()
         CC = get_compute_capability()
         build_ver = get_build_version()
         source_dir = os.path.dirname(__file__) + '/../cuda/'
+        sysinfo = sys.version_info
 
-        # Set up temp directory; its lifetime is tied with the present instance
-        self.dir_obj = tempfile.TemporaryDirectory()
-        self.dir = self.dir_obj.name
+        # Generate module filename: all modules with the identical callbacks
+        # are considered identical regardless of which plan was actually
+        # executed at the time of generation
+        mod_name = 'cupy_callback_' + __version__ + '_PY'
+        for i in range(3):
+            mod_name += str(sysinfo[i])
+        mod_name += '_cuFFT' + str(get_cufft_version())
+        if cb_load:
+            mod_name += '_' + hashlib.md5(cb_load.encode()).hexdigest()
+        if cb_store:
+            mod_name += '_' + hashlib.md5(cb_store.encode()).hexdigest()
+        mod_name = mod_name.replace('.', '')
 
-        # Cythonize the Cython code; a c++ source cupy_callback.cpp is produced
-        shutil.copyfile(source_dir+'/cupy_cufft.h', self.dir+'/cupy_cufft.h')
-        shutil.copyfile(source_dir+'/cufft.pxd', self.dir+'/cupy_callback.pxd')
-        shutil.copyfile(source_dir+'/cufft.pyx', self.dir+'/cupy_callback.pyx')
-        p = subprocess.run(['cython', '-3', '--cplus',
-                            '-E', 'use_hip=0',
-                            '-E', 'CUDA_VERSION='+str(build_ver),
-                            '-E', 'CUPY_CUFFT_STATIC=True',
-                            self.dir+'/cupy_callback.pyx'],
-                           env=os.environ)
-        p.check_returncode()
+        # Check if the module is already cached on disk
+        cache_dir = _get_cache_dir() + '/'
+        if not os.path.isdir(cache_dir):
+            os.makedirs(cache_dir, exist_ok=True)
+        path = os.path.join(cache_dir, mod_name + '.so')
+        if not os.path.isfile(path):
+            # Set up temp directory
+            self.dir_obj = tempfile.TemporaryDirectory()
+            self.dir = self.dir_obj.name + '/'
 
-        # Compile the Python module
-        self.obj_host = self.dir+'/cupy_callback.o'
-        shutil.copyfile(source_dir+'/cupy_cufftx.h', self.dir+'/cupy_cufftx.h')
-        p = subprocess.run(['g++', '-I'+python_include,
-                            '-fPIC', '-pthread', '-O2', '-std=c++11',
-                            '-c', self.dir+'/cupy_callback.cpp',
-                            '-o', self.obj_host],
-                           env=os.environ)
-        p.check_returncode()
+            # Cythonize the Cython code to produce a c++ source file
+            shutil.copyfile(source_dir + '/cupy_cufft.h',
+                            self.dir+'/cupy_cufft.h')
+            shutil.copyfile(source_dir + '/cufft.pxd',
+                            self.dir + mod_name + '.pxd')
+            shutil.copyfile(source_dir + '/cufft.pyx',
+                            self.dir + mod_name + '.pyx')
+            p = subprocess.run(['cython', '-3', '--cplus',
+                                '-E', 'use_hip=0',
+                                '-E', 'CUDA_VERSION='+str(build_ver),
+                                '-E', 'CUPY_CUFFT_STATIC=True',
+                                self.dir + mod_name + '.pyx',
+                                '-o', self.dir + mod_name + '.cpp'],
+                               env=os.environ)
+            p.check_returncode()
 
-        # Dump and compile device code using nvcc
-        global _callback_dev_code
-        if _callback_dev_code is None:
-            with open(source_dir+'/cupy_cufftx.cu') as f:
-                support = _callback_dev_code = f.read()
-        else:
-            support = _callback_dev_code
-        with open(self.dir+'/cupy_cufftx.cu', 'w') as f:
-            support = string.Template(support).substitute(
-                dev_load_callback_ker=cb_load,
-                dev_store_callback_ker=cb_store,
-            )
-            f.write(support)
-        self.obj_dev = self.dir + '/cupy_callback_dev.o'
-        cmd = [nvcc, '-ccbin', 'g++', '-arch=sm_'+CC, '-dc',
-               '-c', self.dir+'/cupy_cufftx.cu',
-               '-Xcompiler', '-fPIC', '-O2', '-std=c++11']
-        if self.cb_load:
-            cmd.append('-DHAS_LOAD_CALLBACK')
-        if self.cb_store:
-            cmd.append('-DHAS_STORE_CALLBACK')
-        p = subprocess.run(cmd + ['-o', self.obj_dev], env=os.environ)
-        p.check_returncode()
+            # Compile the Python module
+            self.obj_host = self.dir + mod_name + '.o'
+            shutil.copyfile(source_dir + '/cupy_cufftx.h',
+                            self.dir + '/cupy_cufftx.h')
+            p = subprocess.run(['g++', '-I'+python_include,
+                                '-fPIC', '-pthread', '-O2', '-std=c++11',
+                                '-c', self.dir + mod_name + '.cpp',
+                                '-o', self.obj_host],
+                               env=os.environ)
+            p.check_returncode()
 
-        # Use nvcc to link and generate a shared library
-        # WARNING: CANNOT use host compiler to link!
-        self.lib = self.dir + '/cupy_callback.so'
-        p = subprocess.run([nvcc, '-ccbin', 'g++', '-shared', '-arch=sm_'+CC,
-                            self.obj_dev, self.obj_host,
-                            '-lcufft_static', '-lculibos', '-o', self.lib],
-                           env=os.environ)
-        p.check_returncode()
+            # Dump and compile device code using nvcc
+            # TODO(leofang): use the nvcc utilities from cupy/cuda/compiler.py
+            global _callback_dev_code
+            if _callback_dev_code is None:
+                with open(source_dir+'/cupy_cufftx.cu') as f:
+                    support = _callback_dev_code = f.read()
+            else:
+                support = _callback_dev_code
+            with open(self.dir+'/cupy_cufftx.cu', 'w') as f:
+                support = string.Template(support).substitute(
+                    dev_load_callback_ker=cb_load,
+                    dev_store_callback_ker=cb_store,
+                )
+                f.write(support)
+            self.obj_dev = self.dir + mod_name + '_dev.o'
+            cmd = [nvcc, '-ccbin', 'g++', '-arch=sm_'+CC, '-dc',
+                   '-c', self.dir+'/cupy_cufftx.cu',
+                   '-Xcompiler', '-fPIC', '-O2', '-std=c++11']
+            if self.cb_load:
+                cmd.append('-DHAS_LOAD_CALLBACK')
+            if self.cb_store:
+                cmd.append('-DHAS_STORE_CALLBACK')
+            p = subprocess.run(cmd + ['-o', self.obj_dev], env=os.environ)
+            p.check_returncode()
+
+            # Use nvcc to link and generate a shared library
+            # WARNING: CANNOT use host compiler to link!
+            p = subprocess.run([nvcc, '-ccbin', 'g++',
+                                '-shared', '-arch=sm_'+CC,
+                                self.obj_dev, self.obj_host,
+                                '-lcufft_static', '-lculibos',
+                                '-o', path],
+                               env=os.environ)
+            p.check_returncode()
+
+            self.dir_obj.cleanup()
+            del self.dir_obj
+            del self.dir
+            del self.obj_host
+            del self.obj_dev
 
         # Load the Python module
-        spec = importlib.util.spec_from_file_location(
-            'cupy_callback', self.lib)
+        spec = importlib.util.spec_from_file_location(mod_name, path)
         self.mod = module = importlib.util.module_from_spec(spec)
-        sys.modules['cupy_callback'] = module
+        sys.modules[mod_name] = module
         spec.loader.exec_module(module)
 
         # Create a cuFFT plan
@@ -194,9 +229,6 @@ class _CallbackManager:
                 raise ValueError('cb_store_type needs to be speficied')
             self.mod.setCallback(self.handle, cb_store_type, False)
         self.is_callback_set = True
-
-    def __del__(self):
-        self.dir_obj.cleanup()
 
 
 @contextlib.contextmanager
@@ -237,6 +269,13 @@ def set_cufft_callbacks(cb_load='', cb_store=''):
         slow. This is a limitation of cuFFT, so use this feature only when the
         callback-enabled transform is known more performant and can be reused
         to amortize the cost.
+
+    .. warning::
+        The generated Python modules are by default cached in
+        ``~/.cupy/callback_cache`` for possible reuse (with the same set of
+        load/store callbacks). Due to static linking, however, the file sizes
+        can be excessive! The cache position can be changed via setting
+        ``CUPY_CACHE_DIR``.
 
     """
     global _callback_load, _callback_store
