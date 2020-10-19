e8fb0859ec8abc281c49dea46d76d3261f0bb2f2
Masayuki Takagi <kamonama@gmail.com>
2020-10-19 13:11:22 +0000

    Fix cupyx.seterr() when `linalg` not supplied

--- tests/cupyx_tests/test_cupyx.py
+++ tests/cupyx_tests/test_cupyx.py
@@ -10,9 +10,13 @@ from cupy import testing
 class TestErrState(unittest.TestCase):
 
     def test_errstate(self):
+        orig = cupyx.geterr()
         with cupyx.errstate(divide=self.divide):
             state = cupyx.geterr()
             assert state['divide'] == self.divide
+            for key in state:
+                if key != 'divide':
+                    assert state[key] == orig[key]
 
     def test_seterr(self):
         pass
