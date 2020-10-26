f6896e97080be4d3c1504f7d1599ab6bcf8c1b43
Akifumi Imanishi <imanishi@preferred.jp>
2020-10-27 05:14:07 +0900

    Merge pull request #4094 from takagi/pytest-skip
    Make parameterized dtype test skip by `pytest.skip`

--- tests/cupy_tests/math_tests/test_matmul.py
+++ tests/cupy_tests/math_tests/test_matmul.py
@@ -112,7 +112,7 @@ class TestMatmulLarge(unittest.TestCase):
     def test_operator_matmul(self, xp, dtype1, dtype2):
         if ((dtype1, dtype2) in self.skip_dtypes or
                 (dtype2, dtype1) in self.skip_dtypes):
-            return xp.array([])
+            pytest.skip()
         x1 = testing.shaped_random(self.shape_pair[0], xp, dtype1)
         x2 = testing.shaped_random(self.shape_pair[1], xp, dtype2)
         return operator.matmul(x1, x2)
@@ -123,7 +123,7 @@ class TestMatmulLarge(unittest.TestCase):
     def test_cupy_matmul(self, xp, dtype1, dtype2):
         if ((dtype1, dtype2) in self.skip_dtypes or
                 (dtype2, dtype1) in self.skip_dtypes):
-            return xp.array([])
+            pytest.skip()
         shape1, shape2 = self.shape_pair
         x1 = testing.shaped_random(shape1, xp, dtype1)
         x2 = testing.shaped_random(shape2, xp, dtype2)
@@ -132,11 +132,9 @@ class TestMatmulLarge(unittest.TestCase):
 
 class TestMatmulOverflow(unittest.TestCase):
 
-    @testing.for_int_dtypes(name='dtype')
+    @testing.for_int_dtypes(name='dtype', no_bool=True)
     @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-3)  # required for uint8
     def test_overflow(self, xp, dtype):
-        if dtype is numpy.bool_:
-            return xp.array([])
         value = numpy.iinfo(dtype).max
         a = xp.array([value - 10]).astype(dtype)
         b = xp.array([value - 10]).astype(dtype)
