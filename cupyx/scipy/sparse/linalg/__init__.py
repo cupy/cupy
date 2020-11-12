253ac8e715cbd27f33f318d2d7fee1135768bdc6
venkywonka <gvenkatarama@nvidia.com>
2020-11-12 11:18:11 +0530

    🔨 another minor style fix

--- cupyx/scipy/sparse/linalg/__init__.py
+++ cupyx/scipy/sparse/linalg/__init__.py
@@ -7,4 +7,4 @@ from cupyx.scipy.sparse.linalg._solve import lsqr  # NOQA
 from cupyx.scipy.sparse.linalg._eigen import eigsh  # NOQA
 from cupyx.scipy.sparse.linalg._eigen import svds  # NOQA
 from cupyx.scipy.sparse.linalg._iterative import cg  # NOQA
-from cupyx.scipy.sparse.linalg.interface import * # NOQA
+from cupyx.scipy.sparse.linalg.interface import *  # NOQA
