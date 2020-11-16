5c4eec390ae1f28c62889b80b8a7b750d1bbcd4a
Leo Fang <leofang@bnl.gov>
2020-11-16 14:34:11 -0500

    [WIP] only works if the kernel is not cached...

--- cupy/core/_cub_reduction.pyx
+++ cupy/core/_cub_reduction.pyx
@@ -25,9 +25,30 @@ cdef function.Function _create_cub_reduction_function(
         reduce_type, params, arginfos, identity,
         pre_map_expr, reduce_expr, post_map_expr,
         _kernel._TypeMap type_map, preamble, options):
+    print(params)
     print(type_map)
+    print(arginfos)
     # A (incomplete) list of internal variables:
     # _J            : the index of an element in the array
+
+#    # TODO(leofang): handle _raw_in0 and _raw_out0 better when we support
+#    # multiple inputs/outputs from ReductionKernel
+#    cdef _kernel._ArgInfo arginfo
+#    cdef str in_var, out_var
+#    if params[0].name == 'in0' and params[1].name == 'out0':
+#        # from create_reduction_func
+#        in_var = '_raw_' + params[0].name
+#        out_var = '_raw_' + params[1].name
+#    else:
+#        # from ReductionKernel (names supplied by the user)
+#        in_var = params[0].name
+#        out_var = params[1].name
+    cdef str old_in0 = params[0].name, old_out0 = params[1].name
+    cdef str args = _get_cub_kernel_params(params, arginfos)
+    print("old:", old_in0, old_out0)
+    pre_map_expr = pre_map_expr.replace(old_in0, 'in0')
+    post_map_expr = post_map_expr.replace(old_out0, 'out0')
+    print(pre_map_expr, '\n', post_map_expr)
 
     # static_assert needs at least C++11 in NVRTC
     options += ('--std=c++11',)
@@ -116,7 +137,8 @@ __global__ void ${name}(${params}) {
   // Declare reduction operation
   _reduction_op op;
 
-  // input & output raw pointers
+  // input & output raw pointers; they might not be called as "_raw_in0" or
+  // "_raw_out0" if ReductionKernel is in use
   const type_mid_in* _in0 = static_cast<const type_mid_in*>(_raw_in0);
   type_mid_out* _out0 = static_cast<type_mid_out*>(_raw_out0);
 
@@ -201,7 +223,9 @@ __global__ void ${name}(${params}) {
         block_size=block_size,
         items_per_thread=items_per_thread,
         reduce_type=reduce_type,
-        params=_get_cub_kernel_params(params, arginfos),
+        params=args,
+#        _raw_in0=in_var,
+#        _raw_out0=out_var,
         identity=identity,
         reduce_expr=reduce_expr,
         pre_map_expr=pre_map_expr,
@@ -348,19 +372,28 @@ cdef str _get_cub_kernel_params(tuple params, tuple arginfos):
     cdef _kernel.ParameterInfo p
     cdef _kernel._ArgInfo arginfo
     cdef lst = []
-    cdef str c_type
+    cdef str c_type, c_name
     cdef int i
     assert len(params) == len(arginfos)
 
+    # For params coming from ReductionKernel, we rename the variable
+    # names to make it work with the CUB kernel
     for i, (p, arginfo) in enumerate(zip(params, arginfos)):
-        if i < len(params) - 2:
-            c_type = 'const void*' if p.is_const else 'void*'
+        c_name = arginfo.get_c_var_name(p)
+        if i == 0:
+            c_type = 'const void*'
+            if p.name != 'in0':
+                p.name = 'in0'
+                c_name = '_raw_in0'
+        elif i == 1:
+            c_type = 'void*'
+            if p.name != 'out0':
+                p.name = 'out0'
+                c_name = '_raw_out0'
         else:
             # for segment size and array size
             c_type = arginfo.get_param_c_type(p)
-        lst.append('{} {}'.format(
-            c_type,
-            arginfo.get_c_var_name(p)))
+        lst.append('{} {}'.format(c_type, c_name))
     return ', '.join(lst)
 
 
