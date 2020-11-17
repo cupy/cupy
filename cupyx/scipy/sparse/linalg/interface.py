9a77f0b2d8063d510ec10bcd25a853fc8642176c
venkywonka <gvenkatarama@nvidia.com>
2020-11-17 13:20:19 +0530

    💡 convert docstrings to google format

--- cupyx/scipy/sparse/linalg/interface.py
+++ cupyx/scipy/sparse/linalg/interface.py
@@ -12,6 +12,8 @@ skips the storage of zeros, but not ones. By contrast, a LinearOperator is
 able to represent such matrices efficiently. First, we need a compact way to
 represent an all-ones matrix::
 
+
+Example:
     >>> import cupy as cp
     >>> class Ones(LinearOperator):
     ...     def __init__(self, shape):
@@ -19,10 +21,11 @@ represent an all-ones matrix::
     ...     def _matvec(self, x):
     ...         return cp.repeat(x.sum(), self.shape[0])
 
-Instances of this class emulate ``cp.ones(shape)``, but using a constant
-amount of storage, independent of ``shape``. The ``_matvec`` method specifies
-how this linear operator multiplies with (operates on) a vector. We can now
-add this operator to a sparse matrix that stores only offsets from one::
+    Instances of this class emulate ``cp.ones(shape)``, but using a constant
+    amount of storage, independent of ``shape``. The ``_matvec`` method
+    specifies how this linear operator multiplies with (operates on) a vector.
+    We can now add this operator to a sparse matrix that stores only offsets
+    from one::
 
     >>> from cupyx.scipy.sparse import csr_matrix
     >>> offsets = csr_matrix(cupy.array([[1, 0, 2], [0, -1, 0], [0, 0, 3]]))
@@ -30,8 +33,8 @@ add this operator to a sparse matrix that stores only offsets from one::
     >>> A.dot([1, 2, 3])
     array([13,  4, 15])
 
-The result is the same as that given by its dense, explicitly-stored
-counterpart::
+    The result is the same as that given by its dense, explicitly-stored
+    counterpart::
 
     >>> (cp.ones(A.shape, A.dtype) + offsets.toarray()).dot([1, 2, 3])
     array([13,  4, 15])
@@ -75,64 +78,53 @@ class LinearOperator(object):
     ``_adjoint`` is preferable; ``_rmatvec`` is mostly there for
     backwards compatibility.
 
-    Parameters
-    ----------
-    shape : tuple
-        Matrix dimensions (M, N).
-    matvec : callable f(v)
-        Returns returns A * v.
-    rmatvec : callable f(v)
-        Returns A^H * v, where A^H is the conjugate transpose of A.
-    matmat : callable f(V)
-        Returns A * V, where V is a dense matrix with dimensions (N, K).
-    dtype : dtype
-        Data type of the matrix.
-    rmatmat : callable f(V)
-        Returns A^H * V, where V is a dense matrix with dimensions (M, K).
 
-    Attributes
-    ----------
-    args : tuple
-        For linear operators describing products etc. of other linear
-        operators, the operands of the binary operation.
-    ndim : int
-        Number of dimensions (this is always 2)
-
-    See Also
-    --------
-    aslinearoperator : Construct LinearOperators
-
-    Notes
-    -----
-    The user-defined matvec() function must properly handle the case
-    where v has shape (N,) as well as the (N,1) case.  The shape of
-    the return type is handled internally by LinearOperator.
-
-    LinearOperator instances can also be multiplied, added with each
-    other and exponentiated, all lazily: the result of these operations
-    is always a new, composite LinearOperator, that defers linear
-    operations to the original operators and combines the results.
-
-    More details regarding how to subclass a LinearOperator and several
-    examples of concrete LinearOperator instances can be found in the
-    external project `PyLops <https://pylops.readthedocs.io>`_.
+    Attributes:
+        shape (tuple):  Matrix dimensions (M, N).
+        matvec (callable f(v)):  Returns returns A * v.
+        rmatvec (callable f(v)):  Returns A^H * v, where A^H is the conjugate
+                                  transpose of A.
+        matmat (callable f(V)):  Returns A * V, where V is a dense matrix
+                                  with dimensions (N, K).
+        dtype (dtype):  Data type of the matrix.
+        rmatmat (callable f(V)):  Returns A^H * V, where V is a dense matrix
+                                   with dimensions (M, K).
 
 
-    Examples
-    --------
-    >>> import cupy as cp
-    >>> from cupyx.scipy.sparse.linalg import LinearOperator
-    >>> def mv(v):
-    ...     return cp.array([2*v[0], 3*v[1]])
-    ...
-    >>> A = LinearOperator((2,2), matvec=mv)
-    >>> A
-    <2x2 _CustomLinearOperator with dtype=float64>
-    >>> A.matvec(cp.ones(2))
-    array([ 2.,  3.])
-    >>> A * cp.ones(2)
-    array([ 2.,  3.])
+    Args:
+        args (tuple):  For linear operators describing products etc. of other
+                        linear operators, the operands of the binary operation.
+        ndim (int):  Number of dimensions (this is always 2)
 
+
+    Note:
+        The user-defined matvec() function must properly handle the case
+        where v has shape (N,) as well as the (N,1) case.  The shape of
+        the return type is handled internally by LinearOperator.
+
+        LinearOperator instances can also be multiplied, added with each
+        other and exponentiated, all lazily: the result of these operations
+        is always a new, composite LinearOperator, that defers linear
+        operations to the original operators and combines the results.
+
+        More details regarding how to subclass a LinearOperator and several
+        examples of concrete LinearOperator instances can be found in the
+        external project `PyLops <https://pylops.readthedocs.io>`_.
+
+
+    Examples:
+        >>> import cupy as cp
+        >>> from cupyx.scipy.sparse.linalg import LinearOperator
+        >>> def mv(v):
+        ...     return cp.array([2*v[0], 3*v[1]])
+        ...
+        >>> A = LinearOperator((2,2), matvec=mv)
+        >>> A
+        <2x2 _CustomLinearOperator with dtype=float64>
+        >>> A.matvec(cp.ones(2))
+        array([ 2.,  3.])
+        >>> A * cp.ones(2)
+        array([ 2.,  3.])
     """
 
     ndim = 2
@@ -202,21 +194,19 @@ class LinearOperator(object):
         Performs the operation y=A*x where A is an MxN linear
         operator and x is a column vector or 1-d array.
 
-        Parameters
-        ----------
-        x : {ndarray}
-            An array with shape (N,) or (N,1).
 
-        Returns
-        -------
-        y : {ndarray}
-            A matrix or ndarray with shape (M,) or (M,1) depending
-            on the type and shape of the x argument.
+        Args:
+            x (ndarray):  array with shape (N,) or (N,1).
 
-        Notes
-        -----
-        This matvec wraps the user-specified matvec routine or overridden
-        _matvec method to ensure that y has the correct shape and type.
+
+        Returns:
+            y (ndarray):  An ndarray with shape (M,) or (M,1)
+                          depending on the type and shape of the x argument.
+
+
+        Note:
+            This matvec wraps the user-specified matvec routine or overridden
+            _matvec method to ensure that y has the correct shape and type.
 
         """
 
@@ -246,21 +236,19 @@ class LinearOperator(object):
         Performs the operation y = A^H * x where A is an MxN linear
         operator and x is a column vector or 1-d array.
 
-        Parameters
-        ----------
-        x : {matrix, ndarray}
-            An array with shape (M,) or (M,1).
 
-        Returns
-        -------
-        y : {matrix, ndarray}
-            A matrix or ndarray with shape (N,) or (N,1) depending
-            on the type and shape of the x argument.
+        Args:
+            x (ndarray):  An array with shape (M,) or (M,1).
 
-        Notes
-        -----
-        This rmatvec wraps the user-specified rmatvec routine or overridden
-        _rmatvec method to ensure that y has the correct shape and type.
+
+        Returns:
+            y (ndarray):  An ndarray with shape (N,) or (N,1)
+                          depending on the type and shape of the x argument.
+
+
+        Note:
+            This rmatvec wraps the user-specified rmatvec routine or overridden
+            _rmatvec method to ensure that y has the correct shape and type.
 
         """
 
@@ -299,21 +287,19 @@ class LinearOperator(object):
         Performs the operation y=A*X where A is an MxN linear
         operator and X dense N*K matrix or ndarray.
 
-        Parameters
-        ----------
-        X : {ndarray}
-            An array with shape (N,K).
 
-        Returns
-        -------
-        Y : {ndarray}
-            A matrix or ndarray with shape (M,K) depending on
-            the type of the X argument.
+        Args:
+            X (ndarray):  An array with shape (N,K).
 
-        Notes
-        -----
-        This matmat wraps any user-specified matmat routine or overridden
-        _matmat method to ensure that y has the correct type.
+
+        Returns:
+            Y (ndarray):  An ndarray with shape (M,K)
+                          depending on the type of the X argument.
+
+
+        Note:
+            This matmat wraps any user-specified matmat routine or overridden
+            _matmat method to ensure that y has the correct type.
 
         """
 
@@ -338,19 +324,18 @@ class LinearOperator(object):
         operator and x is a column vector or 1-d array, or 2-d array.
         The default implementation defers to the adjoint.
 
-        Parameters
-        ----------
-        X : {ndarray}
-            A matrix or 2D array.
 
-        Returns
-        -------
-        Y : {ndarray}
-            A matrix or 2D array depending on the type of the input.
+        Args:
+            X (ndarray):  A 2D array.
 
-        Notes
-        -----
-        This rmatmat wraps the user-specified rmatmat routine.
+
+        Returns:
+            Y (ndarray):  A 2D array depending on the type of the
+                          input
+
+
+        Note:
+            This rmatmat wraps the user-specified rmatmat routine.
 
         """
 
@@ -383,16 +368,15 @@ class LinearOperator(object):
     def dot(self, x):
         """Matrix-matrix or matrix-vector multiplication.
 
-        Parameters
-        ----------
-        x : array_like
-            1-d or 2-d array, representing a vector or matrix.
 
-        Returns
-        -------
-        Ax : array
-            1-d or 2-d array (depending on the shape of x) that represents
-            the result of applying this linear operator on x.
+        Args:
+            x (array_like):  1-d or 2-d array, representing a vector or matrix.
+
+
+        Returns:
+            Ax (array):  1-d or 2-d array (depending on the shape of x) that
+                         represents the result of applying this linear
+                         operator on x.
 
         """
         if isinstance(x, LinearOperator):
@@ -464,10 +448,9 @@ class LinearOperator(object):
 
         Can be abbreviated self.H instead of self.adjoint().
 
-        Returns
-        -------
-        A_H : LinearOperator
-            Hermitian adjoint of self.
+
+        Returns:
+            A_H (LinearOperator):  Hermitian adjoint of self.
         """
         return self._adjoint()
 
@@ -776,18 +759,18 @@ def aslinearoperator(A):
 
     See the LinearOperator documentation for additional information.
 
-    Notes
-    -----
-    If 'A' has no .dtype attribute, the data type is determined by calling
-    :func:`LinearOperator.matvec()` - set the .dtype attribute to prevent this
-    call upon the linear operator creation.
 
-    Examples
-    --------
-    >>> from cupyx.scipy.sparse.linalg import aslinearoperator
-    >>> M = cp.array([[1,2,3],[4,5,6]], dtype=cp.int32)
-    >>> aslinearoperator(M)
-    <2x3 MatrixLinearOperator with dtype=int32>
+    Note:
+        If 'A' has no .dtype attribute, the data type is determined by calling
+        :func:`LinearOperator.matvec()` - set the .dtype attribute to prevent
+        this call upon the linear operator creation.
+
+
+    Examples:
+        >>> from cupyx.scipy.sparse.linalg import aslinearoperator
+        >>> M = cp.array([[1,2,3],[4,5,6]], dtype=cp.int32)
+        >>> aslinearoperator(M)
+        <2x3 MatrixLinearOperator with dtype=int32>
     """
     if isinstance(A, LinearOperator):
         return A
