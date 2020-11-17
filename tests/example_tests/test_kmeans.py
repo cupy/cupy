53433da1ae66ef1dba02a24649a6498b7f0b1918
Toshiki Kataoka <kataoka@preferred.jp>
2020-11-17 15:46:47 +0900

    Replace `self.assertRegex` with `assert re.search`

--- tests/example_tests/test_kmeans.py
+++ tests/example_tests/test_kmeans.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 import tempfile
 import unittest
@@ -17,17 +18,19 @@ class TestKmeans(unittest.TestCase):
     def test_default(self):
         output = example_test.run_example(
             'kmeans/kmeans.py', '-m', '1', '--num', '10')
-        self.assertRegex(
+        assert re.search(
+            r' CPU :  [0-9\.]+ sec\s+GPU :  [0-9\.]+ sec',
             output.decode('utf-8'),
-            r' CPU :  [0-9\.]+ sec\s+GPU :  [0-9\.]+ sec')
+        )
 
     def test_custom_kernel(self):
         output = example_test.run_example(
             'kmeans/kmeans.py', '-m', '1', '--num', '10',
             '--use-custom-kernel')
-        self.assertRegex(
+        assert re.search(
+            r' CPU :  [0-9\.]+ sec\s+GPU :  [0-9\.]+ sec',
             output.decode('utf-8'),
-            r' CPU :  [0-9\.]+ sec\s+GPU :  [0-9\.]+ sec')
+        )
 
     def test_result_image(self):
         dir_path = tempfile.mkdtemp()
