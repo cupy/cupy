00216fef47b24c7c4d371cb350db7305d85e7d7b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-11-24 19:45:13 +0900

    fix link to numpy func

--- cupy/random/__init__.py
+++ cupy/random/__init__.py
@@ -4,7 +4,7 @@ import numpy as _numpy
 def bytes(length):
     """Returns random bytes.
 
-    .. note:: This function is just a wrapper for :meth:`numpy.random.bytes`.
+    .. note:: This function is just a wrapper for :obj:`numpy.random.bytes`.
         The resulting bytes are generated on the host (NumPy), not GPU.
 
     .. seealso:: :meth:`numpy.random.bytes
