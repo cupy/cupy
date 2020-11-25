927abebedffd6cda868f0db83bff2a15ba15f0c0
Peter Andreas Entschev <peter@entschev.com>
2020-11-25 10:59:59 -0800

    Fix is_ptds_enabled

--- cupy_backends/cuda/stream.pyx
+++ cupy_backends/cuda/stream.pyx
@@ -52,6 +52,5 @@ cdef set_current_stream_ptr(intptr_t ptr):
     tls.set_current_stream_ptr(ptr)
 
 cdef bint is_ptds_enabled():
-    return bool(
-        int(os.environ.get('CUPY_CUDA_PER_THREAD_DEFAULT_STREAM', '0')) != 0)
-    )
+    ptds = int(os.environ.get('CUPY_CUDA_PER_THREAD_DEFAULT_STREAM', '0'))
+    return bool(ptds != 0)
