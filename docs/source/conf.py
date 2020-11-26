91be7eb6a4b3ce2a0a90dc0b15170ea3698b0ce8
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-11-26 22:20:45 +0900

    use theme on RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -153,8 +153,7 @@ napoleon_include_special_with_doc = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if not on_rtd:
-    html_theme = 'pydata_sphinx_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
