7e370b19541930065a5196e03e721a04d1b7f3db
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-11-27 14:13:22 +0900

    update classifier

--- setup.py
+++ setup.py
@@ -133,7 +133,6 @@ Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
