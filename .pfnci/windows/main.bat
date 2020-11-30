3ebb012d87bcfb3ada168aad08a67eed21fe83ef
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-11-30 17:12:23 +0900

    check error

--- .pfnci/windows/main.bat
+++ .pfnci/windows/main.bat
@@ -4,23 +4,30 @@ set PYTHON=%1
 set CUDA=%2
 
 :: Set environment variables
-call _use_cuda.bat %CUDA%
-call _use_python.bat %PYTHON%
+call .pfnci\windows\_use_cuda.bat %CUDA% || goto :error
+call .pfnci\windows\_use_python.bat %PYTHON% || goto :error
 
 :: Show environment variables
-set
-python -V
-
+set || goto :error
+python -V || goto :error
 
 :: Install dependencies
-python -m pip install -U Cython
-python -m pip list
+python -m pip install -U Cython || goto :error
+python -m pip list || goto :error
 
 :: Build
-python -m pip install -e ".[jenkins]" -vvv
+python -m pip install -e ".[jenkins]" -vvv || goto :error
 
 :: Test import
-python -c "import cupy; cupy.show_config()"
+python -c "import cupy; cupy.show_config()" || goto :error
 
 :: Run unit tests
-python -m pytest tests
+python -m pytest tests || goto :error
+
+
+goto :EOF
+
+:: Error handling
+:error
+echo Failed with status %errorlevel%.
+exit /b %errorlevel%
