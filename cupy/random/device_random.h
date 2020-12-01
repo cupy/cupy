e4f95a9fdd94213b1d04cbb2a95d4368e74803b7
Emilio Castillo <ecastill@preferred.jp>
2020-12-01 03:02:30 +0000

    Fix hip

--- cupy/random/device_random.h
+++ cupy/random/device_random.h
@@ -1,6 +1,16 @@
 #ifndef _CUPY_TEST_H
 #define _CUPY_TEST_H
 
+#if CUPY_USE_HIP
+
+#include <hiprand/hiprand_kernel.h>
+
+typedef hiprandState curandState;
+typedef hiprandStateMRG32k3a {} curandStateMRG32k3a;
+typedef hiprandStatePhilox4_32_10_t {} curandStatePhilox4_32_10_t;
+
+# else
+
 #ifndef CUPY_NO_CUDA
 
 #include <curand_kernel.h>
@@ -11,5 +21,6 @@ typedef struct {} curandState;
 typedef struct {} curandStateMRG32k3a;
 typedef struct {} curandStatePhilox4_32_10_t;
 
+#endif
 #endif
 #endif
