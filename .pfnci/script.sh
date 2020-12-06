d6afdfccec78bc41d5cf4ba937f4aa9473c6821e
Leo Fang <leofang@bnl.gov>
2020-12-06 03:43:05 -0500

    uninstall cuda from ci

--- .pfnci/script.sh
+++ .pfnci/script.sh
@@ -70,6 +70,11 @@ main() {
       wget -qO - http://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -
       echo 'deb [arch=amd64] http://repo.radeon.com/rocm/apt/debian/ xenial main' | tee /etc/apt/sources.list.d/rocm.list
 
+      # Uninstall CUDA to ensure it's a clean ROCm environment
+      # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#removing-cuda-tk-and-driver
+      apt-get --purge remove "*cublas*" "*cufft*" "*curand*" "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*"
+      apt-get autoremove
+
       apt update -qqy
       apt install rocm-dev hipblas hipsparse rocsparse rocrand rocthrust rocsolver rocfft hipcub rocprim rccl -qqy
       apt list --installed | grep cuda  # TODO(leofang): remove cuda toolkit
