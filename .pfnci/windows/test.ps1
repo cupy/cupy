1f9ba6966a85de2aa38509a967873836d25e920a
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-02-07 15:09:09 +0000

    change log extension to avoid download, imporve logging

--- .pfnci/windows/test.ps1
+++ .pfnci/windows/test.ps1
@@ -14,10 +14,12 @@ $cupy_kernel_cache_file = "cupy_kernel_cache_windows.zip"
 
 function DownloadCache {
     pushd $Env:USERPROFILE
+    echo "Downloading kernel cache..."
     gsutil -m -q cp gs://tmp-asia-pfn-public-ci/cupy-ci/$cupy_kernel_cache_file .
     if (-not $?) {
         echo "*** Kernel cache unavailable"
     } else {
+        echo "Extracting kernel cache..."
         RunOrDie 7z x $cupy_kernel_cache_file
         rm $cupy_kernel_cache_file
     }
@@ -26,18 +28,22 @@ function DownloadCache {
 
 function UploadCache {
     # Expires in 3 days, maximum 1 GB
+    echo "Trimming kernel cache..."
     RunOrDie python .pfnci\trim_cupy_kernel_cache.py --expiry 259200 --max-size 1000000000 --rm
 
     pushd $Env:USERPROFILE
     # -mx=0 ... no compression
     # -mtc=on ... preserve timestamp
+    echo "Compressing kernel cache..."
     RunOrDie 7z a -tzip -mx=0 -mtc=on $cupy_kernel_cache_file .cupy
+    echo "Uploading kernel cache..."
     RunOrDie gsutil -m -q cp $cupy_kernel_cache_file gs://tmp-asia-pfn-public-ci/cupy-ci/
     popd
 }
 
 function Main {
     # Setup environment
+    echo "Using CUDA $cuda and Python $python"
     ActivateCUDA $cuda
     ActivatePython $python
 
@@ -48,12 +54,16 @@ function Main {
     RunOrDie cmd.exe /C set
 
     # Build
+    echo "Setting up test environment"
     RunOrDie python -V
     RunOrDie python -m pip install Cython scipy optuna
     RunOrDie python -m pip freeze
-    RunOrDie python -m pip install -e ".[all,jenkins]" -vvv > cupy_build.log
+
+    echo "Building..."
+    RunOrDie python -m pip install -e ".[all,jenkins]" -vvv > cupy_build_log.txt
 
     # Import test
+    echo "CuPy Configuration:"
     RunOrDie python -c "import cupy; cupy.show_config()"
 
     # Unit test
@@ -73,7 +83,8 @@ function Main {
     if ($use_cache) {
         DownloadCache
     }
-    python -m pytest -rfEX $Env:PYTEST_OPTS tests > cupy_test.log
+    echo "Running test..."
+    python -m pytest -rfEX $Env:PYTEST_OPTS tests > cupy_test_log.txt
     if (-not $?) {
         $test_retval = $LastExitCode
     }
@@ -84,9 +95,12 @@ function Main {
     # Upload test results
     echo "Uploading test results..."
     $artifact_id = $Env:CI_JOB_ID
-    RunOrDie gsutil -m -q cp cupy_build.log cupy_test.log "gs://chainer-artifacts-pfn-public-ci/cupy-ci/$artifact_id/"
-    echo "Build Log: https://storage.googleapis.com/chainer-artifacts-pfn-public-ci/cupy-ci/$artifact_id/cupy_build.log"
-    echo "Test Log: https://storage.googleapis.com/chainer-artifacts-pfn-public-ci/cupy-ci/$artifact_id/cupy_test.log"
+    RunOrDie gsutil -m -q cp cupy_build_log.txt cupy_test_log.txt "gs://chainer-artifacts-pfn-public-ci/cupy-ci/$artifact_id/"
+    echo "Build Log: https://storage.googleapis.com/chainer-artifacts-pfn-public-ci/cupy-ci/$artifact_id/cupy_build_log.txt"
+    echo "Test Log: https://storage.googleapis.com/chainer-artifacts-pfn-public-ci/cupy-ci/$artifact_id/cupy_test_log.txt"
+
+    echo "Last 10 lines from the test output:"
+    Get-Content cupy_test_log.txt -Tail 10
 
     if ($test_retval -ne 0) {
         throw "Test failed with status $test_retval"
