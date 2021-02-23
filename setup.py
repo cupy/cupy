ddb892472edfc9d34dc969327e07dd2100f27dc7
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-02-23 14:03:55 +0000

    add classifier

--- setup.py
+++ setup.py
@@ -133,6 +133,7 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3 :: Only
 Programming Language :: Cython
 Topic :: Software Development
