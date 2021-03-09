1ea59aeab0a5c7853193d1e7ad378fb7ad77fffe
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-03-09 10:28:28 +0000

    add tests

--- tests/cupyx_tests/tools_tests/test_install_library.py
+++ tests/cupyx_tests/tools_tests/test_install_library.py
@@ -1,9 +1,16 @@
+import os
+import platform
 import tempfile
 import unittest
 import urllib
 
 from cupy import testing
 from cupyx.tools import install_library
+
+import pytest
+
+
+_platform_name = platform.system()
 
 
 class TestInstallLibrary(unittest.TestCase):
@@ -25,12 +32,26 @@ class TestInstallLibrary(unittest.TestCase):
                 install_library.install_lib(cuda, d, 'cutensor')
 
     @testing.slow
+    @pytest.mark.skipif(
+        _platform_name == 'Windows', reason='NCCL is only available for Linux')
     def test_install_nccl(self):
         # Try installing NCCL for all supported CUDA versions
         for rec in install_library._nccl_records:
             cuda = rec['cuda']
+            version = rec['nccl']
+            filename = rec['assets'][_platform_name]['filename']
             with tempfile.TemporaryDirectory() as d:
                 install_library.install_lib(cuda, d, 'nccl')
+                self._check_file_exists(d, cuda, 'nccl', version, filename)
+
+    def _check_file_exists(self, prefix, cuda, lib, version, filename):
+        install_root = os.path.join(prefix, cuda, lib, version)
+        assert os.path.isdir(install_root)
+        for _x, _y, files in os.walk(install_root):
+            print(_x, _y, files)
+            if filename in files:
+                return
+        pytest.fail('expected file cound not be found')
 
     def test_urls(self):
         assets = [r['assets'] for r in (
