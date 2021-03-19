db8fa2dda63ae746964032ac2bcd851a8ddfcea9
povinsahu1909 <povinsahu@gmail.com>
2021-03-19 14:22:19 +0530

    Added exponential distribution

--- tests/cupy_tests/random_tests/test_generator_api.py
+++ tests/cupy_tests/random_tests/test_generator_api.py
@@ -206,6 +206,28 @@ class TestBeta(GeneratorTestCase):
             a=self.a, b=self.b, size=2000, dtype=dtype)
 
 
+@testing.parameterise(
+    {'scale': 0.5},
+    {'scale': 1},
+    {'scale': 10},
+)
+@testing.with_requires('numpy>=1.17.0')
+@testing.gpu
+@testing.fix_random()
+
+class TestExponential(GeneratorTestCase):
+
+    target_method = 'exponential'
+
+    def test_exponential(self):
+        self.generate(scale=self.scale, size=(3, 2))
+
+    @condition.repeat_with_success_at_least(10, 3)
+    def test_exponential_ks(self, dtype):
+        self.check_ks(0.05)(
+            self.scale, size=2000, dtype=dtype)
+
+
 class InvalidOutsMixin:
 
     def invalid_dtype_out(self, **kwargs):
