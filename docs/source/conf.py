c80bf8c5cdcfcd579fdfd90fe3c49e6d6d31a2a4
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-03-20 17:33:14 +0000

    remove unnecessary configurations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -198,18 +198,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#  html_static_path = ['_static']
-#  
-#  html_style = 'css/theme.css'
-#  
-#  if on_rtd:
-#      html_context = {
-#          'css_files': [
-#              #'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-#              'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-#              #'_static/css/modified_theme.css',
-#          ],
-#      }
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
