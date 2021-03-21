1e7c7b85d13b7c2c8f415446b98bdaec428d93cf
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-21 23:16:33 +0530

    Add files via upload
    Added the implementation of method "alen"
    https://docs.cupy.dev/en/stable/reference/comparison.html

---
+++ cupy/_manipulation/alen.py
@@ -0,0 +1,15 @@
+import cupy._manipulation.shape
+
+def alen(arr):
+	"""Returns the first dimension of an array
+
+   	Args:
+        a (array_like): Input array
+
+   	 Returns:
+        	Single Int value : it gives the length of the first
+		dimension of the array.
+
+    	"""
+
+	return arr.shape[0]
