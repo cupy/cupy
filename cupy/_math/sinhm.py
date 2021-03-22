16087b225635d4f2497f1ddeb2e74f009ae1923d
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-22 14:46:34 +0530

    Implemented Hyperbolic Sine  Function for Matrix
    Returns hyperbolic sine of the given square matrix.
    Args:
    arr: square matrix 
    
    Returns:
    Gyperbolic sine of given square matrix as input

---
+++ cupy/_math/sinhm.py
@@ -0,0 +1,12 @@
+from numpy import exp
+
+def sinhm(arr):
+    """Returns hyperbolic sine of the given square matrix.
+Args:
+arr: square matrix 
+
+Returns:
+Gyperbolic sine of given square matrix as input
+    """
+    
+    return 0.5*(exp(arr)-exp(-1*arr))
