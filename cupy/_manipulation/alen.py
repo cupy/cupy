550cc56c8a7079126b01ac50caef30ae34ba825a
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-22 10:13:27 +0530

    Update alen.py

--- cupy/_manipulation/alen.py
+++ cupy/_manipulation/alen.py
@@ -1,17 +1,16 @@
-import cupy._manipulation.shape
+from numpy import shape
+
 
 def alen(arr):
-	"""Returns the first dimension of an array
+    """Returns the first dimension of an array
 
-   	Args:
-        a (array_like): Input array
+Args:
+a (array_like): Input array
 
-   	 Returns:
-        	Single Int value : it gives the length of the first
-		dimension of the array.
+Returns:
+Single Int value : it gives the length of the first
+dimension of the array.
 
-    	"""
+    """
 
-	return shape(arr)[0]
-
-
+    return arr.shape[0]
