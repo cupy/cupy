aa8f5b4732e7a3ec70f04cd53ec044a785ee484e
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-22 14:55:30 +0530

    Update sinhm.py

--- cupy/_math/sinhm.py
+++ cupy/_math/sinhm.py
@@ -1,10 +1,10 @@
 from numpy import exp
 
+
 def sinhm(arr):
     """Returns hyperbolic sine of the given square matrix.
 Args:
 arr: square matrix 
-
 Returns:
 Gyperbolic sine of given square matrix as input
     """
