2810dc1277fffd3a80e834a287e5498e45684fe8
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-23 08:04:13 +0530

    Update matfuncs.py

--- cupyx/scipy/linalg/matfuncs.py
+++ cupyx/scipy/linalg/matfuncs.py
@@ -5,10 +5,10 @@ def sinhm(arr):
     """Returns hyperbolic sine of the given square matrix.
     
     Args: 
-    arr(cupy.ndarray) : Square matrix whose hyperbolic sine has to be calculated .
+        arr(cupy.ndarray) : Square matrix whose hyperbolic sine has to be calculated .
 
     Returns: 
     
-    (cupy.ndarray):  Hyperbolic sine of given square matrix as input.
+        (cupy.ndarray):  Hyperbolic sine of given square matrix as input.
     """
     return 0.5 * (exp(arr) - exp(-1 * arr))
