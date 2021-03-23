dd59b1310e94000f265cd393ab232e5925c0cd32
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-23 07:55:03 +0530

    Update cupyx/scipy/linalg/matfuncs.py
    Added Space between operands
    
    Co-authored-by: emcastillo <ecastill@preferred.jp>

--- cupyx/scipy/linalg/matfuncs.py
+++ cupyx/scipy/linalg/matfuncs.py
@@ -6,4 +6,4 @@ def sinhm(arr):
 Args: arr: square matrix .
 Returns: Hyperbolic sine of given square matrix as input.
     """
-    return 0.5*(exp(arr)-exp(-1*arr))
+    return 0.5 * (exp(arr) - exp(-1 * arr))
