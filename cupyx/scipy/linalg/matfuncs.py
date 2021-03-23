3902b0ae15ebaff84f74801137557893c7ab5a6f
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-23 22:38:45 +0530

    Updated the function file covering edge cases

--- cupyx/scipy/linalg/matfuncs.py
+++ cupyx/scipy/linalg/matfuncs.py
@@ -1,17 +1,31 @@
-from numpy import exp
+import numpy
 
 
 def sinhm(arr):
     """Hyperbolic Sine
     Returns hyperbolic sine of the given square matrix.
-
     Args:.
-
         arr(cupy.ndarray) : Square matrix whose hyperbolic .
         sine has to be calculated .
-
     Returns: .
-
         (cupy.ndarray):  Hyperbolic sine of given square matrix as input.
     """
-    return 0.5 * (exp(arr) - exp(-1 * arr))
+    arr = numpy.asanyarray(arr)
+
+    "Checking whether the input is a 2D matrix or not"
+    if(len(arr.shape) != 2):
+        raise ValueError("Dimensions of matrix should be 2")
+
+    "Checking whether the input matrix is square matrix or not"
+    if(arr.shape[0] != arr.shape[1]):
+        raise ValueError("Input matrix should be a square matrix")
+
+    "Checking whether the input matrix elements are nan or not"
+    if(numpy.isnan(numpy.sum(arr))):
+        raise ValueError("Input matrix elements cannot be nan")
+
+    "Checking whether the input matrix elements are infinity or not"
+    if(numpy.isinf(numpy.sum(arr))):
+        raise ValueError("Input matrix elements cannot be infinity")
+
+    return 0.5 * (numpy.exp(arr) - numpy.exp(-1 * arr))
