c69fbe73922aead9905b9dc6b8ee3987fb23d41c
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-26 15:32:49 +0530

    Update matfuncs.py

--- cupyx/scipy/linalg/matfuncs.py
+++ cupyx/scipy/linalg/matfuncs.py
@@ -12,19 +12,19 @@ def sinhm(arr):
     """
     arr = numpy.asanyarray(arr)
 
-    #Checking whether the input is a 2D matrix or not
+    # Checking whether the input is a 2D matrix or not
     if(len(arr.shape) != 2):
         raise ValueError("Dimensions of matrix should be 2")
 
-    #Checking whether the input matrix is square matrix or not
+    # Checking whether the input matrix is square matrix or not
     if(arr.shape[0] != arr.shape[1]):
         raise ValueError("Input matrix should be a square matrix")
 
-    #Checking whether the input matrix elements are nan or not
+    # Checking whether the input matrix elements are nan or not
     if(numpy.isnan(numpy.sum(arr))):
         raise ValueError("Input matrix elements cannot be nan")
 
-    #Checking whether the input matrix elements are infinity or not
+    # Checking whether the input matrix elements are infinity or not
     if(numpy.isinf(numpy.sum(arr))):
         raise ValueError("Input matrix elements cannot be infinity")
 
