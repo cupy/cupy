d8b04e19e7b5a51805b2f4cb6b17268187ef5b55
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-26 16:33:01 +0530

    Added comments inside functions

--- tests/cupyx_tests/scipy_tests/linalg_tests/test_matfuncs.py
+++ tests/cupyx_tests/scipy_tests/linalg_tests/test_matfuncs.py
@@ -5,28 +5,32 @@ import unittest
 
 class TestMatFuncsNan(unittest.TestCase):
 
-    "Testing for nan values in matrix"
 
     def test_values_nan(self):
+
+    # Testing for nan values in matrix
         r = numpy.empty((3, 3,))
         r.fill(numpy.nan)
         self.assertRaises(ValueError, sinhm, r)
 
-    "Testing for infinity values in matrix"
 
     def test_values_inf(self):
+
+    # Testing for nan values in matrix
         z = numpy.zeros([2, 2])
         z[0][0] = numpy.inf
         self.assertRaises(ValueError, sinhm, z)
 
-    "Testing for matrix shape "
+
 
     def test_shape(self):
+
+    # Testing for matrix shape 
         k = numpy.zeros([3, 2])
         self.assertRaises(ValueError, sinhm, k)
 
-    "Testing whether it is as 2D array or not"
 
     def test_dimension_count(self):
+    # Testing whether it is as 2D array or not
         g = numpy.zeros([3, 3, 3])
         self.assertRaises(ValueError, sinhm, g)
