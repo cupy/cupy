bf66dae3e6ad9d1c11a0ca448d31d497b5204053
ameyachawla99 <78856161+ameyachawla99@users.noreply.github.com>
2021-03-27 18:41:25 +0530

    Replaced numpy with cupy

--- tests/cupyx_tests/scipy_tests/linalg_tests/test_matfuncs.py
+++ tests/cupyx_tests/scipy_tests/linalg_tests/test_matfuncs.py
@@ -1,5 +1,5 @@
-from matfuncs import sinhm
-import numpy
+from cupyx.scipy.linalg.matfuncs import sinhm
+import cupy
 import unittest
 
 
@@ -7,34 +7,44 @@ class TestMatFuncsNan(unittest.TestCase):
 
     def test_sinhm_values(self):
         # Testing whether it is producing right output or not
-        z = numpy.array([
-                        [1, 2], [3, 1]])
+        z = cupy.zeros((2, 2))
+        z[0][0] = 1
+        z[0][1] = 2
+        z[1][0] = 3
+        z[1][1] = 1
 
-        r = numpy.array([[1.1752011936438014, 3.626860407847019],
-                         [10.017874927409903, 1.1752011936438014]])
+        r = cupy.zeros((2, 2))
 
-        assert numpy.alltrue(sinhm(z) == r)
+        r[0][0] = 1.1752011936438014
+        r[0][1] = 3.626860407847019
+        r[1][0] = 10.017874927409903
+        r[1][1] = 1.1752011936438014
+
+        z = sinhm(z)
+        for i in range(z.shape[0]):
+            for j in range(z.shape[1]):
+                assert r[i][j] == z[i][j]
 
     def test_values_nan(self):
         # Testing for nan values in matrix
-        r = numpy.empty((3, 3,))
-        r.fill(numpy.nan)
+        r = cupy.zeros((3, 3))
+        r.fill(cupy.nan)
         self.assertRaises(ValueError, sinhm, r)
 
     def test_values_inf(self):
 
         # Testing for nan values in matrix
-        z = numpy.zeros([2, 2])
-        z[0][0] = numpy.inf
+        z = cupy.zeros((2, 2))
+        z[0][0] = cupy.inf
         self.assertRaises(ValueError, sinhm, z)
 
     def test_shape(self):
 
         # Testing for matrix shape
-        k = numpy.zeros([3, 2])
+        k = cupy.zeros((3, 2))
         self.assertRaises(ValueError, sinhm, k)
 
     def test_dimension_count(self):
         # Testing whether it is as 2D array or not
-        g = numpy.zeros([3, 3, 3])
+        g = cupy.zeros((3, 3, 3))
         self.assertRaises(ValueError, sinhm, g)
