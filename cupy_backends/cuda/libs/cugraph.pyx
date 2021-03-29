31a64f2837c2415f05432b3f3da9a23901786214
Akira Naruse <anaruse@nvidia.com>
2021-03-29 21:34:07 +0900

    flake8

--- cupy_backends/cuda/libs/cugraph.pyx
+++ cupy_backends/cuda/libs/cugraph.pyx
@@ -9,19 +9,21 @@ cdef extern from '../../cupy_cugraph.h' namespace 'cugraph':
         CUGRAPH_STRONG "cugraph::cugraph_cc_t::CUGRAPH_STRONG"
         NUM_CONNECTIVITY_TYPES "cugraph::cugraph_cc_t::NUM_CONNECTIVITY_TYPES"
 
-    cdef cppclass GraphViewBase[VT,ET,WT]:
-        GraphViewBase(WT*,VT,ET)
+    cdef cppclass GraphViewBase[VT, ET, WT]:
+        GraphViewBase(WT*, VT, ET)
 
-    cdef cppclass GraphCompressedSparseBaseView[VT,ET,WT](GraphViewBase[VT,ET,WT]):
-        GraphCompressedSparseBaseView(const VT *, const ET *, const WT *, size_t, size_t)
+    cdef cppclass GraphCompressedSparseBaseView[VT, ET, WT](
+            GraphViewBase[VT, ET, WT]):
+        GraphCompressedSparseBaseView(const VT *, const ET *, const WT *,
+                                      size_t, size_t)
 
-    cdef cppclass GraphCSRView[VT,ET,WT](GraphCompressedSparseBaseView[VT,ET,WT]):
+    cdef cppclass GraphCSRView[VT, ET, WT](
+            GraphCompressedSparseBaseView[VT, ET, WT]):
         GraphCSRView()
         GraphCSRView(const ET *, const VT *, const WT *, size_t, size_t)
 
-    cdef void connected_components[VT,ET,WT](
-        const GraphCSRView[VT,ET,WT] &graph,
-        cugraph_cc_t connect_type,
+    cdef void connected_components[VT, ET, WT](
+        const GraphCSRView[VT, ET, WT] &graph, cugraph_cc_t connect_type,
         VT *labels) except +
 
     # Built time version
