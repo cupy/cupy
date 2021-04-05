41171aa50233cc1d450f60f08dc00b62fd3ee365
Akifumi Imanishi <imanishi@preferred.jp>
2021-04-05 14:16:42 +0900

    Merge pull request #5015 from leofang/doc_repeat
    Document `cupyx.time.repeat`

--- cupyx/time.py
+++ cupyx/time.py
@@ -7,7 +7,17 @@ import cupy as _cupy
 from cupy import _util
 
 
-class _PerfCaseResult(object):
+class _PerfCaseResult:
+    """ An obscure object encompassing timing results recorded by
+    :func:`~cupyx.time.repeat`. Simple statistics can be obtained by converting
+    an instance of this class to a string.
+
+    .. warning::
+        This API is currently experimental and subject to change in future
+        releases.
+
+    """
+
     def __init__(self, name, ts, devices):
         assert ts.ndim == 2
         assert ts.shape[0] == len(devices) + 1
@@ -18,10 +28,12 @@ class _PerfCaseResult(object):
 
     @property
     def cpu_times(self):
+        """ Returns an array of CPU times of size ``n_repeat``. """
         return self._ts[0]
 
     @property
     def gpu_times(self):
+        """ Returns an array of GPU times of size ``n_repeat``. """
         return self._ts[1:]
 
     @staticmethod
@@ -52,6 +64,53 @@ class _PerfCaseResult(object):
 def repeat(
         func, args=(), kwargs={}, n_repeat=10000, *,
         name=None, n_warmup=10, max_duration=_math.inf, devices=None):
+    """ Timing utility for measuring time spent by both CPU and GPU.
+
+    This function is a very convenient helper for setting up a timing test. The
+    GPU time is properly recorded by synchronizing internal streams. As a
+    result, to time a multi-GPU function all participating devices must be
+    passed as the ``devices`` argument so that this helper knows which devices
+    to record. A simple example is given as follows:
+
+    .. code-block:: py
+
+        import cupy as cp
+        from cupyx.time import repeat
+
+        def f(a, b):
+            return 3 * cp.sin(-a) * b
+
+        a = 0.5 - cp.random.random((100,))
+        b = cp.random.random((100,))
+        print(repeat(f, (a, b), n_repeat=1000))
+
+
+    Args:
+        func (callable): a callable object to be timed.
+        args (tuple): positional argumens to be passed to the callable.
+        kwargs (dict): keyword arguments to be passed to the callable.
+        n_repeat (int): number of times the callable is called. Increasing
+            this value would improve the collected statistics at the cost
+            of longer test time.
+        name (str): the function name to be reported. If not given, the
+            callable's ``__name__`` attribute is used.
+        n_warmup (int): number of times the callable is called. The warm-up
+            runs are not timed.
+        max_duration (float): the maximum time (in seconds) that the entire
+            test can use. If the taken time is longer than this limit, the test
+            is stopped and the statistics collected up to the breakpoint is
+            reported.
+        devices (tuple): a tuple of device IDs (int) that will be timed during
+            the timing test. If not given, the current device is used.
+
+    Returns:
+        :class:`_PerfCaseResult`: an object collecting all test results.
+
+    .. warning::
+        This API is currently experimental and subject to change in future
+        releases.
+
+    """
 
     _util.experimental('cupyx.time.repeat')
     if name is None:
@@ -69,9 +128,11 @@ def repeat(
     if not isinstance(n_repeat, int):
         raise ValueError('`n_repeat` should be an integer.')
     if not isinstance(name, str):
-        raise ValueError('`str` should be a string.')
+        raise ValueError('`name` should be a string.')
     if not isinstance(n_warmup, int):
         raise ValueError('`n_warmup` should be an integer.')
+    if not _numpy.isreal(max_duration):
+        raise ValueError('`max_duration` should be given in seconds')
     if not isinstance(devices, tuple):
         raise ValueError('`devices` should be of tuple type')
 
