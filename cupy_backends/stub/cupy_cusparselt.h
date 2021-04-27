18d4c846297672b5350791c009a70dc1cce3f948
Masayuki Takagi <kamonama@gmail.com>
2021-04-27 07:37:44 +0000

    Remove extra indent

--- cupy_backends/stub/cupy_cusparselt.h
+++ cupy_backends/stub/cupy_cusparselt.h
@@ -7,92 +7,92 @@
 
 extern "C" {
 
-    typedef enum {
-	CUSPARSE_STATUS_SUCCESS=0,
-    }  cusparseStatus_t;
-    typedef enum {} cudaDataType;
-    typedef enum {} cusparseOrder_t;
-    typedef enum {} cusparseOperation_t;
-    typedef enum {} cusparseLtSparsity_t;
-    typedef enum {} cusparseComputeType;
-    typedef enum {} cusparseLtMatmulAlg_t;
-    typedef enum {} cusparseLtMatmulAlgAttribute_t;
-    typedef enum {} cusparseLtPruneAlg_t;
+typedef enum {
+    CUSPARSE_STATUS_SUCCESS=0,
+} cusparseStatus_t;
+typedef enum {} cudaDataType;
+typedef enum {} cusparseOrder_t;
+typedef enum {} cusparseOperation_t;
+typedef enum {} cusparseLtSparsity_t;
+typedef enum {} cusparseComputeType;
+typedef enum {} cusparseLtMatmulAlg_t;
+typedef enum {} cusparseLtMatmulAlgAttribute_t;
+typedef enum {} cusparseLtPruneAlg_t;
 
-    typedef void* cudaStream_t;
-    typedef void* cusparseLtHandle_t;
-    typedef void* cusparseLtMatDescriptor_t;
-    typedef void* cusparseLtMatmulDescriptor_t;
-    typedef void* cusparseLtMatmulAlgSelection_t;
-    typedef void* cusparseLtMatmulPlan_t;
+typedef void* cudaStream_t;
+typedef void* cusparseLtHandle_t;
+typedef void* cusparseLtMatDescriptor_t;
+typedef void* cusparseLtMatmulDescriptor_t;
+typedef void* cusparseLtMatmulAlgSelection_t;
+typedef void* cusparseLtMatmulPlan_t;
 
-    cusparseStatus_t cusparseLtInit(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtInit(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtDestroy(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtDestroy(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtDenseDescriptorInit(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtDenseDescriptorInit(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtStructuredDescriptorInit(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtStructuredDescriptorInit(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulDescriptorInit(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulDescriptorInit(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulAlgSelectionInit(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulAlgSelectionInit(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulAlgSetAttribute(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulAlgSetAttribute(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulAlgGetAttribute(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulAlgGetAttribute(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulGetWorkspace(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulGetWorkspace(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulPlanInit(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulPlanInit(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulPlanDestroy(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulPlanDestroy(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmul(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmul(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtMatmulSearch(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtMatmulSearch(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtSpMMAPrune(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtSpMMAPrune(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtSpMMAPruneCheck(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtSpMMAPruneCheck(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtSpMMACompressedSize(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtSpMMACompressedSize(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
-    cusparseStatus_t cusparseLtSpMMACompress(...) {
-	return CUSPARSE_STATUS_SUCCESS;
-    }
+cusparseStatus_t cusparseLtSpMMACompress(...) {
+    return CUSPARSE_STATUS_SUCCESS;
+}
 
 } // extern "C"
 
