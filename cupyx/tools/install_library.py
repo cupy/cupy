f970c417df1ef7affd8f4b97343ad04527724e83
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-05-20 13:29:48 +0000

    add libraries for CUDA 11.3

--- cupyx/tools/install_library.py
+++ cupyx/tools/install_library.py
@@ -53,6 +53,10 @@ def _make_cudnn_record(
 
 # Latest cuDNN versions: https://developer.nvidia.com/rdp/cudnn-download
 _cudnn_records.append(_make_cudnn_record(
+    '11.3', '8.2.0',
+    'cudnn-11.3-linux-x64-v8.2.0.53.tgz',
+    'cudnn-11.3-windows-x64-v8.2.0.53.zip'))
+_cudnn_records.append(_make_cudnn_record(
     '11.2', '8.2.0',
     'cudnn-11.3-linux-x64-v8.2.0.53.tgz',
     'cudnn-11.3-windows-x64-v8.2.0.53.zip'))
@@ -109,6 +113,10 @@ def _make_cutensor_record(
 
 
 _cutensor_records.append(_make_cutensor_record(
+    '11.3', '1.3.0',
+    'libcutensor-linux-x86_64-1.3.0.3.tar.gz',
+    'libcutensor-windows-x86_64-1.3.0.3.zip'))
+_cutensor_records.append(_make_cutensor_record(
     '11.2', '1.2.2',
     'libcutensor-linux-x86_64-1.2.2.5.tar.gz',
     'libcutensor-windows-x86_64-1.2.2.5.zip'))
@@ -152,6 +160,9 @@ def _make_nccl_record(
     }
 
 
+_nccl_records.append(_make_nccl_record(
+    '11.3', '2.9.8', '2.9',
+    'nccl_2.9.8-1+cuda11.3_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
     '11.2', '2.8.4', '2.8',
     'nccl_2.8.4-1+cuda11.2_x86_64.txz'))
