fe5ff9a61995c7b70b5fcc08cfba513af45f9f54
povinsahu1909 <povinsahu@gmail.com>
2021-05-30 00:38:46 +0530

    add test for reshape method CSR/CSC format

--- tests/cupyx_tests/scipy_tests/sparse_tests/test_csc.py
+++ tests/cupyx_tests/scipy_tests/sparse_tests/test_csc.py
@@ -275,6 +275,19 @@ class TestCscMatrix(unittest.TestCase):
         if scipy_available:
             assert (s.get() != s2.get()).count_nonzero() == 0
 
+    def test_reshape_0(self):
+        assert self.m.reshape((12, 1)).shape == (12, 1)
+
+    def test_reshape_1(self):
+        m = self.m.reshape((1, 12)).toarray()
+        expect = [[0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0]]
+        cupy.testing.assert_allclose(m, expect)
+
+    def test_reshape_2(self):
+        m = self.m.reshape((1, 12), order='F').toarray()
+        expect = [[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0]]
+        cupy.testing.assert_allclose(m, expect)
+
 
 @testing.parameterize(*testing.product({
     'dtype': [numpy.float32, numpy.float64, numpy.complex64, numpy.complex128],
