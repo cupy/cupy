085db094086b74435de8323150898a9261c88211
Toshiki Kataoka <kataoka@preferred.jp>
2021-07-06 16:32:31 +0900

    Do not abbrev `np` in test

--- tests/cupy_tests/core_tests/test_ndarray_ufunc.py
+++ tests/cupy_tests/core_tests/test_ndarray_ufunc.py
@@ -1,6 +1,6 @@
 import unittest
 
-import numpy as np
+import numpy
 import pytest
 
 import cupy
@@ -12,71 +12,71 @@ class TestArrayUfunc(unittest.TestCase):
 
     @testing.for_all_dtypes()
     def test_unary_op(self, dtype):
-        a = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        outa = np.sin(a)
+        a = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        outa = numpy.sin(a)
         # numpy operation produced a cupy array
         assert isinstance(outa, cupy.ndarray)
         b = a.get()
-        outb = np.sin(b)
-        assert np.allclose(outa.get(), outb)
+        outb = numpy.sin(b)
+        assert numpy.allclose(outa.get(), outb)
 
     @testing.for_all_dtypes()
     def test_unary_op_out(self, dtype):
-        a = cupy.array(np.array([0, 1, 2]), dtype=dtype)
+        a = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
         b = a.get()
-        outb = np.sin(b)
+        outb = numpy.sin(b)
         # pre-make output with same type as input
-        outa = cupy.array(np.array([0, 1, 2]), dtype=outb.dtype)
-        np.sin(a, out=outa)
-        assert np.allclose(outa.get(), outb)
+        outa = cupy.array(numpy.array([0, 1, 2]), dtype=outb.dtype)
+        numpy.sin(a, out=outa)
+        assert numpy.allclose(outa.get(), outb)
 
     @testing.for_all_dtypes()
     def test_binary_op(self, dtype):
-        a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        outa = np.add(a1, a2)
+        a1 = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        a2 = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        outa = numpy.add(a1, a2)
         # numpy operation produced a cupy array
         assert isinstance(outa, cupy.ndarray)
         b1 = a1.get()
         b2 = a2.get()
-        outb = np.add(b1, b2)
-        assert np.allclose(outa.get(), outb)
+        outb = numpy.add(b1, b2)
+        assert numpy.allclose(outa.get(), outb)
 
     @testing.for_all_dtypes()
     def test_binary_op_out(self, dtype):
-        a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        outa = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        np.add(a1, a2, out=outa)
+        a1 = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        a2 = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        outa = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        numpy.add(a1, a2, out=outa)
         b1 = a1.get()
         b2 = a2.get()
-        outb = np.add(b1, b2)
-        assert np.allclose(outa.get(), outb)
+        outb = numpy.add(b1, b2)
+        assert numpy.allclose(outa.get(), outb)
 
     @testing.for_all_dtypes()
     def test_binary_mixed_op(self, dtype):
-        a1 = cupy.array(np.array([0, 1, 2]), dtype=dtype)
-        a2 = cupy.array(np.array([0, 1, 2]), dtype=dtype).get()
+        a1 = cupy.array(numpy.array([0, 1, 2]), dtype=dtype)
+        a2 = cupy.array(numpy.array([0, 1, 2]), dtype=dtype).get()
         with self.assertRaises(TypeError):
             # attempt to add cupy and numpy arrays
-            np.add(a1, a2)
+            numpy.add(a1, a2)
         with self.assertRaises(TypeError):
             # check reverse order
-            np.add(a2, a1)
+            numpy.add(a2, a1)
         with self.assertRaises(TypeError):
             # reject numpy output from cupy
-            np.add(a1, a1, out=a2)
+            numpy.add(a1, a1, out=a2)
         with self.assertRaises(TypeError):
             # reject cupy output from numpy
-            np.add(a2, a2, out=a1)
+            numpy.add(a2, a2, out=a1)
         with self.assertRaises(ValueError):
             # bad form for out=
             # this is also an error with numpy array
-            np.sin(a1, out=())
+            numpy.sin(a1, out=())
         with self.assertRaises(ValueError):
             # bad form for out=
             # this is also an error with numpy array
-            np.sin(a1, out=(a1, a1))
+            numpy.sin(a1, out=(a1, a1))
 
     @testing.numpy_cupy_array_equal()
     def test_indexing(self, xp):
@@ -100,7 +100,7 @@ class TestUfunc:
     @testing.numpy_cupy_equal()
     def test_types(self, xp, ufunc):
         types = getattr(xp, ufunc).types
-        if xp == np:
+        if xp == numpy:
             assert isinstance(types, list)
             types = list(dict.fromkeys(  # remove dups: numpy/numpy#7897
                 sig for sig in types
