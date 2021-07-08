3544fedff0460392e8b1206cc97608909f76f285
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-07-08 08:23:06 +0000

    improve message

--- .pfnci/linux/main-flexci.sh
+++ .pfnci/linux/main-flexci.sh
@@ -2,11 +2,12 @@
 
 # Bootstrap script for FlexCI.
 
-set -uex
+set -ue
 
 TARGET="${1}"
 export CACHE_DIR=/tmp/cupy_cache
 
+echo "Environment Variables:"
 env
 
 stages=(cache_get test)
@@ -15,12 +16,15 @@ if [[ "${FLEXCI_BRANCH:-}" != refs/pull/* ]]; then
     stages+=(cache_put)
 fi
 
+echo "Starting: "${TARGET}" "${stages[@]}""
 "$(dirname ${0})/run.sh" "${TARGET}" "${stages[@]}" > /tmp/log.txt 2>&1 && echo Test succeeded!
 test_retval=$?
+echo "Exit with status ${test_retval}"
 
+echo "Uploading the log..."
 gsutil -m -q cp /tmp/log.txt "gs://chainer-artifacts-pfn-public-ci/cupy-ci/${CI_JOB_ID}/"
-echo "Full log is available at: https://storage.googleapis.com/chainer-artifacts-pfn-public-ci/cupy-ci/${CI_JOB_ID}/log.txt"
 echo "Last 100 lines of the log:"
 tail -n 100 /tmp/log.txt
+echo "Full log is available at: https://storage.googleapis.com/chainer-artifacts-pfn-public-ci/cupy-ci/${CI_JOB_ID}/log.txt"
 
 exit ${test_retval}
