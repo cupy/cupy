9e7307fee946d83af3aeddeda62b93ae9462611b
Toshiki Kataoka <kataoka@preferred.jp>
2021-07-13 17:58:51 +0900

    Merge pull request #5478 from emcastillo/fix-rng-out
    Fix random generator output not being raveled

--- cupy/random/_generator_api.pyx
+++ cupy/random/_generator_api.pyx
@@ -521,9 +521,11 @@ cdef void _launch_dist(bit_generator, func, out, args) except*:
     cdef ssize_t size = out.size
     cdef ndarray chunk
     cdef int generator = bit_generator.generator
-
+    # out is always contiguous, when out parameter is specified the checks
+    # ensure it
+    out = out.ravel(order='A')
     cdef bsize = bit_generator._state_size()
-    if out.shape == () or bsize == 0:
+    if bsize == 0:
         func(generator, state, y_ptr, out.size, strm, *args)
     else:
         chunks = (out.size + bsize - 1) // bsize
