296f42d3d33593fdd1d54120fbcbd3e9e1494c77
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-08-13 05:23:55 +0000

    fix to work without git

--- .pfnci/linux/tests/cuda11x-cuda-python.sh
+++ .pfnci/linux/tests/cuda11x-cuda-python.sh
@@ -8,6 +8,6 @@ export CUPY_USE_CUDA_PYTHON="1"
 . "$(dirname $0)/_environment.sh"
 
 python3 -m pip install --user Cython
-python3 -m pip install --user git+https://github.com/NVIDIA/cuda-python.git
+python3 -m pip install --user https://github.com/NVIDIA/cuda-python/archive/refs/heads/master.tar.gz
 
 bash "$(dirname $0)/_build_and_test.sh" "not slow"
