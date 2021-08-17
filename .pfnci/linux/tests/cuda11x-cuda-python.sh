b935111a1f6395e4e07238135c4a22b36464bb94
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-08-17 01:43:56 +0000

    fix branch name of cuda-python

--- .pfnci/linux/tests/cuda11x-cuda-python.sh
+++ .pfnci/linux/tests/cuda11x-cuda-python.sh
@@ -8,6 +8,6 @@ export CUPY_USE_CUDA_PYTHON="1"
 . "$(dirname $0)/_environment.sh"
 
 python3 -m pip install --user Cython
-python3 -m pip install --user https://github.com/NVIDIA/cuda-python/archive/refs/heads/master.tar.gz
+python3 -m pip install --user https://github.com/NVIDIA/cuda-python/archive/refs/heads/main.tar.gz
 
 bash "$(dirname $0)/_build_and_test.sh" "not slow"
