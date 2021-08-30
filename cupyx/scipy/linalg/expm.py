1152aa31a63c976768b8f5c7c66e46ee08accef9
MrRobot2211 <felipebihaiek@gmail.com>
2021-08-30 16:27:01 -0300

    moves expm to its correct place with scipy linalg functions

--- cupyx/scipy/linalg/expm.py
+++ cupyx/scipy/linalg/expm.py
@@ -1,21 +1,21 @@
-
 import cupy
 from cupy_backends.cuda.libs import cublas
 from cupy_backends.cuda.libs import cusolver
 from cupy.cuda import device
 from cupy.linalg import _util
 
+
 def expm(a):
     """
-    This method calculates the exponential of a square matrix, `` \exp{a}``.
+    This method calculates the exponential of a square matrix, `` /exp{a}``.
 
     Args:
         a (cupy.ndarray): A symmetric 2-D square matrix ``(M, M)`` or a batch
             of symmetric 2-D square matrices ``(..., M, M)``.
-        
+
     Returns:
         scalar :class:`~cupy.ndarray`:
-        
+
     .. seealso:: :func:`numpy.linalg.expm`
     """
     pass
