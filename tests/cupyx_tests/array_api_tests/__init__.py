3e9c65dee7f72bb72d05b5a686bac9a786ff3f29
Leo Fang <leof@nvidia.com>
2021-09-05 23:47:20 -0700

    require PY38+ to run the local array api tests

--- tests/cupyx_tests/array_api_tests/__init__.py
+++ tests/cupyx_tests/array_api_tests/__init__.py
@@ -5,3 +5,12 @@ Note, full compliance with the array API can be tested with the official array A
 suite https://github.com/data-apis/array-api-tests. This test suite primarily
 focuses on those things that are not tested by the official test suite.
 """
+
+import sys
+
+import pytest
+
+
+if sys.version_info < (3, 8):
+    pytest.skip('Python array API requires Python 3.8+',
+                allow_module_level=True)
