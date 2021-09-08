a38ab7d936b531a5c1e19563f0652e0f7ac97274
Masayuki Takagi <kamonama@gmail.com>
2021-09-08 03:15:09 +0000

    Define async malloc stubs for older CUDA

--- cupy_backends/cuda/cupy_cuda_runtime.h
+++ cupy_backends/cuda/cupy_cuda_runtime.h
@@ -12,6 +12,46 @@ bool hip_environment = false;
 const int cudaErrorContextIsDestroyed = 709;
 #endif
 
+#if CUDA_VERSION < 11020
+
+typedef void* cudaMemPool_t;
+enum cudaMemPoolAttr {};
+
+cudaError_t cudaDeviceGetDefaultMemPool(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaDeviceGetMemPool(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaDeviceSetMemPool(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaFreeAsync(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaMallocAsync(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaMemPoolTrimTo(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaMemPoolGetAttribute(...) {
+    return cudaSuccess;
+}
+
+cudaError_t cudaMemPoolSetAttribute(...) {
+    return cudaSuccess;
+}
+
+#endif
+
+
 } // extern "C"
 
 #endif // #ifndef INCLUDE_GUARD_CUDA_CUPY_CUDA_RUNTIME_H
