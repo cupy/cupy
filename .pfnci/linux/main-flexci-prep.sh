895deef7c2d41a9b5b951f862b44bd2798c7f1b5
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-11 14:45:06 +0000

    only build CUDA images on FlexCI and make it quiet

--- .pfnci/linux/main-flexci-prep.sh
+++ .pfnci/linux/main-flexci-prep.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Build and push Docker images for all test targets.
+# Build and push Docker images for CUDA test targets.
 
 set -ue
 
 env
 
 WAIT_PIDS=""
-for DF in "$(dirname ${0})"/tests/*.Dockerfile; do
+for DF in "$(dirname ${0})"/tests/cuda*.Dockerfile; do
     TARGET="$(echo "$(basename "${DF}")" | cut -d . -f 1)"
-    "$(dirname ${0})/run.sh" "${TARGET}" build push &
+    "$(dirname ${0})/run.sh" "${TARGET}" build > /dev/null 2>&1 &
     docker_pid=$!
     echo "flexci-prep: Building docker image for target $TARGET (PID: ${docker_pid})"
     WAIT_PIDS="${WAIT_PIDS} ${docker_pid}"
