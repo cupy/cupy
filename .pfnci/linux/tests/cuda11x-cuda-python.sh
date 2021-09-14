bdef1370f2bcd1a28aea2d8aac9958f9e6e96288
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-14 08:36:09 +0000

    fix script

--- .pfnci/linux/tests/cuda11x-cuda-python.sh
+++ .pfnci/linux/tests/cuda11x-cuda-python.sh
@@ -9,6 +9,8 @@ export NVCC="ccache nvcc"
 
 export CUPY_USE_CUDA_PYTHON="1"
 
+python3 -m pip install --user https://github.com/NVIDIA/cuda-python/archive/refs/heads/main.tar.gz
+
 "$ACTIONS/build.sh"
 "$ACTIONS/unittest.sh" "not slow"
 "$ACTIONS/cleanup.sh"
