71ca3c4ccc36f65de2dfe7b4757780a84916bfe2
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-14 14:43:06 +0000

    fix script

--- .pfnci/linux/main-flexci-rocm-build.sh
+++ .pfnci/linux/main-flexci-rocm-build.sh
@@ -9,12 +9,12 @@ main() {
       # ROCM_REPO can be set via config.pbtxt and will be used as a
       # part of ROCm repository URL (http://repo.radeon.com/rocm/apt/).
       ROCM_REPO=${ROCM_REPO:-debian}
-      cd "$(dirname "${BASH_SOURCE}")"/..
+      cd "$(dirname "${BASH_SOURCE}")"/../..
       apt update -qqy
       apt install python3.7-dev python3-apt python3-pip python3-setuptools -qqy
       python3.7 -m pip install cython numpy
 
-      wget -qO - http://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -
+      wget -qO - https://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -
       echo "deb [arch=amd64] http://repo.radeon.com/rocm/apt/${ROCM_REPO}/ xenial main" | tee /etc/apt/sources.list.d/rocm.list
 
       # Uninstall CUDA to ensure it's a clean ROCm environment
