ceb7bedb8503970c6c79b86bcf58c1deadd0f5f1
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-14 07:48:24 +0000

    fix manually defined test script

--- .pfnci/linux/tests/cuda11x-cuda-python.sh
+++ .pfnci/linux/tests/cuda11x-cuda-python.sh
@@ -2,12 +2,13 @@
 
 set -uex
 
+ACTIONS="$(dirname $0)/actions"
+. "$ACTIONS/_environment.sh"
+
 export NVCC="ccache nvcc"
+
 export CUPY_USE_CUDA_PYTHON="1"
 
-. "$(dirname $0)/_environment.sh"
-
-python3 -m pip install --user Cython
-python3 -m pip install --user https://github.com/NVIDIA/cuda-python/archive/refs/heads/main.tar.gz
-
-bash "$(dirname $0)/_build_and_test.sh" "not slow"
+"$ACTIONS/build.sh"
+"$ACTIONS/unittest.sh" "not slow"
+"$ACTIONS/cleanup.sh"
