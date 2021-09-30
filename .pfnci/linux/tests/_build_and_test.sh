dd585ad97f07a3498be0b3fd8103805d6bef7048
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-30 15:30:34 +0000

    show_config with full, set timeout to 5 hours

--- .pfnci/linux/tests/_build_and_test.sh
+++ .pfnci/linux/tests/_build_and_test.sh
@@ -20,8 +20,27 @@ python3 -m pip install --user pytest-timeout pytest-xdist
 python3 -m pip install --user -v ".[all,test]"
 
 pushd tests
-python3 -c 'import cupy; cupy.show_config()'
-python3 -m pytest "${pytest_opts[@]}" .
+python3 -c 'import cupy; cupy.show_config(_full=True)'
+test_retval=0
+timeout --signal INT --kill-after 60 18000 python3 -m pytest "${pytest_opts[@]}" . || test_retval=$?
 popd
+
+case ${test_retval} in
+    0 )
+        echo "Result: SUCCESS"
+        ;;
+    124 )
+        echo "Result: TIMEOUT (INT)"
+        exit $test_retval
+        ;;
+    137 )
+        echo "Result: TIMEOUT (KILL)"
+        exit $test_retval
+        ;;
+    * )
+        echo "Result: FAIL ($test_retval)"
+        exit $test_retval
+        ;;
+esac
 
 python3 .pfnci/trim_cupy_kernel_cache.py --max-size $((5*1024*1024*1024)) --rm
