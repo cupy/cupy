159c0686fc45e8e647e256ad9f1a279274f09de1
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-10-12 02:09:04 +0000

    copy source directory to support pip 21.3

--- .pfnci/linux/tests/actions/build.sh
+++ .pfnci/linux/tests/actions/build.sh
@@ -2,4 +2,13 @@
 
 set -uex
 
+src_dir=/src
+if ! touch "${src_dir}"; then
+    echo "Source directory (${src_dir}) is read-only; copying the source tree to /src-build"
+    cp -a /src /src-build
+    src_dir=/src-build
+fi
+
+pushd "${src_dir}"
 python3 -m pip install --user -v ".[test]"
+popd
