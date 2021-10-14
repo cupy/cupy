ca5d7fc831142f1281a3804cfbc966a2d492c4d9
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-10-14 02:13:07 +0000

    use temporary directory for doctests and examples

--- .pfnci/linux/tests/actions/build.sh
+++ .pfnci/linux/tests/actions/build.sh
@@ -2,13 +2,4 @@
 
 set -uex
 
-src_dir=.
-if ! touch .; then
-    src_dir=$(mktemp -d)
-    echo "Source directory ($(pwd)) is read-only; copying the source tree to ${src_dir}"
-    cp -a . "${src_dir}"
-fi
-
-pushd "${src_dir}"
 time python3 -m pip install --user -v ".[test]"
-popd
