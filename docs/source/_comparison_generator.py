03c3929333f898592a5c36844352371ae4ebc614
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-10-17 15:50:06 +0900

    specify more functions in exclude list

--- docs/source/_comparison_generator.py
+++ docs/source/_comparison_generator.py
@@ -4,7 +4,7 @@ import importlib
 def _get_functions(obj, exclude=[]):
     return set([
         n for n in dir(obj)
-        if (n not in exclude
+        if ((exclude is None or n not in exclude)
             and callable(getattr(obj, n))  # callable
             and not isinstance(getattr(obj, n), type)  # not class
             and n[0].islower()  # starts with lower char
@@ -67,7 +67,7 @@ def _generate_comparison_rst(
 
 def _section(
         header, base_mod, cupy_mod,
-        base_type='NumPy', klass=None, exclude_mod=None, exclude=['test']):
+        base_type='NumPy', klass=None, exclude_mod=None, exclude=None):
     return [
         header,
         '~' * len(header),
@@ -89,19 +89,25 @@ def generate():
     ]
     buf += _section(
         'Module-Level',
-        'numpy', 'cupy')
+        'numpy', 'cupy', exclude=[
+            'add_docstring',
+            'add_newdoc',
+            'add_newdoc_ufunc',
+            'fastCopyAndTranspose',
+            'test',
+        ])
     buf += _section(
         'Multi-Dimensional Array',
         'numpy', 'cupy', klass='ndarray')
     buf += _section(
         'Linear Algebra',
-        'numpy.linalg', 'cupy.linalg')
+        'numpy.linalg', 'cupy.linalg', exclude=['test'])
     buf += _section(
         'Discrete Fourier Transform',
-        'numpy.fft', 'cupy.fft')
+        'numpy.fft', 'cupy.fft', exclude=['test'])
     buf += _section(
         'Random Sampling',
-        'numpy.random', 'cupy.random')
+        'numpy.random', 'cupy.random', exclude=['test'])
 
     buf += [
         'SciPy / CuPy APIs',
@@ -110,35 +116,39 @@ def generate():
     ]
     buf += _section(
         'Discrete Fourier Transform',
-        'scipy.fft', 'cupyx.scipy.fft', 'SciPy')
+        'scipy.fft', 'cupyx.scipy.fft', 'SciPy', exclude=['test'])
     buf += _section(
         'Legacy Discrete Fourier Transform',
-        'scipy.fftpack', 'cupyx.scipy.fftpack', 'SciPy')
+        'scipy.fftpack', 'cupyx.scipy.fftpack', 'SciPy', exclude=['test'])
     buf += _section(
         'Advanced Linear Algebra',
         'scipy.linalg', 'cupyx.scipy.linalg', 'SciPy',
-        exclude_mod='numpy.linalg')
+        exclude_mod='numpy.linalg', exclude=['test'])
     buf += _section(
         'Multidimensional Image Processing',
-        'scipy.ndimage', 'cupyx.scipy.ndimage', 'SciPy')
+        'scipy.ndimage', 'cupyx.scipy.ndimage', 'SciPy', exclude=['test'])
     buf += _section(
         'Signal processing',
-        'scipy.signal', 'cupyx.scipy.signal', 'SciPy')
+        'scipy.signal', 'cupyx.scipy.signal', 'SciPy', exclude=['test'])
     buf += _section(
         'Sparse Matrices',
-        'scipy.sparse', 'cupyx.scipy.sparse', 'SciPy')
+        'scipy.sparse', 'cupyx.scipy.sparse', 'SciPy', exclude=['test'])
     buf += _section(
         'Sparse Linear Algebra',
-        'scipy.sparse.linalg', 'cupyx.scipy.sparse.linalg', 'SciPy')
+        'scipy.sparse.linalg', 'cupyx.scipy.sparse.linalg', 'SciPy',
+        exclude=['test'])
     buf += _section(
         'Compressed sparse graph routines',
-        'scipy.sparse.csgraph', 'cupyx.scipy.sparse.csgraph', 'SciPy')
+        'scipy.sparse.csgraph', 'cupyx.scipy.sparse.csgraph', 'SciPy',
+        exclude=['test'])
     buf += _section(
         'Special Functions',
-        'scipy.special', 'cupyx.scipy.special', 'SciPy')
+        'scipy.special', 'cupyx.scipy.special', 'SciPy',
+        exclude=['test'])
     buf += _section(
         'Statistical Functions',
-        'scipy.stats', 'cupyx.scipy.stats', 'SciPy')
+        'scipy.stats', 'cupyx.scipy.stats', 'SciPy',
+        exclude=['test'])
 
 
     # numpy.array_api is not ready yet...
