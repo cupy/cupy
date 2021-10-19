9e0e2e406455a8a91a1d09d085d0086897291002
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-10-19 01:40:14 +0000

    remove pip freeze display in test code

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,24 +12,7 @@ import pytest
 pytest_plugins = ['pytester']
 
 
-def _is_pip_installed():
-    try:
-        import pip  # NOQA
-        return True
-    except ImportError:
-        return False
-
-
-def _is_in_ci():
-    ci_name = os.environ.get('CUPY_CI', '')
-    return ci_name != ''
-
-
 def pytest_configure(config):
-    # Print installed packages
-    if _is_in_ci() and _is_pip_installed():
-        print("***** Installed packages *****", flush=True)
-        subprocess.check_call([sys.executable, '-m', 'pip', 'freeze', '--all'])
     if config.pluginmanager.hasplugin("xdist"):
         config.pluginmanager.register(DeferPlugin())
 
