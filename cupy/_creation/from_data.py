949a5bf2ff9d37f78f8e5c6c2f541fb27cdb125d
SaharCarmel <saharsela271@gmail.com>
2021-10-19 16:33:09 +0300

    added fromiter function

--- cupy/_creation/from_data.py
+++ cupy/_creation/from_data.py
@@ -160,7 +160,15 @@ def fromfunction(*args, **kwargs):
     return asarray(numpy.fromfunction(*args, **kwargs))
 
 
-# TODO(okuta): Implement fromiter
+def fromiter(*args, **kwargs):
+    """Create a new 1-dimensional array from an iterable object.
+
+    .. note::
+        Uses NumPy's ``fromiter`` and coerces the result to a CuPy array.
+
+    .. seealso:: :func:`numpy.fromiter`
+    """
+    return asarray(numpy.fromiter(*args, **kwargs))
 
 
 # TODO(okuta): Implement fromstring
