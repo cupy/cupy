c0da68a699a525c9e46be60cca397ae0f246d3a0
SaharCarmel <saharsela271@gmail.com>
2021-10-19 16:13:15 +0300

    added fromfunction to cupy.__init__

--- cupy/__init__.py
+++ cupy/__init__.py
@@ -240,6 +240,7 @@ from cupy._creation.from_data import asanyarray  # NOQA
 from cupy._creation.from_data import asarray  # NOQA
 from cupy._creation.from_data import ascontiguousarray  # NOQA
 from cupy._creation.from_data import fromfile  # NOQA
+from cupy._creation.from_data import fromfunction  # NOQA
 
 from cupy._creation.ranges import arange  # NOQA
 from cupy._creation.ranges import linspace  # NOQA
