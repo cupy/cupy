ece821031adc3c0f696a1cba33a36eef1e164fab
Masayuki Takagi <kamonama@gmail.com>
2021-10-22 03:15:51 +0000

    Fix `trim_mean` test

--- tests/cupyx_tests/scipy_tests/stats_tests/test_stats.py
+++ tests/cupyx_tests/scipy_tests/stats_tests/test_stats.py
@@ -1,53 +1,70 @@
-import unittest
-
+import numpy
 import pytest
 
-import cupy as cp
+import cupy
 from cupy import testing
-from cupyx.scipy import stats
+import cupyx
+import cupyx.scipy.stats  # NOQA
+
+try:
+    import scipy.stats
+except ImportError:
+    pass
 
 
-@testing.gpu
-class TestTrim(unittest.TestCase):
-    # test trim functions
+@testing.with_requires('scipy')
+class TestTrim:
 
-    def test_trim_mean(self):
-        # don't use pre-sorted arrays
-        a = cp.array([4, 8, 2, 0, 9, 5, 10, 1, 7, 3, 6])
-        idx = cp.array([3, 5, 0, 1, 2, 4])
-        a2 = cp.arange(24).reshape(6, 4)[idx, :]
-        a3 = cp.arange(24).reshape(6, 4, order='F')[idx, :]
-        testing.assert_array_equal(stats.trim_mean(a3, 2 / 6.),
-                                   cp.array([2.5, 8.5, 14.5, 20.5]))
-        testing.assert_array_equal(stats.trim_mean(a2, 2 / 6.),
-                                   cp.array([10., 11., 12., 13.]))
-        idx4 = cp.array([1, 0, 3, 2])
-        a4 = cp.arange(24).reshape(4, 6)[idx4, :]
-        testing.assert_array_equal(stats.trim_mean(a4, 2 / 6.),
-                                   cp.array([9., 10., 11., 12., 13., 14.]))
-        # shuffled arange(24) as array_like
-        a = cp.array([
+    @testing.for_CF_orders()
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_base1(self, xp, scp, dtype, order):
+        idx = xp.array([3, 5, 0, 1, 2, 4])
+        a = testing.shaped_arange(
+            (6, 4), xp=xp, dtype=dtype, order=order)[idx, :]
+        return scp.stats.trim_mean(a, 2 / 6.)
+
+    @testing.for_CF_orders()
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_base2(self, xp, scp, dtype, order):
+        idx = xp.array([1, 0, 3, 2])
+        a = testing.shaped_arange(
+            (4, 6), xp=xp, dtype=dtype, order=order)[idx, :]
+        return scp.stats.trim_mean(a, 2 / 6.)
+
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_array_like(self, xp, scp):
+        a = xp.array([
             7, 11, 12, 21, 16, 6, 22, 1, 5, 0, 18, 10, 17, 9, 19, 15, 23, 20,
             2, 14, 4, 13, 8, 3
         ])
-        testing.assert_array_equal(stats.trim_mean(a, 2 / 6.), 11.5)
-        testing.assert_array_equal(
-            stats.trim_mean(cp.array([5, 4, 3, 1, 2, 0]), 2 / 6.), 2.5)
+        return scp.stats.trim_mean(a, 2 / 6.)
 
-        # check axis argument
-        cp.random.seed(1234)
-        a = cp.random.randint(20, size=(5, 6, 4, 7))
-        for axis in [0, 1, 2, 3, -1]:
-            res1 = stats.trim_mean(a, 2 / 6., axis=axis)
-            res2 = stats.trim_mean(cp.moveaxis(a, axis, 0), 2 / 6.)
-            testing.assert_array_equal(res1, res2)
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_reversed(self, xp, scp):
+        a = xp.array([5, 4, 3, 1, 2, 0])
+        return scp.stats.trim_mean(a, 2 / 6.)
 
-        res1 = stats.trim_mean(a, 2 / 6., axis=None)
-        res2 = stats.trim_mean(a.ravel(), 2 / 6.)
-        testing.assert_array_equal(res1, res2)
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_axis(self, xp, scp, dtype):
+        a = testing.shaped_arange((5, 6, 4, 7), xp=xp, dtype=dtype)
+        return [scp.stats.trim_mean(a, 2 / 6., axis=axis)
+                for axis in [0, 1, 2, 3, -1]]
 
-        pytest.raises(ValueError, stats.trim_mean, a, 0.6)
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_axis_none(self, xp, scp):
+        a = xp.array([4, 8, 2, 0, 9, 5, 10, 1, 7, 3, 6])
+        return scp.stats.trim_mean(a, 2 / 6., axis=None)
 
-        # empty input
-        testing.assert_array_equal(stats.trim_mean(cp.array([]), 0.0), cp.nan)
-        testing.assert_array_equal(stats.trim_mean(cp.array([]), 0.6), cp.nan)
+    def test_invalid_propotiontocut(self):
+        for xp, scp in [(numpy, scipy), (cupy, cupyx.scipy)]:
+            a = xp.array([4, 8, 2, 0, 9, 5, 10, 1, 7, 3, 6])
+            with pytest.raises(ValueError):
+                scp.stats.trim_mean(a, 0.6)
+
+    @pytest.mark.parametrize('propotiontocut', [0.0, 0.6])
+    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    def test_empty_input(self, xp, scp, propotiontocut):
+        return scp.stats.trim_mean(xp.array([]), propotiontocut)
