12692a58b5bbca15cfc9cdc77fa68b37eda45870
Leo Fang <leo80042@gmail.com>
2021-10-23 20:39:28 -0700

    deprecate profile

--- cupyx/profiler/__init__.py
+++ cupyx/profiler/__init__.py
@@ -1,2 +1,28 @@
+import contextlib as _contextlib
+from cupy.cuda import profiler as _profiler
 from cupyx.profiler._time import repeat
 from cupyx.profiler._time_range import time_range
+
+
+@_contextlib.contextmanager
+def profile():
+    """Enable CUDA profiling during with statement.
+
+    This function enables profiling on entering a with statement, and disables
+    profiling on leaving the statement.
+
+    >>> with cupyx.profiler.profile():
+    ...    # do something you want to measure
+    ...    pass
+
+    .. note::
+        When starting ``nvprof`` from the command line, manually setting
+        ``--profile-from-start off`` may be required for the desired behavior.
+        Likewise, when using ``nsys profile`` setting ``-c cudaProfilerApi``
+        may be required.
+    """
+    _profiler.start()
+    try:
+        yield
+    finally:
+        _profiler.stop()
