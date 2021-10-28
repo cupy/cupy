280ce968448de1297c3f58346422a3912b513bfd
SaharCarmel <saharsela271@gmail.com>
2021-10-28 11:24:36 +0300

    fixed assertion and context manager

--- tests/cupy_tests/io_tests/test_text.py
+++ tests/cupy_tests/io_tests/test_text.py
@@ -13,14 +13,10 @@ import numpy
 class TestText(unittest.TestCase):
         
     def test_savetxt(self):
-        array = [[1, 2, 3], [2, 3, 4]]
-        tmp_cupy = tempfile.NamedTemporaryFile('w+t')
-        tmp_cupy.name = "test_cupy.txt"
-        tmp_numpy = tempfile.NamedTemporaryFile('w+t')
-        tmp_numpy.name = "test_numpy.txt"
-        cupy.savetxt(tmp_cupy.name, cupy.array(array))
-        numpy.savetxt(tmp_numpy.name, numpy.array(array))
-        assert filecmp.cmp(tmp_cupy.name, tmp_numpy.name) is True
-        os.remove(str(tmp_cupy.name))
-        os.remove(str(tmp_numpy.name))
+        with (tempfile.NamedTemporaryFile() as tmp_cupy,
+              tempfile.NamedTemporaryFile() as tmp_numpy):
+            array = [[1, 2, 3], [2, 3, 4]]
+            cupy.savetxt(tmp_cupy.name, cupy.array(array))
+            numpy.savetxt(tmp_numpy.name, numpy.array(array))
+            assert filecmp.cmp(tmp_cupy.name, tmp_numpy.name)
     pass
