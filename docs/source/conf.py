12b65640c15db8b2348ca9d8d69058270163aa97
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-11-01 16:39:43 +0000

    add favicon to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -210,12 +210,12 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
