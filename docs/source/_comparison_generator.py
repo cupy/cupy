6bb1a9406fb4507a3d6ce4d0d54df444f01a91ea
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-11-11 15:21:49 +0000

    use `__all__` for comparison table generator

--- docs/source/_comparison_generator.py
+++ docs/source/_comparison_generator.py
@@ -8,8 +8,9 @@ _footnotes = {}
 
 
 def _get_functions(obj, exclude=None):
+    children = obj.__all__ if hasattr(obj, '__all__') else dir(obj)
     return set([
-        n for n, target in [(n, getattr(obj, n)) for n in dir(obj)]
+        n for n, target in [(n, getattr(obj, n)) for n in children]
         if (
             # not in exclude list
             (exclude is None or n not in exclude)
