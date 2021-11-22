5ea1fbc5c50af41a88ce963104f4895cb993ded5
Yutaro Sanada <yutarosanada@g.ecc.u-tokyo.ac.jp>
2021-11-22 20:34:41 +0900

    delete unused modules

--- cupyx/scipy/fft/_fft.py
+++ cupyx/scipy/fft/_fft.py
@@ -9,9 +9,6 @@ from cupy.cuda import cufft
 from cupy.fft._fft import (_fft, _default_fft_func, hfft as _hfft,
                            ihfft as _ihfft, _size_last_transform_axis,
                            _swap_direction)
-from cupy.fft import fftshift, ifftshift, fftfreq, rfftfreq
-
-from cupyx.scipy.fftpack import get_fft_plan
 
 _scipy_150 = False
 try:
