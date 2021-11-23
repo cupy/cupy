e16722ff1bb33162f7a517f3eb16f29bf66a0065
Yutaro Sanada <yutarosanada@g.ecc.u-tokyo.ac.jp>
2021-11-23 14:57:41 +0900

    fix __init__.py

--- cupyx/scipy/fft/__init__.py
+++ cupyx/scipy/fft/__init__.py
@@ -4,3 +4,5 @@ from cupyx.scipy.fft._fft import (
     __ua_domain__, __ua_convert__, __ua_function__)
 from cupyx.scipy.fft._fft import _scipy_150
 from cupyx.scipy.fft._helper import next_fast_len  # NOQA
+from cupy.fft import fftshift, ifftshift, fftfreq, rfftfreq
+from cupyx.scipy.fftpack import get_fft_plan
