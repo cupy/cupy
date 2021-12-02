5400dba35c4a7aaea76d67818165c922bd707a75
Masayuki Takagi <kamonama@gmail.com>
2021-12-02 03:25:56 +0000

    Fix cross-refs in docstring

--- cupy/_indexing/generate.py
+++ cupy/_indexing/generate.py
@@ -433,20 +433,20 @@ def mask_indices(n, mask_func, k=0):
     Assume `mask_func` is a function that, for a square array a of
     size ``(n, n)`` with a possible offset argument `k`, when called
     as ``mask_func(a, k)`` returns a new array with zeros in certain
-    locations (functions like `triu` or `tril` do precisely
-    this). Then this function returns the indices where the non-zero
+    locations (functions like :func:`~cupy.triu` or :func:`~cupy.tril` do
+    precisely this). Then this function returns the indices where the non-zero
     values would be located.
 
     Args:
         n (int): The returned indices will be valid to access arrays
             of shape (n, n).
         mask_func (callable): A function whose call signature is
-            similar to that of `triu`, `tril`.  That is,
+            similar to that of :func:`~cupy.triu`, :func:`~tril`.  That is,
             ``mask_func(x, k)`` returns a boolean array, shaped like
             `x`.  `k` is an optional argument to the function.
         k (scalar): An optional argument which is passed through to
-            `mask_func`. Functions like `triu`, `tril` take a second
-            argument that is interpreted as an offset.
+            `mask_func`. Functions like :func:`~cupy.triu`, :func:`~cupy.tril`
+            take a second argument that is interpreted as an offset.
 
     Returns:
         tuple of arrays: The `n` arrays of indices corresponding to
