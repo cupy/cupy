b09de775850439d4390e0c9d94f9c4a21375bef7
Yutaro Sanada <yutarosanada@g.ecc.u-tokyo.ac.jp>
2021-12-08 17:50:58 +0900

    fix __init__

--- cupyx/scipy/fft/__init__.py
+++ cupyx/scipy/fft/__init__.py
@@ -1,5 +1,12 @@
 # flake8: NOQA
 from cupyx.scipy.fft._fft import (
+    fft, ifft, fft2, ifft2, fftn, ifftn,
+    rfft, irfft, rfft2, irfft2, rfftn, irfftn,
+    hfft, ihfft, hfft2, ihfft2, hfftn, ihfftn,
+    fftshift, ifftshift, fftfreq, rfftfreq,
+    get_fft_plan
+)
+from cupyx.scipy.fft._fft import (
     __ua_domain__, __ua_convert__, __ua_function__)
 from cupyx.scipy.fft._fft import _scipy_150
 from cupyx.scipy.fft._helper import next_fast_len  # NOQA
