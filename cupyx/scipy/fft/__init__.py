85844b5bc1b39829e4b215cafcd85cafae7d918e
Yutaro Sanada <yutarosanada@g.ecc.u-tokyo.ac.jp>
2021-12-08 18:06:34 +0900

    fix scipy/fft

--- cupyx/scipy/fft/__init__.py
+++ cupyx/scipy/fft/__init__.py
@@ -2,9 +2,7 @@
 from cupyx.scipy.fft._fft import (
     fft, ifft, fft2, ifft2, fftn, ifftn,
     rfft, irfft, rfft2, irfft2, rfftn, irfftn,
-    hfft, ihfft, hfft2, ihfft2, hfftn, ihfftn,
-    fftshift, ifftshift, fftfreq, rfftfreq,
-    get_fft_plan
+    hfft, ihfft, hfft2, ihfft2, hfftn, ihfftn
 )
 from cupyx.scipy.fft._fft import (
     __ua_domain__, __ua_convert__, __ua_function__)
