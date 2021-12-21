ba85be74a09ed54f11928628ccb268ae577176ae
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-12-21 15:13:02 +0000

    separate compiler and custom command (code move only)

---
+++ install/cupy_builder/_command.py
@@ -0,0 +1,34 @@
+class custom_build_ext(build_ext.build_ext):
+
+    """Custom `build_ext` command to include CUDA C source files."""
+
+    def run(self):
+        if (build.get_nvcc_path() is not None
+                or build.get_hipcc_path() is not None):
+            def wrap_new_compiler(func):
+                def _wrap_new_compiler(*args, **kwargs):
+                    try:
+                        return func(*args, **kwargs)
+                    except errors.DistutilsPlatformError:
+                        if not PLATFORM_WIN32:
+                            CCompiler = _UnixCCompiler
+                        else:
+                            CCompiler = _MSVCCompiler
+                        return CCompiler(
+                            None, kwargs['dry_run'], kwargs['force'])
+                return _wrap_new_compiler
+            ccompiler.new_compiler = wrap_new_compiler(ccompiler.new_compiler)
+            # Intentionally causes DistutilsPlatformError in
+            # ccompiler.new_compiler() function to hook.
+            self.compiler = 'nvidia'
+        ctx = cupy_builder.get_context()
+        ext_modules = get_ext_modules(True, ctx)  # get .pyx modules
+        cythonize(ext_modules, ctx)
+        check_extensions(self.extensions)
+        build_ext.build_ext.run(self)
+
+    def build_extensions(self):
+        num_jobs = int(os.environ.get('CUPY_NUM_BUILD_JOBS', '4'))
+        if num_jobs > 1:
+            self.parallel = num_jobs
+        super().build_extensions()
