75a717c9b0ad428c2367319284d81e84bc7b92d2
emcastillo <ecastill@preferred.jp>
2021-12-24 09:51:11 +0900

    Fix code style

--- cupyx/scipy/sparse/construct.py
+++ cupyx/scipy/sparse/construct.py
@@ -365,7 +365,7 @@ def random(m, n, density=0.01, format='coo', dtype=None,
         data_rvs = random_state.rand
 
     ind = random_state.choice(mn, size=k, replace=False)
-    j = ind//m
+    j = ind // m
     i = ind - j * m
     vals = data_rvs(k).astype(dtype)
     return coo.coo_matrix(
