eec3d8fde971b8fd47c89adf441bc8568286ca57
Masayuki Takagi <kamonama@gmail.com>
2022-01-17 13:08:10 +0900

    Update cupyx/tools/install_library.py
    Co-authored-by: Kenichi Maehashi <webmaster@kenichimaehashi.com>

--- cupyx/tools/install_library.py
+++ cupyx/tools/install_library.py
@@ -29,8 +29,8 @@ library_records = {}
 def _make_cudnn_url(public_version, cuda_version, filename):
     # https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz
     return (
-        'https://developer.download.nvidia.com/compute/redist/cudnn/' +
-        '/{}/local_installers/{}/{}'.format(
+        'https://developer.download.nvidia.com/compute/redist/cudnn' +
+        '/v{}/local_installers/{}/{}'.format(
             public_version, cuda_version, filename))
 
 
