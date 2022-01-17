96e8601106c15cbe4f72dc76785b082eeacbf751
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2022-01-17 18:44:36 +0900

    Merge pull request #6314 from takagi/cudnn-8.3.2
    Support cuDNN 8.3.2

--- cupyx/tools/install_library.py
+++ cupyx/tools/install_library.py
@@ -26,39 +26,36 @@ _nccl_records = []
 library_records = {}
 
 
-def _make_cudnn_url(public_version, filename):
-    # https://developer.download.nvidia.com/compute/redist/cudnn/v8.0.2/cudnn-11.0-linux-x64-v8.0.2.39.tgz
+def _make_cudnn_url(public_version, cuda_version, filename):
+    # https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz
     return (
         'https://developer.download.nvidia.com/compute/redist/cudnn' +
-        '/v{}/{}'.format(public_version, filename))
+        '/v{}/local_installers/{}/{}'.format(
+            public_version, cuda_version, filename))
 
 
 def _make_cudnn_record(
-        cuda_version, public_version, filename_linux, filename_windows):
+        cuda_version, public_version, archive_cuda_version, filename_linux,
+        filename_windows):
     major_version = public_version.split('.')[0]
-
-    if major_version == '7':
-        suffix_list = ['']
-    elif major_version == '8':
-        # Dependency order is documented at:
-        # https://docs.nvidia.com/deeplearning/cudnn/api/index.html
-        suffix_list = ['', '_ops_infer', '_ops_train',
-                       '_cnn_infer', '_cnn_train',
-                       '_adv_infer', '_adv_train']
-    else:
-        raise AssertionError
-
+    # Dependency order is documented at:
+    # https://docs.nvidia.com/deeplearning/cudnn/api/index.html
+    suffix_list = ['', '_ops_infer', '_ops_train',
+                   '_cnn_infer', '_cnn_train',
+                   '_adv_infer', '_adv_train']
     return {
         'cuda': cuda_version,
         'cudnn': public_version,
         'assets': {
             'Linux': {
-                'url': _make_cudnn_url(public_version, filename_linux),
+                'url': _make_cudnn_url(
+                    public_version, archive_cuda_version, filename_linux),
                 'filenames': [f'libcudnn{suffix}.so.{public_version}'
                               for suffix in suffix_list]
             },
             'Windows': {
-                'url': _make_cudnn_url(public_version, filename_windows),
+                'url': _make_cudnn_url(
+                    public_version, archive_cuda_version, filename_windows),
                 'filenames': [f'cudnn{suffix}64_{major_version}.dll'
                               for suffix in suffix_list]
             },
@@ -68,33 +65,33 @@ def _make_cudnn_record(
 
 # Latest cuDNN versions: https://developer.nvidia.com/rdp/cudnn-download
 _cudnn_records.append(_make_cudnn_record(
-    '11.5', '8.3.0',
-    'cudnn-11.5-linux-x64-v8.3.0.98.tgz',
-    'cudnn-11.5-windows-x64-v8.3.0.98.zip'))
+    '11.5', '8.3.2', '11.5',
+    'cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip'))
 _cudnn_records.append(_make_cudnn_record(
-    '11.4', '8.3.0',
-    'cudnn-11.5-linux-x64-v8.3.0.98.tgz',
-    'cudnn-11.5-windows-x64-v8.3.0.98.zip'))
+    '11.4', '8.3.2', '11.5',
+    'cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip'))
 _cudnn_records.append(_make_cudnn_record(
-    '11.3', '8.3.0',
-    'cudnn-11.5-linux-x64-v8.3.0.98.tgz',
-    'cudnn-11.5-windows-x64-v8.3.0.98.zip'))
+    '11.3', '8.3.2', '11.5',
+    'cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip'))
 _cudnn_records.append(_make_cudnn_record(
-    '11.2', '8.3.0',
-    'cudnn-11.5-linux-x64-v8.3.0.98.tgz',
-    'cudnn-11.5-windows-x64-v8.3.0.98.zip'))
+    '11.2', '8.3.2', '11.5',
+    'cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip'))
 _cudnn_records.append(_make_cudnn_record(
-    '11.1', '8.3.0',
-    'cudnn-11.5-linux-x64-v8.3.0.98.tgz',
-    'cudnn-11.5-windows-x64-v8.3.0.98.zip'))
+    '11.1', '8.3.2', '11.5',
+    'cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip'))
 _cudnn_records.append(_make_cudnn_record(
-    '11.0', '8.3.0',
-    'cudnn-11.5-linux-x64-v8.3.0.98.tgz',
-    'cudnn-11.5-windows-x64-v8.3.0.98.zip'))
+    '11.0', '8.3.2', '11.5',
+    'cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda11.5-archive.zip'))
 _cudnn_records.append(_make_cudnn_record(
-    '10.2', '8.3.0',
-    'cudnn-10.2-linux-x64-v8.3.0.98.tgz',
-    'cudnn-10.2-windows10-x64-v8.3.0.98.zip'))
+    '10.2', '8.3.2', '10.2',
+    'cudnn-linux-x86_64-8.3.2.44_cuda10.2-archive.tar.xz',
+    'cudnn-windows-x86_64-8.3.2.44_cuda10.2-archive.zip'))
 library_records['cudnn'] = _cudnn_records
 
 
@@ -199,6 +196,21 @@ _nccl_records.append(_make_nccl_record(
 library_records['nccl'] = _nccl_records
 
 
+def _unpack_archive(filename, extract_dir):
+    try:
+        shutil.unpack_archive(filename, extract_dir)
+    except shutil.ReadError:
+        print('The archive format is not supported in your Python '
+              'environment. Falling back to "tar" command...')
+        try:
+            os.makedirs(extract_dir, exist_ok=True)
+            subprocess.run(
+                ['tar', 'xf', filename, '-C', extract_dir], check=True)
+        except subprocess.CalledProcessError:
+            msg = 'Failed to extract the archive using "tar" command.'
+            raise RuntimeError(msg)
+
+
 def install_lib(cuda, prefix, library):
     if platform.uname().machine.lower() not in ('x86_64', 'amd64'):
         raise RuntimeError('''
@@ -253,17 +265,7 @@ The current platform ({}) is not supported.'''.format(target_platform))
                 f.write(response.read())
         print('Extracting...')
         outdir = os.path.join(tmpdir, 'extract')
-        try:
-            shutil.unpack_archive(f.name, outdir)
-        except shutil.ReadError:
-            print('The archive format is not supported in your Python '
-                  'environment. Falling back to "tar" command...')
-            try:
-                os.makedirs(outdir, exist_ok=True)
-                subprocess.run(['tar', 'xf', f.name, '-C', outdir], check=True)
-            except subprocess.CalledProcessError:
-                msg = 'Failed to extract the archive using "tar" command.'
-                raise RuntimeError(msg)
+        _unpack_archive(f.name, outdir)
         print('Installing...')
         if library == 'cudnn':
             shutil.move(os.path.join(outdir, 'cuda'), destination)
