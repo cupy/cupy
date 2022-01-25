75531dce612edafb5aaf8d727484444bf920ee78
emcastillo <ecastill@preferred.jp>
2022-01-25 11:03:00 +0900

    Merge pull request #6269 from kmaehashi/discard-cache
    CI: allow discarding docker image cache manually

--- .pfnci/linux/main-flexci-prep.sh
+++ .pfnci/linux/main-flexci-prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Build and push Docker images for CUDA test targets.
+# Build and push Docker images for all test targets.
 
 set -ue
 
@@ -8,6 +8,6 @@ env
 
 gcloud auth configure-docker
 
-for DF in "$(dirname ${0})"/tests/cuda*.Dockerfile; do
+for DF in "$(dirname ${0})"/tests/*.Dockerfile; do
     echo "$(basename "${DF}")" | cut -d . -f 1
-done | xargs -i -n 1 -P 4 bash -c 'echo "flexci-prep: $2 start"; "$1" "$2" build push rmi > /dev/null 2>&1 && echo "flexci-prep: $2 succeeded" || echo "flexci-prep: $2 failed"' -- "$(dirname ${0})/run.sh" "{}"
+done | xargs -i -n 1 -P 4 bash -c 'echo "flexci-prep: $2 start"; DOCKER_IMAGE_CACHE=0 "$1" "$2" build push rmi > /dev/null 2>&1 && echo "flexci-prep: $2 succeeded" || echo "flexci-prep: $2 failed"' -- "$(dirname ${0})/run.sh" "{}"
