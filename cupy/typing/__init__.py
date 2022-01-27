a57f527b6592082b5ba1dd94d7ff7e1f3d0d347e
Akifumi Imanishi <imanishi@preferred.jp>
2022-01-27 10:32:33 +0000

    Fix for numpy<1.20

--- cupy/typing/__init__.py
+++ cupy/typing/__init__.py
@@ -1,8 +1,4 @@
-from numpy.typing import ArrayLike  # NOQA
-from numpy.typing import DTypeLike  # NOQA
-from numpy.typing import NBitBase  # NOQA
-
-try:
-    from cupy.typing._generic_alias import NDArray  # NOQA
-except ImportError:
-    pass
+from cupy.typing._generic_alias import ArrayLike  # NOQA
+from cupy.typing._generic_alias import DTypeLike  # NOQA
+from cupy.typing._generic_alias import NBitBase  # NOQA
+from cupy.typing._generic_alias import NDArray  # NOQA
--- cupy/typing/_generic_alias.py
+++ cupy/typing/_generic_alias.py
@@ -6,13 +6,23 @@ import numpy
 import cupy
 
 
+if numpy.lib.NumpyVersion(numpy.__version__) >= '1.20.0':
+    from numpy.typing import ArrayLike  # NOQA
+    from numpy.typing import DTypeLike  # NOQA
+    from numpy.typing import NBitBase  # NOQA
+else:
+    ArrayLike = Any
+    DTypeLike = Any
+    NBitBase = Any
+
+
 if sys.version_info >= (3, 9):
     from types import GenericAlias
 elif numpy.lib.NumpyVersion(numpy.__version__) >= '1.21.0':
     from numpy.typing import _GenericAlias as GenericAlias
 else:
-    raise ImportError(
-        'cupy.ndarray.__class_getitem__ requires Python>=3.9 or numpy>=1.21.')
+    def GenericAlias(*args):
+        return Any
 
 
 _ScalarType = TypeVar("ScalarType", bound=numpy.generic, covariant=True)
