1e39c29934cec2cbe5ad0c9bf56e6362811ca02b
BasLaa <baslaarakker@gmail.com>
2022-01-30 10:17:50 +0100

    Added dtype, fweights, aweights to cupy.cov

--- cupy/_statistics/correlation.py
+++ cupy/_statistics/correlation.py
@@ -7,7 +7,7 @@ import cupy
 from cupy import _core
 
 
-def corrcoef(a, y=None, rowvar=True, bias=None, ddof=None):
+def corrcoef(a, y=None, rowvar=True, bias=None, ddof=None, *, dtype=None):
     """Returns the Pearson product-moment correlation coefficients of an array.
 
     Args:
@@ -31,7 +31,7 @@ def corrcoef(a, y=None, rowvar=True, bias=None, ddof=None):
         warnings.warn('bias and ddof have no effect and are deprecated',
                       DeprecationWarning)
 
-    out = cov(a, y, rowvar)
+    out = cov(a, y, rowvar, dtype=dtype)
     try:
         d = cupy.diag(out)
     except ValueError:
@@ -78,7 +78,7 @@ def correlate(a, v, mode='valid'):
     return out
 
 
-def cov(a, y=None, rowvar=True, bias=False, ddof=None):
+def cov(a, y=None, rowvar=True, bias=False, ddof=None, fweights=None, aweights=None, *, dtype=None):
     """Returns the covariance matrix of an array.
 
     This function currently does not support ``fweights`` and ``aweights``
@@ -97,6 +97,16 @@ def cov(a, y=None, rowvar=True, bias=False, ddof=None):
             overridden. Note that ``ddof=1`` will return the unbiased estimate
             and ``ddof=0`` will return the simple average.
 
+        fweights (cupy.ndarray, int): 1-D array of integer frequency weights.
+            the number of times each observation vector should be repeated.
+        aweights (cupy.ndarray): 1-D array of observation vector weights.
+            These relative weights are typically large for observations 
+            considered "important" and smaller for observations considered 
+            less "important". If ``ddof=0`` the array of weights can be used 
+            to assign probabilities to observation vectors.
+        dtype: Data type specifier. By default, the return data-type will have
+            at least `numpy.float64` precision.
+
     Returns:
         cupy.ndarray: The covariance matrix of the input array.
 
@@ -109,13 +119,14 @@ def cov(a, y=None, rowvar=True, bias=False, ddof=None):
     if a.ndim > 2:
         raise ValueError('Input must be <= 2-d')
 
-    if y is None:
-        dtype = numpy.promote_types(a.dtype, numpy.float64)
-    else:
-        if y.ndim > 2:
-            raise ValueError('y must be <= 2-d')
-        dtype = functools.reduce(numpy.promote_types,
-                                 (a.dtype, y.dtype, numpy.float64))
+    if dtype is None:
+        if y is None:
+            dtype = numpy.promote_types(a.dtype, numpy.float64)
+        else:
+            if y.ndim > 2:
+                raise ValueError('y must be <= 2-d')
+            dtype = functools.reduce(numpy.promote_types,
+                                    (a.dtype, y.dtype, numpy.float64))
 
     X = cupy.array(a, ndmin=2, dtype=dtype)
     if not rowvar and X.shape[0] != 1:
@@ -131,13 +142,62 @@ def cov(a, y=None, rowvar=True, bias=False, ddof=None):
     if ddof is None:
         ddof = 0 if bias else 1
 
-    fact = X.shape[1] - ddof
+    w = None
+    if fweights is not None:
+        fweights = cupy.asarray(fweights, dtype=float)
+        if not cupy.all(fweights == cupy.around(fweights)):
+            raise TypeError(
+                "fweights must be integer")
+        if fweights.ndim > 1:
+            raise RuntimeError(
+                "cannot handle multidimensional fweights")
+        if fweights.shape[0] != X.shape[1]:
+            raise RuntimeError(
+                "incompatible numbers of samples and fweights")
+        if any(fweights < 0):
+            raise ValueError(
+                "fweights cannot be negative")
+        w = fweights
+    
+    if aweights is not None:
+        aweights = cupy.asarray(aweights, dtype=float)
+        if aweights.ndim > 1:
+            raise RuntimeError(
+                "cannot handle multidimensional aweights")
+        if aweights.shape[0] != X.shape[1]:
+            raise RuntimeError(
+                "incompatible numbers of samples and aweights")
+        if any(aweights < 0):
+            raise ValueError(
+                "aweights cannot be negative")
+        if w is None:
+            w = aweights
+        else:
+            w *= aweights
+
+    avg, w_sum = cupy.average(X, axis=1, weights=w, returned=True)
+    w_sum = w_sum[0]
+
+    # Determine the normalization
+    if w is None:
+        fact = X.shape[1] - ddof
+    elif ddof == 0:
+        fact = w_sum
+    elif aweights is None:
+        fact = w_sum - ddof
+    else:
+        fact = w_sum - ddof*sum(w*aweights)/w_sum
+
     if fact <= 0:
         warnings.warn('Degrees of freedom <= 0 for slice',
                       RuntimeWarning, stacklevel=2)
         fact = 0.0
 
     X -= X.mean(axis=1)[:, None]
-    out = X.dot(X.T.conj()) * (1 / cupy.float64(fact))
+    if w is None:
+        X_T = X.T
+    else:
+        X_T = (X*w).T
+    out = X.dot(X_T.conj()) * (1 / cupy.float64(fact))
 
     return out.squeeze()
