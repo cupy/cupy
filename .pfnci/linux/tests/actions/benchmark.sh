fa162bfb21d76223b52e3262a545edc9a30b1bb8
Emilio Castillo <ecastill@preferred.jp>
2022-02-03 06:20:14 +0000

    wip

--- .pfnci/linux/tests/actions/benchmark.sh
+++ .pfnci/linux/tests/actions/benchmark.sh
@@ -12,22 +12,23 @@ pushd performance
 # Concat all results
 # Upload result to chainer-public
 # gsutil -m -q cp *.csv "gs://chainer-artifacts-pfn-public-ci/cupy-ci/${CI_JOB_ID}/"
+echo "test" > test.csv
 popd
 
-case ${test_retval} in
-    0 )
-        echo "Result: SUCCESS"
-        ;;
-    124 )
-        echo "Result: TIMEOUT (INT)"
-        exit $test_retval
-        ;;
-    137 )
-        echo "Result: TIMEOUT (KILL)"
-        exit $test_retval
-        ;;
-    * )
-        echo "Result: FAIL ($test_retval)"
-        exit $test_retval
-        ;;
-esac
+# case ${test_retval} in
+#     0 )
+#         echo "Result: SUCCESS"
+#         ;;
+#     124 )
+#         echo "Result: TIMEOUT (INT)"
+#         exit $test_retval
+#         ;;
+#     137 )
+#         echo "Result: TIMEOUT (KILL)"
+#         exit $test_retval
+#         ;;
+#     * )
+#         echo "Result: FAIL ($test_retval)"
+#         exit $test_retval
+#         ;;
+# esac
