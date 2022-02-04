b58347e3341a4be8cfa577cf52227f60295bd3d1
Emilio Castillo <ecastill@preferred.jp>
2022-02-04 05:35:50 +0000

    detect dir

--- .pfnci/linux/tests/actions/benchmark.sh
+++ .pfnci/linux/tests/actions/benchmark.sh
@@ -6,7 +6,7 @@ set -uex
 git clone https://github.com/cupy/cupy-performance.git performance
 # TODO(ecastill): make this optional
 pip install seaborn
-
+ls
 pushd performance
 # python prof.py benchmarks/bench_ufunc_cupy.py -c
 # Concat all results
