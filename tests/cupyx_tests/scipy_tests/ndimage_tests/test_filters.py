4f2342be11294c016b7ef2f603b472f14d6de87f
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-02-17 13:46:24 +0530

    Add a test and skip for now

--- tests/cupyx_tests/scipy_tests/ndimage_tests/test_filters.py
+++ tests/cupyx_tests/scipy_tests/ndimage_tests/test_filters.py
@@ -7,9 +7,18 @@ from cupy import testing
 import cupyx.scipy.ndimage  # NOQA
 
 try:
-    import scipy.ndimage  # NOQA
+    import scipy.ndimage as scipy_ndimage
+    import scipy
 except ImportError:
-    pass
+    scipy_ndimage = None
+    scipy = None
+
+
+# TODO: After the feature is released
+# requires_scipy_ndimage_backend = testing.with_requires('scipy>=1.x.x')
+requires_scipy_ndimage_backend = pytest.mark.skip(
+    'scipy.ndimage backend feature has not been released'
+)
 
 
 class FilterTestCaseBase:
@@ -226,6 +235,17 @@ class TestFilter(FilterTestCaseBase):
         if self.dtype == getattr(self, 'output', None):
             pytest.skip("redundant")
         return self._filter(xp, scp)
+
+    @requires_scipy_ndimage_backend
+    @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name='scp')
+    def test_filter_backend(self, xp, scp):
+        self._hip_skip_invalid_condition()
+        if self.dtype == getattr(self, 'output', None):
+            pytest.skip("redundant")
+        backend = 'scipy' if xp is numpy else cupyx.scipy.ndimage
+        with scipy_ndimage.set_backend(backend):
+            out = self._filter(xp, scp)
+        return out
 
 
 @testing.with_requires('scipy')
