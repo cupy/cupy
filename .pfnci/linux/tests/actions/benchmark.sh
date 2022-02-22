43644fecfa6f31145fef63d8f682c62ce7999a89
Emilio Castillo <ecastill@preferred.jp>
2022-02-22 01:25:50 +0000

    Regression

--- .pfnci/linux/tests/actions/benchmark.sh
+++ .pfnci/linux/tests/actions/benchmark.sh
@@ -9,26 +9,11 @@ pip install seaborn
 ls
 pushd performance
 python prof.py benchmarks/bench_ufunc_cupy.py -c
-# Concat all results
-# Upload result to chainer-public
-# gsutil -m -q cp *.csv "gs://chainer-artifacts-pfn-public-ci/cupy-ci/${CI_JOB_ID}/"
+# Compare with current branch
+for bench in *.csv
+do
+    python regression_detect.py ${bench} /perf-results/head/${bench}
+done
+
 cp *.csv /perf-results/
 popd
-
-# case ${test_retval} in
-#     0 )
-#         echo "Result: SUCCESS"
-#         ;;
-#     124 )
-#         echo "Result: TIMEOUT (INT)"
-#         exit $test_retval
-#         ;;
-#     137 )
-#         echo "Result: TIMEOUT (KILL)"
-#         exit $test_retval
-#         ;;
-#     * )
-#         echo "Result: FAIL ($test_retval)"
-#         exit $test_retval
-#         ;;
-# esac
