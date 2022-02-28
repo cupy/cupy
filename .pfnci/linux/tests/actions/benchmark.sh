399d301ceeeae5b3dc061eb730c2bcac49be01c2
Emilio Castillo <ecastill@preferred.jp>
2022-02-28 02:10:10 +0000

    Compare with head

--- .pfnci/linux/tests/actions/benchmark.sh
+++ .pfnci/linux/tests/actions/benchmark.sh
@@ -9,10 +9,23 @@ pip install seaborn
 ls
 pushd performance
 python prof.py benchmarks/bench_ufunc_cupy.py -c
+
+mkdir pr
+mv *.csv pr/
+
+pip uninstall -y cupy
+pip install cupy-cuda115
+
+# Run benchmarks for master
+python prof.py benchmarks/bench_ufunc_cupy.py -c
+mkdir master
+mv *.csv master/
+
 # Compare with current branch
-for bench in *.csv
+for bench in master/*.csv
 do
-    python regresion_detect.py /perf-results/head/${bench} ${bench}
+    # python regresion_detect.py /perf-results/head/${bench} ${bench}
+    python regresion_detect.py master/${bench} pr/${bench}
 done
 
 cp *.csv /perf-results/
