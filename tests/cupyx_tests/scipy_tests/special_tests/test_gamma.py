bb76cbfa63b1012652304b24b00ec6c890b696a7
Gregory Lee <grlee77@gmail.com>
2022-03-02 23:51:39 -0500

    TST: test gamma with complex-valued inputs

--- tests/cupyx_tests/scipy_tests/special_tests/test_gamma.py
+++ tests/cupyx_tests/scipy_tests/special_tests/test_gamma.py
@@ -17,12 +17,14 @@ class TestGamma(unittest.TestCase):
         a = testing.shaped_arange((2, 3), xp, dtype)
         return scp.special.gamma(a)
 
-    @testing.for_all_dtypes(no_complex=True, no_bool=True)
+    @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(atol=1e-5, rtol=1e-5, scipy_name='scp')
     def test_linspace(self, xp, scp, dtype):
         import scipy.special  # NOQA
 
         a = numpy.linspace(-30, 30, 1000, dtype=dtype)
+        if a.dtype.kind == 'c':
+            a -= 1j * a
         a = xp.asarray(a)
         return scp.special.gamma(a)
 
