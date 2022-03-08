b93fcddc22387dcb07f49f6099a233a08822b43b
Masayuki Takagi <kamonama@gmail.com>
2022-03-08 07:34:37 +0000

    Support NCCL 2.12.7

--- cupyx/tools/install_library.py
+++ cupyx/tools/install_library.py
@@ -181,17 +181,17 @@ def _make_nccl_record(
 
 
 _nccl_records.append(_make_nccl_record(
-    '11.6', '2.11.4', '2.11',
-    'nccl_2.11.4-1+cuda11.4_x86_64.txz'))
+    '11.6', '2.12.7', '2.12',
+    'nccl_2.12.7-1+cuda11.6_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
-    '11.5', '2.11.4', '2.11',
-    'nccl_2.11.4-1+cuda11.4_x86_64.txz'))
+    '11.5', '2.12.7', '2.12',
+    'nccl_2.12.7-1+cuda11.6_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
-    '11.4', '2.11.4', '2.11',
-    'nccl_2.11.4-1+cuda11.4_x86_64.txz'))
+    '11.4', '2.12.7', '2.12',
+    'nccl_2.12.7-1+cuda11.6_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
-    '11.3', '2.9.9', '2.9',
-    'nccl_2.9.9-1+cuda11.3_x86_64.txz'))
+    '11.3', '2.12.7', '2.12',
+    'nccl_2.12.7-1+cuda11.6_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
     '11.2', '2.8.4', '2.8',
     'nccl_2.8.4-1+cuda11.2_x86_64.txz'))
@@ -199,11 +199,11 @@ _nccl_records.append(_make_nccl_record(
     '11.1', '2.8.4', '2.8',
     'nccl_2.8.4-1+cuda11.1_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
-    '11.0', '2.11.4', '2.11',
-    'nccl_2.11.4-1+cuda11.0_x86_64.txz'))
+    '11.0', '2.12.7', '2.12',
+    'nccl_2.12.7-1+cuda11.0_x86_64.txz'))
 _nccl_records.append(_make_nccl_record(
-    '10.2', '2.11.4', '2.11',
-    'nccl_2.11.4-1+cuda10.2_x86_64.txz'))
+    '10.2', '2.12.7', '2.12',
+    'nccl_2.12.7-1+cuda10.2_x86_64.txz'))
 library_records['nccl'] = _nccl_records
 
 
