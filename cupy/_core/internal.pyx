c7874a90faa996f7b81b765bd57c699741f6b0d2
Masayuki Takagi <kamonama@gmail.com>
2022-03-18 01:28:36 +0000

    Fix internal.get_size

--- cupy/_core/internal.pyx
+++ cupy/_core/internal.pyx
@@ -8,7 +8,6 @@ from libc.stdint cimport uint32_t
 import sys
 import warnings
 
-import operator
 import numpy
 
 from cupy._core.core cimport ndarray
@@ -45,6 +44,18 @@ cpdef inline bint is_in(const vector.vector[Py_ssize_t]& args, Py_ssize_t x):
 
 
 @cython.profile(False)
+cpdef inline void _check_not_bool(object x) except *:
+    if isinstance(x, (bool, numpy.bool_)):
+        raise TypeError('an integer is required')
+    if (
+        isinstance(x, numpy.ndarray)
+        and x.shape == ()
+        and x.dtype == numpy.bool_
+    ):
+        raise TypeError('an integer is required')
+
+
+@cython.profile(False)
 cpdef inline tuple get_size(object size):
     if size is None:
         warnings.warn(
@@ -54,18 +65,19 @@ cpdef inline tuple get_size(object size):
         )
         return ()
     if cpython.PySequence_Check(size):
+        # A numpy.ndarray unconditionally succeeds in PySequence_Check as
+        # it implements __getitem__, but zero-dim one is an unsized object
+        # and fails to make a tuple from it.
         try:
-            return tuple(size)
+            ret = tuple(size)
         except TypeError:
-            # A numpy.ndarray unconditionally succeeds in PySequence_Check as
-            # it implements __getitem__, but zero-dim one is an unsized object
-            # and fails to make a tuple from it.
-            pass
-    try:
-        return operator.index(size),
-    except TypeError as e:
-        raise ValueError(
-            'size should be None, collections.abc.Sequence, or int') from e
+            _check_not_bool(size)
+            return size,
+        for x in ret:
+            _check_not_bool(x)
+        return ret
+    _check_not_bool(size)
+    return size,
 
 
 @cython.profile(False)
