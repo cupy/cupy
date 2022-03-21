17db871c19c06766cff3ef71b725a6bc06f14ebe
pri1311 <nagdapriya13@gmail.com>
2022-03-21 13:57:26 +0530

    add `cupy.setxor1d` api

--- cupy/_logic/truth.py
+++ cupy/_logic/truth.py
@@ -255,6 +255,44 @@ def setdiff1d(ar1, ar2, assume_unique=False):
     return ar1[in1d(ar1, ar2, assume_unique=True, invert=True)]
 
 
+def setxor1d(ar1, ar2, assume_unique=False):
+    """Find the set exclusive-or of two arrays.
+
+    Parameters
+    ----------
+    ar1, ar2 : cupy.ndarray
+        Input arrays. They are flattend if they are not already 1-D.
+    assume_unique : bool
+        By default, False, i.e. input arrays are not unique.
+        If True, input arrays are assumed to be unique. This can
+        speed up the calculation.
+
+    Returns
+    -------
+    setxor1d : cupy.ndarray
+        Return the sorted, unique values that are in only one
+        (not both) of the input arrays.
+
+    See Also
+    --------
+    numpy.setxor1d
+
+    """
+    if not assume_unique:
+        ar1 = cupy.unique(ar1)
+        ar2 = cupy.unique(ar2)
+
+    aux = cupy.concatenate((ar1, ar2), axis=None)
+    if aux.size == 0:
+        return aux
+
+    aux.sort()
+    flag = aux
+    flag = cupy.concatenate((cupy.asanyarray([True]), cupy.asanyarray(
+        aux[1:] != aux[:-1]), cupy.asanyarray([True])), axis=None)
+    return aux[flag[1:] & flag[:-1]]
+
+
 def union1d(arr1, arr2):
     """Find the union of two arrays.
 
