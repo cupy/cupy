7785533463c38d6a2dbf6c1039350944e79caa58
Emilio Castillo <ecastill@preferred.jp>
2022-03-29 07:40:22 +0000

    Comment

--- .pfnci/linux/tests/actions/benchmark.sh
+++ .pfnci/linux/tests/actions/benchmark.sh
@@ -18,10 +18,14 @@ mv *.csv target/
 pip uninstall -y cupy
 if [[ "${PULL_REQUEST:-}" == "" ]]; then
     # For branches we compare against the latest release
+    # TODO(ecastill) find a programatical way of doing this
+    # sorting tags, or just checking the dates may mix the
+    # stable & master branches
     git checkout tags/v11.0.0a2 -b v11.0.0a2
 else
     git checkout master
 fi
+
 pip install --user -v .
 python prof.py benchmarks/bench_ufunc_cupy.py -c
 mkdir baseline
