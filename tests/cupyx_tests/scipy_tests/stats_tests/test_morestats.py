1c4ba68f0f5901332dfe4ffae8fb46b185e1e6fd
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-07-05 10:38:58 +0530

    address reviews

--- tests/cupyx_tests/scipy_tests/stats_tests/test_morestats.py
+++ tests/cupyx_tests/scipy_tests/stats_tests/test_morestats.py
@@ -11,11 +11,13 @@ import scipy.stats  # NOQA
 atol = {
     'default': 1e-6,
     cupy.float16: 5e-2,
+    cupy.float32: 1e-5,
     cupy.float64: 1e-14
 }
 rtol = {
     'default': 1e-6,
     cupy.float16: 5e-2,
+    cupy.float32: 1e-5,
     cupy.float64: 1e-14
 }
 
@@ -44,7 +46,7 @@ class TestBoxcox_llf:
         rtol=rtol_low
     )
     def test_array_1dim(self, xp, scp, dtype):
-        data = testing.shaped_arange((2,), xp, dtype=dtype)
+        data = testing.shaped_random((2,), xp, dtype=dtype, scale=9)
         lmb = 4.0
         return scp.stats.boxcox_llf(lmb, data)
 
@@ -64,10 +66,7 @@ class TestBoxcox_llf:
 
     @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose(
-        scipy_name='scp',
-        atol=atol_low,
-        rtol=rtol_low
-    )
+        scipy_name='scp', atol=atol, rtol=rtol)
     def test_array_multi_dim(self, xp, scp, dtype):
         data = testing.shaped_random((2, 5, 8, 9), xp, dtype=dtype)
         lmb = 9.0
