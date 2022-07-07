3de7196b32c7ce3b4e9e4ec3487fc69e135dab85
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-07-07 15:15:34 +0530

    use shaped_arange, and use larger inputs

--- tests/cupyx_tests/scipy_tests/stats_tests/test_morestats.py
+++ tests/cupyx_tests/scipy_tests/stats_tests/test_morestats.py
@@ -28,52 +28,38 @@ rtol = {
 class TestBoxcox_llf:
 
     @testing.for_all_dtypes(no_float16=True, no_bool=True)
-    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-6)
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-3, atol=1e-3)
     def test_array_1dim(self, xp, scp, dtype):
-        data = testing.shaped_random((2,), xp, dtype=dtype, scale=9)
+        data = testing.shaped_arange((10,), xp, dtype=dtype)
         lmb = 4.0
         return scp.stats.boxcox_llf(lmb, data)
 
     @testing.numpy_cupy_allclose(scipy_name='scp', rtol=5e-3)
     def test_array_1dim_float16(self, xp, scp):
-        data = xp.array([2, 3], dtype=xp.float16)
+        data = testing.shaped_arange((5,), xp, dtype=xp.float16)
         lmb = 4.0
         return scp.stats.boxcox_llf(lmb, data)
 
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', atol=atol, rtol=rtol)
     def test_array_2dim(self, xp, scp, dtype):
-        data = xp.array([[1], [2]], dtype=dtype)
+        data = testing.shaped_arange((3, 3), xp, dtype=dtype)
         lmb = 6.0
-        return scp.stats.boxcox_llf(lmb, data)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            return scp.stats.boxcox_llf(lmb, data)
 
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', atol=atol, rtol=rtol)
     def test_array_3dim(self, xp, scp, dtype):
-        data = xp.array([
-            [
-                [1, 2, 3], [4, 5, 6]
-            ],
-            [
-                [7, 8, 9], [10, 11, 12]
-            ],
-        ], dtype=dtype)
+        data = testing.shaped_arange((6, 3, 4), xp, dtype=dtype)
         lmb = 1.0
         return scp.stats.boxcox_llf(lmb, data)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_allclose(scipy_name='scp', atol=atol, rtol=rtol)
+    @testing.numpy_cupy_allclose(scipy_name='scp', atol=1e-6, rtol=1e-6)
     def test_array_multi_dim(self, xp, scp, dtype):
-        data = xp.array([
-            [
-                [[1, 2], [3, 4]],
-                [[5, 6], [7, 8]]
-            ],
-            [
-                [[9, 10], [11, 12]],
-                [[13, 14], [15, 16]]
-            ]
-        ], dtype=dtype)
+        data = testing.shaped_arange((3, 2, 4, 3), xp, dtype=dtype)
         lmb = 9.0
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
@@ -82,14 +68,14 @@ class TestBoxcox_llf:
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', atol=atol, rtol=rtol)
     def test_array_zero_lmb(self, xp, scp, dtype):
-        data = xp.array([[1, 2, 3], [6, 8, 9]], dtype=dtype)
+        data = testing.shaped_arange((9, 10), xp, dtype=dtype)
         lmb = 0.0
         return scp.stats.boxcox_llf(lmb, data)
 
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_array_empty(self, xp, scp, dtype):
-        data = xp.array([], dtype=dtype)
+        data = testing.shaped_arange((0,), xp, dtype=dtype)
         lmb = 1
         return scp.stats.boxcox_llf(lmb, data)
 
@@ -103,13 +89,13 @@ class TestBoxcox_llf:
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', atol=atol, rtol=rtol)
     def test_array_lmb_neg2(self, xp, scp, dtype):
-        data = xp.array([[1, 2, 3], [4, 5, 6]], dtype=dtype)
+        data = testing.shaped_arange((3, 5), xp, dtype=dtype)
         lmb = -3.0
         return scp.stats.boxcox_llf(lmb, data)
 
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', atol=atol, rtol=rtol)
     def test_array_empty_neg_lmb(self, xp, scp, dtype):
-        data = xp.array([], dtype=dtype)
+        data = testing.shaped_arange((0,), xp, dtype=dtype)
         lmb = -1.0
         return scp.stats.boxcox_llf(lmb, data)
