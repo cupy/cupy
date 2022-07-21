d60e8316534e3b9ad92492de79ba1beb64464227
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-07-21 11:33:38 -0500

    Address style issue

--- tests/cupy_tests/manipulation_tests/test_add_remove.py
+++ tests/cupy_tests/manipulation_tests/test_add_remove.py
@@ -146,7 +146,6 @@ class TestUnique(unittest.TestCase):
         a = testing.shaped_random((100, 100), xp, dtype)
         return xp.unique(a, return_counts=True)[1]
 
-
     @testing.for_all_dtypes(no_float16=True, no_bool=True, no_complex=True)
     @testing.numpy_cupy_array_equal()
     def test_unique_counts(self, xp, dtype):
