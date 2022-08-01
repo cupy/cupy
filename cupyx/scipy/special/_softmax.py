47af040f1d1f86e4ed729c7ddcde514d4f910879
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-08-01 12:42:00 +0530

    add comment

--- cupyx/scipy/special/_softmax.py
+++ cupyx/scipy/special/_softmax.py
@@ -1,6 +1,41 @@
 import cupy
 
 
+# Note: here, we used three kernel switches for the
+# implementation, we tried using `cupy.fusion`,
+# but the performance degraded. Because CuPy fusion does
+# not generate competitive codes for all cases.
+# The fusion technique we iterated:
+#
+# def make_expander(shape, axis):
+#     axis = internal._normalize_axis_indices(axis, len(shape))
+#     expander = []
+#     for i, s in enumerate(x.shape):
+#         if i in axis:
+#             expander.append(None)
+#         else:
+#             expander.append(slice(None))
+#     return tuple(expander)
+#
+# @_util.memoize(for_each_device=True)
+# def _softmax_fuse(shape, axis):
+#     expander = make_expander(shape, axis)
+#     @_core.fusion.fuse()
+#     def softmax_fuse(x):
+#         x_max = cupy.amax(x, axis=axis)
+#         exp_x_shifted = cupy.exp(x - x_max[expander])
+#         return exp_x_shifted / cupy.sum(exp_x_shifted, axis=axis)[expander]
+#     return softmax_fuse
+#
+# def softmax(x, axis=None):
+#     fused_softmax = _softmax_fuse(shape=x.shape, axis=axis)
+#     return fused_softmax(x)
+#
+# TODO: Add the `shape` method in cupy.fusion so we can
+# fuse the make_expander function, too, which might
+# increment the performance.
+
+
 def softmax(x, axis=None):
     """Softmax function.
 
