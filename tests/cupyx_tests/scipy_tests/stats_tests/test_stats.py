fed3a931186ceaed95c116d793d629f175fabe4e
Masayuki Takagi <kamonama@gmail.com>
2022-08-02 08:28:38 +0000

    Ignore numpy's warning on an invalid value

--- tests/cupyx_tests/scipy_tests/stats_tests/test_stats.py
+++ tests/cupyx_tests/scipy_tests/stats_tests/test_stats.py
@@ -129,7 +129,8 @@ class TestZmap:
     def test_zmap_nan_policy_propagate(self, xp, scp, dtype):
         x = xp.array([4.0, 1.0, 1.0, xp.nan], dtype=dtype)
         y = xp.array([xp.nan, -4.0, -1.0, -5.0], dtype=dtype)
-        return scp.stats.zmap(x, y, nan_policy='propagate')
+        with numpy.errstate(invalid='ignore'):  # numpy warns with complex
+            return scp.stats.zmap(x, y, nan_policy='propagate')
 
     @testing.for_dtypes('fdFD')
     @testing.numpy_cupy_allclose(scipy_name='scp')
@@ -212,7 +213,8 @@ class TestZscore:
     @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_zscore_nan_policy_propagate(self, xp, scp, dtype):
         x = xp.array([4.0, 1.0, 1.0, xp.nan], dtype=dtype)
-        return scp.stats.zscore(x, nan_policy='propagate')
+        with numpy.errstate(invalid='ignore'):  # numpy warns with complex
+            return scp.stats.zscore(x, nan_policy='propagate')
 
     @testing.for_dtypes('fdFD')
     @testing.numpy_cupy_allclose(scipy_name='scp')
