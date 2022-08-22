ae62695c440ccced3baacf413f687097c23b5fb5
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-08-22 15:59:48 +0530

    try to fix doc build

--- cupyx/scipy/special/_logsumexp.py
+++ cupyx/scipy/special/_logsumexp.py
@@ -6,21 +6,22 @@ def logsumexp(a, axis=None, b=None, keepdims=False, return_sign=False):
 
     Parametres
     ----------
-    a : array_like
-        Input array.
+    a : cupy.ndarray
+        Input array
     axis : None or int or tuple of ints, optional
-        Axis or axes over which the sum is taken. By default `axis` is None,
-        and all elements are summed.
+        Axis or axes over which the sum is taken. By default
+        `axis` is None, and all elements are summed
 
     keepdims : bool, optional
-        If this is set to True, the axes which are reduced are left in the
-        result as dimensions with size one. With this option, the result
-        will broadcast correctly against the original array.
+        If this is set to True, the axes which are reduced
+        are left in the result as dimensions with size one. With
+        this option, the result will broadcast correctly
+        against the original array
 
-    b : array-like, optional
+    b : cupy.ndarray, optional
         Scaling factor for exp(`a`) must be of the same shape as `a` or
         broadcastable to `a`. These values may be negative in order to
-        implement subtraction.
+        implement subtraction
 
     return_sign : bool, optional
         If this is set to True, the result will be a pair containing sign
@@ -30,13 +31,13 @@ def logsumexp(a, axis=None, b=None, keepdims=False, return_sign=False):
     Returns
     -------
     res : cupy.ndarray
-        The result, ``cp.log(cp.sum(cp.exp(a)))`` calculated in a numerically
-        more stable way. If `b` is given then ``cp.log(cp.sum(b*cp.exp(a)))``
-        is returned.
+        The result, ``cp.log(cp.sum(cp.exp(a)))`` calculated
+        in a numerically more stable way. If `b` is given then
+        ``cp.log(cp.sum(b*cp.exp(a)))`` is returned
     sgn : cupy.ndarray
         If return_sign is True, this will be an array of floating-point
         numbers matching res and +1, 0, or -1 depending on the sign of
-        the result. If False, only onw result is returned.
+        the result. If False, only onw result is returned
 
     See Also
     --------
