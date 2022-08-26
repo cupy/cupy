0e4221e6a0f9faae5451a9c61a78e5943ae216d5
maxim.eremeev <maxim9236@yandex.ru>
2022-08-26 22:57:07 +0300

    Fixes tests names and adds random floats test

--- tests/cupyx_tests/scipy_tests/special_tests/test_exp1.py
+++ tests/cupyx_tests/scipy_tests/special_tests/test_exp1.py
@@ -9,33 +9,39 @@ from cupy.testing import numpy_cupy_allclose
 class TestExp1:
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_zero(self, xp, scp):
+    def test_exp1_zero(self, xp, scp):
         return scp.special.exp1(0.0)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_negative(self, xp, scp):
+    def test_exp1_negative(self, xp, scp):
         return scp.special.exp1(-42.0)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_large_negative(self, xp, scp):
+    def test_exp1_large_negative(self, xp, scp):
         return scp.special.exp1(-1000)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_positive(self, xp, scp):
+    def test_exp1_positive(self, xp, scp):
         return scp.special.exp1(42.0)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_large_positive(self, xp, scp):
+    def test_exp1_large_positive(self, xp, scp):
         return scp.special.exp1(1000)
 
     @testing.for_float_dtypes()
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_array_inputs(self, xp, scp, dtype):
+    def test_exp1_array_inputs(self, xp, scp, dtype):
         x = testing.shaped_arange((8, 9, 10), xp, dtype)
         return scp.special.exp1(x)
 
     @testing.for_float_dtypes()
     @numpy_cupy_allclose(scipy_name="scp", atol=1e-15)
-    def test_expn_array_inputs(self, xp, scp, dtype):
-        x = xp.linspace(-1000, 1000, 1000, dtype=dtype)
+    def test_exp1_linspace(self, xp, scp, dtype):
+        x = xp.linspace(-1000, 1000, 500, dtype=dtype)
+        return scp.special.exp1(x)
+
+    @testing.for_float_dtypes()
+    @numpy_cupy_allclose(scipy_name="scp", atol=1e-9)
+    def test_exp1_random(self, xp, scp, dtype):
+        x = testing.shaped_random((5, 5, 5), xp, dtype=dtype)
         return scp.special.exp1(x)
--- tests/cupyx_tests/scipy_tests/special_tests/test_expi.py
+++ tests/cupyx_tests/scipy_tests/special_tests/test_expi.py
@@ -9,33 +9,39 @@ from cupy.testing import numpy_cupy_allclose
 class TestExpi:
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_zero(self, xp, scp):
+    def test_expi_zero(self, xp, scp):
         return scp.special.expi(0.0)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_negative(self, xp, scp):
+    def test_expi_negative(self, xp, scp):
         return scp.special.expi(-42.0)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_large_negative(self, xp, scp):
+    def test_expi_large_negative(self, xp, scp):
         return scp.special.expi(-1000)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_positive(self, xp, scp):
+    def test_expi_positive(self, xp, scp):
         return scp.special.expi(42.0)
 
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_large_positive(self, xp, scp):
+    def test_expi_large_positive(self, xp, scp):
         return scp.special.expi(1000)
 
     @testing.for_float_dtypes()
     @numpy_cupy_allclose(scipy_name="scp")
-    def test_expn_array_inputs(self, xp, scp, dtype):
+    def test_expi_array_inputs(self, xp, scp, dtype):
         x = testing.shaped_arange((8, 9, 10), xp, dtype)
         return scp.special.expi(x)
 
     @testing.for_float_dtypes()
     @numpy_cupy_allclose(scipy_name="scp", atol=1e-15)
-    def test_expn_array_inputs(self, xp, scp, dtype):
-        x = xp.linspace(-1000, 1000, 1000, dtype=dtype)
+    def test_expi_linspace(self, xp, scp, dtype):
+        x = xp.linspace(-1000, 1000, 500, dtype=dtype)
+        return scp.special.expi(x)
+
+    @testing.for_float_dtypes()
+    @numpy_cupy_allclose(scipy_name="scp", atol=1e-9)
+    def test_expi_random(self, xp, scp, dtype):
+        x = testing.shaped_random((5, 5, 5), xp, dtype=dtype)
         return scp.special.expi(x)
