95654cc715f432b6bc0d2fc36cbfbf74f266b011
maronuu <oguri.ytr.2000@gmail.com>
2022-08-27 20:02:55 +0000

    Simplify test cases

--- tests/cupy_tests/misc_tests/test_byte_bounds.py
+++ tests/cupy_tests/misc_tests/test_byte_bounds.py
@@ -1,50 +1,34 @@
-import numpy
 import unittest
 
+import cupy
 from cupy import testing
 
 
 @testing.gpu
 class TestByteBounds(unittest.TestCase):
 
-    @testing.numpy_cupy_equal()
-    def test_contiguous_1d_arrays(self, xp):
-        a = xp.arange(12, dtype=xp.float32)
-        if xp == numpy:
-            a_low, a_high = xp.byte_bounds(a)
-        else:
-            a_low = a.data.ptr
-            a_high = a_low + 12*4
-        return xp.byte_bounds(a) == (a_low, a_high)
+    def test_contiguous_1d_arrays(self):
+        a = cupy.arange(12, dtype=cupy.float32)
+        a_low = a.data.ptr
+        a_high = a_low + 12*4
+        return cupy.byte_bounds(a) == (a_low, a_high)
 
-    @testing.numpy_cupy_equal()
-    def test_contiguous_2d_arrays(self, xp):
-        a = xp.zeros((4, 7), dtype=xp.float32)
-        if xp == numpy:
-            a_low, a_high = xp.byte_bounds(a)
-        else:
-            a_low = a.data.ptr
-            a_high = a_low + 4*7*4
-        return xp.byte_bounds(a) == (a_low, a_high)
+    def test_contiguous_2d_arrays(self):
+        a = cupy.zeros((4, 7), dtype=cupy.float32)
+        a_low = a.data.ptr
+        a_high = a_low + 4*7*4
+        return cupy.byte_bounds(a) == (a_low, a_high)
 
-    @testing.numpy_cupy_equal()
-    def test_non_contiguous_1d_arrays(self, xp):
-        a = xp.arange(12, dtype=xp.float32)
+    def test_non_contiguous_1d_arrays(self):
+        a = cupy.arange(12, dtype=cupy.float32)
         a = a[::2]
-        if xp == numpy:
-            a_low, a_high = xp.byte_bounds(a)
-        else:
-            a_low = a.data.ptr
-            a_high = a_low + 11*4  # a[10]
-        return xp.byte_bounds(a) == (a_low, a_high)
+        a_low = a.data.ptr
+        a_high = a_low + 11*4  # a[10]
+        return cupy.byte_bounds(a) == (a_low, a_high)
 
-    @testing.numpy_cupy_equal()
-    def test_non_contiguous_2d_arrays(self, xp):
-        a = xp.zeros((4, 7), dtype=xp.float32)
+    def test_non_contiguous_2d_arrays(self):
+        a = cupy.zeros((4, 7), dtype=cupy.float32)
         a = a[::2, ::2]
-        if xp == numpy:
-            a_low, a_high = xp.byte_bounds(a)
-        else:
-            a_low = a.data.ptr
-            a_high = a_low + 3*7*4  # a[2][6]
-        return xp.byte_bounds(a) == (a_low, a_high)
+        a_low = a.data.ptr
+        a_high = a_low + 3*7*4  # a[2][6]
+        return cupy.byte_bounds(a) == (a_low, a_high)
