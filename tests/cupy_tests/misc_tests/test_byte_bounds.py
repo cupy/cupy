b18562ad747cc29ea5358821a614bfc13bbdea0c
maronuu <oguri.ytr.2000@gmail.com>
2022-08-28 14:26:40 +0000

    Format operations

--- tests/cupy_tests/misc_tests/test_byte_bounds.py
+++ tests/cupy_tests/misc_tests/test_byte_bounds.py
@@ -10,25 +10,25 @@ class TestByteBounds(unittest.TestCase):
     def test_contiguous_1d_arrays(self):
         a = cupy.arange(12, dtype=cupy.float32)
         a_low = a.data.ptr
-        a_high = a_low + 12*4
+        a_high = a_low + 12 * 4
         return cupy.byte_bounds(a) == (a_low, a_high)
 
     def test_contiguous_2d_arrays(self):
         a = cupy.zeros((4, 7), dtype=cupy.float32)
         a_low = a.data.ptr
-        a_high = a_low + 4*7*4
+        a_high = a_low + 4 * 7 * 4
         return cupy.byte_bounds(a) == (a_low, a_high)
 
     def test_non_contiguous_1d_arrays(self):
         a = cupy.arange(12, dtype=cupy.float32)
         a = a[::2]
         a_low = a.data.ptr
-        a_high = a_low + 11*4  # a[10]
+        a_high = a_low + 11 * 4  # a[10]
         return cupy.byte_bounds(a) == (a_low, a_high)
 
     def test_non_contiguous_2d_arrays(self):
         a = cupy.zeros((4, 7), dtype=cupy.float32)
         a = a[::2, ::2]
         a_low = a.data.ptr
-        a_high = a_low + 3*7*4  # a[2][6]
+        a_high = a_low + 3 * 7 * 4  # a[2][6]
         return cupy.byte_bounds(a) == (a_low, a_high)
