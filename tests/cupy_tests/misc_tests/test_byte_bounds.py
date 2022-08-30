3d07223e55928068f30675960804777d9184d0f3
maronuu <oguri.ytr.2000@gmail.com>
2022-08-30 17:25:55 +0000

    Add testcases for negative strides, other dtypes

--- tests/cupy_tests/misc_tests/test_byte_bounds.py
+++ tests/cupy_tests/misc_tests/test_byte_bounds.py
@@ -1,34 +1,83 @@
-import unittest
+import pytest
 
 import cupy
 from cupy import testing
 
 
 @testing.gpu
-class TestByteBounds(unittest.TestCase):
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        # built-in types
+        int, float, bool, complex, bytes,
+        # cupy types
+        cupy.int8, cupy.int16, cupy.int32, cupy.int64,
+        cupy.uint8, cupy.uint16, cupy.uint32, cupy.uint64,
+        cupy.float16, cupy.float32, cupy.float64,
+        cupy.complex64, cupy.complex128,
+    ]
+)
+class TestByteBounds:
 
-    def test_contiguous_1d_arrays(self):
-        a = cupy.arange(12, dtype=cupy.float32)
+    def test_1d_contiguous(self, dtype):
+        a = cupy.zeros(12, dtype=dtype)
+        itemsize = a.itemsize
         a_low = a.data.ptr
-        a_high = a_low + 12 * 4
-        return cupy.byte_bounds(a) == (a_low, a_high)
+        a_high = a.data.ptr + 12 * itemsize
+        assert cupy.byte_bounds(a) == (a_low, a_high)
 
-    def test_contiguous_2d_arrays(self):
-        a = cupy.zeros((4, 7), dtype=cupy.float32)
+    def test_2d_contiguous(self, dtype):
+        a = cupy.zeros((4, 7), dtype=dtype)
+        itemsize = a.itemsize
         a_low = a.data.ptr
-        a_high = a_low + 4 * 7 * 4
-        return cupy.byte_bounds(a) == (a_low, a_high)
+        a_high = a.data.ptr + 4 * 7 * itemsize
+        assert cupy.byte_bounds(a) == (a_low, a_high)
 
-    def test_non_contiguous_1d_arrays(self):
-        a = cupy.arange(12, dtype=cupy.float32)
-        a = a[::2]
-        a_low = a.data.ptr
-        a_high = a_low + 11 * 4  # a[10]
-        return cupy.byte_bounds(a) == (a_low, a_high)
+    def test_1d_noncontiguous_pos_stride(self, dtype):
+        a = cupy.zeros(12, dtype=dtype)
+        itemsize = a.itemsize
+        b = a[::2]
+        b_low = b.data.ptr
+        b_high = b.data.ptr + 11 * itemsize  # a[10]
+        assert cupy.byte_bounds(b) == (b_low, b_high)
 
-    def test_non_contiguous_2d_arrays(self):
-        a = cupy.zeros((4, 7), dtype=cupy.float32)
-        a = a[::2, ::2]
-        a_low = a.data.ptr
-        a_high = a_low + 3 * 7 * 4  # a[2][6]
-        return cupy.byte_bounds(a) == (a_low, a_high)
+    def test_2d_noncontiguous_pos_stride(self, dtype):
+        a = cupy.zeros((4, 7), dtype=dtype)
+        b = a[::2, ::2]
+        itemsize = b.itemsize
+        b_low = a.data.ptr
+        b_high = b.data.ptr + 3 * 7 * itemsize  # a[2][6]
+        assert cupy.byte_bounds(b) == (b_low, b_high)
+
+    def test_1d_contiguous_neg_stride(self, dtype):
+        a = cupy.zeros(12, dtype=dtype)
+        b = a[::-1]
+        itemsize = b.itemsize
+        b_low = b.data.ptr - 11 * itemsize
+        b_high = b.data.ptr + 1 * itemsize
+        assert cupy.byte_bounds(b) == (b_low, b_high)
+
+    def test_2d_noncontiguous_neg_stride(self, dtype):
+        a = cupy.zeros((4, 7), dtype=dtype)
+        b = a[::-2, ::-2]  # strides = (-56, -8), shape = (2, 4)
+        itemsize = b.itemsize
+        b_low = b.data.ptr - 2 * 7 * itemsize * \
+            (2 - 1) - 2 * itemsize * (4 - 1)
+        b_high = b.data.ptr + 1 * itemsize
+        assert cupy.byte_bounds(b) == (b_low, b_high)
+
+    def test_2d_noncontiguous_posneg_stride_1(self, dtype):
+        a = cupy.zeros((4, 7), dtype=dtype)
+        b = a[::1, ::-1]  # strides = (28, -4), shape=(4, 7)
+        itemsize = b.itemsize
+        b_low = b.data.ptr - itemsize * (7 - 1)
+        b_high = b.data.ptr + 1 * itemsize + 7 * itemsize * (4 - 1)
+        assert cupy.byte_bounds(b) == (b_low, b_high)
+
+    def test_2d_noncontiguous_posneg_stride_2(self, dtype):
+        a = cupy.zeros((4, 7), dtype=dtype)
+        b = a[::2, ::-2]  # strides = (56, -8), shape=(2, 4)
+        itemsize = b.itemsize
+        b_low = b.data.ptr - 2 * itemsize * (4 - 1)
+        b_high = b.data.ptr + 1 * itemsize + 2 * 7 * itemsize * (2 - 1)
+        assert cupy.byte_bounds(b) == (b_low, b_high)
