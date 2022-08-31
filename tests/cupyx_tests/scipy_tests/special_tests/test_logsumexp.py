07eb9a818e69f2d3a03eec296613fd29d3f262e1
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-08-31 15:31:18 +0530

    use numpy_cupy_allclose

--- tests/cupyx_tests/scipy_tests/special_tests/test_logsumexp.py
+++ tests/cupyx_tests/scipy_tests/special_tests/test_logsumexp.py
@@ -11,24 +11,24 @@ import cupyx.scipy.special  # NOQA
 class TestLogsumexp:
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_large_inputs(self, xp, scp, dtype):
         a = xp.arange(400)
         return scp.special.logsumexp(a)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_more_large_inputs(self, xp, scp, dtype):
         a = xp.arange(10000)
         return scp.special.logsumexp(a)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_large_numbers(self, xp, scp, dtype):
         a = xp.array([1000, 1000], dtype=dtype)
         return scp.special.logsumexp(a)
 
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_keep_dimensions(self, xp, scp):
         a = xp.array([[100, 1000], [1e10, 1e-10]])
         return scp.special.logsumexp(a, axis=-1, keepdims=True)
@@ -36,20 +36,20 @@ class TestLogsumexp:
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-6)
     def test_array_inputs(self, xp, scp, dtype):
-        if xp.dtype(dtype).kind in 'iu':
+        if xp.dtype(dtype).kind in 'u':
             pytest.skip()
         a = testing.shaped_random((100, 1000), xp, dtype=dtype)
         return scp.special.logsumexp(a)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_sign_argument(self, xp, scp, dtype):
         a = xp.array([1, 1, 1], dtype=dtype)
         b = xp.array([1, -1, -1], dtype=dtype)
         return scp.special.logsumexp(a, b=b, return_sign=True)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_sign_zero(self, xp, scp, dtype):
         a = xp.array([1, 1], dtype=dtype)
         b = xp.array([1, -1], dtype=dtype)
@@ -57,8 +57,8 @@ class TestLogsumexp:
 
     @testing.for_all_dtypes(no_bool=True)
     @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-6)
-    def test_sign_shape(self, xp, scp, dtype):
-        if xp.dtype(dtype).kind in 'iu':
+    def test_sign_multi_dims(self, xp, scp, dtype):
+        if xp.dtype(dtype).kind in 'u':
             pytest.skip()
         a = testing.shaped_random((1, 1, 3, 4), xp, dtype=dtype)
         b = testing.shaped_random((1, 1, 1, 4), xp, dtype=dtype)
@@ -79,22 +79,22 @@ class TestLogsumexp:
         return scp.special.logsumexp(a, axis=(1, 3), b=b, return_sign=True)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_b_zero(self, xp, scp, dtype):
         a = xp.array([1, 100], dtype=dtype)
         b = xp.array([1, 0], dtype=dtype)
         return scp.special.logsumexp(a, b=b)
 
     @testing.for_all_dtypes(no_bool=True)
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_b_shape(self, xp, scp, dtype):
-        if xp.dtype(dtype).kind in 'iu':
+        if xp.dtype(dtype).kind in 'u':
             pytest.skip()
         a = testing.shaped_arange((4, 1, 2, 1), xp, dtype=dtype)
         b = testing.shaped_arange((3, 1, 5), xp, dtype=dtype)
         return scp.special.logsumexp(a, b=b)
 
-    @testing.numpy_cupy_array_equal(scipy_name='scp')
+    @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_special_values(self, xp, scp):
         a = xp.array([cupy.inf, -cupy.inf, cupy.nan, -cupy.nan])
         return scp.special.logsumexp(a)
