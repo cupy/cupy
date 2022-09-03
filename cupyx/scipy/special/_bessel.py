2ca81aae1af459d3bcb0179cb0f2e07d6c2ee6ee
maxim.eremeev <maxim9236@yandex.ru>
2022-09-04 01:33:21 +0300

    Fixes bessel k0 and k0e implementation

--- cupyx/scipy/special/_bessel.py
+++ cupyx/scipy/special/_bessel.py
@@ -105,7 +105,7 @@ i1e = _core.create_ufunc(
     ''')
 
 
-k_implementation = """
+k0_implementation = """
 #include <cupy/math_constants.h>
 
 /* Chebyshev coefficients for K0(x) + log(x/2) I0(x)
@@ -116,25 +116,29 @@ k_implementation = """
  */
 __device__ double A[] = {
     1.37446543561352307156E-16,
-    0,
     4.25981614279661018399E-14,
-    0,
     1.03496952576338420167E-11,
-    0,
     1.90451637722020886025E-9,
-    0,
     2.53479107902614945675E-7,
-    0,
     2.28621210311945178607E-5,
-    0,
     1.26461541144692592338E-3,
-    0,
     3.59799365153615016266E-2,
-    0,
     3.44289899924628486886E-1,
-    0,
     -5.35327393233902768720E-1
 };
+__device__ float AF[] = {
+    1.37446543561352307156E-16,
+    4.25981614279661018399E-14,
+    1.03496952576338420167E-11,
+    1.90451637722020886025E-9,
+    2.53479107902614945675E-7,
+    2.28621210311945178607E-5,
+    1.26461541144692592338E-3,
+    3.59799365153615016266E-2,
+    3.44289899924628486886E-1,
+    -5.35327393233902768720E-1
+};
+
 
 /* Chebyshev coefficients for exp(x) sqrt(x) K0(x)
  * in the inverted interval [2,infinity].
@@ -168,9 +172,34 @@ __device__ double B[] = {
     -3.14481013119645005427E-2,
     2.44030308206595545468E0
 };
+__device__ float BF[] = {
+    5.30043377268626276149E-18,
+    -1.64758043015242134646E-17,
+    5.21039150503902756861E-17,
+    -1.67823109680541210385E-16,
+    5.51205597852431940784E-16,
+    -1.84859337734377901440E-15,
+    6.34007647740507060557E-15,
+    -2.22751332699166985548E-14,
+    8.03289077536357521100E-14,
+    -2.98009692317273043925E-13,
+    1.14034058820847496303E-12,
+    -4.51459788337394416547E-12,
+    1.85594911495471785253E-11,
+    -7.95748924447710747776E-11,
+    3.57739728140030116597E-10,
+    -1.69753450938905987466E-9,
+    8.57403401741422608519E-9,
+    -4.66048989768794782956E-8,
+    2.76681363944501510342E-7,
+    -1.83175552271911948767E-6,
+    1.39498137188764993662E-5,
+    -1.28495495816278026384E-4,
+    1.56988388573005337491E-3,
+    -3.14481013119645005427E-2,
+    2.44030308206595545468E0
+};
 
-// template<typename T>
-// __device__ T k0e(T x){
 __device__ double k0(double x){
     if (x == 0) {
         return CUDART_INF;
@@ -180,35 +209,75 @@ __device__ double k0(double x){
         return CUDART_NAN;
     }
 
+    double y, z;
+
     if (x <= 2.0) {
-        double y = x * x - 2.0;
-        y = chbevl(y, A, 19) - log(0.5 * x) * cyl_bessel_i0(x);
-        return (y);
+        y = x * x - 2.0;
+        y = chbevl(y, A, 10) - log(0.5 * x) * cyl_bessel_i0(x);
+        return y;
     }
 
-    return chbevl(8.0 / x - 2.0, B, 25) / sqrt(x);
+    z = 8.0 / x - 2.0;
+    y = chbevl(z, B, 25) / sqrt(x);
+    return y * exp(-x);
 }
 
+
+__device__ float k0f(float x){
+    if (x == 0) {
+        return CUDART_INF;
+    }
+    
+    if (x < 0) {
+        return CUDART_NAN;
+    }
+
+    float y, z;
+
+    if (x <= 2.0) {
+        y = x * x - 2.0;
+        y = chbevl(y, AF, 10) - logf(0.5 * x) * cyl_bessel_i0f(x);
+        return y;
+    }
+
+    z = 8.0 / x - 2.0;
+    y = chbevl(z, BF, 25) / sqrtf(x);
+    return y * expf(-x);
+}
 """
 
 k0 = _core.create_ufunc(
-    'cupyx_scipy_special_k0e',
-    ('f->f', 'd->d'),
-    'out0 = exp(-in0)*k0(in0)',
-    preamble=chbevl_implementation + k_implementation,
-    doc='''Modified Bessel function of order 1.
+    'cupyx_scipy_special_k0',
+    (('f->f', 'out0 = k0f(in0)'),
+     'd->d'),
+    'out0 = k0(in0)',
+    preamble=chbevl_implementation + k0_implementation,
+    doc='''Modified Bessel function of the second kind of order 0.
+    This function is also sometimes referred to as the modified Bessel function of the third kind of order 0.
 
-    .. seealso:: :meth:`scipy.special.i1`
+    Args:
+        x (cupy.ndarray): argument (float)
+
+    Returns:
+        cupy.ndarray: Value of the modified Bessel function K of order 0 at x.
+
+    .. seealso:: :meth:`scipy.special.k0`
 
     ''')
 
 k0e = _core.create_ufunc(
-    'cupyx_scipy_special_k0',
-    ('f->f', 'd->d'),
-    'out0 = k0(in0)',
-    preamble=chbevl_implementation+k_implementation,
-    doc='''Modified Bessel function of order 1.
+    'cupyx_scipy_special_k0e',
+    (('f->f', 'out0 = expf(in0) * k0f(in0)'), 'd->d'),
+    'out0 = exp(in0) * k0(in0)',
+    preamble=chbevl_implementation + k0_implementation,
+    doc='''Exponentially scaled modified Bessel function K of order 0
 
-    .. seealso:: :meth:`scipy.special.i1`
+    Args:
+        x (cupy.ndarray): argument (float)
+
+    Returns:
+        cupy.ndarray: Value of the exponentially scaled modified Bessel function K of order 0 at x.
+
+    .. seealso:: :meth:`scipy.special.k0e`
 
     ''')
