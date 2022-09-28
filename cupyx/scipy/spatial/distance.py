be62fdfca3aadf5723c608dda02ae3bddab279ee
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-09-28 12:03:46 -0500

    Add pdist definition

--- cupyx/scipy/spatial/distance.py
+++ cupyx/scipy/spatial/distance.py
@@ -194,6 +194,35 @@ def cdist(XA, XB, metric='euclidean', out=None, **kwargs):
         raise TypeError('2nd argument metric must be a string identifier')
 
 
+def pdist(X, metric='euclidean', out=None, **kwargs):
+    """Compute distance between observations in n-dimensional space.
+
+    Args:
+        X (array_like): An :math:`m` by :math:`n` array of :math:`m`
+            original observations in an :math:`n`-dimensional space.
+            Inputs are converted to float type.
+        metric (str, optional): The distance metric to use.
+            The distance function can be 'canberra', 'chebyshev',
+            'cityblock', 'correlation', 'cosine', 'euclidean', 'hamming',
+            'hellinger', 'jensenshannon', 'kl_divergence', 'matching',
+            'minkowski', 'russellrao', 'sqeuclidean'.
+        out (cupy.ndarray, optional): The output array. If not None, the
+            distance matrix Y is stored in this array.
+        **kwargs (dict, optional): Extra arguments to `metric`: refer to each
+            metric documentation for a list of all possible arguments.
+            Some possible arguments:
+            p (float): The p-norm to apply for Minkowski, weighted and
+            unweighted. Default: 2.0
+
+    Returns:
+        Y (cupy.ndarray): A :math:`m` by :math:`m` distance matrix is
+            returned. For each :math:`i` and :math:`j`, the metric
+            ``dist(u=X[i], v=X[j])`` is computed and stored in the
+            :math:`ij` th entry.
+    """
+    return cdist(X, X, metric=metric, out=out, **kwargs)
+
+
 def distance_matrix(x, y, p=2.0):
     """Compute the distance matrix.
     Returns the matrix of all pair-wise distances.
