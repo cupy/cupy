4f7720c899e22d7724b988e9a37a4becf552dcd5
Akifumi Imanishi <imanishi@preferred.jp>
2022-10-10 04:59:28 +0000

    Support subtract.at, bitwise_and.at, bitwise_or.at, bitwise_xor.at

--- tests/cupy_tests/core_tests/test_ufunc_methods.py
+++ tests/cupy_tests/core_tests/test_ufunc_methods.py
@@ -42,6 +42,26 @@ class TestUfuncAtAtomicOps:
         xp.add.at(x, indices, 3)
         return x
 
+    @testing.for_dtypes('iI')
+    @testing.numpy_cupy_array_equal()
+    def test_at_subtract(self, xp, dtype):
+        shape = (50,)
+        x = testing.shaped_random(shape, xp=xp, dtype=dtype, seed=0)
+        mask = testing.shaped_random(shape, xp=xp, dtype=bool, seed=1)
+        indices = xp.nonzero(mask)[0]
+        xp.subtract.at(x, indices, 3)
+        return x
+
+    @testing.for_dtypes('iI')
+    @testing.numpy_cupy_array_equal()
+    def test_at_subtract_duplicate_indices(self, xp, dtype):
+        shape = (50,)
+        x = testing.shaped_random(shape, xp=xp, dtype=dtype, seed=0)
+        indices = testing.shaped_random(
+            shape, xp=xp, dtype=numpy.int32, scale=shape[0], seed=1)
+        xp.subtract.at(x, indices, 3)
+        return x
+
     @testing.for_dtypes('iIQfd')
     @testing.numpy_cupy_allclose()
     def test_at_min(self, xp, dtype):
@@ -84,6 +104,42 @@ class TestUfuncAtAtomicOps:
         values = testing.shaped_random(
             indices.shape, xp=xp, dtype=dtype, seed=2)
         xp.maximum.at(x, indices, values)
+        return x
+
+    @testing.for_dtypes('iIlLqQ')
+    @testing.numpy_cupy_array_equal()
+    def test_at_bitwise_and(self, xp, dtype):
+        shape = (50,)
+        x = testing.shaped_random(shape, xp=xp, dtype=dtype, seed=0)
+        indices = testing.shaped_random(
+            shape, xp=xp, dtype=numpy.int32, scale=shape[0], seed=1)
+        values = testing.shaped_random(
+            indices.shape, xp=xp, dtype=dtype, seed=2)
+        xp.bitwise_and.at(x, indices, values)
+        return x
+
+    @testing.for_dtypes('iIlLqQ')
+    @testing.numpy_cupy_array_equal()
+    def test_at_bitwise_or(self, xp, dtype):
+        shape = (50,)
+        x = testing.shaped_random(shape, xp=xp, dtype=dtype, seed=0)
+        indices = testing.shaped_random(
+            shape, xp=xp, dtype=numpy.int32, scale=shape[0], seed=1)
+        values = testing.shaped_random(
+            indices.shape, xp=xp, dtype=dtype, seed=2)
+        xp.bitwise_or.at(x, indices, values)
+        return x
+
+    @testing.for_dtypes('iIlLqQ')
+    @testing.numpy_cupy_array_equal()
+    def test_at_bitwise_xor(self, xp, dtype):
+        shape = (50,)
+        x = testing.shaped_random(shape, xp=xp, dtype=dtype, seed=0)
+        indices = testing.shaped_random(
+            shape, xp=xp, dtype=numpy.int32, scale=shape[0], seed=1)
+        values = testing.shaped_random(
+            indices.shape, xp=xp, dtype=dtype, seed=2)
+        xp.bitwise_xor.at(x, indices, values)
         return x
 
     @testing.for_dtypes('iIQefd')
