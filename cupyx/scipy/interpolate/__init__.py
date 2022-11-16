c45b5120f1cd0fd31d54e98882649d71fa75796c
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-11-16 17:39:06 +0300

    TST: scipyx/interpolate: tests from scipy, np->cp

--- cupyx/scipy/interpolate/__init__.py
+++ cupyx/scipy/interpolate/__init__.py
@@ -4,5 +4,11 @@ from cupyx.scipy.interpolate._polyint import KroghInterpolator  # NOQA
 from cupyx.scipy.interpolate._polyint import barycentric_interpolate  # NOQA
 from cupyx.scipy.interpolate._polyint import krogh_interpolate  # NOQA
 
+
 # 1-D Splines
 from cupyx.scipy.interpolate._bspline import BSpline, splantider, splder  # NOQA
+
+
+# Radial basis functions
+from cupyx.scipy.interpolate._rbfinterp import RBFInterpolator
+
