e38d591184ebdad0702ded180d9079cc691cfd0c
Michael Zhang <mzzhang2014@gmail.com>
2022-11-30 20:53:36 -0600

    Resolving merge conflict in __init__

--- cupyx/scipy/interpolate/__init__.py
+++ cupyx/scipy/interpolate/__init__.py
@@ -3,11 +3,8 @@ from cupyx.scipy.interpolate._polyint import BarycentricInterpolator  # NOQA
 from cupyx.scipy.interpolate._polyint import KroghInterpolator  # NOQA
 from cupyx.scipy.interpolate._polyint import barycentric_interpolate  # NOQA
 from cupyx.scipy.interpolate._polyint import krogh_interpolate  # NOQA
-
-
 # 1-D Splines
 from cupyx.scipy.interpolate._bspline import BSpline, splantider, splder  # NOQA
-
-
-# Radial basis functions
 from cupyx.scipy.interpolate._rbfinterp import RBFInterpolator  # NOQA
+from cupyx.scipy.interpolate._rgi import RegularGridInterpolator
+from cupyx.scipy.interpolate._rgi import interpn
