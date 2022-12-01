cd6562c2cb104365658772b87d3c1445e51f8fc7
Michael Zhang <mzzhang2014@gmail.com>
2022-11-30 21:03:14 -0600

    Adding NOQA to __init__

--- cupyx/scipy/interpolate/__init__.py
+++ cupyx/scipy/interpolate/__init__.py
@@ -6,5 +6,5 @@ from cupyx.scipy.interpolate._polyint import krogh_interpolate  # NOQA
 # 1-D Splines
 from cupyx.scipy.interpolate._bspline import BSpline, splantider, splder  # NOQA
 from cupyx.scipy.interpolate._rbfinterp import RBFInterpolator  # NOQA
-from cupyx.scipy.interpolate._rgi import RegularGridInterpolator
-from cupyx.scipy.interpolate._rgi import interpn
+from cupyx.scipy.interpolate._rgi import RegularGridInterpolator # NOQA
+from cupyx.scipy.interpolate._rgi import interpn # NOQA
