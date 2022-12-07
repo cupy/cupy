af1cbe1bf861694774506df12fc21621b507e132
Akifumi Imanishi <imanishi@preferred.jp>
2022-12-07 07:43:44 +0000

    Add thrust.set_symmetric_difference

--- cupyx/jit/thrust.py
+++ cupyx/jit/thrust.py
@@ -700,6 +700,48 @@ def set_intersection_by_key(
     )
 
 
+@_wrap_thrust_func(['thrust/set_operations.h'])
+def set_symmetric_difference(
+        env, exec_policy, first1, last1, first2, last2, result, comp=None):
+    """Constructs a sorted range that is the symmetric difference.
+    """
+    # TODO(asi1024): Typecheck for Comparable.
+    _assert_exec_policy_type(exec_policy)
+    _assert_same_type(first1, last1)
+    _assert_same_type(first2, last2)
+    _assert_pointer_type(result)
+    args = [exec_policy, first1, last1, first2, last2, result]
+    if comp is not None:
+        raise NotImplementedError('comp option is not supported')
+    params = ', '.join([a.code for a in args])
+    return _Data(f'thrust::set_symmetric_difference({params})', result.ctype)
+
+
+@_wrap_thrust_func(['thrust/set_operations.h'])
+def set_symmetric_difference_by_key(
+        env, exec_policy, keys_first1, keys_last1, keys_first2, keys_last2,
+        values_first1, values_first2, keys_result, values_result, comp=None):
+    """Constructs the key-value symmetric difference of sorted inputs.
+    """
+    # TODO(asi1024): Typecheck for Comparable.
+    _assert_exec_policy_type(exec_policy)
+    _assert_same_type(keys_first1, keys_last1)
+    _assert_same_type(keys_first2, keys_last2)
+    _assert_pointer_type(values_first1)
+    _assert_pointer_type(values_first2)
+    _assert_pointer_type(keys_result)
+    _assert_pointer_type(values_result)
+    args = [exec_policy, keys_first1, keys_last1, keys_first2, keys_last2,
+            values_first1, values_first2, keys_result, values_result]
+    if comp is not None:
+        raise NotImplementedError('comp option is not supported')
+    params = ', '.join([a.code for a in args])
+    return _Data(
+        f'thrust::set_symmetric_difference_by_key({params})',
+        _cuda_types.Tuple([keys_result.ctype, values_result.ctype])
+    )
+
+
 @_wrap_thrust_func(['thrust/sort.h'])
 def sort(env, exec_policy, first, last, comp=None):
     """Sorts the elements in [first, last) into ascending order.
