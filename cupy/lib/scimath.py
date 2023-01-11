aa94b6e63301d2ca6907bb26f7d65409138464b8
Priya Nagda <nagdapriya13@gmail.com>
2023-01-11 11:40:11 +0530

    fix documentation

--- cupy/lib/scimath.py
+++ cupy/lib/scimath.py
@@ -38,13 +38,17 @@ def _tocomplex(arr):
     the original data: types like single, byte, short, etc. become csingle,
     while others become cdouble.
     A copy of the input is always made.
+
     Parameters
     ----------
-    arr : array
+    arr : cupy.ndarray
+          Input array.
+
     Returns
     -------
-    array
-        An array with the same input data as the input but in complex form.
+    array : cupy.ndarray
+            An array with the same input data as the input but in complex form.
+
     Examples
     --------
     First, consider an input of type short:
@@ -71,6 +75,11 @@ def _tocomplex(arr):
     array([2.+0.j,  4.+0.j,  6.+0.j], dtype=complex64)
     >>> cc
     array([1.+0.j,  2.+0.j,  3.+0.j], dtype=complex64)
+
+    See Also
+    --------
+    numpy.lib.scimath._tocomplex
+
     """
     if issubclass(arr.dtype.type,
                   (cupy.single, cupy.byte,
@@ -85,18 +94,26 @@ def _fix_real_lt_zero(x):
     """Convert `x` to complex if it has real, negative components.
     Otherwise, output is just the array version of the input
     (via cupy.asarray).
+
     Parameters
     ----------
     x : array_like
+
     Returns
     -------
-    array
+    array : cupy.ndarray
+
     Examples
     --------
     >>> cupy.lib.scimath._fix_real_lt_zero([1,2])
     array([1, 2])
     >>> cupy.lib.scimath._fix_real_lt_zero([-1,2])
     array([-1.+0.j,  2.+0.j])
+
+    See Also
+    --------
+    numpy.lib.scimath._fix_real_lt_zero
+
     """
     x = cupy.asarray(x)
     if cupy.any(cupy.isreal(x) & (x < 0)):
@@ -108,18 +125,26 @@ def _fix_int_lt_zero(x):
     """Convert `x` to double if it has real, negative components.
     Otherwise, output is just the array version of the input
     (via cupy.asarray).
+
     Parameters
     ----------
     x : array_like
+
     Returns
     -------
-    array
+    array : cupy.ndarray
+
     Examples
     --------
     >>> cupy.lib.scimath._fix_int_lt_zero([1,2])
     array([1, 2])
     >>> cupy.lib.scimath._fix_int_lt_zero([-1,2])
     array([-1.,  2.])
+
+    See Also
+    --------
+    numpy.lib.scimath._fix_int_lt_zero
+
     """
     x = cupy.asarray(x)
     if cupy.any(cupy.isreal(x) & (x < 0)):
@@ -131,18 +156,26 @@ def _fix_real_abs_gt_1(x):
     """Convert `x` to complex if it has real components x_i with abs(x_i)>1.
     Otherwise, output is just the array version of the input
     (via cupy.asarray).
+
     Parameters
     ----------
     x : array_like
+
     Returns
     -------
-    array
+    array : cupy.ndarray
+
     Examples
     --------
     >>> cupy.lib.scimath._fix_real_abs_gt_1([0,1])
     array([0, 1])
     >>> cupy.lib.scimath._fix_real_abs_gt_1([0,2])
     array([0.+0.j, 2.+0.j])
+
+    See Also
+    --------
+    numpy.lib.scimath._fix_real_abs_gt_1
+
     """
     x = cupy.asarray(x)
     if cupy.any(cupy.isreal(x) & (abs(x) > 1)):
@@ -155,17 +188,17 @@ def sqrt(x):
     Compute the square root of x.
     For negative input elements, a complex value is returned
     (unlike `cupy.sqrt` which returns NaN).
+
     Parameters
     ----------
     x : array_like
        The input value(s).
+
     Returns
     -------
     out : cupy.ndarray
        The square root of `x`.
-    See Also
-    --------
-    cupy.sqrt
+
     Examples
     --------
     For real, non-negative inputs this works just like `cupy.sqrt`:
@@ -185,6 +218,12 @@ def sqrt(x):
     array(0.+2.j)
     >>> cupy.emath.sqrt(complex(-4.0, -0.0))
     array(0.-2.j)
+
+    See Also
+    --------
+    cupy.sqrt
+    numpy.emath.sqrt
+
     """
     x = _fix_real_lt_zero(x)
     return cupy.sqrt(x)
@@ -197,23 +236,24 @@ def log(x):
     of :math:`log_e(x)`. For real `x > 0`, this is a real number (``log(0)``
     returns ``-inf`` and ``log(cupy.inf)`` returns ``inf``). Otherwise, the
     complex principle value is returned.
+
     Parameters
     ----------
     x : array_like
        The value(s) whose log is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The log of the `x` value(s).
-    See Also
-    --------
-    cupy.log
+
     Notes
     -----
     For a log() that returns ``NAN`` when real `x < 0`, use `cupy.log`
     (note, however, that otherwise `cupy.log` and this `log` are identical,
     i.e., both return ``-inf`` for `x = 0`, ``inf`` for `x = inf`, and,
     notably, the complex principle value if ``x.imag != 0``).
+
     Examples
     --------
     >>> cupy.emath.log(cupy.exp(1))
@@ -222,6 +262,12 @@ def log(x):
     ``exp(log(x)) == x`` does *not* hold for real ``x < 0``):
     >>> cupy.emath.log(-cupy.exp(1)) == (1 + cupy.pi * 1j)
     array(True)
+
+    See Also
+    --------
+    cupy.log
+    numpy.emath.log
+
     """
     x = _fix_real_lt_zero(x)
     return cupy.log(x)
@@ -234,23 +280,24 @@ def log10(x):
     `cupy.log10`) of :math:`log_{10}(x)`. For real `x > 0`, this
     is a real number (``log10(0)`` returns ``-inf`` and ``log10(cupy.inf)``
     returns ``inf``). Otherwise, the complex principle value is returned.
+
     Parameters
     ----------
     x : array_like or scalar
        The value(s) whose log base 10 is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The log base 10 of the `x` value(s).
-    See Also
-    --------
-    cupy.log10
+
     Notes
     -----
     For a log10() that returns ``NAN`` when real `x < 0`, use `cupy.log10`
     (note, however, that otherwise `cupy.log10` and this `log10` are
     identical, i.e., both return ``-inf`` for `x = 0`, ``inf`` for `x = inf`,
     and, notably, the complex principle value if ``x.imag != 0``).
+
     Examples
     --------
     (We set the printing precision so the example can be auto-tested)
@@ -259,6 +306,12 @@ def log10(x):
     array(1.)
     >>> cupy.emath.log10([-10**1, -10**2, 10**2])
     array([1.+1.3644j, 2.+1.3644j, 2.+0.j    ])
+
+    See Also
+    --------
+    cupy.log10
+    numpy.emath.log10
+
     """
     x = _fix_real_lt_zero(x)
     return cupy.log10(x)
@@ -269,16 +322,19 @@ def logn(n, x):
     Take log base n of x.
     If `x` contains negative inputs, the answer is computed and returned in the
     complex domain.
+
     Parameters
     ----------
     n : array_like
        The integer base(s) in which the log is taken.
     x : array_like
        The value(s) whose log base `n` is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The log base `n` of the `x` value(s).
+
     Examples
     --------
     >>> cupy.set_printoptions(precision=4)
@@ -286,6 +342,11 @@ def logn(n, x):
     array([2., 3.])
     >>> cupy.emath.logn(2, [-4, -8, 8])
     array([2.+4.5324j, 3.+4.5324j, 3.+0.j    ])
+
+    See Also
+    --------
+    numpy.emath.logn
+
     """
     x = _fix_real_lt_zero(x)
     n = _fix_real_lt_zero(n)
@@ -299,23 +360,24 @@ def log2(x):
     `cupy.log2`) of :math:`log_2(x)`. For real `x > 0`, this is
     a real number (``log2(0)`` returns ``-inf`` and ``log2(cupy.inf)`` returns
     ``inf``). Otherwise, the complex principle value is returned.
+
     Parameters
     ----------
     x : array_like
        The value(s) whose log base 2 is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The log base 2 of the `x` value(s).
-    See Also
-    --------
-    cupy.log2
+
     Notes
     -----
     For a log2() that returns ``NAN`` when real `x < 0`, use `cupy.log2`
     (note, however, that otherwise `cupy.log2` and this `log2` are
     identical, i.e., both return ``-inf`` for `x = 0`, ``inf`` for `x = inf`,
     and, notably, the complex principle value if ``x.imag != 0``).
+
     Examples
     --------
     We set the printing precision so the example can be auto-tested:
@@ -324,6 +386,12 @@ def log2(x):
     array(3.)
     >>> cupy.emath.log2([-4, -8, 8])
     array([2.+4.5324j, 3.+4.5324j, 3.+0.j    ])
+
+    See Also
+    --------
+    cupy.log2
+    numpy.emath.log2
+
     """
     x = _fix_real_lt_zero(x)
     return cupy.log2(x)
@@ -334,6 +402,7 @@ def power(x, p):
     Return x to the power p, (x**p).
     If `x` contains negative values, the output is converted to the
     complex domain.
+
     Parameters
     ----------
     x : array_like
@@ -343,13 +412,12 @@ def power(x, p):
         `p` has to either be a scalar, or contain the same number of values
         as `x`. In the latter case, the result is
         ``x[0]**p[0], x[1]**p[1], ...``.
+
     Returns
     -------
     out : cupy.ndarray
         The result of ``x**p``.
-    See Also
-    --------
-    cupy.power
+
     Examples
     --------
     >>> cupy.set_printoptions(precision=4)
@@ -359,6 +427,12 @@ def power(x, p):
     array([0.25  ,  0.0625])
     >>> cupy.emath.power([-2, 4], 2)
     array([ 4.-0.j, 16.+0.j])
+
+    See Also
+    --------
+    cupy.power
+    numpy.emath.power
+
     """
     x = _fix_real_lt_zero(x)
     p = _fix_int_lt_zero(p)
@@ -372,21 +446,22 @@ def arccos(x):
     `cupy.arccos`) of the inverse cosine of `x`. For real `x` such that
     `abs(x) <= 1`, this is a real number in the closed interval
     :math:`[0, \\pi]`.  Otherwise, the complex principle value is returned.
+
     Parameters
     ----------
     x : array_like or scalar
        The value(s) whose arccos is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The inverse cosine(s) of the `x` value(s).
-    See Also
-    --------
-    cupy.arccos
+
     Notes
     -----
     For an arccos() that returns ``NAN`` when real `x` is not in the
     interval ``[-1,1]``, use `cupy.arccos`.
+
     Examples
     --------
     >>> cupy.set_printoptions(precision=4)
@@ -394,6 +469,12 @@ def arccos(x):
     array(0.)
     >>> cupy.emath.arccos([1, 2])
     array([0.-0.j   , 0.-1.317j])
+
+    See Also
+    --------
+    cupy.arccos
+    numpy.emath.arccos
+
     """
     x = _fix_real_abs_gt_1(x)
     return cupy.arccos(x)
@@ -407,21 +488,22 @@ def arcsin(x):
     `abs(x) <= 1`, this is a real number in the closed interval
     :math:`[-\\pi/2, \\pi/2]`.  Otherwise, the complex principle value is
     returned.
+
     Parameters
     ----------
     x : array_like or scalar
        The value(s) whose arcsin is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The inverse sine(s) of the `x` value(s).
-    See Also
-    --------
-    cupy.arcsin
+
     Notes
     -----
     For an arcsin() that returns ``NAN`` when real `x` is not in the
     interval ``[-1,1]``, use `cupy.arcsin`.
+
     Examples
     --------
     >>> cupy.set_printoptions(precision=4)
@@ -429,6 +511,12 @@ def arcsin(x):
     array(0.)
     >>> cupy.emath.arcsin([0,1])
     array([0.    , 1.5708])
+
+    See Also
+    --------
+    cupy.arcsin
+    numpy.emath.arcsin
+
     """
     x = _fix_real_abs_gt_1(x)
     return cupy.arcsin(x)
@@ -442,22 +530,23 @@ def arctanh(x):
     ``abs(x) < 1``, this is a real number.  If `abs(x) > 1`, or if `x` is
     complex, the result is complex. Finally, `x = 1` returns``inf`` and
     ``x=-1`` returns ``-inf``.
+
     Parameters
     ----------
     x : array_like
        The value(s) whose arctanh is (are) required.
+
     Returns
     -------
     out : cupy.ndarray
        The inverse hyperbolic tangent(s) of the `x` value(s).
-    See Also
-    --------
-    cupy.arctanh
+
     Notes
     -----
     For an arctanh() that returns ``NAN`` when real `x` is not in the
     interval ``(-1,1)``, use `cupy.arctanh` (this latter, however, does
     return +/-inf for ``x = +/-1``).
+
     Examples
     --------
     >>> cupy.set_printoptions(precision=4)
@@ -466,6 +555,12 @@ def arctanh(x):
            [ 0., inf]])
     >>> cupy.emath.arctanh([1j])
     array([0.+0.7854j])
+
+    See Also
+    --------
+    cupy.arctanh
+    numpy.emath.archtanh
+
     """
     x = _fix_real_abs_gt_1(x)
     return cupy.arctanh(x)
