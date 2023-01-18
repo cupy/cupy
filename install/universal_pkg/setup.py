b4947ac7dfdd1657317666f4225bac98c6ebd641
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2023-01-18 14:51:55 +0900

    bump version to v12.0.0b3

--- install/universal_pkg/setup.py
+++ install/universal_pkg/setup.py
@@ -7,7 +7,7 @@ from typing import Dict, List, Optional
 from setuptools import setup
 
 
-VERSION = '12.0.0b2'
+VERSION = '12.0.0b3'
 
 # List of packages supported by this version of CuPy.
 PACKAGES = [
