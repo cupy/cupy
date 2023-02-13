12a7d4b62b3168c5648bd9cdff9b6bfff48f79da
Masayuki Takagi <kamonama@gmail.com>
2023-02-13 17:07:49 +0900

    Update tests/cupyx_tests/scipy_tests/signal_tests/test_signaltools.py
    Co-authored-by: Akifumi Imanishi <akifumi.imanishi@gmail.com>

--- tests/cupyx_tests/scipy_tests/signal_tests/test_signaltools.py
+++ tests/cupyx_tests/scipy_tests/signal_tests/test_signaltools.py
@@ -217,8 +217,10 @@ class TestConvolve2DEdgeCase:
     def test_convolve2d_1(self, xp, scp):
         # see cupy/cupy#5989
         from scipy import misc
-        with pytest.warns(DeprecationWarning):
-            ascent = misc.ascent()
+        if np.lib.NumpyVersion(scipy.__version__) >= '1.10.0':
+            ascent = scipy.datasets.ascent()
+        else:
+            ascent = scipy.misc.ascent()
         if xp is cupy:
             ascent = xp.asarray(ascent)
         scharr = xp.array(
