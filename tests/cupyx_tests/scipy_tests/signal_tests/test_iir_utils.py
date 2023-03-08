ec95ce497df28d83b78705ecef3219e985f4f6da
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-03-07 19:35:02 -0500

    Add non-zero starting conditions test

--- tests/cupyx_tests/scipy_tests/signal_tests/test_iir_utils.py
+++ tests/cupyx_tests/scipy_tests/signal_tests/test_iir_utils.py
@@ -50,3 +50,14 @@ class TestIIRUtils:
         par2 = apply_iir(x, b)
         testing.assert_allclose(par, par2)
         testing.assert_allclose(seq, par)
+
+    @pytest.mark.parametrize('size', [11, 20, 51, 120, 128, 250])
+    @pytest.mark.parametrize('order', [1, 2, 3, 4, 5])
+    def test_order_starting_cond(self, size, order):
+        signs = cupy.tile([1, -1], int(2 * np.ceil(size / 4)))
+        zi = testing.shaped_random((order,))
+        x = cupy.arange(3, size + 3, dtype=cupy.float64) * signs[:size]
+        b = testing.shaped_random((order,))
+        seq = self._sequential_impl(x, b, zi)
+        par = apply_iir(x, b, zi)
+        testing.assert_allclose(seq, par)
