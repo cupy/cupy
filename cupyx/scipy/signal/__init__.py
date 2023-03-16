f7c29dcacfc1f1b64a2169be219524d742751961
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2023-03-16 21:39:07 +0000

    ENH: cupyx/scipy/signal: add savgol_coeffs and savgol_filter

--- cupyx/scipy/signal/__init__.py
+++ cupyx/scipy/signal/__init__.py
@@ -11,3 +11,5 @@ from cupyx.scipy.signal._signaltools import medfilt  # NOQA
 from cupyx.scipy.signal._signaltools import medfilt2d  # NOQA
 
 from cupyx.scipy.signal._bsplines import sepfir2d  # NOQA
+
+from cupyx.scipy.signal._savitzky_golay import savgol_coeffs, savgol_filter   # NOQA
