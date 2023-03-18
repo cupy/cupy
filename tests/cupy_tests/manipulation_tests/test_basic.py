9d53554b4b1d257f90e2b713245c645be24392b2
root <root@ixt-hq-31.local.lan>
2023-03-18 21:46:26 +0000

    Skip manipulation tests causing hang

--- tests/cupy_tests/manipulation_tests/test_basic.py
+++ tests/cupy_tests/manipulation_tests/test_basic.py
@@ -7,7 +7,7 @@ import pytest
 import cupy
 from cupy import cuda
 from cupy import testing
-
+from cupy.cuda import runtime
 
 @testing.gpu
 class TestBasic:
@@ -143,23 +143,26 @@ class TestBasic:
 
     @testing.multi_gpu(2)
     @testing.for_all_dtypes()
-    @pytest.mark.xfail(cuda.runtime.is_hip, reason='ROCm/HIP may have a bug')
+    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     def test_copyto_where_multigpu_raises(self, dtype):
         self._check_copyto_where_multigpu_raises(dtype, 2)
 
     @testing.multi_gpu(4)
     @testing.for_all_dtypes()
+    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     def test_copyto_where_multigpu_raises_4(self, dtype):
         self._check_copyto_where_multigpu_raises(dtype, 4)
 
     @testing.multi_gpu(6)
     @testing.for_all_dtypes()
+    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     def test_copyto_where_multigpu_raises_6(self, dtype):
         self._check_copyto_where_multigpu_raises(dtype, 6)
 
     @testing.multi_gpu(2)
     @testing.for_all_dtypes()
     @testing.numpy_cupy_array_equal()
+    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     def test_copyto_multigpu(self, xp, dtype):
         with cuda.Device(0):
             a = testing.shaped_arange((2, 3, 4), xp, dtype)
@@ -170,7 +173,7 @@ class TestBasic:
 
     @testing.multi_gpu(2)
     @testing.for_all_dtypes()
-    @pytest.mark.xfail(cuda.runtime.is_hip, reason='ROCm/HIP may have a bug')
+    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     def test_copyto_multigpu_noncontinguous(self, dtype):
         with cuda.Device(0):
             src = testing.shaped_arange((2, 3, 4), cupy, dtype)
