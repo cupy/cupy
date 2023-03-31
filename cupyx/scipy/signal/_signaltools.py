c3714600d3ad86cfbf88474f3c20aa7c01cfe77c
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-03-30 19:12:37 -0500

    Add padding methods

--- cupyx/scipy/signal/_signaltools.py
+++ cupyx/scipy/signal/_signaltools.py
@@ -6,6 +6,7 @@ from cupy._core import internal
 from cupyx.scipy.ndimage import _util
 from cupyx.scipy.ndimage import _filters
 from cupyx.scipy.signal import _signaltools_core as _st_core
+from cupyx.scipy.signal._arraytools import const_ext, even_ext, odd_ext
 from cupyx.scipy.signal._iir_utils import apply_iir, compute_correction_factors
 from cupyx.scipy.signal._arraytools import axis_slice, axis_assign
 
@@ -1039,6 +1040,41 @@ def _filtfilt_gust(b, a, x, axis=-1, irlen=None):
     return y_opt, x0, x1
 
 
+def _validate_pad(padtype, padlen, x, axis, ntaps):
+    """Helper to validate padding for filtfilt"""
+    if padtype not in ['even', 'odd', 'constant', None]:
+        raise ValueError(("Unknown value '%s' given to padtype.  padtype "
+                          "must be 'even', 'odd', 'constant', or None.") %
+                         padtype)
+
+    if padtype is None:
+        padlen = 0
+
+    if padlen is None:
+        # Original padding; preserved for backwards compatibility.
+        edge = ntaps * 3
+    else:
+        edge = padlen
+
+    # x's 'axis' dimension must be bigger than edge.
+    if x.shape[axis] <= edge:
+        raise ValueError("The length of the input vector x must be greater "
+                         "than padlen, which is %d." % edge)
+
+    if padtype is not None and edge > 0:
+        # Make an extension of length `edge` at each
+        # end of the input array.
+        if padtype == 'even':
+            ext = even_ext(x, edge, axis=axis)
+        elif padtype == 'odd':
+            ext = odd_ext(x, edge, axis=axis)
+        else:
+            ext = const_ext(x, edge, axis=axis)
+    else:
+        ext = x
+    return edge, ext
+
+
 def filtfilt(b, a, x, axis=-1, padtype='odd', padlen=None, method='pad',
              irlen=None):
     """
@@ -1128,6 +1164,13 @@ def filtfilt(b, a, x, axis=-1, padtype='odd', padlen=None, method='pad',
     if method == "gust":
         y, z1, z2 = _filtfilt_gust(b, a, x, axis=axis, irlen=irlen)
         return y
+
+        # method == "pad"
+    edge, ext = _validate_pad(padtype, padlen, x, axis,
+                              ntaps=max(len(a), len(b)))
+
+    # Get the steady state of the filter's step response.
+    zi = lfilter_zi(b, a)  # NOQA
 
 
 def deconvolve(signal, divisor):
