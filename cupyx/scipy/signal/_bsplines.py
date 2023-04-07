eabe6bdb7527ecc9ff87bf752be441fea272dadc
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-04-07 17:53:55 -0500

    Add tests

--- cupyx/scipy/signal/_bsplines.py
+++ cupyx/scipy/signal/_bsplines.py
@@ -74,21 +74,36 @@ def symiirorder1(input, c0, z1, precision=1.0):
     if cupy.abs(z1) >= 1:
         raise ValueError('|z1| must be less than 1.0')
 
+    if precision <= 0.0 or precision > 1.0:
+        precision = cupy.finfo(input.dtype).resolution
+
     precision *= precision
-    pos = cupy.arange(1, input.size + 1)
+    pos = cupy.arange(0, input.size + 1, dtype=input.dtype)
     pow_z1 = z1 ** pos
 
     diff = pow_z1 * cupy.conjugate(pow_z1)
-    cum_poly = cupy.cumsum(pow_z1 * input) + input[0]
-    valid_starting = cupy.where(diff < precision, cupy.nan, cum_poly)
-    zi = cupy.nanmax(valid_starting, keepdims=True)
+    cum_poly = cupy.cumsum(pow_z1[1:] * input) + input[0]
 
-    if cupy.isnan(zi):
-        raise RuntimeError(
+    overflow = False
+    if precision == 1.0:
+        zi = cum_poly[0]
+    else:
+        all_valid = diff <= precision
+        valid_before = all_valid[1:]
+        valid_after = all_valid[:-1]
+        valid = cupy.logical_xor(valid_before, valid_after)
+        valid_starting = cupy.where(valid, cum_poly, cupy.nan)
+        zi = cupy.nanmax(valid_starting, keepdims=True)
+        zi_pos = pos[1:][valid]
+        overflow = cupy.where(zi_pos >= input.size, True, False)
+
+    if cupy.isnan(zi) or overflow:
+        raise ValueError(
             'Sum to find symmetric boundary conditions did not converge.')
 
     # Apply first the system 1 / (1 - z1 * z^-1)
-    y1, _ = lfilter(cupy.ones(1), cupy.r_[1, -z1], input[1:], zi=zi)
+    y1, _ = lfilter(
+        cupy.ones(1, dtype=input.dtype), cupy.r_[1, -z1], input[1:], zi=zi)
     y1 = cupy.r_[zi, y1]
 
     # Compute backward symmetric condition and apply the system
