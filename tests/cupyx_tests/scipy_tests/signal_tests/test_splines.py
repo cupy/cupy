31f8958095b9229124a0200228b0c01df5c858e2
Masayuki Takagi <mtakagi@preferred.jp>
2023-04-18 05:09:40 +0000

    Require newer SciPy

--- tests/cupyx_tests/scipy_tests/signal_tests/test_splines.py
+++ tests/cupyx_tests/scipy_tests/signal_tests/test_splines.py
@@ -13,7 +13,7 @@ except ImportError:
     pass
 
 
-@testing.with_requires('scipy')
+@testing.with_requires('scipy>=1.9')
 class TestSymIIROrder:
     @pytest.mark.parametrize('size', [11, 20, 32, 51, 64, 120])
     @pytest.mark.parametrize(
