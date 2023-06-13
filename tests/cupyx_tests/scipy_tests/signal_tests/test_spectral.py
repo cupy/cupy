1d791efc7b20103cfb3cd7ca7f01dafaf7e0969d
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-06-13 14:12:39 -0500

    Add welch tests

--- tests/cupyx_tests/scipy_tests/signal_tests/test_spectral.py
+++ tests/cupyx_tests/scipy_tests/signal_tests/test_spectral.py
@@ -320,3 +320,296 @@ class TestPeriodogram:
         win = scp.signal.get_window('hann', 10)
         with pytest.raises(ValueError):
             scp.signal.periodogram(x, window=win)
+
+
+@testing.with_requires('scipy')
+class TestWelch:
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_onesided_even(self, xp, scp):
+        x = xp.zeros(16)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_onesided_odd(self, xp, scp):
+        x = xp.zeros(16)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=9)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_twosided(self, xp, scp):
+        x = xp.zeros(16)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8, return_onesided=False)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_spectrum(self, xp, scp):
+        x = xp.zeros(16)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8, scaling='spectrum')
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_integer_onesided_even(self, xp, scp):
+        x = xp.zeros(16, dtype=int)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_integer_onesided_odd(self, xp, scp):
+        x = xp.zeros(16, dtype=int)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=9)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_integer_twosided(self, xp, scp):
+        x = xp.zeros(16, dtype=int)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8, return_onesided=False)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_complex(self, xp, scp):
+        x = xp.zeros(16, xp.complex128)
+        x[0] = 1.0 + 2.0j
+        x[8] = 1.0 + 2.0j
+        f, p = scp.signal.welch(x, nperseg=8, return_onesided=False)
+        return f, p
+
+    @pytest.mark.parametrize('mod', [(cupy, cupyx.scipy), (np, scipy)])
+    def test_unk_scaling(self, mod):
+        xp, scp = mod
+        with pytest.raises(ValueError):
+            x = xp.zeros(4, xp.complex128)
+            scp.signal.welch(x, scaling='foo', nperseg=4)
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_detrend_linear(self, xp, scp):
+        x = xp.arange(10, dtype=xp.float64) + 0.04
+        f, p = scp.signal.welch(x, nperseg=10, detrend='linear')
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_no_detrending(self, xp, scp):
+        x = xp.arange(10, dtype=xp.float64) + 0.04
+        f1, p1 = scp.signal.welch(x, nperseg=10, detrend=False)
+        f2, p2 = scp.signal.welch(x, nperseg=10, detrend=lambda x: x)
+        return f1, p1, f2, p2
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_detrend_external(self, xp, scp):
+        x = xp.arange(10, dtype=xp.float64) + 0.04
+        f, p = scp.signal.welch(
+            x, nperseg=10,
+            detrend=lambda seg: scp.signal.detrend(seg, type='l'))
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_detrend_external_nd_m1(self, xp, scp):
+        x = xp.arange(40, dtype=xp.float64) + 0.04
+        x = x.reshape((2, 2, 10))
+        f, p = scp.signal.welch(
+            x, nperseg=10,
+            detrend=lambda seg: scp.signal.detrend(seg, type='l'))
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_detrend_external_nd_0(self, xp, scp):
+        x = xp.arange(20, dtype=xp.float64) + 0.04
+        x = x.reshape((2, 1, 10))
+        x = xp.moveaxis(x, 2, 0)
+        f, p = scp.signal.welch(
+            x, nperseg=10, axis=0,
+            detrend=lambda seg: scp.signal.detrend(seg, axis=0, type='l'))
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_nd_axis_m1(self, xp, scp):
+        x = xp.arange(20, dtype=xp.float64) + 0.04
+        x = x.reshape((2, 1, 10))
+        f, p = scp.signal.welch(x, nperseg=10)
+        f0, p0 = scp.signal.welch(x[0, 0, :], nperseg=10)
+        return f, p, f0, p0
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_nd_axis_0(self, xp, scp):
+        x = xp.arange(20, dtype=xp.float64) + 0.04
+        x = x.reshape((10, 2, 1))
+        f, p = scp.signal.welch(x, nperseg=10, axis=0)
+        f0, p0 = scp.signal.welch(x[:, 0, 0], nperseg=10)
+        return f, p, f0, p0
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_window_external(self, xp, scp):
+        x = xp.zeros(16)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, 10, 'hann', nperseg=8)
+        win = scp.signal.get_window('hann', 8)
+        fe, pe = scp.signal.welch(x, 10, win, nperseg=None)
+
+        with pytest.raises(ValueError):
+            scp.signal.welch(x, 10, win, nperseg=4)
+
+        with pytest.raises(ValueError):
+            win_err = scp.signal.get_window('hann', 32)
+            scp.signal.welch(x, 10, win_err, nperseg=None)
+
+        return f, p, fe, pe
+
+    @pytest.mark.parametrize('shape', [(0,), (3, 0), (0, 5, 2)])
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_empty_input(self, shape, xp, scp):
+        f, p = scp.signal.welch(xp.empty(shape))
+        return f, p
+
+    @pytest.mark.parametrize('shape', [(3, 0), (0, 5, 2)])
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_empty_input_other_axis(self, shape, xp, scp):
+        f, p = scp.signal.welch(xp.empty(shape), axis=1)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_short_data(self, xp, scp):
+        x = np.zeros(8)
+        x[0] = 1
+
+        # default nperseg
+        f, p = scp.signal.welch(x, window='hann')
+        # user-specified nperseg
+        f1, p1 = scp.signal.welch(x, window='hann', nperseg=256)
+        # valid nperseg, doesn't give warning
+        f2, p2 = scp.signal.welch(x, nperseg=8)
+        return f, p, f1, p1, f2, p2
+
+    @pytest.mark.parametrize('mod', [(cupy, cupyx.scipy), (np, scipy)])
+    def test_window_long_or_nd(self, mod):
+        xp, scp = mod
+
+        with pytest.raises(ValueError):
+            scp.signal.welch(xp.zeros(4), 1, xp.array([1, 1, 1, 1, 1]))
+
+        with pytest.raises(ValueError):
+            scp.signal.welch(xp.zeros(4), 1, xp.arange(6).reshape((2, 3)))
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_nondefault_noverlap(self, xp, scp):
+        x = xp.zeros(64)
+        x[::8] = 1
+        f, p = scp.signal.welch(x, nperseg=16, noverlap=4)
+        return f, p
+
+    @pytest.mark.parametrize('mod', [(cupy, cupyx.scipy), (np, scipy)])
+    def test_bad_noverlap(self, mod):
+        xp, scp = mod
+        with pytest.raises(ValueError):
+            scp.signal.welch(xp.zeros(4), 1, 'hann', 2, 7)
+
+    @pytest.mark.parametrize('mod', [(cupy, cupyx.scipy), (np, scipy)])
+    def test_nfft_too_short(self, mod):
+        xp, scp = mod
+        with pytest.raises(ValueError):
+            scp.signal.welch(xp.ones(12), nfft=3, nperseg=4)
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_onesided_even_32(self, xp, scp):
+        x = xp.zeros(16, 'f')
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_onesided_odd_32(self, xp, scp):
+        x = xp.zeros(16, 'f')
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=9)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_real_twosided_32(self, xp, scp):
+        x = xp.zeros(16, 'f')
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8, return_onesided=False)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_complex_32(self, xp, scp):
+        x = xp.zeros(16, 'F')
+        x[0] = 1.0 + 2.0j
+        x[8] = 1.0 + 2.0j
+        f, p = scp.signal.welch(x, nperseg=8, return_onesided=False)
+        return f, p
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_padded_freqs(self, xp, scp):
+        x = xp.zeros(12)
+
+        nfft = 24
+        fodd1, _ = scp.signal.welch(x, nperseg=5, nfft=nfft)
+        feven1, _ = scp.signal.welch(x, nperseg=6, nfft=nfft)
+
+        nfft = 25
+        fodd2, _ = scp.signal.welch(x, nperseg=5, nfft=nfft)
+        feven2, _ = scp.signal.welch(x, nperseg=6, nfft=nfft)
+
+        return fodd1, feven1, fodd2, feven2
+
+    @pytest.mark.parametrize(
+        'window', ['hann', 'bartlett', ('tukey', 0.1), 'flattop'])
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_window_correction(self, window, xp, scp):
+        A = 20
+        fs = 1e4
+        nperseg = int(fs // 10)
+        fsig = 300
+
+        tt = xp.arange(fs) / fs
+        x = A * xp.sin(2 * np.pi * fsig * tt)
+
+        _, p_spec = scp.signal.welch(
+            x, fs=fs, nperseg=nperseg, window=window, scaling='spectrum')
+        freq, p_dens = scp.signal.welch(
+            x, fs=fs, nperseg=nperseg, window=window, scaling='density')
+
+        return p_spec, freq, p_dens
+
+    @pytest.mark.parametrize('axis', [0, 1, 2])
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_axis_rolling(self, axis, xp, scp):
+        x_flat = testing.shaped_random((1024,), xp)
+        _, p_flat = scp.signal.welch(x_flat)
+
+        newshape = [1,] * 3
+        newshape[axis] = -1
+        x = x_flat.reshape(newshape)
+
+        _, p_plus = scp.signal.welch(x, axis=axis)  # Positive axis index
+        # Negative axis index
+        _, p_minus = scp.signal.welch(x, axis=axis - x.ndim)
+        return p_flat, p_plus, p_minus
+
+    @testing.numpy_cupy_allclose(scipy_name='scp', rtol=1e-5, atol=1e-5)
+    def test_average(self, xp, scp):
+        x = xp.zeros(16)
+        x[0] = 1
+        x[8] = 1
+        f, p = scp.signal.welch(x, nperseg=8, average='median')
+
+        with pytest.raises(ValueError):
+            scp.signal.welch(x, nperseg=8, average='unrecognised-average')
+        return f, p
