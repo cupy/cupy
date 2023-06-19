4720a4fe6136d1850bdb9da8b364b82dff805bb6
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2023-06-19 16:47:52 +0000

    drop python 3.8 in setup.py

--- setup.py
+++ setup.py
@@ -108,7 +108,6 @@ Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
@@ -140,7 +139,7 @@ setup(
     packages=find_packages(exclude=['install', 'tests']),
     package_data=package_data,
     zip_safe=False,
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     setup_requires=setup_requires,
     install_requires=install_requires,
     tests_require=tests_require,
