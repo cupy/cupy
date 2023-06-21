6a272a7a2ce0f01096eb0f329314657ef73b4adf
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-06-21 17:01:08 -0500

    Add peak_widths

--- cupyx/scipy/signal/_peak_finding.py
+++ cupyx/scipy/signal/_peak_finding.py
@@ -116,12 +116,65 @@ __global__ void peak_prominences(
     while(i <= i_max && x[i] <= x[peak]) {
         if(x[i] < right_min) {
             right_min = x[i];
-            right_bases[peak_nr] = i;
+            right_bases[idx] = i;
         }
         i++;
     }
+
+    prominences[idx] = x[peak] - max(left_min, right_min)
 }
 
+template<typename T>
+__global__ void peak_widths(
+        const int n, const T* __restrict__ x,
+        const long long* __restrict__ peaks,
+        const float64 rel_height,
+        const T* __restrict__ prominences,
+        const long long* __restrict__ left_bases,
+        const long long* __restrict__ right_bases,
+        float64* widths, float64* width_heights,
+        float64* left_ips, float64* right_ips) {
+
+    const int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    if(idx >= n) {
+        return;
+    }
+
+    long long i_min = left_bases[idx];
+    long long i_max = right_bases[idx];
+    long long peak = peaks[idx];
+
+    float64 height = x[peak] - prominences[idx] * rel_height;
+    width_heights[p] = height;
+
+    // Find intersection point on left side
+    long long i = peak;
+    while (i_min < i && height < x[i]) {
+        i--;
+    }
+
+    float64 left_ip = (float64) i;
+    if(x[i] < height) {
+        // Interpolate if true intersection height is between samples
+        left_ip += (height - x[i]) / (x[i + 1] - x[i]);
+    }
+
+    // Find intersection point on right side
+    i = peak;
+    while(i < i_max && height < x[i]) {
+        i++;
+    }
+
+    float64 right_ip = (float64) i;
+    if(x[i] < height) {
+        // Interpolate if true intersection height is between samples
+        right_ip -= (height - x[i]) / (x[i - 1] - x[i]);
+    }
+
+    widths[idx] = right_ip - left_ip;
+    left_ips[idx] = left_ip;
+    right_ips[idx] = right_ip;
+}
 """
 
 PEAKS_MODULE = cupy.RawModule(
@@ -416,6 +469,31 @@ def _peak_prominences(x, peaks, wlen=None):
     return prominences, left_bases, right_bases
 
 
+def _peak_widths(x, peaks, rel_height, prominences, left_bases, right_bases):
+    if rel_height < 0:
+        raise ValueError('`rel_height` must be greater or equal to 0.0')
+    if not (peaks.shape[0] == prominences.shape[0] == left_bases.shape[0]
+            == right_bases.shape[0]):
+        raise ValueError("arrays in `prominence_data` must have the same "
+                         "shape as `peaks`")
+
+    widths = cupy.empty(peaks.shape[0], dtype=cupy.float64)
+    width_heights = cupy.empty(peaks.shape[0], dtype=cupy.float64)
+    left_ips = cupy.empty_like(peaks)
+    right_ips = cupy.empty_like(peaks)
+
+    n = peaks.shape[0]
+    block_sz = 128
+    n_blocks = (n + block_sz - 1) // block_sz
+
+    peak_widths_kernel = _get_module_func(PEAKS_MODULE, 'peak_widths', x)
+    peak_widths_kernel(
+        (n_blocks,), (block_sz,),
+        (n, x, peaks, rel_height, prominences, left_bases, right_bases,
+         widths, width_heights, left_ips, right_ips))
+    return widths, width_heights, left_ips, right_ips
+
+
 def peak_prominences(x, peaks, wlen=None):
     """
     Calculate the prominence of each peak in a signal.
@@ -505,6 +583,99 @@ def peak_prominences(x, peaks, wlen=None):
     peaks = _arg_peaks_as_expected(peaks)
     wlen = _arg_wlen_as_expected(wlen)
     return _peak_prominences(x, peaks, wlen)
+
+
+def peak_widths(x, peaks, rel_height=0.5, prominence_data=None, wlen=None):
+    """
+    Calculate the width of each peak in a signal.
+
+    This function calculates the width of a peak in samples at a relative
+    distance to the peak's height and prominence.
+
+    Parameters
+    ----------
+    x : sequence
+        A signal with peaks.
+    peaks : sequence
+        Indices of peaks in `x`.
+    rel_height : float, optional
+        Chooses the relative height at which the peak width is measured as a
+        percentage of its prominence. 1.0 calculates the width of the peak at
+        its lowest contour line while 0.5 evaluates at half the prominence
+        height. Must be at least 0. See notes for further explanation.
+    prominence_data : tuple, optional
+        A tuple of three arrays matching the output of `peak_prominences` when
+        called with the same arguments `x` and `peaks`. This data are
+        calculated internally if not provided.
+    wlen : int, optional
+        A window length in samples passed to `peak_prominences` as an optional
+        argument for internal calculation of `prominence_data`. This argument
+        is ignored if `prominence_data` is given.
+
+    Returns
+    -------
+    widths : ndarray
+        The widths for each peak in samples.
+    width_heights : ndarray
+        The height of the contour lines at which the `widths` where evaluated.
+    left_ips, right_ips : ndarray
+        Interpolated positions of left and right intersection points of a
+        horizontal line at the respective evaluation height.
+
+    Raises
+    ------
+    ValueError
+        If `prominence_data` is supplied but doesn't satisfy the condition
+        ``0 <= left_base <= peak <= right_base < x.shape[0]`` for each peak,
+        has the wrong dtype, is not C-contiguous or does not have the same
+        shape.
+
+    Warns
+    -----
+    PeakPropertyWarning
+        Raised if any calculated width is 0. This may stem from the supplied
+        `prominence_data` or if `rel_height` is set to 0.
+
+    Warnings
+    --------
+    This function may return unexpected results for data containing NaNs. To
+    avoid this, NaNs should either be removed or replaced.
+
+    See Also
+    --------
+    find_peaks
+        Find peaks inside a signal based on peak properties.
+    peak_prominences
+        Calculate the prominence of peaks.
+
+    Notes
+    -----
+    The basic algorithm to calculate a peak's width is as follows:
+
+    * Calculate the evaluation height :math:`h_{eval}` with the formula
+      :math:`h_{eval} = h_{Peak} - P \\cdot R`, where :math:`h_{Peak}` is the
+      height of the peak itself, :math:`P` is the peak's prominence and
+      :math:`R` a positive ratio specified with the argument `rel_height`.
+    * Draw a horizontal line at the evaluation height to both sides, starting
+      at the peak's current vertical position until the lines either intersect
+      a slope, the signal border or cross the vertical position of the peak's
+      base (see `peak_prominences` for an definition). For the first case,
+      intersection with the signal, the true intersection point is estimated
+      with linear interpolation.
+    * Calculate the width as the horizontal distance between the chosen
+      endpoints on both sides. As a consequence of this the maximal possible
+      width for each peak is the horizontal distance between its bases.
+
+    As shown above to calculate a peak's width its prominence and bases must be
+    known. You can supply these yourself with the argument `prominence_data`.
+    Otherwise, they are internally calculated (see `peak_prominences`).
+    """
+    peaks = _arg_peaks_as_expected(peaks)
+    if prominence_data is None:
+        # Calculate prominence if not supplied and use wlen if supplied.
+        wlen = _arg_wlen_as_expected(wlen)
+        prominence_data = _peak_prominences(x, peaks, wlen)
+    return _peak_widths(x, peaks, rel_height, *prominence_data)
 
 
 def find_peaks(x, height=None, threshold=None, distance=None,
