423cffc5430ba97124c466d8f88e4839adc4b4d3
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-07-04 16:50:45 -0500

    Start adding tests

--- cupyx/scipy/signal/_peak_finding.py
+++ cupyx/scipy/signal/_peak_finding.py
@@ -1231,8 +1231,9 @@ def _peak_finding(data, comparator, axis, order, mode, results):
     device_id = cupy.cuda.Device()
     num_blocks = (device_id.attributes["MultiProcessorCount"] * 20,)
     block_sz = (512,)
+    call_args = data.shape[axis], order, clip, comp, data, results
 
-    kernel_name = "boolrelextrema_2D"
+    kernel_name = "boolrelextrema_1D"
     if data.ndim > 1:
         kernel_name = "boolrelextrema_2D"
         block_sz_x, block_sz_y = 16, 16
@@ -1240,10 +1241,11 @@ def _peak_finding(data, comparator, axis, order, mode, results):
         n_blocks_y = (data.shape[1] + block_sz_y - 1) // block_sz_y
         block_sz = (block_sz_x, block_sz_y)
         num_blocks = (n_blocks_x, n_blocks_y)
+        call_args = (data.shape[1], data.shape[0], order, clip, comp, axis,
+                     data, results)
 
     boolrelextrema = _get_module_func(ARGREL_MODULE, kernel_name, data)
-    boolrelextrema(num_blocks, block_sz,
-                   (data, comp, axis, order, clip, results))
+    boolrelextrema(num_blocks, block_sz, call_args)
 
 
 def _boolrelextrema(data, comparator, axis=0, order=1, mode="clip"):
