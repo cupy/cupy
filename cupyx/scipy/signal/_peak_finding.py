dfeb4457646480dd0a9d8033dbdaa7d2038678c1
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-07-05 10:17:30 -0500

    Fix 2D kernel invocation

--- cupyx/scipy/signal/_peak_finding.py
+++ cupyx/scipy/signal/_peak_finding.py
@@ -1237,8 +1237,8 @@ def _peak_finding(data, comparator, axis, order, mode, results):
     if data.ndim > 1:
         kernel_name = "boolrelextrema_2D"
         block_sz_x, block_sz_y = 16, 16
-        n_blocks_x = (data.shape[0] + block_sz_x - 1) // block_sz_x
-        n_blocks_y = (data.shape[1] + block_sz_y - 1) // block_sz_y
+        n_blocks_x = (data.shape[1] + block_sz_x - 1) // block_sz_x
+        n_blocks_y = (data.shape[0] + block_sz_y - 1) // block_sz_y
         block_sz = (block_sz_x, block_sz_y)
         num_blocks = (n_blocks_x, n_blocks_y)
         call_args = (data.shape[1], data.shape[0], order, clip, comp, axis,
