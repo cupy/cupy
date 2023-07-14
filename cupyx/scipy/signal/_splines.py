a51b05a412c1264ec5b4cd1e26b385b9fa14897c
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2023-07-14 18:23:41 -0500

    Generalize symiirorder1 to n-dimensions

--- cupyx/scipy/signal/_splines.py
+++ cupyx/scipy/signal/_splines.py
@@ -2,8 +2,12 @@
 import cupy
 from cupy._core._scalar import get_typename
 from cupy_backends.cuda.api import runtime
+from cupy._core.internal import _normalize_axis_index
 
 from cupyx.scipy.signal._signaltools import lfilter
+from cupyx.scipy.signal._arraytools import (
+    axis_slice, axis_assign, axis_reverse)
+from cupyx.scipy.signal._iir_utils import collapse_2d, apply_iir_sos
 
 
 if runtime.is_hip:
@@ -130,13 +134,90 @@ def _get_module_func(module, func_name, *template_args):
     return kernel
 
 
-def _find_initial_cond(all_valid, cum_poly, n, off=0):
+def _find_initial_cond(all_valid, cum_poly, n, off=0, axis=-1):
     indices = cupy.where(all_valid)[0] + 1 + off
     zi = cupy.nan
     if indices.size > 0:
         zi = cupy.where(
-            indices[0] >= n, cupy.nan, cum_poly[indices[0] - 1 - off])
+            indices[0] >= n, cupy.nan,
+            axis_slice(cum_poly, indices[0] - 1 - off,
+                       indices[0] - off, axis=axis))
     return zi
+
+
+def _symiirorder1_nd(input, c0, z1, precision=-1.0, axis=-1):
+    axis = _normalize_axis_index(axis, input.ndim)
+    input_shape = input.shape
+    input_ndim = input.ndim
+    if input.ndim > 1:
+        input, input_shape = collapse_2d(input, axis)
+
+    if cupy.abs(z1) >= 1:
+        raise ValueError('|z1| must be less than 1.0')
+
+    if precision <= 0.0 or precision > 1.0:
+        precision = cupy.finfo(input.dtype).resolution
+
+    precision *= precision
+    pos = cupy.arange(1, input_shape[axis] + 1, dtype=input.dtype)
+    pow_z1 = z1 ** pos
+
+    diff = pow_z1 * cupy.conjugate(pow_z1)
+    cum_poly = cupy.cumsum(
+        pow_z1 * input, axis=-1) + axis_slice(input, 0, 1, axis=-1)
+
+    # cupy.expand_dims(input_2d[:, 0], -1)
+    all_valid = diff <= precision
+
+    zi = _find_initial_cond(all_valid, cum_poly, input_shape[axis])
+
+    if cupy.isnan(zi):
+        raise ValueError(
+            'Sum to find symmetric boundary conditions did not converge.')
+
+    # Apply first the system 1 / (1 - z1 * z^-1)
+    zi_shape = (1, 4)
+    if input_ndim > 1:
+        zi_shape = (1, input.shape[0], 4)
+
+    all_zi = cupy.zeros(zi_shape, dtype=input.dtype)
+    all_zi = axis_assign(all_zi, zi, 3, 4)
+    # all_zi[:, :, -1] = zi
+
+    coef = cupy.r_[1, 0, 0, 1, -z1, 0]
+    coef = cupy.atleast_2d(coef)
+
+    y1 = cupy.array(input, dtype=input.dtype, copy=True)
+    y1 = axis_assign(y1, zi, 0, 1)
+
+    apply_iir_sos(axis_slice(input, 1), coef, zi=all_zi,
+                  out=axis_slice(y1, 1), dtype=input.dtype,
+                  apply_fir=False)
+    # y1 = cupy.c_[zi, y1]
+
+    # Compute backward symmetric condition and apply the system
+    # c0 / (1 - z1 * z)
+    zi = -c0 / (z1 - 1.0) * axis_slice(y1, -1)
+    # all_zi[:, :, -1] = zi
+    all_zi = axis_assign(all_zi, zi, 3, 4)
+
+    coef = cupy.r_[c0, 0, 0, 1, -z1, 0]
+    coef = cupy.atleast_2d(coef)
+
+    out, _ = apply_iir_sos(
+        axis_slice(y1, -2, step=-1), coef, zi=all_zi, dtype=input.dtype)
+
+    if input_ndim > 1:
+        out = cupy.c_[axis_reverse(out), zi]
+    else:
+        out = cupy.r_[axis_reverse(out), zi]
+
+    if input_ndim > 1:
+        out = out.reshape(input.shape)
+        out = cupy.moveaxis(out, -1, axis)
+        if not out.flags.c_contiguous:
+            out = out.copy()
+    return out
 
 
 def symiirorder1(input, c0, z1, precision=-1.0):
@@ -168,7 +249,6 @@ def symiirorder1(input, c0, z1, precision=-1.0):
     output : ndarray
         The filtered signal.
     """
-
     if cupy.abs(z1) >= 1:
         raise ValueError('|z1| must be less than 1.0')
 
