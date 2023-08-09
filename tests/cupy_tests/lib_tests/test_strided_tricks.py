53f563ad98342d6673c7fb7abd4eab17d820fabd
Negin Sobhani <negins@ucar.edu>
2023-08-09 17:06:18 -0600

    Apply suggestions from code review
    Co-authored-by: Akifumi Imanishi <akifumi.imanishi@gmail.com>

--- tests/cupy_tests/lib_tests/test_strided_tricks.py
+++ tests/cupy_tests/lib_tests/test_strided_tricks.py
@@ -34,6 +34,7 @@ class TestSlidingWindowView(unittest.TestCase):
         arr = testing.shaped_arange((3, 4), xp)
         window_size = 2
         arr_view = xp.lib.stride_tricks.sliding_window_view(arr, window_size, 0)
+        assert arr_view.strides == (16, 4, 16)
         return arr_view
 
     @testing.numpy_cupy_array_equal()
@@ -43,14 +44,17 @@ class TestSlidingWindowView(unittest.TestCase):
         arr_view = xp.lib.stride_tricks.sliding_window_view(
             arr, window_shape=window_shape
         )
+        assert arr_view.strides == (16, 4, 16, 4)
         return arr_view
 
     @testing.numpy_cupy_array_equal()
     def test_2d_with_axis(self, xp):
         arr = testing.shaped_arange((3, 4), xp)
         window_shape = 3
-        axis = 0
-        arr_view = xp.lib.stride_tricks.sliding_window_view(arr, window_shape, axis)
+        axis = 1
+        arr_view = xp.lib.stride_tricks.sliding_window_view(
+            arr, window_shape, axis)
+        assert arr_view.strides == (16, 4, 4)
         return arr_view
 
     @testing.numpy_cupy_array_equal()
@@ -59,32 +63,36 @@ class TestSlidingWindowView(unittest.TestCase):
         window_shape = (2, 3)
         axis = (0, 1)
         arr_view = xp.lib.stride_tricks.sliding_window_view(arr, window_shape, axis)
+        assert arr_view.strides == (16, 4, 16, 4)
         return arr_view
 
     def test_0d(self):
-        # Create a 0-D array (scalar) for testing
-        arr = cupy.array(42)
-        # Sliding window with window size 1
-        window_size = 1
+        for xp in (numpy, cupy):
+            # Create a 0-D array (scalar) for testing
+            arr = xp.array(42)
+            # Sliding window with window size 1
+            window_size = 1
 
-        # Test if the correct ValueError is raised!
-        with pytest.raises(ValueError, match="axis 0 is out of bounds"):
-            cupy.lib.stride_tricks.sliding_window_view(arr, window_size, 0)
+            # Test if the correct ValueError is raised!
+            with pytest.raises(ValueError, match="axis 0 is out of bounds"):
+                xp.lib.stride_tricks.sliding_window_view(arr, window_size, 0)
 
     def test_window_shape_axis_length_mismatch(self):
-        x = cupy.arange(24).reshape((2, 3, 4))
-        window_shape = (2, 2)
-        axis = None
+        for xp in (numpy, cupy):
+            x = xp.arange(24).reshape((2, 3, 4))
+            window_shape = (2, 2)
+            axis = None
 
-        # Test if ValueError is raised when len(window_shape) != len(axis)
-        with pytest.raises(ValueError, match="Since axis is `None`"):
-            stride_tricks.sliding_window_view(x, window_shape, axis)
+            # Test if ValueError is raised when len(window_shape) != len(axis)
+            with pytest.raises(ValueError, match="Since axis is `None`"):
+                xp.lib.stride_tricks.sliding_window_view(x, window_shape, axis)
 
-    def test_otherarrays(self):
-        x = numpy.arange(5)
-        arr_view = stride_tricks.sliding_window_view(x, 2)
-        expected = [[0, 1], [1, 2], [2, 3], [3, 4]]
-        testing.assert_array_equal(arr_view, expected)
+    @testing.numpy_cupy_array_equal()
+    def test_arraylike_input(self, xp):
+        x = [0, 1, 2, 3, 4]
+        arr_view = xp.lib.stride_tricks.sliding_window_view(x, 2)
+        assert arr_view.strides == (8, 8)
+        return arr_view
 
     def test_writeable_views_not_supported(self):
         x = cupy.arange(24).reshape((2, 3, 4))
