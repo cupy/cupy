b42e3b8379d5b2c4a8748d0fc474c7f885f5f221
Masato Shinokawa <masatosinokawa@gmail.com>
2023-08-17 02:17:30 +0000

    Add test cases

--- tests/cupyx_tests/distributed_tests/test_array.py
+++ tests/cupyx_tests/distributed_tests/test_array.py
@@ -23,127 +23,147 @@ def mem_pool():
 
 @testing.multi_gpu(4)
 class TestDistributedArray:
-    def test_array_creation_from_numpy(self, mem_pool):
-        array = numpy.arange(64, dtype='q').reshape(8, 8)
+    shape_dim2 = (8, 8)
+    mapping_dim2 = {
+        0: (slice(4), slice(4)),
+        1: (slice(4), slice(4, None)),
+        2: (slice(4, None), slice(None, None, 2)),
+        3: (slice(4, None), slice(1, None, 2))}
+    mapping_dim2_2 = {
+        0: (slice(None, None, 2), slice(None, None, 2)),
+        1: (slice(None, None, 2), slice(1, None, 4)),
+        2: (slice(None, None, 2), slice(3, None, 4)),
+        3: slice(1, None, 2)}
+
+    shape_dim3 = (4, 4, 4)
+    mapping_dim3 = {
+        0: slice(2),
+        1: (slice(2, None), slice(None, 3)),
+        2: (slice(2, None), 3, slice(None, 3)),
+        3: (slice(2, None), 3, 3)
+    }
+    mapping_dim3_2 = {
+        0: (slice(1, None, 2), 3),
+        1: (slice(1, None, 2), slice(3), 3),
+        2: (slice(1, None, 2), slice(3), slice(None, 3)),
+        3: slice(None, None, 2),
+    }
+
+    @pytest.mark.parametrize(
+            'shape, mapping',
+            [(shape_dim2, mapping_dim2), (shape_dim3, mapping_dim3)])
+    def test_array_creation_from_numpy(self, mem_pool, shape, mapping):
+        array = numpy.arange(64, dtype='q').reshape(shape)
         assert mem_pool.used_bytes() == 0
-        device_mapping = {
-            0: (slice(0, 4), slice(0, 4)),
-            1: (slice(0, 4), slice(4, 8)),
-            2: (slice(4, 8), slice(0, 8, 2)),
-            3: (slice(4, 8), slice(1, 8, 2))}
-        da = cupyx.distributed._array.distributed_array(array, device_mapping)
+        da = cupyx.distributed._array.distributed_array(array, mapping)
         assert da.device.id == -1
         # Ensure no memory allocation other than the chunks
         assert da.data.ptr == 0
-        assert da.shape == (8, 8)
+        assert da.shape == shape
         assert mem_pool.used_bytes() == array.nbytes
-        testing.assert_array_equal(da._chunks[0], array[:4, :4])
-        testing.assert_array_equal(da._chunks[1], array[:4, 4:])
-        testing.assert_array_equal(da._chunks[2], array[4:, ::2])
-        testing.assert_array_equal(da._chunks[3], array[4:, 1::2])
-        for dev in device_mapping:
+        for dev, idx in mapping.items():
+            assert da._chunks[dev].ndim == array.ndim
+            testing.assert_array_equal(da._chunks[dev].squeeze(), array[idx])
             assert da._chunks[dev].device.id == dev
 
-    def test_array_creation_from_cupy(self, mem_pool):
-        array = cupy.arange(64, dtype='q').reshape(8, 8)
+    @pytest.mark.parametrize(
+            'shape, mapping',
+            [(shape_dim2, mapping_dim2), (shape_dim3, mapping_dim3)])
+    def test_array_creation_from_cupy(self, mem_pool, shape, mapping):
+        array = cupy.arange(64, dtype='q').reshape(shape)
         assert mem_pool.used_bytes() == array.nbytes
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', cupy._util.PerformanceWarning)
-            device_mapping = {
-                0: (slice(0, 4), slice(0, 4)),
-                1: (slice(0, 4), slice(4, 8)),
-                2: (slice(4, 8), slice(0, 8, 2)),
-                3: (slice(4, 8), slice(1, 8, 2))}
-            da = cupyx.distributed._array.distributed_array(array, device_mapping)
+            da = cupyx.distributed._array.distributed_array(array, mapping)
         assert da.device.id == -1
         # Ensure no memory allocation other than chunks & original array
         assert da.data.ptr == 0
-        assert da.shape == (8, 8)
+        assert da.shape == shape
         assert mem_pool.used_bytes() == 2 * array.nbytes
-        testing.assert_array_equal(da._chunks[0], array[:4, :4])
-        testing.assert_array_equal(da._chunks[1], array[:4, 4:])
-        testing.assert_array_equal(da._chunks[2], array[4:, ::2])
-        testing.assert_array_equal(da._chunks[3], array[4:, 1::2])
-        for dev in device_mapping:
+        for dev, idx in mapping.items():
+            assert da._chunks[dev].ndim == array.ndim
+            testing.assert_array_equal(da._chunks[dev].squeeze(), array[idx])
             assert da._chunks[dev].device.id == dev
 
-    def test_array_creation(self, mem_pool):
-        array = numpy.arange(64, dtype='q').reshape(8, 8)
+    @pytest.mark.parametrize(
+            'shape, mapping',
+            [(shape_dim2, mapping_dim2), (shape_dim3, mapping_dim3)])
+    def test_array_creation(self, mem_pool, shape, mapping):
+        array = numpy.arange(64, dtype='q').reshape(shape)
         assert mem_pool.used_bytes() == 0
-        device_mapping = {
-            0: (slice(0, 4), slice(0, 4)),
-            1: (slice(0, 4), slice(4, 8)),
-            2: (slice(4, 8), slice(0, 8, 2)),
-            3: (slice(4, 8), slice(1, 8, 2))}
-        da = cupyx.distributed._array.distributed_array(
-                array.tolist(), device_mapping)
+        da = cupyx.distributed._array.distributed_array(array.tolist(), mapping)
         assert da.device.id == -1
         # Ensure no memory allocation other than the chunks
         assert da.data.ptr == 0
-        assert da.shape == (8, 8)
+        assert da.shape == shape
         assert mem_pool.used_bytes() == array.nbytes
-        testing.assert_array_equal(da._chunks[0], array[:4, :4])
-        testing.assert_array_equal(da._chunks[1], array[:4, 4:])
-        testing.assert_array_equal(da._chunks[2], array[4:, ::2])
-        testing.assert_array_equal(da._chunks[3], array[4:, 1::2])
-        for dev in device_mapping:
+        for dev, idx in mapping.items():
+            assert da._chunks[dev].ndim == array.ndim
+            testing.assert_array_equal(da._chunks[dev].squeeze(), array[idx])
             assert da._chunks[dev].device.id == dev
 
-    @pytest.mark.parametrize('device_mapping', [
-        {0: (slice(0, 4),), 1: (slice(4, 8),)},
-        {0: (slice(None), slice(0, 4)), 1: (slice(None), slice(4, 8))}])
-    def test_ufuncs(self, device_mapping):
-        np_a = numpy.arange(64).reshape(8, 8)
-        np_b = numpy.arange(64).reshape(8, 8) * 2
+    @pytest.mark.parametrize(
+            'shape, mapping',
+            [(shape_dim2, mapping_dim2), (shape_dim3, mapping_dim3)])
+    def test_ufuncs(self, shape, mapping):
+        np_a = numpy.arange(64).reshape(shape)
+        np_b = numpy.arange(64).reshape(shape) * 2
         np_r = numpy.sin(np_a * np_b)
-        d_a = cupyx.distributed._array.distributed_array(np_a, device_mapping)
-        d_b = cupyx.distributed._array.distributed_array(np_b, device_mapping)
+        d_a = cupyx.distributed._array.distributed_array(np_a, mapping)
+        d_b = cupyx.distributed._array.distributed_array(np_b, mapping)
         d_r = cupy.sin(d_a * d_b)
         testing.assert_array_almost_equal(d_r.asnumpy(), np_r)
 
-    @pytest.mark.parametrize('device_mapping', [
-        {0: (slice(0, 4),), 1: (slice(4, 8),)},
-        {0: (slice(None), slice(0, 4)), 1: (slice(None), slice(4, 8))}])
-    def test_elementwise_kernel(self, device_mapping):
+    @pytest.mark.parametrize(
+            'shape, mapping',
+            [(shape_dim2, mapping_dim2), (shape_dim3, mapping_dim3)])
+    def test_elementwise_kernel(self, shape, mapping):
         custom_kernel = cupy.ElementwiseKernel(
             'float32 x, float32 y',
             'float32 z',
             'z = (x - y) * (x - y)',
             'custom')
-        np_a = numpy.arange(64).reshape(8, 8).astype(numpy.float32)
-        np_b = (numpy.arange(64).reshape(8, 8) * 2.0).astype(numpy.float32)
+        np_a = numpy.arange(64).reshape(shape).astype(numpy.float32)
+        np_b = (numpy.arange(64).reshape(shape) * 2.0).astype(numpy.float32)
         np_r = (np_a - np_b) * (np_a - np_b)
-        d_a = cupyx.distributed._array.distributed_array(np_a, device_mapping)
-        d_b = cupyx.distributed._array.distributed_array(np_b, device_mapping)
+        d_a = cupyx.distributed._array.distributed_array(np_a, mapping)
+        d_b = cupyx.distributed._array.distributed_array(np_b, mapping)
         d_r = custom_kernel(d_a, d_b)
         testing.assert_array_almost_equal(d_r.asnumpy(), np_r)
 
-    def test_incompatible_chunk_shapes(self):
-        np_a = numpy.arange(64).reshape(8, 8)
-        np_b = numpy.arange(64).reshape(8, 8) * 2
-        mapping_a = {0: (slice(0, 4),), 1: (slice(4, 8),)}
-        mapping_b = {0: (slice(None), slice(0, 4)), 1: (slice(None), slice(4, 8))}
+    @pytest.mark.parametrize(
+            'shape, mapping_a, mapping_b',
+            [(shape_dim2, mapping_dim2, mapping_dim2_2),
+             (shape_dim3, mapping_dim3, mapping_dim3_2)])
+    def test_incompatible_chunk_shapes(self, shape, mapping_a, mapping_b):
+        np_a = numpy.arange(64).reshape(shape)
+        np_b = numpy.arange(64).reshape(shape) * 2
         d_a = cupyx.distributed._array.distributed_array(np_a, mapping_a)
         d_b = cupyx.distributed._array.distributed_array(np_b, mapping_b)
         with pytest.raises(RuntimeError, match=r'chunk sizes'):
             cupy.sin(d_a * d_b)
 
-    def test_incompatible_chunk_shapes_resharded(self):
-        np_a = numpy.arange(64).reshape(8, 8)
-        np_b = numpy.arange(64).reshape(8, 8) * 2
+    @pytest.mark.parametrize(
+            'shape, mapping_a, mapping_b',
+            [(shape_dim2, mapping_dim2, mapping_dim2_2),
+             (shape_dim3, mapping_dim3, mapping_dim3_2)])
+    def test_incompatible_chunk_shapes_resharded(
+            self, shape, mapping_a, mapping_b):
+        np_a = numpy.arange(64).reshape(shape)
+        np_b = numpy.arange(64).reshape(shape) * 2
         np_r = numpy.sin(np_a * np_b)
-        mapping_a = {0: (slice(0, 4),), 1: (slice(4, 8),)}
-        mapping_b = {0: (slice(None), slice(0, 4)), 1: (slice(None), slice(4, 8))}
         d_a = cupyx.distributed._array.distributed_array(np_a, mapping_a)
         d_b = cupyx.distributed._array.distributed_array(np_b, mapping_b)
         d_r = cupy.sin(d_a * d_b.reshard(mapping_a))
         testing.assert_array_almost_equal(d_r.asnumpy(), np_r)
 
-    def test_incompatible_operand(self):
-        np_a = numpy.arange(64).reshape(8, 8)
-        cp_b = cupy.arange(64).reshape(8, 8)
-        mapping_a = {0: slice(0, 4), 1: slice(4, 8)}
-        d_a = cupyx.distributed._array.distributed_array(np_a, mapping_a)
+    @pytest.mark.parametrize(
+            'shape, mapping',
+            [(shape_dim2, mapping_dim2), (shape_dim3, mapping_dim3)])
+    def test_incompatible_operand(self, shape, mapping):
+        np_a = numpy.arange(64).reshape(shape)
+        cp_b = cupy.arange(64).reshape(shape)
+        d_a = cupyx.distributed._array.distributed_array(np_a, mapping)
         with pytest.raises(RuntimeError, match=r'Mix `cupy.ndarray'):
             cupy.sin(d_a * cp_b)
 
@@ -189,27 +209,19 @@ class TestDistributedArray:
                 p = cupyx.distributed._array._subslice_index(a, c, max_value)
                 assert indices(c) == indices(a, p)
 
-    def test_reshard(self, mem_pool):
-        array = numpy.arange(64, dtype='q').reshape(8, 8)
+    @pytest.mark.parametrize(
+            'shape, mapping_a, mapping_b',
+            [(shape_dim2, mapping_dim2, mapping_dim2_2),
+             (shape_dim3, mapping_dim3, mapping_dim3_2)])
+    def test_reshard(self, mem_pool, shape, mapping_a, mapping_b):
+        array = numpy.arange(64, dtype='q').reshape(shape)
         assert mem_pool.used_bytes() == 0
-        device_mapping = {
-            0: (slice(0, 4), slice(0, 4)),
-            1: (slice(0, 4), slice(4, 8)),
-            2: (slice(4, 8), slice(0, 8, 2)),
-            3: (slice(4, 8), slice(1, 8, 2))}
-        da = cupyx.distributed._array.distributed_array(array, device_mapping)
+        da = cupyx.distributed._array.distributed_array(array, mapping_a)
         assert mem_pool.used_bytes() == array.nbytes
-        new_device_mapping = {
-            0: (slice(0, 8, 2), slice(0, 8, 2)),
-            1: (slice(0, 8, 2), slice(1, 8, 4)),
-            2: (slice(0, 8, 2), slice(3, 8, 4)),
-            3: (slice(1, 8, 2),)}
-        db = da.reshard(new_device_mapping)
+        db = da.reshard(mapping_b)
         assert mem_pool.used_bytes() == 2 * array.nbytes
         testing.assert_array_equal(da.asnumpy(), db.asnumpy())
-        testing.assert_array_equal(db._chunks[0], array[::2, ::2])
-        testing.assert_array_equal(db._chunks[1], array[::2, 1::4])
-        testing.assert_array_equal(db._chunks[2], array[::2, 3::4])
-        testing.assert_array_equal(db._chunks[3], array[1::2])
-        for dev in device_mapping:
+        for dev, idx in mapping_b.items():
+            assert db._chunks[dev].ndim == array.ndim
+            testing.assert_array_equal(db._chunks[dev].squeeze(), array[idx])
             assert db._chunks[dev].device.id == dev
