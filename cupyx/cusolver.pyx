7337ee0004204dcaabcafcc2d96dfbd56789972b
Leo Fang <leo80042@gmail.com>
2023-08-22 02:37:30 -0700

    force dead code elimination

--- cupyx/cusolver.pyx
+++ cupyx/cusolver.pyx
@@ -947,7 +947,6 @@ cpdef _geqrf_orgqr_batched(a, mode):
     x_ptr = q.data.ptr
 
     # compute working space of orgqr and solve Q
-    cdef object orgqr_bufferSize = None
     cdef orgqr_ptr orgqr = NULL
     if dtype == 'f':
         orgqr_bufferSize = sorgqr_bufferSize
@@ -961,6 +960,8 @@ cpdef _geqrf_orgqr_batched(a, mode):
     elif dtype == 'D':
         orgqr_bufferSize = zungqr_bufferSize
         orgqr = orgqr_loop[cuDoubleComplex]
+    else:
+        raise ValueError
 
     # this wrapper also sets the stream for us
     buffersize = orgqr_bufferSize(
