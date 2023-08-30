1e1bcb25e6755d518eaaddebcff14e12d138a203
Akifumi Imanishi <imanishi@preferred.jp>
2023-08-30 05:47:13 +0000

    Fix to use pytest parametrize

--- tests/typing_tests/test_run.py
+++ tests/typing_tests/test_run.py
@@ -1,26 +1,19 @@
 from pathlib import Path
 
+import pytest
 
-def generate_test(_t: Path) -> None:
-    assert not _t.name.endswith("._numpy.pyi")
-    name = str(_t.relative_to(Path(__file__).parent))
-    name = name.replace("_tests/", "/").replace("/test_", "/")
-    name = name.replace("/", "_").replace(".pyi", "")
 
-    with open(_t) as f:
+@pytest.mark.parametrize("t", map(str, Path(__file__).parent.glob("**/*.pyi")))
+def test_run(t: Path) -> None:
+    assert not t.endswith("._numpy.pyi")
+
+    with open(t) as f:
         lines = f.readlines()
 
-    def test() -> None:
-        for _lineno, _line in enumerate(lines, start=1):
-            if not "# E: " in _line:
-                try:
-                    exec(_line)
-                except Exception:
-                    print(f"{_t.stem}:{_lineno} {_line}")
-                    raise
-
-    globals()[f"test_{name}"] = test
-
-
-for t in Path(__file__).parent.glob("**/*.pyi"):
-    generate_test(t)
+    for _lineno, _line in enumerate(lines, start=1):
+        if "# E: " not in _line:
+            try:
+                exec(_line)
+            except Exception:
+                print(f"{t}:{_lineno} {_line}")
+                raise
