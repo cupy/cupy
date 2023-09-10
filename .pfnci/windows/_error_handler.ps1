1faf221cb374901ac2dd0f61034836b52837ca94
emcastillo <ecastill@preferred.jp>
2023-09-10 21:41:25 +0900

    Merge pull request #7775 from kmaehashi/fix-windows-timeout
    Add timeout to Windows CI

--- .pfnci/windows/_error_handler.ps1
+++ .pfnci/windows/_error_handler.ps1
@@ -13,3 +13,42 @@ function RunOrDie {
         throw "Command failed (exit code = $LastExitCode): $args"
     }
 }
+
+function RunWithTimeout {
+    param(
+        [Parameter(Mandatory=$true)]
+        [int]$timeout,
+        [Parameter(Mandatory=$true)]
+        [string]$output,
+        [Parameter(Mandatory=$true)]
+        [string]$command,
+        [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
+        [string[]]$params
+    )
+    $process = Start-Process -NoNewWindow -PassThru -RedirectStandardOutput $output -FilePath $command -ArgumentList $params
+
+    for ($i = 0; $i -lt $timeout; $i++) {
+        if ($process.HasExited) {
+            break
+        }
+        Start-Sleep -Seconds 1
+    }
+    if (!$process.HasExited) {
+        Write-Warning "Command timed out: $command $params"
+        $process | Stop-Process -Force
+
+        # Wait until the process exit to retrieve the exit code.
+        for ($i = 0; $i -lt 100; $i++) {
+            if ($process.HasExited) {
+                break
+            }
+            Start-Sleep -Seconds 0.3
+        }
+        if (!$process.HasExited) {
+            Write-Warning "Failed to stop the process: $command $params"
+            return 999  # ExitCode unavailable, return a dummy value
+        }
+    }
+
+    return $process.ExitCode
+}
