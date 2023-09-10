28278a5f9a6c3760104d0c99ce11169179ca75aa
Aadya Chinubhai <77720426+aadya940@users.noreply.github.com>
2023-09-10 15:30:54 +0530

    Update _csc.py

--- cupyx/scipy/sparse/_csc.py
+++ cupyx/scipy/sparse/_csc.py
@@ -80,8 +80,10 @@ class csc_matrix(_compressed._compressed_sparse_matrix):
         return (y, x)
 
     def __repr__(self):
-        return f"<{self._shape[0]}x{self._shape[1]} sparse matrix of type '{self.dtype}'\n" \
-               f"with {self.getnnz()} stored elements in Compressed Sparse Column format>"
+        return (
+            f"<{self._shape[0]}x{self._shape[1]} sparse matrix of type '{self.dtype}'\n" \
+            f"with {self.getnnz()} stored elements in Compressed Sparse Column format>"
+        )
     
     def __mul__(self, other):
         from cupyx import cusparse
