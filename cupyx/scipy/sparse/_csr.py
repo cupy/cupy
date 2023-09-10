8c4fe3ea2938f2d1760848b926842af9c3f472d7
Aadya Chinubhai <77720426+aadya940@users.noreply.github.com>
2023-09-10 13:51:18 +0530

    ENH: Implement __repr__ for CSR matrix

--- cupyx/scipy/sparse/_csr.py
+++ cupyx/scipy/sparse/_csr.py
@@ -128,6 +128,10 @@ class csr_matrix(_compressed._compressed_sparse_matrix):
             return csr_matrix(out)
         raise NotImplementedError
 
+    def __repr__(self):
+        return f"<{self._shape[0]}x{self._shape[1]} sparse matrix of type '{self.dtype}'\n" \
+               f"with {self.getnnz()} stored elements in Compressed Sparse Row format>"
+    
     def __eq__(self, other):
         return self._comparison(other, operator.eq, '_eq_')
 
