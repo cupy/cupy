5a1e4412fc1e4c327010335dc80ebbf218a214e8
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2023-10-13 08:16:09 +0000

    revert structs and header functions for stub

--- cupy_backends/stub/cupy_cuda_runtime.h
+++ cupy_backends/stub/cupy_cuda_runtime.h
@@ -9,6 +9,43 @@ extern "C" {
 
 bool hip_environment = false;
 
+// Memory management
+enum cudaMemAllocationType {};
+enum cudaMemAllocationHandleType {};
+enum cudaMemLocationType {};
+struct cudaMemLocation {
+    int id;
+};
+struct cudaMemPoolProps {
+    cudaMemAllocationType allocType;
+    cudaMemAllocationHandleType handleTypes;
+    struct cudaMemLocation location;
+    unsigned char reserved[64];
+    void* win32SecurityAttributes;
+};
+
+
+// Stream and Event
+enum cudaStreamCaptureMode {};
+enum cudaStreamCaptureStatus {};
+
+
+// Texture
+cudaExtent make_cudaExtent(...) {
+    struct cudaExtent ex = {0};
+    return ex;
+}
+
+cudaPitchedPtr make_cudaPitchedPtr(...) {
+    struct cudaPitchedPtr ptr = {0};
+    return ptr;
+}
+
+cudaPos make_cudaPos(...) {
+    struct cudaPos pos = {0};
+    return pos;
+}
+
 } // extern "C"
 
 #endif // #ifndef INCLUDE_GUARD_STUB_CUPY_CUDA_RUNTIME_H
