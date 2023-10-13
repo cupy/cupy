9f706b11e53cb6dba4b305cf16493232fda460f9
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2023-10-13 06:20:34 +0000

    introduce library debugging feature

--- cupy_backends/cuda/_softlink.pyx
+++ cupy_backends/cuda/_softlink.pyx
@@ -1,14 +1,25 @@
 import ctypes
+import os
+import sys
 import warnings
 
 from libc.stdint cimport intptr_t
 cimport cython
 
 
+_debug = os.environ.get('CUPY_DEBUG_LIBRARY_LOAD', '0') == '1'
+
+
+def _log(msg: str) -> None:
+    if _debug:
+        sys.stderr.write(f'[CUPY_DEBUG_LIBRARY_LOAD] {msg}\n')
+
+
 cdef class SoftLink:
     def __init__(self, object libname, str prefix, *, bint mandatory=False):
         self.error = None
         self.prefix = prefix
+        self._libname = libname
         self._cdll = None
         if libname is None:
             # Stub build or CUDA/HIP only library.
@@ -17,6 +28,7 @@ cdef class SoftLink:
         else:
             try:
                 self._cdll = ctypes.CDLL(libname)
+                _log(f'Library "{libname}" loaded')
             except Exception as e:
                 self.error = e
                 msg = (
@@ -29,13 +41,16 @@ cdef class SoftLink:
         """
         Returns a function pointer for the API.
         """
-        if self._cdll is None:
-            return <func_ptr>_fail_unsupported
         cdef str funcname = f'{self.prefix}{name}'
+        if self._cdll is None:
+            _log(f'[NOTICE] {self._libname} ({funcname}): library not loaded')
+            return <func_ptr>_fail_unsupported
         cdef object func = getattr(self._cdll, funcname, None)
         if func is None:
+            _log(f'[NOTICE] {self._libname} ({funcname}): function not found')
             return <func_ptr>_fail_not_found
         cdef intptr_t ptr = ctypes.addressof(func)
+        _log(f'{self._libname} ({funcname}): function loaded')
         return cython.operator.dereference(<func_ptr*>ptr)
 
 
