298d8a0cadda972f87ce02dfcf0acdd64022d87b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2023-10-21 13:45:05 +0000

    reduce verbosity of pip install output

--- .pfnci/windows/test.ps1
+++ .pfnci/windows/test.ps1
@@ -86,7 +86,7 @@ function Main {
     echo "Building..."
     $build_retval = 0
     RunOrDie python -m pip install -U "numpy" "scipy"
-    python -m pip install ".[all,test]" -vvv > cupy_build_log.txt
+    python -m pip install ".[all,test]" -v > cupy_build_log.txt
     if (-not $?) {
         $build_retval = $LastExitCode
     }
