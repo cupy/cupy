191ec4129af843a6db3d34e54035a0032abea882
Zhyrek <scott@dpeters.net>
2023-11-07 15:45:09 -0800

    Attempt to add local memory array

--- cupyx/jit/_cuda_types.py
+++ cupyx/jit/_cuda_types.py
@@ -243,6 +243,33 @@ class SharedMem(ArrayBase):
             code = f'{code} __shared__ {self.child_type} {x}[{self._size}]'
         return code
 
+class LocalMem(ArrayBase):
+
+    def __init__(
+            self,
+            child_type: TypeBase,
+            size: Optional[int],
+            alignment: Optional[int] = None,
+    ) -> None:
+        if not (isinstance(size, int) or size is None):
+            raise 'size of local_memory must be integer or `None`'
+        if not (isinstance(alignment, int) or alignment is None):
+            raise 'alignment must be integer or `None`'
+        self._size = size
+        self._alignment = alignment
+        super().__init__(child_type, 1)
+
+    def declvar(self, x: str, init: Optional['Data']) -> str:
+        assert init is None
+        if self._alignment is not None:
+            code = f'__align__({self._alignment})'
+        else:
+            code = ''
+        if self._size is None:
+            code = f'extern {code} {self.child_type} {x}[]'
+        else:
+            code = f'{code} {self.child_type} {x}[{self._size}]'
+        return code
 
 class Ptr(PointerBase):
 
