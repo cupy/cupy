f931e56d87d5d97a28180824bc718bbfb9fe0087
Masayuki Takagi <mtakagi@preferred.jp>
2023-12-08 06:46:12 +0000

    Add parametrized test

--- tests/cupyx_tests/signal_tests/test_radartools.py
+++ tests/cupyx_tests/signal_tests/test_radartools.py
@@ -1,3 +1,5 @@
+import pytest
+
 import cupy
 from cupy import testing
 from cupyx import signal
@@ -5,12 +7,13 @@ from cupyx import signal
 
 class TestPulseCompression:
 
-    def test_pulse_compression(self):
+    @pytest.mark.parametrize('dtype', [cupy.float32, cupy.float64])
+    def test_pulse_compression(self, dtype):
         num_pulses = 128
         num_samples_per_pulse = 9000
         template_length = 1000
         
-        x = cupy.random.randn(num_pulses, num_samples_per_pulse) + 1j * cupy.random.randn(num_pulses, num_samples_per_pulse)
-        template = cupy.random.randn(template_length) + 1j * cupy.random.randn(template_length)
+        x = cupy.random.randn(num_pulses, num_samples_per_pulse, dtype=dtype) + 1j * cupy.random.randn(num_pulses, num_samples_per_pulse, dtype=dtype)
+        template = cupy.random.randn(template_length, dtype=dtype) + 1j * cupy.random.randn(template_length, dtype=dtype)
 
         signal.pulse_compression(x, template, normalize=True, window='hamming')
