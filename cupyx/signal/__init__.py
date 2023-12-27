a85e738dc6f3148d6b44e2277ac9b97bb373210f
Emilio Castillo <ecastill87@gmail.com>
2023-12-27 03:12:17 +0000

    Make packages private

--- cupyx/signal/__init__.py
+++ cupyx/signal/__init__.py
@@ -1,4 +1,4 @@
-from cupyx.signal.radartools import pulse_compression  # NOQA
-from cupyx.signal.io._writer import write_bin  # NOQA
-from cupyx.signal.io._reader import read_bin  # NOQA
-from cupyx.signal.utils.arraytools import get_shared_mem, get_pinned_mem  # NOQA
+from cupyx.signal._radartools import pulse_compression  # NOQA
+from cupyx.signal._io._writer import write_bin  # NOQA
+from cupyx.signal._io._reader import read_bin  # NOQA
+from cupyx.signal._utils.arraytools import get_shared_mem, get_pinned_mem  # NOQA
