3de436b64514df6b612464efa4015950877eb0a9
Emilio Castillo <ecastill87@gmail.com>
2023-12-27 07:24:12 +0000

    add `cupyx.signal.{complex_cepstrum,real_cepstrum,inverse_complex_cepstrum}`

--- cupyx/signal/__init__.py
+++ cupyx/signal/__init__.py
@@ -1,1 +1,3 @@
 from cupyx.signal._radartools import pulse_compression  # NOQA
+from cupyx.signal._acoustics import complex_cepstrum, real_cepstrum  # NOQA
+from cupyx.signal._acoustics import inverse_complex_cepstrum  # NOQA
