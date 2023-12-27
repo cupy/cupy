88a74f0e60b5deaa3bf3faab1b1f77df59648b27
Emilio Castillo <ecastill87@gmail.com>
2023-12-27 03:13:05 +0000

    Make packages private

--- cupyx/signal/__init__.py
+++ cupyx/signal/__init__.py
@@ -1,3 +1,3 @@
 from cupyx.signal._convolution import convolve1d3o  # NOQA
 from cupyx.signal._radartools import pulse_compression, pulse_doppler, cfar_alpha  # NOQA
-from cupyx.signal.filtering import firfilter, firfilter2, firfilter_zi  # NOQA
+from cupyx.signal._filtering import firfilter, firfilter2, firfilter_zi  # NOQA
