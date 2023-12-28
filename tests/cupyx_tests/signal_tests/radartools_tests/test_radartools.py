e67690d7eca8b63cfc54ec74bd1bdfe5a867e203
Akifumi Imanishi <imanishi@preferred.jp>
2023-12-28 16:32:47 +0900

    Merge pull request #8057 from emcastillo/pulse_doppler
    add `cupyx.signal.{pulse_doppler, cfar_alpha}`

--- tests/cupyx_tests/signal_tests/radartools_tests/test_radartools.py
+++ tests/cupyx_tests/signal_tests/radartools_tests/test_radartools.py
@@ -1,21 +1,41 @@
 import pytest
 
 import cupy
+import numpy
 from cupyx import signal
 
 
-class TestPulseCompression:
+@pytest.mark.parametrize('dtype', [cupy.float32, cupy.float64])
+def test_pulse_compression(dtype):
+    num_pulses = 128
+    num_samples_per_pulse = 9000
+    template_length = 1000
 
-    @pytest.mark.parametrize('dtype', [cupy.float32, cupy.float64])
-    def test_pulse_compression(self, dtype):
-        num_pulses = 128
-        num_samples_per_pulse = 9000
-        template_length = 1000
+    shape = num_pulses, num_samples_per_pulse
+    x = cupy.random.randn(*shape, dtype=dtype) + \
+        1j * cupy.random.rand(*shape, dtype=dtype)
+    template = cupy.random.randn(template_length, dtype=dtype) + \
+        1j * cupy.random.randn(template_length, dtype=dtype)
 
-        shape = num_pulses, num_samples_per_pulse
-        x = cupy.random.randn(*shape, dtype=dtype) + \
-            1j * cupy.random.rand(*shape, dtype=dtype)
-        template = cupy.random.randn(template_length, dtype=dtype) + \
-            1j * cupy.random.randn(template_length, dtype=dtype)
+    signal.pulse_compression(x, template, normalize=True, window='hamming')
 
-        signal.pulse_compression(x, template, normalize=True, window='hamming')
+
+@pytest.mark.parametrize('dtype', [cupy.float32, cupy.float64])
+def test_pulse_doppler(dtype):
+    num_pulses = 128
+    num_samples_per_pulse = 9000
+
+    shape = num_pulses, num_samples_per_pulse
+    x = cupy.random.randn(*shape, dtype=dtype) + \
+        1j * cupy.random.rand(*shape, dtype=dtype)
+
+    signal.pulse_doppler(x, window='hamming')
+
+
+@pytest.mark.parametrize('dtype', [cupy.float32, cupy.float64])
+def test_cfar_alpha(dtype):
+    N = 128
+    pfa = numpy.random.rand(128, 128)
+    gpu = signal.cfar_alpha(cupy.array(pfa), N)
+    cpu = N * (pfa ** (-1.0 / N) - 1)
+    cupy.testing.assert_allclose(gpu, cpu)
