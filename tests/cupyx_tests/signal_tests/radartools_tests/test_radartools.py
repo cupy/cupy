d792e916ed379a28d9c95fa07225f87d1d1abe5f
Akifumi Imanishi <imanishi@preferred.jp>
2024-01-04 08:39:06 +0000

    Add cupyx.signal.mvdr

--- tests/cupyx_tests/signal_tests/radartools_tests/test_radartools.py
+++ tests/cupyx_tests/signal_tests/radartools_tests/test_radartools.py
@@ -3,6 +3,7 @@ import pytest
 import cupy
 import numpy
 from cupyx import signal
+from cupy import testing
 
 
 @pytest.mark.parametrize('dtype', [cupy.float32, cupy.float64])
@@ -39,3 +40,11 @@ def test_cfar_alpha(dtype):
     gpu = signal.cfar_alpha(cupy.array(pfa), N)
     cpu = N * (pfa ** (-1.0 / N) - 1)
     cupy.testing.assert_allclose(gpu, cpu)
+
+
+@testing.for_float_dtypes(no_float16=True)
+def test_mvdr(dtype):
+    x = cupy.array([[1, 2, 3], [4, 5, 7]], dtype=dtype)
+    sv = cupy.array([1, 2], dtype=dtype)
+    out = signal.mvdr(x, sv)
+    testing.assert_array_equal(out, [-2, 1.5])
