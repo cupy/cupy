64e74ade017eab5a674434064f3151bf70d44028
jakirkham <jakirkham@gmail.com>
2024-01-18 19:31:43 -0800

    Run `black` on filters

--- cupyx/scipy/ndimage/_filters_core.py
+++ cupyx/scipy/ndimage/_filters_core.py
@@ -10,25 +10,30 @@ from cupyx.scipy.ndimage import _util
 
 
 def _origins_to_offsets(origins, w_shape):
-    return tuple(x//2+o for x, o in zip(w_shape, origins))
+    return tuple(x // 2 + o for x, o in zip(w_shape, origins))
 
 
-def _check_size_footprint_structure(ndim, size, footprint, structure,
-                                    stacklevel=3, force_footprint=False):
+def _check_size_footprint_structure(
+    ndim, size, footprint, structure, stacklevel=3, force_footprint=False
+):
     if structure is None and footprint is None:
         if size is None:
             raise RuntimeError("no footprint or filter size provided")
-        sizes = _util._fix_sequence_arg(size, ndim, 'size', int)
+        sizes = _util._fix_sequence_arg(size, ndim, "size", int)
         if force_footprint:
             return None, cupy.ones(sizes, bool), None
         return sizes, None, None
     if size is not None:
-        warnings.warn("ignoring size because {} is set".format(
-            'structure' if footprint is None else 'footprint'),
-            UserWarning, stacklevel=stacklevel+1)
+        warnings.warn(
+            "ignoring size because {} is set".format(
+                "structure" if footprint is None else "footprint"
+            ),
+            UserWarning,
+            stacklevel=stacklevel + 1,
+        )
 
     if footprint is not None:
-        footprint = cupy.array(footprint, bool, True, 'C')
+        footprint = cupy.array(footprint, bool, True, "C")
         if not footprint.any():
             raise ValueError("all-zero footprint is not supported")
 
@@ -47,25 +52,25 @@ def _check_size_footprint_structure(ndim, size, footprint, structure,
 
 def _convert_1d_args(ndim, weights, origin, axis):
     if weights.ndim != 1 or weights.size < 1:
-        raise RuntimeError('incorrect filter size')
+        raise RuntimeError("incorrect filter size")
     axis = internal._normalize_axis_index(axis, ndim)
-    w_shape = [1]*ndim
+    w_shape = [1] * ndim
     w_shape[axis] = weights.size
     weights = weights.reshape(w_shape)
-    origins = [0]*ndim
+    origins = [0] * ndim
     origins[axis] = _util._check_origin(origin, weights.size)
     return weights, tuple(origins)
 
 
-def _check_nd_args(input, weights, mode, origin, wghts_name='filter weights'):
+def _check_nd_args(input, weights, mode, origin, wghts_name="filter weights"):
     _util._check_mode(mode)
     # Weights must always be less than 2 GiB
     if weights.nbytes >= (1 << 31):
-        raise RuntimeError('weights must be 2 GiB or less, use FFTs instead')
+        raise RuntimeError("weights must be 2 GiB or less, use FFTs instead")
     weight_dims = [x for x in weights.shape if x != 0]
     if len(weight_dims) != input.ndim:
-        raise RuntimeError('{} array has incorrect shape'.format(wghts_name))
-    origins = _util._fix_sequence_arg(origin, len(weight_dims), 'origin', int)
+        raise RuntimeError("{} array has incorrect shape".format(wghts_name))
+    origins = _util._fix_sequence_arg(origin, len(weight_dims), "origin", int)
     for origin, width in zip(origins, weight_dims):
         _util._check_origin(origin, width)
     return tuple(origins), _util._get_inttype(input)
@@ -79,11 +84,11 @@ def _run_1d_filters(filters, input, args, output, mode, cval, origin=0):
     filter. Individual filters can be None causing that axis to be skipped.
     """
     output = _util._get_output(output, input)
-    modes = _util._fix_sequence_arg(mode, input.ndim, 'mode',
-                                    _util._check_mode)
+    modes = _util._fix_sequence_arg(
+        mode, input.ndim, "mode", _util._check_mode)
     # for filters, "wrap" is a synonym for "grid-wrap".
-    modes = ['grid-wrap' if m == 'wrap' else m for m in modes]
-    origins = _util._fix_sequence_arg(origin, input.ndim, 'origin', int)
+    modes = ["grid-wrap" if m == "wrap" else m for m in modes]
+    origins = _util._fix_sequence_arg(origin, input.ndim, "origin", int)
     n_filters = sum(filter is not None for filter in filters)
     if n_filters == 0:
         _core.elementwise_copy(input, output)
@@ -112,8 +117,15 @@ def _run_1d_filters(filters, input, args, output, mode, cval, origin=0):
     return input
 
 
-def _call_kernel(kernel, input, weights, output, structure=None,
-                 weights_dtype=numpy.float64, structure_dtype=numpy.float64):
+def _call_kernel(
+    kernel,
+    input,
+    weights,
+    output,
+    structure=None,
+    weights_dtype=numpy.float64,
+    structure_dtype=numpy.float64,
+):
     """
     Calls a constructed ElementwiseKernel. The kernel must take an input image,
     an optional array of weights, an optional array for the structure, and an
@@ -134,16 +146,16 @@ def _call_kernel(kernel, input, weights, output, structure=None,
     dtype conversion will occur. The input and output are never converted.
     """
     args = [input]
-    complex_output = input.dtype.kind == 'c'
+    complex_output = input.dtype.kind == "c"
     if weights is not None:
         weights = cupy.ascontiguousarray(weights, weights_dtype)
-        complex_output = complex_output or weights.dtype.kind == 'c'
+        complex_output = complex_output or weights.dtype.kind == "c"
         args.append(weights)
     if structure is not None:
         structure = cupy.ascontiguousarray(structure, structure_dtype)
         args.append(structure)
     output = _util._get_output(output, input, None, complex_output)
-    needs_temp = cupy.shares_memory(output, input, 'MAY_SHARE_BOUNDS')
+    needs_temp = cupy.shares_memory(output, input, "MAY_SHARE_BOUNDS")
     if needs_temp:
         output, temp = _util._get_output(output.dtype, input), output
     args.append(output)
@@ -155,18 +167,18 @@ def _call_kernel(kernel, input, weights, output, structure=None,
 
 
 if runtime.is_hip:
-    includes = r'''
+    includes = r"""
 // workaround for HIP: line begins with #include
 #include <cupy/math_constants.h>\n
-'''
+"""
 else:
-    includes = r'''
+    includes = r"""
 #include <type_traits>  // let Jitify handle this
 #include <cupy/math_constants.h>
 
 template<> struct std::is_floating_point<float16> : std::true_type {};
 template<> struct std::is_signed<float16> : std::true_type {};
-'''
+"""
 
 
 _CAST_FUNCTION = """
@@ -189,89 +201,115 @@ __device__ __forceinline__ bool nonzero(T x) { return x != static_cast<T>(0); }
 """
 
 
-def _generate_nd_kernel(name, pre, found, post, mode, w_shape, int_type,
-                        offsets, cval, ctype='X', preamble='', options=(),
-                        has_weights=True, has_structure=False, has_mask=False,
-                        binary_morphology=False, all_weights_nonzero=False):
+def _generate_nd_kernel(
+    name,
+    pre,
+    found,
+    post,
+    mode,
+    w_shape,
+    int_type,
+    offsets,
+    cval,
+    ctype="X",
+    preamble="",
+    options=(),
+    has_weights=True,
+    has_structure=False,
+    has_mask=False,
+    binary_morphology=False,
+    all_weights_nonzero=False,
+):
     # Currently this code uses CArray for weights but avoids using CArray for
     # the input data and instead does the indexing itself since it is faster.
     # If CArray becomes faster than follow the comments that start with
     # CArray: to switch over to using CArray for the input data as well.
 
     ndim = len(w_shape)
-    in_params = 'raw X x'
+    in_params = "raw X x"
     if has_weights:
-        in_params += ', raw W w'
+        in_params += ", raw W w"
     if has_structure:
-        in_params += ', raw S s'
+        in_params += ", raw S s"
     if has_mask:
-        in_params += ', raw M mask'
-    out_params = 'Y y'
+        in_params += ", raw M mask"
+    out_params = "Y y"
 
     # for filters, "wrap" is a synonym for "grid-wrap"
-    mode = 'grid-wrap' if mode == 'wrap' else mode
+    mode = "grid-wrap" if mode == "wrap" else mode
 
     # CArray: remove xstride_{j}=... from string
-    size = ('%s xsize_{j}=x.shape()[{j}], ysize_{j} = _raw_y.shape()[{j}]'
-            ', xstride_{j}=x.strides()[{j}];' % int_type)
+    size = (
+        "%s xsize_{j}=x.shape()[{j}], ysize_{j} = _raw_y.shape()[{j}]"
+        ", xstride_{j}=x.strides()[{j}];" % int_type
+    )
     sizes = [size.format(j=j) for j in range(ndim)]
     inds = _util._generate_indices_ops(ndim, int_type, offsets)
     # CArray: remove expr entirely
-    expr = ' + '.join(['ix_{}'.format(j) for j in range(ndim)])
+    expr = " + ".join(["ix_{}".format(j) for j in range(ndim)])
 
-    ws_init = ws_pre = ws_post = ''
+    ws_init = ws_pre = ws_post = ""
     if has_weights or has_structure:
-        ws_init = 'int iws = 0;'
+        ws_init = "int iws = 0;"
         if has_structure:
-            ws_pre = 'S sval = s[iws];\n'
+            ws_pre = "S sval = s[iws];\n"
         if has_weights:
-            ws_pre += 'W wval = w[iws];\n'
+            ws_pre += "W wval = w[iws];\n"
             if not all_weights_nonzero:
-                ws_pre += 'if (nonzero(wval))'
-        ws_post = 'iws++;'
+                ws_pre += "if (nonzero(wval))"
+        ws_post = "iws++;"
 
     loops = []
     for j in range(ndim):
         if w_shape[j] == 1:
             # CArray: string becomes 'inds[{j}] = ind_{j};', remove (int_)type
-            loops.append('{{ {type} ix_{j} = ind_{j} * xstride_{j};'.
-                         format(j=j, type=int_type))
+            loops.append(
+                "{{ {type} ix_{j} = ind_{j} * xstride_{j};".format(
+                    j=j, type=int_type
+                )
+            )
         else:
             boundary = _util._generate_boundary_condition_ops(
-                mode, 'ix_{}'.format(j), 'xsize_{}'.format(j), int_type)
+                mode, "ix_{}".format(j), "xsize_{}".format(j), int_type
+            )
             # CArray: last line of string becomes inds[{j}] = ix_{j};
-            loops.append('''
+            loops.append(
+                """
     for (int iw_{j} = 0; iw_{j} < {wsize}; iw_{j}++)
     {{
         {type} ix_{j} = ind_{j} + iw_{j};
         {boundary}
         ix_{j} *= xstride_{j};
-        '''.format(j=j, wsize=w_shape[j], boundary=boundary, type=int_type))
+        """.format(
+                    j=j, wsize=w_shape[j], boundary=boundary, type=int_type
+                )
+            )
 
     # CArray: string becomes 'x[inds]', no format call needed
-    value = '(*(X*)&data[{expr}])'.format(expr=expr)
-    if mode == 'constant':
-        cond = ' || '.join(['(ix_{} < 0)'.format(j) for j in range(ndim)])
+    value = "(*(X*)&data[{expr}])".format(expr=expr)
+    if mode == "constant":
+        cond = " || ".join(["(ix_{} < 0)".format(j) for j in range(ndim)])
 
     if cval is numpy.nan:
-        cval = 'CUDART_NAN'
+        cval = "CUDART_NAN"
     elif cval == numpy.inf:
-        cval = 'CUDART_INF'
+        cval = "CUDART_INF"
     elif cval == -numpy.inf:
-        cval = '-CUDART_INF'
+        cval = "-CUDART_INF"
 
     if binary_morphology:
         found = found.format(cond=cond, value=value)
     else:
-        if mode == 'constant':
-            value = '(({cond}) ? cast<{ctype}>({cval}) : {value})'.format(
-                cond=cond, ctype=ctype, cval=cval, value=value)
+        if mode == "constant":
+            value = "(({cond}) ? cast<{ctype}>({cval}) : {value})".format(
+                cond=cond, ctype=ctype, cval=cval, value=value
+            )
         found = found.format(value=value)
 
     # CArray: replace comment and next line in string with
     #   {type} inds[{ndim}] = {{0}};
     # and add ndim=ndim, type=int_type to format call
-    operation = '''
+    operation = """
     {sizes}
     {inds}
     // don't use a CArray for indexing (faster to deal with indexing ourselves)
@@ -286,23 +324,39 @@ def _generate_nd_kernel(name, pre, found, post, mode, w_shape, int_type,
         {ws_post}
     {end_loops}
     {post}
-    '''.format(sizes='\n'.join(sizes), inds=inds, pre=pre, post=post,
-               ws_init=ws_init, ws_pre=ws_pre, ws_post=ws_post,
-               loops='\n'.join(loops), found=found, end_loops='}'*ndim)
+    """.format(
+        sizes="\n".join(sizes),
+        inds=inds,
+        pre=pre,
+        post=post,
+        ws_init=ws_init,
+        ws_pre=ws_pre,
+        ws_post=ws_post,
+        loops="\n".join(loops),
+        found=found,
+        end_loops="}" * ndim,
+    )
 
-    mode_str = mode.replace('-', '_')  # avoid potential hyphen in kernel name
-    name = 'cupyx_scipy_ndimage_{}_{}d_{}_w{}'.format(
-        name, ndim, mode_str, '_'.join(['{}'.format(x) for x in w_shape]))
+    mode_str = mode.replace("-", "_")  # avoid potential hyphen in kernel name
+    name = "cupyx_scipy_ndimage_{}_{}d_{}_w{}".format(
+        name, ndim, mode_str, "_".join(["{}".format(x) for x in w_shape])
+    )
     if all_weights_nonzero:
-        name += '_all_nonzero'
-    if int_type == 'ptrdiff_t':
-        name += '_i64'
+        name += "_all_nonzero"
+    if int_type == "ptrdiff_t":
+        name += "_i64"
     if has_structure:
-        name += '_with_structure'
+        name += "_with_structure"
     if has_mask:
-        name += '_with_mask'
+        name += "_with_mask"
     preamble = includes + _CAST_FUNCTION + preamble
-    options += ('--std=c++11', '-DCUPY_USE_JITIFY')
-    return cupy.ElementwiseKernel(in_params, out_params, operation, name,
-                                  reduce_dims=False, preamble=preamble,
-                                  options=options)
+    options += ("--std=c++11", "-DCUPY_USE_JITIFY")
+    return cupy.ElementwiseKernel(
+        in_params,
+        out_params,
+        operation,
+        name,
+        reduce_dims=False,
+        preamble=preamble,
+        options=options,
+    )
