6006b59665e914d72c7c12e55b615956060e7948
Albert Steppi <albert.steppi@gmail.com>
2024-02-06 13:07:27 -0500

    Remove unnecessary kwarg

--- tests/cupyx_tests/scipy_tests/special_tests/test_digamma.py
+++ tests/cupyx_tests/scipy_tests/special_tests/test_digamma.py
@@ -9,7 +9,7 @@ import numpy
 class TestDigamma(unittest.TestCase):
 
     @testing.with_requires('scipy>=1.1.0')
-    @testing.for_all_dtypes(no_complex=False)
+    @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose(atol=1e-13, rtol=1e-15, scipy_name='scp')
     def test_arange(self, xp, scp, dtype):
         import scipy.special  # NOQA
@@ -18,7 +18,7 @@ class TestDigamma(unittest.TestCase):
         return scp.special.digamma(a)
 
     @testing.with_requires('scipy>=1.1.0')
-    @testing.for_all_dtypes(no_complex=False)
+    @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose(atol=1e-13, rtol=1e-10, scipy_name='scp')
     def test_linspace_positive(self, xp, scp, dtype):
         import scipy.special  # NOQA
@@ -28,7 +28,7 @@ class TestDigamma(unittest.TestCase):
         return scp.special.digamma(a)
 
     @testing.with_requires('scipy>=1.1.0')
-    @testing.for_all_dtypes(no_complex=False)
+    @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose(atol=1e-13, rtol=1e-10, scipy_name='scp')
     def test_linspace_negative(self, xp, scp, dtype):
         import scipy.special  # NOQA
@@ -37,7 +37,7 @@ class TestDigamma(unittest.TestCase):
         a = xp.asarray(a)
         return scp.special.digamma(a)
 
-    @testing.for_all_dtypes(no_complex=False)
+    @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose(atol=1e-13, rtol=1e-10, scipy_name='scp')
     def test_scalar(self, xp, scp, dtype):
         import scipy.special  # NOQA
@@ -45,7 +45,7 @@ class TestDigamma(unittest.TestCase):
         return scp.special.digamma(dtype(1.5))
 
     @testing.with_requires('scipy>=1.1.0')
-    @testing.for_all_dtypes(no_complex=False)
+    @testing.for_all_dtypes()
     @testing.numpy_cupy_allclose(atol=1e-13, rtol=1e-10, scipy_name='scp')
     def test_inf_and_nan(self, xp, scp, dtype):
         import scipy.special  # NOQA
