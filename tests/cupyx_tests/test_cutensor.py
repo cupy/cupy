b197c6ed6dde223d18762d9ef9a300b55f4a768a
Akifumi Imanishi <imanishi@preferred.jp>
2024-02-07 14:05:00 +0900

    Merge pull request #8149 from jemiryguo/main
    add incontiguous support for cutensor functions

--- tests/cupyx_tests/test_cutensor.py
+++ tests/cupyx_tests/test_cutensor.py
@@ -337,3 +337,132 @@ class TestCuTensorContraction:
                              self.c, mode_c)
         cupy.testing.assert_allclose(self.c, self.c_ref,
                                      rtol=self.tol, atol=self.tol)
+
+
+@testing.parameterize(*testing.product({
+    'dtype_char': ['e', 'f', 'd', 'F', 'D'],
+    'shape': [(30, 40, 30, 35)],
+    'alpha': [0.5, 1.0],
+    'beta': [0.0, 1.0],
+    'order': ['C', 'F']
+}))
+@pytest.mark.skipif(not ct.available, reason='cuTensor is unavailable')
+class TestCuTensorIncontiguous:
+    _tol = {'e': 1e-3, 'f': 2e-6, 'd': 1e-12}
+
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        compute_capability = int(device.get_compute_capability())
+        if compute_capability < 70 and self.dtype_char == 'e':
+            pytest.skip("Not supported")
+        self.dtype = numpy.dtype(self.dtype_char)
+        self.tol = self._tol[self.dtype_char.lower()]
+
+    def test_contraction(self):
+        mode_a = cutensor.create_mode('a', 'b', 'c')
+        mode_b = cutensor.create_mode('c', 'd', 'b')
+        mode_c = cutensor.create_mode('d', 'a')
+        a, b, c, d = self.shape
+        self.a = testing.shaped_random(
+            (a, b, c), cupy, dtype=self.dtype, order=self.order)
+        self.b = testing.shaped_random(
+            (c, d, b), cupy, dtype=self.dtype, order=self.order)
+        self.c = testing.shaped_random(
+            (d, a), cupy, dtype=self.dtype, order=self.order)
+        delta = 7
+        c_ref = self.c.copy()
+        c_ref = cutensor.contraction(self.alpha,
+                                     self.a, mode_a,
+                                     self.b, mode_b,
+                                     self.beta,
+                                     c_ref, mode_c)
+        for a0 in range(0, a, delta):
+            for d0 in range(0, d, delta):
+                cutensor.contraction(self.alpha,
+                                     self.a[a0:a0+delta], mode_a,
+                                     self.b[:, d0:d0+delta], mode_b,
+                                     self.beta,
+                                     self.c[d0:d0+delta, a0:a0+delta], mode_c)
+                cupy.testing.assert_allclose(self.c[d0:d0+delta, a0:a0+delta],
+                                             c_ref[d0:d0+delta, a0:a0+delta],
+                                             rtol=self.tol, atol=self.tol)
+
+    def test_reduction(self):
+        mode_a = cutensor.create_mode('a', 'b', 'c')
+        mode_c = cutensor.create_mode('b')
+        a, b, c, _ = self.shape
+        self.a = testing.shaped_random(
+            (a, b, c), cupy, dtype=self.dtype, order=self.order)
+        self.c = testing.shaped_random(
+            (b,), cupy, dtype=self.dtype, order=self.order)
+
+        c_ref = self.c.copy()
+        c_ref = cutensor.reduction(self.alpha,
+                                   self.a, mode_a,
+                                   self.beta,
+                                   c_ref, mode_c)
+        delta = 7
+        for b0 in range(0, b, delta):
+            cutensor.reduction(self.alpha,
+                               self.a[:, b0:b0+delta, :], mode_a,
+                               self.beta,
+                               self.c[b0:b0+delta], mode_c)
+            cupy.testing.assert_allclose(self.c[b0:b0+delta],
+                                         c_ref[b0:b0+delta],
+                                         rtol=self.tol, atol=self.tol)
+
+    def test_elementwise_binary(self):
+        mode_a = cutensor.create_mode('a', 'b', 'c')
+        mode_c = cutensor.create_mode('c', 'a', 'b')
+        a, b, c, _ = self.shape
+        self.a = testing.shaped_random(
+            (a, b, c), cupy, dtype=self.dtype, order=self.order)
+        self.c = testing.shaped_random(
+            (c, a, b), cupy, dtype=self.dtype, order=self.order)
+
+        c_ref = self.c.copy()
+        c_ref = cutensor.elementwise_binary(self.alpha,
+                                            self.a, mode_a,
+                                            self.beta,
+                                            c_ref, mode_c)
+        delta = 7
+        for b0 in range(0, b, delta):
+            cutensor.elementwise_binary(self.alpha,
+                                        self.a[:, b0:b0+delta], mode_a,
+                                        self.beta,
+                                        self.c[:, :, b0:b0+delta], mode_c,
+                                        out=self.c[:, :, b0:b0+delta])
+            cupy.testing.assert_allclose(self.c[:, :, b0:b0+delta],
+                                         c_ref[:, :, b0:b0+delta],
+                                         rtol=self.tol, atol=self.tol)
+
+    def test_elementwise_trinary(self):
+        mode_a = cutensor.create_mode('a', 'b', 'c')
+        mode_b = cutensor.create_mode('b', 'c', 'a')
+        mode_c = cutensor.create_mode('c', 'a', 'b')
+        a, b, c, _ = self.shape
+        self.a = testing.shaped_random(
+            (a, b, c), cupy, dtype=self.dtype, order=self.order)
+        self.b = testing.shaped_random(
+            (b, c, a), cupy, dtype=self.dtype, order=self.order)
+        self.c = testing.shaped_random(
+            (c, a, b), cupy, dtype=self.dtype, order=self.order)
+
+        for gamma in [0.0, 1.0]:
+            c_ref = self.c.copy()
+            c_ref = cutensor.elementwise_trinary(self.alpha, self.a, mode_a,
+                                                 self.beta, self.b, mode_b,
+                                                 gamma, c_ref, mode_c,
+                                                 out=c_ref)
+            delta = 7
+            for a0 in range(0, a, delta):
+                cutensor.elementwise_trinary(self.alpha,
+                                             self.a[a0:a0+delta],
+                                             mode_a, self.beta,
+                                             self.b[:, :, a0:a0+delta],
+                                             mode_b, gamma,
+                                             self.c[:, a0:a0+delta], mode_c,
+                                             out=self.c[:, a0:a0+delta])
+                cupy.testing.assert_allclose(self.c[:, a0:a0+delta],
+                                             c_ref[:, a0:a0+delta],
+                                             rtol=self.tol, atol=self.tol)
