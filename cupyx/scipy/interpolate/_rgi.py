3579ab63ae8fe89d7684eb50f5ec30755e757b58
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-02-08 12:13:54 +0000

    MAINT: cupyx/interpolate: remove a duplicate function

--- cupyx/scipy/interpolate/_rgi.py
+++ cupyx/scipy/interpolate/_rgi.py
@@ -4,33 +4,7 @@ import itertools
 import cupy as cp
 from cupyx.scipy.interpolate._bspline2 import make_interp_spline
 from cupyx.scipy.interpolate._cubic import PchipInterpolator
-
-
-def _ndim_coords_from_arrays(points, ndim=None):
-    """
-    Convert a tuple of coordinate arrays to a (..., ndim)-shaped array.
-    """
-    if isinstance(points, tuple) and len(points) == 1:
-        # handle argument tuple
-        points = points[0]
-    if isinstance(points, tuple):
-        p = cp.broadcast_arrays(*points)
-        n = len(p)
-        for j in range(1, n):
-            if p[j].shape != p[0].shape:
-                raise ValueError(
-                    "coordinate arrays do not have the same shape")
-        points = cp.empty(p[0].shape + (len(points),), dtype=float)
-        for j, item in enumerate(p):
-            points[..., j] = item
-    else:
-        points = cp.asanyarray(points)
-        if points.ndim == 1:
-            if ndim is None:
-                points = points.reshape(-1, 1)
-            else:
-                points = points.reshape(-1, ndim)
-    return points
+from cupyx.scipy.interpolate._interpolate import _ndim_coords_from_arrays
 
 
 def _check_points(points):
