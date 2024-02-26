5053b0318956b00d73add497f9a6b35becb4f30c
Masayuki Takagi <kamonama@gmail.com>
2024-02-26 17:52:22 +0900

    Merge pull request #8206 from ev-br/expm_complex
    Fix `expm(complex matrix)`

--- tests/cupyx_tests/scipy_tests/linalg_tests/test_matfuncs.py
+++ tests/cupyx_tests/scipy_tests/linalg_tests/test_matfuncs.py
@@ -103,3 +103,9 @@ class TestExpM:
     def test_nx2x2_input(self, xp, scp, a):
         a = xp.asarray(a)
         return scp.linalg.expm(a)
+
+    @testing.for_all_dtypes(no_bool=True, no_float16=True)
+    @testing.numpy_cupy_allclose(scipy_name='scp', contiguous_check=False)
+    def test_dtypes(self, xp, scp, dtype):
+        a = xp.eye(2, dtype=dtype)
+        return scp.linalg.expm(a)
