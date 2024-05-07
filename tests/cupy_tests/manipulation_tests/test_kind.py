b0f4ed6b21f12daa8cfb9bfd2d7cd8f7af0a2847
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-05-07 07:31:09 +0000

    MAINT: remove testing of cupy.asfarray (removed in NumPy 2.0)

--- tests/cupy_tests/manipulation_tests/test_kind.py
+++ tests/cupy_tests/manipulation_tests/test_kind.py
@@ -29,16 +29,6 @@ class TestKind(unittest.TestCase):
                 xp.asarray_chkfinite(a, dtype=dtype, order=order)
 
     @testing.for_all_dtypes()
-    def test_asfarray(self, dtype):
-        a = cupy.asarray([1, 2, 3])
-        a_gpu = cupy.asfarray(a, dtype)
-        # Original: a_cpu = numpy.asfarray(a.get(), dtype)
-        if not numpy.issubdtype(dtype, numpy.inexact):
-            dtype = numpy.float64
-        a_cpu = numpy.asarray(a.get(), dtype)
-        assert a_cpu.dtype == a_gpu.dtype
-
-    @testing.for_all_dtypes()
     def test_asfortranarray1(self, dtype):
         def func(xp):
             x = xp.zeros((2, 3), dtype)
