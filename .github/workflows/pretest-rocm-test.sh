b0a6c140841048c86ee5941cad58bcf078a6d06a
Adrian Abeyta <adabeyta@amd.com>
2024-05-14 15:34:06 -0500

    Clean up rocm-pre-test

--- .github/workflows/pretest-rocm-test.sh
+++ .github/workflows/pretest-rocm-test.sh
@@ -1,20 +1,15 @@
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /opt/ghc
+sudo rm -rf "/usr/local/share/boost"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+
 #!/bin/bash
 
 set -uex
 export DEBIAN_FRONTEND=noninteractive
 
-# Add the deadsnakes PPA and update package lists
-apt-get update && apt-get install -y software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-apt-get update
-
 # Install necessary tools and dependencies
-apt-get install -y \
-    python3.9-dev \
-    python3-pip \
-    python3.9-distutils \
-    git
-
+apt-get install -y python3.9-dev python3-pip
 python3.9 -m pip install -U pip wheel
 
 # Install hipify-torch (pin for stability) 
@@ -23,10 +18,12 @@ pip install git+https://github.com/ROCmSoftwarePlatform/hipify_torch.git@cbce9fb
 # Display ROCm configuration/environment information
 hipconfig
 
-# Build CuPy on ROCm
+# Setup build env on ROCm
 export CUPY_INSTALL_USE_HIP=1
 export ROCM_HOME="/opt/rocm"
 export HCC_AMDGPU_TARGET="gfx908,gfx90a,gfx940,gfx941,gfx942"
 export CUPY_INSTALL_USE_HIP="1"
+
+# Build CuPy on ROCm
 python3.9 -m pip install -v -e .
 python3.9 -c "import cupy; cupy.show_config()"
