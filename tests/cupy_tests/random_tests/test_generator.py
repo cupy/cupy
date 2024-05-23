fa2292056b7e0d63daed65ea140a5007ec88629d
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-05-23 16:46:09 +0000

    linter

--- tests/cupy_tests/random_tests/test_generator.py
+++ tests/cupy_tests/random_tests/test_generator.py
@@ -970,8 +970,9 @@ class TestRandint(RandomGeneratorTestCase):
         self.generate(6.7, size=(2, 3))
 
     @pytest.mark.xfail(numpy.__version__ < "2",
-        reason='XXX: np 2.0: comparisons with OOB ints are broken in numpy < 2'
-    )
+                       reason='XXX: np 2.0: comparisons with OOB '
+                              'ints are broken in numpy < 2'
+                       )
     def test_randint_int64_1(self):
         self.generate(2**34, 2**40, 3, dtype='q')
 
