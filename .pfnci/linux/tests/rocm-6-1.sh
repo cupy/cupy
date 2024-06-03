f1c4ac500c974d6310fa5b06eaee62fd675ef802
Masayuki Takagi <mtakagi@preferred.jp>
2024-06-03 03:47:16 +0000

    wip

--- .pfnci/linux/tests/rocm-6-1.sh
+++ .pfnci/linux/tests/rocm-6-1.sh
@@ -11,6 +11,9 @@ ACTIONS="$(dirname $0)/actions"
 export CUPY_TEST_FULL_COMBINATION="0"
 export CUPY_INSTALL_USE_HIP=1
 
+# FIXME: move to proper place
+pip install --user git+https://github.com/ROCmSoftwarePlatform/hipify_torch.git
+
 "$ACTIONS/build.sh"
 "$ACTIONS/unittest.sh" "not slow and not multi_gpu"
 "$ACTIONS/cleanup.sh"
