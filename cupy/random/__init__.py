6cd364bdbaa60b4110f5433ac359b5fada8faf9b
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-08-14 15:27:14 +0900

    :bug: Fix dead NOQA

--- cupy/random/__init__.py
+++ cupy/random/__init__.py
@@ -32,12 +32,14 @@ def default_rng(seed=None):  # NOQA  avoid redefinition of seed
 
     Returns:
         Generator: The initialized generator object.
-    """  # NOQA, list of types need to be in one line for sphinx
+    """ # NOQA
     from cupy.random._generator_api import Generator
 
     if _runtime.is_hip and int(str(_runtime.runtimeGetVersion())[:3]) < 403:
-        raise RuntimeError('Generator API not supported in ROCm<4.3,'
-                           ' please use the legacy one or update ROCm.')
+        raise RuntimeError(
+            "Generator API not supported in ROCm<4.3,"
+            " please use the legacy one or update ROCm."
+        )
     if isinstance(seed, BitGenerator):
         return Generator(seed)
     elif isinstance(seed, Generator):
@@ -48,9 +50,10 @@ def default_rng(seed=None):  # NOQA  avoid redefinition of seed
 def __getattr__(key):
     # TODO(kmaehashi): Split cuRAND dependency from Generator class to allow
     # users use the class for type annotation.
-    if key == 'Generator':
+    if key == "Generator":
         # Lazy import libraries depending on cuRAND
         import cupy.random._generator_api
+
         Generator = cupy.random._generator_api.Generator
         _cupy.random.Generator = Generator
         return Generator
