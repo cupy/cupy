5917b6604016d9ace1ffe70f56c92918b4f7e0b8
Akifumi Imanishi <imanishi@preferred.jp>
2024-08-19 07:02:24 +0000

    Fix deprecation backport

--- tests/cupyx_tests/scipy_tests/linalg_tests/test_uarray.py
+++ tests/cupyx_tests/scipy_tests/linalg_tests/test_uarray.py
@@ -2,8 +2,15 @@ from cupy import testing
 import cupyx.scipy.linalg._uarray
 
 
-@testing.with_requires('scipy>=1.5')
+@testing.with_requires('scipy<1.13')
 def test_implements_names():
     # With the newest SciPy, the decorator `@implements` must find the
     # matching scipy functions.
     assert not cupyx.scipy.linalg._uarray._notfound
+
+
+@testing.with_requires('scipy>=1.13')
+def test_implements_names_1_13():
+    # With the newest SciPy, the decorator `@implements` must find the
+    # matching scipy functions.
+    assert cupyx.scipy.linalg._uarray._notfound == ["tri", "tril", "triu"]
