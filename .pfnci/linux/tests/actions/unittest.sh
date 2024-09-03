7396674988a0becf36720d81483df6e9074d67af
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2024-09-03 11:27:54 +0000

    fix test list

--- .pfnci/linux/tests/actions/unittest.sh
+++ .pfnci/linux/tests/actions/unittest.sh
@@ -29,7 +29,8 @@ test_retval=0
 #export CUPY_CUDA_COMPILE_WITH_DEBUG=1
 #mkdir -p "${CUPY_CACHE_DIR}"
 #compute-sanitizer --tool memcheck --padding 16 $(pyenv which python3) -m pytest "${pytest_opts[@]}" -v cupyx_tests/scipy_tests/interpolate_tests/test_ndgriddata.py || test_retval=$?
-compute-sanitizer --tool memcheck --padding 16 $(pyenv which python3) -m pytest "${pytest_opts[@]}" -v -k 'test_multivalue_2d[True-cubic]' cupyx_tests/scipy_tests/interpolate_tests/test_ndgriddata.py || test_retval=$?
+compute-sanitizer --tool memcheck --padding 16 $(pyenv which python3) -m pytest "${pytest_opts[@]}" -v -k 'test_alternative_call[True-linear]' cupyx_tests/scipy_tests/interpolate_tests/test_ndgriddata.py || test_retval=$?
+compute-sanitizer --tool memcheck --padding 16 $(pyenv which python3) -m pytest "${pytest_opts[@]}" -v -k 'test_multivalue_2d[True-linear]' cupyx_tests/scipy_tests/interpolate_tests/test_ndgriddata.py || test_retval=$?
 exit 1
 ###
 
