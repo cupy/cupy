6744982868cf5af4732d38eaa6e287cecea43c85
Albert Steppi <albert.steppi@gmail.com>
2024-09-24 10:07:36 -0400

    Shorten lines for pep8 compliance

--- cupyx/scipy/special/_sici.py
+++ cupyx/scipy/special/_sici.py
@@ -5,12 +5,39 @@ from cupy import _core
 sici = _core.create_ufunc(
     'cupyx_scipy_special_sici',
     (
-        ('f->ff', 'float si, ci; xsf::sici(in0, &si, &ci); out0 = si; out1 = ci;'),
-        ('d->dd', 'double si, ci; xsf::sici(in0, &si, &ci); out0 = si; out1 = ci;'),
-        ('F->FF', 'complex<float> si, ci; xsf::sici(in0, &si, &ci); out0 = si; out1 = ci;'),
-        ('D->DD', 'complex<double> si, ci; xsf::sici(in0, &si, &ci); out0 = si; out1 = ci;'),
+        (
+            'f->ff',
+            '''
+            float si, ci;
+            xsf::sici(in0, &si, &ci);
+            out0 = si; out1 = ci;
+            ''',
+        ),
+        (
+            'd->dd',
+            '''
+            double si, ci;
+            xsf::sici(in0, &si, &ci);
+            out0 = si; out1 = ci;
+            ''',
+        ),
+        (
+            'F->FF',
+            '''
+            complex<float> si, ci;
+            xsf::sici(in0, &si, &ci);
+            out0 = si; out1 = ci;
+            ''',
+        ),
+        (
+            'D->DD',
+            '''
+            complex<double> si, ci;
+            xsf::sici(in0, &si, &ci);
+            out0 = si; out1 = ci;
+            ''',
+        ),
     ),
-    routine=None,
     preamble="#include <cupy/xsf/sici.h>",
     doc="""sici
 
@@ -24,12 +51,39 @@ sici = _core.create_ufunc(
 shichi = _core.create_ufunc(
     'cupyx_scipy_special_shichi',
     (
-        ('f->ff', 'float shi, chi; xsf::shichi(in0, &shi, &chi); out0 = shi; out1 = chi;'),
-        ('d->dd', 'double shi, chi; xsf::shichi(in0, &shi, &chi); out0 = shi; out1 = chi;'),
-        ('F->FF', 'complex<float> shi, chi; xsf::shichi(in0, &shi, &chi); out0 = shi; out1 = chi;'),
-        ('D->DD', 'complex<double> shi, chi; xsf::shichi(in0, &shi, &chi); out0 = shi; out1 = chi;'),
+        (
+            'f->ff',
+            '''
+            float shi, chi;
+            xsf::shichi(in0, &shi, &chi);
+            out0 = shi; out1 = chi;
+            ''',
+        ),
+        (
+            'd->dd',
+            '''
+            double shi, chi;
+            xsf::shichi(in0, &shi, &chi);
+            out0 = shi; out1 = chi;
+            ''',
+        ),
+        (
+            'F->FF',
+            '''
+            complex<float> shi, chi;
+            xsf::shichi(in0, &shi, &chi);
+            out0 = shi; out1 = chi;
+            ''',
+        ),
+        (
+            'D->DD',
+            '''
+            complex<double> shi, chi;
+            xsf::shichi(in0, &shi, &chi);
+            out0 = shi; out1 = chi;
+            ''',
+        ),
     ),
-    routine=None,
     preamble="#include <cupy/xsf/sici.h>",
     doc="""shichi
 
