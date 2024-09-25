7af49ef482d76913bc0e4ca14d386d1e75657a06
Leo Fang <leof@nvidia.com>
2024-09-25 04:33:36 +0000

    minimize diff

--- tests/cupyx_tests/scipy_tests/signal_tests/test_fir_filter_design.py
+++ tests/cupyx_tests/scipy_tests/signal_tests/test_fir_filter_design.py
@@ -325,7 +325,8 @@ class TestFirls:
         N = 11  # number of taps in the filter
         a = 0.1  # width of the transition band
         # design a halfband symmetric low-pass filter
-        return scp.signal.firls(N, [0, a, 0.5-a, 0.5], [1, 1, 0, 0], fs=1.0)
+        h = scp.signal.firls(N, [0, a, 0.5-a, 0.5], [1, 1, 0, 0], fs=1.0)
+        return h
 
     @testing.numpy_cupy_allclose(scipy_name='scp')
     def test_firls_freqz(self, xp, scp):
