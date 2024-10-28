9c998069dd2e1d6d22c57abef0159bf2d990425b
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-10-28 21:59:48 +0000

    ENH: cupyx.scipy: lazy load cupyx.scipy submodules
    The module-level __getattr__ implementation is parroted after
    https://github.com/scipy/scipy/blob/main/scipy/__init__.py

--- cupyx/scipy/__init__.py
+++ cupyx/scipy/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import sys as _sys
+import importlib as _importlib
 
 from cupy._core import ndarray as _ndarray
 
@@ -36,3 +37,20 @@ def get_array_module(*args):
         if isinstance(arg, (_ndarray, _spmatrix)):
             return _cupyx_scipy
     return _scipy
+
+
+# support lazy importing from cupyx.scipy
+
+_submodules = ['fft', 'fftpack', 'interpolate', 'linalg', 'ndimage', 'signal',
+               'sparse', 'spatial', 'special', 'stats']
+
+def __getattr__(name):
+    if name in _submodules:
+        return _importlib.import_module(f'cupyx.scipy.{name}')
+    else:
+        try:
+            return globals()[name]
+        except KeyError:
+            raise AttributeError(
+                f"Module 'cupyx.scipy' has no attribute '{name}'"
+            )
