3b02d2c8c1e5923e59bb6856605e22a1ec6629e6
Bartjan Henkemans <b.henkemans@student.tue.nl>
2025-04-03 22:49:58 +0200

    Inital implementation based on numpy implementation
    Co-authored-by: Celine de Jong <c.m.c.d.jong@student.tue.nl>
    Co-authored-by: Thomas Buschman <thomasbuschman@live.nl>
    Co-authored-by: Veerle Fürst <v.furst@student.tue.nl>

--- cupy/_creation/ranges.py
+++ cupy/_creation/ranges.py
@@ -3,7 +3,7 @@ import math
 import numpy
 
 import cupy
-from cupy import _core
+from cupy import _core, result_type
 
 
 def arange(start, stop=None, step=1, dtype=None):
@@ -253,6 +253,64 @@ def logspace(start, stop, num=50, endpoint=True, base=10.0, dtype=None,
         y = y.astype(cupy.float64)
     return _core.power(base, y).astype(dtype)
 
+def geomspace(start, stop, num=50, endpoint=True, dtype=None, axis=0):
+    """
+    Return numbers spaced evenly on a log scale (a geometric progression).
+
+    This is similar to `logspace`, but with endpoints specified directly.
+    Each output sample is a constant multiple of the previous.
+
+    Args:
+        start: Start of the interval.
+        stop: End of the interval.
+        num: Number of elements.
+        endpoint (bool): If ``True``, the stop value is included as the last
+            element. Otherwise, the stop value is omitted.
+        dtype: Data type specifier. It is inferred from the start and stop
+            arguments by default.
+        axis (int):  The axis in the result to store the samples.  Relevant
+            only if start or stop are array-like.  By default ``0``, the
+            samples will be along a new axis inserted at the beginning.
+            Use ``-1`` to get an axis at the end.
+    Returns:
+        cupy.ndarray: The 1-D array of ranged values.
+
+    .. seealso:: :func:`numpy.geomspace`
+    """
+    start = cupy.asanyarray(start)
+    stop = cupy.asanyarray(stop)
+
+    if cupy.any(start == 0) or cupy.any(stop == 0):
+        raise ValueError('Geometric sequence cannot include zero')
+
+    dt = result_type(start, stop, float(num), cupy.zeros((), dtype=dtype))
+    if dtype is None:
+        dtype = dt
+    else:
+        dtype = cupy.dtype(dtype)
+
+    start = start.astype(dt, copy=True)
+    stop = stop.astype(dt, copy=True)
+
+    out_sign = cupy.sign(start)
+    start /= out_sign
+    stop = stop / out_sign
+
+    log_start = cupy.log10(start)
+    log_stop = cupy.log10(stop)
+
+    result = logspace(log_start, log_stop, num=num, endpoint=endpoint, base=10.0, dtype=dt)
+
+    if num > 0:
+        result[0] = start
+        if num > 1 and endpoint:
+            result[-1] = stop
+    result *= out_sign
+
+    if axis != 0:
+        result = cupy.moveaxis(result, 0, axis)
+
+    return result.astype(dtype, copy=False)
 
 def meshgrid(*xi, **kwargs):
     """Return coordinate matrices from coordinate vectors.
