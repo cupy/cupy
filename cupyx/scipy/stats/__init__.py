81a55fcd531b4c4cc1bd8de45ddda39e2a4436ab
Henkdebutler <137224276+Henkdebutler@users.noreply.github.com>
2025-04-03 23:34:10 +0200

    Adapt the scipy gmean implementation to use cupy functions.
    Co-authored-by: Bartjan Henkemans <b.henkemans@student.tue.nl>
    Co-authored-by: Veerle Fürst <v.furst@student.tue.nl>
    Co-authored-by: Celine de Jong <c.m.c.d.jong@student.tue.nl>

--- cupyx/scipy/stats/__init__.py
+++ cupyx/scipy/stats/__init__.py
@@ -9,3 +9,4 @@ from cupyx.scipy.stats._stats import trim_mean  # NOQA
 from cupyx.scipy.stats._morestats import boxcox_llf  # NOQA
 from cupyx.scipy.stats._stats_py import zmap  # NOQA
 from cupyx.scipy.stats._stats_py import zscore  # NOQA
+from cupyx.scipy.stats._stats_py import gmean  # NOQA
--- cupyx/scipy/stats/_stats_py.py
+++ cupyx/scipy/stats/_stats_py.py
@@ -1,5 +1,65 @@
 import cupy
 
+def gmean(a, axis=0, dtype=None, weights=None):
+    # This is just the gmean function from scipy, but adapted to use cp functions instead of numpy/scipy ones.
+    r"""Compute the weighted geometric mean along the specified axis.
+
+    The weighted geometric mean of the array :math:`a_i` associated to weights
+    :math:`w_i` is:
+
+    .. math::
+
+        \exp \left( \frac{ \sum_{i=1}^n w_i \ln a_i }{ \sum_{i=1}^n w_i }
+                   \right) \, ,
+
+    and, with equal weights, it gives:
+
+    .. math::
+
+        \sqrt[n]{ \prod_{i=1}^n a_i } \, .
+
+    Parameters
+    ----------
+    a : cupy.ndarray
+        Input array or object that can be converted to an array.
+    axis : int or None, optional
+        Axis along which the geometric mean is computed. Default is 0.
+        If None, compute over the whole array `a`.
+    dtype : dtype, optional
+        Type to which the input arrays are cast before the calculation is
+        performed.
+    weights : cupy.ndarray, optional
+        The `weights` array must be broadcastable to the same shape as `a`.
+        Default is None, which gives each value a weight of 1.0.
+
+    Returns
+    -------
+    gmean : ndarray
+        See `dtype` parameter above.
+
+    Notes
+    -----
+    The sample geometric mean is the exponential of the mean of the natural
+    logarithms of the observations.
+    Negative observations will produce NaNs in the output because the *natural*
+    logarithm (as opposed to the *complex* logarithm) is defined only for
+    non-negative reals.
+
+    References
+    ----------
+    .. [1] "Weighted Geometric Mean", *Wikipedia*,
+           https://en.wikipedia.org/wiki/Weighted_geometric_mean.
+    .. [2] Grossman, J., Grossman, M., Katz, R., "Averages: A New Approach",
+           Archimedes Foundation, 1983
+
+    """
+    
+    with cupy.errstate(divide='ignore'):
+        log_a = cupy.log(a)
+
+    return cupy.exp(cupy.mean(log_a, axis=axis, dtype=dtype, weights=weights))
+
+
 
 def _first(arr, axis):
     """Return arr[..., 0:1, ...] where 0:1 is in the `axis` position
