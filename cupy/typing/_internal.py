3c70945c94d733606b39db5910664150dde298fb
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-05-27 20:13:55 +0900

    :recycle: Drop shape

--- cupy/typing/_internal.py
+++ cupy/typing/_internal.py
@@ -5,7 +5,6 @@ from typing import Literal, Sequence, SupportsIndex, TypeVar
 from cupy._core import core
 
 _ScalarT = TypeVar("_ScalarT", bound=numpy.generic)
-_ShapeT_co = TypeVar("_ShapeT_co", bound=tuple[int, ...], covariant=True)
 _DTypeT_co = TypeVar("_DTypeT_co", bound=numpy.dtype, covariant=True)
 _DTypeT = TypeVar("_DTypeT", bound=numpy.dtype)
 _ShapeLike = SupportsIndex | Sequence[SupportsIndex]
--- cupy/typing/_types.py
+++ cupy/typing/_types.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from typing import Any
-
 from numpy.typing import ArrayLike, DTypeLike, NBitBase  # noqa: F401
 import numpy
 from cupy.typing._internal import _ScalarT
 
 from cupy._core import core
 
-NDArray = core.ndarray[Any, numpy.dtype[_ScalarT]]
+NDArray = core.ndarray[numpy.dtype[_ScalarT]]
