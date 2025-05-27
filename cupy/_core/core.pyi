9cfcddb54a2a0992ced808df3747767e4cd68fd7
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-05-27 21:02:20 +0900

    :construction: Annotate indexing routines

--- cupy/_core/core.pyi
+++ cupy/_core/core.pyi
@@ -1,14 +1,18 @@
 from collections.abc import Iterator
-from typing import Any, Generic
+from typing import Any, Generic, Literal
 
 from _typeshed import StrOrBytesPath, SupportsWrite
 from typing import overload
+import numpy
 from typing_extensions import Self
 
 from cupy import dtype
 from cupy._core.flags import Flags
-from cupy.typing._types import NDArray, DTypeLike
+from cupy.typing._types import NDArray, DTypeLike, ArrayLike
 from cupy.typing._internal import (
+    _ArrayLikeInt_co,
+    _SortSide,
+    _ArrayLikeInt,
     _DTypeT_co,
     _Index,
     _OrderKACF,
@@ -16,6 +20,7 @@ from cupy.typing._internal import (
     _ScalarT,
     _ShapeLike,
     _ArrayT,
+    _ModeKind,
 )
 
 class ndarray(Generic[_DTypeT_co]):
@@ -32,9 +37,9 @@ class ndarray(Generic[_DTypeT_co]):
     def itemsize(self) -> int: ...
     def nbytes(self) -> int: ...
     @property
-    def T(self) -> ndarray[_DTypeT_co]: ...
+    def T(self) -> Self: ...
     @property
-    def mT(self) -> ndarray[_DTypeT_co]: ...
+    def mT(self) -> Self: ...
     @property
     def flat(self: NDArray[_ScalarT]) -> Iterator[_ScalarT]: ...
     def item(self: NDArray[_ScalarT]) -> _ScalarT: ...
@@ -73,15 +78,103 @@ class ndarray(Generic[_DTypeT_co]):
     @overload
     def view(self, dtype: DTypeLike, type: type[_ArrayT]) -> _ArrayT: ...
     def fill(self, value: Any) -> None: ...
-    def reshape(
-        self, *shape: _Index, order: _OrderKACF = ...
-    ) -> ndarray[_DTypeT_co]: ...
-    def transpose(self, *axes: _Index) -> ndarray[_DTypeT_co]: ...
-    def swapaxes(
-        self, axis1: _Index, axis2: _Index
-    ) -> ndarray[_DTypeT_co]: ...
-    def flatten(self, order: _OrderKACF = ...) -> ndarray[_DTypeT_co]: ...
+    def reshape(self, *shape: _Index, order: _OrderKACF = ...) -> Self: ...
+    def transpose(self, *axes: _Index) -> Self: ...
+    def swapaxes(self, axis1: _Index, axis2: _Index) -> Self: ...
+    def flatten(self, order: _OrderKACF = ...) -> Self: ...
     def squeeze(
         self,
         axis: _Index | tuple[_Index, ...] | None = ...,
-    ) -> ndarray[_DTypeT_co]: ...
+    ) -> Self: ...
+    @overload
+    def take(
+        self: NDArray[_ScalarT],
+        indices: int | numpy.integer,
+        axis: _Index | None = ...,
+        out: None = ...,
+    ) -> _ScalarT: ...
+    @overload
+    def take(
+        self,
+        indices: _ArrayLikeInt_co,
+        axis: _Index | None = ...,
+        out: None = ...,
+    ) -> Self: ...
+    @overload
+    def take(
+        self,
+        indices: _ArrayLikeInt_co,
+        axis: _Index | None = ...,
+        *out: _ArrayT,
+    ) -> _ArrayT: ...
+    def put(
+        self,
+        indices: _ArrayLikeInt_co,
+        values: ArrayLike,
+        mode: _ModeKind = ...,
+    ) -> None: ...
+    @overload
+    def repeat(
+        self,
+        repeats: _ArrayLikeInt_co,
+        axis: None = ...,
+    ) -> Self: ...
+    @overload
+    def repeat(
+        self,
+        repeats: _ArrayLikeInt_co,
+        axis: _Index,
+    ) -> Self: ...
+    @overload
+    def choose(
+        self, choices: ArrayLike, out: None = ..., mode: _ModeKind = ...
+    ) -> NDArray[Any]: ...
+    @overload
+    def choose(
+        self, choices: ArrayLike, out: _ArrayT, mode: _ModeKind = ...
+    ) -> _ArrayT: ...
+    def sort(
+        self,
+        axis: _Index = ...,
+        kind: Literal["stable"] | None = ...,
+    ) -> None: ...
+    def argsort(
+        self,
+        axis: _Index | None = ...,
+        kind: Literal["stable"] | None = ...,
+    ) -> NDArray[Any]: ...
+    def partition(
+        self,
+        kth: _ArrayLikeInt,
+        axis: _Index = ...,
+    ) -> None: ...
+    def argpartition(
+        self,
+        kth: _ArrayLikeInt,
+        axis: _Index | None = ...,
+    ) -> NDArray[numpy.intp]: ...
+    def searchsorted(
+        self,  # >= 1D array
+        v: ArrayLike,
+        side: _SortSide = ...,
+        sorter: _ArrayLikeInt_co | None = ...,
+    ) -> NDArray[numpy.intp]: ...
+    def nonzero(self) -> tuple[NDArray[numpy.intp], ...]: ...
+    @overload
+    def compress(
+        self,
+        condition: _ArrayLikeInt_co,
+        axis: _Index | None = ...,
+        out: None = ...,
+    ) -> NDArray[Any]: ...
+    @overload
+    def compress(
+        self,
+        condition: _ArrayLikeInt_co,
+        axis: _Index | None = ...,
+        *,
+        out: _ArrayT,
+    ) -> _ArrayT: ...
+    def diagonal(
+        self, offset: _Index = ..., axis1: _Index = ..., axis2: _Index = ...
+    ) -> Self: ...
--- cupy/typing/_internal.py
+++ cupy/typing/_internal.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
 import numpy
-from typing import Literal, Sequence, SupportsIndex, TypeVar
+from typing import Any, Literal, Sequence, SupportsIndex, TypeVar
 from cupy._core import core
 
+# MEMO
+# Protocol types are currently not included for simplicity
+
+# Miscellaneous types
+_Index = int  # MEMO: SupportsIndex in numpy
 _ScalarT = TypeVar("_ScalarT", bound=numpy.generic)
 _DTypeT_co = TypeVar("_DTypeT_co", bound=numpy.dtype, covariant=True)
 _DTypeT = TypeVar("_DTypeT", bound=numpy.dtype)
@@ -11,5 +16,9 @@ _ShapeLike = SupportsIndex | Sequence[SupportsIndex]
 _OrderKACF = Literal["K", "A", "C", "F"] | None
 _DTypeLike = type[_ScalarT] | numpy.dtype[_ScalarT]
 _ArrayT = TypeVar("_ArrayT", bound=core.ndarray)
+_ModeKind = Literal["raise", "wrap", "clip"]
+_SortSide = Literal["left", "right"]
 
-_Index = int  # MEMO: May be SupportsIndex
+# Anything castable to ndarray with specified dtype (work in progress)
+_ArrayLikeInt_co = Any
+_ArrayLikeInt = Any
