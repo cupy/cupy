c206f4f67f8f8eccd3d84ac29f0bd215dd4ee359
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-05-29 20:10:37 +0900

    :recycle: Guard _typeshed

--- cupy/typing/_proto.py
+++ cupy/typing/_proto.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
-from _typeshed import SupportsFlush
-from typing import Protocol, SupportsIndex
+from typing import TYPE_CHECKING, Protocol, SupportsIndex
+
+if TYPE_CHECKING:
+    from _typeshed import SupportsFlush
+else:
+    SupportsFlush = object
 
 class _SupportsFileMethods(SupportsFlush, Protocol):
     def fileno(self) -> SupportsIndex: ...
