e6f6d6f5cc44a108e0f5863c76035d34fef666b5
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2025-05-30 01:44:54 +0000

    return error for stub functions (part of #5393)

--- cupy_backends/cuda/cupy_cusolver.h
+++ cupy_backends/cuda/cupy_cusolver.h
@@ -10,11 +10,11 @@ extern "C" {
 #if CUSOLVER_VERSION < 11700
 // Functions added in cuSOLVER 11.7 (CUDA 12.6.2)
 cusolverStatus_t cusolverDnXgeev_bufferSize(...) {
-    return CUSOLVER_STATUS_SUCCESS;
+    return CUSOLVER_STATUS_NOT_SUPPORTED;
 }
 
 cusolverStatus_t cusolverDnXgeev(...) {
-    return CUSOLVER_STATUS_SUCCESS;
+    return CUSOLVER_STATUS_NOT_SUPPORTED;
 }
 #endif // #if CUSOLVER_VERSION < 11700
 
