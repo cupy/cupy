58d209ff0783e61ff51af1f9e1ac81b6201fed17
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-02 17:03:12 +0900

    :coffin: Remove NBitBase
    Removed in numpy v2.3

--- cupy/typing/__init__.py
+++ cupy/typing/__init__.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 from numpy.typing import DTypeLike as DTypeLike
-from numpy.typing import NBitBase as NBitBase
 
 from cupy.typing._array import ArrayLike, NDArray
 
-__all__ = ["ArrayLike", "DTypeLike", "NBitBase", "NDArray"]
+__all__ = ["ArrayLike", "DTypeLike", "NDArray"]
