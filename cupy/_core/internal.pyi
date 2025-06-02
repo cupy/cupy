6357d8f22e46b5a72cc3585605c94026773e08b1
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-02 21:18:42 +0900

    :construction: Partially annotate internal.pyx

---
+++ cupy/_core/internal.pyi
@@ -0,0 +1,12 @@
+# TODO: Add entries when necessary
+
+from typing import Any
+import cupy
+from cupy.typing._array import NDArray
+
+def _get_strides_for_order_K(
+    x: NDArray[Any], dtype: cupy.dtype, shape: list[int] | None = ...
+) -> list[int]: ...
+def _update_order_char(
+    is_c_contiguous: bool, is_f_contiguous: bool, order_char: int
+) -> int: ...
