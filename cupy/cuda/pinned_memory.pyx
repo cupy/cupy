f5bea07716071ccedd4a5300ce0f528833abe08c
Sebastian Berg <sebastianb@nvidia.com>
2025-07-29 05:37:18 -0700

    Use the new pointerGetMemoryType in the one other place where it works

--- cupy/cuda/pinned_memory.pyx
+++ cupy/cuda/pinned_memory.pyx
@@ -349,5 +349,4 @@ cdef class PinnedMemoryPool:
 
 
 cpdef bint is_memory_pinned(intptr_t data) except*:
-    cdef runtime.PointerAttributes attrs = runtime.pointerGetAttributes(data)
-    return (attrs.type == runtime.memoryTypeHost)
+    return (runtime.pointerGetMemoryType(data) == runtime.memoryTypeHost)
