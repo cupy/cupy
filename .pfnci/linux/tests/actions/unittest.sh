99e51224668fb2369b615887702d5996d48840fa
Vyas Ramasubramani <vyasr@nvidia.com>
2025-12-02 19:53:29 +0000

    Fix condition

--- .pfnci/linux/tests/actions/unittest.sh
+++ .pfnci/linux/tests/actions/unittest.sh
@@ -45,4 +45,4 @@ case ${test_retval} in
 esac
 
 # Validate that importing cupy does not import cupyx
-python3 -Ximporttime -c "import cupy" |& grep -q cupyx || (echo "cupyx was imported by cupy"  && exit 1)
+python3 -Ximporttime -c "import cupy" |& grep -q cupyx && (echo "cupyx was imported by cupy"  && exit 1)
