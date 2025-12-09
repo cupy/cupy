e6238a66cd20c38f983072db1fecde2bfba99496
Aman Srivastava <amansri345@gmail.com>
2025-12-09 12:58:18 +0530

    Apply autopep8 formatting fixes

--- cupyx/__init__.py
+++ cupyx/__init__.py
@@ -12,8 +12,6 @@ from cupyx import time  # NOQA
 from cupyx import scipy  # NOQA
 from cupyx import optimizing  # NOQA
 
-from cupyx.tools._gpu_timeit import load_ipython_extension  # noqa: F401
-
 from cupyx._ufunc_config import errstate  # NOQA
 from cupyx._ufunc_config import geterr  # NOQA
 from cupyx._ufunc_config import seterr  # NOQA
@@ -34,4 +32,19 @@ def __getattr__(key):
         return cupyx.lapack
 
     raise AttributeError(
-        "module '{}' has no attribute '{}'".format(__name__, key))
+        "module '{}' has no attribute '{}'".format(__name__, key)
+    )
+
+# Auto-register IPython magics only when running in IPython/Jupyter
+
+
+try:
+    from IPython import get_ipython
+
+    ip = get_ipython()
+    if ip is not None:
+        from cupyx.profiler._gpu_timeit import try_register_ipython_magic
+        try_register_ipython_magic()
+except Exception:
+    # Must never fail in a normal Python environment
+    pass
