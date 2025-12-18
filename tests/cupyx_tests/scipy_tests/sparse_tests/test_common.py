0232b79ac2a9846d5029f13a1ba7ec9d7ab22285
BarnikRB <barnikroy@DESKTOP-GJJSD3I.localdomain>
2025-12-18 19:16:28 +0200

    Modify the sparse testing code

--- tests/cupyx_tests/scipy_tests/sparse_tests/test_common.py
+++ tests/cupyx_tests/scipy_tests/sparse_tests/test_common.py
@@ -1,52 +1,26 @@
 from __future__ import annotations
 
 import cupy as cp
+from cupy import testing
 import cupyx.scipy.sparse as sp
 
-# dummy csr
+@testing.parameterize(*testing.product({
+    'format': ['csr', 'csc', 'coo', 'dia'],
+}))
+class TestSparseDevice:
 
+    def test_sparse_device(self):
+        data = cp.array([1, 2, 3], dtype=cp.float32)
 
-def make_csr():
-    data = cp.array([1, 2, 3], dtype=cp.float32)
-    row = cp.array([0, 1, 2], dtype=cp.int32)
-    col = cp.array([0, 1, 2], dtype=cp.int32)
-    return sp.csr_matrix((data, (row, col)), shape=(3, 3))
+        if self.format == 'dia':
+            A = sp.dia_matrix(
+                (data[None, :], cp.array([0], dtype=cp.int32)),
+                shape=(3, 3)
+            )
+        else:
+            idx = cp.array([0, 1, 2], dtype=cp.int32)
+            mat_cls = getattr(sp, f'{self.format}_matrix')
+            A = mat_cls((data, (idx, idx)), shape=(3, 3))
 
-# dummy csc
-
-
-def make_csc():
-    data = cp.array([1, 2, 3], dtype=cp.float32)
-    row = cp.array([0, 1, 2], dtype=cp.int32)
-    col = cp.array([0, 1, 2], dtype=cp.int32)
-    return sp.csc_matrix((data, (row, col)), shape=(3, 3))
-
-# dummy coo
-
-
-def make_coo():
-    data = cp.array([1, 2, 3], dtype=cp.float32)
-    row = cp.array([0, 1, 2], dtype=cp.int32)
-    col = cp.array([0, 1, 2], dtype=cp.int32)
-    return sp.coo_matrix((data, (row, col)), shape=(3, 3))
-
-# dummy dia
-
-
-def make_dia():
-    dia_data = cp.array([[1, 2, 3]], dtype=cp.float32)
-    offsets = cp.array([0], dtype=cp.int32)
-    return sp.dia_matrix((dia_data, offsets), shape=(3, 3))
-
-
-def test_sparse_device():
-    makers = [make_csr, make_csc, make_coo, make_dia]
-
-    for maker in makers:
-        A = maker()
-
-        # device must exist and be a cupy.cuda.Device
         assert isinstance(A.device, cp.cuda.Device)
-
-        # device must match the underlying data device
         assert A.device == A.data.device
