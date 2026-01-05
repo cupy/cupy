154301019f6b57465f84cdd410fd50511356b8ca
SS <66886825+EarlMilktea@users.noreply.github.com>
2026-01-05 19:48:18 +0900

    :poop: Resolve import cycle
    Cannot use cupy.dtype directly

--- cupy/_core/internal.pyi
+++ cupy/_core/internal.pyi
@@ -2,11 +2,12 @@
 
 from typing import Any
 
-import cupy
-from cupy.typing import NDArray
+from numpy import dtype # MEMO: mypy complains if imported from cupy
+
+from cupy._core.core import NDArray
 
 def _get_strides_for_order_K(
-    x: NDArray[Any], dtype: cupy.dtype, shape: list[int] | None = ...
+    x: NDArray[Any], dtype: dtype, shape: list[int] | None = ...
 ) -> list[int]: ...
 def _update_order_char(
     is_c_contiguous: bool, is_f_contiguous: bool, order_char: int
