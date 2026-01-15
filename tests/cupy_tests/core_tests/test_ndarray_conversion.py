48177bcbb0006e1897e4f39b84c7d38a1340a9f0
Megha <st189947@stud.uni-stuttgart.de>
2026-01-15 02:09:53 +0100

    test: migrate tests from unittest to pytest

--- tests/cupy_tests/core_tests/test_ndarray_conversion.py
+++ tests/cupy_tests/core_tests/test_ndarray_conversion.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import unittest
-
 import numpy
 import pytest
 
@@ -10,52 +8,41 @@ from cupy.cuda import runtime
 from cupy import testing
 
 
-@testing.parameterize(
-    {'shape': ()},
-    {'shape': (1,)},
-    {'shape': (1, 1, 1)},
+@pytest.mark.parametrize('shape', [(), (1,), (1, 1, 1)])
+@testing.for_all_dtypes()
+@testing.numpy_cupy_equal()
+def test_ndarray_item(shape, xp, dtype):
+    a = xp.full(shape, 3, dtype)
+    return a.item()
+
+
+@pytest.mark.parametrize('shape', [(0,), (2, 3), (1, 0, 1)])
+def test_ndarray_item_raise(shape):
+    for xp in (numpy, cupy):
+        a = testing.shaped_arange(shape, xp, xp.float32)
+        with pytest.raises(ValueError):
+            a.item()
+
+
+@pytest.mark.parametrize(
+    ('shape', 'order'),
+    [
+        ((), None),
+        ((1,), None),
+        ((2, 3), None),
+        ((2, 3), 'C'),
+        ((2, 3), 'F'),
+    ],
 )
-class TestNdarrayItem(unittest.TestCase):
-
-    @testing.for_all_dtypes()
-    @testing.numpy_cupy_equal()
-    def test_item(self, xp, dtype):
-        a = xp.full(self.shape, 3, dtype)
-        return a.item()
-
-
-@testing.parameterize(
-    {'shape': (0,)},
-    {'shape': (2, 3)},
-    {'shape': (1, 0, 1)},
-)
-class TestNdarrayItemRaise(unittest.TestCase):
-
-    def test_item(self):
-        for xp in (numpy, cupy):
-            a = testing.shaped_arange(self.shape, xp, xp.float32)
-            with pytest.raises(ValueError):
-                a.item()
-
-
-@testing.parameterize(
-    {'shape': ()},
-    {'shape': (1,)},
-    {'shape': (2, 3)},
-    {'shape': (2, 3), 'order': 'C'},
-    {'shape': (2, 3), 'order': 'F'},
-)
-class TestNdarrayToBytes(unittest.TestCase):
-
-    @testing.for_all_dtypes()
-    @testing.numpy_cupy_equal()
-    def test_item(self, xp, dtype):
-        if (runtime.is_hip and
-            (self.shape == (1,) or
-             (self.shape == (2, 3) and not hasattr(self, 'order')))):
-            pytest.xfail('ROCm/HIP may have a bug')
-        a = testing.shaped_arange(self.shape, xp, dtype)
-        if hasattr(self, 'order'):
-            return a.tobytes(self.order)
-        else:
-            return a.tobytes()
+@testing.for_all_dtypes()
+@testing.numpy_cupy_equal()
+def test_item(shape, order, xp, dtype):
+    if (runtime.is_hip and
+            (shape == (1,) or
+             (shape == (2, 3) and order is None))):
+        pytest.xfail('ROCm/HIP may have a bug')
+    a = testing.shaped_arange(shape, xp, dtype)
+    if order is not None:
+        return a.tobytes(order)
+    else:
+        return a.tobytes()
