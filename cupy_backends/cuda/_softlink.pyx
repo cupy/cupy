e22092ea9f8d8122a9daaf945cd700f06b2fe5bf
Kenichi Maehashi <939877+kmaehashi@users.noreply.github.com>
2026-02-13 20:00:37 +0900

    Merge pull request #9554 from leofang/more_pathfinder
    Improve pathfinder support

--- cupy_backends/cuda/_softlink.pyx
+++ cupy_backends/cuda/_softlink.pyx
@@ -1,4 +1,5 @@
 import ctypes
+import os
 import warnings
 
 from libc.stdint cimport intptr_t
@@ -11,16 +12,32 @@ def _log(msg: str) -> None:
 
 
 cdef class SoftLink:
-    def __init__(self, object libname, str prefix, *, bint mandatory=False):
+    def __init__(
+            self, str libname, str prefix, *, bint mandatory=False, handle=0):
         self.error = None
         self.prefix = prefix
         self._libname = libname
         self._cdll = None
-        if libname is None:
+        if libname is None and handle == 0:
             # Stub build or CUDA/HIP only library.
             self.error = RuntimeError(
                 'The library is unavailable in the current platform.')
+        elif libname is None and handle > 0:
+            if os.name == 'nt':
+                # WAR: Until python/cpython#136878, on Windows the name could
+                # not be None because it was (mistakenly) assumed iterable. We
+                # give a dummy name here to get pass the constructor.
+                self._cdll = ctypes.CDLL('', handle=handle)
+                self._cdll._name = None
+            else:
+                self._cdll = ctypes.CDLL(None, handle=handle)
+                # Note: It seems CDLL does not honor handle in certain Python
+                # 3.13.x & 3.14.x patch releases, so we overwrite it to be
+                # safe (xref: python/cpython#143304).
+                self._cdll._handle = handle
+            _log(f'Library already loaded (prefix={prefix}, handle={handle})')
         else:
+            # Ignore handle.
             try:
                 self._cdll = ctypes.CDLL(libname)
                 _log(f'Library "{libname}" loaded')
