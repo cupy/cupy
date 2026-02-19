2a81f1c1f41446ef9c1177b6a0fa7f9ad8458445
Sebastian Berg <sebastianb@nvidia.com>
2026-02-19 22:38:34 +0100

    undo weird hack and try setbuf on docker, maybe that does "enough"

--- .pfnci/linux/main-flexci.sh
+++ .pfnci/linux/main-flexci.sh
@@ -28,9 +28,8 @@ STAGES="cache_get build test"
 if [[ "${TARGET}" == "benchmark" ]]; then
     STAGES="cache_get build benchmark"
 fi
-BENCHMARK_DIR=/tmp/benchmark CACHE_DIR=/tmp/cupy_cache PULL_REQUEST="${pull_req}" "$(dirname ${0})/run.sh" "${TARGET}" "${STAGES}" > "${LOG_FILE}" 2>&1
-test_retval=$?
-echo "${LOG_FILE}"
+BENCHMARK_DIR=/tmp/benchmark CACHE_DIR=/tmp/cupy_cache PULL_REQUEST="${pull_req}" "$(dirname ${0})/run.sh" "${TARGET}" "${STAGES}" 2>&1 | tee "${LOG_FILE}"
+test_retval=${PIPESTATUS[0]}
 
 echo "****************************************************************************************************"
 echo "Build & Test: Exit with status ${test_retval}"
@@ -38,9 +37,8 @@ echo "Build & Test: Exit with status ${test_retval}"
 if [[ "${pull_req}" == "" ]]; then
     # Upload cache when testing a branch, even when test failed.
     echo "Uploading cache and Docker image..."
-    CACHE_DIR=/tmp/cupy_cache PULL_REQUEST="${pull_req}" "$(dirname ${0})/run.sh" "${TARGET}" cache_put push >> "${LOG_FILE}" 2>&1
-    echo "Upload: Exit with status $?"
-    echo "${LOG_FILE}"
+    CACHE_DIR=/tmp/cupy_cache PULL_REQUEST="${pull_req}" "$(dirname ${0})/run.sh" "${TARGET}" cache_put push | tee --append "${LOG_FILE}"
+    echo "Upload: Exit with status ${PIPESTATUS[0]}"
 
     # Notify.
     if [[ ${test_retval} != 0 ]]; then
