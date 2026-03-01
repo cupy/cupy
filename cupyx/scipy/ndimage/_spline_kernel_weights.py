aa0615cce334dcaff6edae0838bcc0a42145c085
Gregory R. Lee <grelee@nvidia.com>
2026-03-01 12:07:14 -0500

    use correct float precision for spline interpolation floating point literals

--- cupyx/scipy/ndimage/_spline_kernel_weights.py
+++ cupyx/scipy/ndimage/_spline_kernel_weights.py
@@ -15,61 +15,66 @@ spline_weights_inline = {}
 # path in _interp_kernels.py). I think that existing code is a bit more
 # efficient.
 spline_weights_inline[1] = '''
-wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + 0.5);
-weights_{j}[0] = 1.0 - wx;
+wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + (W)0.5);
+weights_{j}[0] = (W)1.0 - wx;
 weights_{j}[1] = wx;
 '''
 
 spline_weights_inline[2] = '''
-wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + 0.5);
-weights_{j}[1] = 0.75 - wx * wx;
-wy = 0.5 - wx;
-weights_{j}[0] = 0.5 * wy * wy;
-weights_{j}[2] = 1.0 - weights_{j}[0] - weights_{j}[1];
+wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + (W)0.5);
+weights_{j}[1] = (W)0.75 - wx * wx;
+wy = (W)0.5 - wx;
+weights_{j}[0] = (W)0.5 * wy * wy;
+weights_{j}[2] = (W)1.0 - weights_{j}[0] - weights_{j}[1];
 '''
 
 spline_weights_inline[3] = '''
-wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + 0.5);
-wy = 1.0 - wx;
-weights_{j}[1] = (wx * wx * (wx - 2.0) * 3.0 + 4.0) / 6.0;
-weights_{j}[2] = (wy * wy * (wy - 2.0) * 3.0 + 4.0) / 6.0;
-weights_{j}[0] = wy * wy * wy / 6.0;
-weights_{j}[3] = 1.0 - weights_{j}[0] - weights_{j}[1] - weights_{j}[2];
+wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + (W)0.5);
+wy = (W)1.0 - wx;
+weights_{j}[1] = (wx * wx * (wx - (W)2.0) * (W)3.0 + (W)4.0) / (W)6.0;
+weights_{j}[2] = (wy * wy * (wy - (W)2.0) * (W)3.0 + (W)4.0) / (W)6.0;
+weights_{j}[0] = wy * wy * wy / (W)6.0;
+weights_{j}[3] = (W)1.0 - weights_{j}[0] - weights_{j}[1] - weights_{j}[2];
 '''
 
 spline_weights_inline[4] = '''
 wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + 0.5);
 wy = wx * wx;
-weights_{j}[2] = wy * (wy * 0.25 - 0.625) + 115.0 / 192.0;
-wy = 1.0 + wx;
-weights_{j}[1] = wy * (wy * (wy * (5.0 - wy) / 6.0 - 1.25) + 5.0 / 24.0) +
-             55.0 / 96.0;
-wy = 1.0 - wx;
-weights_{j}[3] = wy * (wy * (wy * (5.0 - wy) / 6.0 - 1.25) + 5.0 / 24.0) +
-             55.0 / 96.0;
-wy = 0.5 - wx;
+weights_{j}[2] = wy * (wy * (W)0.25 - (W)0.625) + (W)115.0 / (W)192.0;
+wy = (W)1.0 + wx;
+weights_{j}[1] =
+    wy * (wy * (wy * ((W)5.0 - wy) / (W)6.0 - (W)1.25) + (W)5.0 / (W)24.0) +
+    (W)55.0 / (W)96.0;
+wy = (W)1.0 - wx;
+weights_{j}[3] =
+    wy * (wy * (wy * ((W)5.0 - wy) / (W)6.0 - (W)1.25) + (W)5.0 / (W)24.0) +
+    (W)55.0 / (W)96.0;
+wy = (W)0.5 - wx;
 wy = wy * wy;
-weights_{j}[0] = wy * wy / 24.0;
-weights_{j}[4] = 1.0 - weights_{j}[0] - weights_{j}[1]
-                     - weights_{j}[2] - weights_{j}[3];
+weights_{j}[0] = wy * wy / (W)24.0;
+weights_{j}[4] = (W)1.0 - weights_{j}[0] - weights_{j}[1]
+                        - weights_{j}[2] - weights_{j}[3];
 '''
 
 spline_weights_inline[5] = '''
-wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + 0.5);
+wx = c_{j} - floor({order} & 1 ? c_{j} : c_{j} + (W)0.5);
 wy = wx * wx;
-weights_{j}[2] = wy * (wy * (0.25 - wx / 12.0) - 0.5) + 0.55;
-wy = 1.0 - wx;
+weights_{j}[2] = wy * (wy * ((W)0.25 - wx / (W)12.0) - (W)0.5) + (W)0.55;
+wy = (W)1.0 - wx;
 wy = wy * wy;
-weights_{j}[3] = wy * (wy * (0.25 - (1.0 - wx) / 12.0) - 0.5) + 0.55;
-wy = wx + 1.0;
-weights_{j}[1] = wy * (wy * (wy * (wy * (wy / 24.0 - 0.375) + 1.25) - 1.75)
-                   + 0.625) + 0.425;
-wy = 2.0 - wx;
-weights_{j}[4] = wy * (wy * (wy * (wy * (wy / 24.0 - 0.375) + 1.25) - 1.75)
-                  + 0.625) + 0.425;
-wy = 1.0 - wx;
+weights_{j}[3] =
+    wy * (wy * ((W)0.25 - ((W)1.0 - wx) / (W)12.0) - (W)0.5) + (W)0.55;
+wy = wx + (W)1.0;
+weights_{j}[1] =
+    wy * (wy * (wy * (wy * (wy / (W)24.0 - (W)0.375) + (W)1.25) - (W)1.75)
+   + (W)0.625) + (W)0.425;
+wy = (W)2.0 - wx;
+weights_{j}[4] =
+    wy * (wy * (wy * (wy * (wy / (W)24.0 - (W)0.375) + (W)1.25) - (W)1.75)
+    + (W)0.625) + (W)0.425;
+wy = (W)1.0 - wx;
 wy = wy * wy;
-weights_{j}[0] = (1.0 - wx) * wy * wy / 120.0;
-weights_{j}[5] = 1.0 - weights_{j}[0] - weights_{j}[1] - weights_{j}[2]
-                     - weights_{j}[3] - weights_{j}[4];
+weights_{j}[0] = ((W)1.0 - wx) * wy * wy / 120.0;
+weights_{j}[5] = (W)1.0 - weights_{j}[0] - weights_{j}[1] - weights_{j}[2]
+                        - weights_{j}[3] - weights_{j}[4];
 '''
