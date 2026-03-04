b14f19da044be3b783a3790a16029010c168d0f6
Punit Chandel <punitchandel8@gmail.com>
2026-03-04 16:54:07 +0530

    Fix NumPy signature mismatch for cupy.argmax

--- cupy/_sorting/search.py
+++ cupy/_sorting/search.py
@@ -8,35 +8,20 @@ from cupy import _util
 from cupy._core import _routines_indexing as _indexing
 from cupy._core import _routines_statistics as _statistics
 
-
-def argmax(a, axis=None, dtype=None, out=None, keepdims=False):
+def argmax(a, axis=None, out=None, keepdims=False):
     """Returns the indices of the maximum along an axis.
 
     Args:
         a (cupy.ndarray): Array to take argmax.
-        axis (int): Along which axis to find the maximum. ``a`` is flattened by
-            default.
-        dtype: Data type specifier.
+        axis (int): Along which axis to find the maximum. ``a`` is flattened by default.
         out (cupy.ndarray): Output array.
-        keepdims (bool): If ``True``, the axis ``axis`` is preserved as an axis
-            of length one.
+        keepdims (bool): If ``True``, the axis ``axis`` is preserved as an axis of length one.
 
     Returns:
         cupy.ndarray: The indices of the maximum of ``a`` along an axis.
-
-    .. note::
-       ``dtype`` and ``keepdim`` arguments are specific to CuPy. They are
-       not in NumPy.
-
-    .. note::
-       ``axis`` argument accepts a tuple of ints, but this is specific to
-       CuPy. NumPy does not support it.
-
-    .. seealso:: :func:`numpy.argmax`
-
     """
-    # TODO(okuta): check type
-    return a.argmax(axis=axis, dtype=dtype, out=out, keepdims=keepdims)
+
+    return a.argmax(axis=axis, out=out, keepdims=keepdims)
 
 
 def nanargmax(a, axis=None, dtype=None, out=None, keepdims=False):
