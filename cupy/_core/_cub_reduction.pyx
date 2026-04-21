f021f2d34399e9aa09aa8eceda4680f8d72f90c8
Sebastian Berg <sebastianb@nvidia.com>
2026-04-21 15:54:46 +0200

    MAINT: Clean up cub_params a bit (and improve memoization possibly)

--- cupy/_core/_cub_reduction.pyx
+++ cupy/_core/_cub_reduction.pyx
@@ -246,8 +246,7 @@ def _SimpleCubReductionKernel_get_cached_function(
         map_expr, reduce_expr, post_map_expr, reduce_type,
         params, arginfos, _kernel._TypeMap type_map,
         name, block_size, identity, preamble,
-        options, cub_params):
-    items_per_thread = cub_params[0]
+        options, items_per_thread):
     name = name.replace('cupy_', 'cupy_cub_')
     name = name.replace('cupyx_', 'cupyx_cub_')
     return _create_cub_reduction_function(
@@ -435,7 +434,6 @@ cdef inline void _cub_two_pass_launch(
     cdef memory.MemoryPointer memptr
     cdef str post_map_expr1, post_map_expr2, f
     cdef list inout_args
-    cdef tuple cub_params
     cdef size_t gridx, blockx
     cdef _ndarray_base in_arr
 
@@ -454,7 +452,6 @@ cdef inline void _cub_two_pass_launch(
     inout_args = [in_args[0], out_args[0],
                   _cub_convert_to_c_scalar(segment_size, contiguous_size),
                   _cub_convert_to_c_scalar(segment_size, segment_size)]
-    cub_params = (items_per_thread,)
 
     if 'mean' in name:
         post_map_expr1 = post_map_expr.replace('_in_ind.size()', '1.0')
@@ -480,7 +477,7 @@ cdef inline void _cub_two_pass_launch(
         _kernel._get_arginfos(inout_args),
         type_map,
         name, block_size, identity, preamble,
-        ('-DFIRST_PASS=1',), cub_params)
+        ('-DFIRST_PASS=1',), items_per_thread)
 
     # Kernel arguments passed to the __global__ function.
     gridx = <size_t>(out_block_num * block_size)
@@ -514,7 +511,7 @@ cdef inline void _cub_two_pass_launch(
         _kernel._get_arginfos(inout_args),
         type_map,
         name, block_size, identity, preamble,
-        ('-DSECOND_PASS=1',), cub_params)
+        ('-DSECOND_PASS=1',), items_per_thread)
 
     # Kernel arguments passed to the __global__ function.
     gridx = <size_t>(out_block_num * block_size)
@@ -557,7 +554,7 @@ cdef inline void _launch_cub(
             map_expr, reduce_expr, post_map_expr, reduce_type,
             params, arginfos, type_map,
             self.name, block_size, self.identity, self.preamble,
-            (), cub_params)
+            (), items_per_thread)
 
         func.linear_launch(
             out_block_num * block_size, inout_args, 0, block_size, stream)
