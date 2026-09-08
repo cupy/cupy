469cef9d6884cc2b2e39efdf68627263a3947bc7
Sebastian Berg <sebastianb@nvidia.com>
2026-09-08 11:52:07 +0200

    Add 180 degree to test (since this was mentioned in issue)

--- tests/cupyx_tests/scipy_tests/ndimage_tests/test_interpolation.py
+++ tests/cupyx_tests/scipy_tests/ndimage_tests/test_interpolation.py
@@ -492,7 +492,7 @@ class TestAffineTransformOpenCV:
 @testing.parameterize(*(
     testing.product({
         # Note that certain angles can lead to larger differences.
-        'angle': [-10, 30, 90, 195, 1000],
+        'angle': [-10, 30, 90, 180, 195, 1000],
         'axes': [(1, 0)],
         'reshape': [False, True],
         # Avoid float64 output so rtol dict keyed by result dtype picks up
